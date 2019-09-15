ebcab7163318c962a3a49327ceba1fb6510cf1d2
Talia Ringer <tringer@cs.washington.edu>
2019-09-15 12:59:02 -0700

    user 1, session 4, part 0

---
+++ diffs/1/user-1-session-4.v
@@ -0,0 +1,54 @@
+Ltac
+ safe_intuition_then t :=
+  repeat
+   match goal with
+   | H:_ /\ _ |- _ => destruct H
+   | H:?P -> _
+     |- _ => lazymatch type of P with
+             | Prop => specialize (H _)
+             | _ => fail
+             end
+   | _ => progress t
+   end.
+Tactic Notation "safe_intuition" := (safe_intuition_then ltac:(auto)).
+Tactic Notation "safe_intuition" tactic(t) := (safe_intuition_then t).
+Ltac
+ propositional :=
+  repeat
+   match goal with
+   | |- forall _, _ => intros
+   | H:_ /\ _ |- _ => destruct H
+   | H:_ <-> _ |- _ => destruct H
+   | H:False |- _ => solve [ destruct H ]
+   | H:True |- _ => clear H
+   | H:?P -> _, H':?P
+     |- _ => match type of P with
+             | Prop => specialize (H H')
+             end
+   | H:forall x, x = _ -> _ |- _ => specialize (H _ eq_refl)
+   | H:forall x, _ = x -> _ |- _ => specialize (H _ eq_refl)
+   | H:exists varname, _
+     |- _ => let newvar := fresh varname in
+             destruct H as [newvar ?]
+   | H:?P |- ?P => exact H
+   | _ => progress subst
+   end.
+Ltac
+ prove_hyps H :=
+  match type of H with
+  | ?P -> ?Q =>
+      let HP := fresh in
+      assert (HP : P); [  | specialize (H HP); clear HP; prove_hyps H ]
+  | _ => idtac
+  end.
+Ltac destruct_ands := repeat match goal with
+                             | H:_ /\ _ |- _ => destruct H
+                             end.
+Ltac
+ deex :=
+  match goal with
+  | H:exists varname, _
+    |- _ =>
+        let newvar := fresh varname in
+        destruct H as [newvar ?]; destruct_ands; subst
+  end.
