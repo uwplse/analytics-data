0ec92fcade628539ff2a17d7c213b343a285f86e
Talia Ringer <tringer@cs.washington.edu>
2019-09-15 13:01:12 -0700

    user 2, session 4, part 3

--- diffs/2/user-2-session-4.v
+++ diffs/2/user-2-session-4.v
@@ -8,19 +8,4 @@ Timeout 1 Print LoadPath.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 98.
-Test Debug Analytics.
-Require Import Coq.Strings.String.
-Redirect "/tmp/coqsRY5JP" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
 Unset Silent.
-Set Printing Width 98.
-Unset Silent.
-Set Printing Width 98.
-Inductive term :=
-  | Nil : term
-  | Ident : string -> term
-  | Cons : term -> term -> term
-  | App : term -> term -> term.
-Redirect "/tmp/coqn5gNxo" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
