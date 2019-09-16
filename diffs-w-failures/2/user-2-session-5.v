a2ce5f5cbd620f867740c3d1440bdd52f13c85c9
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:28:43 -0700

    user 2, session 5, part 34

--- diffs-w-failures/2/user-2-session-5.v
+++ diffs-w-failures/2/user-2-session-5.v
@@ -82,6 +82,9 @@ Redirect "/tmp/coqhUNJDd" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Open Scope coucou_scope.
+Unset Silent.
+Set Printing Width 98.
+Open Scope string_scope.
 Check
   [<Nil <Nil (Ident "hi")>> (Cons (Ident "1") (Ident "2")) (Ident "a")
   {(Ident "myfun") (Ident "somArg")}].
