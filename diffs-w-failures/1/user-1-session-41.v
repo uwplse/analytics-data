132952a012d3374be1a42753d470de829592dd6f
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:19:03 -0700

    user 1, session 41, part 90

--- diffs-w-failures/1/user-1-session-41.v
+++ diffs-w-failures/1/user-1-session-41.v
@@ -687,6 +687,17 @@ Set Printing Width 94.
 Show.
 Unset Silent.
 Set Diffs "off".
-Set Printing Width 94.
 Unset Silent.
 Set Diffs "off".
+Set Printing Width 94.
+Show.
+(intros; inversion H0; subst; eauto).
+all: (try (apply singleton_eq in H1; congruence)).
+all: (try specialize (H1 _ (In_singleton _ _))).
+all:
+ (repeat
+   match goal with
+   | H:exists _, _ |- _ => destruct H
+   | H:_ \/ _ |- _ => inversion H; clear H
+   end).
+all: (try congruence).
