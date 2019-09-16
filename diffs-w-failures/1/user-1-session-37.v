505c577b12cc931d24525531f67cb8f0ac37b7a2
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:18:47 -0700

    user 1, session 37, part 0

---
+++ diffs-w-failures/1/user-1-session-37.v
@@ -0,0 +1,10 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 70.
+Set Silent.
+Set Bullet Behavior "Strict Subproofs".
+Require Import Coq.Sets.Powerset_facts.
+Require Import library.
