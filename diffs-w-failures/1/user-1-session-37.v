c358447432da03e5f44602a2fade6065b6e6348c
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:18:47 -0700

    user 1, session 37, part 43

--- diffs-w-failures/1/user-1-session-37.v
+++ diffs-w-failures/1/user-1-session-37.v
@@ -352,16 +352,11 @@ Fixpoint Gamma (G : GT) : SetST :=
       exists l',
         X = SRec l' /\
         Forall2 (fun (S' : option ST) G' => True) l'
-          (option_map Gamma l)
+          (option_map
+             (fun pair =>
+              match pair with
+              | (R, G) => Gamma G
+              | (O, G) => Empty_set _
+              end) l)
   | _ => Empty_set _
-  end
-with GammaPair (x : option (Ann * GT)) : SetST :=
-  match x as m return (x = m -> SetST) with
-  | None => fun _ => Empty_set _
-  | Some P =>
-      fun H =>
-      match P with
-      | (R, G) => Gamma G
-      | (O, G) => Empty_set _
-      end
-  end eq_refl.
+  end.
