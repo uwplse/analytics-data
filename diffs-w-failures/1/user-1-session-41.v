04c21a158ffe7a26805325835d19db9e63775725
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:19:03 -0700

    user 1, session 41, part 54

--- diffs-w-failures/1/user-1-session-41.v
+++ diffs-w-failures/1/user-1-session-41.v
@@ -382,54 +382,13 @@ Unset Silent.
 Set Diffs "off".
 Create HintDb agt discriminated.
 Hint Resolve singleton_eq: agt.
-Theorem alpha_is_partial_function :
-  forall S G G', Alpha S G -> Alpha S G' -> G = G'.
+Unset Silent.
+Set Diffs "off".
 Set Printing Width 94.
-Show.
+Lemma alpha_fun_inversion :
+  forall S,
+  (forall X, Ensembles.In _ S X -> exists S_1 S_2, X = SFun S_1 S_2) ->
+  forall G, Alpha S G -> exists G_1 G_2, G = GFun G_1 G_2.
 Proof.
 (intros).
-generalize dependent G'.
-Unset Silent.
-Set Diffs "off".
-Set Printing Width 94.
-Show.
-Unset Silent.
-Set Diffs "off".
-Set Printing Width 94.
-Show.
-(induction H).
-Set Silent.
--
-(intros; inversion H0; subst; eauto).
-all: (try (apply singleton_eq in H1; congruence)).
-all: (try specialize (H1 _ (In_singleton _ _ _))).
-all: (try specialize (H1 _ (In_singleton _ _))).
-all:
- (repeat
-   match goal with
-   | H:exists _, _ |- _ => destruct H
-   | H:_ \/ _ |- _ => inversion H; clear H
-   end).
-all: (try congruence).
--
-(intros; inversion H0; subst; eauto).
-all: (try (apply singleton_eq in H1; congruence)).
-all: (try specialize (H1 _ (In_singleton _ _ _))).
-all: (try specialize (H1 _ (In_singleton _ _))).
-all:
- (repeat
-   match goal with
-   | H:exists _, _ |- _ => destruct H
-   | H:_ \/ _ |- _ => inversion H; clear H
-   end).
-Unset Silent.
-all: (try congruence).
-Set Silent.
--
-Unset Silent.
-Show.
-Unset Silent.
-Set Diffs "off".
-Set Printing Width 94.
-Show.
-(intros).
+(inversion H0).
