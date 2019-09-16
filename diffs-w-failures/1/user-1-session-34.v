2b93c5bf98e9b62a09b956539807d97c54944d87
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:18:36 -0700

    user 1, session 34, part 0

---
+++ diffs-w-failures/1/user-1-session-34.v
@@ -0,0 +1,10 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 146.
+Set Silent.
+Require Export Arith.
+Unset Silent.
+Require Export Lia.
