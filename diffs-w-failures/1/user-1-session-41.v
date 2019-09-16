d1ac2fbeef5f650266c68b3092778ad4cf631641
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:19:03 -0700

    user 1, session 41, part 69

--- diffs-w-failures/1/user-1-session-41.v
+++ diffs-w-failures/1/user-1-session-41.v
@@ -499,4 +499,14 @@ Show.
 Set Silent.
 -
 (inversion H1).
-specialize (H _ H5).
+Unset Silent.
+Show.
+specialize (H _ H4).
+specialize (H2 _ H4).
+(repeat
+  match goal with
+  | H:exists _, _ |- _ => destruct H
+  | H:_ \/ _ |- _ => inversion H; clear H
+  end).
+congruence.
+-
