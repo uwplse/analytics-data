962d24d7e016a366af0aaa46a005d05e039d1c66
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:19:02 -0700

    user 1, session 41, part 2

--- diffs-w-failures/1/user-1-session-41.v
+++ diffs-w-failures/1/user-1-session-41.v
@@ -283,6 +283,10 @@ econstructor.
 Qed.
 End AGT_Spec.
 Require Import Coq.Lists.List.
+Unset Silent.
+Set Diffs "off".
+Import Coq.Lists.List.ListNotations.
+Set Silent.
 Module AGT_Bounded_Rows_Details.
 Definition label := nat.
 Inductive ST : Type :=
@@ -369,6 +373,26 @@ Fixpoint Gamma (G : GT) : SetST :=
                     end))) l)
   end.
 Unset Silent.
-Set Diffs "off".
-Search -Ensemble -(list _).
-Search -(Ensemble (list _)).
+Inductive Alpha : SetST -> GT -> Prop :=
+  | alpha_int : Alpha (Singleton _ SInt) GInt
+  | alpha_bool : Alpha (Singleton _ SBool) GBool
+  | alpha_fun :
+      forall S G_1 G_2,
+      Inhabited _ S ->
+      (forall X,
+       Ensembles.In _ S X -> exists S_1 S_2, X = SFun S_1 S_2) ->
+      Alpha
+        (SetPMap S
+           (fun S =>
+            match S with
+            | SFun S_1 S_2 => Some S_1
+            | _ => None
+            end)) G_1 ->
+      Alpha
+        (SetPMap S
+           (fun S =>
+            match S with
+            | SFun S_1 S_2 => Some S_2
+            | _ => None
+            end)) G_2 -> Alpha S (GFun G_1 G_2)
+  | alpha_rec_mt : Alpha (Singleton _ (SRec nil)) (GRec []).
