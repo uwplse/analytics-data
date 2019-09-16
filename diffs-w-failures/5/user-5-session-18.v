4f4d6937ad97eeeff67b845cc7871745528a66e2
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:36:32 -0700

    user 5, session 18, part 24

--- diffs-w-failures/5/user-5-session-18.v
+++ diffs-w-failures/5/user-5-session-18.v
@@ -54,6 +54,4 @@ Theorem simplify_correct :
   forall (L : EpsilonLogic) (t : Term) env,
   L.(eval) env t = L.(eval) env (simplify t).
 Proof.
-(induction t; intros; simpl in *).
--
-reflexivity.
+(induction t; intros; simpl in *; try congruence).
