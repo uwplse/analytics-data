6f35f51ee14356568e5cdd7c7970c35865bebd40
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:28:42 -0700

    user 2, session 5, part 3

--- diffs-w-failures/2/user-2-session-5.v
+++ diffs-w-failures/2/user-2-session-5.v
@@ -28,15 +28,26 @@ Timeout 1 Check @value.
 Timeout 1 Check @value.
 Unset Silent.
 Set Printing Width 98.
+Unset Silent.
+Set Printing Width 98.
 Fixpoint value (t : term) : bool :=
   match t with
   | Nil => true
   | Ident _ => true
   | Cons a b => value a && value b
-  | App f a => match f with
-               | Ident name => String.eqb name "lam"
-               | _ => false
-               end
+  | App f a => false
   end.
-Redirect "/tmp/coqp1KVav" Print Ltac Signatures.
+Redirect "/tmp/coqhYSoRC" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
+Module TermNotations.
+Notation "[ ]" := Nil (format "[ ]") : coucou_scope.
+Notation "[ x ]" := (Cons x Nil) : coucou_scope.
+Notation "[ x y .. z ]" := (Cons x (Cons y .. (Cons z Nil) ..)) : coucou_scope.
+End TermNotations.
+Redirect "/tmp/coqY0pFmz" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Import TermNotations.
+Redirect "/tmp/coq34dpHE" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Check [Nil Nil Nil].
