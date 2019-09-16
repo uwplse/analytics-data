48b2c6f392c8ee5b308ab4d770304cc064b625df
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:16:50 -0700

    user 1, session 5, part 0

---
+++ diffs-w-failures/1/user-1-session-5.v
@@ -0,0 +1,91 @@
+Ltac
+ simpl_match :=
+  let repl_match_goal d d' :=
+   replace d with d';
+    lazymatch goal with
+    | |- context [ match d' with
+                   | _ => _
+                   end ] => fail
+    | _ => idtac
+    end
+  in
+  let repl_match_hyp H d d' :=
+   replace d with d' in H;
+    lazymatch type of H with
+    | context [ match d' with
+                | _ => _
+                end ] => fail
+    | _ => idtac
+    end
+  in
+  match goal with
+  | Heq:?d = ?d'
+    |- context [ match ?d with
+                 | _ => _
+                 end ] => repl_match_goal d d'
+  | Heq:?d' = ?d
+    |- context [ match ?d with
+                 | _ => _
+                 end ] => repl_match_goal d d'
+  | Heq:?d = ?d', H:context [ match ?d with
+                              | _ => _
+                              end ] |- _ => repl_match_hyp H d d'
+  | Heq:?d' = ?d, H:context [ match ?d with
+                              | _ => _
+                              end ] |- _ => repl_match_hyp H d d'
+  end.
+Ltac
+ destruct_matches_in e :=
+  lazymatch e with
+  | context [ match ?d with
+              | _ => _
+              end ] => destruct_matches_in d
+  | _ => destruct e eqn:?; intros
+  end.
+Ltac
+ destruct_all_matches :=
+  repeat
+   (try simpl_match;
+     try
+      match goal with
+      | |- context [ match ?d with
+                     | _ => _
+                     end ] => destruct_matches_in d
+      | H:context [ match ?d with
+                    | _ => _
+                    end ] |- _ => destruct_matches_in d
+      end); subst; try congruence; auto.
+Ltac
+ destruct_nongoal_matches :=
+  repeat
+   (try simpl_match;
+     try
+      match goal with
+      | H:context [ match ?d with
+                    | _ => _
+                    end ] |- _ => destruct_matches_in d
+      end); subst; try congruence; auto.
+Ltac
+ destruct_goal_matches :=
+  repeat
+   (try simpl_match;
+     match goal with
+     | |- context [ match ?d with
+                    | _ => _
+                    end ] => destruct_matches_in d
+     end); try congruence; auto.
+Ltac
+ destruct_tuple :=
+  match goal with
+  | H:context [ let '(a, b) := ?p in _ ]
+    |- _ => let a := fresh a in
+            let b := fresh b in
+            destruct p as [a b]
+  | |- context [ let '(a, b) := ?p in _ ] =>
+        let a := fresh a in
+        let b := fresh b in
+        destruct p as [a b]
+  end.
+Tactic Notation "destruct" "matches" "in" "*" := destruct_all_matches.
+Tactic Notation "destruct" "matches" "in" "*|-" := destruct_nongoal_matches.
+Tactic Notation "destruct" "matches" := destruct_goal_matches.
