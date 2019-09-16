9d36968d4cd29bf6a8f772415473fbeee74f7654
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 16:28:41 -0700

    user 2, session 3, part 0

---
+++ diffs-w-failures/2/user-2-session-3.v
@@ -0,0 +1,11 @@
+Redirect "/tmp/coqhq4XzM" Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Redirect "/tmp/coqM2RzcB" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 98.
+Require Import Coq.String.String.
