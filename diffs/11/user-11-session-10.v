1517ff5c4e16796a8fb932f8ca160c279e9c3e3e
Talia Ringer <tringer@cs.washington.edu>
2019-09-15 20:59:10 -0700

    user 11, session 10, part 4

--- diffs/11/user-11-session-10.v
+++ diffs/11/user-11-session-10.v
@@ -131,4 +131,46 @@ Instance showDataX : (Show (kvs_data exp)) :=
          | Kvs_BadRequest => "400"
          | Kvs_PreconditionFailed => "412"
          end |}.
+Unset Silent.
+BackTo 72.
+Unset Silent.
+Redirect "/tmp/coq16819Lkl" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Set Printing Width 115.
+Set Silent.
+From ExtLib Require Import Functor.
+From QuickChick Require Import Decidability Show.
+From DeepWeb Require Import Exp.
+Open Scope string_scope.
+Anomaly ""Assert_failure printing/ppconstr.ml:399:14"." Please report at http://coq.inria.fr/bugs/.
+Arguments kvs_data : clear implicits.
+Instance eqKvs_data  (x y : kvs_data id): (Dec (x = y)).
+Proof.
+dec_eq.
+Defined.
+Instance showData : (Show (kvs_data id)) :=
+ {|
+ show := fun d =>
+         match d with
+         | Kvs_GET k => "GET " ++ show k
+         | Kvs_PUT k v => "PUT " ++ show k ++ " " ++ show v
+         | Kvs_CAS k x v => "CAS " ++ show k ++ " " ++ show x ++ " " ++ show v
+         | Kvs_OK v => "OK  " ++ show (v : N)
+         | Kvs_NoContent => "204"
+         | Kvs_BadRequest => "400"
+         | Kvs_PreconditionFailed => "412"
+         end |}.
+Instance showDataX : (Show (kvs_data exp)) :=
+ {|
+ show := fun d =>
+         match d with
+         | Kvs_GET k => "GET " ++ show k
+         | Kvs_PUT k v => "PUT " ++ show k ++ " " ++ show v
+         | Kvs_CAS k x v => "CAS " ++ show k ++ " " ++ show x ++ " " ++ show v
+         | Kvs_OK v => "OK  " ++ show v
+         | Kvs_NoContent => "204"
+         | Kvs_BadRequest => "400"
+         | Kvs_PreconditionFailed => "412"
+         end |}.
 Definition kvs_get {V} (k : N) : list (N * V) -> option V := fmap snd \226\136\152 find (N.eqb k \226\136\152 fst).
