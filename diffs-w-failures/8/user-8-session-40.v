8b5c12e0f1cdc79aaf1a3c26dd9d8c474afd687f
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 14:24:36 -0700

    user 8, session 40, part 7

--- diffs-w-failures/8/user-8-session-40.v
+++ diffs-w-failures/8/user-8-session-40.v
@@ -47,4 +47,12 @@ Unset Silent.
 Show.
 Set Printing Width 85.
 Show.
+Unset Silent.
+Show.
+Set Printing Width 85.
+Show.
 (rewrite kron_1_r').
+(rewrite subst_pat_fresh_empty).
+(rewrite denote_pat_fresh_id).
+(rewrite super_I; auto).
+Qed.
