563259383a5784bebe9d2e77f804e3d404451a66
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 15:06:02 -0700

    user 8, session 65, part 278

--- diffs-w-failures/8/user-8-session-65.v
+++ diffs-w-failures/8/user-8-session-65.v
@@ -1403,6 +1403,12 @@ Unset Silent.
 Timeout 1 About denote_box.
 Timeout 1 Print denote_box.
 Set Printing Width 85.
+Unset Silent.
+Set Printing Width 85.
 Lemma denote_box_compat :
   forall (safe : bool) (W1 W2 : WType) (c : Box W1 W2) \207\129 \207\129',
   \207\129 == \207\129' -> denote_box safe c \207\129 == denote_box safe c \207\129'.
+Proof.
+(intros).
+(induction c).
+(simpl).
