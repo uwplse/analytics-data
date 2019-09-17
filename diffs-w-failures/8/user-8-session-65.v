91b7442116a9377fd7e3ebf956f5b0b3ca463cd5
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 15:06:01 -0700

    user 8, session 65, part 186

--- diffs-w-failures/8/user-8-session-65.v
+++ diffs-w-failures/8/user-8-session-65.v
@@ -1408,4 +1408,13 @@ Show.
 Set Printing Width 85.
 Show.
 (unfold valid_ancillae_box).
-(intros TB).
+Unset Silent.
+Show.
+Set Printing Width 85.
+Show.
+(intros \207\129 TB).
+(repeat simpl_rewrite inSeq_correct; try apply inSeq_WT; try apply unitary_at1_WT;
+  try apply source_symmetric_WT; trivial).
+(unfold compose_super).
+(apply source_symmetric_WT in H).
+(rewrite IHsource_symmetric; trivial).
