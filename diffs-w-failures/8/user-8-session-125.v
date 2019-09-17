3a0fee9eaa5486fd5811ce46dd2854e559c9e700
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 16:46:52 -0700

    user 8, session 125, part 213

--- diffs-w-failures/8/user-8-session-125.v
+++ diffs-w-failures/8/user-8-session-125.v
@@ -143,6 +143,8 @@ specialize (denote_ctrls_unitary W _ U _ L) as inv.
 replace (size_wtype W1 + size_wtype W2)%nat with \226\159\166 W \226\159\167 by (subst; easy).
 Set Silent.
 (unfold apply_U, apply_unitary, super).
-(destruct W; try (solve [ inversion HeqW ])).
 Unset Silent.
 Show.
+Set Printing Width 85.
+Show.
+(rewrite denote_ctrls_transpose).
