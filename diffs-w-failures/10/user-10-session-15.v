f58c85b49d3c9398271203991b03aee762267c4c
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 17:13:17 -0700

    user 10, session 15, part 37

--- diffs-w-failures/10/user-10-session-15.v
+++ diffs-w-failures/10/user-10-session-15.v
@@ -29,13 +29,9 @@ Set Printing Width 114.
 Set Silent.
 From Coq Require Import NArith Streams.
 Open Scope N_scope.
-CoFixpoint Fib (a b : N) : Stream N := Cons a (print_id (Fib b (a + b))).
 Unset Silent.
 Set Printing Width 114.
-Unset Silent.
-Set Printing Width 114.
-Unset Silent.
-Set Printing Width 114.
-Unset Silent.
-Set Printing Width 114.
+Anomaly ""Assert_failure printing/ppconstr.ml:399:14"." Please report at http://coq.inria.fr/bugs/.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqp7icPD" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
 Eval compute in Str_nth 10 (Fib 0 1).
