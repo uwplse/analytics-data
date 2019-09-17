084bee3a8d60ad8c80748cd64ef67027b14b29e2
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 16:46:51 -0700

    user 8, session 125, part 43

--- diffs-w-failures/8/user-8-session-125.v
+++ diffs-w-failures/8/user-8-session-125.v
@@ -70,14 +70,5 @@ Unset Silent.
 Show.
 Set Printing Width 85.
 Show.
-(repeat rewrite Mmult_assoc).
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
 Unset Silent.
 Show.
