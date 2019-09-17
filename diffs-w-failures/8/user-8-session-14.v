c746fbae4815184a2d38b915ec42dcee97aaad7c
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 13:42:15 -0700

    user 8, session 14, part 92

--- diffs-w-failures/8/user-8-session-14.v
+++ diffs-w-failures/8/user-8-session-14.v
@@ -199,18 +199,9 @@ Unset Silent.
 Show.
 Set Printing Width 85.
 Show.
+Unset Silent.
+Show.
+Set Printing Width 85.
+Show.
 (simpl).
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-Unset Silent.
-Show.
+(apply mixed_unitary; trivial).
