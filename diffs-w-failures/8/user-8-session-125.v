5fa1f1d531056d60e84a30ae5a65c8da195ae311
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 16:46:52 -0700

    user 8, session 125, part 233

--- diffs-w-failures/8/user-8-session-125.v
+++ diffs-w-failures/8/user-8-session-125.v
@@ -233,7 +233,16 @@ Qed.
 Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqvIAyK0"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
-Set Silent.
-Lemma even_bias : biased_coin (1 / 2) = fair_coin.
-Proof.
 Unset Silent.
+Set Printing Width 85.
+Lemma even_bias : biased_coin (1 / 2) == fair_coin.
+Proof.
+lma.
+Qed.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqSb2JHl"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Set Silent.
+Lemma fair_toss : (\226\159\166 coin_flip \226\159\167) (I 1) == fair_coin.
+Proof.
+lma.
