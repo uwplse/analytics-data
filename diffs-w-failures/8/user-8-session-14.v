201e058dcbf74f772cc358e4b6a4b0bfde6c2341
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 13:42:14 -0700

    user 8, session 14, part 1

--- diffs-w-failures/8/user-8-session-14.v
+++ diffs-w-failures/8/user-8-session-14.v
@@ -1221,8 +1221,9 @@ subst.
 (repeat rewrite Nat.pow_add_r).
 (intros).
 Unset Silent.
+Show.
+Set Printing Width 85.
+Show.
 Msimpl.
-Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqa34QT0"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
+Set Printing Implicit.
+Show.
