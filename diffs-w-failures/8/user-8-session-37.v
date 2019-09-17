a07b11bc0a462a44d37f57858f9df7b55f6b0364
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 14:19:45 -0700

    user 8, session 37, part 58

--- diffs-w-failures/8/user-8-session-37.v
+++ diffs-w-failures/8/user-8-session-37.v
@@ -454,18 +454,13 @@ dependent destruction AF.
 dependent destruction AF.
 (inversion H).
 +
+Unset Silent.
+Show.
+Set Printing Width 85.
+Show.
 dependent destruction AF.
 (assert (VA : forall b, valid_ancillae (c b))).
 (intros; apply H; apply H0).
 clear H.
 (unfold valid_ancillae in *).
 (intros \206\147 \206\1470 WT).
-(unfold denote_circuit in *).
-(simpl in *).
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-replace (size_ctx \206\147 - 1)%nat with size_ctx (DBCircuits.remove_pat p \206\147).
-(unfold compose_super).
-(erewrite VA).
