c1c7b3172d74d010904d67acea3ea1b33adc6c8a
Talia Ringer <tringer@cs.washington.edu>
2019-09-16 17:13:08 -0700

    user 10, session 10, part 8

--- diffs-w-failures/10/user-10-session-10.v
+++ diffs-w-failures/10/user-10-session-10.v
@@ -327,5 +327,8 @@ Definition match_event_list {X} : networkE X -> X -> list (itree tE unit) -> lis
   compose pfmap \226\136\152 match_event.
 Unset Silent.
 Check nmi_of_smi.
+Check nmi_of_smi.
+Check network_of_app.
+Print sE.
 Definition server : itree sE void :=
   sk <- translate subevent serverHandshake;; interp (network_of_app sk) (nmi_of_smi kvs).
