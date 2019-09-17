0010240eba4fa242633a4d16ccb518c1d94a9de9
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 15:06:03 -0700

    user 8, session 65, part 328

--- diffs-w-failures/8/user-8-session-65.v
+++ diffs-w-failures/8/user-8-session-65.v
@@ -1394,12 +1394,4 @@ Unset Silent.
 Timeout 1 About denote_box.
 Timeout 1 Print denote_box.
 Set Printing Width 85.
-Require Import Setoid.
-Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqfL4nV2"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Unset Silent.
-Timeout 1 About denote_box.
-Timeout 1 Print denote_box.
 Unset Silent.
