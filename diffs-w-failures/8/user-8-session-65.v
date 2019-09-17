0ceb48c5ea90563919c6183a9cfbd085239653ca
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 15:06:01 -0700

    user 8, session 65, part 135

--- diffs-w-failures/8/user-8-session-65.v
+++ diffs-w-failures/8/user-8-session-65.v
@@ -1089,4 +1089,4 @@ Set Printing Width 85.
 Show.
 mat_replace \226\159\1681\226\136\163 \195\151 \226\136\1631\226\159\169 with I 1 by lma.
 Msimpl.
-(rewrite id_kron).
+(rewrite (id_kron (2 ^ i) (2 ^ d))).
