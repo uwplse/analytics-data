ddca4d6bbfc3b031f2b715072ac9ab34077de7c4
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 14:24:36 -0700

    user 8, session 40, part 17

--- diffs-w-failures/8/user-8-session-40.v
+++ diffs-w-failures/8/user-8-session-40.v
@@ -72,4 +72,11 @@ Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqcAMjSv"
 Print Ltac Signatures.
 Unset Silent.
 Set Printing Width 85.
-Hint Rewrite kron_1_r'.
+Hint Rewrite @kron_1_r'.
+Lemma X_spec :
+  forall b safe : bool,
+  denote_box safe (boxed_gate _X) (bool_to_matrix b) == bool_to_matrix (\194\172 b).
+Proof.
+(intros).
+vector_denote.
+(destruct b; unfold bool_to_ket; simpl; Msimpl).
