e0760a7cff681eeead86bcee66cc1ddd419722bf
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 16:18:24 -0700

    user 8, session 108, part 2

--- diffs-w-failures/8/user-8-session-108.v
+++ diffs-w-failures/8/user-8-session-108.v
@@ -84,5 +84,13 @@ gen \207\129.
 (induction (c p)).
 -
 (unfold denote_u_db_box).
-(simpl).
 Unset Silent.
+Show.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coq7ET26j"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Set Printing Width 85.
+Show.
+(simpl).
+(rewrite pad_nothing).
