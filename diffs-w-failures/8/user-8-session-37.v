9bb0970e212979a6d7dd20a5678afa302be86f75
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 14:19:45 -0700

    user 8, session 37, part 2

--- diffs-w-failures/8/user-8-session-37.v
+++ diffs-w-failures/8/user-8-session-37.v
@@ -60,4 +60,15 @@ Show.
 Set Printing Width 85.
 Show.
 (apply mixed_state_trace_1).
-(apply denote_circuit_correct).
+admit.
+-
+(induction c as [| W' W0 g p c IH| IH]).
++
+Set Silent.
+reflexivity.
++
+(intros H \206\147 \206\1470 H').
+replace (gate g p c) with compose (gate g p (fun p' => output p')) c by auto.
+dependent destruction H'.
+(destruct \206\1471 as [| \206\1471]; try invalid_contradiction).
+(erewrite denote_compose with (\206\1471 := []); trivial).
