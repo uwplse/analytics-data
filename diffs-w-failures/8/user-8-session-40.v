c85261cb28f0734b76a68b0f2ab415d8924511a2
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 14:24:36 -0700

    user 8, session 40, part 23

--- diffs-w-failures/8/user-8-session-40.v
+++ diffs-w-failures/8/user-8-session-40.v
@@ -73,20 +73,25 @@ Print Ltac Signatures.
 Unset Silent.
 Set Printing Width 85.
 Unset Silent.
+Unset Silent.
 Set Printing Width 85.
 Hint Rewrite @kron_1_r' : M_db.
-Set Silent.
 Lemma X_spec :
   forall b safe : bool,
   denote_box safe (boxed_gate _X) (bool_to_matrix b) == bool_to_matrix (\194\172 b).
+Set Silent.
 Proof.
 (intros).
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
+vector_denote.
 (destruct b; unfold bool_to_ket; simpl; Msimpl; easy).
+Unset Silent.
 Qed.
-Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqi8zFxo"
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqYJshKs"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
+Set Silent.
+Lemma init0_spec : forall safe, denote_box safe init0 (I (2 ^ 0)) = \226\136\1630\226\159\169\226\159\1680\226\136\163.
+Proof.
+(intros).
+matrix_denote.
+Msimpl.
