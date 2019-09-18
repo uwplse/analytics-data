259459915c6aa8aaf39f958e52dc0290a9a25c5d
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:18:46 -0700

    user 7, session 108, part 14

--- diffs-w-failures/7/user-7-session-108.v
+++ diffs-w-failures/7/user-7-session-108.v
@@ -258,5 +258,6 @@ exists 0,(TCName c).
 (destruct (IHt2 k) as [w2 [v2 Hm2]]).
 exists (Nat.max w1 w2),(TPair v1 v2).
 Unset Silent.
-(apply match_ty_pair).
-(eapply match_ty__ge_w).
+Set Printing Width 148.
+(apply match_ty_pair; eapply match_ty__ge_w; try eassumption).
+(apply Nat.max_le_r).
