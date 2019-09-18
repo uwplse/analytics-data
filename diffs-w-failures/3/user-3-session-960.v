d606f6870c4c8fe1682c822db55e2f60e08ce3f2
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:47:25 -0700

    user 3, session 960, part 331

--- diffs-w-failures/3/user-3-session-960.v
+++ diffs-w-failures/3/user-3-session-960.v
@@ -206,6 +206,9 @@ Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Check @Ascii.nat_ascii_bounded.
 Timeout 1 Check @Ascii.nat_ascii_bounded.
+Timeout 1 Check @Ascii.nat_ascii_bounded.
+Timeout 1 Check @Ascii.nat_ascii_bounded.
+Timeout 1 Check @Ascii.nat_ascii_bounded.
 Definition nat64_to_le (x : nat) : list byte :=
   let digits := nat_to_le 254 x in
-  digits ++ repeat (8 - length digits) bounded0.
+  digits ++ repeat bounded0 (8 - length digits).
