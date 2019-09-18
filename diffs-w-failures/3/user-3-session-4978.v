e778dcab30fb8e3f932fd3c2087a8b87313a495e
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:05:31 -0700

    user 3, session 4978, part 0

---
+++ diffs-w-failures/3/user-3-session-4978.v
@@ -0,0 +1,9 @@
+Time Cd "replicated-disk/extract/".
+Time From Coq Require Extraction.
+Time Require Import ExtrHaskellNatInt.
+Time Require Import ExtrHaskellBasic.
+Time
+From Perennial Require Import Examples.ReplicatedDisk.ReplicatedDiskImpl.
+Time Extraction Language Haskell.
+Time Separate Extraction impl.
+Time Cd "../../".
