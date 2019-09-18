67ea83b77a9bd60277052dca22c206bbe4171623
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:18 -0700

    user 7, session 19, part 145

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -127,5 +127,12 @@ Lemma match_ty_i_eq__inv_depth_eq :
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
-(induction t; induction t'; intros H; try reflexivity).
+Set Printing Width 148.
+(induction t; induction t'; intros H; try reflexivity;
+  try (solve
+   [ match goal with
+     | |- | ?t1 | = | ?t2 | =>
+           assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+            (H 0 _ Hv); destruct H as [H _]; specialize (H Hm); contradiction
+     end ])).
 Show.
