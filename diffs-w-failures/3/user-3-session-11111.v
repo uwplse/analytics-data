23722e8266645b6fe2458de12b2f40cb6a86b80b
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:35:26 -0700

    user 3, session 11111, part 0

---
+++ diffs-w-failures/3/user-3-session-11111.v
@@ -0,0 +1,11 @@
+Time From Perennial Require Import Examples.StatDb.Refinement.
+Time From Perennial Require Import Examples.AtomicPair.RefinementShadow.
+Time From Perennial Require Import Examples.AtomicPair.RefinementLog.
+Time
+From Perennial Require Import
+  Examples.ReplicatedDisk.ReplicatedDiskRefinement.
+Time From Perennial Require Import Examples.MailServer.MailRefinement.
+Time From Perennial Require Import Goose.ExplicitModel.
+Time From Perennial Require Import Goose.Examples.UnitTests.
+Time From Perennial Require Import Goose.Examples.SimpleDb.
+Time From Perennial Require Import Goose.Examples.MailServer.
