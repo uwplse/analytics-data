c1f16ff9016011940c7e6b6a1250f2801fdb3d83
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:55:48 -0700

    user 3, session 3009, part 0

---
+++ diffs-w-failures/3/user-3-session-3009.v
@@ -0,0 +1,9 @@
+Time
+From iris.bi Require Export derived_laws_bi derived_laws_sbi big_op updates
+  plainly embedding.
+Time Set Default Proof Using "Type".
+Time Module Import bi.
+Time Export bi.interface.bi.
+Time Export bi.derived_laws_bi.bi.
+Time Export bi.derived_laws_sbi.bi.
+Time End bi.
