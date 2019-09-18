e89f77e2cd2484849029b8b050e427ae2ba865bb
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 20:26:38 -0700

    user 7, session 198, part 1

--- diffs-w-failures/7/user-7-session-198.v
+++ diffs-w-failures/7/user-7-session-198.v
@@ -83,4 +83,13 @@ Proof.
 (intros).
 reflexivity.
 Qed.
-Lemma b_subst_var_eq : forall (X : id) (s : ty), [BX := s] TVar X = s.
+Lemma b_subst_bvar_eq : forall (X : id) (s : ty), [BX := s] TBVar X = s.
+Unset Silent.
+Proof.
+(intros).
+(simpl).
+(rewrite <- beq_id_refl).
+reflexivity.
+Qed.
+Set Silent.
+Lemma b_subst_bvar_neq : forall (X : id) (s : ty) (Y : id), X <> Y -> [BX := s] TVar Y = TVar Y.
