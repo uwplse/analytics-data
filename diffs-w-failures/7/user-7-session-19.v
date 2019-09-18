9af89e4ac7f8322a54a5e17c4f2b5511d7e14b2a
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:19 -0700

    user 7, session 19, part 310

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -209,6 +209,7 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 (induction k; intros v Hv; induction Hv; intros ta tb Hsem; unfold sem_sub_k_i in Hsem;
   try
    match goal with
@@ -222,6 +223,8 @@ Set Printing Width 148.
      |- _ =>
          assert (Hvv : value_type (TPair v1 v2)) by (constructor; assumption);
           assert (Hmv : |-[ k] TPair v1 v2 <$ TPair v1 v2) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hvv Hmv);
-          apply match_ty_i_union__inv in Hsem; destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hv' Hm'
+          apply match_ty_i_union__inv in Hsem; destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hv' Hm';
+          apply match_ty_i__transitive_on_value_type with (TPair v1 v2)
    end).
+Show.
 Show.
