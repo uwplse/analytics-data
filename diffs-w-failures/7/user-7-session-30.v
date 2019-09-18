c10e08a838d5de8aa88c66b9422570d746a80342
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:16:41 -0700

    user 7, session 30, part 23

--- diffs-w-failures/7/user-7-session-30.v
+++ diffs-w-failures/7/user-7-session-30.v
@@ -104,6 +104,5 @@ clear IHv1 IHv2.
 (destruct Hm as [Hm| Hm]; [ specialize (IHt1 _ Hm) | specialize (IHt2 _ Hm) ]; split; try tauto; rewrite inv_depth_union;
   [ apply Nat.le_trans with (| t1 |) | apply Nat.le_trans with (| t2 |) ]; try tauto; apply Max.le_max_l || apply Max.le_max_r).
 -
-(intros t k Hm).
-Unset Silent.
-(apply match_ty__value_type_l in Hm).
+Set Printing Width 148.
+(apply match_ty__value_type in Hm).
