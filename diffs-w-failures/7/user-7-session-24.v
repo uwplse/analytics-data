3a65240f8f6aad45fe0e56836162cfdf6a8ac336
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:12:08 -0700

    user 7, session 24, part 22

--- diffs-w-failures/7/user-7-session-24.v
+++ diffs-w-failures/7/user-7-session-24.v
@@ -69,4 +69,9 @@ Notation "'||-' '[' t1 ']' '<=' '[' t2 ']'" := (sem_sub t1 t2) (at level 50) : b
 Set Silent.
 Unset Silent.
 Definition sem_eq (t1 t2 : ty) := forall k : nat, ||-[ k][t1]= [t2].
-Notation "'||-' '[' t1 ']' '=' '[' t2 ']' '-||'" := (sem_eq t1 t2) (at level 50) : btjm_scope.
+Set Printing Width 148.
+Notation "'||-' '[' t1 ']' '=' '[' t2 ']'" := (sem_eq t1 t2) (at level 50) : btjm_scope.
+Hint Unfold sem_sub_k sem_eq_k sem_sub sem_eq: DBBetaJulia.
+Example ex : ||- [tint]= [tint].
+Proof.
+(unfold sem_eq).
