7c960e808a2c4f68a7a93ba65512871b2e616494
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:15:43 -0700

    user 7, session 104, part 21

--- diffs-w-failures/7/user-7-session-104.v
+++ diffs-w-failures/7/user-7-session-104.v
@@ -115,4 +115,8 @@ clear Heq.
 specialize (Href 1).
 Unset Silent.
 (destruct Href as [w2 Hsem]).
-(pose proof (sem_eq_k__exists_not 0 t)).
+Set Printing Width 148.
+(destruct (sem_eq_k__exists_not 0 t) as [t' Hnoteq]).
+(assert (Hm : |-[ 1, 1] TRef tint <$ TExist vX (TRef tX))).
+{
+(simpl).
