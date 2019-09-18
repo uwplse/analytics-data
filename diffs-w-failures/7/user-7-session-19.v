ff6bee191758e0b00d7884728aee837b0b2d5942
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:20 -0700

    user 7, session 19, part 458

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -217,5 +217,9 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 (split; intros k; specialize (Hsem (S k)); assert (Hvref : value_type (TRef t)) by constructor;
-  assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hm); simpl in Hsem).
+  assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hm); simpl in Hsem; 
+  intros v' Hm').
+auto.
+Show.
