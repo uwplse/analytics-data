7c55b09061ac6f6af967852065f288d801f7602d
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:19 -0700

    user 7, session 19, part 318

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -201,13 +201,6 @@ Lemma value_sem_sub_k_union__value_sem_sub_k_component :
   forall v : ty, value_type v -> forall (k : nat) (ta tb : ty), ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
 Set Printing Width 148.
 Set Printing Width 148.
-(intros v Hv; induction Hv; intros k ta tb Hsem; unfold sem_sub_k_i in Hsem;
-  try
-   match goal with
-   | Hsem:forall v', value_type v' -> |-[ ?k] v' <$ ?v -> _
-     |- _ =>
-         assert (Hvv : value_type v) by (constructor; assumption); assert (Hmv : |-[ k] v <$ v) by (apply match_ty_i__reflexive; assumption);
-          specialize (Hsem _ Hvv Hmv); apply match_ty_i_union__inv in Hsem; destruct Hsem; [ left | right ]; unfold sem_sub_k_i; 
-          intros v' Hv' Hm'; apply match_ty_i__transitive_on_value_type with v; assumption
-   end).
+Set Printing Width 148.
+(intros v Hv; induction Hv; intros k ta tb Hsem; unfold sem_sub_k_i in Hsem).
 Show.
