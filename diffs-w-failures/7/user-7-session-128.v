5c070dea86b442e1c2410b4ab3af4a139ee516ab
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:33:54 -0700

    user 7, session 128, part 14

--- diffs-w-failures/7/user-7-session-128.v
+++ diffs-w-failures/7/user-7-session-128.v
@@ -107,4 +107,5 @@ Unset Silent.
 (assert (Hmp : |-[ k, w1'] TPair v v2 <$ TPair t1 t2)).
 {
 Set Printing Width 148.
-(apply match_ty_pair; eapply match_ty__ge_w; try eassumption).
+Set Printing Width 148.
+(apply match_ty_pair; eapply match_ty__ge_w; try eassumption; [ apply Nat.le_max_l | apply Nat.le_max_r ]).
