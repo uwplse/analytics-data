319c56754b6edc50706331bea3e53444193af0a9
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:19 -0700

    user 7, session 19, part 247

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -144,6 +144,6 @@ Set Silent.
 (assert (Hvref : value_type (TRef t)) by constructor).
 (assert (Hmref : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption)).
 (pose proof (Hsem _ Hvref Hmref) as Hm).
-(apply match_ty_i_union__inv in Hm).
-Unset Silent.
-(destruct Hm as [Hm1| Hm2]; [ left | right ]).
+Set Printing Width 148.
+(destruct Hm as [Hm1| Hm2]; [ left | right ]; intros v Hv).
+Show.
