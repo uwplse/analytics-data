28622f6c2f2c9bf73cb82a3df9d0c69cecf5fcd1
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:17 -0700

    user 7, session 19, part 57

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -78,4 +78,7 @@ Proof.
          apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; auto
    end).
 -
-(destruct v; contradiction).
+Unset Silent.
+Show.
+(destruct v).
+contradiction.
