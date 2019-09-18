8b2418d0cbc06e812a8a23d09fd4c1a123577e86
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:20 -0700

    user 7, session 19, part 357

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -197,6 +197,7 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 (split; intros v Hm; [ destruct (match_ty_i_exists t2 k) as [v' Hm'] | destruct (match_ty_i_exists t1 k) as [v' Hm'] ];
   [ assert (Hmp : |-[ k] TPair v v' <$ TPair t1 t2) by (apply match_ty_i_pair; assumption)
-  | assert (Hmp : |-[ k] TPair v' v <$ TPair t1 t2) by (apply match_ty_i_pair; assumption) ]).
+  | assert (Hmp : |-[ k] TPair v' v <$ TPair t1 t2) by (apply match_ty_i_pair; assumption) ]; specialize (Hsem _ Hmp)).
