d5b1747bb7c7960e996c314503d2c693ff74ddaa
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:16:11 -0700

    user 3, session 7199, part 200

--- diffs-w-failures/3/user-3-session-7199.v
+++ diffs-w-failures/3/user-3-session-7199.v
@@ -316,5 +316,4 @@ intuition eauto.
 -
 step_proc.
 (intuition eauto; autorewrite with upd; auto).
-Theorem append_ok :
-  forall v, proc_spec (append_spec v) (append v) recover abstr.
+Unset Silent.
