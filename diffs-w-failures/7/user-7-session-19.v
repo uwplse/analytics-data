e5a0a99fdd73a5c9e4877f2eff447231fc3e1a75
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:22 -0700

    user 7, session 19, part 675

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -580,4 +580,4 @@ Set Printing Width 148.
 (intros ta1 ta2 Hat1 IH1 Hat2 IH2 Hdep).
 (assert (Hat : atom_type (TPair ta1 ta2)) by (constructor; assumption)).
 Set Printing Width 148.
-(apply pair_sem_sub_k_i__sub_d; assumption).
+(apply pair_sem_sub_k_i__sub_d; try assumption).
