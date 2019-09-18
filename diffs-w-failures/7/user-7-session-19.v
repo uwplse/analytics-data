fae7ccb21fa70dec700b6d37e68a212b907043bf
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:17 -0700

    user 7, session 19, part 44

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -80,7 +80,5 @@ Set Printing Width 148.
 Show.
 Set Printing Width 148.
 Set Printing Width 148.
-(apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; apply match_ty_i_pair).
-Show.
-auto.
-Show.
+Set Printing Width 148.
+(apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; apply match_ty_i_pair; auto).
