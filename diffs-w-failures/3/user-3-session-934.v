4974aa9f2775a68b1653681d43ed2284df8fe65a
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:47:10 -0700

    user 3, session 934, part 0

---
+++ diffs-w-failures/3/user-3-session-934.v
@@ -0,0 +1,9 @@
+Time From Armada Require Import Examples.StatDb.Refinement.
+Time From Armada Require Import Examples.AtomicPair.RefinementShadow.
+Time From Armada Require Import Examples.AtomicPair.RefinementLog.
+Time
+From Armada Require Import Examples.ReplicatedDisk.ReplicatedDiskRefinement.
+Time From Armada Require Import Examples.MailServer.MailRefinement.
+Time From Armada Require Import Goose.Examples.UnitTests.
+Time From Armada Require Import Goose.Examples.SimpleDb.
+Time From Armada Require Import Goose.Examples.MailServer.
