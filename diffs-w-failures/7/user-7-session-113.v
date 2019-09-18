6a3717c8adbad6713a1aa424319eecd3298ac437
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:22:32 -0700

    user 7, session 113, part 9

--- diffs-w-failures/7/user-7-session-113.v
+++ diffs-w-failures/7/user-7-session-113.v
@@ -29,4 +29,10 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
-(induction t; intros s1 s2; try (solve [ simpl; reflexivity | simpl; specialize (IHt1 s1 s2); specialize (IHt2 s1 s2); auto ])).
+Set Printing Width 148.
+(induction t; intros s1 s2; try (solve [ simpl; reflexivity | simpl; specialize (IHt1 s1 s2); specialize (IHt2 s1 s2); tauto ])).
+Set Silent.
+-
+Unset Silent.
+(specialize (IHt1 s1 s2); specialize (IHt2 s1 s2)).
+(simpl).
