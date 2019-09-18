bc3f534dd7f6ca4ae1cb4190085deccef25ea4bc
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:52:19 -0700

    user 7, session 73, part 7

--- diffs-w-failures/7/user-7-session-73.v
+++ diffs-w-failures/7/user-7-session-73.v
@@ -211,7 +211,14 @@ assumption.
 Qed.
 Theorem nf_sem_sub__sub_d : forall t t' : ty, InNF( t) -> ||- [t]<= [t'] -> |- t << t'.
 Proof.
-(intros t t' Hsem).
-Unset Silent.
+Set Printing Width 148.
+(intros t t' Hnf Hsem).
+(apply nf_sem_sub_k__sub_d with (| t |)).
 Show.
-(apply nf_sem_sub_k__sub_d).
+assumption.
+Show.
+constructor.
+Show.
+(apply Hsem).
+Show.
+Qed.
