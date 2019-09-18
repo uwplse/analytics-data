8dd692ac6913068b0fd382c4f8c25a3d83a8e507
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 20:10:07 -0700

    user 7, session 176, part 41

--- diffs-w-failures/7/user-7-session-176.v
+++ diffs-w-failures/7/user-7-session-176.v
@@ -164,4 +164,4 @@ Set Printing Width 148.
       [ destruct (IHt1 v) as [Hm1 Hm2] | destruct (IHt2 v) as [Hm1 Hm2] ]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto ])).
 Show.
 (split; repeat rewrite subst_union; intros Hm; apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm];
-  [ destruct (IHt1 v) as [Hm1 Hm2] | destruct (IHt2 v) as [Hm1 Hm2] ]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
+  [ destruct (IHt1 v) as [Hm1 Hm2] | destruct (IHt2 v) as [Hm1 Hm2] ]).
