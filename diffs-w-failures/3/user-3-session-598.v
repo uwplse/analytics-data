4b02823a8e1988ae70d8d34edfc504241b544740
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:45:35 -0700

    user 3, session 598, part 0

---
+++ diffs-w-failures/3/user-3-session-598.v
@@ -0,0 +1,32 @@
+Ltac descend := repeat match goal with
+                       | |- exists _, _ => eexists
+                       end.
+Ltac
+ especialize H :=
+  match type of H with
+  | forall x : ?T, _ =>
+      let x := fresh x in
+      evar ( x : T ); specialize (H x); subst x
+  end.
+#[local]
+Ltac
+ _especialize H :=
+  lazymatch type of H with
+  | forall x : ?T, _ =>
+      let x := fresh x in
+      lazymatch type of T with
+      | Prop => unshelve (evar ( x : T ); specialize (H x); subst x)
+      | _ => evar ( x : T ); specialize (H x); subst x
+      end
+  end.
+Ltac
+ epose_proof H :=
+  let H' := fresh in
+  pose proof H as H'; repeat _especialize H'.
+Ltac
+ eexists_t t :=
+  match goal with
+  | |- exists _ : ?T, _ => eexists (_ : T)
+  | |- {_ : ?T | _} => eexists (_ : T)
+  end.
+Ltac exists_econstructor := eexists_t ltac:(econstructor); simpl.
