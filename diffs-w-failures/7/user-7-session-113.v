130cbf075a604654f6964a3ba9956877676f6d7c
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:22:33 -0700

    user 7, session 113, part 33

--- diffs-w-failures/7/user-7-session-113.v
+++ diffs-w-failures/7/user-7-session-113.v
@@ -14,29 +14,21 @@ Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Open Scope btjt.
 Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
-Proof.
-(intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
-Qed.
-Lemma not_fresh_in_union__inv : forall (X : id) (fvs1 fvs2 : id_set), not_fresh X (IdSet.union fvs1 fvs2) -> not_fresh X fvs1 /\ not_fresh X fvs2.
-Unset Silent.
-Proof.
+Set Printing Width 148.
 Set Silent.
+Lemma not_fresh_in_union__inv : forall (X : id) (fvs1 fvs2 : id_set), not_fresh X (IdSet.union fvs1 fvs2) -> not_fresh X fvs1 /\ not_fresh X fvs2.
+Proof.
 (intros X fvs1 fvs2 H).
 Unset Silent.
 (unfold not_fresh in *).
-Search -IdSet.union.
 Show.
-Search -IdSet.union.
-Set Printing Width 148.
 Set Silent.
 Abort.
-Unset Silent.
 Lemma subs_not_in_FV : forall (X : id) (t : ty), not_fresh_in_ty X t -> forall s : ty, [X := s] t = t.
-Set Silent.
 Proof.
 (intros X t).
 (induction t; intros Hnfresh s; try (solve [ reflexivity ])).
 -
-Set Printing Width 148.
+(simpl).
+Unset Silent.
 (unfold not_fresh_in_ty in *).
-Show.
