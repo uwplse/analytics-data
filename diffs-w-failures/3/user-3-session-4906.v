4e80a85cfa25835375c14aa84fa0a3536df7a98c
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:05:06 -0700

    user 3, session 4906, part 0

---
+++ diffs-w-failures/3/user-3-session-4906.v
@@ -0,0 +1,10 @@
+Time From Classes Require Import EqualDec.
+Time From Perennial Require Export Spec.Proc.
+Time From Perennial Require Export Spec.LockDefs.
+Time From Perennial.Goose Require Export GoZeroValues.
+Time From Perennial.Goose Require Export GoLayer.
+Time Export EqualDecNotation.
+Time Export ProcNotations.
+Time Open Scope proc.
+Time Open Scope string.
+Time Notation proc := (Proc.proc Op).
