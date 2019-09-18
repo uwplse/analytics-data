82064b04d28af9fbd71095e7235b985659858d43
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:01:24 -0700

    user 7, session 10, part 29

--- diffs-w-failures/7/user-7-session-10.v
+++ diffs-w-failures/7/user-7-session-10.v
@@ -196,4 +196,7 @@ Unset Silent.
 (destruct (in_nf_pair__inv _ _ Hnfm2) as [Hnfm21 Hnfm22]).
 Set Printing Width 148.
 Set Printing Width 148.
-(destruct IHHsub1 as [IHHsub11 IHHsub12]; assumption).
+(destruct IHHsub1 as [IHHsub11 IHHsub12]; try assumption).
+(split; intros tx Hsub'; [ remember (TPair t1 t2) as ty eqn:Heqy  | remember (TPair t1' t2') as ty eqn:Heqy  ]; induction Hsub'; inversion Heqy;
+  subst).
++
