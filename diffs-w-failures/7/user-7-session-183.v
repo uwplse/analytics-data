6417c27a43ad1a7cffdbfb87dacee3c79ca76405
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 20:15:22 -0700

    user 7, session 183, part 13

--- diffs-w-failures/7/user-7-session-183.v
+++ diffs-w-failures/7/user-7-session-183.v
@@ -126,4 +126,8 @@ reflexivity.
 Unset Silent.
 (rewrite subst_equation).
 (rewrite (false_beq_id _ _ HXY)).
-(pose proof (IdSetFacts.mem_2 Hin) as Hmem).
+Show.
+Search -IdSet.mem.
+(assert (Hmem : IdSet.mem Y (FV s) = false)).
+{
+(aply IdSetFacts.not_mem_iff).
