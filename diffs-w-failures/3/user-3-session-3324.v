e02ee7af586dde3c7ba0ac482d79d0bf814cfd40
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:57:15 -0700

    user 3, session 3324, part 11

--- diffs-w-failures/3/user-3-session-3324.v
+++ diffs-w-failures/3/user-3-session-3324.v
@@ -142,6 +142,11 @@ step_proc.
 step_proc.
 (destruct (lt_dec r0 (diskSize (stateDisk state)))).
 step_proc.
-(case_eq (diskGet (stateDisk state) (diskSize (stateDisk state) - 1)); intros).
 Unset Silent.
 Set Diffs "off".
+Set Printing Width 78.
+Show.
+(case_eq (diskGet (stateDisk state) (diskSize (stateDisk state) - 1)); intros).
+{
+exists (diskUpd (diskShrink (stateDisk state)) (stateBadBlock state) b).
+(unfold inited_any).
