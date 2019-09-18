2ce8aedd60efdf3d031fa6db4f80d13c8c2c1a3f
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:05:57 -0700

    user 7, session 16, part 52

--- diffs-w-failures/7/user-7-session-16.v
+++ diffs-w-failures/7/user-7-session-16.v
@@ -417,7 +417,8 @@ Set Printing Width 148.
 Show.
 (match goal with
  | Hcontra:|- ?t1 << ?t2
-   |- False =>
-       remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]);
-        subst
+   |- False => remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]); subst
  end).
+Show.
+(inversion Heqx).
+Show.
