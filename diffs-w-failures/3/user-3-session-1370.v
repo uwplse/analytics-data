00273828829973f80a9cc9f1b7601f615810255e
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:48:59 -0700

    user 3, session 1370, part 0

---
+++ diffs-w-failures/3/user-3-session-1370.v
@@ -0,0 +1,5 @@
+From Coq Require Import ProofIrrelevance.
+From Coq Require Export String.
+From Coq Require Import Program.
+From Coq Require Import Program.Wf.
+From Classes Require Import EqualDec.
