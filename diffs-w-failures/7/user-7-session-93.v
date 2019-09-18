ab6c1bd86ae569d716f0df3a2cd0d4c9bec913c1
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:07:23 -0700

    user 7, session 93, part 6

--- diffs-w-failures/7/user-7-session-93.v
+++ diffs-w-failures/7/user-7-session-93.v
@@ -38,4 +38,29 @@ Definition vZ := 3.
 Definition tX := TVar vX.
 Definition tY := TVar vY.
 Unset Silent.
+Set Printing Width 148.
+Set Silent.
 Definition teXX := TExist vX tX.
+Unset Silent.
+Definition tyXRefX := TExist vX (TRef tX).
+Set Silent.
+Declare Scope btjt_scope.
+Delimit Scope btjt_scope with btjt.
+Open Scope btjt.
+Reserved Notation "'|' t '|'" (at level 20).
+Unset Silent.
+Fixpoint inv_depth (t : ty) :=
+  match t with
+  | TCName _ => 0
+  | TPair t1 t2 => Nat.max (| t1 |) (| t2 |)
+  | TUnion t1 t2 => Nat.max (| t1 |) (| t2 |)
+  | TRef t' => 1 + | t' |
+  | TVar _ => 0
+  | TExist _ t' => | t' |
+  end
+where "'|' t '|'" := (inv_depth t) : btjt_scope.
+Inductive value_type : ty -> Prop :=
+  | VT_CName : forall cn, value_type (TCName cn)
+  | VT_Pair : forall v1 v2, value_type v1 -> value_type v2 -> value_type (TPair v1 v2)
+  | VT_Ref : forall t, value_type (TRef t).
+Hint Constructors value_type: DBBetaJulia.
