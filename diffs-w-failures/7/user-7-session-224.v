b4618b9723d330d94b3615ae2a4eceff032bb8ca
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 20:46:16 -0700

    user 7, session 224, part 31

--- diffs-w-failures/7/user-7-session-224.v
+++ diffs-w-failures/7/user-7-session-224.v
@@ -65,19 +65,25 @@ exists (TEV X').
 split.
 reflexivity.
 (induction w'; induction t'; intros Hm'; try (solve [ destruct v; contradiction || tauto ])).
-Set Silent.
-*
-Unset Silent.
+Set Printing Width 148.
+Show.
 Set Silent.
 (rewrite f_subst_union).
+Unset Silent.
 (apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
   destruct IHt' as [IHt'a IHt'b]; split; intros HX').
+Set Silent.
 {
 (destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
 (apply match_ty_union_1; auto).
+Unset Silent.
 }
-{
-(destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
+Set Silent.
 {
 Unset Silent.
-Show.
+(destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
+{
+specialize (IHt'b HXt'1).
+(destruct IHt'b as [w2 IHt'b]).
+exists w2.
+(apply match_ty_union_1; auto).
