e2911fb842c54df98245b5b2f16b15732c122ed4
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:15:43 -0700

    user 7, session 104, part 38

--- diffs-w-failures/7/user-7-session-104.v
+++ diffs-w-failures/7/user-7-session-104.v
@@ -94,4 +94,34 @@ Set Printing Width 148.
 Show.
 specialize (Hsem1 _ Hm).
 Show.
-(destruct k, w2, t; try (solve [ simpl in Hsem1; contraidction ])).
+(destruct k, w2, t; try (solve [ simpl in Hsem1; contradiction ])).
+Show.
+Set Silent.
+Admitted.
+Lemma not_sem_sub__refeXrefX_eYrefrefY : ~ ||- [TRef (TExist vX (TRef tX))]<= [TExist vY (TRef (TRef tY))].
+Proof.
+(intros Hcontra).
+specialize (Hcontra 2 1).
+(destruct Hcontra as [w Hcontra]).
+(assert (Hm : |-[ 2, 1] TRef (TExist vX (TRef tX)) <$ TRef (TExist vX (TRef tX))) by (apply match_ty_value_type__reflexive; constructor)).
+(unfold sem_sub_k_w in Hcontra).
+specialize (Hcontra _ Hm).
+clear Hm.
+(destruct w).
+-
+(apply Hcontra).
+-
+(apply match_ty_exist__inv in Hcontra).
+(destruct Hcontra as [t Hcontra]).
+(assert (Heq : [vY := t] TRef (TRef tY) = TRef (TRef t)) by reflexivity).
+(rewrite Heq in Hcontra).
+clear Heq.
+(apply match_ty_ref__inv in Hcontra).
+(destruct Hcontra as [t' [Heq Href]]).
+(inversion Heq; subst).
+clear Heq.
+(unfold sem_eq_k in Href).
+(destruct Href as [Href _]).
+specialize (Href 1).
+Unset Silent.
+(destruct Href as [w2 Hsem]).
