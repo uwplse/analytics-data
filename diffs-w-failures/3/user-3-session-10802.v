bcba52afbac5851da5900df414ef6912b2ef29a3
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:33:38 -0700

    user 3, session 10802, part 0

---
+++ diffs-w-failures/3/user-3-session-10802.v
@@ -0,0 +1,40 @@
+Time Ltac subst_var := repeat match goal with
+                              | v:=_:_ |- _ => subst v
+                              end.
+Time Create HintDb false.
+Time Ltac solve_false := solve [ exfalso; eauto with false ].
+Time
+Ltac
+ rename_by_type type name :=
+  match goal with
+  | x:type |- _ => rename x into name
+  end.
+Time Ltac is_one_goal := let n := numgoals in
+                         guard
+                         n=1.
+Time
+Ltac
+ add_hypothesis pf :=
+  let P := type of pf in
+  lazymatch goal with
+  | H:P |- _ => fail "already known"
+  | _ => pose proof pf
+  end.
+Time Tactic Notation "gen" constr(X1) := generalize dependent X1.
+Time Tactic Notation "gen" constr(X1) constr(X2) := (gen X2; gen X1).
+Time
+Tactic Notation "gen" constr(X1) constr(X2) constr(X3) :=
+ (gen X3; gen X2; gen X1).
+Time
+Tactic Notation "gen" constr(X1) constr(X2) constr(X3) constr(X4) :=
+ (gen X4; gen X3; gen X2; gen X1).
+Time
+Tactic Notation "gen" constr(X1) constr(X2) constr(X3) constr(X4) constr(X5)
+ := (gen X5; gen X4; gen X3; gen X2; gen X1).
+Time
+Tactic Notation "pose" "unfolded" constr(pf) tactic(t) :=
+ (let H := fresh in
+  pose proof pf as H; t H;
+   repeat match goal with
+          | H:_ /\ _ |- _ => destruct H
+          end).
