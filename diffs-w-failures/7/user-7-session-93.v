647e95b8946c600802da88a4f495ae8bf4ed8945
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:07:23 -0700

    user 7, session 93, part 0

---
+++ diffs-w-failures/7/user-7-session-93.v
@@ -0,0 +1,20 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 148.
+Set Silent.
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Create HintDb DBBetaJulia.
+Inductive cname : Type :=
+  | NInt : _
+  | NFlt : _
+  | NStr : _.
+Unset Silent.
+Inductive tvar : Type :=
+    nat : _.
