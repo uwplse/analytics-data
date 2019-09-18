552db671b4fc8db0e968034ca5133a2b7c5294e4
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:22 -0700

    user 7, session 19, part 681

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -555,23 +555,11 @@ Lemma nf_sem_sub_k_i__sub_d : forall (k : nat) (t1 : ty), InNF( t1) -> | t1 | <=
 Proof.
 Unset Silent.
 Set Printing Width 148.
+Set Printing Width 148.
 (induction k;
   match goal with
   | |- forall t1 : ty, InNF( t1) -> | t1 | <= ?k -> forall t2 : ty, ||-[ ?k][t1]<= [t2] -> |- t1 << t2 =>
         apply
          (in_nf_mut (fun (t1 : ty) (_ : atom_type t1) => | t1 | <= k -> forall t2 : ty, ||-[ k][t1]<= [t2] -> |- t1 << t2)
             (fun (t1 : ty) (_ : in_nf t1) => | t1 | <= k -> forall t2 : ty, ||-[ k][t1]<= [t2] -> |- t1 << t2))
-  end;
-  try
-   match goal with
-   | |- context [ |- TCName _ << _ ] => intros c Hdep; apply cname_sem_sub_k_i__sub_d; assumption
-   | |- context [ |- TPair _ _ << _ ] =>
-         intros ta1 ta2 Hat1 IH1 Hat2 IH2 Hdep; assert (Hat : atom_type (TPair ta1 ta2)) by (constructor; assumption);
-          destruct (max_inv_depth_le__components_le _ _ _ Hdep) as [Hdep1 Hdep2]; apply pair_sem_sub_k_i__sub_d; tauto
-   end).
-Set Silent.
--
-Unset Silent.
-(intros t Hnft _ Hdep).
-(inversion Hdep).
--
+  end).
