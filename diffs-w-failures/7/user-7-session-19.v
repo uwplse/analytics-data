4252ac4f622af20ae4fdfe25eaa2a1f2194828e4
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:20 -0700

    user 7, session 19, part 361

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -219,7 +219,12 @@ Proof.
      end ])).
 (assert (Hv : value_type (TCName c)) by constructor).
 (pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ H) as Hsemu).
-(destruct Hsemu as [Hsemu| Hsemu]; [ apply Nat.le_trans with (| t'1 |) | apply Nat.le_trans with (| t'2 |) ];
-  tauto || apply Max.le_max_l || apply Max.le_max_r).
+Set Printing Width 148.
+Set Silent.
+admit.
+Unset Silent.
+admit.
+Set Silent.
+admit.
 Unset Silent.
 admit.
