db6fef57ff41d437dfee3d3ccf74301d192407ae
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:47:20 -0700

    user 3, session 960, part 45

--- diffs-w-failures/3/user-3-session-960.v
+++ diffs-w-failures/3/user-3-session-960.v
@@ -124,4 +124,9 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqfRsBS8"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
+Unset Silent.
+Set Diffs "off".
+Set Printing Width 78.
+Show.
 Proof.
+Search -"div" -"smaller".
