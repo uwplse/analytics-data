0caa3996b180af92a9315464d4f7a9feb0498ebe
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:03:35 -0700

    user 3, session 4633, part 0

---
+++ diffs-w-failures/3/user-3-session-4633.v
@@ -0,0 +1,32 @@
+Time From iris.algebra Require Import auth agree functions csum.
+Time From Armada.CSL Require Import Counting.
+Time From iris.base_logic.lib Require Export own.
+Time From iris.bi.lib Require Import fractional.
+Time From iris.proofmode Require Import tactics.
+Time Require Export Armada.Spec.LockDefs.
+Time Set Default Proof Using "Type".
+Time Import uPred.
+Time From Classes Require Import EqualDec.
+Time Import EqualDecNotation.
+Time #[global]
+Instance partial_fn_insert  (A T : Type) `{EqualDec A}:
+ (Insert A T (A \226\134\146 option T)) :=
+ (\206\187 (a : A) (t : T) (f : A \226\134\146 option T) (b : A),
+    if b == a then Some t else f b).
+Time #[global]
+Instance partial_fn_delete  (A T : Type) `{EqualDec A}:
+ (Delete A (A \226\134\146 option T)) :=
+ (\206\187 (a : A) (f : A \226\134\146 option T) (b : A), if b == a then None else f b).
+Time Definition lockR := csumR natR (agreeR unitO).
+Time
+Definition to_lock (s : LockStatus) : lockR :=
+  match s with
+  | Locked => Cinr (to_agree tt)
+  | ReadLocked n => Cinl (S n)
+  | Unlocked => Cinl O
+  end.
+Time
+Definition gen_heapUR (L V : Type) `{EqualDec L} : ucmraT :=
+  ofe_funUR
+    (fun a : L =>
+     optionUR (prodR countingR (prodR lockR (agreeR (leibnizO V))))).
