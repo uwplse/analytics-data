bce7dec6aa148f8242b2661bef05b9993cf41e81
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:34:45 -0700

    user 3, session 10983, part 0

---
+++ diffs-w-failures/3/user-3-session-10983.v
@@ -0,0 +1,11 @@
+Time From Coq Require Export List.
+Time From Coq Require Export Omega.
+Time From Classes Require Export Classes.
+Time From Transitions Require Export Relations.
+Time Require Export Tactical.ProofAutomation.
+Time Require Export Spec.Proc.
+Time Require Export Spec.ProcTheorems.
+Time Require Export Spec.Layer.
+Time Export ProcNotations.
+Time From iris.proofmode Require Export tactics.
+Time From iris.program_logic Require weakestpre.
