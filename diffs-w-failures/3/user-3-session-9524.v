e8d1f3c84761ba50ce7a700bb5f308c25ca85530
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:26:56 -0700

    user 3, session 9524, part 0

---
+++ diffs-w-failures/3/user-3-session-9524.v
@@ -0,0 +1,194 @@
+Require Import POCS.
+Require Import POCS.
+Require Import POCS.
+Definition State := disk.
+Definition read_spec (a : addr) : Specification _ block unit State :=
+  fun (_ : unit) state =>
+  {|
+  pre := True;
+  post := fun r state' => state' = state /\ diskGet state a =?= r;
+  recovered := fun _ state' => state' = state |}.
+Definition write_spec (a : addr) (v : block) :
+  Specification _ _ unit State :=
+  fun (_ : unit) state =>
+  {|
+  pre := True;
+  post := fun r state' => r = tt /\ state' = diskUpd state a v;
+  recovered := fun _ state' => state' = state \/ state' = diskUpd state a v |}.
+Definition size_spec : Specification _ nat unit State :=
+  fun (_ : unit) state =>
+  {|
+  pre := True;
+  post := fun r state' => state' = state /\ r = diskSize state;
+  recovered := fun _ state' => state' = state |}.
+Module Type OneDiskAPI.
+Axiom (init : proc InitResult).
+Record State := mkState {stateDisk : disk; stateBadBlock : addr}.
+Definition read_spec a : Specification _ block unit State :=
+  fun (_ : unit) state =>
+  {|
+  pre := True;
+  post := fun r state' =>
+          state' = state /\
+          (a <> stateBadBlock state -> diskGet (stateDisk state) a =?= r);
+  recovered := fun _ state' => state' = state |}.
+Definition write_spec a v : Specification _ _ unit State :=
+  fun (_ : unit) state =>
+  {|
+  pre := True;
+  post := fun r state' =>
+          r = tt /\
+          state' =
+          mkState (diskUpd (stateDisk state) a v) (stateBadBlock state);
+  recovered := fun _ state' =>
+               state' = state \/
+               state' =
+               mkState (diskUpd (stateDisk state) a v) (stateBadBlock state) |}.
+Definition getBadBlock_spec : Specification _ addr unit State :=
+  fun (_ : unit) state =>
+  {|
+  pre := True;
+  post := fun r state' => state' = state /\ r = stateBadBlock state;
+  recovered := fun _ state' => state' = state |}.
+Axiom (Handle : Type).
+Inductive Request :=
+  | Read : forall (h : Handle) (off : addr) (blocks : nat), _
+  | Write :
+      forall (h : Handle) (off : addr) (len : nat)
+        (dat : bytes (len * blockbytes)), _
+  | Flush : forall h : Handle, _
+  | Disconnect : _
+  | UnknownOp : forall h : Handle, _.
+Inductive ErrorCode :=
+  | ESuccess : _
+  | EInvalid : _
+  | ENospc : _.
+Record Response :={rhandle : Handle;
+                   error : ErrorCode;
+                   data_len : nat;
+                   data : bytes data_len}.
+Record State := mkState {StateDisk : disk; StateReq : option Request}.
+Definition inited (s : State) : Prop := StateReq s = None.
+Axiom (read : addr -> proc block).
+Axiom (write : addr -> block -> proc unit).
+Axiom (size : proc nat).
+Axiom (recover : proc unit).
+Axiom (abstr : Abstraction State).
+Axiom (init_ok : init_abstraction init recover abstr inited_any).
+Axiom (read_ok : forall a, proc_spec (read_spec a) (read a) recover abstr).
+Axiom
+  (write_ok :
+     forall a v, proc_spec (write_spec a v) (write a v) recover abstr).
+Axiom (size_ok : proc_spec size_spec size recover abstr).
+Axiom (recover_wipe : rec_wipe recover abstr no_wipe).
+Hint Resolve init_ok: core.
+Hint Resolve read_ok: core.
+Hint Resolve write_ok: core.
+Hint Resolve size_ok: core.
+Hint Resolve recover_wipe: core.
+End OneDiskAPI.
+Definition size_spec : Specification _ nat unit State :=
+  fun (_ : unit) state =>
+  {|
+  pre := True;
+  post := fun r state' => state' = state /\ r = diskSize (stateDisk state);
+  recovered := fun _ state' => state' = state |}.
+Module Type BadBlockAPI.
+Axiom (init : proc InitResult).
+Axiom (read : addr -> proc block).
+Axiom (write : addr -> block -> proc unit).
+Axiom (getBadBlock : proc addr).
+Axiom (size : proc nat).
+Axiom (recover : proc unit).
+Axiom (abstr : Abstraction State).
+Axiom (init_ok : init_abstraction init recover abstr inited_any).
+Axiom (read_ok : forall a, proc_spec (read_spec a) (read a) recover abstr).
+Axiom
+  (write_ok :
+     forall a v, proc_spec (write_spec a v) (write a v) recover abstr).
+Axiom (getBadBlock_ok : proc_spec getBadBlock_spec getBadBlock recover abstr).
+Axiom (size_ok : proc_spec size_spec size recover abstr).
+Axiom (recover_wipe : rec_wipe recover abstr no_wipe).
+Fixpoint read_match (d : disk) (off : addr) (blocks : nat) :
+bytes (blocks * blockbytes) -> Prop :=
+  match blocks with
+  | O => fun data => True
+  | S blocks' =>
+      fun data : bytes (S blocks' * blockbytes) =>
+      let (thisdata, otherdata) := bsplit data in
+      (diskGet d off = Some thisdata \/ diskGet d off = None) /\
+      read_match d (S off) blocks' otherdata
+  end.
+Fixpoint write_upd (d : disk) (off : addr) (blocks : list (bytes blockbytes))
+   : disk :=
+  match blocks with
+  | nil => d
+  | b :: blocks' => write_upd (diskUpd d off b) (S off) blocks'
+  end.
+Hint Resolve init_ok: core.
+Hint Resolve read_ok: core.
+Hint Resolve write_ok: core.
+Hint Resolve getBadBlock_ok: core.
+Hint Resolve size_ok: core.
+Hint Resolve recover_wipe: core.
+End BadBlockAPI.
+Definition read_match' (d : disk) (off : addr) (blocks : nat) 
+  (len : nat) (H : len = blocks * blockbytes) (data : bytes len) : Prop.
+(rewrite H in data).
+exact (read_match d off blocks data).
+Defined.
+Definition getRequest_spec : Specification _ Request unit State :=
+  fun (_ : unit) state =>
+  {|
+  pre := True;
+  post := fun r state' => state' = mkState (StateDisk state) (Some r);
+  recovered := fun _ state' => state' = mkState (StateDisk state) None |}.
+Definition sendResponse_spec (resp : Response) :
+  Specification _ unit unit State :=
+  fun '(disk, disk', req) state =>
+  {|
+  pre := let
+         '@Build_Response rhandle error data_len data := resp in
+          state = mkState disk (Some req) /\
+          match req with
+          | Read h off blocks =>
+              h = rhandle /\
+              disk' = disk /\
+              error = ESuccess /\
+              (exists Hlen, @read_match' disk off blocks data_len Hlen data) \/
+              error <> ESuccess /\ data_len = 0
+          | Write h off len data =>
+              h = rhandle /\
+              data_len = 0 /\
+              error = ESuccess /\
+              disk' = write_upd disk off (bsplit_list data) \/
+              error <> ESuccess /\ disk' = disk
+          | Flush h => h = rhandle /\ data_len = 0 /\ disk' = disk
+          | Disconnect => False
+          | UnknownOp h =>
+              h = rhandle /\ error = EInvalid /\ data_len = 0 /\ disk' = disk
+          end;
+  post := fun r state' => state' = mkState disk' None;
+  recovered := fun _ state' =>
+               state' = mkState disk None \/ state' = mkState disk' None |}.
+Definition wipe_req (state state' : State) :=
+  state' = mkState (StateDisk state) None.
+Hint Unfold wipe_req: core.
+Module Type NbdAPI.
+Axiom (init : proc InitResult).
+Axiom (getRequest : proc Request).
+Axiom (sendResponse : Response -> proc unit).
+Axiom (recover : proc unit).
+Axiom (abstr : Abstraction State).
+Axiom (init_ok : init_abstraction init recover abstr inited).
+Axiom (getRequest_ok : proc_spec getRequest_spec getRequest recover abstr).
+Axiom
+  (sendResponse_ok :
+     forall resp,
+     proc_spec (sendResponse_spec resp) (sendResponse resp) recover abstr).
+Axiom (recover_wipe : rec_wipe recover abstr wipe_req).
+Hint Resolve init_ok: core.
+Hint Resolve getRequest_ok: core.
+Hint Resolve sendResponse_ok: core.
+Hint Resolve recover_wipe: core.
+End NbdAPI.
