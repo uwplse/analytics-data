e905643ae152b6db083f7bff86efbf19d5c8fa3f
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:43:48 -0700

    user 3, session 159, part 5

--- diffs-w-failures/3/user-3-session-159.v
+++ diffs-w-failures/3/user-3-session-159.v
@@ -34,47 +34,13 @@ Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 Set Silent.
-Require Import Spec.Hoare.
-Require Import Spec.HoareTactics.
-Require Import Spec.AbstractionSpec.
-Definition absr : relation DB.l.(State) Var.l.(State) unit :=
-  fun l res =>
-  match res with
-  | Val s _ => fst s = fold_right plus 0 l /\ snd s = length l
-  | Err _ _ => False
-  end.
-Unset Silent.
-Instance absr_non_error : (NonError absr).
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq6gXHSd"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqdPtyvh"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
-Proof.
 Unset Silent.
 Set Diffs "off".
-Timeout 1 Check @non_erroring.
 Set Printing Width 78.
-Show.
-(hnf; simpl; auto).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqqs9j8f"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Qed.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqt6HaGC"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqB6UgcK"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+Set Silent.
+Instance absr_non_error : (NonError absr).
+Unset Silent.
+Proof.
+Timeout 1 Check @compile_exec_ok.
+Timeout 1 Check @compile_exec_ok.
+(compute).
