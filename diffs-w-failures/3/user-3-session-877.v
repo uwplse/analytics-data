514a4f0d23fdc7592064f2bd314535f00d0fea94
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:46:56 -0700

    user 3, session 877, part 5

--- diffs-w-failures/3/user-3-session-877.v
+++ diffs-w-failures/3/user-3-session-877.v
@@ -1377,4 +1377,4 @@ Unset Silent.
 Set Diffs "off".
 Set Printing Width 104.
 Show.
-iDestruct "Hmbox" as "(Hwlock&Hlockinv&Hstatus)".
+(<ssreflect_plugin::ssrtclseq@0> iDestruct "Hmbox" as "[>Hmbox|Hmbox]" ; last  first).
