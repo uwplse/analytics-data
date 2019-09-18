9d79d17ed5618ad79c26423ae63f126628c20b9a
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:17 -0700

    user 7, session 19, part 40

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -68,15 +68,15 @@ specialize (Href v' Hv').
 Set Printing Width 148.
 Lemma match_ty_i_k__match_le_k : forall (k : nat) (v t : ty), |-[ k] v <$ t -> forall k' : nat, k' <= k -> |-[ k'] v <$ t.
 Set Silent.
-Proof.
-Unset Silent.
+Set Printing Width 148.
 (induction k; intros v t; generalize dependent v; induction t; intros v Hm k' Hle;
   try
    match goal with
-   | |- |-[ ?k'] ?v <$ TCName _ => apply match_ty_i_cname__inv in Hm; subst; reflexivity
+   | |- |-[ ?k'] ?v <$ TCName _ => apply match_ty_i_cname__inv in Hm; subst; destruct k'; reflexivity
    | |- |-[ ?k'] ?v <$ TPair _ _ => apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; apply match_ty_i_pair; tauto
    | |- |-[ ?k'] ?v <$ TUnion _ _ =>
          apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto
    end).
-(apply match_ty_i_cname__inv in Hm; subst).
-(destruct k'; reflexivity).
+Show.
+(apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+Show.
