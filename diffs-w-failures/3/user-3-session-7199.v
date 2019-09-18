383e446089c227a1abd3571bd7f1505d3c987a4f
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:16:04 -0700

    user 3, session 7199, part 103

--- diffs-w-failures/3/user-3-session-7199.v
+++ diffs-w-failures/3/user-3-session-7199.v
@@ -590,15 +590,15 @@ Set Diffs "off".
 Set Printing Width 78.
 Unset Silent.
 Set Diffs "off".
-Set Printing Width 78.
-Set Silent.
-Lemma firstn_one_more :
-  forall (a : nat) (state : list block),
-  S a <= length state ->
-  firstn a state ++ [nth a state block0] = firstn (S a) state.
-Proof.
-Unset Silent.
-(intros a state H).
-Timeout 1 Check @sig.
 Unset Silent.
 Set Diffs "off".
+Set Printing Width 78.
+Lemma firstn_one_more :
+  forall (a : nat) (d : list block),
+  S a <= length d -> firstn a d ++ [nth a d block0] = firstn (S a) d.
+Proof.
+Timeout 1 Check @Ascii.nat_ascii_embedding.
+(intros).
+Timeout 1 Check @Ascii.nat_ascii_embedding.
+Timeout 1 Check @find.
+Print firstn.
