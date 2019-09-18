1809dd86e6cf0522b8966ab1020eda5784f19d94
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:43:48 -0700

    user 3, session 159, part 0

---
+++ diffs-w-failures/3/user-3-session-159.v
@@ -0,0 +1,12 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0oM8Ug"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 78.
+Set Silent.
+Require Import Examples.StatDb.Impl.
+Require Import Helpers.RelationRewriting.
