55e8025cfc368fb9b2113be095a47c73bfbf6409
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:36:43 -0700

    user 3, session 11371, part 0

---
+++ diffs-w-failures/3/user-3-session-11371.v
@@ -0,0 +1,232 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqDiPe1x"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 51.
+Set Silent.
+Require Export LF.Basics.
+Theorem plus_n_O_firsttry :
+  forall n : nat, n = n + 0.
+Proof.
+(intros n).
+(simpl).
+Abort.
+Theorem plus_n_O_secondtry :
+  forall n : nat, n = n + 0.
+Proof.
+(intros n).
+(destruct n as [| n']).
+-
+reflexivity.
+-
+(simpl).
+Abort.
+Theorem plus_n_O : forall n : nat, n = n + 0.
+Proof.
+(intros n).
+(induction n as [| n' IHn']).
+-
+reflexivity.
+-
+(simpl).
+(rewrite <- IHn').
+reflexivity.
+Qed.
+Theorem minus_diag : forall n, minus n n = 0.
+Proof.
+(intros n).
+(induction n as [| n' IHn']).
+-
+(simpl).
+reflexivity.
+-
+(simpl).
+(rewrite IHn').
+reflexivity.
+Qed.
+Theorem mult_0_r : forall n : nat, n * 0 = 0.
+Proof.
+(intros n).
+(induction n as [| n' IHn']).
+-
+reflexivity.
+-
+(simpl).
+(rewrite IHn').
+reflexivity.
+Qed.
+Theorem plus_n_Sm :
+  forall n m : nat, S (n + m) = n + S m.
+Proof.
+(intros n m).
+(induction n as [| n' IHn']).
+-
+reflexivity.
+-
+(simpl).
+(rewrite IHn').
+reflexivity.
+Qed.
+Theorem plus_comm :
+  forall n m : nat, n + m = m + n.
+Proof.
+(intros n m).
+(induction n as [| n' IHn']).
+-
+(rewrite <- plus_n_O).
+reflexivity.
+-
+(simpl).
+(rewrite IHn').
+(rewrite plus_n_Sm).
+reflexivity.
+Qed.
+Theorem plus_assoc :
+  forall n m p : nat, n + (m + p) = n + m + p.
+Proof.
+(intros n m p).
+(induction n as [| n' IHn']).
+-
+reflexivity.
+-
+(simpl).
+(rewrite IHn').
+reflexivity.
+Qed.
+Fixpoint double (n : nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+Lemma double_plus : forall n, double n = n + n.
+Proof.
+(intros n).
+(induction n as [| n' IHn']).
+-
+reflexivity.
+-
+(simpl).
+(rewrite IHn').
+(rewrite plus_n_Sm).
+reflexivity.
+Qed.
+Theorem evenb_S :
+  forall n : nat, evenb (S n) = negb (evenb n).
+Proof.
+(intros n).
+(induction n as [| n' IHn']).
+-
+reflexivity.
+-
+(rewrite IHn').
+(rewrite negb_involutive).
+reflexivity.
+Qed.
+Theorem mult_0_plus' :
+  forall n m : nat, (0 + n) * m = n * m.
+Proof.
+(intros n m).
+(assert (H : 0 + n = n)).
+{
+reflexivity.
+}
+(rewrite H).
+reflexivity.
+Qed.
+Theorem plus_rearrange_firsttry :
+  forall n m p q : nat,
+  n + m + (p + q) = m + n + (p + q).
+Proof.
+(intros n m p q).
+(rewrite plus_comm).
+Abort.
+Theorem plus_rearrange :
+  forall n m p q : nat,
+  n + m + (p + q) = m + n + (p + q).
+Proof.
+(intros n m p q).
+(assert (H : n + m = m + n)).
+{
+(rewrite plus_comm).
+reflexivity.
+}
+(rewrite H).
+reflexivity.
+Qed.
+Theorem plus_assoc' :
+  forall n m p : nat, n + (m + p) = n + m + p.
+Proof.
+(intros n m p).
+(induction n as [| n' IHn']).
+reflexivity.
+(simpl).
+(rewrite IHn').
+reflexivity.
+Qed.
+Theorem plus_assoc'' :
+  forall n m p : nat, n + (m + p) = n + m + p.
+Proof.
+(intros n m p).
+(induction n as [| n' IHn']).
+-
+reflexivity.
+-
+(simpl).
+(rewrite IHn').
+reflexivity.
+Qed.
+Theorem plus_swap :
+  forall n m p : nat, n + (m + p) = m + (n + p).
+Proof.
+(intros n m p).
+(rewrite plus_assoc).
+(assert (n + m = m + n)).
+{
+(rewrite plus_comm).
+reflexivity.
+}
+(rewrite H).
+(rewrite plus_assoc).
+reflexivity.
+Qed.
+Unset Silent.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqilpubO"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqfpDDta"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Set Silent.
+Theorem mult_comm' :
+  forall m n : nat, m * n = n * m.
+Proof.
+(intros).
+(induction n as [| n' IHn']).
+-
+(simpl).
+(rewrite mult_0_r).
+reflexivity.
+-
+Unset Silent.
+(simpl).
+(assert (H1 : m * S n' = m + m * n')).
+{
+(induction m as [| m' IHm']).
+-
+(simpl).
+reflexivity.
+-
+(simpl).
+(rewrite IHm').
+(rewrite plus_swap).
+reflexivity.
