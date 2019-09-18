35173b3154a2db3c8b732e58da05461b9846c378
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:34:30 -0700

    user 3, session 10945, part 0

---
+++ diffs-w-failures/3/user-3-session-10945.v
@@ -0,0 +1,8 @@
+Time From iris.base_logic Require Export derived proofmode.
+Time From iris.bi Require Export bi.
+Time Set Default Proof Using "Type".
+Time Module Import uPred.
+Time Export base_logic.bi.uPred.
+Time Export derived.uPred.
+Time Export bi.bi.
+Time End uPred.
