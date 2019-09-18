e4a0a2ad7296b832db63c6318328a84791fc9b32
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:15:43 -0700

    user 7, session 104, part 1

--- diffs-w-failures/7/user-7-session-104.v
+++ diffs-w-failures/7/user-7-session-104.v
@@ -3,11 +3,14 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
+Set Printing Width 148.
 Set Silent.
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0270a.BaseDefs.
-Require Import BetaJulia.Sub0270a.BaseMatchProps.
+Require Import BetaJulia.Sub0280a.BaseDefs.
+Unset Silent.
+Require Import BetaJulia.Sub0280a.BaseMatchProps.
+Set Silent.
 Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
@@ -16,5 +19,14 @@ Open Scope btjm.
 Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
 Proof.
 (intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
-Unset Silent.
 Qed.
+Definition sem_sub (t1 t2 : ty) := forall k : nat, ||-[ k][t1]<= [t2].
+Unset Silent.
+Notation "'||-' '[' t1 ']' '<=' '[' t2 ']'" := (sem_sub t1 t2) (at level 50) : btjm_scope.
+Lemma sem_sub__refint_eXrefX : ||- [TRef tint]<= [TExist vX (TRef tX)].
+Proof.
+Show.
+(intros k).
+(destruct k; intros w1; exists 1).
+-
+(intros v Hm).
