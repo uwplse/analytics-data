4130c89deb4cf42fa9ec11dfa5a6e69914603d33
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:30:58 -0700

    user 3, session 10289, part 7

--- diffs-w-failures/3/user-3-session-10289.v
+++ diffs-w-failures/3/user-3-session-10289.v
@@ -116,5 +116,35 @@ Proof.
 Qed.
 Hint Resolve tt: core.
 Unset Silent.
-Inductive Marker : string -> Type :=
+Inductive Marker : String.string -> Type :=
     mark : forall s, Marker s.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqExPtxA"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0PM91J"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Theorem proc_spec_rx :
+  forall `(spec : Specification A T R State) `(p : proc T) 
+    `(rec : proc R) `(rx : T -> proc T')
+    `(spec' : Specification A' T' R State) `(abs : Abstraction State),
+  proc_spec spec p rec abs ->
+  (forall a' state,
+   pre (spec' a' state) ->
+   exists a,
+     pre (spec a state) /\
+     (forall r state',
+      recovered (spec a state) r state' ->
+      Marker "recovered condition" -> recovered (spec' a' state) r state') /\
+     (forall r,
+      Marker "post condition" ->
+      proc_spec
+        (fun (_ : unit) state' =>
+         {|
+         pre := post (spec a state) r state';
+         post := fun r state'' => post (spec' a' state) r state'';
+         recovered := fun r state'' => recovered (spec' a' state) r state'' |})
+        (rx r) rec abs)) -> proc_spec spec' (Bind p rx) rec abs.
