d13a9c19962bfd71fd50aca237932968a1947be7
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:57:15 -0700

    user 3, session 3324, part 8

--- diffs-w-failures/3/user-3-session-3324.v
+++ diffs-w-failures/3/user-3-session-3324.v
@@ -145,24 +145,5 @@ step_proc.
 (case_eq (diskGet (stateDisk state) (diskSize (stateDisk state) - 1)); intros).
 {
 exists (diskUpd (diskShrink (stateDisk state)) (stateBadBlock state) b).
-(unfold inited_any).
-Unset Silent.
-Set Diffs "off".
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqVPbCeu"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqu1n37z"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
-Set Printing Width 78.
-Show.
-Unset Silent.
-Set Diffs "off".
-Set Printing Width 78.
-Show.
 Unset Silent.
 Set Diffs "off".
