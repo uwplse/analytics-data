477a4d4a922b761aad826b2d2b440199671ad120
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:05:57 -0700

    user 7, session 16, part 2

--- diffs-w-failures/7/user-7-session-16.v
+++ diffs-w-failures/7/user-7-session-16.v
@@ -332,13 +332,12 @@ assumption.
 Qed.
 Lemma nf_sub_r__decidable : forall t1 t2 : ty, InNF( t1) -> Decidable.decidable (|- t1 << t2).
 Proof.
-(intros t1 t2 Hnf1).
-generalize dependent t2.
-Check in_nf.
-Search -in_nf.
 Set Printing Width 148.
+Set Silent.
+(intros t1 t2).
+generalize dependent t2.
+Unset Silent.
+generalize dependent t1.
 (apply
   (in_nf_mut (fun (t1 : ty) (Hat : atom_type t1) => forall t2 : ty, Decidable.decidable (|- t1 << t2))
      (fun (t1 : ty) (Hnf : in_nf t1) => forall t2 : ty, Decidable.decidable (|- t1 << t2)))).
-Show.
--
