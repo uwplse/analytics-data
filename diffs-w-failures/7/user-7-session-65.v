85df919a5a451bb4ddfb5bcef5f84d5dd51fad5d
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:45:47 -0700

    user 7, session 65, part 7

--- diffs-w-failures/7/user-7-session-65.v
+++ diffs-w-failures/7/user-7-session-65.v
@@ -160,6 +160,7 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 (induction k;
   match goal with
   | |- forall t1 : ty, InNF( t1) -> | t1 | <= ?k -> forall t2 : ty, ||-[ ?k][t1]<= [t2] -> |- t1 << t2 =>
@@ -175,8 +176,7 @@ Set Printing Width 148.
           destruct (max_inv_depth_le__inv _ _ _ Hdep) as [Hdep1 Hdep2]; specialize (IH1 Hdep1); specialize (IH2 Hdep2); 
           apply pair_sem_sub_k__sub_d; assumption
    | |- context [ |- TUnion _ _ << _ ] =>
-         intros t1 t2 Hnf1 IH1 Hnf2 IH2 Hdep; destruct (max_inv_depth_le__components_le _ _ _ Hdep) as [Hdep1 Hdep2]; intros t' Hsem;
+         intros t1 t2 Hnf1 IH1 Hnf2 IH2 Hdep; destruct (max_inv_depth_le__inv _ _ _ Hdep) as [Hdep1 Hdep2]; intros t' Hsem;
           apply sem_sub_k_union_l__inv in Hsem; destruct Hsem as [Hsem1 Hsem2]; constructor; auto
    | |- forall ta : ty, atom_type ta -> _ => tauto
    end).
-Show.
