6793eda810e15d6e2c4328c92ffe7a857195da3e
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:15:43 -0700

    user 7, session 104, part 20

--- diffs-w-failures/7/user-7-session-104.v
+++ diffs-w-failures/7/user-7-session-104.v
@@ -74,10 +74,45 @@ assumption.
 Set Printing Width 148.
 Lemma sem_eq_k__exists_not : forall (k : nat) (t : ty), exists t' : ty, ~ ||-[ k][t']= [t].
 Proof.
+Set Printing Width 148.
+Set Silent.
+(intros k t).
 (induction k; induction t).
-Show.
-Show.
-Set Printing Width 148.
-Set Printing Width 148.
 admit.
 admit.
+Unset Silent.
+-
+Show.
+(destruct IHt1 as [t'1 Hnot1]).
+exists t'1.
+(intros Hcontra).
+Admitted.
+Set Silent.
+Lemma not_sem_sub__refeXrefX_eYrefrefY : ~ ||- [TRef (TExist vX (TRef tX))]<= [TExist vY (TRef (TRef tY))].
+Proof.
+(intros Hcontra).
+specialize (Hcontra 2 1).
+(destruct Hcontra as [w Hcontra]).
+(assert (Hm : |-[ 2, 1] TRef (TExist vX (TRef tX)) <$ TRef (TExist vX (TRef tX))) by (apply match_ty_value_type__reflexive; constructor)).
+(unfold sem_sub_k_w in Hcontra).
+specialize (Hcontra _ Hm).
+clear Hm.
+(destruct w).
+-
+(apply Hcontra).
+-
+(apply match_ty_exist__inv in Hcontra).
+(destruct Hcontra as [t Hcontra]).
+(assert (Heq : [vY := t] TRef (TRef tY) = TRef (TRef t)) by reflexivity).
+(rewrite Heq in Hcontra).
+clear Heq.
+(apply match_ty_ref__inv in Hcontra).
+(destruct Hcontra as [t' [Heq Href]]).
+(inversion Heq; subst).
+clear Heq.
+(unfold sem_eq_k in Href).
+(destruct Href as [Href _]).
+specialize (Href 1).
+Unset Silent.
+(destruct Href as [w2 Hsem]).
+(pose proof (sem_eq_k__exists_not 0 t)).
