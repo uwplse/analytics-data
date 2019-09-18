76952d41bc147d4b6971f5676711e1ddf43d3d86
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:47:21 -0700

    user 3, session 960, part 106

--- diffs-w-failures/3/user-3-session-960.v
+++ diffs-w-failures/3/user-3-session-960.v
@@ -82,7 +82,7 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Function
- nat_to_le base (x : nat) {struct x} : list {x : nat | x < S (S base)} :=
+ nat_to_le base (x : nat) {measure x : list {x : nat | x < S (S base)} :=
    match x with
    | 0 => nil
    | _ =>
