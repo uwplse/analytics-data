f0693dce7841dccf529d2adb4f1d9968bb4a0dea
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:19 -0700

    user 7, session 19, part 299

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -206,11 +206,12 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 (try
   match goal with
   | Hsem:forall v, value_type v -> |-[ ?k] v <$ TCName ?c -> _
     |- _ =>
         assert (Hvv : value_type (TCName c)) by constructor;
-         assert (Hmv : |-[ k] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption)
+         assert (Hmv : |-[ k] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hvv Hmv)
   end).
 Show.
