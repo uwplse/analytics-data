2b201684c63b9ebe2474e10331862b82547ec615
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:04:17 -0700

    user 3, session 4761, part 0

---
+++ diffs-w-failures/3/user-3-session-4761.v
@@ -0,0 +1,6 @@
+Notation "a === b" := (a = b) (at level 100).
+Definition aReallyBigIdentifier := 3.
+Definition anotherLongName := 10.
+Check
+  1 + 2 + aReallyBigIdentifier + 4 + anotherLongName ===
+  6 + anotherLongName + 8 + 9.
