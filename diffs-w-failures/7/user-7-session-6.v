8bbb1596f6a215f6c4c4d89ee45f034f846ba4c0
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 17:58:22 -0700

    user 7, session 6, part 1

--- diffs-w-failures/7/user-7-session-6.v
+++ diffs-w-failures/7/user-7-session-6.v
@@ -5,5 +5,16 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
 Set Silent.
 Add LoadPath "../..".
+Set Printing Width 148.
+Set Silent.
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0250b.BaseDefs.
+Require Import BetaJulia.Sub0250b.BaseProps.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Close Scope btj_scope.
+Open Scope btjnf_scope.
 Unset Silent.
-Require Import Analytical.Analytics.
+Open Scope btjr_scope.
