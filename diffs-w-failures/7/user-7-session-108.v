ee212057cc8ef968b98bf1973ee311036b784d18
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:18:46 -0700

    user 7, session 108, part 46

--- diffs-w-failures/7/user-7-session-108.v
+++ diffs-w-failures/7/user-7-session-108.v
@@ -272,4 +272,5 @@ Unset Silent.
 (assert (Hcontra' : ~ ((exists v1 : ty, |-[ k, w] v1 <$ t1) /\ (exists v2 : ty, |-[ k, w] v2 <$ t2)))).
 {
 Set Printing Width 148.
-(intros [Hcontra'1 Hcontra'2]).
+Set Printing Width 148.
+(intros [[v1 Hcontra'1] [v2 Hcontra'2]]).
