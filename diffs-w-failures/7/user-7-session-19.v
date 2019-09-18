48402a2ab4229db2eed1454081a2be84618a8584
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:20 -0700

    user 7, session 19, part 377

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -218,11 +218,12 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 (induction k; induction t; induction t'; intros Hnft Hsem; try (solve [ simpl; constructor ]);
   try (solve
    [ match goal with
      | Hsem:||-[ ?k][?t1]<= [?t2]
        |- | ?t1 | <= | ?t2 | =>
-           assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
-            (Hsem _ Hm); contradiction || destruct k; contradiction
+           assert (Hv : value_type t1) by constructor; assert (Hm : |-[ k] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+            (Hsem _ Hm); contradiction
      end ])).
