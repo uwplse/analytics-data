f841af81a3382373459de5cd2cfbb35b8c044985
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:52:45 -0700

    user 7, session 153, part 21

--- diffs-w-failures/7/user-7-session-153.v
+++ diffs-w-failures/7/user-7-session-153.v
@@ -4,34 +4,3 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
 Set Printing Width 148.
-Set Silent.
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0281a.BaseDefs.
-Require Import BetaJulia.Sub0281a.BaseProps.
-Require Import BetaJulia.Sub0281a.BaseMatchProps.
-Require Import BetaJulia.Sub0281a.BaseSemSubProps.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Silent.
-Lemma build_v : forall (X X' : id) (tx : ty) (w : nat) (t v : ty), |-[ w] v <$ [X := tx] t -> exists v' : ty, |-[ w] v' <$ [X := TVar X'] t.
-Proof.
-(intros X X' tx).
-Unset Silent.
-(induction w; induction t; intros v Hm).
-Set Silent.
--
-exists (TCName c).
-Unset Silent.
-(apply match_ty_cname).
-Set Silent.
--
-Show.
-Show.
-Check subst.
