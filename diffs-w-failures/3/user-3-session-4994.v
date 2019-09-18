743f976c144e675e9882c1f434a4ead5ccce3990
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:05:37 -0700

    user 3, session 4994, part 0

---
+++ diffs-w-failures/3/user-3-session-4994.v
@@ -0,0 +1,19 @@
+Require Import POCS.
+Require Import VariablesAPI.
+Extraction Language Haskell.
+Module Vars<: VarsAPI.
+Axiom (init : proc InitResult).
+Axiom (read : var -> proc nat).
+Axiom (write : var -> nat -> proc unit).
+Axiom (recover : proc unit).
+Axiom (abstr : Abstraction State).
+Axiom (init_ok : init_abstraction init recover abstr inited_any).
+Axiom (read_ok : forall v, proc_spec (read_spec v) (read v) recover abstr).
+Axiom
+  (write_ok :
+     forall v val, proc_spec (write_spec v val) (write v val) recover abstr).
+Axiom (recover_wipe : rec_wipe recover abstr no_crash).
+End Vars.
+Extract Constant Vars.init =>  "Variables.Ops.init".
+Extract Constant Vars.read =>  "Variables.Ops.read".
+Extract Constant Vars.write =>  "Variables.Ops.write".
