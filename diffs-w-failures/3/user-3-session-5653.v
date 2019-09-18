77e00bfc69c76b488acac9ae5fff27552a36bc78
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:08:42 -0700

    user 3, session 5653, part 2

--- diffs-w-failures/3/user-3-session-5653.v
+++ diffs-w-failures/3/user-3-session-5653.v
@@ -12,14 +12,5 @@ Require Import LogAPI.
 Unset Silent.
 Set Diffs "off".
 Set Printing Width 78.
-Require Import OneDiskAPI.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqJHYqdf"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqLuR5ez"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
+Unset Silent.
+Set Diffs "off".
