2411d880fd2d19f614d006980777c6ef389a65d4
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:46:12 -0700

    user 3, session 724, part 0

---
+++ diffs-w-failures/3/user-3-session-724.v
@@ -0,0 +1,10 @@
+Time From Classes Require Import EqualDec.
+Time From Armada Require Export Spec.Proc.
+Time From Armada Require Export Spec.LockDefs.
+Time From Armada.Goose Require Export GoZeroValues.
+Time From Armada.Goose Require Export GoLayer.
+Time Export EqualDecNotation.
+Time Export ProcNotations.
+Time Open Scope proc.
+Time Open Scope string.
+Time Notation proc := (Proc.proc Op).
