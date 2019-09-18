041aa8e3e59f5653e2c7cfb0da7bda4ecf4e5094
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:16:19 -0700

    user 3, session 7199, part 363

--- diffs-w-failures/3/user-3-session-7199.v
+++ diffs-w-failures/3/user-3-session-7199.v
@@ -473,7 +473,64 @@ Set Diffs "off".
 Timeout 1 Check @spec_abstraction_compose.
 Set Printing Width 78.
 Set Silent.
-Theorem log_abstraction_preserved d bs d' bs' :
+Unset Silent.
+Theorem log_abstraction_preserved d bs d' :
   log_abstraction d bs ->
   diskGet d' len_addr = diskGet d len_addr ->
   diskSize d' = diskSize d -> log_contents_ok d' bs -> log_abstraction d' bs.
+Proof.
+(unfold log_abstraction, log_length_ok, log_size_ok; intuition).
+-
+replace (diskGet d' len_addr) in *.
+auto.
+-
+congruence.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqQxkvg1"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
+Theorem append_ok :
+  forall v, proc_spec (append_spec v) (append v) recover abstr.
+Proof.
+(unfold append; intros).
+(apply spec_abstraction_compose).
+step_proc.
+(destruct a' as [[] bs]; simpl in *).
+intuition eauto.
+step_proc.
+(descend; intuition eauto).
+Set Silent.
+Set Silent.
+Set Silent.
+destruct matches.
+-
+step_proc.
+(descend; intuition eauto).
+Unset Silent.
+{
+(unfold log_size_ok; autorewrite with list; auto).
+}
+{
+Timeout 1 Check @plus_n_O.
+Timeout 1 Check @log_contents_ok.
+Timeout 1 Check @log_contents_ok.
+Timeout 1 Check @log_addr.
+Timeout 1 Check @log_abstraction.
+Timeout 1 Check @log_abstraction.
+Timeout 1 Check @log_abstraction.
+Timeout 1 Check @log_abstraction.
+Timeout 1 Check @log_abstraction.
+Timeout 1 Check @log_abstraction.
+Timeout 1 Check @log_abstraction.
+Timeout 1 Check @log_abstraction.
+Timeout 1 Check @log_abstraction.
+Timeout 1 Check @log_abstraction_nil.
+(exists bs; intuition eauto using log_abstraction_preserved).
+}
+step_proc.
+intuition.
+{
+eauto using log_abstraction_preserved.
