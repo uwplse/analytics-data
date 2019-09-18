13dcc9bfc9f993154fa6d4e0a09d33c8f9bab13c
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:05:57 -0700

    user 7, session 16, part 1

--- diffs-w-failures/7/user-7-session-16.v
+++ diffs-w-failures/7/user-7-session-16.v
@@ -334,5 +334,11 @@ Lemma nf_sub_r__decidable : forall t1 t2 : ty, InNF( t1) -> Decidable.decidable 
 Proof.
 (intros t1 t2 Hnf1).
 generalize dependent t2.
-Unset Silent.
-(induction Hnf1).
+Check in_nf.
+Search -in_nf.
+Set Printing Width 148.
+(apply
+  (in_nf_mut (fun (t1 : ty) (Hat : atom_type t1) => forall t2 : ty, Decidable.decidable (|- t1 << t2))
+     (fun (t1 : ty) (Hnf : in_nf t1) => forall t2 : ty, Decidable.decidable (|- t1 << t2)))).
+Show.
+-
