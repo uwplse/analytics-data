ab530dff8b26ad2a33d987f2416dfe6295160898
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:47:22 -0700

    user 3, session 960, part 151

--- diffs-w-failures/3/user-3-session-960.v
+++ diffs-w-failures/3/user-3-session-960.v
@@ -226,4 +226,38 @@ Timeout 1 Check @nat_to_le_equation.
 Timeout 1 Check @nat_to_le_equation.
 Timeout 1 Check @nat_to_le_equation.
 Timeout 1 Check @Choice.
-Check nat_to_le_equation.
+Unset Silent.
+Set Diffs "off".
+Set Printing Width 78.
+Fixpoint le_to_nat base (digits : list {x : nat | x < S (S base)}) : nat :=
+  match digits with
+  | nil => 0
+  | digit :: digits' => proj1_sig digit * base + le_to_nat digits'
+  end.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8Bga3l"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqq7lPKp"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Theorem nat_le_inverse base : forall n, le_to_nat (nat_to_le base n) = n.
+Proof.
+(intros).
+(induction n as [n IHn] using lt_wf_ind).
+(destruct n; simpl).
+Timeout 1 Check @pointwise_relation.
+Timeout 1 Check @nat_to_le_equation.
+Timeout 1 Check @nat_to_le_equation.
+Timeout 1 Check @nat_to_le_equation.
+Timeout 1 Check @nat_to_le_equation.
+Timeout 1 Check @nat_to_le_equation.
+Timeout 1 Check @nat_to_le_equation.
+Timeout 1 Check @nat_to_le_equation.
+Timeout 1 Check @nat_to_le_equation.
+Timeout 1 Check @nat_to_le_equation.
+Timeout 1 Check @nat_to_le_equation.
+Timeout 1 Check @sig.
+(rewrite nat_to_le_equation; simpl).
