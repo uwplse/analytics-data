2bad6ab358a3adbb58d553189a3be7f07144865a
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:48:58 -0700

    user 7, session 148, part 5

--- diffs-w-failures/7/user-7-session-148.v
+++ diffs-w-failures/7/user-7-session-148.v
@@ -101,5 +101,6 @@ tauto.
 (simpl in *).
 (induction t').
 +
-(intros k; destruct (match_ty__exists_w_v (TPair ([X := TVar X'] t1) ([X := TVar X'] t2)) k) as [w [v Hm]]; specialize (Hsem k w);
-  destruct Hsem as [w2 Hsem]; specialize (Hsem _ Hm); apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+Unset Silent.
+(intros k).
+Check match_ty__exists_w_v.
