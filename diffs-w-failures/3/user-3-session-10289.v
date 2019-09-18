92d1c4a1e8f91886ad3f37094cd8b960f2f036bf
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:30:58 -0700

    user 3, session 10289, part 5

--- diffs-w-failures/3/user-3-session-10289.v
+++ diffs-w-failures/3/user-3-session-10289.v
@@ -9,4 +9,15 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 78.
 Unset Silent.
 Set Diffs "off".
-Timeout 1 Check @Ret.
+Set Printing Width 78.
+Require Import Coq.Strings.String.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqbBzgV2"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqLU0K7s"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
