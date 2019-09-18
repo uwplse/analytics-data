fd03ef0a182fa43da8b0689fc3311a6bfc018bd1
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:05:58 -0700

    user 7, session 16, part 138

--- diffs-w-failures/7/user-7-session-16.v
+++ diffs-w-failures/7/user-7-session-16.v
@@ -483,10 +483,10 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
-(split; intros t'; induction t'; intros Hnf'; specialize (IH11 _ Hnf'); specialize (IH12 _ Hnf'); specialize (IH21 _ Hnf'); specialize
-  (IH22 _ Hnf'); try (solve [ solve_union_sub_r__decidable IH11 IH21 | solve_atom_sub_r_union__decidable IH12 IH22 ])).
-Show.
+Set Printing Width 148.
+(split; intros t'; induction t'; intros Hnf'; try (inversion Hnf'; subst); specialize (IH11 _ Hnf'); specialize (IH12 _ Hnf'); specialize
+  (IH21 _ Hnf'); specialize (IH22 _ Hnf'); try (solve [ solve_union_sub_r__decidable IH11 IH21 | solve_atom_sub_r_union__decidable IH12 IH22 ])).
 Set Silent.
 +
 Unset Silent.
-(destruct IH12 as [IH12| IH12]; destruct IH22 as [IH22| IH22]; try (solve [ left; constructor; assumption ])).
+(destruct IHt'1 as [IH1| IH1]; destruct IHt'2 as [IH2| IH2]).
