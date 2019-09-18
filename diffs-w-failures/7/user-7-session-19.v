ea0f7d8d54930d4ba725efed6755201df5249e26
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:18 -0700

    user 7, session 19, part 135

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -130,6 +130,7 @@ Unset Silent.
 Show.
 Set Printing Width 148.
 Set Printing Width 148.
+Show.
 (match goal with
  | |- | ?t1 | = | ?t2 | =>
        assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); destruct H as [H _]
