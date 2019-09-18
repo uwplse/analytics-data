f5e0c8451c2154efd6a5e2d9464d5396c0dd3f9e
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:53:59 -0700

    user 3, session 2643, part 0

---
+++ diffs-w-failures/3/user-3-session-2643.v
@@ -0,0 +1,5 @@
+From Coq Require Import FunInd Recdef.
+From Coq Require Import PeanoNat.
+From Coq Require Import Arith.
+From Coq Require Import ProofIrrelevance.
+From stdpp Require Import decidable countable.
