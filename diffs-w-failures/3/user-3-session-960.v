c1811e4d3517d3225165c8ff99592f3ce11b3f1a
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:47:20 -0700

    user 3, session 960, part 29

--- diffs-w-failures/3/user-3-session-960.v
+++ diffs-w-failures/3/user-3-session-960.v
@@ -12,4 +12,87 @@ From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Unset Silent.
 Set Diffs "off".
-Timeout 1 Check @MRet.
+Set Printing Width 78.
+From Coq Require Import Program.Wf.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq07ihSA"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqCZ3ZYe"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Set Silent.
+From Classes Require Import EqualDec.
+From RecordUpdate Require Import RecordUpdate.
+From stdpp Require Import decidable countable.
+From stdpp Require gmap.
+From stdpp Require Import fin_maps.
+Set Implicit Arguments.
+Instance eqdecision  `{dec : EqualDec A}: (EqDecision A) := dec.
+Definition uint64 := nat.
+Definition compare_to x y (c : comparison) :
+  {match c with
+   | Lt => x < y
+   | Gt => y < x
+   | Eq => x = y
+   end} + {match c with
+           | Lt => x >= y
+           | Gt => y >= x
+           | Eq => x <> y
+           end}.
+(destruct c).
+-
+(apply Nat.eq_dec).
+-
+(destruct (lt_dec x y); auto; right; abstract omega).
+-
+(destruct (lt_dec y x); auto; right; abstract omega).
+Defined.
+Record FixedLengthEncoder bytes intTy byteTy (enc : intTy -> list byteTy)
+(dec : list byteTy -> option intTy) :={encode_length_ok :
+                                        forall x, length (enc x) = bytes;
+                                       encode_decode_ok :
+                                        forall x, dec (enc x) = Some x}.
+Module Ptr.
+Inductive ty : Type :=
+  | Heap : forall T : Type, _
+  | Map : forall V : Type, _
+  | Lock : _.
+End Ptr.
+Class GoModel : Type :={byte : Type;
+                        byte0 : byte;
+                        uint64_to_string : uint64 -> string;
+                        ascii_to_byte : Ascii.ascii -> byte;
+                        byte_to_ascii : byte -> Ascii.ascii;
+                        uint64_to_le : uint64 -> list byte;
+                        uint64_from_le : list byte -> option uint64;
+                        File : Type;
+                        nilFile : File;
+                        Ptr : Ptr.ty -> Type;
+                        nullptr : forall ty, Ptr ty}.
+Search -"endian".
+Definition byte_nat := {x : nat | x < 256}.
+Unset Silent.
+#[program]
+Fixpoint nat_to_le (x : nat) {measure x : list byte_nat :=
+  match x with
+  | 0 => nil
+  | _ => exist _ (x mod 256) _ :: nat_to_le (x / 256)
+  end.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqP1Pyki"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoHRNyo"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Timeout 1 Check @Neqe.
+Next Obligation.
+Timeout 1 Check @sig.
+(simpl).
