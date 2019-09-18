33f2113cc83e9606b9e9045d2f921aec4ebce469
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:16:06 -0700

    user 3, session 7199, part 138

--- diffs-w-failures/3/user-3-session-7199.v
+++ diffs-w-failures/3/user-3-session-7199.v
@@ -808,4 +808,11 @@ Timeout 1 Check @spec_abstraction_compose.
 (apply spec_abstraction_compose).
 step_proc.
 Timeout 1 Check @sig.
-(simpl).
+Unset Silent.
+Set Diffs "off".
+Timeout 1 Check @Ascii.nat_ascii_bounded.
+Timeout 1 Check @Ascii.nat_ascii_embedding.
+Timeout 1 Check @spec_abstraction_compose.
+Set Printing Width 78.
+Show.
+(destruct a' as [_ bs]).
