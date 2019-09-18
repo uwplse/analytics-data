fe28474f9a8c8cc69c3f33f909debf5aaa011a79
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:15:43 -0700

    user 7, session 104, part 43

--- diffs-w-failures/7/user-7-session-104.v
+++ diffs-w-failures/7/user-7-session-104.v
@@ -89,4 +89,7 @@ Fixpoint inv_depth (t : ty) :=
   | TEV _ => 0
   end
 where "'|' t '|'" := (inv_depth t) : btjt_scope.
-Lemma sem_eq_k_inv_depth__exists_not : forall (k : nat) (t : ty), | t | <= k -> exists t' : ty, ~ ||-[ k][t']= [t].
+Set Printing Width 148.
+Lemma not__ref_t_match_ty_t : forall (k : nat) (t : ty), | t | <= k -> forall w : nat, ~ |-[ S k, w] TRef t <$ t.
+Proof.
+(induction k).
