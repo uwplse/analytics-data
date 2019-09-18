babcd2dac5a758775bc22b8c16c6a3fcd9eeeece
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:52:45 -0700

    user 7, session 153, part 14

--- diffs-w-failures/7/user-7-session-153.v
+++ diffs-w-failures/7/user-7-session-153.v
@@ -18,12 +18,13 @@ Require Import Coq.Bool.Bool.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 Set Silent.
-Lemma build_v : forall (X X' : id) (w : nat) (tx t v : ty), |-[ w] v <$ [X := tx] t -> exists v' : ty, |-[ w] v' <$ [X := TVar X'] t.
+Lemma build_v : forall (X X' : id) (tx : ty) (w : nat) (t v : ty), |-[ w] v <$ [X := tx] t -> exists v' : ty, |-[ w] v' <$ [X := TVar X'] t.
 Proof.
-(intros X X' w tx t).
+(intros X X' tx).
 Unset Silent.
-(induction t; intros v Hm).
+(induction w; induction t; intros v Hm).
 Set Silent.
 -
 exists (TCName c).
@@ -31,21 +32,5 @@ Unset Silent.
 (apply match_ty_cname).
 Set Silent.
 -
-Unset Silent.
-Show.
-Set Printing Width 148.
-Set Silent.
 (simpl in *).
 (apply match_ty_pair__inv in Hm).
-(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(destruct (IHt1 _ Hm1) as [v1' Hm1']).
-(destruct (IHt2 _ Hm2) as [v2' Hm2']).
-exists (TPair v1' v2').
-(apply match_ty_pair; assumption).
--
-Unset Silent.
-(simpl in *).
-(apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ destruct (IHt1 _ Hm) as [v' Hm'] | destruct (IHt2 _ Hm) as [v' Hm'] ]; exists v';
-  [ apply match_ty_union_1 | apply match_ty_union_2 ]; assumption).
--
