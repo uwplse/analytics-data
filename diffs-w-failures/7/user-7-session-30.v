0ff8c16a1109eb4c119a3bddca53d80712570a9e
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:16:41 -0700

    user 7, session 30, part 3

--- diffs-w-failures/7/user-7-session-30.v
+++ diffs-w-failures/7/user-7-session-30.v
@@ -31,5 +31,31 @@ Proof.
 (intros t1 t2 k Hm).
 exists v1,v2.
 (split; try reflexivity).
-(simpl in Hm).
 Show.
+Set Printing Width 148.
+(destruct k; simpl in Hm; assumption).
+Qed.
+Set Silent.
+Lemma match_ty_union__inv : forall (v t1 t2 : ty) (k : nat), |-[ k] v <$ TUnion t1 t2 -> |-[ k] v <$ t1 \/ |-[ k] v <$ t2.
+Proof.
+(intros v t1 t2 k Hm).
+(destruct k; destruct v; assumption).
+Unset Silent.
+Qed.
+Set Silent.
+Lemma match_ty_ref__inv :
+  forall (v t : ty) (k : nat),
+  |-[ S k] v <$ TRef t -> exists t' : ty, v = TRef t' /\ (inv_depth t <= k /\ inv_depth t' = inv_depth t) /\ ||-[ k][t']= [t].
+Proof.
+Unset Silent.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; contradiction ])).
+clear IHv.
+(intros t k Hm).
+(simpl in Hm).
+(exists v; auto).
+Qed.
+Set Silent.
+Lemma match_ty__value_type : forall (v t : ty) (k : nat), |-[ k] v <$ t -> value_type v.
+Unset Silent.
+Proof.
+(induction v; induction t).
