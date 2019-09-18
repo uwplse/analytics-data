6408f5e3b94ef89047e7ed451a16eaf393ccb0f0
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:58:46 -0700

    user 7, session 161, part 36

--- diffs-w-failures/7/user-7-session-161.v
+++ diffs-w-failures/7/user-7-session-161.v
@@ -85,88 +85,38 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Silent.
+Set Printing Width 148.
+Set Silent.
 Lemma subst_nested : forall (X Y : id) (tX tY : ty), X <> Y -> forall t : ty, [X := tX] ([Y := tY] t) = [Y := [X := tX] tY] ([X := tX] t).
-Unset Silent.
 Proof.
-Set Silent.
 (intros X Y tX tY Hneq t).
-Unset Silent.
 (induction t; try reflexivity).
-Set Silent.
 -
 (repeat rewrite subst_pair).
 (rewrite IHt1, IHt2).
-Unset Silent.
 reflexivity.
-Set Silent.
 -
 (repeat rewrite subst_union).
-Unset Silent.
 (rewrite IHt1, IHt2).
 reflexivity.
-Set Silent.
 -
-Unset Silent.
-Show.
 (destruct (beq_idP Y i) as [HY| HY]).
 +
 subst.
 (destruct (beq_idP X i) as [HX| HX]).
 *
-Show.
-Set Printing Width 148.
-Set Silent.
 subst.
-Unset Silent.
 contradiction.
 *
-Show.
-Set Printing Width 148.
-Show.
-Set Printing Width 148.
 (rewrite subst_exist_eq).
 (rewrite (subst_exist_neq _ _ _ _ HX)).
 (rewrite subst_exist_eq).
 reflexivity.
 +
-Set Silent.
 (destruct (beq_idP X i) as [HX| HX]).
 *
-Unset Silent.
-Show.
 subst.
 (rewrite (subst_exist_neq _ _ _ _ HY)).
 (repeat rewrite subst_exist_eq).
-(rewrite (subst_exist_neq _ _ _ _ HY)).
-Abort.
-Set Silent.
-Lemma build_v_full :
-  forall (X X' : id) (w : nat) (t v : ty) (tx : ty),
-  |-[ w] v <$ [X := tx] t ->
-  exists v' : ty, |-[ w] v' <$ [X := TVar X'] t /\ (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> |-[ w'] v <$ [X' := tx] t').
-Proof.
-(intros X X').
-(induction w; induction t; intros v tx Hm).
--
-exists v.
-split.
-assumption.
-(apply match_ty_cname__inv in Hm; subst).
-(induction w'; induction t'; intros Hm; try assumption || contradiction).
-+
-(rewrite subst_union).
-(apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
-+
-(rewrite subst_union).
-(apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
-+
-(destruct (beq_idP X' i) as [Hbeq| Hbeq]).
-*
-subst.
-(rewrite subst_exist_eq).
-assumption.
-*
 Unset Silent.
-Show.
+(rewrite (subst_exist_neq _ _ _ _ HY)).
