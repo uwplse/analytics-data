f2491c2f38b1079603a53876eb898d06998482fd
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:30:58 -0700

    user 3, session 10289, part 6

--- diffs-w-failures/3/user-3-session-10289.v
+++ diffs-w-failures/3/user-3-session-10289.v
@@ -10,14 +10,111 @@ Set Printing Width 78.
 Unset Silent.
 Set Diffs "off".
 Set Printing Width 78.
-Require Import Coq.Strings.String.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqbBzgV2"
+Unset Silent.
+Set Diffs "off".
+Set Printing Width 78.
+Require Coq.Strings.String.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq6EHQSi"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqLU0K7s"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoeGQqP"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
+Import Coq.Strings.String.StringSyntax.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqk16bqe"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq77Gl8C"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Require Import Helpers.Helpers.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqzcx1u8"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqO6RHU6"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Require Import Proc.
+Set Silent.
+Set Silent.
+Require Import ProcTheorems.
+Unset Silent.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4DEKPQ"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8Qd0ER"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Set Silent.
+Record LayerAbstraction State1 State2 :={abstraction :
+                                          State1 -> State2 -> Prop}.
+Definition Abstraction State := LayerAbstraction world State.
+Definition abstraction_compose `(abs1 : Abstraction State1)
+  `(abs2 : LayerAbstraction State1 State2) :=
+  {|
+  abstraction := fun w state' =>
+                 exists state,
+                   abstraction abs2 state state' /\ abstraction abs1 w state |}.
+Definition IdAbstraction State : LayerAbstraction State State :=
+  {| abstraction := fun state state' => state' = state |}.
+Record SpecProps T R State :=
+ Spec {pre : Prop; post : T -> State -> Prop; recovered : R -> State -> Prop}.
+Definition Specification A T R State := A -> State -> SpecProps T R State.
+Definition proc_spec `(spec : Specification A T R State) 
+  `(p : proc T) `(rec : proc R) `(abs : Abstraction State) :=
+  forall a w state,
+  abstraction abs w state ->
+  pre (spec a state) ->
+  forall r,
+  rexec p rec w r ->
+  match r with
+  | RFinished v w' =>
+      exists state',
+        abstraction abs w' state' /\ post (spec a state) v state'
+  | Recovered v w' =>
+      exists state',
+        abstraction abs w' state' /\ recovered (spec a state) v state'
+  end.
+Definition spec_impl `(spec1 : Specification A' T R State)
+  `(spec2 : Specification A T R State) :=
+  forall a state,
+  pre (spec2 a state) ->
+  exists a',
+    pre (spec1 a' state) /\
+    (forall v state',
+     post (spec1 a' state) v state' -> post (spec2 a state) v state') /\
+    (forall rv state',
+     recovered (spec1 a' state) rv state' ->
+     recovered (spec2 a state) rv state').
+Theorem proc_spec_weaken :
+  forall `(spec1 : Specification A T R State)
+    `(spec2 : Specification A' T R State) `(p : proc T) 
+    `(rec : proc R) (abs : Abstraction State),
+  proc_spec spec1 p rec abs ->
+  spec_impl spec1 spec2 -> proc_spec spec2 p rec abs.
+Proof.
+(unfold proc_spec at 2; intros).
+(eapply H0 in H2; eauto; repeat deex).
+(eapply H in H3; eauto).
+(destruct r; simpl in *; repeat deex; intuition eauto).
+Qed.
+Hint Resolve tt: core.
+Unset Silent.
+Inductive Marker : string -> Type :=
+    mark : forall s, Marker s.
