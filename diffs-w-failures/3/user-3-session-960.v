5ecb116a8d5b081a9ba292d3719cd41764f3f16f
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:47:24 -0700

    user 3, session 960, part 324

--- diffs-w-failures/3/user-3-session-960.v
+++ diffs-w-failures/3/user-3-session-960.v
@@ -171,4 +171,4 @@ Timeout 1 Check @Plength.
 Timeout 1 Check @Ascii.nat_ascii_embedding.
 Timeout 1 Check @pointwise_relation.
 Definition nat64_to_le (x : nat) : list byte :=
-  let digits := nat_to_le x in digits ++ repeat (8 - length digits) 0.
+  let digits := nat_to_le 254 x in digits ++ repeat (8 - length digits) 0.
