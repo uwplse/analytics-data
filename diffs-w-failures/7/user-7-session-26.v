a99e0e5a8a9db0c1ecac9e4a1188181a953b6b59
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:13:38 -0700

    user 7, session 26, part 34

--- diffs-w-failures/7/user-7-session-26.v
+++ diffs-w-failures/7/user-7-session-26.v
@@ -296,5 +296,6 @@ Proof.
 (intros k t1 t2 t' Hsem).
 Unset Silent.
 (unfold sem_sub_k in Hsem).
-(split; intros v Hm; assert (Hmu : |-[ k] v <$ TUnion t1 t2) by (apply match_ty_union_1; assumption) || (apply match_ty_union_2; assumption);
-  apply Hsem; assumption).
+Set Printing Width 148.
+(split; intros v Hm; assert (Hmu : |-[ k] v <$ TUnion t1 t2) by (apply match_ty_union_1 || apply match_ty_union_2; assumption); apply Hsem;
+  assumption).
