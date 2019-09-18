34bb8ade621447144dc337aed6e37853dc4a2303
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:15:59 -0700

    user 3, session 7199, part 4

--- diffs-w-failures/3/user-3-session-7199.v
+++ diffs-w-failures/3/user-3-session-7199.v
@@ -157,5 +157,9 @@ Hint Resolve get_len_ok: core.
 Theorem get_ok : proc_spec get_spec get recover abstr.
 Proof.
 (unfold get; intros).
-(apply spec_abstraction_compose; simpl).
+Unset Silent.
+Set Diffs "off".
+Set Printing Width 78.
+Show.
 step_proc.
+Timeout 1 Check @spec_abstraction_compose.
