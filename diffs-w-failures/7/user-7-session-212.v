b0ca910e6b82b67c552fbc367b6044c6def11bce
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 20:37:10 -0700

    user 7, session 212, part 6

--- diffs-w-failures/7/user-7-session-212.v
+++ diffs-w-failures/7/user-7-session-212.v
@@ -102,4 +102,8 @@ Proof.
 (simpl in HX).
 assumption.
 Qed.
-Lemma free_in_ty_pair__inv : forall (X : id) (t1 t2 : ty), free_in_ty X (TPair t1 t2) -> free_in_ty X t1 \/ free_in_ty X t2.
+Lemma f_free_in_ty_pair__inv : forall (X : id) (t1 t2 : ty), f_free_in_ty X (TPair t1 t2) -> f_free_in_ty X t1 \/ f_free_in_ty X t2.
+Unset Silent.
+Proof.
+Show.
+(unfold d_free_in_ty).
