c2ab8d3872e8496bcf80adc4197502b76a84c8ae
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:18 -0700

    user 7, session 19, part 69

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -142,4 +142,5 @@ Set Printing Width 148.
 (apply le_S_n in Htk').
 Set Printing Width 148.
 Set Printing Width 148.
-(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl).
+Set Printing Width 148.
+(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v Hv).
