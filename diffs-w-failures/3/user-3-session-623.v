29cbba2f2cac354ed7869773788d213fde22e1f3
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:45:41 -0700

    user 3, session 623, part 0

---
+++ diffs-w-failures/3/user-3-session-623.v
@@ -0,0 +1,51 @@
+Time Ltac descend := repeat match goal with
+                            | |- exists _, _ => eexists
+                            end.
+Time #[local]
+Lemma abstract_away_helper {A} (P : A -> Prop) (x y : A) :
+  P y -> y = x -> P x.
+Time
+Ltac
+ especialize H :=
+  match type of H with
+  | forall x : ?T, _ =>
+      let x := fresh x in
+      evar ( x : T ); specialize (H x); subst x
+  end.
+Time #[local]
+Ltac
+ _especialize H :=
+  lazymatch type of H with
+  | forall x : ?T, _ =>
+      let x := fresh x in
+      lazymatch type of T with
+      | Prop => unshelve (evar ( x : T ); specialize (H x); subst x)
+      | _ => evar ( x : T ); specialize (H x); subst x
+      end
+  end.
+Time
+Ltac
+ epose_proof H :=
+  let H' := fresh in
+  pose proof H as H'; repeat _especialize H'.
+Time
+Ltac
+ eexists_t t :=
+  match goal with
+  | |- exists _ : ?T, _ => eexists (_ : T)
+  | |- {_ : ?T | _} => eexists (_ : T)
+  end.
+Time Ltac exists_econstructor := eexists_t ltac:(econstructor); simpl.
+Time Proof.
+Time (intros; subst; auto).
+Time Qed.
+Time
+Ltac
+ abstract_term t :=
+  match goal with
+  | |- ?g =>
+        let p := eval pattern t in g in
+        match p with
+        | ?P ?x => eapply (abstract_away_helper P)
+        end
+  end.
