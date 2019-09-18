07eecbf3d34b9275f4bde5afeec1235919033cc5
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:22 -0700

    user 7, session 19, part 691

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -360,5 +360,8 @@ Set Printing Width 148.
           [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; [ apply IHt1 | apply IHt2 ]; assumption
    end; try (solve [ destruct v; contradiction ])).
 Show.
-(simpl).
+Set Printing Width 148.
+(rewrite mk_nf_ref).
+Show.
+(apply match_ty_i_ref__weak_inv in Hm; subst).
 Show.
