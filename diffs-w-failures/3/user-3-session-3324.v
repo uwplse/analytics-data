92178ab2a9e6f8cf29fc650476c3b951292e33c1
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:57:16 -0700

    user 3, session 3324, part 45

--- diffs-w-failures/3/user-3-session-3324.v
+++ diffs-w-failures/3/user-3-session-3324.v
@@ -183,5 +183,12 @@ Unset Silent.
 Set Diffs "off".
 Set Printing Width 78.
 Show.
+Unset Silent.
+Set Diffs "off".
+Set Printing Width 78.
+Show.
 (constructor; autorewrite with upd in *; intuition idtac).
-(rewrite diskUpd_eq; auto).
+Timeout 1 Check @remapped_abstraction.
+Timeout 1 Check @no_wipe.
+Timeout 1 Check @nodup.
+autorewrite with upd.
