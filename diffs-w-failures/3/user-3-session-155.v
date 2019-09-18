2b4653c2ef5843721bcbfc1217decac1f22ab570
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:43:47 -0700

    user 3, session 155, part 1

--- diffs-w-failures/3/user-3-session-155.v
+++ diffs-w-failures/3/user-3-session-155.v
@@ -7,18 +7,5 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 78.
-Set Silent.
-Require Import Examples.StatDb.Impl.
-From Transitions Require Import Relations Rewriting.
-Import RelationNotations.
 Unset Silent.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqbHt1FS"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqmEP9Fe"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
+Set Diffs "off".
