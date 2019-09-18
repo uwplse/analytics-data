27164428b4757342335801523ac087ea4906634c
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 20:22:06 -0700

    user 7, session 192, part 6

--- diffs-w-failures/7/user-7-session-192.v
+++ diffs-w-failures/7/user-7-session-192.v
@@ -39,9 +39,7 @@ assumption.
 (rewrite subst_union).
 Set Printing Width 148.
 Set Printing Width 148.
-(destruct (either_free_or_fresh_in_ty X t'1) as [HXt'1| HXt'1]).
-Show.
-(apply match_ty_union_1).
-Show.
-(apply IHt'b).
+Set Printing Width 148.
+(destruct (either_free_or_fresh_in_ty X' t'1) as [HXt'1| HXt'1]).
+(apply match_ty_union_1; tauto).
 Show.
