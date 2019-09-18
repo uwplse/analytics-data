c35f5051f012a68469f73ddbf0d405548178492a
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:17:13 -0700

    user 7, session 106, part 9

--- diffs-w-failures/7/user-7-session-106.v
+++ diffs-w-failures/7/user-7-session-106.v
@@ -193,4 +193,4 @@ Set Printing Width 148.
 (destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
 Set Printing Width 148.
 Set Printing Width 148.
-(apply match_ty_pair; [ apply IHt1 | apply IHt2 ]).
+(apply match_ty_pair; [ eapply IHt1 | eapply IHt2 ]).
