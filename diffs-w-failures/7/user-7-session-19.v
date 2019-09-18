a759a4a88478b18450c73a6470fda960a50ec7b7
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:18 -0700

    user 7, session 19, part 110

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -72,17 +72,40 @@ specialize (Href v' Hv').
 Set Printing Width 148.
 Set Printing Width 148.
 Set Silent.
-Lemma match_ty_i_eq__inv_depth_eq : forall t t' : ty, forall (k : nat) (v : ty), |-[ k] v <$ t <-> |-[ k] v <$ t' -> | t | = | t' |.
+Lemma match_ty_i_t_le_k__v_ke_t : forall (k : nat) (t : ty), | t | <= k -> forall v : ty, |-[ k] v <$ t -> | v | <= | t |.
+Proof.
+(induction k; induction t; intros Htk v Hm;
+  try
+   match goal with
+   | Hm:|-[ ?k'] ?v <$ TCName _ |- _ => apply match_ty_i_cname__inv in Hm; subst; constructor
+   | H:|-[ ?k'] ?v <$ TPair _ _
+     |- _ =>
+         destruct (max_inv_depth_le__components_le _ _ _ Htk) as [Htk1 Htk2]; apply match_ty_i_pair__inv in Hm;
+          destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; apply Nat.max_le_compat; auto
+   | H:|-[ ?k'] ?v <$ TUnion _ _
+     |- _ =>
+         destruct (max_inv_depth_le__components_le _ _ _ Htk) as [Htk1 Htk2]; apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2];
+          [ apply Nat.le_trans with (| t1 |) | apply Nat.le_trans with (| t2 |) ]; auto; apply Max.le_max_l || apply Max.le_max_r
+   end).
+-
+(destruct v; try contradiction).
+(inversion Htk).
+-
+clear IHt.
+(simpl in Htk).
+(apply le_S_n in Htk).
+(apply match_ty_i_ref__inv in Hm).
 Unset Silent.
+(destruct Hm as [t' [Heq Href]]; subst).
+Set Silent.
+Abort.
+Lemma match_ty_i_eq__inv_depth_eq : forall t t' : ty, forall (k : nat) (v : ty), |-[ k] v <$ t <-> |-[ k] v <$ t' -> | t | = | t' |.
 Proof.
 (induction t; induction t'; intros k v Hsemeq).
 reflexivity.
-Show.
-Set Printing Width 148.
 15: {
 idtac.
 clear IHt'.
 (simpl).
-Show.
+Unset Silent.
 (apply f_equal).
-(eapply IHt).
