1a571caac7bac1788b4874c337cb910daad3fbdc
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:08:08 -0700

    user 7, session 94, part 16

--- diffs-w-failures/7/user-7-session-94.v
+++ diffs-w-failures/7/user-7-session-94.v
@@ -83,4 +83,5 @@ Set Silent.
 Theorem match_ty__value_type_l : forall (k : nat) (v t : ty), |-[ k] v <$ t -> value_type v.
 Unset Silent.
 Proof.
-(induction k).
+Set Printing Width 148.
+(induction k; intros v t; generalize dependent v; induction t; intros v Hm).
