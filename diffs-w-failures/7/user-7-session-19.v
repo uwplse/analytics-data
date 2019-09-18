6daab7cda43a2a669a7c394c2b8dd952b9deffa2
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:22 -0700

    user 7, session 19, part 648

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -563,4 +563,4 @@ Unset Silent.
 (apply match_ty_i_ref__inv in Hm).
 (destruct Hm as [tx [Heq Href]]; subst).
 (simpl).
-(intros v; split; specialize (Href v); tauto).
+(intros v; split; specialize (Href v)).
