38859c22d4f38e73d80ee8798d772282c023f136
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:21:48 -0700

    user 7, session 112, part 1

--- diffs-w-failures/7/user-7-session-112.v
+++ diffs-w-failures/7/user-7-session-112.v
@@ -215,6 +215,30 @@ assumption.
 Set Silent.
 (intros v1 t3 k Hm1 Hm2).
 (apply match_ty_cname__inv in Hm1; subst).
-Unset Silent.
+Set Printing Width 148.
+Set Silent.
+(intros v1 t3 k Hm1 Hm2).
+(apply match_ty_cname__inv in Hm1; subst).
 assumption.
 -
+Unset Silent.
+Set Silent.
+(intros v0 t3 k Hm1 Hm2).
+(apply match_ty_pair__inv in Hm1).
+(destruct Hm1 as [pv11 [pv12 [Heq [Hmpv11 Hmpv12]]]]; subst).
+Unset Silent.
+(induction t3; try (solve [ destruct k; contradiction ])).
+Set Silent.
++
+(apply match_ty_pair__inv in Hm2).
+(destruct Hm2 as [pv21 [pv22 [Heq [Hmpv21 Hm22]]]]).
+(inversion Heq; subst).
+auto using match_ty_pair.
++
+(apply match_ty_union__inv in Hm2).
+Unset Silent.
+(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
+Set Silent.
++
+Unset Silent.
+Show.
