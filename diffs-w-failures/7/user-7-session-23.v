32e7b088ef9df314fe600b5c6e69a1ae2b6f3a40
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:11:24 -0700

    user 7, session 23, part 3

--- diffs-w-failures/7/user-7-session-23.v
+++ diffs-w-failures/7/user-7-session-23.v
@@ -87,4 +87,7 @@ Qed.
 Theorem sem_sub_i__sem_sub_deq : forall t1 t2 : ty, (||- [t1]<= [t2])%btjmi -> ||- [t1]<= [t2].
 Unset Silent.
 Proof.
-(intros Hsemi).
+Set Printing Width 148.
+(intros t1 t2 Hsemi).
+(apply sub_d__semantic_sound).
+(apply sub_d__sem_sub_i).
