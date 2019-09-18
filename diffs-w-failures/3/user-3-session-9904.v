f58c1264ea11f9307179caecdeeefa111734a24d
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:28:58 -0700

    user 3, session 9904, part 48

--- diffs-w-failures/3/user-3-session-9904.v
+++ diffs-w-failures/3/user-3-session-9904.v
@@ -237,6 +237,9 @@ Unset Silent.
 Set Diffs "off".
 Set Printing Width 78.
 Show.
-exists [].
-Timeout 1 Check @firstn_length.
-left.
+Unset Silent.
+Set Diffs "off".
+Timeout 1 Check @Ascii.nat_ascii_embedding.
+Set Printing Width 78.
+Show.
+(exists []; intuition).
