3fe1dfbc794ac4cdc9b0004c8fc783e772341dd4
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:17 -0700

    user 7, session 19, part 5

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -104,5 +104,13 @@ Theorem sem_sub_i__sem_sub_deq : forall t1 t2 : ty, (||- [t1]<= [t2])%btjmi -> (
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 (intros ta; induction ta; intros tb; induction tb; intros Hsem; pose proof Hsem as Hsem'; unfold sem_sub_i, sem_sub_k_i in Hsem';
-  unfold sem_sub_deq, sem_sub_k_deq; intros k v Hv Hm1).
+  unfold sem_sub_deq, sem_sub_k_deq; intros k v Hv Hm1;
+  try
+   match goal with
+   | Hm1:(|-[ ?k] ?v <$ TCName ?c)%btjmdeq
+     |- _ =>
+         apply match_ty_deq_cname__inv in Hm1; subst; assert (Hm : (|-[ k] TCName c <$ TCName c)%btjmi) by (destruct k; reflexivity); specialize
+          (Hsem' _ _ Hv Hm)
+   end).
