0cae37586794dddfe70145379015c39637cc869b
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:47:21 -0700

    user 3, session 960, part 103

--- diffs-w-failures/3/user-3-session-960.v
+++ diffs-w-failures/3/user-3-session-960.v
@@ -71,79 +71,8 @@ Set Printing Width 78.
 Set Silent.
 Opaque Nat.modulo Nat.div.
 Unset Silent.
-Set Diffs "off".
-Set Printing Width 78.
 Unset Silent.
 Set Diffs "off".
+Timeout 1 Check @FinFun.bFun.
+Timeout 1 Check @FinFun.bFun.
 Set Printing Width 78.
-Set Silent.
-Next Obligation of nat_to_le_func.
-Proof.
-(apply PeanoNat.Nat.mod_upper_bound; auto).
-Defined.
-Next Obligation of nat_to_le_func.
-Proof.
-subst digit.
-(apply PeanoNat.Nat.div_lt; auto; try lia).
-Unset Silent.
-Defined.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqR5t07S"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq1sUxE7"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Set Silent.
-Next Obligation of nat_to_le_func.
-Proof.
-lia.
-Unset Silent.
-Defined.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqLYnJFy"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqrXfjTc"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Set Silent.
-Next Obligation of nat_to_le_func.
-Proof.
-(unfold MR).
-(apply (wf_projected lt projT2); auto).
-(apply lt_wf).
-Unset Silent.
-Defined.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqVkQJro"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqJwp8ky"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Set Silent.
-Fixpoint le_to_nat base (digits : list {x : nat | x < S (S base)}) : nat :=
-  match digits with
-  | nil => 0
-  | digit :: digits' => proj1_sig digit * base + le_to_nat digits'
-  end.
-Theorem nat_le_inverse base : forall n, le_to_nat (nat_to_le base n) = n.
-Proof.
-(intros).
-Unset Silent.
-Set Diffs "off".
-Set Printing Width 78.
-Show.
-(destruct n; simpl).
-Timeout 1 Check @nat_to_le.
-Timeout 1 Check @nat_to_le.
-Timeout 1 Check @nat_to_le.
-Timeout 1 Check @nat_to_le.
-Print nat_to_le.
