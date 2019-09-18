c15fad64cebbf78b934a75fcfd486357b2a29f78
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:44:24 -0700

    user 3, session 314, part 0

---
+++ diffs-w-failures/3/user-3-session-314.v
@@ -0,0 +1,89 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqiK6yJO"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 51.
+Set Silent.
+Require Import Examples.StatDb.Impl.
+From Transitions Require Import NonError.
+Require Import Spec.Hoare.
+Require Import Spec.HoareTactics.
+Require Import Spec.AbstractionSpec.
+Definition absr :
+  relation DB.l.(State) Var.l.(State) unit :=
+  fun l res =>
+  match res with
+  | Val s _ =>
+      fst s = fold_right plus 0 l /\
+      snd s = length l
+  | Err _ _ => False
+  end.
+Instance absr_non_error : (NonError absr).
+Proof.
+(compute; auto).
+Qed.
+Definition init_hspec :
+  Specification InitStatus unit Var.State :=
+  fun state =>
+  {|
+  pre := state = (0, 0);
+  post := fun state' _ => state' = (0, 0);
+  alternate := fun state' (_ : unit) => True |}.
+Definition add_hspec n :
+  Specification unit unit Var.State :=
+  fun state =>
+  {|
+  pre := True;
+  post := fun state' (_ : unit) =>
+          fst state' = n + fst state /\
+          snd state' = S (snd state);
+  alternate := fun state' (_ : unit) =>
+               state' = (0, 0) |}.
+Definition add_rspec n :
+  Specification unit unit Var.State :=
+  fun state =>
+  {|
+  pre := True;
+  post := fun state' (_ : unit) =>
+          fst state' = n + fst state /\
+          snd state' = S (snd state);
+  alternate := fun state' (_ : unit) =>
+               state' = (0, 0) |}.
+Definition avg_hspec :
+  Specification nat unit Var.State :=
+  fun state =>
+  {|
+  pre := True;
+  post := fun state' v =>
+          state = state' /\
+          v = fst state / snd state';
+  alternate := fun state' v => state' = (0, 0) |}.
+Definition avg_rspec :
+  Specification nat unit Var.State :=
+  fun state =>
+  {|
+  pre := True;
+  post := fun state' v =>
+          state = state' /\
+          v = fst state / snd state';
+  alternate := fun state' v => state' = (0, 0) |}.
+Definition recover_spec :
+  Specification unit unit Var.State :=
+  fun state =>
+  {|
+  pre := state = (0, 0);
+  post := fun state' (_ : unit) => state' = (0, 0);
+  alternate := fun state' (_ : unit) =>
+               state' = (0, 0) |}.
+Lemma read_op_ok :
+  forall i,
+  proc_hspec Var.dynamics (read i)
+    (op_spec Var.dynamics (Var.Read i)).
+Proof.
+(intros).
+(eapply op_spec_sound).
+Qed.
