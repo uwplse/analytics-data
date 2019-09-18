14040a2458ec6cb863e0ba27439a171b49dff294
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:19:32 -0700

    user 7, session 109, part 10

--- diffs-w-failures/7/user-7-session-109.v
+++ diffs-w-failures/7/user-7-session-109.v
@@ -115,6 +115,7 @@ Reserved Notation "'|-' t1 '<<' t2" (at level 50).
 Unset Silent.
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 Inductive sub_d : ty -> ty -> Prop :=
   | SD_Refl : forall t, |- t << t
   | SD_Trans : forall t1 t2 t3, |- t1 << t2 -> |- t2 << t3 -> |- t1 << t3
@@ -126,4 +127,24 @@ Inductive sub_d : ty -> ty -> Prop :=
   | SD_Distr2 : forall t1 t21 t22, |- TPair t1 (TUnion t21 t22) << TUnion (TPair t1 t21) (TPair t1 t22)
   | SD_Ref : forall t t', |- t << t' -> |- t' << t -> |- TRef t << TRef t'
   | SD_ExistL : forall (X : id) (t t' : ty) (X' : id), fresh_in_ty X' t' -> |- [X := TVar X'] t << t' -> |- TExist X t << t'
+  | SD_ExistR : forall (X : id) (t t' : ty), (exists tx : ty, |- t << [X := tx] t') -> |- t << TExist X t'
  where "|- t1 '<<' t2" := (sub_d t1 t2) : btjd_scope.
+Hint Constructors sub_d: DBBetaJulia.
+Set Silent.
+Lemma union_right_1 : forall t t1 t2 : ty, |- t << t1 -> |- t << TUnion t1 t2.
+Proof.
+(intros t t1 t2 H).
+(eapply SD_Trans).
+eassumption.
+constructor.
+Qed.
+Lemma union_right_2 : forall t t1 t2 : ty, |- t << t2 -> |- t << TUnion t1 t2.
+Proof.
+(intros t t1 t2 H).
+(eapply SD_Trans).
+eassumption.
+constructor.
+Qed.
+Unset Silent.
+Hint Resolve union_right_1 union_right_2: DBBetaJulia.
+Ltac solve_trans := eapply SD_Trans; eassumption.
