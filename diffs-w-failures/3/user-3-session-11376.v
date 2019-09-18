77a233c8a8f5afbbba65f0ef996ad5682e09b700
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:36:45 -0700

    user 3, session 11376, part 0

---
+++ diffs-w-failures/3/user-3-session-11376.v
@@ -0,0 +1,11 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqpOybRz"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 51.
+Set Silent.
+Require Export LF.Basics.
