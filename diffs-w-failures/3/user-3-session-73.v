138ea0717c99810c9f46ad6df6cdecc1c2d5d153
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:43:29 -0700

    user 3, session 73, part 18

--- diffs-w-failures/3/user-3-session-73.v
+++ diffs-w-failures/3/user-3-session-73.v
@@ -145,6 +145,7 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Set Silent.
+Unset Silent.
 Lemma proc_hspec_init_ok (init : proc C_Op InitStatus)
   (A_initP : AState -> Prop) (C_initP : CState -> Prop) 
   spec :
@@ -152,8 +153,13 @@ Lemma proc_hspec_init_ok (init : proc C_Op InitStatus)
   (forall sC, C_initP sC -> (spec sC).(pre)) ->
   (forall sC sC',
    (spec sC).(post) sC' Initialized ->
-   exists sA', absr sA' sC' tt /\ A_initP sA') ->
+   exists sA', absr sA' (Val sC' tt) /\ A_initP sA') ->
   (test C_initP;; exec c_sem init)
   --->
    (any (T:=unit);; test A_initP;; absr;; pure Initialized) +
    (any (T:=unit);; pure InitFailed).
+Set Silent.
+Proof  using ((c_sem) + (C_Op)).
+clear a_sem A_Op.
+(intros Hproc Hinit_pre Hinit_post).
+(intros sC sC' i ([], (?, ((HCinit, <-), Hexec)))).
