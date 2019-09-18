4d2f9e1df61c7c49812e879b7894b6c5a2ac46a9
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:07:00 -0700

    user 3, session 5295, part 20

--- diffs-w-failures/3/user-3-session-5295.v
+++ diffs-w-failures/3/user-3-session-5295.v
@@ -557,5 +557,14 @@ Proof.
 step.
 (destruct r; try step).
 (destruct r; try step).
-(destruct (v == v0); subst).
 Unset Silent.
+Set Diffs "off".
+Set Printing Width 78.
+Show.
+(destruct (v == v0); subst).
+-
+step.
+-
+step.
+Unshelve.
+auto.
