ba68589c76304cf2191d5024f11a91660c553984
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:34:11 -0700

    user 3, session 10894, part 0

---
+++ diffs-w-failures/3/user-3-session-10894.v
@@ -0,0 +1,15 @@
+From RecoveryRefinement Require Import Lib.
+Require Import Examples.Logging.TxnDiskAPI.
+Require Import Examples.ReplicatedDisk.TwoDiskAPI.
+Require Import Examples.ReplicatedDisk.OneDiskAPI.
+Require Import Examples.Logging.HoareProof.
+Require Import Examples.ReplicatedDisk.ReplicatedDiskImpl.
+Module LoggingTwoDiskRefinement.
+Definition rf : LayerRefinement TwoDisk.TDLayer TxnD.l :=
+  refinement_transitive ReplicatedDisk.Refinement_TD_OD LoggingRefinement.rf.
+Check compile_exec_seq_ok rf.
+Print Assumptions rf.
+Definition compile := compile rf.
+Definition init := init rf.
+Definition recover := recover rf.
+End LoggingTwoDiskRefinement.
