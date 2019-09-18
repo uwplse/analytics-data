7fa7533ee1ccc0c69f735942a8a025d35f9ddceb
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:05:49 -0700

    user 3, session 5034, part 11

--- diffs-w-failures/3/user-3-session-5034.v
+++ diffs-w-failures/3/user-3-session-5034.v
@@ -161,5 +161,65 @@ Lemma diskGet_eq_values :
   diskGet d a =?= b -> diskGet d a =?= b' -> a < diskSize d -> b = b'.
 Proof.
 (intros).
-(destruct (diskGet d a) eqn:?; simpl in *).
 Unset Silent.
+Set Diffs "off".
+Set Printing Width 78.
+Show.
+(destruct (diskGet d a) eqn:?; simpl in *).
+Set Silent.
+-
+congruence.
+-
+exfalso.
+(apply disk_inbounds_not_none in Heqo; eauto).
+Qed.
+Unset Silent.
+Set Silent.
+Ltac
+ eq_values :=
+  match goal with
+  | H:diskGet ?d ?a =?= ?b, H':diskGet ?d ?a =?= ?b'
+    |- _ =>
+        assert (b = b') by
+         (apply (@diskGet_eq_values d a b b'); try lia; auto); subst
+  end.
+Lemma disk_matches_state_next :
+  forall disk start b blocks,
+  disk_matches_list disk start (b :: blocks) ->
+  disk_matches_list disk (start + 1) blocks.
+Proof.
+(unfold disk_matches_list; simpl; intuition).
+Qed.
+Hint Resolve disk_matches_state_next: core.
+Lemma disk_matches_list_first :
+  forall disk start b blocks,
+  disk_matches_list disk start (b :: blocks) -> diskGet disk start =?= b.
+Proof.
+(unfold disk_matches_list; simpl; intuition).
+Qed.
+Definition get_helper_spec start count :
+  Specification (list block) (list block) unit State :=
+  fun (blocks : list block) state =>
+  {|
+  pre := disk_matches_list state start blocks /\
+         start + count <= diskSize state /\ length blocks = count;
+  post := fun r state' => r = blocks /\ state' = state;
+  recovered := fun _ state' => state' = state |}.
+Theorem get_helper_ok :
+  forall count start,
+  proc_spec (get_helper_spec start count) (get_helper start count) d.recover
+    d.abstr.
+Proof.
+(induction count; simpl; intros).
+-
+step_proc.
+(simpl in *; intuition).
+(destruct a; simpl in *; auto; lia).
+-
+step_proc.
+step_proc.
+(simpl in *; intuition).
+(destruct a'; simpl in *; try lia).
+exists a'.
+intuition eauto.
+lia.
