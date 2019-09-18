bbeedba02b71503b4fd5e65cade05ac53ae8fb9b
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 20:22:06 -0700

    user 7, session 192, part 26

--- diffs-w-failures/7/user-7-session-192.v
+++ diffs-w-failures/7/user-7-session-192.v
@@ -34,4 +34,5 @@ Set Printing Width 148.
 (rewrite subst_union; destruct (fresh_in_ty_union__inv _ _ _ HX) as [HX1 HX2]; apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm]).
 Show.
 Set Printing Width 148.
-(apply match_ty_union_1; tauto).
+Show.
+(apply match_ty_union_1; auto).
