16bf075febb14fd1973eb2dd10f8e0ac5089b3f6
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:19 -0700

    user 7, session 19, part 322

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -211,4 +211,27 @@ Set Printing Width 148.
 specialize (Hsem _ Hv Hm).
 Show.
 (apply match_ty_i_union__inv in Hsem).
+Set Printing Width 148.
 (destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hv' Hm'; apply match_ty_i__transitive_on_value_type with v; assumption).
+Show.
+Qed.
+Set Silent.
+Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
+Proof.
+Unset Silent.
+(induction k; induction t; induction t'; intros H; try (solve [ simpl; constructor ]);
+  try (solve
+   [ match goal with
+     | |- | ?t1 | <= | ?t2 | =>
+           (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+             (H _ Hm); contradiction) ||
+             (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
+               (H _ Hm); contradiction)
+     end ])).
+Show.
+(match goal with
+ | |- | ?t1 | <= | ?t2 | =>
+       assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize (H _ Hm);
+        apply match_ty_i_union__inv in H; rewrite inv_depth_union; destruct H as [Hm1| Hm2];
+        [ apply Nat.le_trans with (| t'1 |) | apply Nat.le_trans with (| t'2 |) ]; try apply Max.le_max_l || apply Max.le_max_r
+ end).
