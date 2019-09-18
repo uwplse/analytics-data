2932ff16828014055a73f254fef5841c1bdc4ab9
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:47:24 -0700

    user 3, session 960, part 297

--- diffs-w-failures/3/user-3-session-960.v
+++ diffs-w-failures/3/user-3-session-960.v
@@ -809,4 +809,4 @@ Timeout 1 Check @Eqdep_dec.inj_pair2_eq_dec.
 Timeout 1 Check @Eqdep_dec.inj_pair2_eq_dec.
 Check Eqdep_dec.inj_pair2_eq_dec.
 Timeout 1 Check @sigT_eq_dec.
-(destruct (Eqdep_dec.inj_pair2_eq_dec x y)).
+(destruct (Eqdep_dec.inj_pair2_eq_dec _ x y)).
