d2d128b2030adeca29c99e5dfc21cb4632186c1c
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:37:30 -0700

    user 7, session 55, part 6

--- diffs-w-failures/7/user-7-session-55.v
+++ diffs-w-failures/7/user-7-session-55.v
@@ -4,23 +4,3 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
 Set Printing Width 148.
-Set Silent.
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0250a.BaseDefs.
-Require Import BetaJulia.Sub0250a.BaseProps.
-Require Import BetaJulia.Sub0250a.MatchProps.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Open Scope btjm_scope.
-Lemma value_sem_sub_k_i_union__inv :
-  forall v : ty, value_type v -> forall (k : nat) (ta tb : ty), ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
-Proof.
-(intros v Hv k ta tb Hsem; unfold sem_sub_k in Hsem).
-Unset Silent.
-Set Silent.
-(assert (Hdep : | v | <= k)).
-Unset Silent.
-(apply match_ty__inv_depth_l_le_index with (TUnion ta tb)).
