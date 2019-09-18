c7f6ca340dcc1eefcd80f4f6f7f23c7e94a86226
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:43:46 -0700

    user 3, session 150, part 0

---
+++ diffs-w-failures/3/user-3-session-150.v
@@ -0,0 +1,12 @@
+From Coq Require Export List.
+From Coq Require Export Omega.
+From Classes Require Export Classes.
+From Transitions Require Export Relations.
+From Tactical Require Export ProofAutomation.
+Require Export Spec.Proc.
+Require Export Spec.ProcTheorems.
+Require Export Spec.Hoare.
+Require Export Spec.Abstraction.
+Require Export Spec.AbstractionSpec.
+Require Export Spec.Layer.
+Export ProcNotations.
