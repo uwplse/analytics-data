764adea80a4de16b9252eb0d4c79b29971a60139
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:44:24 -0700

    user 3, session 314, part 1

--- diffs-w-failures/3/user-3-session-314.v
+++ diffs-w-failures/3/user-3-session-314.v
@@ -86,4 +86,16 @@ Lemma read_op_ok :
 Proof.
 (intros).
 (eapply op_spec_sound).
-Qed.
+Unset Silent.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq5kkA0H"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqWDv50N"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
