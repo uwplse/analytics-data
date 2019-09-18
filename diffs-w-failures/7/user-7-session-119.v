90052a38dff6ac0ccc31952e7e651a9be2ed52c2
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 19:27:02 -0700

    user 7, session 119, part 7

--- diffs-w-failures/7/user-7-session-119.v
+++ diffs-w-failures/7/user-7-session-119.v
@@ -3,31 +3,3 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
-Set Silent.
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0280a.BaseDefs.
-Require Import BetaJulia.Sub0280a.BaseProps.
-Require Import BetaJulia.Sub0280a.BaseMatchProps.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Open Scope btjm.
-Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
-Proof.
-(intros t1 t2 Hsub).
-Set Printing Width 148.
-(induction Hsub).
-Set Silent.
--
-(apply sem_sub__refl).
--
-Unset Silent.
-Show.
-Set Printing Width 148.
-(apply sem_sub__trans with t2; assumption).
-Set Silent.
--
-Unset Silent.
-Show.
