5971678e1f45c84da739e3a5cf1fd4815d9fd9dc
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:47:21 -0700

    user 3, session 960, part 89

--- diffs-w-failures/3/user-3-session-960.v
+++ diffs-w-failures/3/user-3-session-960.v
@@ -149,5 +149,14 @@ Set Printing Width 78.
 Fixpoint le_to_nat base (digits : list {x : nat | x < S (S base)}) : nat :=
   match digits with
   | nil => 0
-  | digit :: digits' => proj1_sig digit * base + le_to_nat base digits'
+  | digit :: digits' => proj1_sig digit * base + le_to_nat digits'
   end.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqA2kjC4"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqofvrZu"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
