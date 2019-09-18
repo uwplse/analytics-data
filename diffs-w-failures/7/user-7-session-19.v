c363a47fc39a9f9159d73dee918c2fd7c1388315
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:18 -0700

    user 7, session 19, part 183

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -102,4 +102,5 @@ Show.
 constructor.
 -
 (apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(constructor; auto).
+Set Printing Width 148.
+(constructor; [ apply IHt1 | apply IHt2 ]; auto).
