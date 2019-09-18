67756669270f4dd7e107e60a0bece1009376f269
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 14:04:12 -0700

    user 3, session 4740, part 27

--- diffs-w-failures/3/user-3-session-4740.v
+++ diffs-w-failures/3/user-3-session-4740.v
@@ -66,5 +66,8 @@ Redirect
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Check 1 + 2 + aReallyBigIdentifier + 4 + anotherLongName
-  === 6 + anotherLongName + 8 + 9.
+Unset Silent.
+Set Diffs "off".
+Timeout 1 Check @Choice.
+Set Printing Width 69.
+Check 3=== 3.
