bfc299ec279bcc19d30c3cc7d2e3ce2439d12302
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 20:22:51 -0700

    user 7, session 193, part 5

--- diffs-w-failures/7/user-7-session-193.v
+++ diffs-w-failures/7/user-7-session-193.v
@@ -28,6 +28,7 @@ Inductive ty : Type :=
   | TBVar : id -> ty
   | TFVar : id -> ty
   | TEV : id -> ty.
+Set Printing Width 148.
 Set Silent.
 Definition tint := TCName NInt.
 Definition tflt := TCName NFlt.
@@ -38,10 +39,7 @@ Definition vY := Id 2.
 Definition vZ := Id 3.
 Definition tX := TBVar vX.
 Definition tY := TBVar vY.
-Unset Silent.
 Definition teXX := TExist vX tX.
-Set Printing Width 148.
-Set Silent.
 Fixpoint FFV (t : ty) : id_set :=
   match t with
   | TCName _ => IdSet.empty
@@ -52,7 +50,6 @@ Fixpoint FFV (t : ty) : id_set :=
   | TFVar y => IdSet.singleton y
   | TEV _ => IdSet.empty
   end.
-Unset Silent.
 Fixpoint FBV (t : ty) : id_set :=
   match t with
   | TCName _ => IdSet.empty
@@ -65,14 +62,20 @@ Fixpoint FBV (t : ty) : id_set :=
   end.
 Definition free (X : id) (fvs : id_set) := IdSet.In X fvs.
 Definition not_free (X : id) (fvs : id_set) := ~ IdSet.In X fvs.
-Set Printing Width 148.
-Set Silent.
 Definition f_free_in_ty (X : id) (t : ty) := free X (FFV t).
 Definition not_f_free_in_ty (X : id) (t : ty) := not_free X (FFV t).
 Definition b_free_in_ty (X : id) (t : ty) := free X (FBV t).
-Unset Silent.
 Definition not_b_free_in_ty (X : id) (t : ty) := not_free X (FBV t).
 Hint Unfold free not_free f_free_in_ty not_f_free_in_ty b_free_in_ty not_b_free_in_ty: DBBetaJulia.
-Print IdSet.
-Set Printing Width 148.
 Definition wf_ty (t : ty) := IdSet.Equal (FBV t) IdSet.empty.
+Unset Silent.
+Fixpoint f_subst (X : id) (s : ty) (t : ty) :=
+  match t with
+  | TCName _ => t
+  | TPair t1 t2 => TPair (f_subst X s t1) (f_subst X s t2)
+  | TUnion t1 t2 => TPair (f_subst X s t1) (f_subst X s t2)
+  | TExist y t' => TExist y (f_subst X s t')
+  | TBVar _ => t
+  | TFVar y => if beq_id x y then s else t
+  | TEV _ => t
+  end.
