bb961892484efb902eb6ef08d1be88e67d086c99
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 18:08:18 -0700

    user 7, session 19, part 190

--- diffs-w-failures/7/user-7-session-19.v
+++ diffs-w-failures/7/user-7-session-19.v
@@ -69,7 +69,8 @@ Proof.
 (intros v t1 t2 k Hm).
 (destruct k; destruct v; assumption).
 Qed.
-Unset Silent.
+Set Printing Width 148.
+Set Silent.
 Lemma match_ty_i_ref__inv :
   forall v t : ty, forall k : nat, |-[ S k] v <$ TRef t -> exists t' : ty, v = TRef t' /\ (forall v' : ty, |-[ k] v' <$ t' <-> |-[ k] v' <$ t).
 Proof.
@@ -84,30 +85,20 @@ split.
 reflexivity.
 (intros v').
 specialize (Href v').
-Set Printing Width 148.
 (destruct Href; split; assumption).
 Qed.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Silent.
 Lemma match_ty_i__value_type : forall (t : ty) (k : nat) (v : ty), |-[ k] v <$ t -> value_type v.
 Proof.
 (induction t; intros k v Hm).
 -
-Unset Silent.
-Show.
 (apply match_ty_i_cname__inv in Hm; subst).
 constructor.
 -
 (apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
 (constructor; [ eapply IHt1 | eapply IHt2 ]; eauto).
 -
 (apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto).
 -
-(apply match_ty_i_ref__inv in Hm; subst).
+Unset Silent.
+Show.
+(destruct k).
