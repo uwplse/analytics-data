6a0f0a78b8f74816436f8d01bd33a3855b1fff6b
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 20:22:06 -0700

    user 7, session 192, part 30

--- diffs-w-failures/7/user-7-session-192.v
+++ diffs-w-failures/7/user-7-session-192.v
@@ -15,29 +15,15 @@ Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Set Printing Width 148.
-Lemma match_ty_subst_fresh : forall (X : id) (s : ty) (w : nat) (t v : ty), fresh_in_ty X t -> |-[ w] v <$ t -> |-[ w] v <$ [X := s] t.
+Set Printing Width 148.
+Lemma fresh_in_ty_var__neq : forall X Y : id, fresh_in_ty X (TVar Y) -> X <> Y.
 Proof.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-(intros X s; induction w; induction t; intros v HX Hm;
-  try (solve
-   [ rewrite subst_cname in *; assumption
-   | rewrite subst_pair; destruct (fresh_in_ty_pair__inv _ _ _ HX) as [HX1 HX2]; apply match_ty_pair__inv in Hm;
-      destruct Hm as [v1 [v2 [heq [Hm1 Hm2]]]]; subst; apply match_ty_pair; auto
-   | rewrite subst_union; destruct (fresh_in_ty_union__inv _ _ _ HX) as [HX1 HX2]; apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm];
-      [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto
-   | rewrite subst_ev in *; assumption ])).
-Show.
 Set Silent.
--
+(intros X Y H).
 Unset Silent.
-Show.
-Set Printing Width 148.
-(apply match_ty_exist__0_inv in Hm; contradiction).
+(unfold fresh_in_ty, fresh in H).
+(simpl in H).
+(destruct (beq_idP X Y)).
 -
-(unfold fresh_in_ty, fresh in HX).
-Search -IdSet.In.
+subst.
+Search -IdSet.singleton.
