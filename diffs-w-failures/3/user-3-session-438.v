ee6fe3376d55ea4bf71ab21e2588dfc5865ce0d7
Talia Ringer <tringer@cs.washington.edu>
2019-09-18 13:44:53 -0700

    user 3, session 438, part 0

---
+++ diffs-w-failures/3/user-3-session-438.v
@@ -0,0 +1,10 @@
+Cd "statdb-cli/extract/".
+Require Import ExtrHaskellBasic.
+Require Import ExtrHaskellNatInteger.
+Extraction Language Haskell.
+Require Import Lab1.StatDbCli.
+Extract Inlined Constant PeanoNat.Nat.div => 
+ "(\n m -> if m Prelude.== 0 then 0 else Prelude.div n m)".
+Separate Extraction cli.
+Recursive Extraction Library Helpers.
+Cd "../../".
