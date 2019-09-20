a5151e9d4282d7fad9972ddf4ec2716df68f27eb
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:49:28 -0700

    user 3, session 2594, part 1

--- diffs-w-failures-no-compiles/3/user-3-session-2594.v
+++ diffs-w-failures-no-compiles/3/user-3-session-2594.v
@@ -10,18 +10,5 @@ Set Printing Width 78.
 Set Silent.
 From Coq Require Import FunInd Recdef.
 Unset Silent.
-From Armada Require Import Goose.Machine.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqarQs4n"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqOY0Jn5"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
-Set Silent.
-Opaque Nat.modulo Nat.div.
-#[local]Obligation Tactic := (intros; simpl; subst).
-Unset Silent.
+Set Diffs "off".
+Timeout 1 Check @QArith_base.Q.
