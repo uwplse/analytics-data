67281044d4c1fdea73e637fc4ea4c47b03512719
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:38:59 -0700

    user 2, session 5, part 54

--- diffs-w-failures-no-compiles/2/user-2-session-5.v
+++ diffs-w-failures-no-compiles/2/user-2-session-5.v
@@ -80,4 +80,83 @@ Timeout 1 Check @primitive.
 Timeout 1 Check @primitive.
 Timeout 1 Check @primitive.
 Timeout 1 Check @primitive.
-Timeout 1 Check @step.
+Fixpoint step (t : term) : term :=
+  match t with
+  | Nil => Nil
+  | Ident _ => Nil
+  | <a b> => if value a then <a (step b)> else <(step a) b>
+  | {f a} =>
+      if value f
+      then
+       match f with
+       | [(Ident lam) (Ident x) body] => if value a then subst x a body else {f (step a)}
+       | Ident prim =>
+           if String.eqb prim "if"
+           then
+            match a with
+            | [cond bThen bElse] =>
+                if value cond
+                then match cond with
+                     | Nil => bElse
+                     | _ => bThen
+                     end
+                else {"if" [(step cond) bThen bElse]}
+            | _ => Nil
+            end
+           else
+            if oneArgCbvPrimitive prim
+            then
+             if value a
+             then
+              if String.eqb prim "fst"
+              then match a with
+                   | <x y> => x
+                   | _ => Nil
+                   end
+              else
+               if String.eqb prim "snd"
+               then match a with
+                    | <x y> => y
+                    | _ => Nil
+                    end
+               else
+                if String.eqb prim "fun"
+                then match a with
+                     | {f a} => f
+                     | _ => Nil
+                     end
+                else
+                 if String.eqb prim "arg"
+                 then match a with
+                      | {f a} => a
+                      | _ => Nil
+                      end
+                 else
+                  if String.eqb prim "nil?"
+                  then match a with
+                       | Nil => "t"
+                       | _ => Nil
+                       end
+                  else
+                   if String.eqb prim "cons?"
+                   then match a with
+                        | <_ _> => "t"
+                        | _ => Nil
+                        end
+                   else
+                    if String.eqb prim "app?"
+                    then match a with
+                         | {_ _} => "t"
+                         | _ => Nil
+                         end
+                    else Nil
+             else {f (step a)}
+            else Nil
+       | _ => Nil
+       end
+      else {(step f) a}
+  end.
+Redirect "/tmp/coqfAE0Rj" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Print step.
