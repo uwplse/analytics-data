7866f9752ddb33af7841bdd614f7fceb2ff8be03
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:38:43 -0700

    user 1, session 37, part 34

--- diffs-w-failures-no-compiles/1/user-1-session-37.v
+++ diffs-w-failures-no-compiles/1/user-1-session-37.v
@@ -333,23 +333,4 @@ Set Diffs "off".
 Print Forall.
 Print Forall2.
 Set Printing Width 70.
-Fixpoint Gamma (G : GT) : SetST :=
-  match G with
-  | GDyn => Full_set _
-  | GInt => Singleton _ SInt
-  | GBool => Singleton _ SBool
-  | GFun G_1 G_2 => zipWith_ensembles SFun (Gamma G_1) (Gamma G_2)
-  | GRec l =>
-      fun X =>
-      exists l',
-        X = SRec l' /\
-        Forall2 (fun (S' : option ST) G' => True) l'
-          (map GammaPair l)
-  | _ => Empty_set _
-  end
-with GammaPair (x : option (Ann * GT)) : SetST :=
-  match x with
-  | None => Empty_set _
-  | Some (R, G) => Gamma G
-  | Some (O, G) => Empty_set _
-  end.
+Fixpoint Gamma (G : GT) : SetST.
