bccdc7dfcff7edd18a78d9f5c7010ead30164223
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:38:39 -0700

    user 1, session 30, part 0

---
+++ diffs-w-failures-no-compiles/1/user-1-session-30.v
@@ -0,0 +1,18 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 63.
+Require Import Lists.List.
+Require Import Coq.Sorting.Permutation.
+Require Import Coq.Sorting.Sorted.
+Set Silent.
+Require Import Orders.
+Require Import FunInd.
+Require Import Recdef.
+Require Import Extraction.
+Require Import ExtrOcamlBasic.
+Require Import Coq.Program.Equality.
+Unset Silent.
+Require Import Math.
