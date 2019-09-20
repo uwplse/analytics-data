c74099672ba77f6a5871558044314d7106b2c847
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:38:56 -0700

    user 2, session 5, part 5

--- diffs-w-failures-no-compiles/2/user-2-session-5.v
+++ diffs-w-failures-no-compiles/2/user-2-session-5.v
@@ -50,4 +50,5 @@ Import TermNotations.
 Redirect "/tmp/coq34dpHE" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
-Check [Nil Nil Nil]%coucou_scope.
+Open Scope coucou_scope.
+Check [Nil Nil Nil].
