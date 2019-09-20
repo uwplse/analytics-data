54ff77932467ea433b1b488c8d18e9927025f415
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:53:06 -0700

    user 3, session 3307, part 0

---
+++ diffs-w-failures-no-compiles/3/user-3-session-3307.v
@@ -0,0 +1,23 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqdPf9GM"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 78.
+Set Silent.
+Require Export List.
+Unset Silent.
+Require Export Lia.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqYb3Ku6"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqlUzZqI"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
