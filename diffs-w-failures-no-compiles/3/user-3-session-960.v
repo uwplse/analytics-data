818f9c0a5c1e9e9922edf342aa7b6b1cfff6cf4a
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:43:44 -0700

    user 3, session 960, part 303

--- diffs-w-failures-no-compiles/3/user-3-session-960.v
+++ diffs-w-failures-no-compiles/3/user-3-session-960.v
@@ -810,4 +810,4 @@ Timeout 1 Check @Eqdep_dec.inj_pair2_eq_dec.
 Check Eqdep_dec.inj_pair2_eq_dec.
 Timeout 1 Check @sigT_eq_dec.
 Timeout 1 Check @FinMapToList.
-(apply (Eqdep_dec.inj_pair2_eq_dec _ _ _ _ x y)).
+Check Eqdep_dec.inj_pair2_eq_dec.
