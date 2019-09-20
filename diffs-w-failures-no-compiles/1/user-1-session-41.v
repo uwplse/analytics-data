3c00b2935ba209dfe7d132259d71fc9a3fd91292
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:38:52 -0700

    user 1, session 41, part 109

--- diffs-w-failures-no-compiles/1/user-1-session-41.v
+++ diffs-w-failures-no-compiles/1/user-1-session-41.v
@@ -323,20 +323,29 @@ Inductive ST : Type :=
 Inductive Ann : Type :=
   | R : Ann
   | O : Ann.
-Inductive GT : Type :=
-  | GDyn : GT
-  | GInt : GT
-  | GBool : GT
-  | GFun : GT -> GT -> GT
-  | GRec : list (option (Ann * GT)) -> GT
-  | GRow : list (option (option (Ann * GT))) -> GT.
 Unset Silent.
 Set Diffs "off".
 Set Printing Width 66.
+Fixpoint size_gt (G : GT) : nat :=
+  match G with
+  | GFun G_1 G_2 => 1 + size_gt G_1 + size_gt G_2
+  | GRec l =>
+      fold_right
+        (fun x acc =>
+         match x with
+         | Some (_, G) => size_gt G
+         | _ => 0
+         end + acc) 1 l
+  | GRow l =>
+      fold_right
+        (fun x acc =>
+         match x with
+         | Some (Some (_, G)) => size_gt G
+         | _ => 0
+         end + acc) 1 l
+  | _ => 1
+  end.
 Theorem size_gt_g0 : forall x, 0 < size_gt x.
 Proof.
-Unset Silent.
-Set Diffs "off".
-Set Printing Width 66.
-Show.
 (induction x; intros; simpl; eauto with math).
+all: (induction l; eauto with math).
