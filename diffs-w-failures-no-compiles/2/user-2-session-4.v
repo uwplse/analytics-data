dcbeb5450f069fbcc0528c8b1170b81e3d9f1c14
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:38:55 -0700

    user 2, session 4, part 2

--- diffs-w-failures-no-compiles/2/user-2-session-4.v
+++ diffs-w-failures-no-compiles/2/user-2-session-4.v
@@ -15,10 +15,12 @@ Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Unset Silent.
 Set Printing Width 98.
+Unset Silent.
+Set Printing Width 98.
 Inductive term :=
   | Nil : term
   | Ident : string -> term
   | Cons : term -> term -> term
   | App : term -> term -> term.
-Redirect "/tmp/coqJpJYaV" Print Ltac Signatures.
+Redirect "/tmp/coqn5gNxo" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
