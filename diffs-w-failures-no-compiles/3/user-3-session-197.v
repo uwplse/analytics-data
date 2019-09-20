eaebe39ff679801d06bee1a4cd875bdd7c2991e5
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:40:00 -0700

    user 3, session 197, part 9

--- diffs-w-failures-no-compiles/3/user-3-session-197.v
+++ diffs-w-failures-no-compiles/3/user-3-session-197.v
@@ -12,4 +12,55 @@ From Coq Require Import List.
 Unset Silent.
 Set Diffs "off".
 Timeout 1 Check @True.
-Timeout 1 Check @readNone.
+Set Printing Width 78.
+From Transitions Require Import NonError.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqHY4UKl"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqfnUJFb"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Timeout 1 Check @None.
+Timeout 1 Check @None.
+Timeout 1 Check @NonError.
+Timeout 1 Check @NonError.
+Timeout 1 Check @NonError.
+Timeout 1 Check @NonError.
+Timeout 1 Check @Bvector.BVand.
+Set Silent.
+From RecoveryRefinement Require Export Lib.
+Module Var.
+Inductive id :=
+  | Sum : _
+  | Count : _.
+Inductive Op : Type -> Type :=
+  | Read : forall i : id, Op nat
+  | Write : forall (i : id) (v : nat), Op unit.
+Definition State := (nat * nat)%type.
+Definition get (i : id) : State -> nat :=
+  match i with
+  | Sum => fun '(x, _) => x
+  | Count => fun '(_, y) => y
+  end.
+Definition set (i : id) (v : nat) : State -> State :=
+  match i with
+  | Sum => fun '(_, y) => (v, y)
+  | Count => fun '(x, _) => (x, v)
+  end.
+Definition dynamics : Dynamics Op State :=
+  {|
+  step := fun T (op : Op T) =>
+          match op with
+          | Read i => reads (get i)
+          | Write i v => puts (set i v)
+          end;
+  crash_step := puts (fun _ => (0, 0)) |}.
+Definition l : Layer Op :=
+  {| Layer.State := State; sem := dynamics; initP := fun s => s = (0, 0) |}.
+End Var.
+Unset Silent.
+Instance var_crash_step_nonerror : (NonError Var.crash_step).
