87c72635c5db66c667afd960573cd9a13108d0a4
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:43:36 -0700

    user 3, session 960, part 147

--- diffs-w-failures-no-compiles/3/user-3-session-960.v
+++ diffs-w-failures-no-compiles/3/user-3-session-960.v
@@ -166,6 +166,10 @@ Timeout 1 Check @mod_S_lt.
 Timeout 1 Check @mod_S_lt.
 Timeout 1 Check @mod_S_lt.
 Set Printing Width 78.
+Unset Silent.
+Set Diffs "off".
+Timeout 1 Check @N.lbase.
+Set Printing Width 78.
 Function
  nat_to_le base_m2 (x : nat) {wf lt x} : list {x : nat | x < S (S base_m2)}
  :=
@@ -174,37 +178,6 @@ Function
    | _ =>
        let base := S (S base_m2) in
        let digit := x `mod` base in
-       exist (fun x => x < base) digit _ :: nat_to_le base_m2 (x / base)
+       exist (fun x => x < S (S base_m2)) digit _
+       :: nat_to_le base_m2 (x / S (S base_m2))
    end.
-Proof.
--
-Timeout 1 Check @pointwise_relation.
-Timeout 1 Check @sum.
-(intros; subst).
-Unset Silent.
-Set Diffs "off".
-Set Printing Width 78.
-Show.
-Set Silent.
-(apply PeanoNat.Nat.div_lt; auto; try lia).
-Unset Silent.
--
-Timeout 1 Check @applicative_ap.
-Timeout 1 Check @map_filter.
-(apply lt_wf).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqyzCofd"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Qed.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqlawfPH"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq7j1lNr"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
