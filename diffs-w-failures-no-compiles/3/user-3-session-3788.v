bc33c43195ec3761ee06b2208d733e734b7944d4
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:55:04 -0700

    user 3, session 3788, part 34

--- diffs-w-failures-no-compiles/3/user-3-session-3788.v
+++ diffs-w-failures-no-compiles/3/user-3-session-3788.v
@@ -110,7 +110,11 @@ Timeout 1 Check @ProcMarker.
 Timeout 1 Check @Znumtheory.prime.
 Timeout 1 Check @spec_abstraction_compose.
 Set Printing Width 78.
-Notation "'proc:' p" := (ProcMarker p) (at level 0, only printing).
+Unset Silent.
+Set Diffs "off".
+Timeout 1 Check @Byte.x10.
+Set Printing Width 78.
+Notation "'proc:' p" := (ProcMarker p) (at level 10, only printing).
 Theorem swapXY_ok :
   proc_spec
     (fun (_ : unit) state =>
@@ -123,3 +127,4 @@ Proof.
 (match goal with
  | |- proc_spec _ ?p _ _ => pose proof (AProc p) as Hbefore
  end).
+(unfold swapXY).
