5b54d788db3e7c77b9223378ba6bad88a8f68f10
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:40:04 -0700

    user 3, session 214, part 0

---
+++ diffs-w-failures-no-compiles/3/user-3-session-214.v
@@ -0,0 +1,12 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqCYzfGC"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 78.
+Set Silent.
+From Coq Require Import Morphisms.
+From Tactical Require Import ProofAutomation.
