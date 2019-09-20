8bf39283f19fcddd52667f49e3eacbffd1e2fcac
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:49:32 -0700

    user 3, session 2609, part 0

---
+++ diffs-w-failures-no-compiles/3/user-3-session-2609.v
@@ -0,0 +1,41 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq3uOXWe"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 78.
+Set Silent.
+From Coq Require Import ProofIrrelevance.
+Unset Silent.
+From Coq Require Export String.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4zfvjp"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqJtGai1"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Set Silent.
+From Classes Require Import EqualDec.
+From RecordUpdate Require Import RecordUpdate.
+From stdpp Require Import decidable countable.
+From stdpp Require gmap.
+Unset Silent.
+From stdpp Require Import fin_maps.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq6eqSP3"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq577Tap"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Search -"mod".
