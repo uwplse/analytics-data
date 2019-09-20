2104130e907147cd111729b526a137847c84170b
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:38:41 -0700

    user 1, session 33, part 1

--- diffs-w-failures-no-compiles/1/user-1-session-33.v
+++ diffs-w-failures-no-compiles/1/user-1-session-33.v
@@ -4,4 +4,7 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 63.
-Inductive Sorted : Type :=.
+Unset Silent.
+Set Diffs "off".
+Set Printing Width 63.
+Inductive Sorted :=.
