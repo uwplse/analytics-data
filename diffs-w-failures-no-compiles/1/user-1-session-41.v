c53f22afe6bf0ae2a0b7fcd05bbeec53db58a077
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:38:50 -0700

    user 1, session 41, part 65

--- diffs-w-failures-no-compiles/1/user-1-session-41.v
+++ diffs-w-failures-no-compiles/1/user-1-session-41.v
@@ -477,20 +477,16 @@ Show.
   end).
 specialize (H _ H7).
 specialize (H2 _ H7).
-(repeat
-  match goal with
-  | H:exists _, _ |- _ => destruct H
-  | H:_ \/ _ |- _ => inversion H; clear H
-  end).
-congruence.
 Unset Silent.
 Set Diffs "off".
 Set Printing Width 94.
 Show.
-specialize (H _ H4).
-(repeat
-  match goal with
-  | H:exists _, _ |- _ => destruct H
-  | H:_ \/ _ |- _ => inversion H; clear H
-  end).
-congruence.
+2: specialize (H _ H4).
+all:
+ (repeat
+   match goal with
+   | H:exists _, _ |- _ => destruct H
+   | H:_ \/ _ |- _ => inversion H; clear H
+   end).
+all: congruence.
+-
