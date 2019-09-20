f2d3e1e2f7e8f4556222905fd9bee24e9f49a3d8
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:39:23 -0700

    user 3, session 73, part 0

---
+++ diffs-w-failures-no-compiles/3/user-3-session-73.v
@@ -0,0 +1,38 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqIyHzwp"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Diffs "off".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 78.
+Set Silent.
+Require Import Spec.Proc.
+Require Import Spec.Hoare.
+From Transitions Require Import Relations Rewriting.
+Require Import Abstraction.
+Require Import Layer.
+Section Abstraction.
+Context (AState CState : Type).
+Context (absr : relation AState CState unit).
+Definition refine_spec T R (spec : Specification T R AState) :
+  AState -> Specification T R CState :=
+  fun s cs =>
+  {|
+  pre := absr s (Val cs tt) /\ (spec s).(pre);
+  post := fun cs' r =>
+          exists s', absr s' (Val cs' tt) /\ (spec s).(post) s' r;
+  alternate := fun cs' r =>
+               exists s', absr s' (Val cs' tt) /\ (spec s).(alternate) s' r |}.
+Context A_Op C_Op `(a_sem : Dynamics A_Op AState)
+ `(c_sem : Dynamics C_Op CState).
+Import RelationNotations.
+Lemma proc_rspec_refine_exec T R (p : proc C_Op T) 
+  (rec : proc C_Op R) spec :
+  (forall t, proc_rspec c_sem p rec (refine_spec spec t)) ->
+  (forall sA sC, absr sA (Val sC tt) -> (spec sA).(pre)) ->
+  _ <- absr; exec c_sem p ---> v <- spec_exec spec; _ <- absr; pure v.
+Proof.
+(intros Hprspec Habstr_pre).
+(intros sA sC b ([], (sTstart, (Hrd, Hexec)))).
