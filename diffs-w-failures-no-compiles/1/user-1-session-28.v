6288f3bfa7d44e61ac13155b6b27700adc4169a2
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:38:39 -0700

    user 1, session 28, part 1

--- diffs-w-failures-no-compiles/1/user-1-session-28.v
+++ diffs-w-failures-no-compiles/1/user-1-session-28.v
@@ -4,4 +4,4 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 40.
-Require Import Analytical.Analytics.
+Unset Silent.
