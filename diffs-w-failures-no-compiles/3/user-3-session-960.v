7506b9800f40054f2711e2949852e50d03bea303
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 16:43:39 -0700

    user 3, session 960, part 201

--- diffs-w-failures-no-compiles/3/user-3-session-960.v
+++ diffs-w-failures-no-compiles/3/user-3-session-960.v
@@ -406,5 +406,24 @@ Timeout 1 Check @Ascii.Ascii.
 Timeout 1 Check @false.
 Set Silent.
 Unset Silent.
+Timeout 1 Check @false.
 Definition ascii0 :=
-  Ascii.Ascii false false false false false false false fals.
+  Ascii.Ascii false false false false false false false false.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqOF8kVi"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqcc1tDj"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Check @last.
+Timeout 1 Check @gen.
+Set Silent.
+Definition bounded_to_ascii (x : {x | x < 256}) : Ascii.ascii.
+Unset Silent.
+(match Byte.of_nat (proj1_sig x) with
+ | Some b => Ascii.ascii_of_byte b
+ | None => ascii0
+ end).
