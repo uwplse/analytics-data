0f358c13049b4bc8506eef204e68604e2fc6df25
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:07:42 -0700

    user 3, session 7199, part 291

--- diffs-w-failures-no-compiles/3/user-3-session-7199.v
+++ diffs-w-failures-no-compiles/3/user-3-session-7199.v
@@ -139,4 +139,4 @@ Theorem log_length_ok_nil d b :
   diskGet d 0 = Some b -> block_to_addr b = 0 -> log_length_ok d nil.
 Proof.
 (unfold log_length_ok; intros).
-(rewrite H in *; simpl in *; subst).
+Unset Silent.
