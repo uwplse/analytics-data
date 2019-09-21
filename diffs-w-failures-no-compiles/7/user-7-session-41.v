cc759acc91cff67161511dfa2549b4ff4d5c69c4
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:50 -0700

    user 7, session 41, part 10

--- diffs-w-failures-no-compiles/7/user-7-session-41.v
+++ diffs-w-failures-no-compiles/7/user-7-session-41.v
@@ -4,11 +4,33 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 Set Silent.
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0250a.BaseDefs.
 Require Import BetaJulia.Sub0250a.BaseProps.
-Set Printing Width 148.
-Set Silent.
+Require Import BetaJulia.Sub0250a.MatchProps.
 Unset Silent.
+Set Silent.
+Require Import BetaJulia.Sub0250a.AltMatchDef.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Open Scope btjm.
+Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
+Proof.
+(intros t1 t2 Hsub).
+(unfold sem_sub).
+(induction Hsub; intros k v Hm).
+-
+assumption.
+-
+(unfold sem_sub_k in *).
+auto.
+-
+Unset Silent.
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(inversion Hv; subst).
