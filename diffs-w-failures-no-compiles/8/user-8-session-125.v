f3df914b82326666dd3cd0178724a1b1baec7dfc
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:28:52 -0700

    user 8, session 125, part 114

--- diffs-w-failures-no-compiles/8/user-8-session-125.v
+++ diffs-w-failures-no-compiles/8/user-8-session-125.v
@@ -98,10 +98,8 @@ Unset Silent.
 Show.
 Set Printing Width 85.
 Show.
-specialize (denote_ctrls_unitary W (\226\159\166 W \226\159\167) U li) as inv.
 Unset Silent.
 Show.
 Set Printing Width 85.
 Show.
-Unset Silent.
-Show.
+specialize (denote_ctrls_unitary W (\226\159\166 W \226\159\167) U li) as inv.
