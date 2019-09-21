3e98a0d2e8d45bd3236eedde0a9c152f1c48d2bf
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:56 -0700

    user 7, session 113, part 34

--- diffs-w-failures-no-compiles/7/user-7-session-113.v
+++ diffs-w-failures-no-compiles/7/user-7-session-113.v
@@ -15,15 +15,8 @@ Require Import Coq.Bool.Bool.
 Open Scope btjt.
 Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
 Set Printing Width 148.
+Set Printing Width 148.
 Set Silent.
-Lemma not_fresh_in_union__inv : forall (X : id) (fvs1 fvs2 : id_set), not_fresh X (IdSet.union fvs1 fvs2) -> not_fresh X fvs1 /\ not_fresh X fvs2.
-Proof.
-(intros X fvs1 fvs2 H).
-Unset Silent.
-(unfold not_fresh in *).
-Show.
-Set Silent.
-Abort.
 Lemma subs_not_in_FV : forall (X : id) (t : ty), not_fresh_in_ty X t -> forall s : ty, [X := s] t = t.
 Proof.
 (intros X t).
