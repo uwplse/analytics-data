10f843b054c686f73a48f74b81c8c92296ecf1d0
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:47 -0700

    user 7, session 108, part 26

--- diffs-w-failures-no-compiles/7/user-7-session-108.v
+++ diffs-w-failures-no-compiles/7/user-7-session-108.v
@@ -299,4 +299,5 @@ clear Hm.
 -
 specialize (Hcontra 0).
 (destruct Hcontra as [w Hcontra]).
-(assert (Hm : |-[ S k, 0] TCName c <$ TCName c) by (apply match_ty_value_type__reflexive; constructor)).
+Unset Silent.
+Show.
