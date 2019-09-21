1f1764bd6d758dfb40bb82ce18c66def09118c0a
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:25 -0700

    user 7, session 96, part 2

--- diffs-w-failures-no-compiles/7/user-7-session-96.v
+++ diffs-w-failures-no-compiles/7/user-7-session-96.v
@@ -60,4 +60,39 @@ Lemma match_ty_exist__0_inv : forall (v : ty) (X : id) (t : ty), |-[ 0] v <$ TEx
 Unset Silent.
 Proof.
 Set Silent.
+Unset Silent.
+(intros v; induction v; intros X t Hm; assumption).
+Qed.
+Set Silent.
+Lemma match_ty_exist__inv : forall (v : ty) (X : id) (t : ty) (k : nat), |-[ S k] v <$ TExist X t -> exists tx : ty, |-[ k] v <$ [X := tx] t.
+Proof.
 (intros v; induction v; intros X t k Hm; assumption).
+Qed.
+Theorem match_ty__value_type_l : forall (k : nat) (v t : ty), |-[ k] v <$ t -> value_type v.
+Proof.
+(induction k; intros v t; generalize dependent v; induction t; intros v Hm;
+  try (solve
+   [ apply match_ty_cname__inv in Hm; subst; constructor
+   | apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; constructor; [ eapply IHt1 | eapply IHt2 ]; eauto
+   | apply match_ty_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto
+   | apply match_ty_ref__weak_inv in Hm; destruct Hm as [t' Heq]; subst; constructor
+   | destruct v; contradiction ])).
+-
+Unset Silent.
+Show.
+(apply match_ty_exist__0_inv in Hm).
+auto.
+Set Silent.
+-
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hmx]).
+(eapply IHk; eassumption).
+Unset Silent.
+Qed.
+Set Silent.
+Lemma sem_sub__refint_eXrefX : ||- [TRef tint]<= [TExist vX (TRef tX)].
+Unset Silent.
+Proof.
+(intros k v Hv).
+(simpl).
+Show.
