da4592f34041456f1256fbc2b4412c97c3462cea
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:07:33 -0700

    user 3, session 7199, part 129

--- diffs-w-failures-no-compiles/3/user-3-session-7199.v
+++ diffs-w-failures-no-compiles/3/user-3-session-7199.v
@@ -752,9 +752,5 @@ Set Diffs "off".
 Timeout 1 Check @Ascii.nat_ascii_embedding.
 Set Printing Width 78.
 Show.
-(unfold spec_impl; simpl; intuition).
-Timeout 1 Check @Ascii.nat_ascii_bounded.
-Timeout 1 Check @Ascii.nat_ascii_bounded.
-Timeout 1 Check @Ascii.nat_ascii_embedding.
-Timeout 1 Check @repeat_length.
-(descend; intuition eauto).
+Unset Silent.
+Set Diffs "off".
