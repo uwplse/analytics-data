d256366382310fb822d97f7249b946249794c586
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:50 -0700

    user 7, session 41, part 7

--- diffs-w-failures-no-compiles/7/user-7-session-41.v
+++ diffs-w-failures-no-compiles/7/user-7-session-41.v
@@ -9,38 +9,6 @@ Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0250a.BaseDefs.
 Require Import BetaJulia.Sub0250a.BaseProps.
-Require Import BetaJulia.Sub0250a.AltMatchDef.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Unset Silent.
-Set Printing Width 148.
-Open Scope btjm.
-Set Printing Width 148.
-Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
-Set Silent.
-Proof.
-Unset Silent.
-(intros t1 t2 Hsub).
-(unfold sem_sub).
-(induction Hsub; intros k v Hm).
-Set Silent.
--
-Unset Silent.
-assumption.
-Set Silent.
--
-Unset Silent.
-Show.
-Set Printing Width 148.
 Set Printing Width 148.
 Set Silent.
-(unfold sem_sub_k in *).
-Unset Silent.
-auto.
-Set Silent.
--
-Unset Silent.
-Show.
-(apply match_ty_pair__inv in Hm).
+Require Import BetaJulia.Sub0250a.MatchProps.
