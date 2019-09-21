f3df33973b84cf87b263075140be6eb3a3d98791
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:10 -0700

    user 7, session 19, part 147

--- diffs-w-failures-no-compiles/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles/7/user-7-session-19.v
@@ -139,6 +139,7 @@ Set Printing Width 148.
                (H 0 _ Hv); destruct H as [_ H]; specialize (H Hm); contradiction)
      end ])).
 Show.
+Set Printing Width 148.
 (assert (Hv : value_type (TCName c)) by constructor; assert (Hm : |-[ 0] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption);
-  specialize (H 0 _ Hv)).
+  specialize (H 0 _ Hv); destruct H as [H _]; specialize (H Hm)).
 Show.
