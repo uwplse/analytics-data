7bc86bc3bcdd062ebe4a3c0fa138a3bdb41592e2
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:04:40 -0700

    user 3, session 6414, part 43

--- diffs-w-failures-no-compiles/3/user-3-session-6414.v
+++ diffs-w-failures-no-compiles/3/user-3-session-6414.v
@@ -225,5 +225,58 @@ Timeout 1 Check @log_addr.
 Timeout 1 Check @block.
 Timeout 1 Check @log_addr.
 Set Printing Width 78.
-Definition log_length_ok d log :=
+Timeout 1 Check @Ascii.nat_ascii_embedding.
+Timeout 1 Check @split.
+Timeout 1 Check @block.
+Timeout 1 Check @block.
+Timeout 1 Check @block.
+Definition log_length_ok (d : disk) (log : list block) :=
   exists b, diskGet d 0 =?= b /\ block_to_addr b = length log.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqFp3lFJ"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqnfSPdu"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Check @block.
+Timeout 1 Check @log_addr.
+Timeout 1 Check @log_addr.
+Timeout 1 Check @log_length_ok.
+Timeout 1 Check @log_length_ok.
+Timeout 1 Check @log_length_ok.
+Timeout 1 Check @log_length_ok.
+Timeout 1 Check @log_length_ok.
+Timeout 1 Check @block.
+Definition log_abstraction (d : disk) (log : list block) : Prop :=
+  log_length_ok d log /\
+  (forall a, a < length log -> diskGet d (log_addr a) =?= nth a log block0).
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq06DbI9"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqhweL2o"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Definition abstr : Abstraction State :=
+  abstraction_compose d.abstr {| abstraction := log_abstraction |}.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq6ttMg2"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq81nWO4"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Theorem init_ok : init_abstraction init recover abstr inited_any.
+Proof.
+Timeout 1 Check @Ascii.nat_ascii_bounded.
+Timeout 1 Check @Wf.F_unfold.
+Timeout 1 Check @Wf.F_unfold.
+Timeout 1 Check @Wf.F_unfold.
+(eapply then_init_compose; eauto).
