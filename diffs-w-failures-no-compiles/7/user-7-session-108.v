85358a2bf295910f4f27cc4400ed54db53ae5a5d
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:48 -0700

    user 7, session 108, part 64

--- diffs-w-failures-no-compiles/7/user-7-session-108.v
+++ diffs-w-failures-no-compiles/7/user-7-session-108.v
@@ -326,5 +326,10 @@ admit.
 -
 (destruct Hcontra as [v Hcontra]).
 Search -id.
-(destruct (beq_id X i)).
-Show.
+Set Printing Width 148.
+(destruct (beq_idP X i)).
++
+subst.
+(apply Hnotm).
+exists v.
+assumption.
