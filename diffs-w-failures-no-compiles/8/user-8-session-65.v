230f80302c15ca6b5d8d613c8119c9adb2c847d2
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:28:17 -0700

    user 8, session 65, part 169

--- diffs-w-failures-no-compiles/8/user-8-session-65.v
+++ diffs-w-failures-no-compiles/8/user-8-session-65.v
@@ -1143,4 +1143,8 @@ Msimpl.
 (destruct b; simpl; Msimpl).
 +
 mat_replace \226\159\1681\226\136\163 \195\151 \226\136\1631\226\159\169 with I 1 by lma.
+Unset Silent.
+Show.
+Set Printing Width 85.
+Show.
 Msimpl.
