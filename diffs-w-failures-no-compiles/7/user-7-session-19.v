21af2575a2a36bd2f5bc493fa4fd2eb6040e9a45
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:09 -0700

    user 7, session 19, part 137

--- diffs-w-failures-no-compiles/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles/7/user-7-session-19.v
@@ -131,6 +131,9 @@ Show.
 Set Printing Width 148.
 Set Printing Width 148.
 Show.
+Set Printing Width 148.
 (match goal with
- | |- | ?t1 | = | ?t2 | => assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption)
+ | |- | ?t1 | = | ?t2 | =>
+       assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+        (H _ _ Hv)
  end).
