b61c409d53062837ad898c7a6fd05d1ca4168fc8
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:07:52 -0700

    user 3, session 7199, part 463

--- diffs-w-failures-no-compiles/3/user-3-session-7199.v
+++ diffs-w-failures-no-compiles/3/user-3-session-7199.v
@@ -705,7 +705,8 @@ Show.
 (assert (len_addr < diskSize d') by eauto).
 (unfold log_abstraction; intuition).
 -
-(unfold log_length_ok in *; intros).
-Timeout 1 Check @rec_wipe_compose.
-Timeout 1 Check @nodup.
-autorewrite with upd in *.
+Unset Silent.
+Set Diffs "off".
+Set Printing Width 78.
+Show.
+(unfold log_length_ok in *; intros; autorewrite with upd in *).
