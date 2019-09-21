b3971e87eee2ccf26f1f70f9a3f25108163680af
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:19:22 -0700

    user 3, session 10289, part 4

--- diffs-w-failures-no-compiles/3/user-3-session-10289.v
+++ diffs-w-failures-no-compiles/3/user-3-session-10289.v
@@ -7,10 +7,6 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 78.
-Set Silent.
-Require Import Helpers.Helpers.
-Require Import Proc.
-Require Import ProcTheorems.
 Unset Silent.
 Set Diffs "off".
 Timeout 1 Check @Ret.
