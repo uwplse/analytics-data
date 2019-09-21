d2587b0377925d52fabaef2ca618067ee2e632b7
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:29:21 -0700

    user 10, session 15, part 32

--- diffs-w-failures-no-compiles/10/user-10-session-15.v
+++ diffs-w-failures-no-compiles/10/user-10-session-15.v
@@ -24,26 +24,14 @@ Unset Silent.
 Set Printing Width 114.
 Unset Silent.
 Set Printing Width 114.
-From Coq Require Import List NArith Streams.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coq23gu0W" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
 Unset Silent.
 Set Printing Width 114.
-Import ListNotations.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqOesgEy" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
 Set Silent.
+From Coq Require Import NArith Streams.
 Open Scope N_scope.
+CoFixpoint Fib (a b : N) : Stream N := Cons a (print_id (Fib b (a + b))).
 Unset Silent.
-Fixpoint fib (fuel : nat) (a b : N) : list N :=
-  match fuel with
-  | O => []
-  | S fuel => a :: print_id (fib fuel b (a + b))
-  end.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqUPskrz" Print Ltac Signatures.
+Eval compute in Str_nth 10 (Fib 0 1).
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqaE2wIH" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
-Unset Silent.
-Set Printing Width 114.
-Unset Silent.
+Timeout 1 Print LoadPath.
