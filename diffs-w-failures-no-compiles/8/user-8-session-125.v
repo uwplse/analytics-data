c94ec63ea42f601106ace8062c8bd7299875fb44
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:28:54 -0700

    user 8, session 125, part 170

--- diffs-w-failures-no-compiles/8/user-8-session-125.v
+++ diffs-w-failures-no-compiles/8/user-8-session-125.v
@@ -130,26 +130,5 @@ easy.
 Unset Silent.
 }
 replace (size_wtype W1 + size_wtype W2)%nat with \226\159\166 W \226\159\167 by (subst; easy).
-(unfold apply_U, apply_unitary, super).
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
 Unset Silent.
 Show.
