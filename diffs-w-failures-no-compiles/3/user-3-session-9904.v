17a4d356f545af55ac1da6f958755001dd540de9
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:18:14 -0700

    user 3, session 9904, part 98

--- diffs-w-failures-no-compiles/3/user-3-session-9904.v
+++ diffs-w-failures-no-compiles/3/user-3-session-9904.v
@@ -240,8 +240,18 @@ Timeout 1 Check @recovered.
 Timeout 1 Check @Ascii.nat_ascii_embedding.
 Set Printing Width 78.
 Show.
+Unset Silent.
+Set Diffs "off".
+Timeout 1 Check @firstn_length.
+Timeout 1 Check @Ascii.nat_ascii_embedding.
+Set Printing Width 78.
+Show.
 (match goal with
  | H:pre (match ?a with
           | (x, y) => _
-          end _) |- _ => destruct a as [x y]; cbn[pre post recovered] in *
+          end _)
+   |- _ =>
+       let x := fresh x in
+       let y := fresh y in
+       destruct a as [x y]; cbn[pre post recovered] in *
  end).
