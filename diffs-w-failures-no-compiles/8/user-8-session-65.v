629600ba3f0757cffc3a974cdcb190806fcfc9f1
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:28:21 -0700

    user 8, session 65, part 250

--- diffs-w-failures-no-compiles/8/user-8-session-65.v
+++ diffs-w-failures-no-compiles/8/user-8-session-65.v
@@ -1465,7 +1465,5 @@ Unset Silent.
 Show.
 Set Printing Width 85.
 Show.
-(repeat simpl_rewrite inSeq_correct; try apply inSeq_WT; try apply unitary_at1_WT;
-  try apply source_symmetric_WT; trivial).
 Unset Silent.
 Show.
