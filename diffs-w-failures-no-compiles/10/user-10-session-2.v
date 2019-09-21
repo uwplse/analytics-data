39d4fdeb442e342a153261096d6fc61b66d383ac
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:29:10 -0700

    user 10, session 2, part 1

--- diffs-w-failures-no-compiles/10/user-10-session-2.v
+++ diffs-w-failures-no-compiles/10/user-10-session-2.v
@@ -1,3 +1,7 @@
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqWXn2UD" Print Ltac Signatures.
+Unset Silent.
+Set Printing Width 114.
+Require Import Analytical.Analytics.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqBDrtIb" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
+Print Analytics.
