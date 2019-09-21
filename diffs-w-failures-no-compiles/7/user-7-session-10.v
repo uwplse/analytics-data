78d64c58138b7c9177cde34597e2111cae69cf3c
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:23:48 -0700

    user 7, session 10, part 37

--- diffs-w-failures-no-compiles/7/user-7-session-10.v
+++ diffs-w-failures-no-compiles/7/user-7-session-10.v
@@ -222,5 +222,6 @@ Set Silent.
 +
 Set Printing Width 148.
 Set Printing Width 148.
-(induction Hsub'; inversion Heqy; subst; try (solve [ auto ])).
+Set Printing Width 148.
+(induction Hsub'; inversion Heqy; subst; try (solve [ auto || constructor; tauto ])).
 Show.
