9cd3d6c0382f5a3e5d5b27391b89f32b0096af11
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:53 -0700

    user 7, session 192, part 12

--- diffs-w-failures-no-compiles/7/user-7-session-192.v
+++ diffs-w-failures-no-compiles/7/user-7-session-192.v
@@ -35,7 +35,15 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 (rewrite subst_union).
+Set Printing Width 148.
+(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
+  destruct IHt' as [IHt'a IHt'b]; split; intros HX').
+*
+(destruct (fresh_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
+(apply match_ty_union_1; auto).
+*
 Show.
-Set Silent.
-(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']).
-Unset Silent.
+(destruct (either_free_or_fresh_in_ty X' t'1) as [HXt'1| HXt'1]).
+Show.
+(apply match_ty_union_1; auto).
+Show.
