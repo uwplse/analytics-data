54110f9a18348a294cf1e81064f9612f3cd10198
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:17:24 -0700

    user 3, session 9671, part 38

--- diffs-w-failures-no-compiles/3/user-3-session-9671.v
+++ diffs-w-failures-no-compiles/3/user-3-session-9671.v
@@ -553,4 +553,30 @@ Unset Silent.
 Set Diffs "off".
 Set Printing Width 68.
 Show.
-(apply log_abstraction_nil with (b := r)).
+Unset Silent.
+Set Diffs "off".
+Timeout 1 Check @Tauto.A.
+Set Printing Width 68.
+Show.
+(apply log_abstraction_nil with (b := r); auto).
+(rewrite diskUpd_eq; eauto).
+}
+{
+(exists []; intuition eauto).
+(apply log_abstraction_nil with (b := r); auto).
+(rewrite diskUpd_eq; eauto).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqWPcUzk"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+}
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqFCHAAk"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
