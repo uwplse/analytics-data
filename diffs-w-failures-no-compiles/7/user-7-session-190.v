f323aa3a03ddb1c48323580211db13e490554638
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:51 -0700

    user 7, session 190, part 2

--- diffs-w-failures-no-compiles/7/user-7-session-190.v
+++ diffs-w-failures-no-compiles/7/user-7-session-190.v
@@ -41,4 +41,7 @@ Unset Silent.
 Set Silent.
 Show.
 Set Printing Width 148.
-(destruct (free_in_ty__decidable X t'1)).
+Set Printing Width 148.
+(destruct (free_in_ty__decidable X t'1) as [HXt'1| HXt'1]).
+(apply match_ty_union_1; auto).
+(exfalso; apply not__free_and_fresh).
