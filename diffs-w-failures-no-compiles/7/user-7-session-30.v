95e047bc203c7a19a84e87b54394554bf44a32d6
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:45 -0700

    user 7, session 30, part 29

--- diffs-w-failures-no-compiles/7/user-7-session-30.v
+++ diffs-w-failures-no-compiles/7/user-7-session-30.v
@@ -115,6 +115,18 @@ Unset Silent.
 (apply match_ty__value_type_l in Hm).
 (inversion Hm).
 Set Silent.
--
-Unset Silent.
+Set Printing Width 148.
 (intros t; induction t; intros k Hm; try (solve [ destruct k; contradiction | solve_match_ty__inv_depth_l__union_r IHt1 IHt2 ])).
+Set Silent.
++
+clear IHt.
+Unset Silent.
+(destruct k).
+contradiction.
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [t' [Heqt' [[Hdept Hdept'] _]]]).
+(inversion Heqt'; subst).
+(simpl).
+(rewrite Hdept').
+(split; apply le_n_S; assumption || constructor).
+Qed.
