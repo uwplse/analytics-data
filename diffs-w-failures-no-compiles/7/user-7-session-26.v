3f2cc63c26b8b05867ff30318cc0ff9cbe51c81a
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:40 -0700

    user 7, session 26, part 5

--- diffs-w-failures-no-compiles/7/user-7-session-26.v
+++ diffs-w-failures-no-compiles/7/user-7-session-26.v
@@ -62,7 +62,42 @@ Proof.
 Unset Silent.
 (destruct t; try (solve [ simpl; reflexivity ])).
 exfalso.
+Set Printing Width 148.
+Set Silent.
 (apply Hcontra).
-eexists.
-eexists.
+Unset Silent.
+(do 2 eexists).
 reflexivity.
+Qed.
+Set Silent.
+Ltac resolve_not_union := intros [tx [ty Hcontra]]; inversion Hcontra.
+Lemma unite_pairs_union_t : forall t1 t2 t' : ty, unite_pairs (TUnion t1 t2) t' = TUnion (unite_pairs t1 t') (unite_pairs t2 t').
+Proof.
+(intros t1 t2 t').
+(destruct t'; try (solve [ simpl; reflexivity ])).
+Unset Silent.
+Qed.
+Set Silent.
+Lemma mk_nf_pair : forall t1 t2 : ty, MkNF( TPair t1 t2) = unite_pairs (MkNF( t1)) (MkNF( t2)).
+Proof.
+reflexivity.
+Unset Silent.
+Qed.
+Set Silent.
+Lemma mk_nf_union : forall t1 t2 : ty, MkNF( TUnion t1 t2) = TUnion (MkNF( t1)) (MkNF( t2)).
+Proof.
+reflexivity.
+Unset Silent.
+Qed.
+Set Silent.
+Lemma mk_nf_ref : forall t : ty, MkNF( TRef t) = TRef (MkNF( t)).
+Proof.
+reflexivity.
+Unset Silent.
+Qed.
+Hint Resolve mk_nf_pair mk_nf_union mk_nf_ref: DBBetaJulia.
+Set Silent.
+Theorem mk_nf__in_nf : forall t : ty, InNF( MkNF( t)).
+Proof.
+Unset Silent.
+(intros t; induction t; try (solve [ auto using unite_pairs__preserves_nf with DBBetaJulia ])).
