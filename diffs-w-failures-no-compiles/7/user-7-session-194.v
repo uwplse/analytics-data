253cb373ec101f08c67425170ddb75896b1a1ed3
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:57 -0700

    user 7, session 194, part 8

--- diffs-w-failures-no-compiles/7/user-7-session-194.v
+++ diffs-w-failures-no-compiles/7/user-7-session-194.v
@@ -3,19 +3,20 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
+Set Printing Width 148.
 Set Silent.
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
-Set Printing Width 148.
-Set Silent.
 Require Import BetaJulia.Sub0282a.BaseDefs.
 Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Open Scope btjt.
-Set Printing Width 148.
-Set Silent.
+Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
+Proof.
+(intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
+Qed.
 Lemma f_free_in_ty__decidable : forall (X : id) (t : ty), Decidable.decidable (f_free_in_ty X t).
 Proof.
 (intros X t).
@@ -35,30 +36,25 @@ Lemma b_free_in_ty__dec : forall (X : id) (t : ty), b_free_in_ty X t \/ not_b_fr
 Proof.
 (intros X t).
 (apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
-Unset Silent.
 Qed.
-Set Silent.
 Lemma not_free_union__inv : forall (X : id) (fvs1 fvs2 : id_set), not_free X (IdSet.union fvs1 fvs2) -> not_free X fvs1 /\ not_free X fvs2.
 Proof.
 (intros X fvs1 fvs2 H).
 (unfold not_free in *).
 (split; intros Hcontra; [ apply (IdSetFacts.union_2 fvs2) in Hcontra | apply (IdSetFacts.union_3 fvs1) in Hcontra ]; contradiction).
-Unset Silent.
 Qed.
-Set Silent.
 Lemma not_f_free_in_ty_pair__inv : forall (X : id) (t1 t2 : ty), not_f_free_in_ty X (TPair t1 t2) -> not_f_free_in_ty X t1 /\ not_f_free_in_ty X t2.
 Proof.
 (intros X t1 t2 Hfresh).
 (unfold not_f_free_in_ty in *; simpl in Hfresh; simpl).
 (apply not_free_union__inv in Hfresh).
 assumption.
-Unset Silent.
 Qed.
-Set Silent.
 Lemma not_b_free_in_ty_pair__inv : forall (X : id) (t1 t2 : ty), not_b_free_in_ty X (TPair t1 t2) -> not_b_free_in_ty X t1 /\ not_b_free_in_ty X t2.
 Proof.
 (intros X t1 t2 Hfresh).
 (unfold not_b_free_in_ty in *; simpl in Hfresh; simpl).
 (apply not_free_union__inv in Hfresh).
+assumption.
 Unset Silent.
-Show.
+Qed.
