9fccadb38253ec7771339ea84b297d651ca7f2dc
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:10 -0700

    user 7, session 137, part 13

--- diffs-w-failures-no-compiles/7/user-7-session-137.v
+++ diffs-w-failures-no-compiles/7/user-7-session-137.v
@@ -3,34 +3,3 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
-Set Silent.
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0280a.BaseDefs.
-Require Import BetaJulia.Sub0280a.BaseProps.
-Require Import BetaJulia.Sub0280a.BaseMatchProps.
-Require Import BetaJulia.Sub0280a.BaseSemSubProps.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Silent.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Silent.
-(induction t; intros Hfresh; intros w1; exists w1; intros v Hm; destruct w1; try (solve [ apply match_ty_exist__0_inv in Hm; contradiction ])).
--
-(apply match_ty_exist__inv in Hm).
-(destruct Hm as [tx Hm]).
-(simpl in Hm).
-(eapply match_ty__ge_w).
-eassumption.
-(repeat constructor).
--
-Unset Silent.
-Show.
