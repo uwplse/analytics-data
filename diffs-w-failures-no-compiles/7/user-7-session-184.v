00e872f7564c4fd429c5e02b7afe17ad62021a9f
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:49 -0700

    user 7, session 184, part 23

--- diffs-w-failures-no-compiles/7/user-7-session-184.v
+++ diffs-w-failures-no-compiles/7/user-7-session-184.v
@@ -14,114 +14,11 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-Lemma subst_nested :
-  forall (X Y : id) (tX tY : ty), X <> Y -> fresh_in_ty Y tX -> forall t : ty, [X := tX] ([Y := tY] t) = [Y := [X := tX] tY] ([X := tX] t).
-Proof.
-(intros X Y tX tY Hneq HYfresh t).
-(induction t; try reflexivity).
--
-(repeat rewrite subst_pair).
-(rewrite IHt1, IHt2).
-reflexivity.
--
-(repeat rewrite subst_union).
-(rewrite IHt1, IHt2).
-reflexivity.
--
-(destruct (beq_idP Y i) as [HY| HY]).
-+
-subst.
-(destruct (beq_idP X i) as [HX| HX]).
-*
-subst.
-contradiction.
-*
-(rewrite subst_exist_eq).
-(rewrite subst_exist_neq; try assumption).
-(rewrite subst_exist_eq).
-reflexivity.
-+
-(destruct (beq_idP X i) as [HX| HX]).
-*
-subst.
-(repeat rewrite subst_exist_eq).
-(destruct (IdSetProps.In_dec i (FV tY)) as [Hin| Hin]).
-{
-(pose proof (IdSetFacts.mem_1 Hin) as Hmem).
-(rewrite (subst_equation Y tY)).
-(rewrite (false_beq_id _ _ HY)).
-(rewrite Hmem).
-(remember (gen_fresh (IdSet.union (FV tY) (IdSet.add Y (FV t)))) as z).
-Abort.
 Set Printing Width 148.
 Lemma build_v_full :
   forall (X X' : id) (w : nat) (t v : ty) (tx : ty),
   |-[ w] v <$ [X := tx] t ->
-  exists v' : ty, |-[ w] v' <$ [X := TVar X'] t /\ (forall (w' : nat) (t' : ty), fresh_in_ty X' t' -> |-[ w'] v' <$ t' -> |-[ w'] v <$ t').
-Set Silent.
+  exists v' : ty,
+    |-[ w] v' <$ [X := TVar X'] t /\
+    (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> (fresh_in_ty X' t' -> |-[ w'] v <$ t') /\ (free_in_ty X' t' -> |-[ w'] v <$ [X' := tx] t')).
 Proof.
-(intros X X').
-(induction w; induction t; intros v tx Hm).
--
-exists v.
-Unset Silent.
-split.
-assumption.
-tauto.
-Set Silent.
--
-Unset Silent.
-(rewrite subst_pair in *).
-Set Silent.
-(apply match_ty_pair__inv in Hm).
-(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-specialize (IHt1 _ _ Hm1).
-specialize (IHt2 _ _ Hm2).
-(destruct IHt1 as [v1' [Hm1' IHt1]]).
-(destruct IHt2 as [v2' [Hm2' IHt2]]).
-exists (TPair v1' v2').
-split.
-(apply match_ty_pair; assumption).
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-(induction w'; induction t'; intros HX' Hm'; try contradiction).
-Set Silent.
-+
-Unset Silent.
-Show.
-Show.
-(destruct (fresh_in_ty_pair__inv _ _ _ HX') as [HX'1 HX'2]).
-Set Silent.
-(apply match_ty_pair_pair__inv in Hm').
-(destruct Hm' as [Hm'1 Hm'2]).
-Unset Silent.
-(apply match_ty_pair; auto).
-Set Silent.
-+
-Unset Silent.
-(destruct (fresh_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]; apply match_ty_union__inv in Hm').
-(destruct Hm' as [Hm'| Hm']; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
-Set Silent.
-+
-Unset Silent.
-(destruct (fresh_in_ty_pair__inv _ _ _ HX') as [HX'1 HX'2]).
-Set Silent.
-(apply match_ty_pair_pair__inv in Hm').
-(destruct Hm' as [Hm'1 Hm'2]).
-Unset Silent.
-(apply match_ty_pair; auto).
-Set Silent.
-+
-Unset Silent.
-(destruct (fresh_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
-Set Silent.
-(apply match_ty_union__inv in Hm').
-Unset Silent.
-(destruct Hm' as [Hm'| Hm']; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
-Set Silent.
-+
-Unset Silent.
-Show.
-(apply match_ty_exist__inv in Hm').
-(destruct Hm' as [ti Hm']).
