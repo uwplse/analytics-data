2f3750cc83a7f95affe235b83b02d439be9fa448
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:55 -0700

    user 7, session 46, part 2

--- diffs-w-failures-no-compiles/7/user-7-session-46.v
+++ diffs-w-failures-no-compiles/7/user-7-session-46.v
@@ -9,13 +9,7 @@ Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0250a.BaseDefs.
 Require Import BetaJulia.Sub0250a.AltMatchDef.
 Require Import BetaJulia.BasicTactics.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Close Scope btjm.
-Unset Silent.
-Open Scope btjmi.
+Set Printing Width 148.
 Set Silent.
 Lemma match_ty_i_pair : forall (v1 v2 t1 t2 : ty) (k : nat), |-[ k] v1 <$ t1 -> |-[ k] v2 <$ t2 -> |-[ k] TPair v1 v2 <$ TPair t1 t2.
 Proof.
@@ -34,30 +28,41 @@ Proof.
 Qed.
 Lemma match_ty_i_cname__inv : forall (v : ty) (c : cname) (k : nat), |-[ k] v <$ TCName c -> v = TCName c.
 Proof.
-Unset Silent.
 (intros v; induction v; try (solve [ intros c k Hm; destruct k; contradiction ])).
-Set Silent.
 (intros c0 k Hm).
 (destruct k; simpl in Hm; subst; reflexivity).
-Unset Silent.
 Qed.
-Set Silent.
 Lemma match_ty_i_pair__inv :
   forall (v t1 t2 : ty) (k : nat), |-[ k] v <$ TPair t1 t2 -> exists v1 v2 : ty, v = TPair v1 v2 /\ |-[ k] v1 <$ t1 /\ |-[ k] v2 <$ t2.
 Proof.
-Unset Silent.
 (intros v; induction v; try (solve [ intros t1 t2 k Hm; destruct k; contradiction ])).
-Set Silent.
 (intros t1 t2 k Hm).
 exists v1,v2.
 (split; try reflexivity).
 (destruct k; simpl in Hm; assumption).
-Unset Silent.
 Qed.
-Set Silent.
 Lemma match_ty_i_union__inv : forall (v t1 t2 : ty) (k : nat), |-[ k] v <$ TUnion t1 t2 -> |-[ k] v <$ t1 \/ |-[ k] v <$ t2.
 Proof.
 (intros v t1 t2 k Hm).
 (destruct k; destruct v; assumption).
+Qed.
+Lemma match_ty_i_ref__weak_inv : forall (v t : ty) (k : nat), |-[ k] v <$ TRef t -> exists t' : ty, v = TRef t'.
+Proof.
+Unset Silent.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; contradiction ])).
+Set Silent.
+clear IHv.
+(intros t k).
+(intros Hm).
+exists v.
+reflexivity.
 Unset Silent.
 Qed.
+Set Silent.
+Lemma match_ty_i_ref__inv : forall (v t : ty) (k : nat), |-[ S k] v <$ TRef t -> exists t' : ty, v = TRef t' /\ ||-[ k][t']= [t].
+Unset Silent.
+Proof.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; contradiction ])).
+clear IHv.
+(intros t k).
+(intros Hm).
