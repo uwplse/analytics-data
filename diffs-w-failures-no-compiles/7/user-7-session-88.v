0372eb7fae82e96ec42b14b471caeb8bab31b01d
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:20 -0700

    user 7, session 88, part 62

--- diffs-w-failures-no-compiles/7/user-7-session-88.v
+++ diffs-w-failures-no-compiles/7/user-7-session-88.v
@@ -419,4 +419,4 @@ tauto.
 Qed.
 Lemma sem_sub_k_i_nf__inv_depth_le_1 : forall (k : nat) (t t' : ty), InNF( t) -> | t | <= k -> ||-[ k][t]<= [t'] -> | t | <= | t' |.
 Set Printing Width 148.
-(apply sem_sub_k_i_nf__inv_depth_le with k; tauto).
+(apply sem_sub_k_i_nf__inv_depth_le; tauto).
