bdef0e5ab0b15b7dc67fc5b5d308ea95c8917db9
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:23 -0700

    user 7, session 93, part 7

--- diffs-w-failures-no-compiles/7/user-7-session-93.v
+++ diffs-w-failures-no-compiles/7/user-7-session-93.v
@@ -16,51 +16,19 @@ Inductive cname : Type :=
   | NFlt : _
   | NStr : _.
 Set Printing Width 148.
-Set Printing Width 148.
-Definition varid : Type := nat.
 Set Silent.
 Inductive ty : Type :=
   | TCName : cname -> ty
   | TPair : ty -> ty -> ty
   | TUnion : ty -> ty -> ty
   | TRef : ty -> ty
-  | TVar : varid -> ty
-  | TExist : varid -> ty -> ty.
+  | TVar : id -> ty
+  | TExist : id -> ty -> ty.
 Definition tint := TCName NInt.
 Definition tflt := TCName NFlt.
 Definition tstr := TCName NStr.
 Definition tIntInt := TPair tint tint.
-Set Printing Width 148.
-Set Silent.
 Definition vX := 1.
 Definition vY := 2.
 Definition vZ := 3.
 Definition tX := TVar vX.
-Definition tY := TVar vY.
-Unset Silent.
-Set Printing Width 148.
-Set Silent.
-Definition teXX := TExist vX tX.
-Unset Silent.
-Definition tyXRefX := TExist vX (TRef tX).
-Set Silent.
-Declare Scope btjt_scope.
-Delimit Scope btjt_scope with btjt.
-Open Scope btjt.
-Reserved Notation "'|' t '|'" (at level 20).
-Unset Silent.
-Fixpoint inv_depth (t : ty) :=
-  match t with
-  | TCName _ => 0
-  | TPair t1 t2 => Nat.max (| t1 |) (| t2 |)
-  | TUnion t1 t2 => Nat.max (| t1 |) (| t2 |)
-  | TRef t' => 1 + | t' |
-  | TVar _ => 0
-  | TExist _ t' => | t' |
-  end
-where "'|' t '|'" := (inv_depth t) : btjt_scope.
-Inductive value_type : ty -> Prop :=
-  | VT_CName : forall cn, value_type (TCName cn)
-  | VT_Pair : forall v1 v2, value_type v1 -> value_type v2 -> value_type (TPair v1 v2)
-  | VT_Ref : forall t, value_type (TRef t).
-Hint Constructors value_type: DBBetaJulia.
