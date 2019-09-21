1527609c033fc7f0ce95c338f94d2402afb357b0
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:27:38 -0700

    user 8, session 31, part 1

--- diffs-w-failures-no-compiles/8/user-8-session-31.v
+++ diffs-w-failures-no-compiles/8/user-8-session-31.v
@@ -2190,6 +2190,7 @@ Proof.
 (intros).
 (unfold denote_circuit).
 (simpl; fold_denotation).
+Unset Silent.
 replace (process_gate g p1 \206\147) with
  (process_gate_pat g p1 \206\147, process_gate_state g p1 \206\147)
  by (symmetry; apply surjective_pairing).
