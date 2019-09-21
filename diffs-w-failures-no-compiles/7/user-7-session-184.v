36ba79214c7c6ee40f9598ca2764b224e0c0494b
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:48 -0700

    user 7, session 184, part 8

--- diffs-w-failures-no-compiles/7/user-7-session-184.v
+++ diffs-w-failures-no-compiles/7/user-7-session-184.v
@@ -94,7 +94,22 @@ Show.
 Show.
 +
 Show.
+Set Printing Width 148.
+Set Silent.
 (apply match_ty_union__inv in Hm').
 (destruct Hm' as [Hm'| Hm']; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
-Show.
+Unset Silent.
 +
+Show.
+Set Silent.
+(apply match_ty_pair_pair__inv in Hm').
+(destruct Hm' as [Hm'1 Hm'2]).
+Unset Silent.
+(apply match_ty_pair; auto).
++
+Set Silent.
+(apply match_ty_union__inv in Hm').
+Unset Silent.
+(destruct Hm' as [Hm'| Hm']; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
++
+admit.
