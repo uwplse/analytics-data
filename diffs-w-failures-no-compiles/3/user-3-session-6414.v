46c85fd49125736fa35c729785564ca4d59e023a
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:04:38 -0700

    user 3, session 6414, part 1

--- diffs-w-failures-no-compiles/3/user-3-session-6414.v
+++ diffs-w-failures-no-compiles/3/user-3-session-6414.v
@@ -26,13 +26,15 @@ Timeout 1 Print LoadPath.
 Set Silent.
 Module Log (d: OneDiskAPI)<: LogAPI.
 Unset Silent.
+Set Diffs "off".
+Set Printing Width 78.
 Definition init : proc InitResult := Ret Initialized.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqiioFE0"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq9tVkPi"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqPEXB6G"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq5hDUtn"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
