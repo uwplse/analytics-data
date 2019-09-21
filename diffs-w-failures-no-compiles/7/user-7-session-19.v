9f4fe1488b7238b103f44183f4b6399dbeb1ddf2
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:15 -0700

    user 7, session 19, part 278

--- diffs-w-failures-no-compiles/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles/7/user-7-session-19.v
@@ -211,5 +211,11 @@ Set Printing Width 148.
 (apply match_ty_i_union__inv in Hmu).
 Show.
 Set Printing Width 148.
+Set Printing Width 148.
 (destruct Hmu as [Hmu1| Hmu2]; [ left | right ]; intros v Hv Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst).
 Show.
+(assert (Hmt't : |-[ S k] TRef t' <$ TRef t)).
+Show.
+(simpl).
+Show.
+assumption.
