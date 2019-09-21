53162b49eaa4753426f91fcc50862ba399f1288e
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:27:55 -0700

    user 8, session 40, part 13

--- diffs-w-failures-no-compiles/8/user-8-session-40.v
+++ diffs-w-failures-no-compiles/8/user-8-session-40.v
@@ -75,7 +75,8 @@ Lemma X_spec :
   forall b safe : bool,
   denote_box safe (boxed_gate _X) (bool_to_matrix b) == bool_to_matrix (\194\172 b).
 Proof.
-(intros).
-vector_denote.
-Set Silent.
 Unset Silent.
+Show.
+Set Printing Width 85.
+Show.
+vector_denote.
