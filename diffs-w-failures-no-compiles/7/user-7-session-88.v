a33681a9cbe403f328c39bac13ed59f0773be98a
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:18 -0700

    user 7, session 88, part 7

--- diffs-w-failures-no-compiles/7/user-7-session-88.v
+++ diffs-w-failures-no-compiles/7/user-7-session-88.v
@@ -415,6 +415,6 @@ Qed.
 Set Printing Width 148.
 Set Silent.
 Lemma sem_sub_k_i_nf__inv_depth_le : forall (k : nat) (t t' : ty), InNF( t) -> | t | <= k \/ | t' | <= k -> ||-[ k][t]<= [t'] -> | t | <= | t' |.
-Proof.
-Unset Silent.
-(induction k; induction t; induction t'; intros Hnft Hdept Hsem; try (solve [ simpl; constructor ])).
+Set Printing Width 148.
+(induction k; induction t; induction t'; intros Hnft Hdept Hsem).
+Show.
