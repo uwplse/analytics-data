3084e6792f9c1efd272dec62dab58a023b8a0d2c
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:27:56 -0700

    user 8, session 40, part 56

--- diffs-w-failures-no-compiles/8/user-8-session-40.v
+++ diffs-w-failures-no-compiles/8/user-8-session-40.v
@@ -297,9 +297,28 @@ Proof.
 vector_denote.
 (destruct x, z; unfold bool_to_ket; simpl; Msimpl; lma).
 Qed.
+Unset Silent.
+Set Printing Width 85.
+Set Silent.
 Lemma XOR_spec :
   forall x y z safe : bool,
-  denote_box safe XOR (bool_to_matrix x \226\138\151 bool_to_matrix y \226\138\151 bool_to_matrix z) =
+  denote_box safe XOR (bool_to_matrix x \226\138\151 bool_to_matrix y \226\138\151 bool_to_matrix z) ==
   bool_to_matrix x \226\138\151 bool_to_matrix y \226\138\151 bool_to_matrix ((x \226\138\149 y) \226\138\149 z).
 Proof.
+vector_denote.
+Msimpl.
+(destruct x, y, z; simpl; lma).
+Qed.
+Lemma AND_spec :
+  forall x y z safe : bool,
+  denote_box safe AND (bool_to_matrix x \226\138\151 bool_to_matrix y \226\138\151 bool_to_matrix z) ==
+  bool_to_matrix x \226\138\151 bool_to_matrix y \226\138\151 bool_to_matrix ((x && y) \226\138\149 z).
+Proof.
+vector_denote.
+Msimpl.
+(destruct x, y, z; simpl; Msimpl; lma).
 Unset Silent.
+Qed.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coq7zG7mX"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
