bb7cd56e541e337274c11042eaad2b3823696f74
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:23:56 -0700

    user 7, session 16, part 44

--- diffs-w-failures-no-compiles/7/user-7-session-16.v
+++ diffs-w-failures-no-compiles/7/user-7-session-16.v
@@ -414,4 +414,7 @@ Set Printing Width 148.
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
 +
-(right; solve_not_x_sub_r_y_full).
+right.
+(match goal with
+ | |- ~ |- ?tx << ?ty => remember tx as tx eqn:Heq1 ; remember ty as ty eqn:Heq2 
+ end).
