0a7fdacd03fac02e13d71a0cfc9b4756632b1c7c
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:28:56 -0700

    user 8, session 125, part 210

--- diffs-w-failures-no-compiles/8/user-8-session-125.v
+++ diffs-w-failures-no-compiles/8/user-8-session-125.v
@@ -148,6 +148,5 @@ Set Silent.
 Unset Silent.
 (remember (denote_ctrls (\226\159\166 W3 \226\138\151 W4 \226\159\167) U li) as A).
 (remember (swap_list (\226\159\166 W3 \226\138\151 W4 \226\159\167) li) as S).
-(rewrite <- (Mmult_assoc _ (A \195\151 \207\129) _)).
 Unset Silent.
 Show.
