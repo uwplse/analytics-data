d4db673ec2bf3f047cdecbc303a8202ac014333d
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:02 -0700

    user 7, session 120, part 12

--- diffs-w-failures-no-compiles/7/user-7-session-120.v
+++ diffs-w-failures-no-compiles/7/user-7-session-120.v
@@ -42,4 +42,5 @@ Lemma sem_sub_pair : forall t1 t2 t1' t2' : ty, ||- [t1]<= [t1'] -> ||- [t2]<= [
 Unset Silent.
 Proof.
 (intros t1 t2 t1' t2' Hem1 Hsem2 k).
-(apply sem_sub_k_pair; auto).
+Set Printing Width 148.
+(apply sem_sub_k_pair; tauto).
