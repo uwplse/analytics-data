ff943377f60101847224e04582e06f8d303da31c
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:14 -0700

    user 7, session 19, part 266

--- diffs-w-failures-no-compiles/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles/7/user-7-session-19.v
@@ -176,8 +176,7 @@ Set Silent.
 Set Printing Width 148.
 (destruct v1; contradiction || constructor).
 Show.
-(apply match_ty_i_ref__inv in Hm2).
 Show.
 Set Printing Width 148.
-(destruct Hm2 as [t'' [Heq' Href']]; inversion Heq'; subst).
+(apply match_ty_i_ref__inv in Hm1).
 Show.
