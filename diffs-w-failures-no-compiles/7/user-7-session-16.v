9758cececf69c7d4b98a0c5fe585672c297b2393
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:23:57 -0700

    user 7, session 16, part 67

--- diffs-w-failures-no-compiles/7/user-7-session-16.v
+++ diffs-w-failures-no-compiles/7/user-7-session-16.v
@@ -373,33 +373,26 @@ assumption.
 (right; intros Hcontra).
 (apply SR_NormalForm in Hcontra; contradiction).
 Qed.
-Ltac
- solve_not_x_sub_r_y_full :=
-  match goal with
-  | |- ~ |- ?t1 << ?t2 =>
-        remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra;
-         try (solve [ inversion Heqx | inversion Heqy ]); subst
-  end; match goal with
-       | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto
-       end.
 Set Printing Width 148.
+Ltac
+ solve_atom_sub_r_union__decidable IHt2_1 IHt2_2 :=
+  destruct IHt2_1 as [IH1| IH1]; try assumption; destruct IHt2_2 as [IH2| IH2]; try assumption;
+   try (solve [ left; apply SR_UnionR1; assumption | left; apply SR_UnionR2; assumption ]); right; intros Hcontra;
+   apply atom_sub_r_union__inv in Hcontra; tauto || constructor.
 Set Silent.
 Lemma nf_sub_r__decidable2 :
   forall t : ty,
   InNF( t) -> (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t)).
 Proof.
-Unset Silent.
 (apply
   (in_nf_mut
      (fun (t : ty) (Hat : atom_type t) =>
       (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t)))
      (fun (t : ty) (Hnf : in_nf t) =>
       (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t))))).
-Set Silent.
 -
-Set Printing Width 148.
-Set Printing Width 148.
-Check in_nf_union__inv.
+(intros c).
+Unset Silent.
 (split; intros t'; induction t'; intros Hnf'; try destruct (in_nf_union__inv _ _ Hnf') as [Hnf'1 Hnf'2];
   try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 ]);
   try
@@ -408,4 +401,12 @@ Check in_nf_union__inv.
          destruct (cname_eq__decidable c1 c2);
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
-Show.
+Set Silent.
++
+Unset Silent.
+right.
+(match goal with
+ | |- ~ |- ?t1 << ?t2 =>
+       remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]);
+        subst
+ end).
