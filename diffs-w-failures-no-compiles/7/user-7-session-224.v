f429ed7394d3e9a440855132b16c538a3ec1830a
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:27:18 -0700

    user 7, session 224, part 53

--- diffs-w-failures-no-compiles/7/user-7-session-224.v
+++ diffs-w-failures-no-compiles/7/user-7-session-224.v
@@ -15,6 +15,8 @@ Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Set Printing Width 148.
+Set Printing Width 148.
+Set Silent.
 Lemma build_v_full :
   forall (X X' : id) (tx : ty) (w : nat) (t v : ty),
   wf_ty tx ->
@@ -23,196 +25,44 @@ Lemma build_v_full :
   exists v' : ty,
     |-[ w] v' <$ [BX := TFVar X'] t /\
     (forall (w' : nat) (t' : ty),
-     |-[ w'] v' <$ t' -> (not_f_free_in_ty X' t' -> |-[ w'] v <$ t') /\ (f_free_in_ty X' t' -> exists w2, |-[ w2] v <$ [FX' := tx] t')).
-Set Printing Width 148.
-(induction w; induction t; intros v Hwftx HX Hm;
-  try (solve [ unfold b_free_in_ty, free in HX; simpl in HX; rewrite IdSetFacts.empty_iff in HX; contradiction ])).
-Show.
-Set Silent.
-Set Printing Width 148.
-admit.
-Show.
+     |-[ w'] v' <$ t' -> (not_f_free_in_ty X' t' -> exists w2, |-[ w2] v <$ t') /\ (f_free_in_ty X' t' -> exists w2, |-[ w2] v <$ [FX' := tx] t')).
+Proof.
+Abort.
+Open Scope btjm.
+Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
+Proof.
+(intros t1 t2 Hsub).
+(induction Hsub).
 -
-Show.
-admit.
-Show.
-Set Silent.
+(apply sem_sub__refl).
+-
+(apply sem_sub__trans with t2; assumption).
+-
+(apply sem_sub_pair; assumption).
+-
+(apply sem_sub_union; assumption).
+-
+(apply sem_sub_union_1).
+(apply sem_sub__refl).
+-
+(apply sem_sub_union_2).
+(apply sem_sub__refl).
+-
+(intros w1).
+exists w1.
+(intros v Hm).
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_union__inv in Hm1).
+(destruct Hm1; [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto using match_ty_pair).
+-
+(intros w1).
+exists w1.
+(intros v Hm).
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto using match_ty_pair).
 -
 Unset Silent.
 Show.
-(destruct (beq_idP X i)).
-+
-subst.
-Show.
-Set Printing Width 148.
-(rewrite b_subst_exist_eq in Hm).
-(apply match_ty_exist__0_inv in Hm).
-contradiction.
-+
-Set Printing Width 148.
-Set Silent.
-subst.
-(rewrite b_subst_exist_neq in Hm; try assumption).
-(apply match_ty_exist__0_inv in Hm).
-Unset Silent.
-contradiction.
--
-(destruct (beq_idP X i)).
-+
-subst.
-(rewrite b_subst_bvar_eq in *).
-exists (TEV X').
-split.
-reflexivity.
-(induction w'; induction t'; intros Hm'; try (solve [ destruct v; contradiction || tauto ])).
-Set Printing Width 148.
-Show.
-Set Silent.
-(rewrite f_subst_union).
-Unset Silent.
-(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
-  destruct IHt' as [IHt'a IHt'b]; split; intros HX').
-Set Silent.
-{
-(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
-(apply match_ty_union_1; auto).
-Unset Silent.
-}
-Set Silent.
-{
-Unset Silent.
-(destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
-{
-specialize (IHt'b HXt'1).
-(destruct IHt'b as [w2 IHt'b]).
-exists w2.
-Set Printing Width 148.
-Set Silent.
-(apply match_ty_union_1; assumption).
-Unset Silent.
-}
-{
-exists 0.
-(apply match_ty_union_1).
-(rewrite f_subst_not_b_free_in_ty; auto).
-}
-}
-{
-(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
-(apply match_ty_union_2; auto).
-}
-{
-(destruct (f_free_in_ty__dec X' t'2) as [HXt'2| HXt'2]).
-{
-specialize (IHt'b HXt'2).
-(destruct IHt'b as [w2 IHt'b]).
-Set Silent.
-Unset Silent.
-exists w2.
-(apply match_ty_union_2; auto).
-}
-{
-exists 0.
-(apply match_ty_union_2).
-(rewrite f_subst_not_b_free_in_ty; auto).
-}
-}
-*
-(destruct (beq_idP X' i0)).
-{
-subst.
-Set Printing Width 148.
-(split; intros HX').
-Set Silent.
-{
-Unset Silent.
-(unfold not_f_free_in_ty, not_free in HX').
-(simpl in HX').
-Set Silent.
-exfalso.
-(apply HX').
-(apply IdSetFacts.singleton_2).
-reflexivity.
-}
-{
-exists 0.
-(rewrite f_subst_fvar_eq).
-assumption.
-}
-}
-Show.
-Set Printing Width 148.
-(simpl in Hm').
-Show.
-contradiction.
-}
-Show.
-*
-Show.
-Show.
-Set Printing Width 148.
-Set Printing Width 148.
-admit.
-*
-Show.
-Set Silent.
-(rewrite f_subst_union).
-(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
-  destruct IHt' as [IHt'a IHt'b]; split; intros HX').
-{
-(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
-(apply match_ty_union_1; auto).
-}
-{
-(destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
-{
-specialize (IHt'b HXt'1).
-(destruct IHt'b as [w2 IHt'b]).
-exists w2.
-(apply match_ty_union_1; assumption).
-}
-{
-Unset Silent.
-Show.
-Show.
-exists (S w').
-(apply match_ty_union_1).
-(rewrite f_subst_not_b_free_in_ty; auto).
-}
-}
-{
-(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
-(apply match_ty_union_2; auto).
-}
-{
-(destruct (f_free_in_ty__dec X' t'2) as [HXt'2| HXt'2]).
-{
-specialize (IHt'b HXt'2).
-(destruct IHt'b as [w2 IHt'b]).
-exists w2.
-(apply match_ty_union_2; auto).
-}
-{
-Set Printing Width 148.
-exists (S w').
-Set Silent.
-(apply match_ty_union_2).
-(rewrite f_subst_not_b_free_in_ty; auto).
-}
-Unset Silent.
-}
-*
-Show.
-Set Printing Width 148.
-(split; intros HX').
-{
-(apply match_ty_exist__inv in Hm').
-(destruct Hm' as [ti [Hwfti Hm']]).
-specialize (IHw' _ Hm').
-Show.
-Set Printing Width 148.
-(destruct (f_free_in_ty__dec X' ([Bi0 := ti] t'))).
-{
-Set Printing Width 148.
-Set Printing Width 148.
-(destruct IHw' as [_ IHw']).
