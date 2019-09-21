15ca583d451164e98be98308b7f6bfe27f3134c1
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:29:18 -0700

    user 10, session 13, part 9

--- diffs-w-failures-no-compiles/10/user-10-session-13.v
+++ diffs-w-failures-no-compiles/10/user-10-session-13.v
@@ -13,23 +13,6 @@ Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coq5lrxpQ" Print Ltac
 Timeout 1 Print Grammar tactic.
 Unset Silent.
 Set Printing Width 114.
-From Coq Require Import NArith Streams.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqznWAeW" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-From ReductionEffect Require Import PrintingEffect.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqTlmINW" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Check map.
-CoFixpoint fib (x y : N) : Stream N := Cons y (fib y (x + y)).
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqbF6gTS" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Check @fib.
 Unset Silent.
 Set Printing Width 114.
-Unset Silent.
-Set Printing Width 114.
-Unset Silent.
-Set Printing Width 114.
-Eval compute in Str_nth 10 (map print_id (fib 0 1)).
+From Coq Require Import NArith Lists Streams.
