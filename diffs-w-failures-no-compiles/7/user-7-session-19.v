271880d2a3a714a3d8792ffe2cf06acc5131f6b6
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:19 -0700

    user 7, session 19, part 387

--- diffs-w-failures-no-compiles/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles/7/user-7-session-19.v
@@ -237,6 +237,5 @@ Set Printing Width 148.
             tauto || apply Max.le_max_l || apply Max.le_max_r
      end ])).
 -
-(destruct (match_ty_i_exists t1 0) as [v1 Hm1]; destruct (match_ty_i_exists t2 0) as [v2 Hm2]).
 Set Printing Width 148.
-(assert (Hmp : |-[ 0] TPair v1 v2 <$ TPair t1 t2) by (apply match_ty_i_pair; assumption)).
+(destruct (match_ty_i_exists t1 0) as [v1 Hm1]; destruct (match_ty_i_exists t2 0) as [v2 Hm2]).
