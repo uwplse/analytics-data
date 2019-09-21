8772ef10694c69a6bb67af3c80de70a3b5f53b08
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:37 -0700

    user 7, session 24, part 18

--- diffs-w-failures-no-compiles/7/user-7-session-24.v
+++ diffs-w-failures-no-compiles/7/user-7-session-24.v
@@ -60,8 +60,7 @@ Function odd (n : nat) := match n with
                           end even (n : nat) := false.
 Set Silent.
 Reserved Notation "'|-[' k ']' v '<$' t" (at level 50).
-Reserved Notation "'||-[' k ']' '[' t1 ']' '=' '[' t2 ']'" (at level 45).
-Unset Silent.
+Set Printing Width 148.
 Fixpoint match_ty (k : nat) :=
   fix mty (v : ty) :=
     fix mty' (t : ty) :=
@@ -69,8 +68,7 @@ Fixpoint match_ty (k : nat) :=
       | _, TCName c, TCName c' => c = c'
       | _, TPair v1 v2, TPair t1 t2 => mty v1 t1 /\ mty v2 t2
       | _, _, TUnion t1 t2 => mty' t1 \/ mty' t2
-      | S k, TRef t', TRef t => (inv_depth t <= k /\ inv_depth t' = inv_depth t) /\ sem_eq_k k t' t
+      | S k, TRef t', TRef t => (inv_depth t <= k /\ inv_depth t' = inv_depth t) /\ (forall v, |-[ k] v <$ t' <-> |-[ k] v <$ t)
       | _, _, _ => False
       end
-where "|-[ k ']' v '<$' t" := (match_ty k v t) : btjm_scope
-with sem_eq_k (k : nat) (t1 t2 : ty) := forall v : ty, |-[ k] v <$ t1 <-> |-[ k] v <$ t2.
+where "|-[ k ']' v '<$' t" := (match_ty k v t) : btjm_scope.
