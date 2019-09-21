4a3ccddfb7e60924bf9389fd2c0b2df64e350438
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:18:12 -0700

    user 3, session 9904, part 59

--- diffs-w-failures-no-compiles/3/user-3-session-9904.v
+++ diffs-w-failures-no-compiles/3/user-3-session-9904.v
@@ -220,37 +220,13 @@ Show.
 (step_proc; intuition).
 Timeout 1 Check @spec_abstraction_compose.
 Timeout 1 Check @Ascii.nat_ascii_embedding.
-(step_proc; intuition).
-Timeout 1 Check @eq_existT_curried.
-Timeout 1 Check @eq_existT_curried.
-Timeout 1 Check @Ascii.nat_ascii_bounded.
-Timeout 1 Check @Ascii.nat_ascii_embedding.
 Unset Silent.
 Set Diffs "off".
-Timeout 1 Check @eq_existT_curried.
-Timeout 1 Check @eq_existT_curried.
 Set Printing Width 78.
 Show.
-Unset Silent.
-Set Diffs "off".
-Timeout 1 Check @eq_existT_curried.
-Timeout 1 Check @eq_existT_curried.
-Timeout 1 Check @eq_existT_curried.
-Set Printing Width 78.
-Show.
-Unset Silent.
-Set Diffs "off".
-Timeout 1 Check @Wf.F_unfold.
-Timeout 1 Check @statdb_abstraction.
-Timeout 1 Check @statdb_abstraction.
-Timeout 1 Check @statdb_abstraction.
-Timeout 1 Check @statdb_abstraction.
-Timeout 1 Check @statdb_abstraction.
-Timeout 1 Check @statdb_abstraction.
-Timeout 1 Check @statdb_abstraction.
-Timeout 1 Check @statdb_abstraction.
-Timeout 1 Check @Ascii.nat_ascii_embedding.
-Set Printing Width 78.
-Show.
-Unset Silent.
-Set Diffs "off".
+(destruct s; intuition).
++
+(unfold statdb_abstraction in *; simpl in *).
+intuition.
++
+(exists (n0 :: s); intuition).
