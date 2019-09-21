190748ab15c55bb9c11bf44ef9d53774981e25db
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:50 -0700

    user 7, session 41, part 2

--- diffs-w-failures-no-compiles/7/user-7-session-41.v
+++ diffs-w-failures-no-compiles/7/user-7-session-41.v
@@ -3,6 +3,7 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
+Set Printing Width 148.
 Set Silent.
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
@@ -13,4 +14,4 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-Unset Silent.
+Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
