55457cde1a38703967aae5682dd17d7787270499
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:09 -0700

    user 7, session 137, part 11

--- diffs-w-failures-no-compiles/7/user-7-session-137.v
+++ diffs-w-failures-no-compiles/7/user-7-session-137.v
@@ -20,20 +20,6 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Silent.
 Set Printing Width 148.
-(induction t; intros Hfresh; intros w1; exists w1; intros v Hm; destruct w1; try (solve [ apply match_ty_exist__0_inv in Hm; contradiction ])).
+Set Printing Width 148.
+(induction t; intros Hfresh).
 Show.
-Set Silent.
--
-(apply match_ty_exist__inv in Hm).
-(destruct Hm as [tx Hm]).
-(simpl in Hm).
-(eapply match_ty__ge_w).
-eassumption.
-Unset Silent.
-(repeat constructor).
-Set Silent.
--
-Unset Silent.
-Show.
-(apply match_ty_exist__inv in Hm).
-(destruct Hm as [tx Hm]).
