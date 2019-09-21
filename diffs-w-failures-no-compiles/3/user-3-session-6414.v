09d2ec130bb5b246a58b105ed0466574d71a478d
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:04:40 -0700

    user 3, session 6414, part 41

--- diffs-w-failures-no-compiles/3/user-3-session-6414.v
+++ diffs-w-failures-no-compiles/3/user-3-session-6414.v
@@ -226,15 +226,18 @@ Timeout 1 Check @block0.
 Unset Silent.
 Set Diffs "off".
 Set Printing Width 78.
+Unset Silent.
+Set Diffs "off".
+Set Printing Width 78.
 Definition log_abstraction (d : disk) (log : list block) : Prop :=
   (exists b, diskGet d 0 =?= b /\ block_to_addr b = length log) /\
   (forall a, a < length log -> diskGet d (log_addr a) =?= nth a log block0).
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqTrBoPZ"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqDHJ38c"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqpZ3RmU"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqEMvFKg"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
