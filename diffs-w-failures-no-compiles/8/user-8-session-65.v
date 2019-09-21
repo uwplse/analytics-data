8621cb326b16ba98f10fd1252cf1dc9fa96b649b
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:28:21 -0700

    user 8, session 65, part 252

--- diffs-w-failures-no-compiles/8/user-8-session-65.v
+++ diffs-w-failures-no-compiles/8/user-8-session-65.v
@@ -1460,4 +1460,11 @@ Unset Silent.
 Show.
 Set Printing Width 85.
 Show.
+Unset Silent.
+Show.
+Set Printing Width 85.
+Show.
 (intros \207\129 TB).
+(repeat simpl_rewrite inSeq_correct; try apply inSeq_WT; try apply unitary_at1_WT;
+  try apply source_symmetric_WT; trivial).
+(unfold compose_super).
