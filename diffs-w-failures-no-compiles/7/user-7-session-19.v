9a31d16d4238f6fdced88401bdd2a392e752d117
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:06 -0700

    user 7, session 19, part 39

--- diffs-w-failures-no-compiles/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles/7/user-7-session-19.v
@@ -79,4 +79,4 @@ Unset Silent.
          apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto
    end).
 (apply match_ty_i_cname__inv in Hm; subst).
-reflexivity.
+(destruct k'; reflexivity).
