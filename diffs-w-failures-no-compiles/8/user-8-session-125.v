9cfc089a2a6219054e5e4f28e8e5a79f8417d287
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:28:54 -0700

    user 8, session 125, part 164

--- diffs-w-failures-no-compiles/8/user-8-session-125.v
+++ diffs-w-failures-no-compiles/8/user-8-session-125.v
@@ -131,4 +131,8 @@ Unset Silent.
 }
 replace (size_wtype W1 + size_wtype W2)%nat with \226\159\166 W \226\159\167 by (subst; easy).
 (unfold apply_U, apply_unitary, super).
+Unset Silent.
+Show.
+Set Printing Width 85.
+Show.
 (destruct W; try (solve [ inversion HeqW ])).
