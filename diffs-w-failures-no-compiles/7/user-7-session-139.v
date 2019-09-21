3d92cf2ca093f63dafd2adcf2e07dd75c951f470
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:10 -0700

    user 7, session 139, part 1

--- diffs-w-failures-no-compiles/7/user-7-session-139.v
+++ diffs-w-failures-no-compiles/7/user-7-session-139.v
@@ -26,5 +26,54 @@ Proof.
 eassumption.
 (repeat constructor).
 -
-Unset Silent.
+Set Printing Width 148.
+(apply match_ty_exist__inv in Hm).
+Show.
+(destruct Hm as [tx Hm]).
+Show.
+(simpl in Hm).
+Show.
 (destruct (fresh_in_ty_pair__inv X t1 t2 Hfresh) as [Hfresh1 Hfresh2]).
+Show.
+Check subs_fresh_in_ty.
+(rewrite (subs_fresh_in_ty _ _ Hfresh1) in Hm).
+Show.
+(rewrite (subs_fresh_in_ty _ _ Hfresh2) in Hm).
+Set Silent.
+(eapply match_ty__ge_w).
+Unset Silent.
+eassumption.
+(repeat constructor).
+-
+(apply match_ty_exist__inv in Hm).
+Set Silent.
+(destruct Hm as [tx Hm]).
+Unset Silent.
+(simpl in Hm).
+(destruct (fresh_in_ty_pair__inv X t1 t2 Hfresh) as [Hfresh1 Hfresh2]).
+Set Silent.
+(rewrite (subs_fresh_in_ty _ _ Hfresh1) in Hm).
+Unset Silent.
+(rewrite (subs_fresh_in_ty _ _ Hfresh2) in Hm).
+Set Silent.
+(eapply match_ty__ge_w).
+eassumption.
+Unset Silent.
+(repeat constructor).
+-
+Set Silent.
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hm]).
+Unset Silent.
+(simpl in Hm).
+(apply fresh_in_ty_ref__inv in Hfresh).
+(rewrite (subs_fresh_in_ty _ _ Hfresh) in Hm).
+(eapply match_ty__ge_w).
+eassumption.
+(repeat constructor).
+-
+Set Silent.
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hm]).
+Unset Silent.
+(simpl in Hm).
