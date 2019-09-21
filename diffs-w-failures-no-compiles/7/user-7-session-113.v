79ee8b059e64b1c8f2490c16e1048fa42aa793f3
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:54 -0700

    user 7, session 113, part 4

--- diffs-w-failures-no-compiles/7/user-7-session-113.v
+++ diffs-w-failures-no-compiles/7/user-7-session-113.v
@@ -24,5 +24,5 @@ Set Printing Width 148.
 Lemma subs_neq__permute : forall X Y : id, X <> Y -> forall t s1 s2 : ty, [X := s1] ([Y := s2] t) = [Y := s2] ([X := s1] t).
 Proof.
 (intros X Y Hneq t).
-(induction t; intros s1 s2).
--
+Set Printing Width 148.
+(induction t; intros s1 s2; try (solve [ simpl; reflexivity ])).
