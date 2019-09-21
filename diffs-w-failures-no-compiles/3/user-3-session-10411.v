d1b35dd4fe523060d26402fff8dac20a3fb218af
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:19:46 -0700

    user 3, session 10411, part 23

--- diffs-w-failures-no-compiles/3/user-3-session-10411.v
+++ diffs-w-failures-no-compiles/3/user-3-session-10411.v
@@ -360,4 +360,10 @@ Set Diffs "off".
 Timeout 1 Check @Ascii.nat_ascii_embedding.
 Set Printing Width 78.
 Show.
-(unfold proc_spec in *; intuition; subst; repeat deex).
+Unset Silent.
+Set Diffs "off".
+Timeout 1 Check @sig.
+Timeout 1 Check @Ascii.nat_ascii_embedding.
+Set Printing Width 78.
+Show.
+(unfold proc_spec in *; intuition; simpl in *; intuition).
