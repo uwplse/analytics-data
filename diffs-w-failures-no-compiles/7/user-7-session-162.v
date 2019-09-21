47f75db69a4cc1883eae7740cd577e6a7153b3cd
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:27 -0700

    user 7, session 162, part 3

--- diffs-w-failures-no-compiles/7/user-7-session-162.v
+++ diffs-w-failures-no-compiles/7/user-7-session-162.v
@@ -49,4 +49,5 @@ Definition fresh_in_ty (X : id) (t : ty) := fresh X (FV t).
 Definition free_in_ty (X : id) (t : ty) := IdSet.In X (FV t).
 Hint Unfold fresh fresh_in_ty free_in_ty: DBBetaJulia.
 Set Printing Width 148.
-Variable (gen_fresh : (fvs : id_set) -> id).
+Variable (gen_fresh : id_set -> id).
+Variable (gen_fresh : id_set -> id).
