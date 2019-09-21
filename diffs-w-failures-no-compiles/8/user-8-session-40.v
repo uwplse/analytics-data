947e2f3cb130d04a4ab3935bfa7d16b6358d3e58
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:27:55 -0700

    user 8, session 40, part 16

--- diffs-w-failures-no-compiles/8/user-8-session-40.v
+++ diffs-w-failures-no-compiles/8/user-8-session-40.v
@@ -70,18 +70,6 @@ Show.
 Qed.
 Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqcAMjSv"
 Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Lemma X_spec :
-  forall b safe : bool,
-  denote_box safe (boxed_gate _X) (bool_to_matrix b) == bool_to_matrix (\194\172 b).
-Proof.
 Unset Silent.
-Show.
 Set Printing Width 85.
-Show.
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-vector_denote.
-(destruct b; unfold bool_to_ket; simpl; Msimpl).
+Hint Rewrite kron_1_r'.
