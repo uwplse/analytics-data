20da70d971061a1315dd4ba389665b4b5a19c3d1
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:07 -0700

    user 7, session 19, part 63

--- diffs-w-failures-no-compiles/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles/7/user-7-session-19.v
@@ -112,17 +112,26 @@ split.
 *
 Abort.
 Set Printing Width 148.
+Set Printing Width 148.
 Set Silent.
 Lemma match_ty_i__inv_depth_stable :
-  forall (t : ty) (k k' : nat),
+  forall (k k' : nat) (t : ty),
   inv_depth t <= k -> inv_depth t <= k' -> forall v : ty, inv_depth v <= k -> inv_depth v <= k' -> |-[ k] v <$ t <-> |-[ k'] v <$ t.
 Proof.
+(induction k; induction k').
+-
+tauto.
+-
+admit.
+-
+admit.
 Unset Silent.
-(induction t; intros k k' Htk Htk' v Hvk Hvk').
 -
+(induction t).
 admit.
--
 admit.
--
 admit.
--
++
+clear IHk'.
+(intros Htk Htk' v Hvk Hvk').
+(simpl in Htk).
