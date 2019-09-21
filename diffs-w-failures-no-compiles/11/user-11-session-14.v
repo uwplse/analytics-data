700a89121423da77e816c55c6eaa9d6c7373ce04
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:29:29 -0700

    user 11, session 14, part 13

--- diffs-w-failures-no-compiles/11/user-11-session-14.v
+++ diffs-w-failures-no-compiles/11/user-11-session-14.v
@@ -5,49 +5,4 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 115.
-Set Silent.
-From Coq Require Import ExtrOcamlIntConv.
-From ExtLib Require Import Applicative StateMonad Monad.
-From ITree Require Import Exception Nondeterminism ITree.
-From SimpleIO Require Import IO_Random SimpleIO.
-From DeepWeb Require Import CryptoLib KvsLib.
-Unset Silent.
-Set Printing Width 115.
-Unset Silent.
-Redirect "/tmp/coq16819_Ty" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Set Printing Width 115.
-Unset Silent.
-Redirect "/tmp/coq16819xdB" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Set Printing Width 115.
-Unset Silent.
-Redirect "/tmp/coq16819-nH" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Set Printing Width 115.
-Unset Silent.
-Redirect "/tmp/coq16819LyN" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Set Printing Width 115.
-Unset Silent.
-Redirect "/tmp/coq16819Y8T" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Set Printing Width 115.
-Set Silent.
-Import ApplicativeNotation FunctorNotation ListNotations MonadNotation SumNotations.
-Unset Silent.
-Set Printing Width 115.
-Unset Silent.
-Set Printing Width 115.
-Set Silent.
-Open Scope sum_scope.
-Unset Silent.
-Set Printing Width 115.
-Set Silent.
-Open Scope monad_scope.
 Unset Silent.
