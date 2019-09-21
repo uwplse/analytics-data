ff29f0f6058689d8bace10f394616f505315b726
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:29:18 -0700

    user 10, session 13, part 1

--- diffs-w-failures-no-compiles/10/user-10-session-13.v
+++ diffs-w-failures-no-compiles/10/user-10-session-13.v
@@ -6,7 +6,6 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 114.
-From ReductionEffect Require Import PrintingEffect.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqrYfIcK" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
+Unset Silent.
+Set Printing Width 114.
+From Coq Require Import Stream.
