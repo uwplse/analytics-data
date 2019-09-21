1441038f1a1ce9b515348e31b3dc3aa08536d288
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:55 -0700

    user 7, session 113, part 30

--- diffs-w-failures-no-compiles/7/user-7-session-113.v
+++ diffs-w-failures-no-compiles/7/user-7-session-113.v
@@ -3,6 +3,7 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
+Set Printing Width 148.
 Set Silent.
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
@@ -11,19 +12,17 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-Set Printing Width 148.
 Open Scope btjt.
-Set Silent.
 Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
 Proof.
 (intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
 Qed.
+Lemma not_fresh_in_union__inv : forall (X : id) (fvs1 fvs2 : id_set), not_fresh X (IdSet.union fvs1 fvs2) -> not_fresh X fvs1 /\ not_fresh X fvs2.
 Unset Silent.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Set Printing Width 148.
-Lemma not_fresh_in_union__inv : forall (X : id) (fvs1 fvs2 : id_set), ~ fresh X (IdSet.union fvs1 fvs2) -> ~ fresh X fvs1 /\ ~ fresh X fvs2.
 Proof.
+Set Silent.
 (intros X fvs1 fvs2 H).
-(unfold fresh in *).
+Unset Silent.
+(unfold not_fresh in *).
+Search -IdSet.union.
+(simpl in H).
