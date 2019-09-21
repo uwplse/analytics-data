5f2b77b137e94ae2c15152262cb8b75337d226c9
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:29:10 -0700

    user 10, session 5, part 1

--- diffs-w-failures-no-compiles/10/user-10-session-5.v
+++ diffs-w-failures-no-compiles/10/user-10-session-5.v
@@ -14,7 +14,9 @@ From Coq Require Import Basics ExtrOcamlIntConv List NArith String.
 Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqRp5hKL" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
-Require Import Analytical.Analytics.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqE0q7Fg" Print Ltac Signatures.
+Unset Silent.
+Set Printing Width 114.
+Import Analytics.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coq8OecSp" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
