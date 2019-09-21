c40a7a36cf1ff60df9356bf5c16fbb3440ad7556
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:28:04 -0700

    user 8, session 54, part 10

--- diffs-w-failures-no-compiles/8/user-8-session-54.v
+++ diffs-w-failures-no-compiles/8/user-8-session-54.v
@@ -6,44 +6,4 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 85.
-Set Silent.
-Require Import HOASLib.
-Require Import Denotation.
-Require Import TypeChecking.
-Open Scope matrix_scope.
 Unset Silent.
-Lemma id_circ_spec : forall W \207\129 safe, denote_box safe (@id_circ W) \207\129 == \207\129.
-Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqVoxw9J"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Set Silent.
-Proof.
-(intros W \207\129 safe H).
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-Set Silent.
-(intros W \207\129 safe).
-(simpl).
-(unfold denote_box).
-(simpl).
-autorewrite with proof_db.
-(rewrite add_fresh_split).
-(simpl).
-(unfold pad).
-(simpl).
-(rewrite Nat.sub_diag).
-(rewrite kron_1_r').
-(rewrite subst_pat_fresh_empty).
-(rewrite denote_pat_fresh_id).
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
-Unset Silent.
-Show.
-Timeout 1 About super_I.
-Timeout 1 Print super_I.
-Timeout 1 Print Ltac super_I.
