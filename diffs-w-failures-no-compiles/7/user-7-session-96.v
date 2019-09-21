93237d71c10a330817d0e1111528aa180612875e
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:25 -0700

    user 7, session 96, part 1

--- diffs-w-failures-no-compiles/7/user-7-session-96.v
+++ diffs-w-failures-no-compiles/7/user-7-session-96.v
@@ -54,22 +54,10 @@ clear IHv.
 exists v.
 auto.
 Qed.
-Lemma match_ty_exist__inv : forall (v : ty) (X : id) (t : ty) (k : nat), |-[ S k] v <$ TExist X t -> exists tx : ty, |-[ k] v <$ [X := tx] t.
+Set Printing Width 148.
+Set Silent.
+Lemma match_ty_exist__0_inv : forall (v : ty) (X : id) (t : ty), |-[ 0] v <$ TExist X t -> |-[ 0] v <$ t.
+Unset Silent.
 Proof.
+Set Silent.
 (intros v; induction v; intros X t k Hm; assumption).
-Unset Silent.
-Qed.
-Set Silent.
-Theorem match_ty__value_type_l : forall (k : nat) (v t : ty), |-[ k] v <$ t -> value_type v.
-Proof.
-Unset Silent.
-(induction k; intros v t; generalize dependent v; induction t; intros v Hm;
-  try (solve
-   [ apply match_ty_cname__inv in Hm; subst; constructor
-   | apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; constructor; [ eapply IHt1 | eapply IHt2 ]; eauto
-   | apply match_ty_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto
-   | apply match_ty_ref__weak_inv in Hm; destruct Hm as [t' Heq]; subst; constructor
-   | destruct v; contradiction ])).
-Set Silent.
--
-Unset Silent.
