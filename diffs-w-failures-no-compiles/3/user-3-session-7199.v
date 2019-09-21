585053301d488c0be12633ba80a2b7c2e7019ec7
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:07:50 -0700

    user 3, session 7199, part 419

--- diffs-w-failures-no-compiles/3/user-3-session-7199.v
+++ diffs-w-failures-no-compiles/3/user-3-session-7199.v
@@ -507,4 +507,65 @@ reflexivity.
 {
 (rewrite app_length in *; simpl in *; lia).
 }
+Unset Silent.
+Set Diffs "off".
+Timeout 1 Check @rec_wipe_compose.
+Timeout 1 Check @app.
+Timeout 1 Check @app.
+Timeout 1 Check @app_nth1.
+Timeout 1 Check @app_nth1.
+Timeout 1 Check @app_nth1.
+Timeout 1 Check @app_nth1.
+Timeout 1 Check @bytes.
+Timeout 1 Check @split.
+Set Printing Width 78.
+Show.
+(rewrite app_nth1 by lia).
+Timeout 1 Check @Tauto.A.
 auto.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqOPXDAN"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
+Timeout 1 Check @Ret.
+Timeout 1 Check @log_contents_ok.
+Timeout 1 Check @log_contents_ok.
+Timeout 1 Check @log_contents_ok.
+Timeout 1 Check @log_contents_ok.
+Timeout 1 Check @log_contents_ok.
+Timeout 1 Check @log_contents_ok.
+Timeout 1 Check @log_contents_ok_prefix.
+Timeout 1 Check @spec_abstraction_compose.
+Hint Resolve log_contents_ok_append: core.
+Set Silent.
+Theorem append_at_ok a bs' :
+  proc_spec
+    (fun (bs : list block) state =>
+     {|
+     pre := a = length bs /\
+            log_size_ok state (bs ++ bs') /\ log_contents_ok state bs;
+     post := fun r state' =>
+             diskGet state' len_addr = diskGet state len_addr /\
+             diskSize state' = diskSize state /\
+             log_size_ok state' (bs ++ bs') /\
+             log_contents_ok state' (bs ++ bs');
+     recovered := fun _ state' =>
+                  diskGet state' len_addr = diskGet state len_addr /\
+                  diskSize state' = diskSize state /\
+                  log_contents_ok state' bs |}) (append_at a bs') recover
+    d.abstr.
+Proof.
+generalize dependent a.
+(induction bs'; simpl; intros).
+-
+step_proc.
+intuition eauto.
+(rewrite app_nil_r; auto).
+-
+step_proc.
+(intuition eauto; autorewrite with upd; auto).
+{
+(apply log_contents_ok_unchanged; eauto).
