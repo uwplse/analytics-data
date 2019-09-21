6f23a066242bca126ff6eb0a586798c594c55932
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:51 -0700

    user 7, session 42, part 0

---
+++ diffs-w-failures-no-compiles/7/user-7-session-42.v
@@ -0,0 +1,26 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Width 148.
+Set Silent.
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0250a.BaseDefs.
+Require Import BetaJulia.Sub0250a.BaseProps.
+Unset Silent.
+Require Import BetaJulia.Sub0250a.MatchProps.
+Set Silent.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Open Scope btjt_scope.
+Open Scope btjm_scope.
+Open Scope btjnf_scope.
+Open Scope btjd_scope.
+Lemma sub_d__inv_depth_le : forall t t' : ty, |- t << t' -> | t | <= | t' |.
+Proof.
+(intros t t' Hsub).
+Unset Silent.
+(induction Hsub).
