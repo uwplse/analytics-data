f872b5e78762722f42c8c51610e4001bd43fb5a9
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:29:21 -0700

    user 10, session 15, part 29

--- diffs-w-failures-no-compiles/10/user-10-session-15.v
+++ diffs-w-failures-no-compiles/10/user-10-session-15.v
@@ -65,4 +65,7 @@ Unset Silent.
 Set Printing Width 114.
 Unset Silent.
 Set Printing Width 114.
-Eval compute in nth 5 (fib 9 0 1) 14.
+Unset Silent.
+Set Printing Width 114.
+Eval compute in nth 5 (fib 9 0 1) 42.
+Eval compute in Str_nth 5 (Fib 0 1).
