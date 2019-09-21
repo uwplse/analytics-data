f0ebb9311b19c69771d770182c0174d4ba852f0e
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:22 -0700

    user 7, session 92, part 4

--- diffs-w-failures-no-compiles/7/user-7-session-92.v
+++ diffs-w-failures-no-compiles/7/user-7-session-92.v
@@ -15,12 +15,8 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-Close Scope btjd_scope.
-Unset Silent.
-Open Scope btjr_scope.
-Set Silent.
-Unset Silent.
-Theorem sub_r__sound : forall t1 t2 : ty, |- t1 << t2 -> (|- t1 << t2)%btjd.
+Set Printing Width 148.
+Theorem sub_r__sound : forall t1 t2 : ty, (|- t1 << t2)%btjr -> (|- t1 << t2)%btjd.
 Set Silent.
 Proof.
 (intros t1 t2 Hsub; induction Hsub; try (solve [ constructor; assumption ])).
@@ -29,12 +25,28 @@ Proof.
 -
 (apply union_right_2; assumption).
 -
-Unset Silent.
 (apply SD_Trans with (MkNF( t))).
 (apply mk_nf__sub_d_r).
-Set Silent.
 assumption.
+Qed.
+Unset Silent.
+Theorem sub_r__complete : forall t1 t2 : ty, (|- t1 << t2)%btjd -> (|- t1 << t2)%btjr.
+Set Silent.
+Proof.
+(intros t1 t2 Hsub; induction Hsub; try (solve [ constructor; assumption ])).
+-
+(apply sub_r__reflexive).
+-
+(apply sub_r__transitive with t2; assumption).
+-
+(apply SR_UnionR1; apply sub_r__reflexive).
+-
+(apply SR_UnionR2; apply sub_r__reflexive).
+-
+(apply mk_nf_sub_r__sub_r).
+(apply mk_nf__distr11).
+-
+(apply mk_nf_sub_r__sub_r).
+(apply mk_nf__distr21).
 Unset Silent.
 Qed.
-Set Silent.
-Unset Silent.
