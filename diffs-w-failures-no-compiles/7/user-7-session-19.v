a1744512e189ff326e959a1ba51b966b77dcf5b5
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:09 -0700

    user 7, session 19, part 133

--- diffs-w-failures-no-compiles/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles/7/user-7-session-19.v
@@ -129,6 +129,9 @@ Unset Silent.
 (induction t; induction t'; intros H; try reflexivity).
 Show.
 Set Printing Width 148.
+Set Printing Width 148.
 (match goal with
- | |- | ?t1 | = | ?t2 | => assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption)
+ | |- | ?t1 | = | ?t2 | =>
+       assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); destruct H as [H _];
+        specialize (H _ _ Hv Hm)
  end).
