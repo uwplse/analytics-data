e1f068f7923965610df614a0c08e64f7ae8953a9
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:17:54 -0700

    user 3, session 9823, part 7

--- diffs-w-failures-no-compiles/3/user-3-session-9823.v
+++ diffs-w-failures-no-compiles/3/user-3-session-9823.v
@@ -149,8 +149,45 @@ step_proc.
 step_proc.
 (case_eq (diskGet (stateDisk state) (diskSize (stateDisk state) - 1)); intros).
 {
-exists (diskUpd (diskShrink (stateDisk state)) (stateBadBlock state) b).
-(unfold inited_any; split; auto).
-Unset Silent.
 Unset Silent.
 Set Diffs "off".
+Set Printing Width 78.
+Show.
+(constructor; intuition; autorewrite with upd in *; intuition).
+-
+(rewrite diskShrink_preserves; auto).
+(rewrite diskShrink_size; lia).
+-
+(rewrite diskUpd_eq; auto).
+(rewrite diskShrink_size; lia).
+-
+lia.
+}
+Set Silent.
+{
+(exfalso; eapply disk_inbounds_not_none; [  | eauto ]; lia).
+}
++
+step_proc.
+Qed.
+Theorem read_ok :
+  forall a, proc_spec (OneDiskAPI.read_spec a) (read a) recover abstr.
+Proof.
+(unfold read).
+(intros).
+(apply spec_abstraction_compose; simpl).
+Unset Silent.
+(step_proc; intros).
+(destruct a'; simpl in *; intuition).
+Timeout 1 Check @repeat_length.
+Set Silent.
+{
+Unset Silent.
+eauto.
+}
+(destruct (a == r)).
+-
+invert_abstraction.
+(step_proc; intuition).
+{
+eauto.
