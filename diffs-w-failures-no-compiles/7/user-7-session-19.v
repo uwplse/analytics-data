99d24c1cc6b15f4e0ca51ae86370e68e0a2378cb
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:09 -0700

    user 7, session 19, part 128

--- diffs-w-failures-no-compiles/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles/7/user-7-session-19.v
@@ -123,7 +123,6 @@ Qed.
 Set Silent.
 Lemma match_ty_i_eq__inv_depth_eq :
   forall t t' : ty, (forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
-Proof.
-Unset Silent.
+Set Printing Width 148.
 (induction t; induction t'; intros H).
-reflexivity.
+Show.
