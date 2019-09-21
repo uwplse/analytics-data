6290d956c876ee3e2cdeb53ebf2f5af7382d470e
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:25:11 -0700

    user 7, session 81, part 9

--- diffs-w-failures-no-compiles/7/user-7-session-81.v
+++ diffs-w-failures-no-compiles/7/user-7-session-81.v
@@ -110,4 +110,7 @@ Set Printing Width 148.
 Set Silent.
 -
 Unset Silent.
-(destruct (value_type_matching_ty__exists k t2) as [pv2 Hpv2]).
+Check value_type_matching_ty__exists.
+(destruct (value_type_matching_ty__exists t2 k) as [pv2 Hpv2]).
+(remember (Nat.max k (| t2 |)) as kmax).
+(assert (Hmp : |-[ kmax] TPair v pv2 <$ TPair t1 t2)).
