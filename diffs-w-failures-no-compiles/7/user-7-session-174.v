51111ed78083bc32d26812bff4370bdc1a1605db
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:37 -0700

    user 7, session 174, part 20

--- diffs-w-failures-no-compiles/7/user-7-session-174.v
+++ diffs-w-failures-no-compiles/7/user-7-session-174.v
@@ -152,4 +152,8 @@ Set Printing Width 148.
 (unfold well_founded).
 Set Printing Width 148.
 Set Printing Width 148.
-(intros t; induction t; constructor; intros t' intros H).
+(intros t; induction t; constructor; intros t' H).
++
+constructor.
+(intros y Hy).
+constructor.
