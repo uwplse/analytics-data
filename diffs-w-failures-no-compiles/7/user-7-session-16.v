283ac37faecf2611789010ce46d0443b81309c03
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:23:56 -0700

    user 7, session 16, part 41

--- diffs-w-failures-no-compiles/7/user-7-session-16.v
+++ diffs-w-failures-no-compiles/7/user-7-session-16.v
@@ -403,14 +403,11 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
-(split; intros t'; induction t';
+Set Printing Width 148.
+(split; intros t'; induction t'; try (solve [ right; solve_not_x_sub_r_y_full ]);
   try
    match goal with
    | |- Decidable.decidable (|- TCName ?c1 << TCName ?c2) =>
          destruct (cname_eq__decidable c1 c2);
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
-Set Silent.
-+
-Unset Silent.
-(right; solve_not_x_sub_r_y_full).
