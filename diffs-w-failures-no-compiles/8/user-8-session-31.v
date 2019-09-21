1f1e83261113aea455463c74a77a25679621e7ac
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:27:44 -0700

    user 8, session 31, part 144

--- diffs-w-failures-no-compiles/8/user-8-session-31.v
+++ diffs-w-failures-no-compiles/8/user-8-session-31.v
@@ -3369,15 +3369,47 @@ Timeout 1 Print Grammar tactic.
 Lemma HOAS_Equiv_trans :
   forall w1 w2 (c1 c2 c3 : Box w1 w2), c1 \226\137\161 c2 -> c2 \226\137\161 c3 -> c1 \226\137\161 c3.
 Proof.
+Unset Silent.
+Show.
+Set Printing Width 85.
+Show.
+Set Silent.
 (intros).
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
 (intros \207\129 b).
-Unset Silent.
-Show.
-Set Printing Width 85.
-Show.
 (unfold HOAS_Equiv in H).
 (rewrite H; auto).
+Unset Silent.
+Qed.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqR1bMEs"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Lemma inSeq_assoc :
+  forall {w1} {w2} {w3} {w4} (c1 : Box w1 w2) (c2 : Box w2 w3) (c3 : Box w3 w4),
+  c3 \194\183 c2 \194\183 c1 = (c3 \194\183 c2) \194\183 c1.
+Set Silent.
+Proof.
+(intros w1 w2 w3 w4 [c1] [c2] [c3]).
+(unfold inSeq).
+(simpl).
+(apply f_equal; apply functional_extensionality; intros p1).
+(simpl).
+(remember (c1 p1) as c).
+clear c1 p1 Heqc.
+dependent induction c.
+-
+reflexivity.
+-
+(simpl).
+(apply f_equal; apply functional_extensionality; intros p2).
+(rewrite H).
+reflexivity.
+-
+(simpl).
+(apply f_equal; apply functional_extensionality; intros p2).
+(rewrite H).
+reflexivity.
+Unset Silent.
+Qed.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqFqTC9s"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
