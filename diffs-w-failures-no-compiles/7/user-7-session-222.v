d1d503fbd7e4c7d0077eae7a574b157874e9fdfd
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:27:16 -0700

    user 7, session 222, part 13

--- diffs-w-failures-no-compiles/7/user-7-session-222.v
+++ diffs-w-failures-no-compiles/7/user-7-session-222.v
@@ -380,4 +380,10 @@ reflexivity.
 *
 Unset Silent.
 (apply b_free_in_ty_exist_neq__inv in HX; try assumption).
-(apply b_free_in_ty_exist_neq).
+Set Printing Width 148.
+(apply b_free_in_ty_exist_neq; try assumption).
+tauto.
+-
+(destruct (beq_idP Y i)).
++
+subst.
