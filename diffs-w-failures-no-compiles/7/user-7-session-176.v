d1f1ee7fde7a54b3aa4c8b98041c743ff17b890d
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:42 -0700

    user 7, session 176, part 55

--- diffs-w-failures-no-compiles/7/user-7-session-176.v
+++ diffs-w-failures-no-compiles/7/user-7-session-176.v
@@ -173,5 +173,4 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 (split; repeat rewrite subst_union; intros Hm; apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm];
-  try (destruct (IHt1 v) as [Hm1 Hm2]; specialize (Hm1 Hm)); destruct (IHt2 v) as [Hm1 Hm2];
-  try (solve [ apply match_ty_union_1; auto | apply match_ty_union_2; auto ])).
+  try (destruct (IHt1 v) as [Hm1 Hm2]; specialize (Hm1 Hm)); destruct (IHt2 v) as [Hm1 Hm2]).
