1e658443c5541d8fc7d1f18d5d68ed2862c95aa1
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:48 -0700

    user 7, session 35, part 2

--- diffs-w-failures-no-compiles/7/user-7-session-35.v
+++ diffs-w-failures-no-compiles/7/user-7-session-35.v
@@ -3,9 +3,3 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Width 148.
-Set Silent.
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0250a.BaseDefs.
-Set Printing Width 148.
-Require Import BetaJulia.Sub0250a.AltMatchDefs.
