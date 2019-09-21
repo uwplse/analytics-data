796c498bc1a9f13ff259138243bd1eb5f9c4dc93
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:23:42 -0700

    user 5, session 36, part 49

--- diffs-w-failures-no-compiles/5/user-5-session-36.v
+++ diffs-w-failures-no-compiles/5/user-5-session-36.v
@@ -268,8 +268,5 @@ clear H.
 (rewrite <- evalEqTrue in H0).
 (rewrite evalVar in H).
 (rewrite extendEnv_eq in H).
-(assert (Z : forall z, 6 = z * 2 -> z = 3) by admit).
-(assert (x0 = 3%Z)).
-{
-(apply Z).
+(assert (Z : forall z : Z, 6 = z * 2 -> z = 3) by admit).
 Unset Silent.
