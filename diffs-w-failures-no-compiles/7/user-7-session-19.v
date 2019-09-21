d434700184fb447bbb200f33f8ca9a0f41b53a5b
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:24:13 -0700

    user 7, session 19, part 238

--- diffs-w-failures-no-compiles/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles/7/user-7-session-19.v
@@ -123,9 +123,10 @@ Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
 Set Printing Width 148.
+Set Printing Width 148.
 Set Silent.
 Lemma value_sem_sub_k_union__value_sem_sub_k_component :
   forall k : nat, forall v : ty, value_type v -> forall ta tb : ty, ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
-Unset Silent.
 Proof.
-Show.
+Unset Silent.
+(induction k; intros v Hv; induction Hv; intros Hsem).
