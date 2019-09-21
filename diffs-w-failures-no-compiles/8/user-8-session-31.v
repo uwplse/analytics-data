6d76d8032df338f509ef93dc26ffdea8b277dde2
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:27:39 -0700

    user 8, session 31, part 30

--- diffs-w-failures-no-compiles/8/user-8-session-31.v
+++ diffs-w-failures-no-compiles/8/user-8-session-31.v
@@ -2828,6 +2828,5 @@ replace (size_ctx \206\147) with size_octx \206\147 by easy.
 dependent destruction p1.
 dependent destruction t.
 (apply singleton_equiv in s; subst).
-(simpl).
 Unset Silent.
 Show.
