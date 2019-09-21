588058b3a5962384495be211a101a303aeb6cc92
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:26:10 -0700

    user 7, session 139, part 7

--- diffs-w-failures-no-compiles/7/user-7-session-139.v
+++ diffs-w-failures-no-compiles/7/user-7-session-139.v
@@ -14,23 +14,18 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
+Set Printing Width 148.
+Set Silent.
 Lemma sem_sub_k_exist_fresh_l : forall (k : nat) (X : id) (t : ty), fresh_in_ty X t -> ||-[ k][TExist X t]<= [t].
 Proof.
-Set Printing Width 148.
 (intros k X t Hfresh).
-Show.
 (intros w1).
-Set Printing Width 148.
-Set Silent.
+exists w1.
 (intros v Hm).
-Unset Silent.
 (destruct w1).
-Set Silent.
 -
-Unset Silent.
 (apply match_ty_exist__0_inv in Hm; contradiction).
 -
-Set Silent.
 (apply match_ty_exist__inv in Hm).
 (destruct Hm as [tx Hm]).
 (simpl in Hm).
@@ -38,5 +33,15 @@ Set Silent.
 (eapply match_ty__ge_w).
 eassumption.
 (repeat constructor).
-Unset Silent.
 Qed.
+Lemma sem_sub_k_exist_fresh_r : forall (k : nat) (X : id) (t : ty), fresh_in_ty X t -> ||-[ k][t]<= [TExist X t].
+Proof.
+(intros k X t Hfresh).
+(intros w1).
+exists w1.
+(intros v Hm).
+Unset Silent.
+(destruct w1).
+Set Silent.
+-
+Unset Silent.
