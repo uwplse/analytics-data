8266d85d3dc454ef2d5cb16cde417088520e3b93
Talia Ringer <tringer@cs.washington.edu>
2019-09-20 17:23:57 -0700

    user 7, session 16, part 63

--- diffs-w-failures-no-compiles/7/user-7-session-16.v
+++ diffs-w-failures-no-compiles/7/user-7-session-16.v
@@ -382,35 +382,27 @@ Ltac
   end; match goal with
        | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto
        end.
-Ltac
- solve_atom_sub_r_union__decidable IHt2_1 IHt2_2 :=
-  destruct IHt2_1 as [IH1| IH1]; destruct IHt2_2 as [IH2| IH2];
-   try (solve [ left; apply SR_UnionR1; assumption | left; apply SR_UnionR2; assumption ]); right; intros Hcontra;
-   apply atom_sub_r_union__inv in Hcontra; tauto || constructor.
+Set Printing Width 148.
+Set Silent.
 Lemma nf_sub_r__decidable2 :
-  forall t : ty, InNF( t) -> (forall t' : ty, Decidable.decidable (|- t << t')) /\ (forall t' : ty, Decidable.decidable (|- t' << t)).
+  forall t : ty,
+  InNF( t) -> (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t)).
 Proof.
+Unset Silent.
 (apply
   (in_nf_mut
-     (fun (t : ty) (Hat : atom_type t) => (forall t' : ty, Decidable.decidable (|- t << t')) /\ (forall t' : ty, Decidable.decidable (|- t' << t)))
-     (fun (t : ty) (Hnf : in_nf t) => (forall t' : ty, Decidable.decidable (|- t << t')) /\ (forall t' : ty, Decidable.decidable (|- t' << t))))).
+     (fun (t : ty) (Hat : atom_type t) =>
+      (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t)))
+     (fun (t : ty) (Hnf : in_nf t) =>
+      (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t))))).
+Set Silent.
 -
 (intros c).
-(split; intros t'; induction t'; try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 ]);
+Unset Silent.
+(split; intros t'; induction t'; intros Hnf'; try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 ]);
   try
    match goal with
    | |- Decidable.decidable (|- TCName ?c1 << TCName ?c2) =>
          destruct (cname_eq__decidable c1 c2);
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
-+
-Show.
-Set Printing Width 148.
-Set Silent.
-(intros Hcontra).
-(apply sub_r__mk_nf_sub_r1 in Hcontra).
-Set Printing Width 148.
-Set Printing Width 148.
-(induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]); subst).
-Show.
-Show 2.
