a80647579f899447703f4f43ccc082a733e4be88
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:38:58 -0700

    user 3, session 7199, part 146

--- diffs-w-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -255,4 +255,5 @@ Theorem get_len_ok :
      recovered := fun _ state' => state' = state |}) get_len recover d.abstr.
 Proof.
 (unfold get_len; intros).
-(apply spec_abstraction_compose).
+step_proc.
+(destruct a' as [_ bs]; simpl in *; intuition eauto).
