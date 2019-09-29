f43672dccd6779c770007bead5e4015dff5323c2
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:38 -0700

    user 7, session 106, part 2

--- diffs-w-failures-no-compiles-clean/7/user-7-session-106.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-106.v
@@ -175,8 +175,8 @@ Abort.
 Lemma match_ty__match_ge_world : forall (t : ty) (w k : nat) (v : ty), |-[ k, w] v <$ t -> forall w' : nat, w <= w' -> |-[ k, w'] v <$ t.
 Proof.
 (induction t; intros w k v Hm w' Hle).
--
 (apply match_ty_cname__inv in Hm).
 subst.
 (apply match_ty_cname).
 -
+(des Abort).
