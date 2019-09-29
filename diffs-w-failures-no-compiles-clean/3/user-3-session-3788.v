bd0cf6b09a52751ffb4d71f392dac33f1a83ad49
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:27:25 -0700

    user 3, session 3788, part 9

--- diffs-w-failures-no-compiles-clean/3/user-3-session-3788.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-3788.v
@@ -42,7 +42,3 @@ Theorem swapXY_ok :
      recovered := fun _ state' => True |}) swapXY vars.recover vars.abstr.
 Proof.
 (unfold swapXY).
-monad_simpl.
-(match goal with
- | |- proc_spec _ ?p _ _ => pose proof (AProc p) as Hbefore
- end).
