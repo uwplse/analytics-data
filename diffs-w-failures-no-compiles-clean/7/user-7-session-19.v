a1d91aa1ea73a572ed894a1bf18f7c1bfd8c8b4f
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:24 -0700

    user 7, session 19, part 650

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -493,4 +493,4 @@ auto using match_ty_i_pair.
 (apply match_ty_i_ref__inv in Hm).
 (destruct Hm as [tx [Heq Href]]; subst).
 (simpl).
-(intros v; split; specialize (Href v); try tauto).
+(intros v; split; intros Hm; specialize (Href v); specialize (IHHsub1 k Hm)).
