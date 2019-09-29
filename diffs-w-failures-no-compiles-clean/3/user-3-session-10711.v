8bad4e00ea732cc67287a9dcea28fdf1190991bd
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:49:36 -0700

    user 3, session 10711, part 3

--- diffs-w-failures-no-compiles-clean/3/user-3-session-10711.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-10711.v
@@ -476,3 +476,35 @@ step.
 intuition.
 {
 (exists (bs ++ v); intuition eauto).
+}
+{
+(exists (bs ++ v); intuition eauto).
+}
+-
+step.
+intuition eauto.
+Qed.
+Theorem reset_ok : proc_spec reset_spec reset recover abstr.
+Proof.
+(unfold reset; intros).
+(apply spec_abstraction_compose).
+step.
+(destruct a' as [[] bs]; simpl in *).
+intuition.
+{
+(exists bs; intuition eauto).
+}
+step.
+intuition eauto.
+{
+(exists []; intuition eauto).
+(apply log_abstraction_nil with (b := r); auto).
+(rewrite diskUpd_eq; eauto).
+}
+{
+(exists []; intuition eauto).
+(apply log_abstraction_nil with (b := r); auto).
+(rewrite diskUpd_eq; eauto).
+}
+Qed.
+End Log.
