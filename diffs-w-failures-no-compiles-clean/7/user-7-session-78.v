f8ebe93fcc3f71124a27f8d4e3e3237f29f183e3
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:56 -0700

    user 7, session 78, part 1

--- diffs-w-failures-no-compiles-clean/7/user-7-session-78.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-78.v
@@ -70,6 +70,6 @@ Proof.
 (apply mk_nf__sub_d_r; assumption).
 (apply nf_sem_sub__sub_d).
 (apply mk_nf__in_nf).
-(eapply sem_sub__trans; try eassumption).
+(apply sem_sub__trans with t1; try assumption).
 (apply mk_nf__sem_sub_l).
 Qed.
