d7cdb52caba13f648cb0f1940e493eae3b29a04e
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:57:39 -0700

    user 8, session 65, part 59

--- diffs-w-failures-no-compiles-clean/8/user-8-session-65.v
+++ diffs-w-failures-no-compiles-clean/8/user-8-session-65.v
@@ -969,7 +969,4 @@ gen \207\129.
 (simpl).
 (rewrite Nat.mul_1_r).
 (intros \207\129).
-fill_differences.
-Timeout 1 About gridify.
-Timeout 1 Print gridify.
-Timeout 1 Print Ltac gridify.
+remember_differences.
