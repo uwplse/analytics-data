2994485f8bebd96f466b5cd03de80b50a0d7c650
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:57:44 -0700

    user 8, session 65, part 133

--- diffs-w-failures-no-compiles-clean/8/user-8-session-65.v
+++ diffs-w-failures-no-compiles-clean/8/user-8-session-65.v
@@ -980,4 +980,6 @@ Msimpl.
 (destruct b; simpl; Msimpl).
 +
 mat_replace \226\159\1680\226\136\163 \195\151 \226\136\1631\226\159\169 with @Zero 1 1 by lma.
+mat_replace \226\159\1681\226\136\163 \195\151 \226\136\1631\226\159\169 with I 1 by lma.
 Msimpl.
+(rewrite id_kron).
