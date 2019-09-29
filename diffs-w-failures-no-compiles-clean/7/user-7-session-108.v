66b684e9b43cfe3c710e966e8764d82ea736c185
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:45 -0700

    user 7, session 108, part 100

--- diffs-w-failures-no-compiles-clean/7/user-7-session-108.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-108.v
@@ -173,4 +173,7 @@ reflexivity.
 Abort.
 Lemma aaa : forall (k : nat) (t t' : ty), | t | <= k -> ||-[ k][t]= [t'] -> | t | = | t' |.
 Proof.
-(induction k; induction t; induction t'; intros Hdep Hsem).
+(induction k; induction t; induction t'; intros Hdep Hsem; try reflexivity).
+-
+(destruct Hsem as [Hsem _]).
+specialize (Hsem 0).
