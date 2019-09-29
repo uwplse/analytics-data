2e4f2515a219e1b5e539eea4a91b03e579c0b48f
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:44 -0700

    user 7, session 16, part 87

--- diffs-w-failures-no-compiles-clean/7/user-7-session-16.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-16.v
@@ -385,10 +385,12 @@ Proof.
   end).
 -
 (assert (Hnf : InNF( TPair ta1 ta2)) by (do 2 constructor; assumption)).
-(destruct (in_nf_pair__inv _ _ Hnf) as [Hnf1 Hnf2]).
 (split; intros t'; induction t'; intros Hnf';
   try
    match goal with
    | Hnf':InNF( TUnion _ _) |- _ => destruct (in_nf_union__inv _ _ Hnf') as [Hnf'1 Hnf'2]
    | Hnf':InNF( TPair _ _) |- _ => destruct (in_nf_pair__inv _ _ Hnf') as [Hnf'1 Hnf'2]
    end; try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 | solve_union_sub_r__decidable IHt'1 IHt'2 ])).
++
+(destruct IHta1 as [IHta11 IHta12]; destruct IHta2 as [IHta21 IHta22]).
+(destruct (IHta11 t'1 _) as [IH1| IH1]; destruct (IHta2 t'2) as [IH2| IH2]).
