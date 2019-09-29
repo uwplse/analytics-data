a16895d3cd1e36522cfb8edc667a1ac2f0bc4c23
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:11 -0700

    user 7, session 19, part 406

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -201,5 +201,6 @@ Lemma sem_sub_k_i_nf__inv_depth_le : forall (k : nat) (t t' : ty), InNF( t) -> |
      | Hsem:||-[ ?k][TPair ?t1 ?t2]<= [?t']
        |- _ <= | ?t' | =>
            assert (Hvp : value_type (TPair t1 t2)) by (apply in_nf_pair__value_type; assumption);
-            assert (Hmp : |-[ k] TPair t1 t2 <$ TPair t1 t2) by (apply match_ty_i__reflexive; assumption)
+            assert (Hmp : |-[ k] TPair t1 t2 <$ TPair t1 t2) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hmp); contradiction
      end ])).
+Search -in_nf.
