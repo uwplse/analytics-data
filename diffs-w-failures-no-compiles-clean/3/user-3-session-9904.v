ae76ae4514506af3de494834ec6c29e43b4a4208
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:47:04 -0700

    user 3, session 9904, part 2

--- diffs-w-failures-no-compiles-clean/3/user-3-session-9904.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-9904.v
@@ -61,4 +61,13 @@ Proof.
 step_proc.
 step_proc.
 step_proc.
-step_proc.
+exists nil.
+(unfold statdb_abstraction, inited).
+intuition auto.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqig39L2"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
