1297fb78f4f57348875b0723b2f48c94224c7c5d
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:22 -0700

    user 5, session 36, part 12

--- diffs-w-failures-no-compiles-clean/5/user-5-session-36.v
+++ diffs-w-failures-no-compiles-clean/5/user-5-session-36.v
@@ -258,5 +258,7 @@ reflexivity.
 clear H.
 (rewrite evalAnd in H0).
 (match goal with
- | H:eval ?L ?env (If (In ?x ?S) _ _) = _ |- _ => idtac
+ | H:eval ?L ?env (If (In ?x ?S) _ _) = _ |- _ => destruct (evalIn L env x S)
  end).
+-
+(rewrite evalIfTrue in H0).
