8e62650586a64fc9696ace574ebc37993d3e2e4a
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:55:53 -0700

    user 7, session 192, part 72

--- diffs-w-failures-no-compiles-clean/7/user-7-session-192.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-192.v
@@ -83,9 +83,6 @@ admit.
 (apply match_ty_exist__inv in Hm').
 (destruct Hm' as [ti Hm']).
 specialize (IHw' _ Hm').
-(destruct (either_free_or_fresh_in_ty X' ([i := ti] t'))).
-*
-specialize (IHw'b H).
 (split; intros HX').
 {
 (apply match_ty_exist).
@@ -93,19 +90,3 @@ exists ti.
 assumption.
 }
 {
-(destruct (beq_idP X' i)).
-{
-subst.
-(unfold free_in_ty in HX').
-(simpl in HX').
-admit.
-}
-{
-(rewrite subst_equation).
-(assert (Hbeq : beq_id X' i = false) by (apply beq_id_false_iff; assumption)).
-(rewrite Hbeq).
-(destruct (IdSet.mem i (FV tx)) eqn:Hmem).
-{
-(remember (gen_fresh (IdSet.union (FV tx) (IdSet.add X' (FV t')))) as z).
-(apply match_ty_exist).
-exists ([X' := tx] ti).
