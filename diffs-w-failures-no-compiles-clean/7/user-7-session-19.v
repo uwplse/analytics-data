87b230b208b915c798c386d5c70b7a965a3aad81
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:55 -0700

    user 7, session 19, part 90

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -73,4 +73,8 @@ Lemma match_ty_i_t_le_k__v_ke_t : forall (k : nat) (t : ty), | t | <= k -> foral
      |- _ =>
          destruct (max_inv_depth_le__components_le _ _ _ Htk) as [Htk1 Htk2]; apply match_ty_i_pair__inv in Hm;
           destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; apply Nat.max_le_compat; auto
+   | H:|-[ ?k'] ?v <$ TUnion _ _
+     |- _ =>
+         destruct (max_inv_depth_le__components_le _ _ _ Htk) as [Htk1 Htk2]; apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2];
+          [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; auto
    end).
