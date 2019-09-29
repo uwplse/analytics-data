92acfe11624f86ae829bff131909d0eca51eaacd
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:05 -0700

    user 7, session 19, part 279

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -152,4 +152,4 @@ idtac.
 (destruct Hmu as [Hmu1| Hmu2]; [ left | right ]; intros v Hv Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst).
 (assert (Hmt't : |-[ S k] TRef t' <$ TRef t)).
 (simpl).
-assumption.
+auto.
