e2504c9649a49ca7ee454ac4d8613a78b6cc31e5
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:55:39 -0700

    user 7, session 180, part 0

---
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-180.v
@@ -0,0 +1,10 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0281a.BaseDefs.
+Require Import BetaJulia.Sub0281a.BaseProps.
+Require Import BetaJulia.Sub0281a.BaseMatchProps.
+Require Import BetaJulia.Sub0281a.BaseSemSubProps.
