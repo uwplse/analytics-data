f37a9bbe65ab95956187a13fed81885b8686894f
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:55:41 -0700

    user 7, session 182, part 23

--- diffs-w-failures-no-compiles-clean/7/user-7-session-182.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-182.v
@@ -11,7 +11,6 @@ Require Import BetaJulia.Sub0281a.BaseSemSubProps.
 Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
 Lemma subst_nested :
   forall (X Y : id) (tX tY : ty), X <> Y -> fresh_in_ty Y tX -> forall t : ty, [X := tX] ([Y := tY] t) = [Y := [X := tX] tY] ([X := tX] t).
 Proof.
@@ -37,6 +36,8 @@ contradiction.
 (rewrite subst_exist_eq).
 (rewrite subst_exist_neq; try assumption).
 (rewrite subst_exist_eq).
+reflexivity.
++
 (destruct (beq_idP X i) as [HX| HX]).
 *
 subst.
@@ -46,45 +47,3 @@ subst.
 (pose proof (IdSetFacts.mem_1 Hin) as Hmem).
 (rewrite (subst_exist_neq _ _ _ _ HY)).
 (rewrite subst_exist_eq).
-Abort.
-Lemma build_v_full :
-  forall (X X' : id) (w : nat) (t v : ty) (tx : ty),
-  |-[ w] v <$ [X := tx] t ->
-  exists v' : ty, |-[ w] v' <$ [X := TVar X'] t /\ (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> |-[ w'] v <$ [X' := tx] t').
-Proof.
-(intros X X').
-(induction w; induction t; intros v tx Hm).
--
-exists v.
-split.
-assumption.
-(apply match_ty_cname__inv in Hm; subst).
-(induction w'; induction t'; intros Hm; try assumption || contradiction).
-+
-(rewrite subst_union).
-(apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
-+
-(rewrite subst_union).
-(apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
-+
-(destruct (beq_idP X' i) as [Hbeq| Hbeq]).
-*
-subst.
-(rewrite subst_exist_eq).
-assumption.
-*
-(apply match_ty_exist__inv in Hm).
-(destruct Hm as [ti Hm]).
-(destruct (IdSetProps.In_dec i (FV tx)) as [Hin| Hin]).
-{
-(pose proof (IdSetFacts.mem_1 Hin) as Hmem).
-(rewrite subst_equation).
-Search -beq_id.
-(pose proof (false_beq_id _ _ Hbeq) as Hneq).
-(rewrite Hneq).
-(rewrite Hmem).
-(remember (gen_fresh (IdSet.union (FV tx) (IdSet.add X' (FV t')))) as Z).
-exists ([X' := tx] ti).
-specialize (IHw' _ Hm).
