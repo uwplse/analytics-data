a50475ccfddc470b4b840868dd944ea57beed929
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:40 -0700

    user 7, session 39, part 1

--- diffs-w-failures-no-compiles-clean/7/user-7-session-39.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-39.v
@@ -5,4 +5,3 @@ Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0250a.BaseDefs.
-Require Import BetaJulia.Sub0250a.AltMatchDefs.
