9e7ae4de98df1e5f2c8fee50d05738130f92a431
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:38:54 -0700

    user 3, session 7199, part 88

--- diffs-w-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -263,4 +263,4 @@ intuition eauto.
 (eexists; intuition eauto).
 Qed.
 Theorem log_size_bound d bs :
-  log_size_ok d bs -> a < length bs -> log_addr a < diskSize d.
+  forall a, log_size_ok d bs -> a < length bs -> log_addr a < diskSize d.
