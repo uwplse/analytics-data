3d43e2586c00c29ae1539616acf2688a0b21bfb7
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:54 -0700

    user 7, session 73, part 2

--- diffs-w-failures-no-compiles-clean/7/user-7-session-73.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-73.v
@@ -202,4 +202,7 @@ tauto.
 (rewrite inv_depth_mk_nf).
 assumption.
 (apply mk_nf__sem_sub_k_l).
-(apply sem_eq_k__trans with t2).
+Check sem_eq_k__trans.
+(apply sem_sub_k__trans with t2).
+(apply mk_nf__sem_sub_k_l).
+(apply sem_eq_k__sem_sub_k).
