7f5ef2301f4b5290afed78f141146d0ce45fab9a
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:22 -0700

    user 5, session 36, part 13

--- diffs-w-failures-no-compiles-clean/5/user-5-session-36.v
+++ diffs-w-failures-no-compiles-clean/5/user-5-session-36.v
@@ -261,4 +261,9 @@ clear H.
  | H:eval ?L ?env (If (In ?x ?S) _ _) = _ |- _ => destruct (evalIn L env x S)
  end).
 -
-(rewrite evalIfTrue in H0).
+(rewrite evalIfTrue in H0; admit).
+-
+exfalso.
+(rewrite evalIfFalse in H0).
++
+(apply evalBoolInj).
