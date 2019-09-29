19448722bf3a68bd725a02aa0d67ad341e232872
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:58:10 -0700

    user 8, session 81, part 13

--- diffs-w-failures-no-compiles-clean/8/user-8-session-81.v
+++ diffs-w-failures-no-compiles-clean/8/user-8-session-81.v
@@ -720,6 +720,12 @@ Goal _ forall m n (A B : Matrix m n), A == B -> I 1 \226\138\151 A \226\138\151 
 Proof.
 (intros).
 (rewrite kron_1_l).
-(rewrite kron_1_l).
-(rewrite Nat.mul_1_l).
+(repeat rewrite Nat.mul_1_l).
 (rewrite (kron_1_r A)).
+(rewrite (kron_1_r B)).
+(repeat rewrite Nat.mul_1_r).
+(apply H).
+Qed.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqTu97u2"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
