1e0e33727087df0ea7680f71ef889c7d073e1d1e
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:12 -0700

    user 7, session 19, part 420

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -211,8 +211,12 @@ Lemma sem_sub_k_i_nf__inv_depth_le : forall (k : nat) (t t' : ty), InNF( t) -> |
        |- _ =>
            destruct (in_nf_pair__inv _ _ Hnft) as [Hnft1 Hnft2]; destruct (sem_sub_k_i_pair__inv _ _ _ _ _ Hsem) as [Hsem1 Hsem2]; simpl;
             apply Nat.max_le_compat; auto
-     | Hsem:||-[ ?k][TUnion _ _]<= [TUnion _ _]
+     | Hsem:||-[ ?k][TUnion _ _]<= [_], Hnft:InNF( TUnion _ _)
        |- _ =>
            destruct (sem_sub_k_union_l__inv _ _ _ _ Hsem) as [HSem1 Hsem2]; destruct (in_nf_union__inv _ _ Hnft) as [Hnft1 Hnft2];
             rewrite inv_depth_union; apply Nat.max_lub; auto
      end ])).
+-
+(simpl).
+(apply le_n_S).
+(apply IHk).
