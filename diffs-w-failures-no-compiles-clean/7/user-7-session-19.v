157ba1558b7d01c8166cab03f318888c4c8d081e
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:15 -0700

    user 7, session 19, part 474

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -194,5 +194,10 @@ Proof.
 (intros t1 t2 t' Hsem).
 (unfold sem_sub_i in Hsem).
 (split; intros k; specialize (Hsem k); destruct (sem_sub_k_union_l__inv _ _ _ _ Hsem); assumption).
-Lemma value_sem_sub_i_union__inv :
-  forall v : ty, value_type v -> forall ta tb : ty, ||- [v]<= [TUnion ta tb] -> ||- [v]<= [ta] \/ ||-[ k][v]<= [tb].
+Lemma value_sem_sub_i_union__inv : forall v : ty, value_type v -> forall ta tb : ty, ||- [v]<= [TUnion ta tb] -> ||- [v]<= [ta] \/ ||- [v]<= [tb].
+Proof.
+(intros v Hv ta tb Hsem; unfold sem_sub_i in Hsem).
+(assert (Hm : |-[ 0] v <$ v) by (apply match_ty_i__reflexive; assumption)).
+specialize (Hsem 0 _ Hm).
+(apply match_ty_i_union__inv in Hsem).
+(destruct Hsem; [ left | right ]).
