29458f4f77ccf1f50b067cc248a14296ae25415c
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:06 -0700

    user 7, session 19, part 299

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -147,5 +147,5 @@ Lemma value_sem_sub_k_union__value_sem_sub_k_component :
   | Hsem:forall v, value_type v -> |-[ ?k] v <$ TCName ?c -> _
     |- _ =>
         assert (Hvv : value_type (TCName c)) by constructor;
-         assert (Hmv : |-[ k] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption)
+         assert (Hmv : |-[ k] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hvv Hmv)
   end).
