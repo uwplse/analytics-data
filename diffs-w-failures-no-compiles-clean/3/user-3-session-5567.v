8807c85ff3ab6388f38eef5676664ffb7b98c7b9
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:33:52 -0700

    user 3, session 5567, part 12

--- diffs-w-failures-no-compiles-clean/3/user-3-session-5567.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-5567.v
@@ -567,4 +567,17 @@ step_proc.
 (destruct r; try step).
 (destruct r; try step).
 (destruct (v == v0); try step).
++
+(assert (a0 < a \/ a0 = a) by lia; intuition auto; simplify).
++
+intuition eauto.
+{
+(destruct (a == a0); simplify; finish).
+}
+step.
+(destruct r; try step).
 *
+(destruct (a == a0); simplify; finish).
+*
+(destruct (a == a0); simplify; finish).
+}
