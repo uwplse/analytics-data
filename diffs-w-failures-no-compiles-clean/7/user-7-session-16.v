8b1d8404de1111042d2cc779f1ac331b3e080aea
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:42 -0700

    user 7, session 16, part 51

--- diffs-w-failures-no-compiles-clean/7/user-7-session-16.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-16.v
@@ -377,12 +377,14 @@ Proof.
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
 +
-right.
 (match goal with
  | |- ~ |- ?t1 << ?t2 =>
        remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]);
         subst
  end).
 (match goal with
- | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto
+ | Hcontra:|- ?t1 << ?t2
+   |- False =>
+       remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]);
+        subst
  end).
