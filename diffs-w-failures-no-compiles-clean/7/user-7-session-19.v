f2eaa97f175dd42d3c3cbd0b78d86f6a40ecb520
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:51 -0700

    user 7, session 19, part 0

---
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -0,0 +1,98 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0250a.BaseDefs.
+Require Import BetaJulia.Sub0250a.AltMatchDefs.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Open Scope btjmi_scope.
+Lemma match_ty_i_cname__inv : forall (v : ty) (c : cname), forall k : nat, |-[ k] v <$ TCName c -> v = TCName c.
+Proof.
+(intros v; induction v; try (solve [ intros c k Hm; destruct k; simpl in Hm; contradiction ])).
+(intros c0 k Hm).
+(destruct k; simpl in Hm; subst; reflexivity).
+Qed.
+Lemma match_ty_i_pair__inv :
+  forall v t1 t2 : ty, forall k : nat, |-[ k] v <$ TPair t1 t2 -> exists v1 v2 : ty, v = TPair v1 v2 /\ |-[ k] v1 <$ t1 /\ |-[ k] v2 <$ t2.
+Proof.
+(intros v; induction v; try (solve [ intros t1 t2 k Hm; destruct k; simpl in Hm; contradiction ])).
+(intros t1 t2 k Hm).
+exists v1,v2.
+(split; try reflexivity).
+(destruct k; simpl in Hm; assumption).
+Qed.
+Lemma match_ty_i_union__inv : forall v t1 t2 : ty, forall k : nat, |-[ k] v <$ TUnion t1 t2 -> |-[ k] v <$ t1 \/ |-[ k] v <$ t2.
+Proof.
+(intros v t1 t2 k Hm).
+(destruct k; destruct v; assumption).
+Qed.
+Lemma match_ty_i_ref__inv :
+  forall v t : ty,
+  forall k : nat, |-[ S k] v <$ TRef t -> exists t' : ty, v = TRef t' /\ (forall v' : ty, value_type v' -> |-[ k] v' <$ t' <-> |-[ k] v' <$ t).
+Proof.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; simpl in Hm; contradiction ])).
+clear IHv.
+(intros t k).
+(intros Hm).
+(pose proof Hm as Href).
+(simpl in Href).
+exists v.
+split.
+reflexivity.
+(intros v' Hv').
+specialize (Href v' Hv').
+(destruct Href; split; assumption).
+Qed.
+Close Scope btjmi_scope.
+Open Scope btjmdeq_scope.
+Lemma match_ty_deq_cname__inv : forall (v : ty) (c : cname), forall k : nat, |-[ k] v <$ TCName c -> v = TCName c.
+Proof.
+(intros v; induction v; try (solve [ intros c k Hm; destruct k; simpl in Hm; contradiction ])).
+(intros c0 k Hm).
+(destruct k; simpl in Hm; subst; reflexivity).
+Qed.
+Lemma match_ty_deq_pair__inv :
+  forall v t1 t2 : ty, forall k : nat, |-[ k] v <$ TPair t1 t2 -> exists v1 v2 : ty, v = TPair v1 v2 /\ |-[ k] v1 <$ t1 /\ |-[ k] v2 <$ t2.
+Proof.
+(intros v; induction v; try (solve [ intros t1 t2 k Hm; destruct k; simpl in Hm; contradiction ])).
+(intros t1 t2 k Hm).
+exists v1,v2.
+(split; try reflexivity).
+(destruct k; simpl in Hm; assumption).
+Qed.
+Lemma match_ty_deq_union__inv : forall v t1 t2 : ty, forall k : nat, |-[ k] v <$ TUnion t1 t2 -> |-[ k] v <$ t1 \/ |-[ k] v <$ t2.
+Proof.
+(intros v t1 t2 k Hm).
+(destruct k; destruct v; assumption).
+Qed.
+Lemma match_ty_deq_ref__inv :
+  forall v t : ty,
+  forall k : nat,
+  |-[ S k] v <$ TRef t ->
+  exists t' : ty,
+    v = TRef t' /\ inv_depth t <= k /\ inv_depth t' = inv_depth t /\ (forall v' : ty, value_type v' -> |-[ k] v' <$ t' <-> |-[ k] v' <$ t).
+Proof.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; simpl in Hm; contradiction ])).
+clear IHv.
+(intros t k).
+(intros Hm).
+(simpl in Hm).
+(destruct Hm as [Hdept [Hdepv Href]]).
+exists v.
+split.
+reflexivity.
+split.
+assumption.
+split.
+assumption.
+(intros v' Hv').
+specialize (Href v' Hv').
+(destruct Href; split; assumption).
+Qed.
+Close Scope btjmdeq_scope.
+Open Scope btjmdle_scope.
