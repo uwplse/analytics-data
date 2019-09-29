0352d5c08ca2b63edda7f99e6e776f48ba3d8700
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:28 -0700

    user 7, session 103, part 10

--- diffs-w-failures-no-compiles-clean/7/user-7-session-103.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-103.v
@@ -8,18 +8,14 @@ Require Import BetaJulia.Sub0280a.BaseDefs.
 Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Open Scope btjm.
 Lemma match_ty_pair : forall (v1 v2 t1 t2 : ty) (k w : nat), |-[ k, w] v1 <$ t1 -> |-[ k, w] v2 <$ t2 -> |-[ k, w] TPair v1 v2 <$ TPair t1 t2.
 Proof.
 (intros v1 v2 t1 t2 k w Hm1 Hm2).
 (destruct k, w; split; assumption).
+Qed.
 Lemma match_ty_union_1 : forall (v t1 t2 : ty) (k w : nat), |-[ k, w] v <$ t1 -> |-[ k, w] v <$ TUnion t1 t2.
 Proof.
 (intros v t1 t2 k w Hm).
-(destruct k, w, v; left; assumption).
-Qed.
-Lemma match_ty_union_2 : forall (v t1 t2 : ty) (k w : nat), |-[ k, w] v <$ t2 -> |-[ k, w] v <$ TUnion t1 t2.
-Proof.
-(intros v t1 t2 k w Hm).
-(destruct k, w, v; right; assumption).
+(destruct k, w).
+(simpl).
+left.
