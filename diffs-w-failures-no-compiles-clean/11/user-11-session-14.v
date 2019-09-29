437c41680df62b01a89dd264e7bc707c083dc191
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:59:13 -0700

    user 11, session 14, part 10

--- diffs-w-failures-no-compiles-clean/11/user-11-session-14.v
+++ diffs-w-failures-no-compiles-clean/11/user-11-session-14.v
@@ -33,30 +33,3 @@ Anomaly ""Assert_failure printing/ppconstr.ml:399:14"." Please report at http://
 Arguments serverAppE : clear implicits.
 Definition smE := serverAppE exp +' evalE.
 Definition kvs_state exp_ := list (N * exp_ N).
-Definition kvs : itree smE void :=
-  rec
-    (fun st : kvs_state exp =>
-     req <- trigger ServerApp_Recv;;
-     match req with
-     | Kvs_GET k =>
-         match kvs_get k st with
-         | Some v => embed ServerApp_Send (Kvs_OK v);; call st
-         | None => v <- trigger (@ServerApp_Fresh exp);; embed ServerApp_Send (Kvs_OK v);; call (kvs_put k v st)
-         end
-     | Kvs_PUT k v => embed ServerApp_Send Kvs_NoContent;; call (kvs_put k (exp_int v) st)
-     | Kvs_CAS k x v' =>
-         match kvs_get k st with
-         | Some v =>
-             b <- trigger (Eval (exp_eq x v));;
-             (if b : bool
-              then embed ServerApp_Send Kvs_NoContent;; call (kvs_put k (exp_int v') st)
-              else embed ServerApp_Send Kvs_PreconditionFailed;; call st)
-         | None =>
-             v <- trigger (@ServerApp_Fresh exp);;
-             b <- trigger (Eval (exp_eq x v));;
-             (if b : bool
-              then embed ServerApp_Send Kvs_NoContent;; call (kvs_put k (exp_int v') st)
-              else embed ServerApp_Send Kvs_PreconditionFailed;; call (kvs_put k v st))
-         end
-     | _ => embed ServerApp_Send Kvs_BadRequest;; call st
-     end) [].
