4b46519f6b05f83bfec7f60659ba30cd725c3177
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:45 -0700

    user 7, session 108, part 102

--- diffs-w-failures-no-compiles-clean/7/user-7-session-108.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-108.v
@@ -246,5 +246,7 @@ Proof.
 (induction k; induction t; induction t'; intros Hdep Hsem; try reflexivity).
 -
 (destruct Hsem as [Hsem _]).
-specialize (Hsem 0).
+(destruct Hsem as [w2 Hsem]).
 (assert (Hm : |-[ 0, 0] TCName c <$ TCName c) by (apply match_ty_value_type__reflexive; constructor)).
+specialize (Hsem _ Hm).
+(destruct w2; simpl in Hsem; contradiction).
