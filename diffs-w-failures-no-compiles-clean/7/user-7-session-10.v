ebeed61ad09f9ebb12f40be056c94aae7dea3cd3
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:35 -0700

    user 7, session 10, part 68

--- diffs-w-failures-no-compiles-clean/7/user-7-session-10.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-10.v
@@ -208,21 +208,22 @@ tauto.
 (apply sub_r_union_l__inv in Hsub').
 (destruct Hsub'; auto).
 -
-(pose proof (in_nf_ref__inv _ Hnfm1) as Hnf1).
-(pose proof (in_nf_ref__inv _ Hnfm2) as Hnf2).
-(destruct IHHsub1 as [IHHsub11 IHHsub12]; try assumption).
-(destruct IHHsub2 as [IHHsub21 IHHsub22]; try assumption).
-(split; intros tx Hsub'; [ remember (TRef t) as ty eqn:Heqy  | remember (TRef t') as ty eqn:Heqy  ]; induction Hsub'; inversion Heqy; subst;
-  try (solve [ constructor; auto ])).
-+
-(apply IHHsub').
-(apply mk_nf_nf__equal; assumption).
-(apply mk_nf__in_nf).
+Lemma sub_r__trans2 :
+  forall tm1 tm2 : ty, |- tm1 << tm2 -> (forall tl : ty, |- tl << tm1 -> |- tl << tm2) /\ (forall tr : ty, |- tm2 << tr -> |- tm1 << tr).
+Proof.
+(intros tm1 tm2 Hsub).
+(induction Hsub).
 -
-(split; intros tx Hsub'; apply SR_NormalForm; apply IHHsub; try tauto || apply mk_nf__in_nf).
-(apply sub_r__mk_nf_sub_r; assumption).
+tauto.
+-
+(destruct IHHsub1 as [IHHsub11 IHHsub12]).
+(destruct IHHsub2 as [IHHsub21 IHHsub22]).
+(split; intros tx Hsub'; [ remember (TPair t1 t2) as ty eqn:Heqy  | remember (TPair t1' t2') as ty eqn:Heqy  ]; induction Hsub'; inversion Heqy;
+  subst; try (solve [ constructor; auto ])).
++
 (apply SR_NormalForm).
 (assert (Hsub : |- TPair t1 t2 << TPair t1' t2') by (constructor; assumption)).
+(apply sub_r__mk_nf_sub_r in Hsub).
 (apply sub_r_nf__trans2 with (MkNF( TPair t1' t2')); assumption || apply mk_nf__in_nf).
 -
 (destruct IHHsub1 as [IHHsub11 IHHsub12]).
@@ -256,3 +257,12 @@ tauto.
 -
 (split; intros tx Hsub'; apply SR_NormalForm; apply IHHsub; try tauto || apply mk_nf__in_nf).
 (apply sub_r__mk_nf_sub_r; assumption).
+Qed.
+Lemma sub_r__reflexive : forall t : ty, |- t << t.
+Proof.
+(apply sub_r__rflxv).
+Qed.
+Lemma sub_r__transitive : forall t1 t2 t3 : ty, |- t1 << t2 -> |- t2 << t3 -> |- t1 << t3.
+Proof.
+(intros t1 t2 t3 Hsub1 Hsub2).
+(pose proof (sub_r__trans2 _ _ Hsub1)).
