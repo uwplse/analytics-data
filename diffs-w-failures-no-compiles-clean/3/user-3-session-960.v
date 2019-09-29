9617a17a0c3b5c2fa7e39684578a4fb202bf6593
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:18:17 -0700

    user 3, session 960, part 296

--- diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
@@ -260,4 +260,5 @@ typeclasses eauto.
 (unfold EqualDec).
 (intros; simpl).
 Search -sigT -"dec".
-(apply Eqdep_dec.inj_pair2_eq_dec).
+Check Eqdep_dec.inj_pair2_eq_dec.
+(destruct (Eqdep_dec.inj_pair2_eq_dec x y)).
