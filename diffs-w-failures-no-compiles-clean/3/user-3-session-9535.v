9ea214eaf243d3c60dbc360d519077644715898b
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:45:50 -0700

    user 3, session 9535, part 0

---
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-9535.v
@@ -0,0 +1,9 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqGcu2hy"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Import POCS.
+Require Import LogAPI.
