6315836f53a0a9b6997fac1884e0155f91bad558
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:46:18 -0700

    user 3, session 9671, part 33

--- diffs-w-failures-no-compiles-clean/3/user-3-session-9671.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-9671.v
@@ -245,4 +245,15 @@ step.
 (pose proof (H3 a); intuition).
 (assert (log_addr a < diskSize state)).
 {
-lia.
+eauto.
+}
+eq_values.
+auto.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqn7pS5Y"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
