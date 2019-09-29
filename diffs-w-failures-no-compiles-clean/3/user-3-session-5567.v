dc34b6eac75ece2027f4e343dbc013813e21b497
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:33:52 -0700

    user 3, session 5567, part 13

--- diffs-w-failures-no-compiles-clean/3/user-3-session-5567.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-5567.v
@@ -580,4 +580,14 @@ step.
 (destruct (a == a0); simplify; finish).
 *
 (destruct (a == a0); simplify; finish).
+Unshelve.
+{
+auto.
 }
+exact (fun _ => True).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqvoBkdJ"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
