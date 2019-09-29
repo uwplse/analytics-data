c4a4b838a9b3627012bc16d8fac55e146c8f16d2
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:02 -0700

    user 7, session 83, part 38

--- diffs-w-failures-no-compiles-clean/7/user-7-session-83.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-83.v
@@ -281,4 +281,5 @@ clear IHt.
 (apply match_ty_i_ref__inv in Hm).
 (destruct Hm as [t' [Heq Href]]; subst).
 (simpl).
-(apply sem_eq_k_i__trans).
+Check sem_eq_k_i__trans.
+(eapply sem_eq_k_i__trans).
