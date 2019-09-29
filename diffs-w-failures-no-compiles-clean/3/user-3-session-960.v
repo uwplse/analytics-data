bda4147aebd3707b399a079d7c09f7e3a45bfb44
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:18:01 -0700

    user 3, session 960, part 3

--- diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
@@ -83,4 +83,7 @@ Instance aModel : GoModel.
 Proof.
 Print uint64.
 Print Ascii.Ascii.
-Search -string -Ascii.ascii.
+refine
+ {|
+ byte := Ascii.ascii;
+ byte0 := Ascii.ascii false false false false false false false false |}.
