89d29358584856b38a3e6678d2eca6b69b062442
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:36:06 -0700

    user 3, session 6295, part 1

--- diffs-w-failures-no-compiles-clean/3/user-3-session-6295.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-6295.v
@@ -5,4 +5,4 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Module Log (d: OneDiskAPI)<: LogAPI.
+Require Import POCS.
