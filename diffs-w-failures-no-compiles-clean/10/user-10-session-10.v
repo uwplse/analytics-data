16344ad6744dba94345d1d355ebf194a718227e1
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:59:02 -0700

    user 10, session 10, part 3

--- diffs-w-failures-no-compiles-clean/10/user-10-session-10.v
+++ diffs-w-failures-no-compiles-clean/10/user-10-session-10.v
@@ -270,8 +270,8 @@ Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqiFtR30"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
-Definition network_of_app {E} {nE} `{networkE -< nE} `{exceptE error -< nE} (k : shared_key) 
-  T (e : (appE id +' E) T) : itree nE T :=
+Definition network_of_app {E} {nE} `{networkE -< nE} `{exceptE error -< nE} `{E -< NE} 
+  (k : shared_key) T (e : (appE id +' E) T) : itree nE T :=
   match e with
   | (ae|) =>
       match ae with
