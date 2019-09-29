60b826d215dc23984adc92998f1351bceacafa6a
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:48:15 -0700

    user 3, session 10289, part 1

--- diffs-w-failures-no-compiles-clean/3/user-3-session-10289.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-10289.v
@@ -71,5 +71,5 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-Inductive Marker : string -> Type :=
+Inductive Marker : String.string -> Type :=
     mark : _.
