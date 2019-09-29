f0ac06880a88ca92856f31309df7b2d2eeb35d01
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:04 -0700

    user 7, session 19, part 260

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -124,7 +124,7 @@ auto using match_ty_i_pair.
 +
 (apply match_ty_i_union__inv in Hm2).
 (destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
--
 (intros v1 t3 k Hm1 Hm2).
-(destruct k).
-(destruct v1; try contradiction).
+(induction t3; try (solve [ destruct k; simpl in Hm2; contradiction ])).
++
+(apply match_ty_union__inv in Hm2).
