48fb329055b2753e10806051a422d7aac8f7667b
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:25:44 -0700

    user 3, session 3324, part 45

--- diffs-w-failures-no-compiles-clean/3/user-3-session-3324.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-3324.v
@@ -144,4 +144,4 @@ step_proc.
 exists (diskUpd (diskShrink (stateDisk state)) (stateBadBlock state) b).
 (unfold inited_any; intuition idtac).
 (constructor; autorewrite with upd in *; intuition idtac).
-(rewrite diskUpd_eq; auto).
+autorewrite with upd.
