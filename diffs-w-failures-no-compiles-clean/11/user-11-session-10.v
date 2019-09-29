c2b5f9db0555807c90826955f4cc3d5d9d484fd3
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:59:11 -0700

    user 11, session 10, part 8

--- diffs-w-failures-no-compiles-clean/11/user-11-session-10.v
+++ diffs-w-failures-no-compiles-clean/11/user-11-session-10.v
@@ -44,4 +44,3 @@ Instance showDataX : (Show (kvs_data exp)) :=
          | Kvs_BadRequest => "400"
          | Kvs_PreconditionFailed => "412"
          end |}.
-Definition kvs_get {V} (k : N) : list (N * V) -> option V := fmap snd \226\136\152 find (N.eqb k \226\136\152 fst).
