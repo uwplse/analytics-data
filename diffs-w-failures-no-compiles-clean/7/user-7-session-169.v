f53beb24598e25b8513c4095cf7a2e10ea4ab702
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:55:25 -0700

    user 7, session 169, part 4

--- diffs-w-failures-no-compiles-clean/7/user-7-session-169.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-169.v
@@ -86,4 +86,8 @@ specialize (Hid Hneq).
 (unfold fresh_in_ty, fresh in HY).
 Search -IdSet.mem.
 Check IdSetFacts.not_mem_iff.
-(destruct (IdSetFacts.not_mem_iff (FV s) Y) as [_ Hmem]).
+(destruct (IdSetFacts.not_mem_iff (FV s) Y) as [Hmem _]).
+specialize (Hmem HY).
+(rewrite Hmem).
+reflexivity.
+Qed.
