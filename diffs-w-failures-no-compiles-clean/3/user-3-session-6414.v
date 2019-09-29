e869dc0c9923ccbef8f2426fa794dffa0f504bf7
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:36:29 -0700

    user 3, session 6414, part 23

--- diffs-w-failures-no-compiles-clean/3/user-3-session-6414.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-6414.v
@@ -98,4 +98,13 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqmIHFZM"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Definition get : proc (list block).
+Definition get : proc (list block) := len <- get_len; get_upto len.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8aT6YT"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq2gZaVH"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
