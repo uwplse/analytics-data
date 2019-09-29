a53d786e4c842c11173f2a2a61b7e3994200b5d9
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:18:11 -0700

    user 3, session 960, part 198

--- diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
@@ -142,4 +142,4 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqlBjurg"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Search -Ascii.ascii.
-Print Ascii.byte_of_ascii.
+Print Ascii.ascii_of_byte.
