34df4254a855982ce361463e7f13bc17e9d5286e
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:57:28 -0700

    user 8, session 55, part 0

---
+++ diffs-w-failures-no-compiles-clean/8/user-8-session-55.v
@@ -0,0 +1,9 @@
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqVQNm49"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Import Psatz.
+Require Import Reals.
