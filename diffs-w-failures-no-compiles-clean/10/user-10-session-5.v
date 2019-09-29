160cb50b979cf3dcb1b8add7054d10b57b0f81e2
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:58:56 -0700

    user 10, session 5, part 0

---
+++ diffs-w-failures-no-compiles-clean/10/user-10-session-5.v
@@ -0,0 +1,19 @@
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqDQo6Ez"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqm16FTG"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+From Coq Require Import Basics ExtrOcamlIntConv List NArith String.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqRp5hKL" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Require Import Analytical.Analytics.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqE0q7Fg" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
