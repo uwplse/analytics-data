38a92b79b2ec456590e55d2ecdc4d13b302eedfe
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:35 -0700

    user 7, session 12, part 3

--- diffs-w-failures-no-compiles-clean/7/user-7-session-12.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-12.v
@@ -295,4 +295,7 @@ generalize dependent t21.
 (induction Hnf1; intros t21 t22).
 -
 (rewrite unite_pairs_atom_union).
-(rewrite unite_pairs_atom_union; assumption).
+(rewrite unite_pairs_atom_union; try assumption).
+(apply sub_r__rflxv).
+-
+(destruct (unite_pairs_union_t t1 t2 t21) as [Heq1| [Heq11 Heq12]]; destruct (unite_pairs_union_t t1 t2 t22) as [Heq2| [Heq21 Heq22]]).
