33d42b1517877a3c37aa146f5a45f820c29f7fa1
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:55 -0700

    user 7, session 116, part 41

--- diffs-w-failures-no-compiles-clean/7/user-7-session-116.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-116.v
@@ -79,4 +79,13 @@ subst.
 subst.
 reflexivity.
 *
-(rewrite IHt; assumption).
+(rewrite IHt; try assumption).
+reflexivity.
+-
+(simpl; destruct (beq_idP X i); destruct (beq_idP Y i); subst).
++
+contradiction.
++
+(simpl).
+(rewrite <- beq_id_refl).
+(apply subs_fresh_in_ty).
