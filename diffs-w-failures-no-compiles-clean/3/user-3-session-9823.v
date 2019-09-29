08a5c961df9fd04797b9a762fac027ee526fbc89
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:46:48 -0700

    user 3, session 9823, part 15

--- diffs-w-failures-no-compiles-clean/3/user-3-session-9823.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-9823.v
@@ -174,4 +174,5 @@ replace (diskSize (stateDisk state) - 1) with diskSize s in * by lia.
 (rewrite <- Hremap by eauto; auto).
 -
 invert_abstraction.
+(step_proc; intuition eauto).
 (step_proc; intuition).
