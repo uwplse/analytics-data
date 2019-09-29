3397264bd38d59e1ba65d4768e40a310588fa427
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:18:02 -0700

    user 3, session 960, part 19

--- diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
@@ -81,5 +81,4 @@ Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 Search -"endian".
 Search -"digit".
-Definition uint64_to_le (x : uint64) : list byte :=
-  if lt_dec x (pow 2 64) then nil else nil.
+Fixpoint nat_to_le (x : nat) : list {x : nat | x < 256}.
