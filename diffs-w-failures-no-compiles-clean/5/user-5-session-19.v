6984d4ef9d5fe13b01f4af3882d951e61e5f4259
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:16 -0700

    user 5, session 19, part 38

--- diffs-w-failures-no-compiles-clean/5/user-5-session-19.v
+++ diffs-w-failures-no-compiles-clean/5/user-5-session-19.v
@@ -198,12 +198,3 @@ intro x.
 (apply evalChoose).
 exists (eval L env (Int 3)).
 (apply evalEqTrue).
-(rewrite evalTimes with (i := 3%Z) (j := 2%Z)).
--
-reflexivity.
--
-(rewrite evalVar).
-(rewrite extendEnv_eq).
-(apply evalIntConst).
--
-reflexivity.
