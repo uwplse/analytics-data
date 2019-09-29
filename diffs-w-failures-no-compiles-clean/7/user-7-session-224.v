8ac44002e25d219632d63b3b17647524b2b1877c
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:56:17 -0700

    user 7, session 224, part 8

--- diffs-w-failures-no-compiles-clean/7/user-7-session-224.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-224.v
@@ -149,4 +149,8 @@ reflexivity.
 Check b_free_in_ty__b_free_in_b_subst_neq.
 (apply b_free_in_ty__b_free_in_b_subst_neq in HX).
 (apply (b_free_in_ty__b_free_in_b_subst_neq i ti) in HX; try assumption).
-specialize (IHw _ _ _ HX Hm).
+specialize (IHw _ _ Hwftx HX Hm).
+(destruct IHw as [v' [Hm' IHw]]).
+exists v'.
+split.
+exists ti.
