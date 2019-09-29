0ed59386d53be5d73893930fe5e10566283a158b
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:21 -0700

    user 7, session 19, part 587

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -381,4 +381,5 @@ Lemma value_sem_sub_i_union__inv : forall v : ty, value_type v -> forall ta tb :
 (remember (| TUnion ta tb |) as kmax).
 (assert (Hdeple : | v | <= kmax)).
 {
+subst.
 (apply sem_sub_k_i__inv_depth_le_2).
