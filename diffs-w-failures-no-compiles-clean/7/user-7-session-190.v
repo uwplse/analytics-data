0e299f40dbc9520dad1738588b5c66eff114b77f
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:55:47 -0700

    user 7, session 190, part 1

--- diffs-w-failures-no-compiles-clean/7/user-7-session-190.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-190.v
@@ -35,5 +35,4 @@ assumption.
 (apply match_ty_union_1; auto).
 *
 (rewrite subst_union).
-(destruct (free_in_ty_union__inv _ _ _ HX') as [HX''| HX'']).
-(apply match_ty_union_1; auto).
+(destruct (free_in_ty__decidable X t'1)).
