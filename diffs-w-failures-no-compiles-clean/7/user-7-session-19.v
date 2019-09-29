04adf4b732ad1a626a60c42795f650348acdc59f
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:03 -0700

    user 7, session 19, part 250

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -107,5 +107,6 @@ Lemma value_sem_sub_k_union__value_sem_sub_k_component :
 idtac.
 (unfold sem_sub_k_i in Hsem).
 (assert (Hvref : value_type (TRef t)) by constructor).
-(assert (Hmref : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption)).
+(pose proof (Hsem _ Hvref Hmref) as Hmu).
 (apply match_ty_i_union__inv in Hmu).
+(destruct Hmu as [Hmu1| Hmu2]; [ left | right ]; intros v Hv Hm).
