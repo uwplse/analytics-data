dd45c2567771b853a5587aa274cd0c36934946e4
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:59:08 -0700

    user 10, session 17, part 0

---
+++ diffs-w-failures-no-compiles-clean/10/user-10-session-17.v
@@ -0,0 +1,26 @@
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coq5mGpzh"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+From ReductionEffect Require Import PrintingEffect.
+Eval compute in (fun f x => f (f (f x))) (fun x => S (print_id x)) 0.
+Eval cbn in (fun f x => f (f (f x))) print_id 0.
+Eval hnf in (fun f x => f (f (f x))) print_id 0.
+Eval simpl in (fun f x => f (f (f x))) (fun x => print_id (1 + x) + 1) 0.
+Anomaly ""Assert_failure printing/ppconstr.ml:399:14"."
+Please report at http://coq.inria.fr/bugs/.
+From Coq Require Import NArith Streams.
+Open Scope N_scope.
+Anomaly ""Assert_failure printing/ppconstr.ml:399:14"."
+Please report at http://coq.inria.fr/bugs/.
+Eval compute in Str_nth 10 (fib1 0 1).
+Anomaly ""Assert_failure printing/ppconstr.ml:399:14"."
+Please report at http://coq.inria.fr/bugs/.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqvTdMQo"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Eval cbn in Str_nth 0 (fib2 0 1).
