472fc41604d30fdfc447aa325652daad2b09017e
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:18:11 -0700

    user 3, session 960, part 183

--- diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
@@ -122,4 +122,4 @@ auto.
 (assert (0 < S n) by lia).
 (generalize dependent S n; clear n; intros n **).
 (rewrite IHn).
-(rewrite (PeanoNat.Nat.div_mod n base)  at 4 by lia).
+(rewrite (PeanoNat.Nat.div_mod n base)  at 3 by lia).
