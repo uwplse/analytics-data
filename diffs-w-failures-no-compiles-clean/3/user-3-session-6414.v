52d30bd8385e5f8ec0b34df77432ea49e1ee62bb
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:36:31 -0700

    user 3, session 6414, part 61

--- diffs-w-failures-no-compiles-clean/3/user-3-session-6414.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-6414.v
@@ -133,11 +133,4 @@ Theorem log_abstraction_nil d b :
 Proof.
 (unfold log_abstraction; intros).
 split.
--
-(intuition; exfalso; simpl in *; lia).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqu9prmS"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+(simpl; lia).
