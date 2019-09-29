e51a27714cefeb0ce20ee508df53fbeecd2ac0c1
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:14:35 -0700

    user 2, session 5, part 62

--- diffs-w-failures-no-compiles-clean/2/user-2-session-5.v
+++ diffs-w-failures-no-compiles-clean/2/user-2-session-5.v
@@ -142,3 +142,8 @@ Fixpoint multistep (n : nat) : term -> term :=
   end.
 Redirect "/tmp/coq1McSmJ" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
+Fixpoint trace (n : nat) : term -> list term :=
+  match n with
+  | O => nil
+  | S m => fun t => t :: trace m (step t)
+  end.
