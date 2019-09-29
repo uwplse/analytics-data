eead9fae237984b7c496a1af83bb68ad024bd382
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:34 -0700

    user 7, session 104, part 47

--- diffs-w-failures-no-compiles-clean/7/user-7-session-104.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-104.v
@@ -64,3 +64,8 @@ Proof.
 (intros k Hdep w Hcontra).
 (apply match_ty_cname__inv in Hcontra).
 (inversion Hcontra).
+-
+(intros k Hdep w Hcontra).
+(apply match_ty_pair__inv in Hcontra).
+(inversion Hcontra).
+-
