a6d9fcea7a74ba3608e8835ba83054bc557d997b
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:28 -0700

    user 7, session 103, part 26

--- diffs-w-failures-no-compiles-clean/7/user-7-session-103.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-103.v
@@ -47,5 +47,4 @@ Proof.
 Qed.
 Lemma match_ty_ref__inv : forall (v t : ty) (k w : nat), |-[ S k, w] v <$ TRef t -> exists t' : ty, v = TRef t' /\ ||-[ k][t']= [t].
 Proof.
-(intros v t k w Hm).
-(destruct k, w, v; simpl in Hm; try contradiction).
+(destruct k, w, v; try contradiction).
