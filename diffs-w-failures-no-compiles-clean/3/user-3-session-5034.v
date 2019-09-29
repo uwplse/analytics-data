e1e8bcd23ce241ec44a0b2d36b0f7e07e72ea32c
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:32:12 -0700

    user 3, session 5034, part 23

--- diffs-w-failures-no-compiles-clean/3/user-3-session-5034.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-5034.v
@@ -386,3 +386,12 @@ step_proc.
 (step_proc; intuition; subst; eauto).
 -
 (step_proc; intuition; subst; eauto).
+{
+(step_proc; intuition; subst; eauto).
+{
+(step_proc; intuition; subst; eauto).
+{
+(step_proc; intuition; subst; eauto).
+*
+(exists (s ++ v); simpl; intuition).
+(apply log_abstraction_post_commit; auto).
