a3bf556812c27e5ff13965882d7d3a353700720b
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:29 -0700

    user 7, session 6, part 2

--- diffs-w-failures-no-compiles-clean/7/user-7-session-6.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-6.v
@@ -2,14 +2,3 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0250b.BaseDefs.
-Require Import BetaJulia.Sub0250b.BaseProps.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Close Scope btj_scope.
-Open Scope btjnf_scope.
-Open Scope btjr_scope.
