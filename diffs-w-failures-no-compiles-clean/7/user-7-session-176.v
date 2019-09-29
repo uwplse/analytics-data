3c16ab47d813f730b4fa2275c730a9d7aa8551fc
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:55:35 -0700

    user 7, session 176, part 16

--- diffs-w-failures-no-compiles-clean/7/user-7-session-176.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-176.v
@@ -161,4 +161,5 @@ Search -IdSet.mem.
 (destruct (IdSet.mem i (FV tint)) eqn:Hmem).
 (simpl in Hmem).
 Search -IdSet.empty.
-(rewrite IdSetFacts.empty_b).
+(rewrite IdSetFacts.empty_b in Hmem).
+contradiction.
