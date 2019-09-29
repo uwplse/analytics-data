bece08241cc3c993361f344c6b372e802e6f4189
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:35 -0700

    user 7, session 104, part 76

--- diffs-w-failures-no-compiles-clean/7/user-7-session-104.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-104.v
@@ -51,4 +51,5 @@ Proof.
 specialize (Hcontra 1 0).
 (destruct Hcontra as [w2 Hcontra]).
 (assert (Hm : |-[ 1, 0] TRef tint <$ TRef tint) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Hcontra Hm).
+specialize (Hcontra _ Hm).
+(apply macth_ty_cname__inv in hcontra).
