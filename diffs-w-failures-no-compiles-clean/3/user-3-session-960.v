dfbaca8e488d61e5a97161d3359053623d15588d
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:18:02 -0700

    user 3, session 960, part 10

--- diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-960.v
@@ -82,9 +82,10 @@ Timeout 1 Print LoadPath.
 Search -nat -string.
 Eval compute in pretty.pretty_nat 3.
 Instance aModel : GoModel.
-Proof.
 refine
  {|
  byte := Ascii.ascii;
  byte0 := Ascii.Ascii false false false false false false false false;
- uint64_to_string := pretty.pretty_nat |}.
+ uint64_to_string := pretty.pretty_nat;
+ ascii_to_byte := id;
+ byte_to_ascii := id |}.
