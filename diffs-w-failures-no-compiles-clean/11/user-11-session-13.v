b4c62dda22fca095c2c7acc0e9c2a2b8eb52b82f
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:59:12 -0700

    user 11, session 13, part 2

--- diffs-w-failures-no-compiles-clean/11/user-11-session-13.v
+++ diffs-w-failures-no-compiles-clean/11/user-11-session-13.v
@@ -4,8 +4,6 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-From Coq Require Import ExtrOcamlIntConv.
-From ExtLib Require Import Applicative StateMonad Monad.
-From ITree Require Import Exception Nondeterminism ITree.
-From SimpleIO Require Import IO_Random SimpleIO.
-From DeepWeb Require Import CryptoLib KvsLib.
+Redirect "/tmp/coq16819Y1f" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
