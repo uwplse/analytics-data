44222519cef4c29f01ae6b0ed28f577995a7464c
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:38:58 -0700

    user 3, session 7199, part 158

--- diffs-w-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -261,8 +261,12 @@ eauto.
 Qed.
 Theorem get_len_abstr_ok :
   proc_spec
-    (fun _ state =>
+    (fun (_ : unit) state =>
      {|
      pre := True;
      post := fun r state' => state' = state /\ r = length state;
      recovered := fun _ state' => state' = state |}) get_len recover abstr.
+Proof.
+(unfold get_len; intros).
+(apply spec_abstraction_compose).
+(apply spec_weaken).
