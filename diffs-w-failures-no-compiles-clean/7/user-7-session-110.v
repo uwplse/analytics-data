12d42c15d23581b97d1f7c94df1d1a6ed5afca82
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:48 -0700

    user 7, session 110, part 12

--- diffs-w-failures-no-compiles-clean/7/user-7-session-110.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-110.v
@@ -182,4 +182,4 @@ assumption.
 (intros v0 t3 k w Hm1 Hm2).
 (apply match_ty_pair__inv in Hm1).
 (destruct Hm1 as [pv11 [pv12 [Heq [Hm11 Hmpv12]]]]; subst).
-(induction t3; try (solve [ destruct k; contradiction ])).
+(induction t3; try (solve [ destruct k, w; contradiction ])).
