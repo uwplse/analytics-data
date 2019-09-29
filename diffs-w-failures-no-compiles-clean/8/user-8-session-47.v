320066be1b0dc443a119df35cfa8d9ad91c9ad31
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:57:23 -0700

    user 8, session 47, part 39

--- diffs-w-failures-no-compiles-clean/8/user-8-session-47.v
+++ diffs-w-failures-no-compiles-clean/8/user-8-session-47.v
@@ -1136,3 +1136,6 @@ Lemma init0_end_superoperator :
 Proof.
 (intros; subst).
 (rewrite <- (kron_1_r \207\129)).
+(repeat rewrite kron_mixed_product).
+Msimpl.
+(apply (mixed_state_kron _ _ \207\129 \226\136\1630\226\159\169\226\159\1680\226\136\163)).
