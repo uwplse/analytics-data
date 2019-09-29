4de0ab062e24b58b36e650cc4ac25580038c9840
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:51:47 -0700

    user 3, session 11376, part 2

--- diffs-w-failures-no-compiles-clean/3/user-3-session-11376.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-11376.v
@@ -5,3 +5,15 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqr6MYHK"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqHxWmNa"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
