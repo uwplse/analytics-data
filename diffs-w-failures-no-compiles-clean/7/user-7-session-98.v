a1b3b48f3ebfd11780be636dd20238198a599449
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:20 -0700

    user 7, session 98, part 29

--- diffs-w-failures-no-compiles-clean/7/user-7-session-98.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-98.v
@@ -72,4 +72,3 @@ clear IHv.
 exists v.
 Lemma match_ty_exist__0_inv : forall (v : ty) (X : id) (t : ty), |-[ 0] v <$ TExist X t -> |-[ 0] v <$ t.
 Proof.
-(intros v; induction v; intros X t Hm; assumption).
