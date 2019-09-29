58fc30de9e5c5626f60b51d0a49b0c35f833f46c
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:32:59 -0700

    user 3, session 5295, part 2

--- diffs-w-failures-no-compiles-clean/3/user-3-session-5295.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-5295.v
@@ -182,4 +182,9 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
+{
+step.
+(destruct r; intuition eauto; simplify).
 }
+(destruct (lt_dec a (diskSize a'))).
+-
