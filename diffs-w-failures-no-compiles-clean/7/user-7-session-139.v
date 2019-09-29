e7a2dde59c3a2eb191eadf1e78fb8bb82c03e089
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:55:06 -0700

    user 7, session 139, part 18

--- diffs-w-failures-no-compiles-clean/7/user-7-session-139.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-139.v
@@ -57,4 +57,13 @@ Proof.
 (unfold fresh_in_ty, fresh).
 (simpl).
 Search -IdSet.empty.
-(intros Hcontra; apply IdSetFacts.empty_iff).
+Check IdSetFacts.empty_iff.
+(pose proof (IdSetFacts.empty_iff X) as H).
+tauto.
+-
+(intros t' X' Hfresh Hsem).
+(simpl in *).
+(induction t').
++
+(intros k w1).
+specialize (Hsem k w1).
