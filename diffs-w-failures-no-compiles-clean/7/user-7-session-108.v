7bf2e9960ae148fee63431e278b00137ce7d54ad
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:46 -0700

    user 7, session 108, part 113

--- diffs-w-failures-no-compiles-clean/7/user-7-session-108.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-108.v
@@ -248,10 +248,14 @@ Lemma aaa : forall (k : nat) (t t' : ty), | t | <= k -> ||-[ k][t]= [t'] -> | t 
     |- | ?t | = | ?t' | =>
         try
          (assert (Hv : value_type t) by constructor; destruct Hsem as [Hsem _]; specialize (Hsem 0); destruct Hsem as [w2 Hsem];
-           assert (Hm : |-[ 0, 0] t <$ t) by (apply match_ty_value_type__reflexive; assumption); specialize (Hsem _ Hm); 
+           assert (Hm : |-[ k, 0] t <$ t) by (apply match_ty_value_type__reflexive; assumption); specialize (Hsem _ Hm); 
            destruct w2; simpl in Hsem; contradiction);
          try
           (assert (Hv : value_type t') by constructor; destruct Hsem as [_ Hsem]; specialize (Hsem 0); destruct Hsem as [w2 Hsem];
-            assert (Hm : |-[ 0, 0] t' <$ t') by (apply match_ty_value_type__reflexive; assumption); specialize (Hsem _ Hm); 
+            assert (Hm : |-[ k, 0] t' <$ t') by (apply match_ty_value_type__reflexive; assumption); specialize (Hsem _ Hm); 
             destruct w2; simpl in Hsem; contradiction)
   end).
+-
+admit.
+-
+(destruct Hsem as [_ Hsem]).
