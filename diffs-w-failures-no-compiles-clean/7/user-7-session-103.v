b821c8b6a55e37b0d754cb1d8295f86fbb0388a8
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:54:28 -0700

    user 7, session 103, part 19

--- diffs-w-failures-no-compiles-clean/7/user-7-session-103.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-103.v
@@ -30,4 +30,5 @@ Proof.
 Qed.
 Lemma match_ty_cname__inv : forall (v : ty) (c : cname) (k w : nat), |-[ k, w] v <$ TCName c -> v = TCName c.
 (intros v c k w Hm).
-(destruct k, w, v; try contradiction).
+(destruct k, w, v; simpl in Hm).
+subst.
