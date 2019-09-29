89e2a78ce0d8e45839d3f34af0046607594066d0
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:38:55 -0700

    user 3, session 7199, part 92

--- diffs-w-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -297,24 +297,13 @@ Proof.
 (eapply diskGet_eq_values; eauto).
 auto.
 Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqypodZ6"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Qed.
+Hint Resolve get_at_ok: core.
 Theorem recover_wipe : rec_wipe recover abstr no_wipe.
 Proof.
 (unfold rec_wipe; simpl; intros).
 (apply spec_abstraction_compose).
 step_proc.
 (destruct a as [_ bs]; simpl in *; intuition eauto).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqTrrpec"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
 Qed.
 Hint Resolve recover_wipe: core.
 Theorem get_upto_ok a :
@@ -331,3 +320,6 @@ Proof.
 step_proc.
 -
 step_proc.
+step_proc.
+intuition eauto.
+lia.
