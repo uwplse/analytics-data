4e4130224b404c7481aa5c769efd40c170d8a228
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:53:49 -0700

    user 7, session 58, part 9

--- diffs-w-failures-no-compiles-clean/7/user-7-session-58.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-58.v
@@ -15,8 +15,12 @@ Open Scope btjm_scope.
 Lemma value_sem_sub_k_i_union__inv :
   forall v : ty, value_type v -> forall (k : nat) (ta tb : ty), ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
 Proof.
-(destruct (match_ty_value_type_r v Hv k) as [Hcontra| Hm]).
+(destruct (match_ty_value_type_r v Hv k) as [Hcontra| Hdep]).
 -
 (left; intros v' Hm').
 (exfalso; apply Hcontra; eauto).
 -
+(unfold sem_sub_k in Hsem).
+(assert (Hm : |-[ k] v <$ v) by (apply match_ty_value_type__reflexive; assumption)).
+specialize (Hsem _ Hm).
+(apply match_ty_i_union__inv in Hsem).
