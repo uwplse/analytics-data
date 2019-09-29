1990a14851dd96b6dfaa6cf7081f3e0bd962baa6
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:14:25 -0700

    user 1, session 41, part 63

--- diffs-w-failures-no-compiles-clean/1/user-1-session-41.v
+++ diffs-w-failures-no-compiles-clean/1/user-1-session-41.v
@@ -429,4 +429,14 @@ congruence.
   end).
 specialize (H _ H7).
 specialize (H2 _ H7).
+(repeat
+  match goal with
+  | H:exists _, _ |- _ => destruct H
+  | H:_ \/ _ |- _ => inversion H; clear H
+  end).
 congruence.
+(repeat
+  match goal with
+  | H:exists _, _ |- _ => destruct H
+  | H:_ \/ _ |- _ => inversion H; clear H
+  end).
