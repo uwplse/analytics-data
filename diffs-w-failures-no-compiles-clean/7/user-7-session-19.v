6e1c7458eed09a84866b224ade6d00f857b01748
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:52:55 -0700

    user 7, session 19, part 80

--- diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-w-failures-no-compiles-clean/7/user-7-session-19.v
@@ -64,3 +64,5 @@ specialize (Href v' Hv').
 (destruct Href; split; assumption).
 Qed.
 Lemma match_ty_i_t_le_k__v_ke_t : forall (k : nat) (t : ty), | t | <= k -> forall v : ty, |-[ k] v <$ t -> | v | <= | t |.
+Proof.
+(induction k; induction t; intros Htk v Hm).
