cdf97e3c52127262352da5dac41f26971adb9256
Talia Ringer <tringer@cs.washington.edu>
2019-09-29 11:33:51 -0700

    user 3, session 5567, part 5

--- diffs-w-failures-no-compiles-clean/3/user-3-session-5567.v
+++ diffs-w-failures-no-compiles-clean/3/user-3-session-5567.v
@@ -189,4 +189,13 @@ eauto.
 -
 simplify.
 }
-{
+step.
+(destruct r; intuition eauto; simplify).
+}
+(destruct r; step).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4tN0F0"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
