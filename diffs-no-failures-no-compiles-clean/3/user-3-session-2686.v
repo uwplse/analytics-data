3dbda4f58c80f0103dd453b83b0da6f669fd020b
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:59:51 -0700

    user 3, session 2686, part 2

--- diffs-no-failures-no-compiles-clean/3/user-3-session-2686.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-2686.v
@@ -5,35 +5,3 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-From Coq Require Import ProofIrrelevance.
-From Coq Require Export String.
-From Classes Require Import EqualDec.
-From RecordUpdate Require Import RecordUpdate.
-From stdpp Require Import decidable countable.
-From stdpp Require gmap.
-From stdpp Require Import fin_maps.
-Set Implicit Arguments.
-Instance eqdecision  `{dec : EqualDec A}: (EqDecision A) := dec.
-Definition uint64 := nat.
-Definition compare_to x y (c : comparison) :
-  {match c with
-   | Lt => x < y
-   | Gt => y < x
-   | Eq => x = y
-   end} + {match c with
-           | Lt => x >= y
-           | Gt => y >= x
-           | Eq => x <> y
-           end}.
-(destruct c).
--
-(apply Nat.eq_dec).
--
-(destruct (lt_dec x y); auto; right; abstract omega).
--
-(destruct (lt_dec y x); auto; right; abstract omega).
-Defined.
-Record FixedLengthEncoder bytes intTy byteTy
-(enc : intTy -> option (list byteTy)) (dec : list byteTy -> intTy) :={
- encode_length_ok : forall x bs, enc x = Some bs -> length bs = bytes;
- encode_decode_ok : forall x bs, enc x = Some bs -> dec bs = Some x}.
