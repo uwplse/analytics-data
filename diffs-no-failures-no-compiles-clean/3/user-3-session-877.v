8d8484a8ff03d63d98e062702cb52a7e92f38b45
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:53:39 -0700

    user 3, session 877, part 17

--- diffs-no-failures-no-compiles-clean/3/user-3-session-877.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-877.v
@@ -1369,4 +1369,5 @@ iFrame.
 (<ssreflect_plugin::ssrtclseq@0> iDestruct "Hmbox" as "[Hmbox|Hmbox]" ; last  first).
 {
 iDestruct "Hmbox" as "(Hlocked'&Hauth)".
-iDestruct "Hauth" as ( S ) "(Hauth&%)".
+iExFalso.
+iDestruct "Hlockinv" as ( S' ? ) "(Hauth'&?)".
