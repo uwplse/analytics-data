30fa0e6894bad3a2a9ebfab3001d7c72720867d7
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:50:05 -0700

    user 1, session 41, part 100

--- diffs-no-failures-no-compiles-clean/1/user-1-session-41.v
+++ diffs-no-failures-no-compiles-clean/1/user-1-session-41.v
@@ -308,8 +308,16 @@ Fixpoint size_gt (G : GT) : nat :=
       fold_right
         (fun x acc =>
          match x with
-         | Some (_, G) => size - gt G
+         | Some (_, G) => size_gt G
+         | _ => 0
+         end + acc) 1 l
+  | GRow l =>
+      fold_right
+        (fun x acc =>
+         match x with
+         | Some (Some (_, G)) => size_gt G
          | _ => 0
          end + acc) 1 l
   | _ => 0
   end.
+Module GTeq.
