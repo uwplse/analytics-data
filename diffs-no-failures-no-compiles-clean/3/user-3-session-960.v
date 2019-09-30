76ef5eab4a399b424273e06624df03d00b170587
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:54:18 -0700

    user 3, session 960, part 357

--- diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
@@ -249,4 +249,6 @@ Check repeat_length.
   end).
 (rewrite nat_from_le_zeros; auto).
 (rewrite nat_le_inverse; auto).
+(generalize dependent Datatypes.length (nat_to_le 254 x); intros).
+(exfalso; apply n1).
 lia.
