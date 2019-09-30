e4265fa8ee1c3f06a3f062c809b7dd795c4bfa97
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:54:16 -0700

    user 3, session 960, part 315

--- diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
@@ -229,8 +229,4 @@ typeclasses eauto.
 (intros; simpl).
 (decide equality; subst).
 (decide equality; subst).
-(decide equality; subst).
-(destruct x, y; simpl).
-(destruct (nat_eq_dec n n0); subst).
-left.
-Search -(existT _ _ = existT _ _) -projT2.
+typeclasses eauto.
