008aafadf497d7b9da5dd03e18439e32bc3f29cf
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:54:05 -0700

    user 3, session 960, part 115

--- diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
@@ -55,20 +55,6 @@ Inductive ty : Type :=
   | Heap : forall T : Type, _
   | Map : forall V : Type, _
   | Lock : _.
-End Ptr.
-Class GoModel : Type :={byte : Type;
-                        byte0 : byte;
-                        uint64_to_string : uint64 -> string;
-                        ascii_to_byte : Ascii.ascii -> byte;
-                        byte_to_ascii : byte -> Ascii.ascii;
-                        uint64_to_le : uint64 -> list byte;
-                        uint64_from_le : list byte -> option uint64;
-                        File : Type;
-                        nilFile : File;
-                        Ptr : Ptr.ty -> Type;
-                        nullptr : forall ty, Ptr ty}.
-Opaque Nat.modulo Nat.div.
-#[local]Obligation Tactic := (intros; simpl; subst).
 Function
  nat_to_le base (x : nat) {wf lt x} : list {x : nat | x < S (S base)} :=
    match x with
@@ -78,13 +64,3 @@ Function
        exist (fun x => x < S (S base)) digit _
        :: nat_to_le base (x / S (S base))
    end.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqzFl6bk"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqXJczSx"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
