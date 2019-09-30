92980949a8efd5b8fe2a41c2c6c98c29ec05c645
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:54:09 -0700

    user 3, session 960, part 186

--- diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
@@ -120,6 +120,8 @@ auto.
 (assert (base_m2 = S (S base_m2) - 2) by lia).
 (generalize dependent S (S base_m2); intros base **; subst).
 (assert (0 < S n) by lia).
-(generalize dependent S n; clear n; intros n **).
 (rewrite IHn).
+{
 (rewrite (PeanoNat.Nat.div_mod n base)  at 3 by lia).
+lia.
+}
