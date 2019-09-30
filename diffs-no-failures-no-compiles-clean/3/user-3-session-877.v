40b24e3a70f7a54a758549df6687600ed1a329f0
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:53:39 -0700

    user 3, session 877, part 11

--- diffs-no-failures-no-compiles-clean/3/user-3-session-877.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-877.v
@@ -1367,4 +1367,4 @@ Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 iFrame.
 (<ssreflect_plugin::ssrtclseq@0> iDestruct "Hmbox" as "[Hmbox|Hmbox]" ; last  first).
-iDestruct "Hmbox" as "(Hwlock&Hlockinv&Hunlocked)".
+iDestruct "Hauth" as ( S ) "(Hauth&%)".
