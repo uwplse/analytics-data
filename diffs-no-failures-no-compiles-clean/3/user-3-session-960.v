4e2cb750c0bc8c4e8a5d4ccfb3aac34436f2ccac
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:54:11 -0700

    user 3, session 960, part 220

--- diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
@@ -188,5 +188,6 @@ refine
  uint64_to_string := pretty.pretty_nat;
  ascii_to_byte := ascii_to_bounded;
  byte_to_ascii := bounded_to_ascii;
- uint64_to_le := nat_to_le 256;
- uint64_from_le := fun digits => Some (le_to_nat digits) |}.
+ uint64_to_le := nat_to_le 254;
+ uint64_from_le := fun digits => Some (le_to_nat digits);
+ Ptr := fun _ => nat |}.
