1f717296d4ea12ff2795d3a26422fc1b5b83a0db
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:53:59 -0700

    user 3, session 960, part 6

--- diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
@@ -79,7 +79,4 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-Instance aModel : GoModel.
-Proof.
-Print uint64.
-Print Ascii.Ascii.
+Print string.
