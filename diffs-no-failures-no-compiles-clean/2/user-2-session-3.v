0c3bd526568e42c1036392d9d28e520a4cbce764
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:50:08 -0700

    user 2, session 3, part 2

--- diffs-no-failures-no-compiles-clean/2/user-2-session-3.v
+++ diffs-no-failures-no-compiles-clean/2/user-2-session-3.v
@@ -7,4 +7,15 @@ Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Import Coq.String.String.
+Redirect "/tmp/coqBdydvw" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Require Import Coq.Strings.String.
+Redirect "/tmp/coqBL0Wgk" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Require Import Analytical.Analytics.
+Redirect "/tmp/coq6eiIGi" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Test Debug Analytics.
