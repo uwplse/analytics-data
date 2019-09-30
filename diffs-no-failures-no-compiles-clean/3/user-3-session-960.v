6537f0da27554fd2c594173984c527627932d906
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:54:05 -0700

    user 3, session 960, part 126

--- diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
@@ -66,7 +66,6 @@ Function
    | 0 => nil
    | _ =>
        let base := base_m2 in
-       let digit := x mod base in
-       exist (fun x => x < S (S base_m2)) digit _
-       :: nat_to_le base (x / base)
+       let digit := x mod S (S base_m2) in
+       exist (fun x => x < base) digit _ :: nat_to_le base (x / base)
    end.
