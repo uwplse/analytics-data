143331f548b77b762424986758cdd29c10dca0db
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:54:09 -0700

    user 3, session 960, part 197

--- diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
@@ -141,4 +141,5 @@ Set Search Output Name Only.
 Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqlBjurg"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
-Definition bounded_to_ascii (x : {x | x < 256}) : Ascii.Ascii.
+Search -Ascii.ascii.
+Print Ascii.byte_of_ascii.
