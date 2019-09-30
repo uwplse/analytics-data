2ee6a2ea8a4babc08cc1eb73ac4fdcdff658c93a
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:54:11 -0700

    user 3, session 960, part 222

--- diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-960.v
@@ -190,6 +190,6 @@ refine
  byte_to_ascii := bounded_to_ascii;
  uint64_to_le := nat_to_le 254;
  uint64_from_le := fun digits => Some (le_to_nat digits);
+ File := 0;
  Ptr := fun _ => nat;
  nullptr := fun _ => 0 |}.
-lia.
