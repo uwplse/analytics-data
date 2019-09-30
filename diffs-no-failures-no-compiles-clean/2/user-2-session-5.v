dbc66805ebeb446f9d076dc5d9caa070929adce6
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 16:50:14 -0700

    user 2, session 5, part 69

--- diffs-no-failures-no-compiles-clean/2/user-2-session-5.v
+++ diffs-no-failures-no-compiles-clean/2/user-2-session-5.v
@@ -13,3 +13,29 @@ Redirect "/tmp/coq4iI7Dj" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Open Scope string_scope.
+Inductive term :=
+  | Nil : term
+  | Ident : string -> term
+  | Symb : string -> term
+  | Cons : term -> term -> term
+  | App : term -> term -> term.
+Redirect "/tmp/coqfgPYy0" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Definition oneArgCbvPrimitive (name : string) : bool :=
+  if
+   List.find (String.eqb name)
+     ("fst" :: "snd" :: "fun" :: "arg" :: "nil?" :: "app?" :: "cons?" :: nil)
+  then true
+  else false.
+Redirect "/tmp/coqdPkGO7" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Definition primitive (name : string) : bool := String.eqb name "if" || oneArgCbvPrimitive name.
+Redirect "/tmp/coq948p4z" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Fixpoint value (t : term) : bool :=
+  match t with
+  | Nil => true
+  | Ident name => primitive name
+  | Cons a b => value a && value b
+  | App f a => false
+  end.
