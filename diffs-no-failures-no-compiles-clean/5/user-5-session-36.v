51996d9cba7e5c0112920cc04e8879df0f0acc02
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:32:53 -0700

    user 5, session 36, part 39

--- diffs-no-failures-no-compiles-clean/5/user-5-session-36.v
+++ diffs-no-failures-no-compiles-clean/5/user-5-session-36.v
@@ -266,6 +266,4 @@ clear H.
 (destruct H).
 (erewrite evalBoolConst in H0).
 (rewrite <- evalEqTrue in H0).
-(rewrite evalVar in H).
-(rewrite extendEnv_eq in H).
-(rewrite H).
+(assert (x0 = 3) by admit).
