825bba8585e96b27c25ebcd02dbfc750b0708039
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:36 -0700

    user 3, session 960, part 117

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -7,5 +7,7 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
-Theorem mod_lt : forall n m, n `mod` S m < S m.
+Theorem mod_S_lt : forall n m, n `mod` S m < S m.
 Proof.
+(apply PeanoNat.Nat.mod_upper_bound).
+(* Failed. *)
