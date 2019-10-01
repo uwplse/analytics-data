8748b26be8d2fb052468fb6d0358ddb043fa8bd6
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:22 -0700

    user 3, session 7199, part 223

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -23,8 +23,7 @@ Axiom
      forall State a recover abstr,
      proc_spec (@addr_to_block_spec State a) (addr_to_block a) recover abstr).
 Hint Resolve addr_to_block_ok: core.
-(unfold log_size_ok, log_contents_ok; intros).
-(specialize (H0 a); intuition).
-(unfold log_addr in *).
+(rewrite disk_oob_eq; simpl; auto).
+autorewrite with upd.
 lia.
 (* Failed. *)
