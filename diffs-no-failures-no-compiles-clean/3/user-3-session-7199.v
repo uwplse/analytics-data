1f5b4cb367feb758bfc11ef8191100b5c0384f70
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:17:55 -0700

    user 3, session 7199, part 430

--- diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -439,5 +439,5 @@ Lemma log_abstraction_extend :
   log_abstraction d' (bs ++ bs').
 Proof.
 (intros).
-(unfold log_abstraction; intuition).
+(unfold log_abstraction in *; intuition).
 (unfold log_length_ok; intros).
