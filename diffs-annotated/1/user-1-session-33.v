8366f9db52cdc0bdef59b3a4bc62b2b1f4996c78
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:23:20 -0700

    user 1, session 33, part 3

--- diffs-annotated/1/user-1-session-33.v
+++ diffs-annotated/1/user-1-session-33.v
@@ -2,5 +2,4 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Import Coq.Sorting.Sorted.
-(* Failed. *)
+Require Import Coq.Sorting.Sorted.
