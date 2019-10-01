9512092e8d31d79ce873f77f69e05c42997cf585
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:29 -0700

    user 7, session 19, part 151

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -110,5 +110,8 @@ Lemma match_ty_i_eq__inv_depth_eq :
              (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
                (H 0 _ Hv); destruct H as [_ H]; specialize (H Hm); contradiction)
      end ])).
-(assert (Hv : value_type (TCName c)) by constructor; assert (Hm : |-[ 0] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption);
-  specialize (H 0 _ Hv); destruct H as [H _]; specialize (H Hm)).
+-
+(assert (H1 : forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ TCName c <-> |-[ k] v <$ t'1)).
+{
+(intros k v Hv).
+specialize (H k v Hv).
