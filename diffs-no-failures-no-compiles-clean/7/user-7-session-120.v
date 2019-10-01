5d983fca72a3f6a78b4ba9fb274a10b69ce46ec3
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:35:46 -0700

    user 7, session 120, part 13

--- diffs-no-failures-no-compiles-clean/7/user-7-session-120.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-120.v
@@ -29,4 +29,4 @@ Qed.
 Lemma sem_sub_pair : forall t1 t2 t1' t2' : ty, ||- [t1]<= [t1'] -> ||- [t2]<= [t2'] -> ||- [TPair t1 t2]<= [TPair t1' t2'].
 Proof.
 (intros t1 t2 t1' t2' Hem1 Hsem2 k).
-(apply sem_sub_k_pair; auto).
+(apply sem_sub_k_pair; tauto).
