c055c77fefb2c4152dc135b3d1464965dd18c784
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:35:08 -0700

    user 7, session 99, part 3

--- diffs-no-failures-no-compiles-clean/7/user-7-session-99.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-99.v
@@ -9,6 +9,9 @@ Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Create HintDb DBBetaJulia.
+Declare Scope btjt_scope.
+Delimit Scope btjt_scope with btjt.
+Open Scope btjt.
 Inductive cname : Type :=
   | NInt : _
   | NFlt : _
@@ -32,9 +35,6 @@ Definition tX := TVar vX.
 Definition tY := TVar vY.
 Definition teXX := TExist vX tX.
 Definition tyXRefX := TExist vX (TRef tX).
-Declare Scope btjt_scope.
-Delimit Scope btjt_scope with btjt.
-Open Scope btjt.
 Reserved Notation "'[' x ':=' s ']' t" (at level 30).
 Fixpoint subst (x : id) (s t : ty) :=
   match t with
@@ -47,3 +47,29 @@ Fixpoint subst (x : id) (s t : ty) :=
   | TEV y => t
   end
 where "'[' x ':=' s ']' t" := (subst x s t) : btjt_scope.
+Inductive value_type : ty -> Prop :=
+  | VT_CName : forall cn, value_type (TCName cn)
+  | VT_Pair : forall v1 v2, value_type v1 -> value_type v2 -> value_type (TPair v1 v2)
+  | VT_Ref : forall t, value_type (TRef t)
+  | VT_EV : forall X : id, value_type (TEV X).
+Hint Constructors value_type: DBBetaJulia.
+Declare Scope btjm_scope.
+Delimit Scope btjm_scope with btjm.
+Open Scope btjm.
+Reserved Notation "'|-[' w ',' k ']' v '<$' t" (at level 40).
+Fixpoint match_ty (w : nat) :=
+  fix mtyk (k : nat) :=
+    fix mty (v : ty) :=
+      fix mty' (t : ty) :=
+        match w, k, v, t with
+        | _, _, TCName c, TCName c' => c = c'
+        | _, _, TPair v1 v2, TPair t1 t2 => mty v1 t1 /\ mty v2 t2
+        | _, _, _, TUnion t1 t2 => mty' t1 \/ mty' t2
+        | _, 0, TRef t', TRef t => True
+        | _, S k, TRef t', TRef t => forall v, |-[ w, k] v <$ t' <-> |-[ w, k] v <$ t
+        | 0, _, v, TExist _ t' => mty' t'
+        | S w, _, v, TExist X t' => exists tx, |-[ w, k] v <$ [X := tx] t'
+        | _, _, v, TVar X => TEV X
+        | _, _, _, _ => False
+        end
+where "'|-[' w ',' k ']' v '<$' t" := (match_ty w k v t) : btjm_scope.
