23ad133ae39e0fb5262d9311b0572e32a1f79f02
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:40:05 -0700

    user 8, session 125, part 201

--- diffs-no-failures-no-compiles-clean/8/user-8-session-125.v
+++ diffs-no-failures-no-compiles-clean/8/user-8-session-125.v
@@ -115,5 +115,3 @@ replace (size_wtype W1 + size_wtype W2)%nat with \226\159\166 W \226\159\167 by 
 Msimpl.
 (rewrite inv by (subst; rewrite size_wtype_length; easy)).
 Msimpl.
-(rewrite Mmult_assoc).
-easy.
