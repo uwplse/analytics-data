2e5ebeb596a72d52446f6e2cb63b4c915898c8ae
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:17:38 -0700

    user 3, session 7199, part 167

--- diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -270,4 +270,5 @@ Theorem get_len_abstr_ok :
 Proof.
 (apply spec_abstraction_compose).
 (eapply proc_spec_weaken; eauto).
-(unfold spec_impl; intuition).
+(unfold spec_impl; simpl; intuition).
+(destruct a as [[] bs]).
