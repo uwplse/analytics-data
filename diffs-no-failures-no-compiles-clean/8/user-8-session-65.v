6bdacfe1c7a4c32de822dddacb2f657844a3341f
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:39:09 -0700

    user 8, session 65, part 162

--- diffs-no-failures-no-compiles-clean/8/user-8-session-65.v
+++ diffs-no-failures-no-compiles-clean/8/user-8-session-65.v
@@ -982,4 +982,11 @@ Msimpl.
 mat_replace \226\159\1680\226\136\163 \195\151 \226\136\1631\226\159\169 with @Zero 1 1 by lma.
 mat_replace \226\159\1681\226\136\163 \195\151 \226\136\1631\226\159\169 with I 1 by lma.
 Msimpl.
+restore_dims.
 (rewrite id_kron' by (apply Nat.pow_nonzero; lia)).
+Msimpl.
+reflexivity.
++
+mat_replace \226\159\1680\226\136\163 \195\151 \226\136\1631\226\159\169 with @Zero 1 1 by lma.
+mat_replace \226\159\1680\226\136\163 \195\151 \226\136\1630\226\159\169 with I 1 by lma.
+Msimpl.
