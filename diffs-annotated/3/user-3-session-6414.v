283add4a9e113e536e6e3448ca1d39a02c415bdf
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:45:51 -0700

    user 3, session 6414, part 16

--- diffs-annotated/3/user-3-session-6414.v
+++ diffs-annotated/3/user-3-session-6414.v
@@ -19,5 +19,14 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-Definition init : proc InitResult := _ <- d.write len_addr 0; Ret Initialized.
-(* Failed. *)
+Definition init : proc InitResult :=
+  len0 <- addr_to_block 0; _ <- d.write len_addr len0; Ret Initialized.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqfMzGCk"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqnP9GMK"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
