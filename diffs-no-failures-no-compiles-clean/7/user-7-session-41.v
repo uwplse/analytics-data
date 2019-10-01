5b1e916fe55c9b894ebad2cecdc2a5028af0b814
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:34:16 -0700

    user 7, session 41, part 13

--- diffs-no-failures-no-compiles-clean/7/user-7-session-41.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-41.v
@@ -2,28 +2,3 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0250a.BaseDefs.
-Require Import BetaJulia.Sub0250a.BaseProps.
-Require Import BetaJulia.Sub0250a.MatchProps.
-Require Import BetaJulia.Sub0250a.AltMatchDef.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Open Scope btjm.
-Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
-Proof.
-(intros t1 t2 Hsub).
-(unfold sem_sub).
-(induction Hsub; intros k v Hm).
--
-assumption.
--
-(unfold sem_sub_k in *).
-auto.
--
-(apply match_ty_pair__inv in Hm).
-(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-auto using match_ty_pair.
