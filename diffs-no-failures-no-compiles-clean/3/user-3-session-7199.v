b7d9c6b4e6915a1b6d8d1e278778afeed69b38d7
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:17:32 -0700

    user 3, session 7199, part 54

--- diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -199,6 +199,5 @@ Proof.
 (destruct a0 as [_ bs]; simpl in *; intuition eauto).
 (descend; intuition eauto).
 (descend; intuition eauto).
-(unfold log_abstraction in H0; intuition).
-(apply H1 in H).
+(pose proof (H1 a); intuition).
 eq_values.
