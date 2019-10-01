fdf47e4281486ec4a226024cc1309e615a588271
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:38:03 -0700

    user 8, session 31, part 32

--- diffs-no-failures-no-compiles-clean/8/user-8-session-31.v
+++ diffs-no-failures-no-compiles-clean/8/user-8-session-31.v
@@ -2812,3 +2812,11 @@ replace (size_ctx \206\147) with size_octx \206\147 by easy.
 (rewrite size_octx_merge by easy).
 dependent destruction p1.
 dependent destruction t.
+(apply singleton_equiv in s; subst).
+(simpl).
+(rewrite singleton_size).
+(simpl).
+(rewrite Nat.add_succ_r).
+specialize (apply_U_correct Qubit) as AUC.
+(simpl in AUC).
+(unfold process_gate_state).
