381d6ea585fa9cfb0808eedea6f1a6566aa29bbb
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:56:09 -0700

    user 3, session 9876, part 1

--- diffs-annotated/3/user-3-session-9876.v
+++ diffs-annotated/3/user-3-session-9876.v
@@ -129,4 +129,15 @@ Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 Qed.
+Theorem maybe_eq_None_holds T (v : T) : maybe_eq None v.
+Proof.
+exact I.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqAJFbNU"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
 Hint Rewrite maybe_eq_None_is_True : upd.
+Hint Resolve maybe_eq_None_holds: core.
