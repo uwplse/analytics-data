9537deaa21e9d5165079959d430e6356c319098a
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:32:11 -0700

    user 3, session 11376, part 1

--- diffs-no-failures-no-compiles-clean/3/user-3-session-11376.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-11376.v
@@ -5,4 +5,3 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Export LF.Basics.
