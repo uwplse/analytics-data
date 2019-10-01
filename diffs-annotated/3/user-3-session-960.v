570d93969eab2b574c4275c7af911547895e2ec6
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:42 -0700

    user 3, session 960, part 208

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -9,5 +9,16 @@ From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Check mod_S_lt.
 Definition bounded_to_ascii (x : {x | x < 256}) : Ascii.ascii :=
-  Ascii.nat_of_ascii (proj1_sig x).
-(* Failed. *)
+  Ascii.ascii_of_nat (proj1_sig x).
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqjMbEGo"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq7rBVPP"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Definition ascii_to_bounded (a : Ascii.ascii) : {x | x < 256}.
+refine (exist _ (Ascii.nat_of_ascii a) _).
+Search -Ascii.nat_of_ascii.
