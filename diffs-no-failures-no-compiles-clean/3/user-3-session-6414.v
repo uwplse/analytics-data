2ce5f844f91844d04f8b512b38071b47da5670fd
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:14:36 -0700

    user 3, session 6414, part 78

--- diffs-no-failures-no-compiles-clean/3/user-3-session-6414.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-6414.v
@@ -35,4 +35,3 @@ Axiom
 Notation "d [ a |-> b ]" := (diskUpd d a b) (at level 8, left associativity).
 Notation "d [ a |=> bs ]" := (diskUpds d a bs)
   (at level 8, left associativity).
-Opaque diskGet.
