28a2b9b5f239f1720f62fceaf99f30f209f8a0bd
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:35:12 -0700

    user 7, session 102, part 3

--- diffs-no-failures-no-compiles-clean/7/user-7-session-102.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-102.v
@@ -70,7 +70,7 @@ Fixpoint match_ty (k : nat) :=
             (forall w1, exists w2, forall v, |-[ k, w1] v <$ t' -> |-[ k, w2] v <$ t) /\
             (forall w1, exists w2, forall v, |-[ k, w1] v <$ t -> |-[ k, w2] v <$ t')
         | _, 0, v, TExist _ t' => mty' t'
-        | _, S w, v, TExist X t' => exists tx, mtyw w v ([X := tx] t')
+        | _, S w', v, TExist X t' => exists tx, mtyw w' v ([X := tx] t')
         | _, _, TEV X, TVar X' => X = X'
         | _, _, TEV X, TEV X' => X = X'
         | _, _, _, _ => False
