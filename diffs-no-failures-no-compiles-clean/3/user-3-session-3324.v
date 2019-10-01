2c1e7da93f5e9de2d85c1d4b127c930ae77283f2
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:02:36 -0700

    user 3, session 3324, part 30

--- diffs-no-failures-no-compiles-clean/3/user-3-session-3324.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-3324.v
@@ -145,4 +145,4 @@ exists (diskUpd (diskShrink (stateDisk state)) (stateBadBlock state) b).
 (unfold inited_any; intuition idtac).
 (constructor; intuition idtac; auto; intros; autorewrite with upd in *;
   intuition idtac).
-(rewrite diskUpd_neq by omega).
+all: (rewrite diskUpd_neq by omega).
