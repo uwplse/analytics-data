8370ac5485ce33cfa6da2155d6f02c0475443cb7
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:29 -0700

    user 7, session 19, part 146

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -101,4 +101,10 @@ constructor.
 tauto.
 Lemma match_ty_i_eq__inv_depth_eq :
   forall t t' : ty, (forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
-(induction t; induction t'; intros H; try reflexivity).
+(induction t; induction t'; intros H; try reflexivity;
+  try (solve
+   [ match goal with
+     | |- | ?t1 | = | ?t2 | =>
+           assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+            (H 0 _ Hv); destruct H as [H _]; specialize (H Hm); contradiction
+     end ])).
