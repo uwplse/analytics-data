66bb26508a4392985abe46a0a69631bb618cc7fe
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:37:07 -0700

    user 7, session 206, part 12

--- diffs-no-failures-no-compiles-clean/7/user-7-session-206.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-206.v
@@ -51,4 +51,4 @@ Search -IdSet.In.
 (pose proof IdSetFacts.In_m).
 Search -Morphisms.Proper.
 Search -Morphisms.respectful.
-(destruct (IdSetFacts.empty_iff Y) as [H _]).
+(destruct (IdSetFacts.empty_iff X) as [H _]).
