133a4b03ac40841e20f285b6443775d0ff2af426
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:19 -0700

    user 3, session 7199, part 187

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -58,9 +58,15 @@ Theorem append_at_ok a bs' :
             log_size_ok state (bs ++ bs') /\ log_contents_ok bs state;
      post := fun r state' =>
              diskGet state' len_addr = diskGet state len_addr /\
+             diskSize state' = diskSize state /\
              log_size_ok (bs ++ bs') state' /\
              log_contents_ok state (bs ++ bs');
      recovered := fun _ state' =>
                   diskGet state' len_addr = diskGet state len_addr /\
+                  diskSize state' = diskSize state /\
                   log_contents_ok bs state' |}) (append_at a bs') recover
     d.abstr.
+Proof.
+Print append_at.
+(induction bs; simpl).
+(* Failed. *)
