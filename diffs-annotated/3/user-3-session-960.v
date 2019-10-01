b272dd7c3152b67d5037897c7bc4cf3ee01cb985
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:45 -0700

    user 3, session 960, part 253

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -9,4 +9,6 @@ From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Check mod_S_lt.
 (destruct c; simpl).
-(unfold ascii_to_bounded, bounded_to_ascii).
+(unfold ascii_to_bounded, bounded_to_ascii; simpl).
+(rewrite Ascii.nat_ascii_embedding).
+(* Failed. *)
