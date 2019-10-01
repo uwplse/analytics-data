90278f1879c3f15304217a95c700fc3ebe053286
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:39:06 -0700

    user 8, session 65, part 117

--- diffs-no-failures-no-compiles-clean/8/user-8-session-65.v
+++ diffs-no-failures-no-compiles-clean/8/user-8-session-65.v
@@ -972,4 +972,6 @@ gen \207\129.
 (rewrite Nat.pow_add_r, <- (Nat.mul_1_r (2 ^ i))).
 (intros \207\129).
 restore_dims try rewrite size_ntensor; unify_pows_two; simpl; try lia.
+(destruct b; simpl; Msimpl).
+(repeat rewrite Mmult_assoc).
 Msimpl.
