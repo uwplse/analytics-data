20efdeccc6497efaca409c374c32e21cd16d2ed5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:45:51 -0700

    user 3, session 6414, part 17

--- diffs-annotated/3/user-3-session-6414.v
+++ diffs-annotated/3/user-3-session-6414.v
@@ -21,12 +21,22 @@ Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 Definition init : proc InitResult :=
   len0 <- addr_to_block 0; _ <- d.write len_addr len0; Ret Initialized.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqfMzGCk"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqbVHk3U"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqnP9GMK"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqGWjpqP"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Definition get_len : proc addr := b <- d.read len_addr; Ret (block_to_addr b).
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqUkAut4"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqee1QKb"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
