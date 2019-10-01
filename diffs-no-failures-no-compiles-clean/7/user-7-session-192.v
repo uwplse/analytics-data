4b2831882f02cfc418b356aae4c859c130a9c731
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:36:53 -0700

    user 7, session 192, part 49

--- diffs-no-failures-no-compiles-clean/7/user-7-session-192.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-192.v
@@ -89,4 +89,7 @@ specialize (IHw' _ Hm').
 (destruct IHw' as [IHw'a IHw'b]).
 (destruct (either_free_or_fresh_in_ty X' ([i := ti] t'))).
 *
-specialize (IHw'a _ H).
+specialize (IHw'b H).
+(split; intros HX').
+{
+exists ti.
