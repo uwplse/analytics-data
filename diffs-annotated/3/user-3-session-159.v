668285c7486bbbf36760d27b24981735b030394a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:24:12 -0700

    user 3, session 159, part 7

--- diffs-annotated/3/user-3-session-159.v
+++ diffs-annotated/3/user-3-session-159.v
@@ -6,20 +6,6 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Require Import Examples.StatDb.Impl.
-(compute; auto).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqxEAe8Z"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Qed.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqFpyRJH"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4r5Pe6"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+Instance absr_non_error : (NonError absr).
+Proof.
+(compute).
