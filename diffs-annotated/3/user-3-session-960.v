a92a145d38db2eb9982ca43d060904f54039708a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:40 -0700

    user 3, session 960, part 181

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -8,3 +8,34 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Check mod_S_lt.
+Fixpoint le_to_nat base_m2 (digits : list {x : nat | x < S (S base_m2)}) :
+nat :=
+  match digits with
+  | nil => 0
+  | digit :: digits' => proj1_sig digit * S (S base_m2) + le_to_nat digits'
+  end.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqfYQaxE"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqkp7dgq"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Theorem nat_le_inverse base_m2 :
+  forall n, le_to_nat (nat_to_le base_m2 n) = n.
+Proof.
+(intros).
+(induction n as [n IHn] using lt_wf_ind).
+(destruct n; rewrite nat_to_le_equation; simpl).
+-
+auto.
+-
+(assert (1 < S (S base_m2)) by lia).
+(assert (base_m2 = S (S base_m2) - 2) by lia).
+(generalize dependent S (S base_m2); intros base **; subst).
+(assert (0 < S n) by lia).
+(generalize dependent S n; clear n; intros n **).
+(rewrite IHn).
+Search -"mod" -div.
