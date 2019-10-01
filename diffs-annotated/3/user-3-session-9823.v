cc5c269f40a3fdf79092847998d7612eccf5bcd5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:55:59 -0700

    user 3, session 9823, part 8

--- diffs-annotated/3/user-3-session-9823.v
+++ diffs-annotated/3/user-3-session-9823.v
@@ -8,36 +8,6 @@ Add Search Blacklist "Private_" "_subproof".
 Require Import POCS.
 Require Import OneDiskAPI.
 Require Import BadBlockAPI.
-(constructor; intuition; autorewrite with upd in *; intuition).
--
-(rewrite diskShrink_preserves; auto).
-(rewrite diskShrink_size; lia).
--
-(rewrite diskUpd_eq; auto).
-(rewrite diskShrink_size; lia).
--
-lia.
-}
-{
-(exfalso; eapply disk_inbounds_not_none; [  | eauto ]; lia).
-}
-+
-step_proc.
-Qed.
-Theorem read_ok :
-  forall a, proc_spec (OneDiskAPI.read_spec a) (read a) recover abstr.
-Proof.
-(unfold read).
-(intros).
-(apply spec_abstraction_compose; simpl).
-(step_proc; intros).
-(destruct a'; simpl in *; intuition).
-{
-eauto.
-}
+(destruct a'; simpl in *; intuition eauto).
 (destruct (a == r)).
 -
-invert_abstraction.
-(step_proc; intuition).
-{
-eauto.
