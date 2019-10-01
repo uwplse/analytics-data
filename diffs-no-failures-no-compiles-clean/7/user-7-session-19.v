893525f1b377f9be2af752cb8b38d7014e606713
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:28 -0700

    user 7, session 19, part 129

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -102,6 +102,4 @@ tauto.
 Qed.
 Lemma match_ty_i_eq__inv_depth_eq :
   forall t t' : ty, (forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
-Proof.
 (induction t; induction t'; intros H).
-reflexivity.
