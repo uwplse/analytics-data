d719aefeedb68aab88fa1f4f09b59bb133fffe67
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:39 -0700

    user 7, session 19, part 336

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -163,4 +163,4 @@ Proof.
                (H _ Hm); contradiction)
      end ])).
 (assert (Hv : value_type (TCName c)) by constructor).
-(pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ H)).
+(pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ H)).
