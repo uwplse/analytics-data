18bc3f3687a251a6a766fa58aa85334bfbf3500e
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:08:22 -0700

    user 3, session 4740, part 29

--- diffs-no-failures-no-compiles-clean/3/user-3-session-4740.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-4740.v
@@ -41,5 +41,3 @@ Redirect
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Check 1 + 2 + aReallyBigIdentifier + 4 + anotherLongName
-  === 6 + anotherLongName + 8 + 9.
