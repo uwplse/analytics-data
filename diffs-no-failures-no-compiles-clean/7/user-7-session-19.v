79072a1cac17709b25b1706d59ac5b6e0524a642
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:57 -0700

    user 7, session 19, part 656

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -493,6 +493,4 @@ auto using match_ty_i_pair.
 (apply match_ty_i_ref__inv in Hm).
 (destruct Hm as [tx [Heq Href]]; subst).
 (simpl).
-(intros v; split; intros Hm; specialize (Href v)).
-specialize (IHHsub2 k _ Hm).
-tauto.
+(intros v; split; intros Hm; specialize (Href v); [ specialize (IHHsub2 k _ Hm) | specialize (IHHsub1 k _ Hm) ]).
