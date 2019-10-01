b86df364145d9f128b860869db8bf9b981b0cba4
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:17:31 -0700

    user 3, session 7199, part 39

--- diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -162,4 +162,4 @@ Check proc_spec_weaken.
 (destruct a0 as [_ bs]; simpl in *; intuition eauto).
 (descend; intuition eauto).
 (descend; intuition eauto).
-autorewrite with disk.
+autorewrite with upd in *.
