3072171436f09c14b8da028e70a0946390d44603
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:13 -0700

    user 3, session 7199, part 74

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -71,4 +71,5 @@ Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 Definition log_length_ok (d : disk) (log : list block) :=
   forall b, diskGet d 0 =?= b -> block_to_addr b = length log.
-(destruct d; simpl in *).
+(destruct d; simpl in *; [  | lia ]).
+Search -diskGet -"inbounds".
