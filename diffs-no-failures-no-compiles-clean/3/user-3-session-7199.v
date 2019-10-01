f740c4bbefbcc3c857452bdb5ea200e23536dab8
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:17:37 -0700

    user 3, session 7199, part 142

--- diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -373,7 +373,3 @@ Theorem append_ok :
   forall v, proc_spec (append_spec v) (append v) recover abstr.
 Proof.
 (unfold append; intros).
-(apply spec_abstraction_compose).
-step_proc.
-(destruct a' as [[] bs]; simpl in *).
-intuition euto.
