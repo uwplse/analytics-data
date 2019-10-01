01de748f8b08081fef75c7ec2dc81239f723d415
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:35:31 -0700

    user 7, session 108, part 74

--- diffs-no-failures-no-compiles-clean/7/user-7-session-108.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-108.v
@@ -295,3 +295,12 @@ admit.
 (assert (Hnotm' : ~ (exists v tx, |-[ k, w] v <$ [i := tx] t))).
 {
 (intros [v [tx Hm]]).
+(assert (Hme : |-[ k, S w] v <$ TExist i t)).
+{
+(apply match_ty_exist).
+exists tx.
+assumption.
+}
+(apply Hnotm).
+exists v.
+assuption.
