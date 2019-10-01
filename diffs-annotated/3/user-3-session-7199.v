2af6fd4c5fb14127317ce891a9b82b0e303553c0
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:09 -0700

    user 3, session 7199, part 4

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -143,9 +143,4 @@ step_proc.
 intuition eauto.
 (eexists; intuition eauto).
 Qed.
-Hint Resolve get_len_ok: core.
-Theorem get_ok : proc_spec get_spec get recover abstr.
-Proof.
-(unfold get; intros).
-(apply spec_abstraction_compose; simpl).
 step_proc.
