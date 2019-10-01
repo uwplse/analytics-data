8d46371ea114be7c86a9ff4df9843339390a605f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:22 -0700

    user 3, session 7199, part 227

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -23,5 +23,5 @@ Axiom
      forall State a recover abstr,
      proc_spec (@addr_to_block_spec State a) (addr_to_block a) recover abstr).
 Hint Resolve addr_to_block_ok: core.
-(rewrite diskUpd_oob_eq).
-(* Failed. *)
+Check diskUpd_oob_eq.
+(rewrite diskUpd_oob_noop).
