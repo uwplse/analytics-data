a1909f58b43c2f6dff2ebe17b084496ae03ead7f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:23:40 -0700

    user 2, session 5, part 42

--- diffs-annotated/2/user-2-session-5.v
+++ diffs-annotated/2/user-2-session-5.v
@@ -13,6 +13,19 @@ Redirect "/tmp/coq4iI7Dj" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Definition primitive (name : string) : bool :=
-  List.find (String.eqb name)
-    ("if" :: "fst" :: "snd" :: "fun" :: "arg" :: "nil?" :: "app?" :: "cons?" :: nil).
-(* Failed. *)
+  if
+   List.find (String.eqb name)
+     ("if" :: "fst" :: "snd" :: "fun" :: "arg" :: "nil?" :: "app?" :: "cons?" :: nil)
+  then true
+  else false.
+Redirect "/tmp/coqgkuM3j" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Fixpoint value (t : term) : bool :=
+  match t with
+  | Nil => true
+  | Ident name => primitive name
+  | Cons a b => value a && value b
+  | App f a => false
+  end.
+Redirect "/tmp/coqSxNVO1" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
