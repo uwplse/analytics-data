19f051e2586d96b7734ebbbebc5a4a74b18dbbc4
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:40:31 -0700

    user 10, session 10, part 11

--- diffs-no-failures-no-compiles-clean/10/user-10-session-10.v
+++ diffs-no-failures-no-compiles-clean/10/user-10-session-10.v
@@ -319,5 +319,9 @@ Check nmi_of_smi.
 Check nmi_of_smi.
 Check network_of_app.
 Print sE.
+Check interp.
+Print interp.
+Check interp.
+Print interp.
 Definition server : itree sE void :=
   sk <- translate subevent serverHandshake;; interp (network_of_app sk) (nmi_of_smi kvs).
