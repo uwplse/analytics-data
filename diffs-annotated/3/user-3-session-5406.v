e0e472eca17b3754ed838a2a514126ac0f24c515
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:42:47 -0700

    user 3, session 5406, part 5

--- diffs-annotated/3/user-3-session-5406.v
+++ diffs-annotated/3/user-3-session-5406.v
@@ -94,9 +94,17 @@ inv_exec.
  | Hexec:exec (rx _) _ _
    |- _ => eapply RExec in Hexec; eapply H4 in Hexec; eauto
  end).
--
-inv_exec.
+(match goal with
+ | Hexec:exec (rx _) _ _
+   |- _ => eapply RExecCrash in Hexec; eauto; eapply H4 in Hexec; eauto
+ end).
 +
+(assert (Hexec : exec p w' (Crashed w')) by (constructor; eauto)).
+(eapply RExecCrash in Hexec; eauto).
+(eapply H0 in H2; repeat deex).
+(eapply H in Hexec; simpl in *; safe_intuition repeat deex; eauto).
++
+inv_exec.
 (match goal with
  | Hexec:exec p _ _ |- _ => eapply RExec in Hexec
  end).
@@ -104,6 +112,8 @@ inv_exec.
 (eapply H in H10; simpl in *; safe_intuition repeat deex; eauto).
 (match goal with
  | Hexec:exec (rx _) _ _
-   |- _ => eapply RExecCrash in Hexec; eauto; eapply H3 in Hexec; eauto
+   |- _ =>
+       apply ExecCrashEnd in Hexec; eapply RExecCrash in Hexec; eauto;
+        eapply H3 in Hexec; eauto
  end).
 (* Failed. *)
