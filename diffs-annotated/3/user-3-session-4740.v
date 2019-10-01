b0b390d4998922430e3ad53375bcac19faf3434e
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:40:28 -0700

    user 3, session 4740, part 1

--- diffs-annotated/3/user-3-session-4740.v
+++ diffs-annotated/3/user-3-session-4740.v
@@ -15,5 +15,4 @@ Timeout 1 Print LoadPath.
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Notation "a \226\137\161 b" := (a = b).
-(* Auto-generated comment: Failed. *)
+Notation "a \226\137\161 b" := (a = b) (at level 30).
