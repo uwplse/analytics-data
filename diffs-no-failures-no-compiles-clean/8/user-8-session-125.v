5c75b6b2fe4b197e92972af53fd3c02e21119cd6
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:39:52 -0700

    user 8, session 125, part 11

--- diffs-no-failures-no-compiles-clean/8/user-8-session-125.v
+++ diffs-no-failures-no-compiles-clean/8/user-8-session-125.v
@@ -37,4 +37,8 @@ Timeout 1 About unitary_gate_unitary.
 Timeout 1 Print unitary_gate_unitary.
 (simpl in *).
 matrix_denote.
-(rewrite denote_unitary_transpose).
+setoid_rewrite denote_unitary_transpose.
+(simpl in *; Msimpl).
+(repeat rewrite Mmult_assoc; try rewrite denote_unitary_unitary).
+Timeout 1 About denote_unitary_unitary.
+Timeout 1 Print denote_unitary_unitary.
