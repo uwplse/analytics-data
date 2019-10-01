1117810cecd99b21583891673651ebf48412db81
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:17:44 -0700

    user 3, session 7199, part 261

--- diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -341,4 +341,8 @@ step_proc.
 (eapply proc_spec_weaken; eauto).
 (unfold spec_impl; simpl; intuition).
 (exists (a' ++ [a]); intuition eauto).
--
++
+(rewrite app_length; simpl; lia).
++
+(unfold log_size_ok in *; simpl in *).
+(rewrite app_length in *).
