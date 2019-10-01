840dc3462cec3beefe4eba2980e7f77217b045fb
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:32 -0700

    user 3, session 960, part 40

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -7,16 +7,10 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
-Opaque Nat.modulo Nat.div.
-#[program]
-Fixpoint nat_to_le base (x : nat) {measure x : list {x : nat | x < S base} :=
-  match x with
-  | 0 => nil
-  | _ => exist _ (x mod S base) _ :: nat_to_le base (x / S base)
-  end.
-Next Obligation.
-Proof.
-(intros).
-(simpl).
-Search -Nat.modulo -"<".
-(apply PeanoNat.Nat.mod_upper_bound).
+(apply PeanoNat.Nat.mod_upper_bound; auto).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqPcKfTK"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
