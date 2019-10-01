5cca7e999b52a0a3728510f71450591aa69e0f8d
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:27 -0700

    user 3, session 7199, part 294

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -23,18 +23,19 @@ Axiom
      forall State a recover abstr,
      proc_spec (@addr_to_block_spec State a) (addr_to_block a) recover abstr).
 Hint Resolve addr_to_block_ok: core.
-Theorem log_length_ok_nil d b :
-  diskGet d len_addr = Some b -> block_to_addr b = 0 -> log_length_ok d nil.
-Proof.
-(unfold log_length_ok; intros).
-(rewrite H in *; simpl in *; subst).
-auto.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqH0Mv0e"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Qed.
 Theorem log_abstraction_nil d b :
-  diskGet d 0 = Some b -> block_to_addr b = 0 -> log_abstraction d nil.
+  diskGet d len_addr = Some b -> block_to_addr b = 0 -> log_abstraction d nil.
+Proof.
+(unfold log_abstraction; intros).
+intuition.
+-
+eauto using log_length_ok_nil.
+-
+(unfold log_size_ok).
+(destruct d; simpl in *; [  | lia ]).
+(assert (diskGet nil 0 = None)).
+{
+(apply disk_oob_eq).
+(simpl; lia).
+}
+congruence.
