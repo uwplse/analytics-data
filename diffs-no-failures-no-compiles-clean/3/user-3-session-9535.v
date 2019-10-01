9d41c7bbdbcbadc8860d52128ca90a67591aba28
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:25:20 -0700

    user 3, session 9535, part 2

--- diffs-no-failures-no-compiles-clean/3/user-3-session-9535.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-9535.v
@@ -5,4 +5,15 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Import POCS.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqhZ9e7G"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqUgLLfY"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
