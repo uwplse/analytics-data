eda776170cdc9dabc91bb7cbd9af85b0a04e09d7
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:27 -0700

    user 7, session 19, part 106

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -64,3 +64,10 @@ reflexivity.
 specialize (Href v' Hv').
 (destruct Href; split; assumption).
 Lemma match_ty_i_eq__inv_depth_eq : forall t t' : ty, forall (k : nat) (v : ty), |-[ k] v <$ t <-> |-[ k] v <$ t' -> | t | = | t' |.
+Proof.
+(induction t; induction t'; intros k v Hsemeq).
+reflexivity.
+admit.
+admit.
+admit.
+admit.
