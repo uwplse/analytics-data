4ad28ef4cf9847987d696972eb04a18beccf733c
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:42:27 -0700

    user 3, session 5295, part 6

--- diffs-annotated/3/user-3-session-5295.v
+++ diffs-annotated/3/user-3-session-5295.v
@@ -215,8 +215,4 @@ Definition equal_after a (d_0 d_1 : disk) :=
 Theorem le_eq_or_S_le : forall n m, n <= m -> n = m \/ S n <= m /\ n <> m.
 Proof.
 (intros).
--
-(autorewrite with upd; eauto).
--
 (apply le_eq_or_S_le in H1; intuition subst).
-(destruct (lt_dec a' (diskSize d_0)); autorewrite with upd).
