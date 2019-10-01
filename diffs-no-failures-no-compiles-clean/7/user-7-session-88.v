6cddb2ba8a79b33507d51417e68018d51297d02e
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:34:47 -0700

    user 7, session 88, part 4

--- diffs-no-failures-no-compiles-clean/7/user-7-session-88.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-88.v
@@ -551,4 +551,3 @@ clear IHk' IHt.
 (apply le_S_n in Htk').
 (split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v; pose proof (Href v) as Hrefv).
 *
-(assert (Hdepeq : | t' | = | t |) by apply (sem_eq_k_i__inv_depth_eq_2 _ _ _ Htk Href)).
