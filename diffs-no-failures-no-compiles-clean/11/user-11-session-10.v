ab90ae31f7c6555d31eb484dbd713c29809e3a80
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:40:41 -0700

    user 11, session 10, part 3

--- diffs-no-failures-no-compiles-clean/11/user-11-session-10.v
+++ diffs-no-failures-no-compiles-clean/11/user-11-session-10.v
@@ -15,15 +15,6 @@ Instance eqKvs_data  (x y : kvs_data id): (Dec (x = y)).
 Proof.
 dec_eq.
 Defined.
-Instance showData : (Show (kvs_data id)) :=
- {|
- show := fun d =>
-         match d with
-         | Kvs_GET k => "GET " ++ show k
-         | Kvs_PUT k v => "PUT " ++ show k ++ " " ++ show v
-         | Kvs_CAS k x v => "CAS " ++ show k ++ " " ++ show x ++ " " ++ show v
-         | Kvs_OK v => "OK  " ++ show (v : N)
-         | Kvs_NoContent => "204"
-         | Kvs_BadRequest => "400"
-         | Kvs_PreconditionFailed => "412"
-         end |}.
+Redirect "/tmp/coq16819xPZ" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
