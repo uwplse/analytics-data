4daeb533fb81d42572ea80a9493035c4b5ab6879
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:35:30 -0700

    user 7, session 108, part 68

--- diffs-no-failures-no-compiles-clean/7/user-7-session-108.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-108.v
@@ -303,4 +303,17 @@ subst.
 admit.
 }
 Search -beq_id_refl.
-(rewrite Heq).
+(rewrite Heq in Hcontra).
+(apply Hnotm).
+exists v.
+assumption.
++
+(simpl in Hcontra).
+(assert (Heq : beq_id X i = false)).
+{
+admit.
+}
+(rewrite Heq in Hcontra).
+(apply match_ty_exist__inv in Hcontra).
+(destruct Hcontra as [tx Hcontra]).
+(apply Hnotm).
