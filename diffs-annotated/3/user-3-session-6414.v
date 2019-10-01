56b6566b030f2230ec9e67921fbe03cf66b4d6d3
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:45:50 -0700

    user 3, session 6414, part 4

--- diffs-annotated/3/user-3-session-6414.v
+++ diffs-annotated/3/user-3-session-6414.v
@@ -19,40 +19,24 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-Axiom (addr_to_block : addr -> proc block).
-Axiom (block_to_addr : block -> addr).
-Definition addr_to_block_spec State a :
-  Specification unit block unit State :=
-  fun (_ : unit) state =>
-  {|
-  pre := True;
-  post := fun r state' => state' = state /\ block_to_addr r = a;
-  recovered := fun _ state' => state' = state |}.
-Axiom
-  (addr_to_block_ok :
-     forall State a recover abstr,
-     proc_spec (@addr_to_block_spec State a) (addr_to_block a) recover abstr).
-Hint Resolve addr_to_block_ok: core.
-Module Log (d: OneDiskAPI)<: LogAPI.
 Definition init : proc InitResult.
 Admitted.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqsDhzFq"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqmkseDM"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
 Definition get : proc (list block).
 Admitted.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqAhArPI"
+Definition append : list block -> proc bool.
+Admitted.
+Definition reset : proc unit.
+Admitted.
+Definition recover : proc unit.
+Admitted.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqX06zjQ"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqxmYXnB"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq1ktiSB"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
+Definition abstr : Abstraction State := True.
+(* Failed. *)
