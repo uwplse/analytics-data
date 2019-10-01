4bad59f8e8acb1d08da6b4b4908cfd84e2fdb58d
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:34:15 -0700

    user 7, session 41, part 1

--- diffs-no-failures-no-compiles-clean/7/user-7-session-41.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-41.v
@@ -11,4 +11,3 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
