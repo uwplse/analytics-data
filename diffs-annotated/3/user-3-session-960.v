8f33985a7e1e51fa00834ec4d49af03226344b99
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:40 -0700

    user 3, session 960, part 174

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -10,6 +10,4 @@ From Coq Require Export String.
 Check mod_S_lt.
 Check PeanoNat.Nat.mod_small.
 Check PeanoNat.Nat.mod_le.
-(rewrite IHn).
-Search -"mod" -div.
-(rewrite (PeanoNat.Nat.div_mod n base)).
+(rewrite (PeanoNat.Nat.div_mod n base)  at 3).
