30c63e19ea5abcb8210ca2972dce7c83f22385a2
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:34:51 -0700

    user 7, session 88, part 65

--- diffs-no-failures-no-compiles-clean/7/user-7-session-88.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-88.v
@@ -410,10 +410,8 @@ specialize (Hsem _ Hm).
 (apply IHk; try assumption).
 (destruct Hdep as [Hdep| Hdep]; [ left | right ]; apply le_S_n; assumption).
 (intros v').
-specialize (Hsem v').
-tauto.
-Qed.
 Lemma sem_sub_k_i_nf__inv_depth_le_1 : forall (k : nat) (t t' : ty), InNF( t) -> | t | <= k -> ||-[ k][t]<= [t'] -> | t | <= | t' |.
+Proof.
 (intros).
 (apply sem_sub_k_i_nf__inv_depth_le with k; tauto).
 Qed.
