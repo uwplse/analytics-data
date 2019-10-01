9989f0b355606e022ac21e35bc281606f8f5d717
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:36:44 -0700

    user 7, session 184, part 18

--- diffs-no-failures-no-compiles-clean/7/user-7-session-184.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-184.v
@@ -74,5 +74,9 @@ specialize (IHt2 _ _ Hm2).
 exists (TPair v1' v2').
 split.
 (apply match_ty_pair; assumption).
-(induction w'; induction t'; intros Hm'; try contradiction).
+(induction w'; induction t'; intros HX' Hm'; try contradiction).
 +
+(apply match_ty_pair_pair__inv in Hm').
+(destruct Hm' as [Hm'1 Hm'2]).
+(apply match_ty_pair; auto).
+Search -fresh_in_ty.
