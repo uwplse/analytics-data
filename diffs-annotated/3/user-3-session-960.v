2509eb275e3f49ffb08adcb1a3a357fbd31a785b
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:36 -0700

    user 3, session 960, part 121

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -7,10 +7,6 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
-Theorem mod_S_lt n m : n `mod` S m < S m.
-Proof.
-(apply PeanoNat.Nat.mod_upper_bound; auto).
-Qed.
 Function
  nat_to_le base_m2 (x : nat) {wf lt x} : list {x : nat | x < S (S base_m2)}
  :=
