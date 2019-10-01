15ec76fd16c97bd304e74ee124c320219dead1c6
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:45:52 -0700

    user 3, session 6414, part 38

--- diffs-annotated/3/user-3-session-6414.v
+++ diffs-annotated/3/user-3-session-6414.v
@@ -23,5 +23,5 @@ Definition log_abstraction (d : disk) (log : list block) : Prop :=
   exists b,
     diskGet d 0 =?= b /\
     block_to_addr b = length log /\
-    (forall a, a < length log -> diskGet d (log_addr a) =?= nth log a).
+    (forall a, a < length log -> diskGet d (log_addr a) =?= nth a log).
 (* Failed. *)
