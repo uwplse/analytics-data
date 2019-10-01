60e3b77686e49178190267406c3aea90b1275e06
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:20 -0700

    user 3, session 7199, part 197

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -50,4 +50,4 @@ proc unit :=
   | [] => Ret tt
   | b :: bs => _ <- d.write (log_addr a) b; append_at (S a) bs
   end.
-(intuition eauto; autorewrite with upd).
+(intuition eauto; autorewrite with upd; auto).
