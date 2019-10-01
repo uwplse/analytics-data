d6af43a65066bb28fa1918b6935d4b7f49ef0c28
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:59:58 -0700

    user 3, session 11016, part 44

--- diffs-annotated/3/user-3-session-11016.v
+++ diffs-annotated/3/user-3-session-11016.v
@@ -297,17 +297,17 @@ Proof.
 reflexivity.
 -
 reflexivity.
-(rewrite IHl; auto).
+(intros).
+(rewrite app_assoc).
+(rewrite app_assoc).
+reflexivity.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq2rsX3F"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqqhKm3r"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Qed.
-Theorem app_assoc4 :
-  forall l1 l2 l3 l4 : natlist,
-  l1 ++ l2 ++ l3 ++ l4 = ((l1 ++ l2) ++ l3) ++ l4.
+Lemma nonzeros_app :
+  forall l1 l2 : natlist, nonzeros (l1 ++ l2) = nonzeros l1 ++ nonzeros l2.
 Proof.
-(rewrite app_assoc).
-(* Failed. *)
