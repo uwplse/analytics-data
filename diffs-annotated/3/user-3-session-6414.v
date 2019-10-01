03980a820b1968afb9629a2f15dc86b11e462f89
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:45:52 -0700

    user 3, session 6414, part 32

--- diffs-annotated/3/user-3-session-6414.v
+++ diffs-annotated/3/user-3-session-6414.v
@@ -26,37 +26,3 @@ Fixpoint get_upto (a : addr) : proc (list block) :=
   end.
 Definition reset : proc unit :=
   len0 <- addr_to_block 0; d.write len_addr len0.
-Definition recover : proc unit := Ret tt.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqt9IPPT"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqWzqFcf"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Print Abstraction.
-Print LayerAbstraction.
-Print Abstraction.
-Definition log_abstraction (d : disk) (log : list block) : Prop := True.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqeTluG2"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8ldc1S"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Definition abstr : Abstraction State :=
-  abstraction_compose d.abstr {| abstraction := log_abstraction |}.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqspZVAW"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqsd3ljh"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
