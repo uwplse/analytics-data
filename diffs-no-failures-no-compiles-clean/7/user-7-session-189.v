d2593f9c945f5576c1030afccd858f8d48eaa1f0
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:36:48 -0700

    user 7, session 189, part 5

--- diffs-no-failures-no-compiles-clean/7/user-7-session-189.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-189.v
@@ -2,24 +2,3 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0281a.BaseDefs.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Open Scope btjt.
-Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
-Proof.
-(intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
-Qed.
-Lemma free_in_ty__decidable : forall (X : id) (t : ty), Decidable.decidable (free_in_ty X t).
-Proof.
-(intros X t).
-(unfold free_in_ty).
-Search -IdSet.In.
-(apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
-Qed.
-Lemma fresh_in_ty__decidable : forall (X : id) (t : ty), Decidable.decidable (fresh_in_ty X t).
-Proof.
