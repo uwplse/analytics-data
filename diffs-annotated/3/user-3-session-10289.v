43cdd7d8cafccdb7128a2a0e6c222c090d18921a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:57:25 -0700

    user 3, session 10289, part 4

--- diffs-annotated/3/user-3-session-10289.v
+++ diffs-annotated/3/user-3-session-10289.v
@@ -5,6 +5,4 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Import Helpers.Helpers.
-Require Import Proc.
-Require Import ProcTheorems.
+(* Failed. *)
