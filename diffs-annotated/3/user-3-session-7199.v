4afae13306cff14c411ec864d1612603b6a10e90
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:23 -0700

    user 3, session 7199, part 240

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -23,6 +23,37 @@ Axiom
      forall State a recover abstr,
      proc_spec (@addr_to_block_spec State a) (addr_to_block a) recover abstr).
 Hint Resolve addr_to_block_ok: core.
-(destruct (a == a0); subst; autorewrite with upd; auto).
-(rewrite diskUpd_oob_eq; simpl; auto).
-(* Failed. *)
+(simpl; auto).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqqkicQk"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
+Theorem append_at_ok a bs' :
+  proc_spec
+    (fun (bs : list block) state =>
+     {|
+     pre := a = length bs /\
+            log_size_ok state (bs ++ bs') /\ log_contents_ok state bs;
+     post := fun r state' =>
+             diskGet state' len_addr = diskGet state len_addr /\
+             diskSize state' = diskSize state /\
+             log_size_ok state' (bs ++ bs') /\
+             log_contents_ok state' (bs ++ bs');
+     recovered := fun _ state' =>
+                  diskGet state' len_addr = diskGet state len_addr /\
+                  diskSize state' = diskSize state /\
+                  log_contents_ok state' bs |}) (append_at a bs') recover
+    d.abstr.
+Proof.
+(induction bs'; simpl).
+-
+step_proc.
+intuition eauto.
+(rewrite app_nil_r; auto).
+-
+step_proc.
+(intuition eauto; autorewrite with upd; auto).
+(apply log_contents_ok_unchanged).
