6abe6ed70830e8e5d643d531644d7a011613124b
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:33 -0700

    user 3, session 960, part 61

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -7,23 +7,13 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
-lia.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq7hBary"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Qed.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqzByAOY"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqpSNkf1"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Next Obligation of nat_to_le_func.
-Proof.
-lia.
+#[program]
+Fixpoint nat_to_le base (x : nat) {measure lt x} :
+list {x : nat | x < S (S base)} :=
+  match x with
+  | 0 => nil
+  | _ =>
+      let digit := x mod S (S base) in
+      exist _ digit _ :: nat_to_le base (x / S (S base))
+  end.
+(* Failed. *)
