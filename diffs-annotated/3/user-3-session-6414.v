3458e1cf0fb4c4a0044ce240870b418b3303afeb
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:45:51 -0700

    user 3, session 6414, part 23

--- diffs-annotated/3/user-3-session-6414.v
+++ diffs-annotated/3/user-3-session-6414.v
@@ -24,13 +24,14 @@ Fixpoint get_upto (a : addr) : proc (list block) :=
   | 0 => Ret []
   | S a => b <- get_at a; bs <- get_upto a; Ret (bs ++ [b])
   end.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqQAqVu1"
+Definition get : proc (list block) := len <- get_len; get_upto len.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8aT6YT"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqmIHFZM"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq2gZaVH"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Definition get : proc (list block).
+(* Failed. *)
