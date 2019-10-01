51718efc6ba4dca9632bb3c2132194b9733024c7
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:35:29 -0700

    user 7, session 108, part 47

--- diffs-no-failures-no-compiles-clean/7/user-7-session-108.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-108.v
@@ -257,4 +257,4 @@ subst.
 {
 (assert (Hcontra' : ~ ((exists v1 : ty, |-[ k, w] v1 <$ t1) /\ (exists v2 : ty, |-[ k, w] v2 <$ t2)))).
 {
-(intros [Hcontra'1 Hcontra'2]).
+(intros [[v1 Hcontra'1] [v2 Hcontra'2]]).
