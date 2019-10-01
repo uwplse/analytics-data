c8fac7c67ef3f2e2e0801fb42b51a27610e2b5ae
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:26 -0700

    user 3, session 7199, part 277

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -25,7 +25,9 @@ Axiom
 Hint Resolve addr_to_block_ok: core.
 (simpl; lia).
 autorewrite with upd list in *.
++
+(unfold log_size_ok in *; simpl in *).
+autorewrite with upd list in *.
 (simpl in *; lia).
 +
-admit.
-+
+autorewrite with upd in *.
