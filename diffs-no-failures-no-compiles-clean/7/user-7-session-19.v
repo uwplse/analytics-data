e3bbd189953d3ba19d748243b209a55217a6b8b6
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:38 -0700

    user 7, session 19, part 317

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -140,5 +140,4 @@ clear IHt3.
 (intros v; split; intros Hm; specialize (Hrefx v); specialize (Hrefy v); tauto).
 Lemma value_sem_sub_k_union__value_sem_sub_k_component :
   forall v : ty, value_type v -> forall (k : nat) (ta tb : ty), ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
-Proof.
 (intros v Hv; induction Hv; intros k ta tb Hsem; unfold sem_sub_k_i in Hsem).
