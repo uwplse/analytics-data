6f1d8bdf88cdb7409740e2a7fb58b4884f58594d
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:25 -0700

    user 3, session 7199, part 273

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -23,4 +23,39 @@ Axiom
      forall State a recover abstr,
      proc_spec (@addr_to_block_spec State a) (addr_to_block a) recover abstr).
 Hint Resolve addr_to_block_ok: core.
-Hint Rewrite app_length : length.
+Hint Rewrite app_length : list.
+Theorem append_at_ok a bs' :
+  proc_spec
+    (fun (bs : list block) state =>
+     {|
+     pre := a = length bs /\
+            log_size_ok state (bs ++ bs') /\ log_contents_ok state bs;
+     post := fun r state' =>
+             diskGet state' len_addr = diskGet state len_addr /\
+             diskSize state' = diskSize state /\
+             log_size_ok state' (bs ++ bs') /\
+             log_contents_ok state' (bs ++ bs');
+     recovered := fun _ state' =>
+                  diskGet state' len_addr = diskGet state len_addr /\
+                  diskSize state' = diskSize state /\
+                  log_contents_ok state' bs |}) (append_at a bs') recover
+    d.abstr.
+Proof.
+generalize dependent a.
+(induction bs'; simpl; intros).
+-
+step_proc.
+intuition eauto.
+(rewrite app_nil_r; auto).
+-
+step_proc.
+(intuition eauto; autorewrite with upd; auto).
+{
+(apply log_contents_ok_unchanged; eauto).
+}
+(eapply proc_spec_weaken; eauto).
+(unfold spec_impl; simpl; intuition).
+(exists (a' ++ [a]); intuition eauto; autorewrite with upd list in *; eauto).
++
+lia.
+(* Failed. *)
