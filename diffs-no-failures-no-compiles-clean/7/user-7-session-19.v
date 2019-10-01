82d28357d7037d5cb7335579e7fbd8dfec1c9556
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:49 -0700

    user 7, session 19, part 506

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -249,4 +249,5 @@ Proof.
 (intros k t t' Hdept Hsem).
 (rewrite <- inv_depth_mk_nf).
 (apply sem_sub_k_i_nf__inv_depth_le with k).
-(apply mk_nf__in_f).
+(apply mk_nf__in_nf).
+(rewrite inv_depth_mk_nf; assumotion).
