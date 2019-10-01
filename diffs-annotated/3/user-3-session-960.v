96cafe5458ca5ab0e759e5b1aa974621fa1356ea
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:35 -0700

    user 3, session 960, part 91

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -7,56 +7,8 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
-#[program]
-Fixpoint nat_to_le base (x : nat) {measure x lt} :
-list {x : nat | x < S (S base)} :=
-  match x with
-  | 0 => nil
-  | _ =>
-      let digit := x mod S (S base) in
-      exist _ digit _ :: nat_to_le base (x / S (S base))
-  end.
-Next Obligation of nat_to_le_func.
+Print FixedLengthEncoder.
+Theorem nat_le_inverse base : forall x, le_to_nat (nat_to_le base x) = x.
 Proof.
-(apply PeanoNat.Nat.mod_upper_bound; auto).
-Qed.
-Next Obligation of nat_to_le_func.
-Proof.
-subst digit.
-(apply PeanoNat.Nat.div_lt; auto; try lia).
-Qed.
-Next Obligation of nat_to_le_func.
-Proof.
-lia.
-Qed.
-Next Obligation of nat_to_le_func.
-Proof.
-(unfold MR).
-(apply (wf_projected lt projT2); auto).
-(apply lt_wf).
-Qed.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqhwf9lL"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqNJr2rI"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Fixpoint le_to_nat base (digits : list {x : nat | x < S (S base)}) : nat :=
-  match digits with
-  | nil => 0
-  | digit :: digits' => proj1_sig digit * base + le_to_nat digits'
-  end.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0fxDJv"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqP3FOAC"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Instance aModel : GoModel.
-Proof.
+(intros).
+Search -nat -div.
