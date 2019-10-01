8af189248882d4b813507b2d10879e68bbe7326d
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:36:30 -0700

    user 7, session 174, part 40

--- diffs-no-failures-no-compiles-clean/7/user-7-session-174.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-174.v
@@ -133,4 +133,6 @@ Lemma triv : forall (X : id) (s : ty) (t1 t2 : ty), [X := s] TPair t1 t2 = TPair
 Proof.
 (intros X s t1 t2).
 Search -subst.
-(apply subst_equation).
+Search -subst_equation.
+Check subst_equation.
+(apply (subst_equation X s (TPair t1 t2))).
