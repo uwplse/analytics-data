eef768e704f786d1881312b346f937a1e5f9ed12
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:42 -0700

    user 3, session 960, part 213

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -8,11 +8,4 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Check mod_S_lt.
-refine
- {|
- byte := {x | x < 256};
- byte0 := exist _ 0 _;
- uint64_to_string := pretty.pretty_nat;
- ascii_to_byte := ascii_to_bounded;
- byte_to_ascii := bounded_to_ascii |}.
-auto.
+lia.
