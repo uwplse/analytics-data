8d8aa3ea39ef5f889a89c56ba76f0bd082e988f1
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:17 -0700

    user 3, session 7199, part 158

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -71,24 +71,14 @@ Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 Definition log_length_ok (d : disk) (log : list block) :=
   forall b, diskGet d 0 =?= b -> block_to_addr b = length log.
-Theorem get_len_ok :
-  proc_spec
-    (fun bs state =>
-     {|
-     pre := log_length_ok state bs /\ log_size_ok state bs;
-     post := fun r state' => state' = state /\ r = length bs;
-     recovered := fun _ state' => state' = state |}) get_len recover d.abstr.
-Proof.
-(unfold get_len; intros).
-step_proc.
-step_proc.
-eauto.
-Qed.
 Theorem get_len_abstr_ok :
   proc_spec
-    (fun _ state =>
+    (fun (_ : unit) state =>
      {|
      pre := True;
      post := fun r state' => state' = state /\ r = length state;
      recovered := fun _ state' => state' = state |}) get_len recover abstr.
-(* Failed. *)
+Proof.
+(unfold get_len; intros).
+(apply spec_abstraction_compose).
+(apply spec_weaken).
