bc145d135a08f9ed4718e386221a42e72ad04709
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:35 -0700

    user 3, session 960, part 95

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -7,3 +7,8 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
+Theorem nat_le_inverse base : forall n, le_to_nat (nat_to_le base n) = n.
+Proof.
+(intros).
+(induction n as [n IHn] using lt_wf_ind).
+(destruct n).
