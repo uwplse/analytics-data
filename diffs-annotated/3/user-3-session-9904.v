dfb9a3125a992bbcd6028765159c13a9b624f9a8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:56:15 -0700

    user 3, session 9904, part 6

--- diffs-annotated/3/user-3-session-9904.v
+++ diffs-annotated/3/user-3-session-9904.v
@@ -53,6 +53,6 @@ Proof.
 (unfold statdb_abstraction; simpl).
 lia.
 Qed.
-(apply spec_abstraction_compose; simpl).
-(step_proc; intros).
+step_proc.
 (destruct a'; simpl in *; intuition).
+step_proc.
