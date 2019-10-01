266992cacf322b1f4ce877f91a36e49cc18f02a8
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:38:25 -0700

    user 8, session 37, part 33

--- diffs-no-failures-no-compiles-clean/8/user-8-session-37.v
+++ diffs-no-failures-no-compiles-clean/8/user-8-session-37.v
@@ -92,4 +92,3 @@ specialize (H (add_fresh_pat W []) (add_fresh_state W []) []).
 (unfold add_fresh_state, add_fresh_pat in *).
 (destruct (add_fresh W []) as [p \206\147] eqn:E).
 (simpl in *).
-(rewrite H).
