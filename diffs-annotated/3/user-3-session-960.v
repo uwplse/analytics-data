2d049bbb1a7cc4a053ac0f53adb0de50e880632d
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:40 -0700

    user 3, session 960, part 183

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -8,5 +8,4 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Check mod_S_lt.
-(rewrite (PeanoNat.Nat.div_mod n base)  at 4 by lia).
-(* Failed. *)
+(rewrite (PeanoNat.Nat.div_mod n base)  at 3 by lia).
