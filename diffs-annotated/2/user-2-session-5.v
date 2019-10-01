aade5942e890bd71267a5fc94d91218dfb2cbb73
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:23:39 -0700

    user 2, session 5, part 31

--- diffs-annotated/2/user-2-session-5.v
+++ diffs-annotated/2/user-2-session-5.v
@@ -20,14 +20,5 @@ Inductive term :=
 Redirect "/tmp/coqmW6Ht8" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Notation "< a b >" := (Cons a b) (format "< a  b >", a  at level 0, b  at level 0) : coucou_scope.
-Notation "[ ]" := Nil (format "[ ]") : coucou_scope.
-Notation "[ x ]" := (Cons x Nil) : coucou_scope.
-Notation "[ x y .. z ]" := (Cons x (Cons y .. (Cons z Nil) ..))
-  (x  at level 0, y  at level 0, z  at level 0) : coucou_scope.
-End TermNotations.
-Import TermNotations.
-Open Scope coucou_scope.
-Check [Nil (Cons (Ident "1") (Ident "2")) (Ident "a") {(Ident "myfun") (Ident "somArg")}].
-Redirect "/tmp/coqouXyBd" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
+Check
+  [<Nil <Nil Nil>> (Cons (Ident "1") (Ident "2")) (Ident "a") {(Ident "myfun") (Ident "somArg")}].
