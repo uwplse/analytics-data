be20c7252bc21521c32e1d5ec0a31a0a9028ea90
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:24:11 -0700

    user 3, session 159, part 2

--- diffs-annotated/3/user-3-session-159.v
+++ diffs-annotated/3/user-3-session-159.v
@@ -10,3 +10,27 @@ Require Import Spec.Hoare.
 Require Import Spec.AbstractionSpec.
 Definition absr : relation DB.l.(State) Var.l.(State) unit :=
   fun l s _ => fst s = fold_right plus 0 l /\ snd s = length l.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0BLEGv"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Definition absr : relation DB.l.(State) Var.l.(State) unit :=
+  fun l res =>
+  match res with
+  | Val s _ => fst s = fold_right plus 0 l /\ snd s = length l
+  | Err _ _ => False
+  end.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqiQZm2s"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4uHC6k"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Instance absr_non_error : (NonError absr).
