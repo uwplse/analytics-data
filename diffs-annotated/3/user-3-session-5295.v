2c87c305ecc6f595314919cfee24c09b29bc6a14
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:42:26 -0700

    user 3, session 5295, part 1

--- diffs-annotated/3/user-3-session-5295.v
+++ diffs-annotated/3/user-3-session-5295.v
@@ -170,5 +170,17 @@ Proof.
 (unfold write).
 step.
 (destruct r; step).
+{
 (descend; intuition eauto).
-(* Auto-generated comment: Failed. *)
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq37NBx4"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqPfNu2C"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+}
+(* Failed. *)
