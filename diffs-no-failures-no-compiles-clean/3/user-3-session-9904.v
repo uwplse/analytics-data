a5eceff07c856b0886a7bd03c11390214656bc3e
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:26:51 -0700

    user 3, session 9904, part 97

--- diffs-no-failures-no-compiles-clean/3/user-3-session-9904.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-9904.v
@@ -147,5 +147,5 @@ step_proc.
 (match goal with
  | H:pre (match ?a with
           | (x, y) => _
-          end _) |- _ => destruct a as [x y]
+          end _) |- _ => destruct a as [x y]; cbn[pre post recovered] in *
  end).
