d9d1b920d737d403ab27104ecd3b78804b9a7877
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:23:30 -0700

    user 1, session 41, part 38

--- diffs-annotated/1/user-1-session-41.v
+++ diffs-annotated/1/user-1-session-41.v
@@ -548,4 +548,5 @@ Inductive Alpha : SetST -> GT -> Prop :=
 Theorem alpha_is_partial_function :
   forall S G G', Alpha S G -> Alpha S G' -> G = G'.
 Hint Resolve singleton_eq: agt.
-(intros; inversion H0; subst).
+(intros; inversion H0; subst; eauto).
+all: (try rewrite singleton_eq in H1; eauto).
