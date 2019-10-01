a03d42fc3ad684f87720c52e8ec4163d180bd317
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:10:36 -0700

    user 3, session 5295, part 32

--- diffs-no-failures-no-compiles-clean/3/user-3-session-5295.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-5295.v
@@ -699,12 +699,11 @@ intuition.
 *
 (exists d,(OutOfSync a0 b); simplify; finish).
 lia.
-*
-(exists (diskUpd d a0 b),FullySynced; simplify; finish).
 lia.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqXSCN0n"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqxD1xWt"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
+Qed.
