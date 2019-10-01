21659ebdfd4fd8d87c9183997904c210313c315c
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:35:22 -0700

    user 7, session 104, part 90

--- diffs-no-failures-no-compiles-clean/7/user-7-session-104.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-104.v
@@ -59,4 +59,7 @@ clear Hm.
 clear Heq.
 (destruct Hcontra as [Hcontra _]).
 (assert (Hm : |-[ 0, 0] tint <$ tint) by (apply match_ty_value_type__reflexive; constructor)).
+specialize (Hcontra 0).
+(destruct Hcontra as [w2' Hcontra]).
 specialize (Hcontra _ Hm).
+(apply match_ty_cname__inv in hcontra).
