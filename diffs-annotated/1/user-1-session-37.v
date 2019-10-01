6940e41e4f909dfe44a10f89fed6b477c565b635
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:23:23 -0700

    user 1, session 37, part 48

--- diffs-annotated/1/user-1-session-37.v
+++ diffs-annotated/1/user-1-session-37.v
@@ -297,7 +297,9 @@ Fixpoint Gamma (G : GT) : SetST :=
       fun X =>
       exists l',
         X = SRec l' /\
-        Forall2 (fun (S' : option ST) G' => True) l'
+        Forall2
+          (fun (S' : option ST) (G' : Ensemble (option ST)) => True)
+          l'
           (map
              (option_map
                 (fun pair =>
@@ -317,3 +319,4 @@ Fixpoint Gamma (G : GT) : SetST :=
                  end)) l)
   | _ => Empty_set _
   end.
+(* Failed. *)
