00a2de51d1d164b0b7fefd5bc078556df749629c
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:59:58 -0700

    user 3, session 11016, part 42

--- diffs-annotated/3/user-3-session-11016.v
+++ diffs-annotated/3/user-3-session-11016.v
@@ -297,10 +297,4 @@ Proof.
 reflexivity.
 -
 reflexivity.
-(rewrite IHl; auto).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqRREFch"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+auto.
