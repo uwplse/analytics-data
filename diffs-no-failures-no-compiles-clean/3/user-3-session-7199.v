113b9caae33740b810217e1c7208b1cb34f946c7
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:17:59 -0700

    user 3, session 7199, part 485

--- diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
+++ diffs-no-failures-no-compiles-clean/3/user-3-session-7199.v
@@ -27,7 +27,6 @@ Notation "d [ a |-> b ]" := (diskUpd d a b) (at level 12, left associativity).
 Notation "d [ a |=> bs ]" := (diskUpds d a bs)
   (at level 12, left associativity).
 Opaque diskGet.
-Set Default Goal Selector "!".
 Module Log (d: OneDiskAPI)<: LogAPI.
 Definition len_addr : addr := 0.
 Definition log_addr a : addr := S a.
@@ -41,201 +40,26 @@ Definition init' : proc InitResult :=
 Definition init := then_init d.init init'.
 Definition get_len : proc addr :=
   b <- d.read len_addr; Ret (block_to_addr b).
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqKp7l6v"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqVnvw58"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 Definition get_at (a : addr) : proc block := d.read (log_addr a).
-Fixpoint get_upto (a : addr) : proc (list block) :=
-  match a with
-  | 0 => Ret nil
-  | S a => b <- get_at a; bs <- get_upto a; Ret (bs ++ [b])
-  end.
-Definition get : proc (list block) := len <- get_len; get_upto len.
-Fixpoint append_at (a : addr) (bs : list block) : 
-proc unit :=
-  match bs with
-  | [] => Ret tt
-  | b :: bs => _ <- d.write (log_addr a) b; append_at (S a) bs
-  end.
-Definition append (bs : list block) : proc bool :=
-  size <- d.size;
-  len <- get_len;
-  (if le_dec (1 + len + length bs) size
-   then
-    _ <- append_at len bs;
-    len_b <- addr_to_block (len + length bs);
-    _ <- d.write len_addr len_b; Ret true
-   else Ret false).
-Definition reset : proc unit :=
-  len0 <- addr_to_block 0; d.write len_addr len0.
-Definition recover : proc unit := d.recover.
-Definition log_length_ok (d : disk) (log : list block) :=
-  forall b,
-  diskGet d len_addr =?= b -> block_to_addr b = length log.
-Definition log_size_ok (d : disk) (log : list block) :=
-  diskSize d >= 1 + length log.
-Definition log_contents_ok (d : disk) (log : list block) :=
-  forall a,
-  a < length log -> diskGet d (log_addr a) =?= nth a log block0.
-Definition log_abstraction (d : disk) (log : list block) : Prop :=
-  log_length_ok d log /\ log_size_ok d log /\ log_contents_ok d log.
-Theorem abstr_length_proj d log :
-  log_abstraction d log -> log_length_ok d log.
-Proof.
-(unfold log_abstraction; intuition).
-Qed.
-Theorem abstr_size_proj d log :
-  log_abstraction d log -> log_size_ok d log.
-Proof.
-(unfold log_abstraction; intuition).
-Qed.
-Theorem abstr_contents_proj d log :
-  log_abstraction d log -> log_contents_ok d log.
-Proof.
-(unfold log_abstraction; intuition).
-Qed.
-Hint Resolve abstr_length_proj abstr_size_proj abstr_contents_proj:
-  core.
-Definition abstr : Abstraction State :=
-  abstraction_compose d.abstr {| abstraction := log_abstraction |}.
-Lemma diskGet_eq_values :
-  forall d a b b',
-  diskGet d a =?= b ->
-  diskGet d a =?= b' -> a < diskSize d -> b = b'.
-Proof.
-(intros).
-(destruct (diskGet d a) eqn:?; simpl in *).
--
-congruence.
--
-exfalso.
-(apply disk_inbounds_not_none in Heqo; eauto).
-Qed.
-Ltac
- eq_values :=
-  match goal with
-  | H:diskGet ?d ?a =?= ?b, H':diskGet ?d ?a =?= ?b'
-    |- _ =>
-        assert (b = b') by
-         (apply (@diskGet_eq_values d a b b'); try lia; auto);
-         subst
-  end.
-Theorem log_length_ok_nil d b :
-  diskGet d len_addr = Some b ->
-  block_to_addr b = 0 -> log_length_ok d nil.
-Proof.
-(unfold log_length_ok; intros).
-(rewrite H in *; simpl in *; subst).
-auto.
-Qed.
-Theorem log_abstraction_nil d b :
-  diskGet d len_addr = Some b ->
-  block_to_addr b = 0 -> log_abstraction d nil.
-Proof.
-(unfold log_abstraction; intros).
-intuition.
--
-eauto using log_length_ok_nil.
--
-(unfold log_size_ok).
-(destruct d; simpl in *; [  | lia ]).
-(assert (diskGet nil len_addr = None)).
-{
-(apply disk_oob_eq).
-(simpl; lia).
-}
-congruence.
--
-(unfold log_contents_ok; simpl in *; intuition).
-(exfalso; lia).
-Qed.
-Theorem init_ok : init_abstraction init recover abstr inited_any.
-Proof.
-(eapply then_init_compose; eauto).
-step_proc.
-(destruct (lt_dec r 1)).
--
-step_proc.
--
-step_proc.
-step_proc.
-step_proc.
-(exists nil; simpl).
-(split; auto).
-(eapply log_abstraction_nil; eauto).
-(unfold len_addr).
-(autorewrite with upd; auto).
-Qed.
-Theorem log_abstraction_length d bs :
-  log_abstraction d bs -> log_length_ok d bs.
-Proof.
-(unfold log_abstraction; intuition).
-Qed.
-Hint Resolve log_abstraction_length: core.
-Lemma abstr_get_len :
-  forall (bs : list block) (state : State),
-  log_length_ok state bs ->
-  forall r : block,
-  diskGet state len_addr =?= r -> block_to_addr r = length bs.
-Proof.
-(intros).
-(unfold log_length_ok in H).
-auto.
-Qed.
-Hint Resolve abstr_get_len: core.
-Theorem get_len_ok :
-  proc_spec
-    (fun bs state =>
-     {|
-     pre := log_length_ok state bs /\ log_size_ok state bs;
-     post := fun r state' => state' = state /\ r = length bs;
-     recovered := fun _ state' => state' = state |}) get_len
-    recover d.abstr.
-Proof.
-(unfold get_len; intros).
-step_proc.
-step_proc.
-eauto.
-Qed.
-Hint Resolve get_len_ok: core.
-Theorem get_len_abstr_ok :
-  proc_spec
-    (fun (_ : unit) state =>
-     {|
-     pre := True;
-     post := fun r state' => state' = state /\ r = length state;
-     recovered := fun _ state' => state' = state |}) get_len
-    recover abstr.
-Proof.
-(apply spec_abstraction_compose).
-(eapply proc_spec_weaken; eauto).
-(unfold spec_impl; intros).
-(destruct a as [[] bs]; simpl in *; intuition eauto).
-(descend; intuition eauto).
-Qed.
-Hint Resolve get_len_abstr_ok: core.
-Theorem log_size_bound d bs a :
-  log_size_ok d bs -> a < length bs -> log_addr a < diskSize d.
-Proof.
-(unfold log_size_ok, log_addr; intros; lia).
-Qed.
-Hint Resolve log_size_bound: core.
-Theorem get_at_ok a :
-  proc_spec
-    (fun (_ : unit) state =>
-     {|
-     pre := a < length state;
-     post := fun r state' =>
-             state' = state /\ nth a state block0 = r;
-     recovered := fun _ state' => state' = state |}) 
-    (get_at a) recover abstr.
-Proof.
-(unfold get_at; intros).
-(apply spec_abstraction_compose).
-(simpl).
-(eapply proc_spec_weaken; eauto).
-(unfold spec_impl; intros).
-(destruct a0 as [_ bs]; simpl in *; intuition eauto).
-(descend; intuition eauto).
-(descend; intuition eauto).
-(unfold log_abstraction in H0; intuition).
-(pose proof (H3 a); intuition).
-(assert (v = nth a bs block0)).
-(eapply diskGet_eq_values; eauto).
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqHtNpYl"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqBlEp9H"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
