f14274446bf8733f716b2c4ebab6a751f9b7fafa
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:57:38 -0700

    user 3, session 10360, part 2

--- diffs-annotated/3/user-3-session-10360.v
+++ diffs-annotated/3/user-3-session-10360.v
@@ -154,21 +154,3 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-Import Coq.Strings.String.StringSyntax.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqN2HmKo"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqDsHa6D"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
-Theorem init_ok : init_abstraction init recover abstr inited_any.
-Proof.
-(eapply then_init_compose; eauto).
-step.
-(destruct (lt_dec r 1)).
--
-step.
