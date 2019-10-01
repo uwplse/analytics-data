94862c88cbdbad7d6185771714b3699dd6745f93
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:36:22 -0700

    user 7, session 171, part 7

--- diffs-no-failures-no-compiles-clean/7/user-7-session-171.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-171.v
@@ -71,5 +71,4 @@ Fixpoint size (t : ty) :=
   end.
 Lemma rename__size : forall (x y : id) (t : ty), size ([x @ y] t) = size t.
 Proof.
-(induction t; simpl; try reflexivity).
-Reserved Notation "'[' x ':=' s ']' t" (at level 30).
+-
