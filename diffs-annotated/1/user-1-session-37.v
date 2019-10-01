6da4bc4e7d57fdfab0c560df9c14f366b7a2d29b
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:23:21 -0700

    user 1, session 37, part 9

--- diffs-annotated/1/user-1-session-37.v
+++ diffs-annotated/1/user-1-session-37.v
@@ -285,5 +285,9 @@ End AGT_Spec.
 Module AGT_Bounded_Rows_Details.
 Search -unique.
 Search -uniqueness.
-Definition label := integer.
-(* Failed. *)
+Definition label := nat.
+Inductive ST : Type :=
+  | SInt : ST
+  | SBool : ST
+  | SFun : ST -> ST -> ST
+  | SRec : forall l : list (label * ST)%type, NoDup (map fst l) -> ST.
