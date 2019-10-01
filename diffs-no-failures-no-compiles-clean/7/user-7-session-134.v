b01caa74b47ae25d2d82ff89046118d5048b54cf
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:35:53 -0700

    user 7, session 134, part 2

--- diffs-no-failures-no-compiles-clean/7/user-7-session-134.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-134.v
@@ -104,3 +104,9 @@ Proof.
 (destruct k).
 -
 (intros w1; exists w1; intros v Hm; simpl).
+(intros w1).
+exists w1.
+(intros v Hm).
+(apply match_ty_ref__weak_inv in Hm).
+(destruct Hm as [tx Heq]; subst).
+(destruct w1; tauto).
