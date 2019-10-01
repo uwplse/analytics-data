c897bbea0c831fb1dadffaf91f4818a082538635
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:56 -0700

    user 7, session 19, part 642

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -457,6 +457,5 @@ Proof.
 (apply mk_nf__in_nf).
 Admitted.
 Theorem sub_d__sem_sub_i : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
-Proof.
-(intros t1 t2 Hsub).
-(unfold sem_sub).
+(unfold sem_sub_i).
+(induction Hsub; intros k v Hv Hm).
