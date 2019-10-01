e90886144789486bfbdbe0ad0873f7b5045eeb9e
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:39 -0700

    user 3, session 960, part 155

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -8,14 +8,5 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Check mod_S_lt.
-Theorem nat_le_inverse base_m2 :
-  forall n, le_to_nat (nat_to_le base_m2 n) = n.
-Proof.
-(intros).
-(induction n as [n IHn] using lt_wf_ind).
-(destruct n; rewrite nat_to_le_equation; simpl).
--
-auto.
--
-(assert (1 < S (S base_m2)) by lia).
-(generalize dependent S (S base_m2); intros base).
+(assert (base_m2 = S (S base_m2) - 2) by auto).
+(* Failed. *)
