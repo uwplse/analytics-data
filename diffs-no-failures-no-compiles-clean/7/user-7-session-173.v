2f42666b9208cb4b3b1a91774447a8a26397cc85
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:36:27 -0700

    user 7, session 173, part 50

--- diffs-no-failures-no-compiles-clean/7/user-7-session-173.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-173.v
@@ -82,9 +82,8 @@ reflexivity.
 assumption.
 -
 (destruct (beq_idP x i); reflexivity).
-Qed.
 Function
- subst (x : id) (s t : ty) {measure size t} : ty :=
+ subst (x : id) (s t : ty) {wf size t} : ty :=
    match t with
    | TCName _ => t
    | TPair t1 t2 => TPair (subst x s t1) (subst x s t2)
@@ -96,28 +95,3 @@ Function
    | TVar y => if beq_id x y then s else t
    | TEV y => t
    end.
--
-(intros).
-(simpl).
-Omega.omega.
--
-(intros).
-(simpl).
-Omega.omega.
--
-(intros).
-(simpl).
-Omega.omega.
--
-(intros).
-(simpl).
-Omega.omega.
--
-(intros).
-(simpl).
-Omega.omega.
--
-(intros).
-(simpl).
-Omega.omega.
-Defined.
