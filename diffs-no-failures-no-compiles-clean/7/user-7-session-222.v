aa249c6b8a1227dced9c0f6862de80677ca6b6c8
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:37:25 -0700

    user 7, session 222, part 7

--- diffs-no-failures-no-compiles-clean/7/user-7-session-222.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-222.v
@@ -357,4 +357,8 @@ subst.
 (unfold b_free_in_ty, free in HX).
 (simpl in HX).
 Search -IdSet.remove.
-exfalso.
+(apply IdSetFacts.remove_1 in HX).
+contradiction.
+reflexivity.
+*
+(apply b_free_in_ty_exist_neq__inv in HX).
