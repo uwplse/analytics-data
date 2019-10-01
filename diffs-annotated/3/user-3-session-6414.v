f6b251d8e5c408252eb33d672db0a7668f463583
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:45:52 -0700

    user 3, session 6414, part 30

--- diffs-annotated/3/user-3-session-6414.v
+++ diffs-annotated/3/user-3-session-6414.v
@@ -24,21 +24,24 @@ Fixpoint get_upto (a : addr) : proc (list block) :=
   | 0 => Ret []
   | S a => b <- get_at a; bs <- get_upto a; Ret (bs ++ [b])
   end.
-Definition append (bs : list block) : proc bool :=
-  size <- d.size;
-  len <- get_len;
-  (if le_dec (1 + len + length bs) size
-   then _ <- append_at len bs; Ret true
-   else Ret false).
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq6RYEMO"
+Definition reset : proc unit :=
+  len0 <- addr_to_block 0; d.write len_addr len0.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqHtMpcu"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqaoLP3Y"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq9AFuGa"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Definition reset : proc unit :=
-  len0 <- addr_to_block 0; d.write len_addr len0 Admitted.
-(* Failed. *)
+Definition recover : proc unit := Ret tt.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq2okb7x"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqGD5cEz"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
