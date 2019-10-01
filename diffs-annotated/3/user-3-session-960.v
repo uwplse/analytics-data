3cc56762930b80d36bf8cc96602918c36cfe786a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:41 -0700

    user 3, session 960, part 196

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -8,5 +8,4 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Check mod_S_lt.
-Definition bounded_to_ascii (x : {x | x < 256}) : Ascii.Ascii.
-(* Failed. *)
+Definition bounded_to_ascii (x : {x | x < 256}) : Ascii.ascii.
