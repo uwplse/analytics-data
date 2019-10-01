01089e02def4d2fffd596d9052f66847358251ab
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:48:23 -0700

    user 3, session 7199, part 246

--- diffs-annotated/3/user-3-session-7199.v
+++ diffs-annotated/3/user-3-session-7199.v
@@ -23,5 +23,4 @@ Axiom
      forall State a recover abstr,
      proc_spec (@addr_to_block_spec State a) (addr_to_block a) recover abstr).
 Hint Resolve addr_to_block_ok: core.
-(apply proc_spec_weaken).
-(* Failed. *)
+(eapply proc_spec_weaken; eauto).
