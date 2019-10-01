aeade7ff6ae1957e75dc5ec5821deb58107bf431
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:35:48 -0700

    user 7, session 127, part 0

---
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-127.v
@@ -0,0 +1,26 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0280a.BaseDefs.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Open Scope btjm.
+Lemma match_ty_cname : forall (c : cname) (k w : nat), |-[ k, w] TCName c <$ TCName c.
+Proof.
+(intros c k w).
+(destruct k, w; reflexivity).
+Qed.
+Lemma match_ty_pair : forall (v1 v2 t1 t2 : ty) (k w : nat), |-[ k, w] v1 <$ t1 -> |-[ k, w] v2 <$ t2 -> |-[ k, w] TPair v1 v2 <$ TPair t1 t2.
+Proof.
+(intros v1 v2 t1 t2 k w Hm1 Hm2).
+(destruct k, w; split; assumption).
+Qed.
+Lemma match_ty_union_1 : forall (v t1 t2 : ty) (k w : nat), |-[ k, w] v <$ t1 -> |-[ k, w] v <$ TUnion t1 t2.
+Proof.
+(intros v t1 t2 k w Hm).
+(destruct k, w, v; left; assumption).
