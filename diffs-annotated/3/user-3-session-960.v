1ac8f6c306134a74040724b6e4b42a5ed96351bf
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:41 -0700

    user 3, session 960, part 205

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -8,22 +8,5 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Check mod_S_lt.
-Definition bounded_to_ascii (x : {x | x < 256}) : Ascii.ascii :=
-  match Byte.of_nat (proj1_sig x) with
-  | Some b => Ascii.ascii_of_byte b
-  | None => ascii0
-  end.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqhtsIcj"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqTyMpHa"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Check Ascii.byte_of_ascii.
-Check Ascii.nat_of_ascii.
-Definition ascii_to_bounded (a : Ascii.ascii) : {x | x < 256} :=
-  Instance aModel : GoModel.
-(* Failed. *)
+Definition ascii_to_bounded (a : Ascii.ascii) : {x | x < 256}.
+refine (exist _ (Ascii.nat_of_ascii a) _).
