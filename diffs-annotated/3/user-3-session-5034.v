e81d5f9f1c53217bc950979bafd7967bb25811bc
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:41:40 -0700

    user 3, session 5034, part 2

--- diffs-annotated/3/user-3-session-5034.v
+++ diffs-annotated/3/user-3-session-5034.v
@@ -114,4 +114,9 @@ Proof.
 step_proc.
 (destruct a'; simpl in *; intuition; subst; eauto).
 -
-(step_proc; intuition; subst; eauto).
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8HKFdP"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
