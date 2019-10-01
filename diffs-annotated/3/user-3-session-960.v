d7342da06ea27f11403fd1b2d1f6faab284b83a6
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:44 -0700

    user 3, session 960, part 237

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -8,57 +8,5 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Check mod_S_lt.
-Instance aModel : GoModel.
-Proof.
-refine
- {|
- byte := {x | x < 256};
- byte0 := exist _ 0 _;
- uint64_to_string := pretty.pretty_nat;
- ascii_to_byte := ascii_to_bounded;
- byte_to_ascii := bounded_to_ascii;
- uint64_to_le := nat_to_le 254;
- uint64_from_le := fun digits => Some (le_to_nat digits);
- File := Z;
- nilFile := (- 1)%Z;
- Ptr := fun _ => nat;
- nullptr := fun _ => 0 |}.
-(apply Nat.lt_0_succ).
-Defined.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqfDpN1C"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqx50w41"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Class GoModelWf (model : GoModel) :={uint64_to_string_inj :
-                                      forall x y,
-                                      uint64_to_string x = uint64_to_string y ->
-                                      x = y;
-                                     ascii_byte_bijection1 :
-                                      forall c,
-                                      byte_to_ascii (ascii_to_byte c) = c;
-                                     ascii_byte_bijection2 :
-                                      forall b,
-                                      ascii_to_byte (byte_to_ascii b) = b;
-                                     uint64_le_enc :
-                                      FixedLengthEncoder 8 uint64_to_le
-                                        uint64_from_le;
-                                     file_eqdec :> EqualDec File;
-                                     file_countable :> Countable File;
-                                     sigPtr_eq_dec :> EqualDec (sigT Ptr)}.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqQqPLC9"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqM2DVBS"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Instance aModel_wf : (GoModelWf aModel).
-Proof.
-(hnf).
+constructor.
+(* Failed. *)
