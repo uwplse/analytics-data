9177dc7fcda36561121ad2f2d3db5f147362bd0a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:23:34 -0700

    user 1, session 41, part 107

--- diffs-annotated/1/user-1-session-41.v
+++ diffs-annotated/1/user-1-session-41.v
@@ -281,3 +281,6 @@ Qed.
 End AGT_Spec.
 Require Import Coq.Lists.List.
 Import Coq.Lists.List.ListNotations.
+Theorem size_gt_g0 : forall x, 0 < size_gt x.
+Proof.
+(induction x; intros; eauto with math).
