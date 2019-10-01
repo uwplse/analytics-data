38a29977d1fa30f05debf9e856fc287ee633204c
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:45:54 -0700

    user 3, session 6414, part 67

--- diffs-annotated/3/user-3-session-6414.v
+++ diffs-annotated/3/user-3-session-6414.v
@@ -19,5 +19,5 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-(apply log_abstraction_nil).
+(eapply log_abstraction_nil).
 (* Failed. *)
