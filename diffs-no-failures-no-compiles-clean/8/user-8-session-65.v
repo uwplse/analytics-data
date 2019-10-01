eba9ce0d7d8ccd82255d1e4d0f273a4989e6f0ea
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:39:02 -0700

    user 8, session 65, part 71

--- diffs-no-failures-no-compiles-clean/8/user-8-session-65.v
+++ diffs-no-failures-no-compiles-clean/8/user-8-session-65.v
@@ -973,4 +973,3 @@ remember_differences.
 restore_dims try rewrite size_ntensor; unify_pows_two; simpl; try lia.
 (repeat rewrite Mmult_assoc).
 restore_dims try rewrite size_ntensor; unify_pows_two; simpl; try lia.
-Msimpl.
