74fc6ae702b95b5fa9f162d247aaeceb824f35da
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:35:40 -0700

    user 7, session 113, part 27

--- diffs-no-failures-no-compiles-clean/7/user-7-session-113.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-113.v
@@ -17,4 +17,4 @@ Qed.
 Lemma subs_not_in_FV : forall (X : id) (t : ty), ~ fresh_in_ty X t -> forall s : ty, [X := s] t = t.
 Proof.
 (intros X t).
-(induction t; intros Hnfresh s).
+(induction t; intros Hnfresh s; try (solve [ reflexivity ])).
