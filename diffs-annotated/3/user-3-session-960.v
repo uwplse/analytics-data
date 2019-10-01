df4d03321880afdaf44633ff9989aaa94f4ce1b3
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:41 -0700

    user 3, session 960, part 198

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -8,5 +8,4 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
 Check mod_S_lt.
-Search -Ascii.ascii.
-Print Ascii.byte_of_ascii.
+Print Ascii.ascii_of_byte.
