c6ee3cbce1b693c80b519b489228f63010bcb2c8
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:37 -0700

    user 7, session 19, part 297

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -143,5 +143,5 @@ Lemma value_sem_sub_k_union__value_sem_sub_k_component :
   forall k : nat, forall v : ty, value_type v -> forall ta tb : ty, ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
 (induction k; intros v Hv; induction Hv; intros ta tb Hsem; unfold sem_sub_k_i in Hsem).
 (try match goal with
-     | Hsem:forall v, value_type v -> |-[ ?k] v <$ TCName ?c -> _ |- _ => idtac
+     | Hsem:forall v, value_type v -> |-[ ?k] v <$ TCName ?c -> _ |- _ => assert (Hvv : value_type (TCName c)) by constructor
      end).
