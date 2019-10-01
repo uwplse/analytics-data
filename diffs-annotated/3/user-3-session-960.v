9c72cdfe847f02100f21d1da7b4cc159e8a337f1
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:32 -0700

    user 3, session 960, part 48

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -7,4 +7,34 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
+#[program]
+Fixpoint nat_to_le base (x : nat) {measure x : list {x : nat | x < S base} :=
+  match x with
+  | 0 => nil
+  | _ =>
+      let digit := x mod S base in
+      exist _ digit _ :: nat_to_le base ((x - digit) / S base)
+  end.
+Next Obligation.
+Proof.
+(apply PeanoNat.Nat.mod_upper_bound; auto).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqkAmYiA"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqTZy0we"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8035nf"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Next Obligation.
+Proof.
 (apply PeanoNat.Nat.div_lt).
+(* Failed. *)
