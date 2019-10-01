0a5122cceb1afb814d8c3a5ce3598e0cfa7082ea
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:11 -0700

    user 7, session 16, part 54

--- diffs-no-failures-no-compiles-clean/7/user-7-session-16.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-16.v
@@ -386,4 +386,4 @@ Proof.
  | Hcontra:|- ?t1 << ?t2
    |- False => remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]); subst
  end).
-(inversion Heqx).
+(rewrite mk_nf_pair in Heqx).
