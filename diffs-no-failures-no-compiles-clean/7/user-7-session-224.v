815b81910db0ee6b8944856531538fd407ed7ff2
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:37:29 -0700

    user 7, session 224, part 19

--- diffs-no-failures-no-compiles-clean/7/user-7-session-224.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-224.v
@@ -184,4 +184,25 @@ reflexivity.
 }
 {
 (apply match_ty_union_1).
-(rewrite f_subst_not_b_free_in_ty; try assumption).
+(rewrite f_subst_not_b_free_in_ty; auto).
+}
+}
+{
+(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
+(apply match_ty_union_2; auto).
+}
+{
+(destruct (f_free_in_ty__dec X' t'2) as [HXt'2| HXt'2]).
+{
+(apply match_ty_union_2; auto).
+}
+{
+(apply match_ty_union_2).
+(rewrite f_subst_not_b_free_in_ty; auto).
+}
+}
+*
+(destruct (beq_idP X' i0)).
+{
+subst.
+(split; intros HX).
