ff0ecffc36aae06e2b66af23e3b1a79bf68c5197
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:23:38 -0700

    user 2, session 5, part 9

--- diffs-annotated/2/user-2-session-5.v
+++ diffs-annotated/2/user-2-session-5.v
@@ -19,17 +19,4 @@ Inductive term :=
   | App : term -> term -> term.
 Redirect "/tmp/coqmW6Ht8" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
-Module TermNotations.
-Notation "[ ]" := Nil (format "[ ]") : coucou_scope.
-Notation "[ x ]" := (Cons x Nil) : coucou_scope.
-Notation "[ x y .. z ]" := (Cons x (Cons y .. (Cons z Nil) ..))
-  (x  at level 200, y  at level 200, z  at level 200) : coucou_scope.
-End TermNotations.
-Redirect "/tmp/coqdiQ4aG" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Import TermNotations.
-Redirect "/tmp/coqixcG4d" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Open Scope coucou_scope.
-Check [Nil].
+Check [Nil Nil].
