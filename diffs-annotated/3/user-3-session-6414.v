6fa7171287a29bc3bcdde3f5851b5c6ba7731aa8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:45:53 -0700

    user 3, session 6414, part 56

--- diffs-annotated/3/user-3-session-6414.v
+++ diffs-annotated/3/user-3-session-6414.v
@@ -19,16 +19,11 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-(rewrite H; simpl; eauto).
+(intuition; eauto using log_length_ok_nil).
+(simpl in *; lia).
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coquvJho6"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqK5CR8r"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Qed.
-Theorem log_abstraction_nil d b :
-  diskGet d 0 = Some b -> block_to_addr b = 0 -> log_abstraction d nil.
-Proof.
-(unfold log_abstraction; intros).
-(split; eauto using log_length_ok_nil).
