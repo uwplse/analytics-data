c7e7f69f74497385ef959c6d255287c3a0047aa4
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:57:48 -0700

    user 3, session 10411, part 11

--- diffs-annotated/3/user-3-session-10411.v
+++ diffs-annotated/3/user-3-session-10411.v
@@ -95,20 +95,3 @@ inv_exec.
  end).
 (eapply H0 in H2; repeat deex).
 (eapply H in H9; simpl in *; safe_intuition repeat deex; eauto).
-(match goal with
- | Hexec:exec (rx _) _ _
-   |- _ => eapply RExec in Hexec; eapply H4 in Hexec; simpl; eauto
- end).
--
-inv_exec.
-+
-(match goal with
- | Hexec:exec p _ _ |- _ => eapply RExec in Hexec
- end).
-(eapply H0 in H2; repeat deex).
-(eapply H in H10; simpl in *; safe_intuition repeat deex; eauto).
-(match goal with
- | Hexec:exec (rx _) _ _
-   |- _ => eapply RExecCrash in Hexec; eauto; eapply H4 in Hexec; eauto
- end).
-+
