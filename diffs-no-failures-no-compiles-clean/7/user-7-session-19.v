cc63a92d91dc6ee4072fe7a77471cd0334c87837
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:33:34 -0700

    user 7, session 19, part 239

--- diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-19.v
@@ -103,3 +103,4 @@ constructor.
 Lemma value_sem_sub_k_union__value_sem_sub_k_component :
   forall k : nat, forall v : ty, value_type v -> forall ta tb : ty, ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
 Proof.
+(induction k; intros v Hv; induction Hv; intros Hsem).
