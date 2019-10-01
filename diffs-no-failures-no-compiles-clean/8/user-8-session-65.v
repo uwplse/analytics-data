3a01885bdabd7cc9f3688301354e6287c7ccd82a
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:39:18 -0700

    user 8, session 65, part 263

--- diffs-no-failures-no-compiles-clean/8/user-8-session-65.v
+++ diffs-no-failures-no-compiles-clean/8/user-8-session-65.v
@@ -1250,4 +1250,4 @@ constructor.
 (intros \207\129 TB).
 (repeat simpl_rewrite inSeq_correct; try apply inSeq_WT; try apply unitary_at1_WT;
   try apply source_symmetric_WT; trivial).
-(simpl_rewrite inSeq_correct).
+(unfold compose_super).
