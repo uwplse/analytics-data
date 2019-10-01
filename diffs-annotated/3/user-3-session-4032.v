4391c0abf9b7ee2f9ca3c0c5bcd1c02175553bd8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:37:45 -0700

    user 3, session 4032, part 2

--- diffs-annotated/3/user-3-session-4032.v
+++ diffs-annotated/3/user-3-session-4032.v
@@ -316,3 +316,6 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
+Check file_truncate.
+Check file_set_sz.
+Search -(addr -> word).
