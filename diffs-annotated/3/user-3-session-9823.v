90367dff270ff71e7bbb637dee21e7466c03ddd9
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:55:59 -0700

    user 3, session 9823, part 6

--- diffs-annotated/3/user-3-session-9823.v
+++ diffs-annotated/3/user-3-session-9823.v
@@ -8,21 +8,3 @@ Add Search Blacklist "Private_" "_subproof".
 Require Import POCS.
 Require Import OneDiskAPI.
 Require Import BadBlockAPI.
-Theorem init_ok : init_abstraction init recover abstr inited_any.
-Proof.
-(eapply then_init_compose; eauto).
-step_proc.
-(destruct (r == 0)).
--
-step_proc.
--
-step_proc.
-(destruct (lt_dec r0 (diskSize (stateDisk state)))).
-+
-step_proc.
-(case_eq (diskGet (stateDisk state) (diskSize (stateDisk state) - 1)); intros).
-{
-exists (diskUpd (diskShrink (stateDisk state)) (stateBadBlock state) b).
-(unfold inited_any; split; auto).
-(constructor; intuition; autorewrite with upd in *; intuition).
--
