c992333188ba091f00a811cf1f349858fdf16cbb
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:27:35 -0700

    user 3, session 960, part 94

--- diffs-annotated/3/user-3-session-960.v
+++ diffs-annotated/3/user-3-session-960.v
@@ -7,4 +7,3 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import ProofIrrelevance.
 From Coq Require Export String.
-Search -nat -"ind".
