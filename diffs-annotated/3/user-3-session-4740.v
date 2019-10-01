baac3c0decae225d124bf0039995f908fdefb4d8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:40:28 -0700

    user 3, session 4740, part 2

--- diffs-annotated/3/user-3-session-4740.v
+++ diffs-annotated/3/user-3-session-4740.v
@@ -16,3 +16,5 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Notation "a \226\137\161 b" := (a = b) (at level 30).
+Notation "a === b" := (a = b) (at level 30).
+Check 3 === 3.
