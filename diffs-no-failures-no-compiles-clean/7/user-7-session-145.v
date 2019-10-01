79a0c86e12afa26ebf3f7845b2753030c383c9c4
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:36:01 -0700

    user 7, session 145, part 7

--- diffs-no-failures-no-compiles-clean/7/user-7-session-145.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-145.v
@@ -71,3 +71,4 @@ Lemma match_ty_ev__match_ty_any :
   forall (k w : nat) (X : id) (t : ty), fresh_in_ty X t -> |-[ k, w] TEV X <$ t -> forall v : ty, value_type v -> |-[ k, w] v <$ t.
 Proof.
 (intros k w X t HX Hm v Hv).
+(induction t).
