893e4d8ac9abb284c0e0b0d18abecb46b1a87093
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 16:57:25 -0700

    user 3, session 10289, part 9

--- diffs-annotated/3/user-3-session-10289.v
+++ diffs-annotated/3/user-3-session-10289.v
@@ -5,76 +5,17 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Import Coq.Strings.String.
-Import StringSyntax.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqrFgtUU"
+Inductive Marker : string -> Type :=
+    mark : forall s, Marker s.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqaM0FGo"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqIuiiUa"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqulu2Yx"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
-Require Import Helpers.Helpers.
-Require Import Proc.
-Require Import ProcTheorems.
-Record LayerAbstraction State1 State2 :={abstraction :
-                                          State1 -> State2 -> Prop}.
-Definition Abstraction State := LayerAbstraction world State.
-Definition abstraction_compose `(abs1 : Abstraction State1)
-  `(abs2 : LayerAbstraction State1 State2) :=
-  {|
-  abstraction := fun w state' =>
-                 exists state,
-                   abstraction abs2 state state' /\ abstraction abs1 w state |}.
-Definition IdAbstraction State : LayerAbstraction State State :=
-  {| abstraction := fun state state' => state' = state |}.
-Record SpecProps T R State :=
- Spec {pre : Prop; post : T -> State -> Prop; recovered : R -> State -> Prop}.
-Definition Specification A T R State := A -> State -> SpecProps T R State.
-Definition proc_spec `(spec : Specification A T R State) 
-  `(p : proc T) `(rec : proc R) `(abs : Abstraction State) :=
-  forall a w state,
-  abstraction abs w state ->
-  pre (spec a state) ->
-  forall r,
-  rexec p rec w r ->
-  match r with
-  | RFinished v w' =>
-      exists state',
-        abstraction abs w' state' /\ post (spec a state) v state'
-  | Recovered v w' =>
-      exists state',
-        abstraction abs w' state' /\ recovered (spec a state) v state'
-  end.
-Definition spec_impl `(spec1 : Specification A' T R State)
-  `(spec2 : Specification A T R State) :=
-  forall a state,
-  pre (spec2 a state) ->
-  exists a',
-    pre (spec1 a' state) /\
-    (forall v state',
-     post (spec1 a' state) v state' -> post (spec2 a state) v state') /\
-    (forall rv state',
-     recovered (spec1 a' state) rv state' ->
-     recovered (spec2 a state) rv state').
-Theorem proc_spec_weaken :
-  forall `(spec1 : Specification A T R State)
-    `(spec2 : Specification A' T R State) `(p : proc T) 
-    `(rec : proc R) (abs : Abstraction State),
-  proc_spec spec1 p rec abs ->
-  spec_impl spec1 spec2 -> proc_spec spec2 p rec abs.
-Proof.
-(unfold proc_spec at 2; intros).
-(eapply H0 in H2; eauto; repeat deex).
-(eapply H in H3; eauto).
-(destruct r; simpl in *; repeat deex; intuition eauto).
-Qed.
-Hint Resolve tt: core.
-Inductive Marker : String.string -> Type :=
-    mark : forall s, Marker s.
 Theorem proc_spec_rx :
   forall `(spec : Specification A T R State) `(p : proc T) 
     `(rec : proc R) `(rx : T -> proc T')
@@ -96,15 +37,18 @@ Theorem proc_spec_rx :
          post := fun r state'' => post (spec' a' state) r state'';
          recovered := fun r state'' => recovered (spec' a' state) r state'' |})
         (rx r) rec abs)) -> proc_spec spec' (Bind p rx) rec abs.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqAYn5wi"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq7GSvsZ"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
 Proof.
 (unfold proc_spec at 3; intros).
+inv_rexec.
+-
+inv_exec.
+(match goal with
+ | Hexec:exec p _ _ |- _ => eapply RExec in Hexec
+ end).
+(eapply H0 in H2; repeat deex).
+(eapply H in H9; simpl in *; safe_intuition repeat deex; eauto).
+(match goal with
+ | Hexec:exec (rx _) _ _
+   |- _ => eapply RExec in Hexec; eapply H4 in Hexec; eauto
+ end).
+-
