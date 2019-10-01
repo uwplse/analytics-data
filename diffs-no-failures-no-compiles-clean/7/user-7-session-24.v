ece508b97fae8c33856f69a980c2c95538e9084f
Talia Ringer <tringer@cs.washington.edu>
2019-09-30 17:34:05 -0700

    user 7, session 24, part 31

--- diffs-no-failures-no-compiles-clean/7/user-7-session-24.v
+++ diffs-no-failures-no-compiles-clean/7/user-7-session-24.v
@@ -56,4 +56,5 @@ Fixpoint match_ty (k : nat) :=
 where "|-[ k ']' v '<$' t" := (match_ty k v t) : btjm_scope.
 Theorem match_ty__value_type_l : forall (k : nat) (t v : ty), |-[ k] v <$ t -> value_type v.
 (intros k; induction t; induction v; intros Hm; try (solve [ constructor | destruct k; contradiction ])).
-(destruct k; simpl in Hm).
+(destruct k).
+(simpl in Hm).
