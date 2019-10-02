edf22b76c663afaa12761a9bfc0bb155c1af4168
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:05 -0700

    user 7, session 19, part 194

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,22 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(apply match_ty_i_ref__weak_inv in Hm).
-(destruct Hm as [t' Heq]; subst).
-constructor.
-Qed.
-Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
-Proof.
-(induction k; induction t; induction t'; intros H).
-32: {
-idtac.
-(simpl).
-(apply le_n_S).
-(apply IHk).
-(assert (Hv : value_type (TRef t)) by constructor).
-(assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; constructor)).
-specialize (H _ Hm).
-(apply match_ty_i_ref__inv in H).
-(destruct H as [tx [Heq Href]]; inversion Heq; subst).
-auto.
+(intros v; tauto).
 (* Failed. *)
