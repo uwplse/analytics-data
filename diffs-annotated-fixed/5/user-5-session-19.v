7f97942c7a696682cff050263d6e8a0a5d6a5199
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:45:06 -0700

    user 5, session 19, part 38

--- diffs-annotated-fixed/5/user-5-session-19.v
+++ diffs-annotated-fixed/5/user-5-session-19.v
@@ -198,14 +198,5 @@ intro x.
 (apply evalChoose).
 exists (eval L env (Int 3)).
 (apply evalEqTrue).
-(rewrite evalTimes with (i := 3%Z) (j := 2%Z)).
--
-reflexivity.
--
-(rewrite evalVar).
-(rewrite extendEnv_eq).
-(apply evalIntConst).
--
-reflexivity.
 (* Auto-generated comment: Succeeded. *)
 
