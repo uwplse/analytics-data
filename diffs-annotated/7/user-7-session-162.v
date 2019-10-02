2562ee991ed48fbc5a4b09bba85779efdb6f3979
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:23 -0700

    user 7, session 162, part 1

--- diffs-annotated/7/user-7-session-162.v
+++ diffs-annotated/7/user-7-session-162.v
@@ -47,5 +47,5 @@ Definition fresh_in_ty (X : id) (t : ty) := fresh X (FV t).
 Definition free_in_ty (X : id) (t : ty) := IdSet.In X (FV t).
 Hint Unfold fresh fresh_in_ty free_in_ty: DBBetaJulia.
 Variable (fresh_var_exist : forall fvs : id_set, exists Z : id, fresh Z fvs).
-Search -IdSet.
-(* Auto-generated comment: Failed. *)
+Print IdSet.
+(* Failed. *)
