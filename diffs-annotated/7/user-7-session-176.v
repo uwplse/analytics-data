63bb1d6a430029b3b6e68429f8a7a15aee699fc8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:31 -0700

    user 7, session 176, part 38

--- diffs-annotated/7/user-7-session-176.v
+++ diffs-annotated/7/user-7-session-176.v
@@ -146,5 +146,10 @@ Lemma match_ty__subst_neq_permute :
   try (solve
    [ split; intros Hm; assumption
    | split; repeat rewrite subst_pair; intros Hm; apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst;
-      destruct (IHt1 v1) as [Hm11 Hm12]; destruct (IHt2 v2) as [Hm21 Hm22]; apply match_ty_pair; tauto ])).
+      destruct (IHt1 v1) as [Hm11 Hm12]; destruct (IHt2 v2) as [Hm21 Hm22]; apply match_ty_pair; tauto
+   | split; repeat rewrite subst_union; intros Hm; apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm];
+      [ destruct (IHt1 v) as [Hm1 Hm2] | destruct (IHt2 v) as [Hm1 Hm1] ]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto ])).
+(split; repeat rewrite subst_union; intros Hm; apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm];
+  [ destruct (IHt1 v) as [Hm1 Hm2] | destruct (IHt2 v) as [Hm1 Hm1] ]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; 
+  tauto Abort).
 (* Failed. *)
