c3bfebae64879db3b52c0eaa5397ccd8517040a4
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:11 -0700

    user 7, session 19, part 307

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -16,6 +16,7 @@ Add Search Blacklist "Private_" "_subproof".
    | Hsem:forall v, value_type v -> |-[ ?k] v <$ TPair ?v1 ?v2 -> _
      |- _ =>
          assert (Hvv : value_type (TPair v1 v2)) by (constructor; assumption);
-          assert (Hmv : |-[ k] TPair v1 v2 <$ TPair v1 v2) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hvv Hmv)
+          assert (Hmv : |-[ k] TPair v1 v2 <$ TPair v1 v2) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hvv Hmv);
+          apply match_ty_i_union__inv in Hsem; destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hv' Hm'
    end).
 (* Failed. *)
