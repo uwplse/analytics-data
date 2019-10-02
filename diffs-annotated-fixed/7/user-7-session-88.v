f9c1199303d521e27fc428e8bec3eb8edb769894
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:49:36 -0700

    user 7, session 88, part 20

--- diffs-annotated-fixed/7/user-7-session-88.v
+++ diffs-annotated-fixed/7/user-7-session-88.v
@@ -424,8 +424,11 @@ Proof.
 (assert (Hv : value_type (TCName c)) by constructor).
 specialize (IHt'1 Hnft).
 specialize (IHt'2 Hnft).
-(destruct Hdep as [Hdept| Hdept']).
+(pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ Hsem) as Hsemu).
+(destruct Hsemu as [Hsemu| Hsemu]).
 +
-(destruct IHt'1 as [IHt'1| IHt'1]).
+(destruct Hdep as [Hdept| Hdept']).
+*
+specialize (IHt'1 (left Hdept)).
 (* Auto-generated comment: Failed. *)
 
