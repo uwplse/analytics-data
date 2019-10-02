152c21349c3da2342602e86ab37b62dd8813ac5b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:31 -0700

    user 3, session 960, part 56

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -121,22 +121,12 @@ Next Obligation.
 Next Obligation.
 Proof.
 subst digit.
+Next Obligation.
+Proof.
+subst digit.
 (apply PeanoNat.Nat.div_lt; auto; try lia).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqKBG919"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
 Qed.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqxhBuKV"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqKrqPJq"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+Next Obligation.
+Proof.
 (* Auto-generated comment: Succeeded. *)
 
