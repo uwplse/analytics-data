54e52c3f0da3d31047946b4c5e7aafa3236a84c2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:51:16 -0700

    user 7, session 113, part 17

--- diffs-annotated-fixed/7/user-7-session-113.v
+++ diffs-annotated-fixed/7/user-7-session-113.v
@@ -21,8 +21,6 @@ Proof.
 (induction t; intros s1 s2; try (solve [ simpl; reflexivity ]); try (specialize (IHt1 s1 s2); specialize (IHt2 s1 s2)); try specialize (IHt s1 s2)).
 (induction t; intros s1 s2;
   try (solve [ simpl; reflexivity | specialize (IHt1 s1 s2); specialize (IHt2 s1 s2); simpl; rewrite IHt1; rewrite IHt2; reflexivity ])).
--
-(simpl).
-(rewrite IHt).
+(induction t; intros s1 s2; try (solve [ simpl; reflexivity | simpl; rewrite IHt1; rewrite IHt2; reflexivity ])).
 (* Auto-generated comment: Failed. *)
 
