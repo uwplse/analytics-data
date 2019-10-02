c6461919dd84c3d458af8baa9cddb5138d27f421
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:23 -0700

    user 7, session 162, part 0

---
+++ diffs-annotated/7/user-7-session-162.v
@@ -0,0 +1,51 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Create HintDb DBBetaJulia.
+Declare Scope btjt_scope.
+Delimit Scope btjt_scope with btjt.
+Open Scope btjt.
+Inductive cname : Type :=
+  | NInt : _
+  | NFlt : _
+  | NStr : _.
+Inductive ty : Type :=
+  | TCName : cname -> ty
+  | TPair : ty -> ty -> ty
+  | TUnion : ty -> ty -> ty
+  | TExist : id -> ty -> ty
+  | TVar : id -> ty
+  | TEV : id -> ty.
+Definition tint := TCName NInt.
+Definition tflt := TCName NFlt.
+Definition tstr := TCName NStr.
+Definition tIntInt := TPair tint tint.
+Definition vX := Id 1.
+Definition vY := Id 2.
+Definition vZ := Id 3.
+Definition tX := TVar vX.
+Definition tY := TVar vY.
+Definition teXX := TExist vX tX.
+Fixpoint FV (t : ty) : id_set :=
+  match t with
+  | TCName _ => IdSet.empty
+  | TPair t1 t2 => IdSet.union (FV t1) (FV t2)
+  | TUnion t1 t2 => IdSet.union (FV t1) (FV t2)
+  | TExist y t' => IdSet.remove y (FV t')
+  | TVar y => IdSet.singleton y
+  | TEV _ => IdSet.empty
+  end.
+Definition fresh (X : id) (fvs : id_set) := ~ IdSet.In X fvs.
+Definition fresh_in_ty (X : id) (t : ty) := fresh X (FV t).
+Definition free_in_ty (X : id) (t : ty) := IdSet.In X (FV t).
+Hint Unfold fresh fresh_in_ty free_in_ty: DBBetaJulia.
+Variable (fresh_var_exist : forall fvs : id_set, exists Z : id, fresh Z fvs).
+Search -IdSet.
+(* Auto-generated comment: Failed. *)
