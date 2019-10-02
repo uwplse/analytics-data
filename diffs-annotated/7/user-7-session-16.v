0ac0aa0270dd7bc524dfd4ed3142d689671275bb
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:51 -0700

    user 7, session 16, part 67

--- diffs-annotated/7/user-7-session-16.v
+++ diffs-annotated/7/user-7-session-16.v
@@ -218,7 +218,23 @@ tauto.
 +
 (apply sub_r_union_l__inv in Hsub').
 Lemma sub_r__mk_nf_sub_r1 : forall t t' : ty, |- t << t' -> |- MkNF( t) << t'.
-Check in_nf_union__inv.
+Ltac
+ solve_atom_sub_r_union__decidable IHt2_1 IHt2_2 :=
+  destruct IHt2_1 as [IH1| IH1]; try assumption; destruct IHt2_2 as [IH2| IH2]; try assumption;
+   try (solve [ left; apply SR_UnionR1; assumption | left; apply SR_UnionR2; assumption ]); right; intros Hcontra;
+   apply atom_sub_r_union__inv in Hcontra; tauto || constructor.
+Lemma nf_sub_r__decidable2 :
+  forall t : ty,
+  InNF( t) -> (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t)).
+Proof.
+(apply
+  (in_nf_mut
+     (fun (t : ty) (Hat : atom_type t) =>
+      (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t)))
+     (fun (t : ty) (Hnf : in_nf t) =>
+      (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t))))).
+-
+(intros c).
 (split; intros t'; induction t'; intros Hnf'; try destruct (in_nf_union__inv _ _ Hnf') as [Hnf'1 Hnf'2];
   try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 ]);
   try
@@ -227,4 +243,11 @@ Check in_nf_union__inv.
          destruct (cname_eq__decidable c1 c2);
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
++
+right.
+(match goal with
+ | |- ~ |- ?t1 << ?t2 =>
+       remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]);
+        subst
+ end).
 (* Failed. *)
