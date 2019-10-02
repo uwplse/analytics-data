b6765f46e95a27694adf0e6be4fb18a81fe67edb
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:52 -0700

    user 8, session 31, part 77

--- diffs-annotated/8/user-8-session-31.v
+++ diffs-annotated/8/user-8-session-31.v
@@ -2666,4 +2666,3 @@ specialize (t0 \206\1471' (\206\1471' \226\139\147 \206\1470) p2 M'' t').
 (apply types_pat_no_trail in t').
 (rewrite <- t').
 (simpl_rewrite (trim_merge_dist \206\1471' \206\1470)).
-unify_pows_two.
