af381314674d1395966f7cab4a97974d6bbaa0e5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:22 -0700

    user 7, session 161, part 13

--- diffs-annotated/7/user-7-session-161.v
+++ diffs-annotated/7/user-7-session-161.v
@@ -103,7 +103,6 @@ assumption.
 *
 subst.
 (rewrite subst_exist_eq).
-(apply match_ty_exist__inv in Hm).
-(destruct Hm as [ti Hm]).
 (rewrite (subst_exist_neq _ _ _ _ Hbeq)).
+exists ([X' := tx] ti).
 (* Failed. *)
