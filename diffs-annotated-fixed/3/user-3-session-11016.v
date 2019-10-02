61fc445f03893fe212f51e929a4fbc87b353881a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:41:45 -0700

    user 3, session 11016, part 9

--- diffs-annotated-fixed/3/user-3-session-11016.v
+++ diffs-annotated-fixed/3/user-3-session-11016.v
@@ -238,5 +238,18 @@ Example test_nonzeros : nonzeros [0; 1; 0; 2; 3; 0; 0] = [1; 2; 3].
 Proof.
 reflexivity.
 Qed.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq2blqVv"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqaOYrJb"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Example test_oddmembers : oddmembers [0; 1; 0; 2; 3; 0; 0] = [1; 3].
+Proof.
+reflexivity.
+Qed.
 (* Auto-generated comment: Failed. *)
 
