c60374721dae574eac891bf76c2918d143b353e6
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:46 -0700

    user 7, session 98, part 11

--- diffs-annotated/7/user-7-session-98.v
+++ diffs-annotated/7/user-7-session-98.v
@@ -88,7 +88,9 @@ tauto.
 Qed.
 (apply match_ty_ref__weak_inv in Hm).
 split.
-split.
-assumption.
-(exists tx; reflexivity).
+(split; eauto).
+-
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hmx]).
+(simpl).
 (* Failed. *)
