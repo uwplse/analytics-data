93854758fe6b1a44ee7461c8562186adc22dc6fb
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:07 -0700

    user 7, session 19, part 238

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -5,4 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 Lemma value_sem_sub_k_union__value_sem_sub_k_component :
   forall k : nat, forall v : ty, value_type v -> forall ta tb : ty, ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
 Proof.
+(induction k; intros v Hv; induction Hv; intros Hsem).
 (* Failed. *)
