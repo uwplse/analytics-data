53ff4625b68e6601dd76d1b830eb835f11cccc2d
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:07 -0700

    user 7, session 226, part 1

--- diffs-annotated/7/user-7-session-226.v
+++ diffs-annotated/7/user-7-session-226.v
@@ -52,6 +52,12 @@ reflexivity.
 (induction w'; exists 0; induction t'; intros Hm'; try (solve [ destruct v; contradiction || tauto ])).
 +
 (rewrite f_subst_union).
-(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
-  destruct IHt' as [IHt'a IHt'b]; split; intros HX').
-(* Auto-generated comment: Failed. *)
+*
+(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
+(apply match_ty_union_1; auto).
+*
+(destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
+{
+specialize (IHt'b HXt'1).
+(destruct IHt'b as [w2 IHt'b]).
+(* Failed. *)
