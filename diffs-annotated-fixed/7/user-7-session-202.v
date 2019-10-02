9dc0d8271dc71404eb5ee7af7d7b9e0072cac8e3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:54:04 -0700

    user 7, session 202, part 9

--- diffs-annotated-fixed/7/user-7-session-202.v
+++ diffs-annotated-fixed/7/user-7-session-202.v
@@ -117,6 +117,7 @@ Qed.
 Lemma wf_ty_pair__inv : forall t1 t2 : ty, wf_ty (TPair t1 t2) -> wf_ty t1 /\ wf_ty t2.
 Proof.
 (intros t1 t2 Hwf).
-(unfold wf_ty in *; simpl).
+(unfold wf_ty in *; simpl in *).
+Search -IdSet.Equal.
 (* Auto-generated comment: Failed. *)
 
