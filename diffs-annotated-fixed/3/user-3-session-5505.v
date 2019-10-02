2c6bee1bd35084d0df8dc20907b014e5c7857442
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:10:55 -0700

    user 3, session 5505, part 7

--- diffs-annotated-fixed/3/user-3-session-5505.v
+++ diffs-annotated-fixed/3/user-3-session-5505.v
@@ -70,26 +70,11 @@ intuition eauto.
 (simpl write).
 step_proc.
 intuition eauto.
-+
+{
 (exists 0; simpl; auto).
-+
+}
+{
 (exists 1; simpl; auto).
-+
-specialize (IHblocks (off + 1)).
-step_proc.
-intuition subst; eauto.
-*
-(f_equal; lia).
-*
-(repeat deex).
-(exists (S nwritten); simpl).
-(f_equal; lia).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqYoLYaG"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Qed.
+}
 (* Auto-generated comment: Succeeded. *)
 
