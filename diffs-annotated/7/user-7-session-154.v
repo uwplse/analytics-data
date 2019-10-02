cf77e74bd6a82f93c87cad129bb370bf20180b24
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:18 -0700

    user 7, session 154, part 1

--- diffs-annotated/7/user-7-session-154.v
+++ diffs-annotated/7/user-7-session-154.v
@@ -34,5 +34,66 @@ Proof.
 (apply fresh_union__inv in Hfresh).
 assumption.
 Qed.
-Lemma subst_pair : forall (X : id) (s t1 t2 : ty), [X := s] TPair t1 t2 = TPair ([X := s] t1) ([X := s] t2).
-(* Auto-generated comment: Failed. *)
+Proof.
+(intros; reflexivity).
+Qed.
+Lemma subst_union : forall (X : id) (s t1 t2 : ty), [X := s] TUnion t1 t2 = TUnion ([X := s] t1) ([X := s] t2).
+Proof.
+(intros; reflexivity).
+Qed.
+Lemma subst_fresh_in_ty : forall (X : id) (t : ty), fresh_in_ty X t -> forall s : ty, [X := s] t = t.
+Proof.
+(intros X t).
+(induction t; intros Hfresh s; try (solve [ reflexivity ]); unfold fresh_in_ty in *; simpl in Hfresh; simpl;
+  try (solve
+   [ apply fresh_union__inv in Hfresh; destruct Hfresh as [Hfresh1 Hfresh2]; rewrite IHt1; try assumption; rewrite IHt2; try assumption;
+      reflexivity ])).
+-
+(destruct (beq_idP X i); try reflexivity).
+(rewrite IHt).
+reflexivity.
+(unfold fresh in *).
+(intros Hcontra).
+(apply Hfresh).
+(apply IdSetFacts.remove_2; try assumption).
+(intros Heq).
+subst.
+contradiction.
+-
+(unfold fresh in Hfresh).
+(destruct (beq_idP X i); try reflexivity).
+subst.
+exfalso.
+(apply Hfresh).
+(apply IdSetFacts.singleton_2).
+reflexivity.
+Qed.
+Lemma subs_neq__permute :
+  forall X Y : id, X <> Y -> forall t s1 s2 : ty, fresh_in_ty X s2 -> fresh_in_ty Y s1 -> [X := s1] ([Y := s2] t) = [Y := s2] ([X := s1] t).
+Proof.
+(intros X Y Hneq t).
+(induction t; intros s1 s2 HXs2 HYs1;
+  try (solve [ simpl; reflexivity | simpl; rewrite IHt1; try assumption; rewrite IHt2; try assumption; reflexivity ])).
+-
+(simpl).
+(destruct (beq_idP X i)).
++
+subst.
+(destruct (beq_idP Y i); reflexivity).
++
+(destruct (beq_idP Y i)).
+*
+subst.
+reflexivity.
+*
+(rewrite IHt; try assumption).
+reflexivity.
+-
+(simpl; destruct (beq_idP X i); destruct (beq_idP Y i); subst).
++
+contradiction.
++
+(simpl).
+(rewrite <- beq_id_refl).
+symmetry.
+(apply subs_fresh_in_ty).
