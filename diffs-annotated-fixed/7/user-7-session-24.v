806849bcfa806a42aa8497f5e208f12e8fbe3544
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:48:19 -0700

    user 7, session 24, part 2

--- diffs-annotated-fixed/7/user-7-session-24.v
+++ diffs-annotated-fixed/7/user-7-session-24.v
@@ -36,6 +36,6 @@ Fixpoint inv_depth (t : ty) :=
   | TUnion t1 t2 => Nat.max (| t1 |) (| t2 |)
   | TRef t' => 1 + | t' |
   end
-where "'|' t '|'" := (inv_depth t) : btjm_scope.
+where "'|'t'|'" := (inv_depth t) : btjm_scope.
 (* Auto-generated comment: Failed. *)
 
