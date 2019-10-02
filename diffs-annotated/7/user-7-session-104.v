4009811b2c8850c38337f3180396455fb73b292f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:52 -0700

    user 7, session 104, part 0

---
+++ diffs-annotated/7/user-7-session-104.v
@@ -0,0 +1,18 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0270a.BaseDefs.
+Require Import BetaJulia.Sub0270a.BaseMatchProps.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Open Scope btjm.
+Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
+Proof.
+(intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
+Qed.
+(* Auto-generated comment: Failed. *)
