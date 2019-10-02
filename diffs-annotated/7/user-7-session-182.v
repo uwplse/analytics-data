b3518ccb04e63e2984f4d1c3ad172ced32455d62
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:33 -0700

    user 7, session 182, part 1

--- diffs-annotated/7/user-7-session-182.v
+++ diffs-annotated/7/user-7-session-182.v
@@ -8,4 +8,32 @@ Require Import BetaJulia.Sub0281a.BaseDefs.
 Require Import BetaJulia.Sub0281a.BaseProps.
 Require Import BetaJulia.Sub0281a.BaseMatchProps.
 Require Import BetaJulia.Sub0281a.BaseSemSubProps.
-(* Auto-generated comment: Failed. *)
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Lemma subst_nested :
+  forall (X Y : id) (tX tY : ty), X <> Y -> fresh_in_ty Y tX -> forall t : ty, [X := tX] ([Y := tY] t) = [Y := [X := tX] tY] ([X := tX] t).
+Proof.
+(intros X Y tX tY Hneq HYfresh t).
+(induction t; try reflexivity).
+-
+(repeat rewrite subst_pair).
+(rewrite IHt1, IHt2).
+reflexivity.
+-
+(repeat rewrite subst_union).
+(rewrite IHt1, IHt2).
+reflexivity.
+-
+(destruct (beq_idP Y i) as [HY| HY]).
++
+subst.
+(destruct (beq_idP X i) as [HX| HX]).
+*
+subst.
+contradiction.
+*
+(rewrite subst_exist_eq).
+(rewrite subst_id).
+(* Failed. *)
