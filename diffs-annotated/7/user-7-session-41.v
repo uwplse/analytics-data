85a05facc24247e62f603afcb45e89deb5e748c7
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:22 -0700

    user 7, session 41, part 5

--- diffs-annotated/7/user-7-session-41.v
+++ diffs-annotated/7/user-7-session-41.v
@@ -8,13 +8,6 @@ Require Import BetaJulia.Sub0250a.BaseDefs.
 Require Import BetaJulia.Sub0250a.BaseProps.
 Require Import BetaJulia.Sub0250a.AltMatchDef.
 Require Import Coq.Lists.List.
-Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
-Proof.
-(intros t1 t2 Hsub).
-(unfold sem_sub).
-(induction Hsub; intros k v Hm).
--
-assumption.
--
+(unfold sem_sub_k in *).
 auto.
 (* Failed. *)
