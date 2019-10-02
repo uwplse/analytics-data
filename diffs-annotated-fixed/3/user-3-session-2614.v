f5f165bba9cd89e22239236d677ee0f594b0a436
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:00:17 -0700

    user 3, session 2614, part 22

--- diffs-annotated-fixed/3/user-3-session-2614.v
+++ diffs-annotated-fixed/3/user-3-session-2614.v
@@ -37,16 +37,6 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4QF2VP"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Import Notations.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqwBSZ54"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqAZLvma"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
-(* Auto-generated comment: Succeeded. *)
+Import EqualDec.Notations.
+(* Auto-generated comment: Failed. *)
 
