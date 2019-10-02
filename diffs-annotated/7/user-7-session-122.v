f1eca954a2b49413c4a16ea085a079bd89d70cb2
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:05 -0700

    user 7, session 122, part 1

--- diffs-annotated/7/user-7-session-122.v
+++ diffs-annotated/7/user-7-session-122.v
@@ -40,5 +40,6 @@ Proof.
 exists (Nat.max w21 w22).
 (intros v Hm).
 (apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ specialize (Hsem1 _ Hm) | specialize (Hsem2 _ Hm) ]).
-(* Auto-generated comment: Failed. *)
+(destruct Hm as [Hm| Hm]; [ specialize (Hsem1 _ Hm) | specialize (Hsem2 _ Hm) ]; [ apply match_ty_union_1 | apply match_ty_union_2 ];
+  eapply match_ty__ge_w; try eassumption).
+(* Failed. *)
