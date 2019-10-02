833af2380eb75caa6006b995484e024dba1d8ae0
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:43 -0700

    user 7, session 10, part 21

--- diffs-annotated/7/user-7-session-10.v
+++ diffs-annotated/7/user-7-session-10.v
@@ -46,6 +46,7 @@ Proof.
 }
 {
 (rewrite unite_pairs_atom_union; try assumption).
+(destruct (atom_sub_r_union__inv _ _ _ Hsub2 H1) as [Hsub21| Hsub22]; [ apply SR_UnionR1 | apply SR_UnionR2 ]; tauto).
 }
 *
 (intros Hnf2'; intros Hsub1 Hsub2).
@@ -60,14 +61,27 @@ Proof.
 -
 (intros Hnf1' Hnf2 Hn2' Hsub1 Hsub2).
 (rewrite (unite_pairs_union_t t1 t0 t2)).
-Check sub_r_union_l__inv.
 (destruct (sub_r_union_l__inv _ _ _ Hsub1) as [Hsub11 Hsub12]).
 (constructor; tauto).
 Qed.
 Lemma unite_pairs_of_nf__preserves_sub_r1 :
   forall t1 t2 t1' t2' : ty, InNF( t1) -> |- t1 << t1' -> InNF( t2) -> |- t2 << t2' -> |- unite_pairs t1 t2 << TPair t1' t2'.
 Proof.
-(intros ta; induction ta; intros tb; induction tb; intros ta' tb' Hnf1 Hsub1 Hnf2 Hsub2; try (solve [ simpl; constructor; assumption ])).
+(intros ta; induction ta; intros tb; induction tb; intros ta' tb' Hnf1 Hsub1 Hnf2 Hsub2;
+  try (solve
+   [ simpl; constructor; assumption
+   | match goal with
+     | Hnf1:InNF( ?t), Hnf2:InNF( TUnion ?t1 ?t2), Hsub:|- TUnion ?t1 ?t2 << _
+       |- |- unite_pairs ?t (TUnion ?t1 ?t2) << TPair _ _ =>
+           destruct (in_nf_union__inv _ _ Hnf2) as [Hnfb1 Hnfb2]; destruct (sub_r_union_l__inv _ _ _ Hsub) as [Hsubb1 Hsubb2];
+            rewrite unite_pairs_atom_union; try (solve [ constructor | inversion Hnf1; subst; assumption ]); constructor;
+            [ apply IHtb1 | apply IHtb2 ]; assumption
+     | Hnf1:InNF( ?t), Hnf2:InNF( TUnion ?t1 ?t2), Hsub:|- TUnion ?t1 ?t2 << _
+       |- |- unite_pairs (TUnion ?t1 ?t2) ?t << TPair ?tx ?ty =>
+           change_no_check (|- TUnion (unite_pairs t1 t) (unite_pairs t2 t) << TPair tx ty); destruct (in_nf_union__inv _ _ Hnf2) as [Hnfb1 Hnfb2];
+            destruct (sub_r_union_l__inv _ _ _ Hsub) as [Hsubb1 Hsubb2]; constructor; [ apply IHta1 | apply IHta2 ]; assumption
+     end ])).
+Qed.
 Lemma sub_r_unite_pairs_nf_l__inv :
   forall t1 t2 t1' t2' : ty, |- unite_pairs t1 t2 << TPair t1' t2' -> InNF( t1) -> InNF( t2) -> |- t1 << t1' /\ |- t2 << t2'.
 Proof.
@@ -104,4 +118,50 @@ Proof.
 (apply sub_r_unite_pairs_nf_l__inv in Hsub; try apply mk_nf__in_nf).
 (destruct Hsub; split; apply SR_NormalForm; assumption).
 Qed.
-(* Failed. *)
+Lemma mk_nf__sub_r_eq : forall t : ty, |- MkNF( t) << t /\ |- t << MkNF( t).
+Proof.
+(induction t).
+-
+(split; simpl; constructor).
+-
+(destruct IHt1; destruct IHt2).
+(split; simpl).
++
+(apply unite_pairs_of_nf__preserves_sub_r1; assumption || apply mk_nf__in_nf).
++
+(apply SR_NormalForm).
+(simpl).
+(apply sub_r__rflxv).
+-
+(destruct IHt1; destruct IHt2).
+(split; simpl; constructor; (apply SR_UnionR1; assumption) || (apply SR_UnionR2; assumption)).
+-
+(simpl).
+(destruct IHt).
+(split; constructor; assumption).
+Qed.
+Lemma mk_nf__sub_r1 : forall t : ty, |- MkNF( t) << t.
+Proof.
+(intros t).
+(pose proof (mk_nf__sub_r_eq t) as H; tauto).
+Qed.
+Lemma mk_nf__sub_r2 : forall t : ty, |- t << MkNF( t).
+Proof.
+(intros t).
+(pose proof (mk_nf__sub_r_eq t) as H; tauto).
+Qed.
+Lemma sub_r__mk_nf_sub_r1 : forall t t' : ty, |- t << t' -> |- MkNF( t) << t'.
+Proof.
+(intros t t' Hsub; induction Hsub; simpl; try (solve [ simpl; constructor; constructor ])).
+-
+(apply unite_pairs_of_nf__preserves_sub_r1; assumption || apply mk_nf__in_nf).
+-
+(constructor; assumption).
+-
+(apply SR_UnionR1; assumption).
+-
+(apply SR_UnionR2; assumption).
+-
+constructor.
+assumption.
+-
