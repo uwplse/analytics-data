c67af13198e1f5b289cd693a0bf3b83634918c46
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:49:05 -0700

    user 7, session 66, part 1

--- diffs-annotated-fixed/7/user-7-session-66.v
+++ diffs-annotated-fixed/7/user-7-session-66.v
@@ -226,7 +226,7 @@ Qed.
 Lemma value_type_matching_ty__exists : forall (t : ty) (k : nat), inv_depth t <= k -> exists v : ty, |-[ k] v <$ t.
 Proof.
 (intros t; induction t; intros k Hdep).
--
-(exists (TCName c); split).
+exists (TCName c).
+reflexivity.
 (* Auto-generated comment: Failed. *)
 
