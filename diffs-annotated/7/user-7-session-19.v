5df91d61fd8d0726bbf2812df4663858d15babec
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:06 -0700

    user 7, session 19, part 211

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,5 +2,19 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Lemma aaa : forall (k : nat) (t t' : ty), forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t' -> | t | <= | t' |.
+Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
+Proof.
+(induction k; induction t; induction t'; intros H; try (solve [ simpl; constructor ]);
+  try (solve
+   [ match goal with
+     | |- | ?t1 | <= | ?t2 | =>
+           (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+             (H _ Hm); contradiction) ||
+             (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
+               (H _ Hm); contradiction)
+     end ])).
+(match goal with
+ | |- | ?t1 | <= | ?t2 | =>
+       assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize (H _ Hm)
+ end).
 (* Failed. *)
