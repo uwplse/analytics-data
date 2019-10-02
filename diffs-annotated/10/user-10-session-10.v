35fb88b6d92653982baa81737073d52d9015d562
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:06:06 -0700

    user 10, session 10, part 18

--- diffs-annotated/10/user-10-session-10.v
+++ diffs-annotated/10/user-10-session-10.v
@@ -271,12 +271,12 @@ Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Check nmi_of_smi.
-Print taE.
 Definition network_of_app_ta sk : taE ~> itree tE :=
   fun _ tae =>
   match tae with
-  | (ae|) => network_of_app sk _ ae
+  | (ae|) => network_of_app sk _ (ae|)
   | (|(e|)) => trigger (map_exceptE Error_App _ e)
   | (||tae) => trigger tae
   end.
-(* Failed. *)
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqeEIa21" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
