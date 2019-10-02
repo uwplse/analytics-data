42a693fffb456aedb06c8efda3fdfd7fcc95cb29
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:15 -0700

    user 8, session 2, part 77

--- diffs-annotated/8/user-8-session-2.v
+++ diffs-annotated/8/user-8-session-2.v
@@ -45,4 +45,38 @@ Timeout 1 Print LoadPath.
 Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqToWrV3"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
-(* Failed. *)
+Lemma ctrls_to_list_transpose_fst :
+  forall W lb li (u : Unitary W),
+  fst (ctrls_to_list lb li u) = fst (ctrls_to_list lb li (trans u)).
+Proof.
+(induction W; intros lb li u; try (solve [ inversion u ])).
+-
+(destruct li as [| k li]).
+(repeat rewrite ctrls_to_list_empty).
+reflexivity.
+(dependent destruction u; simpl; reflexivity).
+-
+dependent destruction u.
++
+(simpl).
+(destruct li as [| k li]; trivial).
+specialize (IHW2 lb li u).
+(destruct (ctrls_to_list lb li u) as [[j l] v] eqn:E).
+(destruct (ctrls_to_list lb li (trans u)) as [[j' l'] v'] eqn:E').
+(inversion IHW2; subst).
+reflexivity.
++
+(simpl).
+(destruct li as [| k li]; trivial).
+specialize (IHW2 lb li u).
+(destruct (ctrls_to_list lb li u) as [[j l] v] eqn:E).
+(destruct (ctrls_to_list lb li (trans u)) as [[j' l'] v'] eqn:E').
+(inversion IHW2; subst).
+reflexivity.
+Qed.
+Lemma ctrls_to_list_transpose_snd :
+  forall W lb li (u : Unitary W),
+  (snd (ctrls_to_list lb li u)) \226\128\160 == snd (ctrls_to_list lb li (trans u)).
+Proof.
+(induction W; intros lb li u; try (solve [ inversion u ])).
+-
