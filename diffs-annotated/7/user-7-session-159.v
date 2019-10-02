e5f631130583e0a3eac5a609f6095d22309ee9ec
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:20 -0700

    user 7, session 159, part 3

--- diffs-annotated/7/user-7-session-159.v
+++ diffs-annotated/7/user-7-session-159.v
@@ -48,17 +48,11 @@ assumption.
 (apply match_ty_pair__inv in Hm).
 (destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
 (destruct (IHt1 _ Hm1) as [v1' Hm1']).
-(destruct (IHt2 _ Hm2) as [v2' Hm2']).
-exists (TPair v1' v2').
-(apply match_ty_pair; assumption).
--
-(rewrite subst_union in *).
-(apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ destruct (IHt1 _ Hm) as [v' Hm'] | destruct (IHt2 _ Hm) as [v' Hm'] ]; exists v';
-  [ apply match_ty_union_1 | apply match_ty_union_2 ]; assumption).
--
-(destruct (beq_idP X i) as [Hbeq| Hbeq]).
+(rewrite subst_exist_eq in *).
+(exists v; assumption).
 +
-subst.
-(rewrite subst_exist_eq).
+(rewrite (subst_exist_neq _ _ _ _ Hbeq) in Hm).
+(rewrite (subst_exist_neq _ _ _ _ Hbeq)).
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hm]).
 (* Failed. *)
