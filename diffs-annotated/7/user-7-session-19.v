209324cef51636633ea0c80f5d988aad30b31605
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:07 -0700

    user 7, session 19, part 243

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,5 +2,9 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(unfold sem_sub_k_i in *).
+(unfold sem_sub_k_i in Hsem).
+(assert (Hv : value_type (TRef t)) by constructor).
+Search -match_ty_i.
+(assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption)).
+(pose proof (Hse _ Hv Hm) as Hmu).
 (* Failed. *)
