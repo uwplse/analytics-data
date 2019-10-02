048906667b6c045ef55b8d7cafc02a18e0ac7e81
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:42 -0700

    user 7, session 192, part 18

--- diffs-annotated/7/user-7-session-192.v
+++ diffs-annotated/7/user-7-session-192.v
@@ -14,4 +14,6 @@ Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Lemma match_ty_subst_fresh : forall (X : id) (s : ty) (w : nat) (t v : ty), fresh_in_ty X t -> |-[ w] v <$ t -> |-[ w] v <$ [X := s] t.
 (intros X s; induction w; induction t; intros v HX Hm; try (solve [ rewrite subst_cname in *; assumption | rewrite subst_ev in *; assumption ])).
+-
+(destruct (fresh_in_ty _ _ _ HX) as [HX1 HX2]).
 (* Failed. *)
