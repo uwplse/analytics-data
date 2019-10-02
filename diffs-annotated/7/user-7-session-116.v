9dd188088ec7c31a8df69380eb876eee8e9769ca
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:01 -0700

    user 7, session 116, part 2

--- diffs-annotated/7/user-7-session-116.v
+++ diffs-annotated/7/user-7-session-116.v
@@ -21,5 +21,5 @@ Proof.
 (split; intros Hcontra).
 SearchPattern (IdSet.In (IdSet.union _ _)).
 Search -IdSet.union.
-(apply IdSetFacts.union_2 with fvs2 in Hcontra).
+(eapply IdSetFacts.union_2 with fvs2 in Hcontra).
 (* Failed. *)
