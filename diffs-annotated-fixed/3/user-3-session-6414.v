7a1f05c578ee52a8d7f64ca774abcf438f13a50d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:13:52 -0700

    user 3, session 6414, part 56

--- diffs-annotated-fixed/3/user-3-session-6414.v
+++ diffs-annotated-fixed/3/user-3-session-6414.v
@@ -136,6 +136,13 @@ Theorem log_abstraction_nil d b :
   diskGet d 0 = Some b -> block_to_addr b = 0 -> log_abstraction d nil.
 Proof.
 (unfold log_abstraction; intros).
-(split; eauto using log_length_ok_nil).
+(intuition; eauto using log_length_ok_nil).
+(simpl in *; lia).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqK5CR8r"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
