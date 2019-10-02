0bde8f6ed1ce7bb83234ced873005c783f733284
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:44 -0700

    user 7, session 94, part 18

--- diffs-annotated/7/user-7-session-94.v
+++ diffs-annotated/7/user-7-session-94.v
@@ -2,18 +2,3 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0260a.BaseDefs.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Open Scope btjm.
-Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
-Proof.
-(intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
-Qed.
-Lemma match_ty_exist__inv : forall (v : ty) (X : id) (t : ty) (k : nat), |-[ S k] v <$ TExist X t -> exists tx : ty, |-[ k] v <$ [X := tx] t.
-(induction k; intros v t; generalize dependent v; induction t; intros v Hm; try (solve [ apply match_ty_cname__inv in Hm; subst; constructor ])).
-(* Failed. *)
