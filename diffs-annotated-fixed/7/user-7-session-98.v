3d5b148fffe7c2ca802b287a8f234c9bc956d37c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:49:58 -0700

    user 7, session 98, part 4

--- diffs-annotated-fixed/7/user-7-session-98.v
+++ diffs-annotated-fixed/7/user-7-session-98.v
@@ -108,6 +108,9 @@ Lemma sem_sub__refint_eXrefX : ||- [TRef tint]<= [TExist vX (TRef tX)].
 Proof.
 (intros k; destruct k; intros v Hm).
 -
-(destruct v; simpl).
+(apply match_ty_ref__weak_inv in Hm).
+(destruct Hm as [t' Heq]; subst).
+(simpl).
+auto.
 (* Auto-generated comment: Failed. *)
 
