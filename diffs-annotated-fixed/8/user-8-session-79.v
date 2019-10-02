9db5ed66df72eb2a5ceb3b2412d236a57eb5b2e6
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:03:56 -0700

    user 8, session 79, part 1323

--- diffs-annotated-fixed/8/user-8-session-79.v
+++ diffs-annotated-fixed/8/user-8-session-79.v
@@ -1144,6 +1144,5 @@ reflexivity.
 specialize inSeq_correct as IS.
 (simpl in IS).
 (repeat (rewrite IS; unfold compose_super; compile_typing compile_WT)).
-clear IS.
 (* Auto-generated comment: Succeeded. *)
 
