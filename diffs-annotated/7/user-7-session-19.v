ad9de30d862e8e54e16cad2e29176d96d32fc3b0
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:03 -0700

    user 7, session 19, part 148

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -4,5 +4,5 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_pair; auto).
 (assert (Hv : value_type (TCName c)) by constructor; assert (Hm : |-[ 0] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption);
-  specialize (H 0 _ Hv); destruct H as [H _]; specialize (H Hm)).
+  specialize (H 0 _ Hv); destruct H as [H1 H2]; specialize (H1 Hm)).
 (* Failed. *)
