a4c0742e38d6b88268626b833f533aaeee633b23
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:03 -0700

    user 7, session 118, part 7

--- diffs-annotated/7/user-7-session-118.v
+++ diffs-annotated/7/user-7-session-118.v
@@ -134,11 +134,7 @@ Proof.
 (apply sem_sub_k__refl).
 Qed.
 Lemma sem_eq__refl : forall t : ty, ||- [t]= [t].
-Proof.
-(intros t k).
-(apply sem_eq_k__refl).
-Qed.
-Lemma sem_sub__trans : forall t1 t2 t3 : ty, ||- [t1]<= [t2] -> ||- [t2]<= [t3] -> ||- [t1]<= [t3].
-Proof.
-(intros t1 t2 t3).
+(intros t1 t2 t3 Hsem1 Hsem2).
+(intros k).
+(apply sem_sub_k__trans).
 (* Failed. *)
