dcf8846dd6d79f8594e5f62a691d0769153b2602
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:02 -0700

    user 7, session 19, part 136

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -4,7 +4,6 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_pair; auto).
 (match goal with
- | |- | ?t1 | = | ?t2 | =>
-       assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); destruct H as [H _]
+ | |- | ?t1 | = | ?t2 | => assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption)
  end).
 (* Failed. *)
