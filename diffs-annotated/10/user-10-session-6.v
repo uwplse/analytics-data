9e8346c9d94e3ce9de6e2772366163773edd1868
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:06:03 -0700

    user 10, session 6, part 0

---
+++ diffs-annotated/10/user-10-session-6.v
@@ -0,0 +1,11 @@
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqME2CHi"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+From ExtLib Require Import Functor Monad.
+From SimpleIO Require Import IO_Random.
+From DeepWeb Require Import Crypto4 IO_Test.
+(* Auto-generated comment: Failed. *)
