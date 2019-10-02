fd3282359d80d2571e7884b3bbd5f2068decad3c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:00:14 -0700

    user 3, session 2609, part 2

--- diffs-annotated-fixed/3/user-3-session-2609.v
+++ diffs-annotated-fixed/3/user-3-session-2609.v
@@ -5,32 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-From Coq Require Import ProofIrrelevance.
-From Coq Require Export String.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4zfvjp"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqJtGai1"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
-From Classes Require Import EqualDec.
-From RecordUpdate Require Import RecordUpdate.
-From stdpp Require Import decidable countable.
-From stdpp Require gmap.
-From stdpp Require Import fin_maps.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq6eqSP3"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq577Tap"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
 (* Auto-generated comment: Succeeded. *)
 
