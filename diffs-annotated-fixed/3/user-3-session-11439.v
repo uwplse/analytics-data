2994316f8fbf5173fffe4f3ff98352a4c6432667
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:44:29 -0700

    user 3, session 11439, part 2

--- diffs-annotated-fixed/3/user-3-session-11439.v
+++ diffs-annotated-fixed/3/user-3-session-11439.v
@@ -162,6 +162,7 @@ Theorem regex_match_is_explicit :
   forall (r : regex) (s : string),
   regex_match r s <-> regex_match_length r s (String.length s).
 Proof.
-(split; intros).
+split.
+(induction 1).
 (* Auto-generated comment: Succeeded. *)
 
