8814c0bce7c47079055605ad58458e126525a021
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:43 -0700

    user 7, session 10, part 24

--- diffs-annotated/7/user-7-session-10.v
+++ diffs-annotated/7/user-7-session-10.v
@@ -13,6 +13,32 @@ Require Import Coq.Bool.Bool.
 Close Scope btj_scope.
 Open Scope btjnf_scope.
 Open Scope btjr_scope.
-constructor.
+Lemma sub_r__mk_nf_sub_r : forall t t' : ty, |- t << t' -> |- MkNF( t) << MkNF( t').
+Proof.
+(intros t t' Hsub; induction Hsub; try (solve [ simpl; constructor ])).
+-
+(simpl).
+(apply unite_pairs_of_nf__preserves_sub_r; assumption || apply mk_nf__in_nf).
+-
+(simpl).
+(constructor; assumption).
+-
+(apply SR_UnionR1; assumption).
+-
+(apply SR_UnionR2; assumption).
+-
+(simpl).
+(constructor; assumption).
+-
+(rewrite <- mk_nf__idempotent).
 assumption.
+Qed.
+Lemma sub_r_nf__trans :
+  forall tm1 tm2 : ty,
+  |- tm1 << tm2 -> InNF( tm1) -> InNF( tm2) -> (forall tl : ty, |- tl << tm1 -> |- tl << tm2) /\ (forall tr : ty, |- tm2 << tr -> |- tm1 << tr).
+Proof.
+(intros tm1 tm2 Hsub).
+(induction Hsub; intros Hnfm1 Hnfm2).
+-
+tauto.
 (* Failed. *)
