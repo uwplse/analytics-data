384b417848a341de21054dc6060ea19c5ae87a0c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:51:39 -0700

    user 7, session 127, part 1

--- diffs-annotated-fixed/7/user-7-session-127.v
+++ diffs-annotated-fixed/7/user-7-session-127.v
@@ -23,6 +23,5 @@ Qed.
 Lemma match_ty_union_1 : forall (v t1 t2 : ty) (k w : nat), |-[ k, w] v <$ t1 -> |-[ k, w] v <$ TUnion t1 t2.
 Proof.
 (intros v t1 t2 k w Hm).
-(destruct k, w, v; left; assumption).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
