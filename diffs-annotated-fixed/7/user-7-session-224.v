345de6389d210cabd96538cf5d57e4913e0e2966
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:54:54 -0700

    user 7, session 224, part 50

--- diffs-annotated-fixed/7/user-7-session-224.v
+++ diffs-annotated-fixed/7/user-7-session-224.v
@@ -161,6 +161,8 @@ exists (S w').
 (apply match_ty_exist__inv in Hm').
 (destruct Hm' as [ti [Hwfti Hm']]).
 specialize (IHw' _ Hm').
-(destruct (f_free_in_ty__dec X' ([Bi0 := ti] t))).
+(destruct (f_free_in_ty__dec X' ([Bi0 := ti] t'))).
+{
+(destruct IHw' as [_ IHw']).
 (* Auto-generated comment: Failed. *)
 
