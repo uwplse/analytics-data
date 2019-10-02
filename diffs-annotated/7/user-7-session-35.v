184bcfec0482459a345647e52b2162ddafc5fa19
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:21 -0700

    user 7, session 35, part 1

--- diffs-annotated/7/user-7-session-35.v
+++ diffs-annotated/7/user-7-session-35.v
@@ -5,6 +5,4 @@ Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0250a.BaseDefs.
-Require Import BetaJulia.Sub0250a.BaseProps.
 Require Import BetaJulia.Sub0250a.AltMatchDefs.
-(* Auto-generated comment: Failed. *)
