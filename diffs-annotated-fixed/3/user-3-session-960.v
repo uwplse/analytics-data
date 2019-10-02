934961ad94994915ca5a4047b786c69bfaa9155f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:42 -0700

    user 3, session 960, part 237

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -280,6 +280,6 @@ Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Instance aModel_wf : (GoModelWf aModel).
 Proof.
-(hnf).
-(* Auto-generated comment: Succeeded. *)
+constructor.
+(* Auto-generated comment: Failed. *)
 
