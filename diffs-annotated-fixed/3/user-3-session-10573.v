7df761198051d90acf1ca66da13ff7308ce84d93
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:38:14 -0700

    user 3, session 10573, part 4

--- diffs-annotated-fixed/3/user-3-session-10573.v
+++ diffs-annotated-fixed/3/user-3-session-10573.v
@@ -203,6 +203,14 @@ Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 (eapply H in H3; simpl in *; eauto).
-(eapply H0 in H2).
+(eapply H0 in H2; eauto).
+(destruct r; intuition eauto).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqXAMGmP"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
 (* Auto-generated comment: Succeeded. *)
 
