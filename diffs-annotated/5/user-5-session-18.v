acd924a9f700b966fdaf61ab55a5df80c1c77a92
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:24 -0700

    user 5, session 18, part 1

--- diffs-annotated/5/user-5-session-18.v
+++ diffs-annotated/5/user-5-session-18.v
@@ -1,2 +1,17 @@
+Require Import String.
+Definition id_eq_dec := string_dec.
+Inductive Term : Set :=
+  | Var : Identifier -> Term
+  | Eq : Term -> Term -> Term
+  | Choose : Identifier -> Term -> Term.
 Module Type EpsilonLogic.
 Parameter (Value : Type).
+Parameter (vTrue : Value).
+Parameter (vFalse : Value).
+Axiom (trueAndFalseDistinct : vTrue <> vFalse).
+Definition Environment := Identifier -> Value.
+Parameter (eval : Environment -> Term -> Value).
+Axiom (evalEqTrue : forall env term, eval env (Eq term term) = vTrue).
+Axiom
+  (evalEqFalse : forall env t1 t2, t1 <> t2 -> eval env (Eq t1 t2) = vFalse).
+Definition isTheorem (t : Term) := forall env, eval env t = vTrue.
