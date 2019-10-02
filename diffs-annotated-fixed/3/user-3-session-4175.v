9596a8240fc7bf8efd207fc8a903e46f8d2e2184
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:05:52 -0700

    user 3, session 4175, part 1

--- diffs-annotated-fixed/3/user-3-session-4175.v
+++ diffs-annotated-fixed/3/user-3-session-4175.v
@@ -6,15 +6,5 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 From Coq Require Extraction.
-From Armada Require Export Lib.
-From Armada Require Import Examples.ReplicatedDisk.ReplicatedDiskImpl.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq2pRKgs" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqT6yT8u" SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
 (* Auto-generated comment: Succeeded. *)
 
