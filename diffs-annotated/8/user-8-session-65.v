cc2d4ffabb5319e1a241da1b22369dbefbddb0af
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:16 -0700

    user 8, session 65, part 42

--- diffs-annotated/8/user-8-session-65.v
+++ diffs-annotated/8/user-8-session-65.v
@@ -950,4 +950,3 @@ Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqqi4Q9u"
 Print Ltac Signatures.
 gen \207\129.
 (rewrite size_ntensor; unify_pows_two; simpl; try lia).
-(rewrite size_ntensor; unify_pows_two; simpl; try lia).
