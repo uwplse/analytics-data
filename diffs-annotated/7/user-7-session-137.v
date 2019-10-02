e11118fa01123494d2732db6c8118e86cffb4dd5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:11 -0700

    user 7, session 137, part 3

--- diffs-annotated/7/user-7-session-137.v
+++ diffs-annotated/7/user-7-session-137.v
@@ -14,10 +14,10 @@ Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Lemma sem_sub_fresh_var__sem_sub_exist :
   forall (X : id) (t t' : ty) (X' : id), fresh_in_ty X' t' -> ||- [[X := TVar X'] t]<= [t'] -> ||- [TExist X t]<= [t'].
-Lemma sem_sub_exist_fresh : forall (X : id) (t : ty), fresh_in_ty X t -> ||- [TExist X t]= [t].
+Lemma sem_eq_k_exist_fresh : forall (k : nat) (X : id) (t : ty), fresh_in_ty X t -> ||-[ k][TExist X t]= [t].
 Proof.
-(intros X t).
+(intros k X t).
 (induction t; intros Hfresh).
 -
-(intros k).
+(intros w1).
 (* Failed. *)
