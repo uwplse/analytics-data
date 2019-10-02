dbd0f2492add61c8684c04c86f870f0b28d13e8b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:05:06 -0700

    user 8, session 125, part 48

--- diffs-annotated-fixed/8/user-8-session-125.v
+++ diffs-annotated-fixed/8/user-8-session-125.v
@@ -39,5 +39,7 @@ specialize (unitary_gate_unitary U) as inv.
 (simpl in *).
 matrix_denote.
 setoid_rewrite denote_unitary_transpose.
+(simpl in *; Msimpl).
+(repeat rewrite Mmult_assoc; try rewrite inv).
 (* Auto-generated comment: Succeeded. *)
 
