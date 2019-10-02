1b706c8e76af269e15a70b17da82a6a185501ff0
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:56 -0700

    user 7, session 108, part 30

--- diffs-annotated/7/user-7-session-108.v
+++ diffs-annotated/7/user-7-session-108.v
@@ -231,5 +231,18 @@ subst.
 (destruct w').
 (inversion Hle).
 exists (TCName c).
-(destruct IHt1 as [v1 Hm1]).
+(destruct (IHt1 k) as [v1 Hm1]).
+(destruct (IHt2 k) as [v2 Hm2]).
+exists (TPair v1 v2).
+(apply match_ty_pair; assumption).
+-
+(destruct (IHt1 k) as [v Hm]).
+exists v.
+(apply match_ty_union_1).
+assumption.
+-
+exists (TRef t).
+(destruct k).
+reflexivity.
+(split; auto).
 (* Failed. *)
