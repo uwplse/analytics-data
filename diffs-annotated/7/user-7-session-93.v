25d52e57631945d08f324a80712253acb1012496
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:43 -0700

    user 7, session 93, part 7

--- diffs-annotated/7/user-7-session-93.v
+++ diffs-annotated/7/user-7-session-93.v
@@ -13,34 +13,18 @@ Inductive cname : Type :=
   | NInt : _
   | NFlt : _
   | NStr : _.
-Definition varid : Type := nat.
 Inductive ty : Type :=
   | TCName : cname -> ty
   | TPair : ty -> ty -> ty
   | TUnion : ty -> ty -> ty
   | TRef : ty -> ty
-  | TVar : varid -> ty
-  | TExist : varid -> ty -> ty.
+  | TVar : id -> ty
+  | TExist : id -> ty -> ty.
 Definition tint := TCName NInt.
-Definition teXX := TExist vX tX.
-Definition tyXRefX := TExist vX (TRef tX).
-Declare Scope btjt_scope.
-Delimit Scope btjt_scope with btjt.
-Open Scope btjt.
-Reserved Notation "'|' t '|'" (at level 20).
-Fixpoint inv_depth (t : ty) :=
-  match t with
-  | TCName _ => 0
-  | TPair t1 t2 => Nat.max (| t1 |) (| t2 |)
-  | TUnion t1 t2 => Nat.max (| t1 |) (| t2 |)
-  | TRef t' => 1 + | t' |
-  | TVar _ => 0
-  | TExist _ t' => | t' |
-  end
-where "'|' t '|'" := (inv_depth t) : btjt_scope.
-Inductive value_type : ty -> Prop :=
-  | VT_CName : forall cn, value_type (TCName cn)
-  | VT_Pair : forall v1 v2, value_type v1 -> value_type v2 -> value_type (TPair v1 v2)
-  | VT_Ref : forall t, value_type (TRef t).
-Hint Constructors value_type: DBBetaJulia.
-(* Failed. *)
+Definition tflt := TCName NFlt.
+Definition tstr := TCName NStr.
+Definition tIntInt := TPair tint tint.
+Definition vX := 1.
+Definition vY := 2.
+Definition vZ := 3.
+Definition tX := TVar vX.
