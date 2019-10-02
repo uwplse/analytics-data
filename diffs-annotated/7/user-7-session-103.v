bdd9c54403e42d8704ca73ab84c87dd3eefdb561
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:51 -0700

    user 7, session 103, part 1

--- diffs-annotated/7/user-7-session-103.v
+++ diffs-annotated/7/user-7-session-103.v
@@ -4,7 +4,6 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0270a.BaseDefs.
 Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
@@ -14,4 +13,9 @@ Lemma match_ty_pair : forall (v1 v2 t1 t2 : ty) (k w : nat), |-[ k, w] v1 <$ t1 
 Proof.
 (intros v1 v2 t1 t2 k w Hm1 Hm2).
 (destruct k, w; split; assumption).
-(* Auto-generated comment: Failed. *)
+Qed.
+Lemma match_ty_union_1 : forall (v t1 t2 : ty) (k w : nat), |-[ k, w] v <$ t1 -> |-[ k, w] v <$ TUnion t1 t2.
+Proof.
+(intros v t1 t2 k w Hm).
+(destruct k, w; destruct v; left; assumption).
+(* Failed. *)
