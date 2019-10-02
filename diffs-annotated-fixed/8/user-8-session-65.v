658adf128016ddc024650c1ead54510c2c86497b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:58:37 -0700

    user 8, session 65, part 155

--- diffs-annotated-fixed/8/user-8-session-65.v
+++ diffs-annotated-fixed/8/user-8-session-65.v
@@ -989,5 +989,6 @@ Msimpl.
 reflexivity.
 +
 mat_replace \226\159\1680\226\136\163 \195\151 \226\136\1631\226\159\169 with @Zero 1 1 by lma.
-(* Auto-generated comment: Succeeded. *)
+mat_replace \226\159\1681\226\136\163 \195\151 \226\136\1631\226\159\169 with I 1 by lma.
+(* Auto-generated comment: Failed. *)
 
