6bcc44a30780a8f59f75571d0de0852659749b47
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:49:27 -0700

    user 7, session 83, part 30

--- diffs-annotated-fixed/7/user-7-session-83.v
+++ diffs-annotated-fixed/7/user-7-session-83.v
@@ -13,6 +13,7 @@ Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import BetaJulia.Sub0250a.DeclSubProps.
 Require Import BetaJulia.BasicTactics.
+Require Import BetaJulia.BasicTactics.
 Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
@@ -171,11 +172,6 @@ Lemma sem_sub_k_i__trans : forall (k : nat) (t1 t2 t3 : ty), ||-[ k][t1]<= [t2] 
 Proof.
 auto with DBBetaJulia.
 Qed.
-Lemma sem_eq_k_i__sem_sub_k_i : forall (k : nat) (t t' : ty), ||-[ k][t]= [t'] -> ||-[ k][t]<= [t'] /\ ||-[ k][t']<= [t].
-Proof.
-(intros k t t' H).
-(split; intros v Hm; specialize (H v); tauto).
-Qed.
 Lemma sem_sub_k_i_union_l__inv : forall (k : nat) (t1 t2 t' : ty), ||-[ k][TUnion t1 t2]<= [t'] -> ||-[ k][t1]<= [t'] /\ ||-[ k][t2]<= [t'].
 Proof.
 (intros k t1 t2 t' Hsem).
@@ -260,10 +256,9 @@ Proof.
 (apply value_sem_sub_k_i_union__inv in Hsem; try assumption).
 (destruct Hsem as [Hsem| Hsem]; [ apply union_right_1 | apply union_right_2 ]; auto).
 +
-clear IHt2.
 (simpl in Hdep).
 (pose proof (le_S_n _ _ Hdep) as Hdep').
-(unfold sem_sub_k in Hsem).
+(unfold sem_sub_k_i in Hsem).
 specialize (Hsem _ Hma).
 (apply match_ty_i_ref__inv in Hsem).
 (destruct Hsem as [t' [Heqt' Href]]).
@@ -272,12 +267,6 @@ clear Heqt'.
 constructor.
 *
 (apply IHk; try assumption).
-(apply sem_eq_k_i__sem_sub_k_i in Href).
-tauto.
-*
-(apply SD_Trans with (MkNF( t2))).
-(apply mk_nf__sub_d_r; assumption).
-(apply IHk).
-(apply mk_nf__in_nf).
+(apply sem_eq_k_i__sem_sub_k_i).
 (* Auto-generated comment: Failed. *)
 
