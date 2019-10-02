aeaa6e6a269b521bab4016a5c7b1cae32b9aff46
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:51:18 -0700

    user 7, session 113, part 34

--- diffs-annotated-fixed/7/user-7-session-113.v
+++ diffs-annotated-fixed/7/user-7-session-113.v
@@ -17,9 +17,6 @@ Qed.
 Lemma not_fresh_in_union__inv : forall (X : id) (fvs1 fvs2 : id_set), not_fresh X (IdSet.union fvs1 fvs2) -> not_fresh X fvs1 /\ not_fresh X fvs2.
 Lemma not_fresh_in_union__inv : forall (X : id) (fvs1 fvs2 : id_set), not_fresh X (IdSet.union fvs1 fvs2) -> not_fresh X fvs1 /\ not_fresh X fvs2.
 Proof.
-(intros X fvs1 fvs2 H).
-(unfold not_fresh in *).
-Abort.
 Lemma subs_not_in_FV : forall (X : id) (t : ty), not_fresh_in_ty X t -> forall s : ty, [X := s] t = t.
 Proof.
 (intros X t).
@@ -27,5 +24,5 @@ Proof.
 -
 (simpl).
 (unfold not_fresh_in_ty in *).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
