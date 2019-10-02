5b17eeb913a17263ac32b602bdc614d19fcc9db6
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:13 -0700

    user 7, session 19, part 345

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,8 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Lemma sem_sub_k_i_pair__inv :
-  forall (t1 t2 t1' t2' : ty) (k : nat), ||-[ k][TPair t1 t2]<= [TPair t1' t2'] -> ||-[ k][t1]<= [t1'] /\ ||-[ k][t2]<= [t2'].
-Proof.
-(intros t1 t2 t1' t2' k).
+(intros t1 t2 t1' t2' k H).
 (* Failed. *)
