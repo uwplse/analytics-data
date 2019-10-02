5b77895da0c6f9712a7d2091307e30acb36a045f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:06:12 -0700

    user 11, session 10, part 11

--- diffs-annotated/11/user-11-session-10.v
+++ diffs-annotated/11/user-11-session-10.v
@@ -10,10 +10,10 @@ Timeout 1 Print LoadPath.
 Redirect "/tmp/coq16819-Zf" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
-From Coq Require Import Basics NArith String.
-Redirect "/tmp/coq16819Lkl" Print Ltac Signatures.
+Redirect "/tmp/coq16819Yur" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
+From Coq Require Import Basics List NArith String.
 From ExtLib Require Import Functor.
 From QuickChick Require Import Decidability Show.
 From DeepWeb Require Import Exp.
@@ -48,3 +48,8 @@ Instance showDataX : (Show (kvs_data exp)) :=
          | Kvs_BadRequest => "400"
          | Kvs_PreconditionFailed => "412"
          end |}.
+Definition kvs_get {V} (k : N) : list (N * V) -> option V := fmap snd \226\136\152 find (N.eqb k \226\136\152 fst).
+Definition kvs_put {K} {V} : K -> V -> list (K * V) -> list (K * V) := compose cons \226\136\152 pair.
+Redirect "/tmp/coq16819l4x" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
