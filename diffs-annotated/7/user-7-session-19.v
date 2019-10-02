e1b82ba6234cf62e02dc88dffae818127c529e47
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:08 -0700

    user 7, session 19, part 257

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,7 +2,11 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(intros v1 v3 k Hm1 Hm2).
-(destruct k).
-(destruct v1; try contradiction).
+Lemma match_ty_i__transitive_on_value_type :
+  forall v1 v2 t3 : ty, value_type v2 -> forall k : nat, |-[ k] v1 <$ v2 -> |-[ k] v2 <$ t3 -> |-[ k] v1 <$ t3.
+Proof.
+(intros v1 v2 t3 Hv2).
+generalize dependent t3.
+generalize dependent v1.
+(induction Hv2; intros v1 t3 k Hm1 Hm2).
 (* Failed. *)
