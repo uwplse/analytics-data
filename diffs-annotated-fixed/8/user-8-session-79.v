16f16dbfe000be487721e579de7e75116d4014f8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:02:12 -0700

    user 8, session 79, part 799

--- diffs-annotated-fixed/8/user-8-session-79.v
+++ diffs-annotated-fixed/8/user-8-session-79.v
@@ -1036,7 +1036,7 @@ Set Printing All.
 Set Printing All.
 (match goal with
  | H:_ -> _ -> denote_box true ?c ?A == _
-   |- context [ denote_box true ?c ?A ] => replace A' with A
+   |- context [ denote_box true ?c ?A' ] => replace A' with A
  end).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
