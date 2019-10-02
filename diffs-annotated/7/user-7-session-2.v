fa1f167f11e691623d7f84853e09b35bbc242699
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:39 -0700

    user 7, session 2, part 78

--- diffs-annotated/7/user-7-session-2.v
+++ diffs-annotated/7/user-7-session-2.v
@@ -16,11 +16,6 @@ Open Scope btjr_scope.
 (apply sub_r_nf_union_l__inv in Hsub2; try assumption).
 Check unite_pairs_union_t.
 Check sub_r_nf_union_l__inv.
-(induction Hsub12; inversion Heqx; inversion Heqy; subst; try clear Heqx Heqy).
-(inversion Hsub22; subst).
-specialize (IHHsub11_1 Hsub12_1 t1'0 Hsub21_1 H2).
-specialize (IHHsub11_2 Hsub12_2 t2'0 Hsub21_2 H4).
-(split; constructor; tauto).
-}
-{
-(* Failed. *)
+Lemma sub_r_pair__inv : forall t1 t2 t1' t2' : ty, |- TPair t1 t2 << TPair t1' t2' -> |- t1 << t1' /\ |- t2 << t2'.
+Proof.
+(remember (TPair t1 t2) as tx eqn:Heqx ).
