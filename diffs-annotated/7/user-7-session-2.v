b576777bc369ba023090f21cc41e70b39c01948a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:35 -0700

    user 7, session 2, part 7

--- diffs-annotated/7/user-7-session-2.v
+++ diffs-annotated/7/user-7-session-2.v
@@ -13,59 +13,21 @@ Require Import Coq.Bool.Bool.
 Close Scope btj_scope.
 Open Scope btjnf_scope.
 Open Scope btjr_scope.
-Lemma unite_pairs_of_nf__preserves_sub_r1 :
-  forall t1 t2 t1' t2' : ty, InNF( t1) -> |- t1 << t1' -> InNF( t2) -> |- t2 << t2' -> |- unite_pairs t1 t2 << TPair t1' t2'.
+Lemma unite_pairs_of_nf__preserves_sub_r :
+  forall t1 t1' t2 t2' : ty,
+  InNF( t1) -> InNF( t1') -> InNF( t2) -> InNF( t2') -> |- t1 << t1' -> |- t2 << t2' -> |- unite_pairs t1 t2 << unite_pairs t1' t2'.
 Proof.
-(intros ta; induction ta; intros tb; induction tb; intros ta' tb' Hnf1 Hsub1 Hnf2 Hsub2;
-  try (solve
-   [ simpl; constructor; assumption
-   | match goal with
-     | Hnf1:InNF( ?t), Hnf2:InNF( TUnion ?t1 ?t2), Hsub:|- TUnion ?t1 ?t2 << _
-       |- |- unite_pairs ?t (TUnion ?t1 ?t2) << TPair _ _ =>
-           destruct (union_in_nf__components_in_nf _ _ Hnf2) as [Hnfb1 Hnfb2];
-            destruct (sub_r_nf_union_l__inv _ _ _ Hsub Hnf2) as [Hsubb1 Hsubb2]; rewrite unite_pairs_atom_union;
-            try (solve [ constructor | inversion Hnf1; subst; assumption ]); constructor; [ apply IHtb1 | apply IHtb2 ]; assumption
-     | Hnf1:InNF( ?t), Hnf2:InNF( TUnion ?t1 ?t2), Hsub:|- TUnion ?t1 ?t2 << _
-       |- |- unite_pairs (TUnion ?t1 ?t2) ?t << TPair ?tx ?ty =>
-           change_no_check (|- TUnion (unite_pairs t1 t) (unite_pairs t2 t) << TPair tx ty);
-            destruct (union_in_nf__components_in_nf _ _ Hnf2) as [Hnfb1 Hnfb2];
-            destruct (sub_r_nf_union_l__inv _ _ _ Hsub Hnf2) as [Hsubb1 Hsubb2]; constructor; [ apply IHta1 | apply IHta2 ];
-            assumption
-     end ])).
-Qed.
-Lemma mk_nf__sub_r_eq : forall t : ty, |- MkNF( t) << t /\ |- t << MkNF( t).
-Proof.
-(induction t).
+(intros t1 t1' t2 t2' Hnf1).
+(induction Hnf1).
 -
-(split; simpl; constructor).
--
-(destruct IHt1; destruct IHt2).
-(split; simpl).
+(intros Hnf1'; induction Hnf1').
 +
-(apply unite_pairs_of_nf__preserves_sub_r1; assumption || apply mk_nf__in_nf).
-+
-(apply SR_NormalForm).
-(simpl).
-(apply sub_r__rflxv).
--
-(destruct IHt1; destruct IHt2).
-(split; simpl; constructor; (apply SR_UnionR1; assumption) || (apply SR_UnionR2; assumption)).
--
-(simpl).
-(destruct IHt).
-(split; constructor; assumption).
-Qed.
-Lemma mk_nf__sub_r1 : forall t : ty, |- MkNF( t) << t.
-Proof.
-(intros t).
-(pose proof (mk_nf__sub_r_eq t) as H; tauto).
-Qed.
-Lemma mk_nf__sub_r2 : forall t : ty, |- t << MkNF( t).
-Proof.
-(intros t).
-(pose proof (mk_nf__sub_r_eq t) as H; tauto).
-Qed.
-(intros t t' Hsub; induction Hsub; try (solve [ simpl; constructor ])).
--
-(simpl).
+(intros Hnf2; induction Hnf2).
+*
+(intros Hnf2'; induction Hnf2'; intros Hsub1 Hsub2).
+{
+(rewrite (unite_pairs_of_atomtys _ _ H H1)).
+(rewrite (unite_pairs_of_atomtys _ _ H0 H2)).
+(constructor; assumption).
+}
 (* Failed. *)
