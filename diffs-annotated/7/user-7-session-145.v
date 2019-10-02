a0aafc4845f1d1b5590850406b2c4d7d8b9362a4
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:14 -0700

    user 7, session 145, part 0

---
+++ diffs-annotated/7/user-7-session-145.v
@@ -0,0 +1,93 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0280a.BaseDefs.
+Require Import BetaJulia.Sub0280a.BaseProps.
+Require Import BetaJulia.Sub0280a.BaseMatchProps.
+Require Import BetaJulia.Sub0280a.BaseSemSubProps.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Lemma sem_sub_k_exist_fresh_l : forall (k : nat) (X : id) (t : ty), fresh_in_ty X t -> ||-[ k][TExist X t]<= [t].
+Proof.
+(intros k X t Hfresh).
+(intros w1).
+exists w1.
+(intros v Hm).
+(destruct w1).
+-
+(apply match_ty_exist__0_inv in Hm; contradiction).
+-
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hm]).
+(simpl in Hm).
+(rewrite subs_fresh_in_ty in Hm; try assumption).
+(eapply match_ty__ge_w).
+eassumption.
+(repeat constructor).
+Qed.
+Lemma sem_sub_k_exist_fresh_r : forall (k : nat) (X : id) (t : ty), fresh_in_ty X t -> ||-[ k][t]<= [TExist X t].
+Proof.
+(intros k X t Hfresh).
+(intros w1).
+exists (S w1).
+(intros v Hm).
+(apply match_ty_exist).
+exists (TEV X).
+(rewrite subs_fresh_in_ty; assumption).
+Qed.
+Lemma sem_sub_exist_fresh_l : forall (X : id) (t : ty), fresh_in_ty X t -> ||- [TExist X t]<= [t].
+Proof.
+(intros X t Hfresh k).
+(apply sem_sub_k_exist_fresh_l).
+assumption.
+Qed.
+Lemma sem_sub_k_exist_pair : forall (k : nat) (X : id) (t1 t2 : ty), ||-[ k][TExist X (TPair t1 t2)]<= [TPair (TExist X t1) (TExist X t2)].
+Proof.
+(intros k X t1 t2 w1).
+exists w1.
+(intros v Hm).
+(destruct w1).
+-
+(apply match_ty_exist__0_inv in Hm; contradiction).
+-
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hm]).
+(simpl in Hm).
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_pair; apply match_ty_exist; exists tx; assumption).
+Qed.
+Lemma sem_sub_exist_pair : forall (X : id) (t1 t2 : ty), ||- [TExist X (TPair t1 t2)]<= [TPair (TExist X t1) (TExist X t2)].
+Proof.
+(intros X t1 t2 k).
+(apply sem_sub_k_exist_pair).
+Qed.
+Lemma sem_sub_fresh_var__sem_sub_any :
+  forall (X : id) (t t' : ty) (X' : id),
+  fresh_in_ty X t -> fresh_in_ty X' t' -> ||- [[X := TVar X'] t]<= [t'] -> forall tx : ty, ||- [[X := tx] t]<= [t'].
+Proof.
+(intros X t).
+(induction t; try (solve [ intros; simpl; tauto ])).
+-
+(induction t').
++
+admit.
++
+admit.
++
+admit.
++
+admit.
++
+(intros X' HX HX' Hsem tx).
+(intros k w1).
+specialize (Hsem k w1).
+(destruct Hsem as [w2 Hsem]).
+exists w2.
+(intros v Hm).
+(* Auto-generated comment: Failed. *)
