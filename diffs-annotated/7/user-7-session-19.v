de48449c38ec357f7469ec427b0331d1aaa8042e
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:11 -0700

    user 7, session 19, part 302

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -4,13 +4,14 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_union__inv in Hm2).
 (destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
-(try
-  match goal with
-  | Hsem:forall v, value_type v -> |-[ ?k] v <$ TCName ?c -> _
-    |- _ =>
-        assert (Hvv : value_type (TCName c)) by constructor;
-         assert (Hmv : |-[ k] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hvv Hmv);
-         apply match_ty_i_union__inv in Hsem; destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hv' Hm';
-         apply match_ty_i_cname__inv in Hm'; subst; assumption
-  end).
+(induction k; intros v Hv; induction Hv; intros ta tb Hsem; unfold sem_sub_k_i in Hsem;
+  try
+   match goal with
+   | Hsem:forall v, value_type v -> |-[ ?k] v <$ TCName ?c -> _
+     |- _ =>
+         assert (Hvv : value_type (TCName c)) by constructor;
+          assert (Hmv : |-[ k] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hvv Hmv);
+          apply match_ty_i_union__inv in Hsem; destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hv' Hm';
+          apply match_ty_i_cname__inv in Hm'; subst; assumption
+   end).
 (* Failed. *)
