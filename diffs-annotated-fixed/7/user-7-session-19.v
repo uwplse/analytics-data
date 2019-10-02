80a9adc48edc0d293b95892ceb551976288e788e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:48:05 -0700

    user 7, session 19, part 681

--- diffs-annotated-fixed/7/user-7-session-19.v
+++ diffs-annotated-fixed/7/user-7-session-19.v
@@ -507,9 +507,12 @@ Proof.
          intros ta1 ta2 Hat1 IH1 Hat2 IH2 Hdep; assert (Hat : atom_type (TPair ta1 ta2)) by (constructor; assumption);
           destruct (max_inv_depth_le__components_le _ _ _ Hdep) as [Hdep1 Hdep2]; apply pair_sem_sub_k_i__sub_d; tauto
    end).
--
-(intros t Hnft _ Hdep).
-(inversion Hdep).
--
+(induction k;
+  match goal with
+  | |- forall t1 : ty, InNF( t1) -> | t1 | <= ?k -> forall t2 : ty, ||-[ ?k][t1]<= [t2] -> |- t1 << t2 =>
+        apply
+         (in_nf_mut (fun (t1 : ty) (_ : atom_type t1) => | t1 | <= k -> forall t2 : ty, ||-[ k][t1]<= [t2] -> |- t1 << t2)
+            (fun (t1 : ty) (_ : in_nf t1) => | t1 | <= k -> forall t2 : ty, ||-[ k][t1]<= [t2] -> |- t1 << t2))
+  end).
 (* Auto-generated comment: Failed. *)
 
