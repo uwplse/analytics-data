f0a4075196ce7f3707541b0f735536f14d2998fe
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:57 -0700

    user 8, session 34, part 31

--- diffs-annotated/8/user-8-session-34.v
+++ diffs-annotated/8/user-8-session-34.v
@@ -25,24 +25,6 @@ Fact denote_compose :
   \206\14701 \226\169\181 \206\1470 \226\136\153 \206\1471 ->
   denote_circuit safe (compose c f) \206\1470 \206\1471' \207\129 =
   compose_super (denote_circuit safe (f (add_fresh_pat w \206\1471)) \206\1470 (add_fresh_state w \206\1471)) (denote_circuit safe c \206\14701 \206\147) \207\129.
-(intros safe w c \206\147 w' f \206\1470 \206\1471 \206\1471' \206\14701 \207\129 TP).
-dependent induction TP.
--
 (intros WT pf_merge1 pf_merge2).
-(simpl).
-(unfold compose_super).
-(unfold denote_circuit).
-(simpl).
-(unfold pad).
-(rewrite (ctx_wtype_size w p \206\147) by easy).
-(rewrite Nat.add_sub).
-(rewrite size_fresh_ctx).
-(destruct pf_merge1 as [V1 M1]).
-replace (size_ctx \206\1471') with size_octx \206\1471' by easy.
-(rewrite M1 in *).
-(rewrite size_octx_merge by easy).
-(simpl).
-(rewrite (ctx_wtype_size w p \206\147 t)).
-admit.
--
-(intros w' h \206\1473 \206\1472 \206\1473' \206\14703 WT pf_merge1 pf_merge2).
+replace (compose (gate g p1 f) h) with gate g p1 (fun p2 => compose (f p2) h) by auto.
+(* Failed. *)
