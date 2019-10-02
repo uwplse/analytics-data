dd62cc86d1ab64d3bbefef033e54390105c4240d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:02:25 -0700

    user 3, session 3117, part 40

--- diffs-annotated-fixed/3/user-3-session-3117.v
+++ diffs-annotated-fixed/3/user-3-session-3117.v
@@ -120,7 +120,7 @@ Proof.
 (change_no_check 256 with (N.to_nat 256)).
 (rewrite <- Nat.compare_lt_iff, <- N2Nat.inj_compare, N.compare_lt_iff).
 (destruct a).
-Search -N.lt.
-Check N.ltb.
+Search -N.lt -N.ltb.
+(apply N.ltb_lt).
 (* Auto-generated comment: Succeeded. *)
 
