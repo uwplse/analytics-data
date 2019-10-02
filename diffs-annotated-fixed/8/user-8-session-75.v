4c39a66137caea10efa309a9f037c601d62c7ec2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:59:32 -0700

    user 8, session 75, part 9

--- diffs-annotated-fixed/8/user-8-session-75.v
+++ diffs-annotated-fixed/8/user-8-session-75.v
@@ -224,7 +224,7 @@ type_check.
 Qed.
 Timeout 1 Print LoadPath.
 Require Import Reals.
-Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coq8FnGtt"
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqPVFYHN"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
