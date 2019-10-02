62a94b4de9a324493143877436b32459dd5884fe
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:59 -0700

    user 7, session 19, part 79

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,6 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(simpl).
-clear IHk' IHt.
+Lemma match_ty_i_t_le_k__v_ke_t : forall (k : nat) (t : ty), | t | <= k -> forall v : ty, |-[ k] v <$ t -> | v | <= | t |.
 (* Failed. *)
