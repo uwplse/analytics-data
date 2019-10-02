c19c221e80c700b1fd821886667f3e6aa4b2f710
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:04 -0700

    user 7, session 19, part 169

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,7 +2,13 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Lemma aaa : forall (k : nat) (t t' v : ty), |-[ k] v <$ t -> |-[ k] v <$ t' -> | t | <= | t' |.
+Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
 Proof.
-(induction k).
+(induction k; induction t; induction t'; intros H).
+32: {
+idtac.
+(simpl).
+(apply le_n_S).
+(apply IHk).
+(assert (Hv : value_type (TRef t)) by (apply match_ty_i__reflexive; constructor)).
 (* Failed. *)
