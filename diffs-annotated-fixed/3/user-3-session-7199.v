128115864012057f3b89a6b2bc4727287e86a279
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:16:20 -0700

    user 3, session 7199, part 154

--- diffs-annotated-fixed/3/user-3-session-7199.v
+++ diffs-annotated-fixed/3/user-3-session-7199.v
@@ -357,6 +357,5 @@ Hint Resolve get_upto_ok: core.
 Theorem get_ok : proc_spec get_spec get recover abstr.
 Proof.
 (unfold get, get_spec; intros).
-step_proc.
 (* Auto-generated comment: Succeeded. *)
 
