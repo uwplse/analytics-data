ebc2c959c21c48a0e5e83c9268b9f288a948c870
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:08 -0700

    user 7, session 19, part 250

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,5 +2,7 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
+(pose proof (Hsem _ Hvref Hmref) as Hmu).
 (apply match_ty_i_union__inv in Hmu).
+(destruct Hmu as [Hmu1| Hmu2]; [ left | right ]; intros v Hv Hm).
 (* Failed. *)
