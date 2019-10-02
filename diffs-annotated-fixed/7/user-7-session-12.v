4d14825d07aa1cfa80ada65213424087b6fe7dd8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:46:11 -0700

    user 7, session 12, part 10

--- diffs-annotated-fixed/7/user-7-session-12.v
+++ diffs-annotated-fixed/7/user-7-session-12.v
@@ -298,11 +298,15 @@ generalize dependent t21.
 (apply sub_r__rflxv).
 -
 (repeat rewrite unite_pairs_union_t).
-(apply SR_UnionL; eapply sub_r__transitive; try apply IHHnf1_1 || apply IHHnf1_2).
-+
-constructor.
+(apply SR_UnionL; eapply sub_r__transitive; try apply IHHnf1_1 || apply IHHnf1_2; constructor).
 (apply SR_UnionR1; apply SR_UnionR1; apply sub_r__reflexive).
 (apply SR_UnionR2; apply SR_UnionR1; apply sub_r__reflexive).
-+
+(apply SR_UnionR1; apply SR_UnionR2; apply sub_r__reflexive).
+(apply SR_UnionR2; apply SR_UnionR2; apply sub_r__reflexive).
+Qed.
+Lemma mk_nf__distr11 : forall t11 t12 t2 : ty, |- MkNF( TPair (TUnion t11 t12) t2) << MkNF( TUnion (TPair t11 t2) (TPair t12 t2)).
+Proof.
+(intros t11 t12 t2).
+(simpl).
 (* Auto-generated comment: Failed. *)
 
