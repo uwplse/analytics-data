b6de80eb4ad71c57055b19729a6d2b0670ef8e67
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:40 -0700

    user 3, session 960, part 210

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -216,23 +216,25 @@ Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Definition ascii_to_bounded (a : Ascii.ascii) : {x | x < 256}.
 refine (exist _ (Ascii.nat_of_ascii a) _).
-Search -Ascii.nat_of_ascii.
 (apply Ascii.nat_ascii_bounded).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqHbUS4W"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Qed.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqHZysVf"
+Defined.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq90h9tp"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqnkGrre"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqMBk2vQ"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-(* Auto-generated comment: Succeeded. *)
+Instance aModel : GoModel.
+Proof.
+refine
+ {|
+ byte := {x | x < 256};
+ byte0 := Ascii.Ascii false false false false false false false false;
+ uint64_to_string := pretty.pretty_nat;
+ ascii_to_byte := ascii_to_bounded;
+ byte_to_ascii := bounded_to_ascii |}.
+(* Auto-generated comment: Failed. *)
 
