50ff6eed75d146600f08e54066d9e782f8fb368f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:01 -0700

    user 7, session 19, part 115

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,18 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Lemma match_ty_i_eq__inv_depth_eq : forall t t' : ty, (forall (k : nat) (v : ty), |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
-Proof.
-(induction t; induction t'; intros H).
-reflexivity.
-15: {
-idtac.
-clear IHt'.
-(simpl).
-(apply f_equal).
-(apply IHt).
-(intros k v).
-(assert (Hm : |-[ S k] TRef t <$ TRef t)).
-(simpl).
-tauto.
+(assert (Hm : |-[ S k] TRef t <$ TRef t) by tauto).
 (* Failed. *)
