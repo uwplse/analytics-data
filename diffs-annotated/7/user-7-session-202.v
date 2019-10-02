08e27d6ebde7ec4d0912a5a37d8f75c4898ad782
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:48 -0700

    user 7, session 202, part 12

--- diffs-annotated/7/user-7-session-202.v
+++ diffs-annotated/7/user-7-session-202.v
@@ -49,82 +49,19 @@ Qed.
 Lemma not_b_free_in_ty_pair__inv : forall (X : id) (t1 t2 : ty), not_b_free_in_ty X (TPair t1 t2) -> not_b_free_in_ty X t1 /\ not_b_free_in_ty X t2.
 Proof.
 (solve_not_free_union not_b_free_in_ty).
-Qed.
-Lemma not_f_free_in_ty_union__inv :
-  forall (X : id) (t1 t2 : ty), not_f_free_in_ty X (TUnion t1 t2) -> not_f_free_in_ty X t1 /\ not_f_free_in_ty X t2.
-Proof.
-(solve_not_free_union not_f_free_in_ty).
-Qed.
-Lemma not_b_free_in_ty_union__inv :
-  forall (X : id) (t1 t2 : ty), not_b_free_in_ty X (TUnion t1 t2) -> not_b_free_in_ty X t1 /\ not_b_free_in_ty X t2.
-Proof.
-(solve_not_free_union not_b_free_in_ty).
-Qed.
-Lemma b_subst_cname : forall (X : id) (s : ty) (c : cname), [BX := s] TCName c = TCName c.
-Proof.
-(intros).
-reflexivity.
-Qed.
-Lemma b_subst_pair : forall (X : id) (s t1 t2 : ty), [BX := s] TPair t1 t2 = TPair ([BX := s] t1) ([BX := s] t2).
-Proof.
-(intros).
-reflexivity.
-Qed.
-Lemma b_subst_union : forall (X : id) (s t1 t2 : ty), [BX := s] TUnion t1 t2 = TUnion ([BX := s] t1) ([BX := s] t2).
-Proof.
-(intros).
-(simpl).
-reflexivity.
-Qed.
-Lemma b_subst_ev : forall (X : id) (s : ty) (Y : id), [BX := s] TEV Y = TEV Y.
-Proof.
-(intros).
-reflexivity.
-Qed.
-Lemma b_subst_bvar_eq : forall (X : id) (s : ty), [BX := s] TBVar X = s.
-Proof.
-(intros).
-(simpl).
-(rewrite <- beq_id_refl).
-reflexivity.
-Qed.
-Lemma b_subst_bvar_neq : forall (X : id) (s : ty) (Y : id), X <> Y -> [BX := s] TBVar Y = TBVar Y.
-Proof.
-(intros X s Y Hneq).
-(simpl).
-(destruct (beq_id_false_iff X Y) as [_ Hid]).
-specialize (Hid Hneq).
-(simpl).
-(rewrite Hid).
-reflexivity.
-Qed.
-Lemma b_subst_exist_eq : forall (X : id) (s : ty) (t : ty), [BX := s] TExist X t = TExist X t.
-Proof.
-(intros).
-(simpl).
-(rewrite <- beq_id_refl).
-reflexivity.
-Qed.
-Lemma b_subst_exist_neq : forall (X : id) (s : ty) (Y : id) (t : ty), X <> Y -> [BX := s] TExist Y t = TExist Y ([BX := s] t).
-Proof.
-(intros X s Y t Hneq).
-(simpl).
-(destruct (beq_id_false_iff X Y) as [_ Hid]).
-specialize (Hid Hneq).
-(rewrite Hid).
-reflexivity.
-Qed.
 Lemma wf_ty_pair__inv : forall t1 t2 : ty, wf_ty (TPair t1 t2) -> wf_ty t1 /\ wf_ty t2.
 Proof.
 (intros t1 t2 Hwf).
 (unfold wf_ty in *; simpl in *).
-Search -IdSet.Empty.
-admit.
 Admitted.
 Lemma wf_ty_union__inv : forall t1 t2 : ty, wf_ty (TUnion t1 t2) -> wf_ty t1 /\ wf_ty t2.
 Proof.
 (intros t1 t2 Hwf).
 (unfold wf_ty in *; simpl in *).
-admit.
 Admitted.
+Lemma b_subst_wf_ty : forall (X : id) (t : ty), wf_ty t -> forall s : ty, [BX := s] t = t.
+Proof.
+(intros X t).
+(induction t; intros Hwf s; try (solve [ reflexivity ])).
+(simpl).
 (* Failed. *)
