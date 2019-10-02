fbbc69ecac942f35c465e7fa795783e5be51502f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:13 -0700

    user 7, session 19, part 347

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,6 +2,17 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(intros t1 t2 t1' t2' k Hsem).
-(unfold sem_sub_k_i in Hsem).
+Lemma match_ty_i_exists : forall (t : ty) (k : nat), exists v : ty, |-[ k] v <$ t.
+Proof.
+(induction t; intros k).
+-
+(exists (TCName c); destruct k; reflexivity).
+-
+(destruct (IHt1 k) as [v1 Hm1]; destruct (IHt2 k) as [v2 Hm2]).
+(exists (TPair v1 v2); apply match_ty_i_pair; assumption).
+-
+(destruct (IHt1 k) as [v1 Hm1]).
+(exists v1; apply match_ty_i_union_1; assumption).
+-
+exists TRef t.
 (* Failed. *)
