3b36a9a96d965e88702cfaa26ca5178a69cb9331
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:26 -0700

    user 2, session 5, part 24

--- diffs-annotated-fixed/2/user-2-session-5.v
+++ diffs-annotated-fixed/2/user-2-session-5.v
@@ -31,6 +31,7 @@ Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Module TermNotations.
 Declare Scope coucou_scope.
+Notation "{ f a }" := (App f a) (f  at level 0, a  at level 0) : coucou_scope.
 Notation "[ ]" := Nil (format "[ ]") : coucou_scope.
 Notation "[ x ]" := (Cons x Nil) : coucou_scope.
 Notation "[ x y .. z ]" := (Cons x (Cons y .. (Cons z Nil) ..))
@@ -39,7 +40,7 @@ End TermNotations.
 Import TermNotations.
 Open Scope coucou_scope.
 Check [Nil (Ident "a") Nil].
-Redirect "/tmp/coqB6m6PE" Print Ltac Signatures.
+Redirect "/tmp/coqEk3t2K" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 (* Auto-generated comment: Succeeded. *)
