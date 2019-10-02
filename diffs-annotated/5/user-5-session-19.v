d91c58af292d312123c804ddc57d2a5d8ad99032
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:25 -0700

    user 5, session 19, part 10

--- diffs-annotated/5/user-5-session-19.v
+++ diffs-annotated/5/user-5-session-19.v
@@ -100,46 +100,7 @@ specialize IHt1 with env.
 specialize IHt2 with env.
 (apply evalEqTrue in IHt1).
 (apply evalEqTrue in IHt2).
-(destruct (eval_eq_true_or_false L env t1 t2)).
-+
-(rewrite H).
-(apply evalEqTrue in H).
-(rewrite H in IHt1).
-(rewrite IHt1 in IHt2).
-symmetry.
-(apply evalEqTrue).
-assumption.
-+
-(rewrite H).
-(apply evalEqFalse in H).
-(assert (eval L env (identity t1) <> eval L env (identity t2)) by congruence).
-symmetry.
-(apply evalEqFalse).
-assumption.
-Admitted.
-Fixpoint free_vars (t : Term) : list Identifier :=
-  match t with
-  | Var x => [x]
-  | Int _ => []
-  | Eq a b => free_vars a ++ free_vars b
-  | Plus a b => free_vars a ++ free_vars b
-  | Times a b => free_vars a ++ free_vars b
-  | Minus a b => free_vars a ++ free_vars b
-  | Choose x P =>
-      filter (fun y => if id_eq_dec x y then false else true) (free_vars P)
-  end.
-Axiom (fresh_var : list Identifier -> Identifier).
-Axiom (fresh_var_unique : forall exclude, ~ In (fresh_var exclude) exclude).
-Definition Divide (t1 : Term) (t2 : Term) :=
-  let x := fresh_var (free_vars t1 ++ free_vars t2) in
-  Choose x (Eq t1 (Times (Var x) t2)).
-Lemma divide_test :
-  forall L env, L.(eval) env (Divide (Int 6) (Int 2)) = L.(eval) env (Int 3).
-Proof.
-(intros).
-(unfold Divide).
-(match goal with
- | |- context [ Choose ?x _ ] => generalize x
- end).
-intro x.
-(rewrite evalChoose).
+(assert
+  (forall res,
+   eval L env (Choose x (Eq (Int 6) (Times (Var x) (Int 2)))) = res ->
+   res = eval L env (Int 3))).
