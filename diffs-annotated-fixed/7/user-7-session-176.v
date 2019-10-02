bfcc8edeea2a357fd6ea3e83544f3777577b55a6
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:53:11 -0700

    user 7, session 176, part 5

--- diffs-annotated-fixed/7/user-7-session-176.v
+++ diffs-annotated-fixed/7/user-7-session-176.v
@@ -143,6 +143,6 @@ Proof.
 (intros X; induction w; induction t; intros v).
 (intros Hm).
 (apply match_ty_cname__inv in Hm).
-(exists v; assumption).
+exists v.
 (* Auto-generated comment: Failed. *)
 
