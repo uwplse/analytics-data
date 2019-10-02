bece9644ffad3dd92f7c4828ba69c2d7672a6b68
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:03 -0700

    user 7, session 19, part 157

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,6 +3,14 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_pair; auto).
-(split; intros Hm).
-+
+(induction t; induction t'; intros H; try reflexivity;
+  try (solve
+   [ match goal with
+     | |- | ?t1 | = | ?t2 | =>
+           (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+             (H 0 _ Hv); destruct H as [H _]; specialize (H Hm); contradiction) ||
+             (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
+               (H 0 _ Hv); destruct H as [_ H]; specialize (H Hm); contradiction)
+     end ])).
+-
 (* Failed. *)
