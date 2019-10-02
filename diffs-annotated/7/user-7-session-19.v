96530ae2542048d37da3c85e38e574fad5e3db27
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:12 -0700

    user 7, session 19, part 333

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,5 +3,5 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hm'; apply match_ty_i__transitive_on_value_type with v; assumption).
-(pose proof (value_sem_sub_k_i_union__inv _ _ _ _ H)).
+Check value_sem_sub_k_i_union__inv.
 (* Failed. *)
