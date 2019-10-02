75be14d61e17dd8a0bb2bbb3e09469be9b8be700
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:22 -0700

    user 7, session 41, part 7

--- diffs-annotated/7/user-7-session-41.v
+++ diffs-annotated/7/user-7-session-41.v
@@ -3,13 +3,5 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0250a.BaseDefs.
-Require Import BetaJulia.Sub0250a.BaseProps.
-Require Import BetaJulia.Sub0250a.AltMatchDef.
-Require Import Coq.Lists.List.
-(unfold sem_sub_k in *).
-auto.
--
-(apply match_ty_pair__inv in Hm).
+Require Import BetaJulia.Sub0250a.MatchProps.
 (* Failed. *)
