4843950d3839fa3f16337c6431c89dfe011fd738
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:51:36 -0700

    user 3, session 293, part 1

--- diffs-annotated-fixed/3/user-3-session-293.v
+++ diffs-annotated-fixed/3/user-3-session-293.v
@@ -8,5 +8,56 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import List.
 From Transitions Require Import NonError.
 From RecoveryRefinement Require Export Lib.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq1GghPx"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq2MzcJR"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Module Var.
+Inductive id :=
+  | Sum : _
+  | Count : _.
+Inductive Op : Type -> Type :=
+  | Read : forall i : id, Op nat
+  | Write : forall (i : id) (v : nat), Op unit.
+Definition State := (nat * nat)%type.
+Definition get (i : id) : State -> nat :=
+  match i with
+  | Sum => fun '(x, _) => x
+  | Count => fun '(_, y) => y
+  end.
+Definition set (i : id) (v : nat) :
+  State -> State :=
+  match i with
+  | Sum => fun '(_, y) => (v, y)
+  | Count => fun '(x, _) => (x, v)
+  end.
+Definition dynamics : Dynamics Op State :=
+  {|
+  step := fun T (op : Op T) =>
+          match op with
+          | Read i => reads (get i)
+          | Write i v => puts (set i v)
+          end;
+  crash_step := puts (fun _ => (0, 0)) |}.
+Definition l : Layer Op :=
+  {|
+  Layer.State := State;
+  sem := dynamics;
+  initP := fun s => s = (0, 0) |}.
+End Var.
+Instance var_crash_step_nonerror :
+ (NonError Var.dynamics.(crash_step)).
+Proof.
+(simpl).
+typeclasses eauto.
+Module DB.
 (* Auto-generated comment: Succeeded. *)
 
