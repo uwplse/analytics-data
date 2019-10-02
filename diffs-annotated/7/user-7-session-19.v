eb3c9d7953bc999574f0fc8a73df48a496f84080
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:08 -0700

    user 7, session 19, part 259

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,26 +2,4 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(induction Hv2).
--
-(intros v1 t3 k Hm1 Hm2).
-(apply match_ty_i_cname__inv in Hm1; subst).
-assumption.
--
-(intros v0 t3 k Hm1 Hm2).
-(apply match_ty_i_pair__inv in Hm1).
-(destruct Hm1 as [pv11 [pv12 [Heq [Hm11 Hmpv12]]]]; subst).
-(induction t3; try (solve [ destruct k; simpl in Hm2; contradiction ])).
-+
-(apply match_ty_i_pair__inv in Hm2).
-(destruct Hm2 as [pv21 [pv22 [Heq [Hm21 Hm22]]]]).
-(inversion Heq; subst).
-auto using match_ty_i_pair.
-+
-(apply match_ty_i_union__inv in Hm2).
-(destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
--
-(intros v1 t3 k Hm1 Hm2).
-(destruct k).
-(destruct v1; try contradiction).
-(apply match_ty_i_ref__inv in Hm1).
+(* Failed. *)
