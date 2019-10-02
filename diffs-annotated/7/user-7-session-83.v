46fc407d0c1ea32a25e69db9627a1e6fa77aad8a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:38 -0700

    user 7, session 83, part 16

--- diffs-annotated/7/user-7-session-83.v
+++ diffs-annotated/7/user-7-session-83.v
@@ -5,6 +5,35 @@ Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0250a.BaseDefs.
+Lemma match_ty_i_exists : forall (t : ty) (k : nat), exists v : ty, |-[ k] v <$ t.
+Proof.
+(induction t; intros k).
+-
+(exists (TCName c); destruct k; reflexivity).
+-
+(destruct (IHt1 k) as [v1 Hm1]; destruct (IHt2 k) as [v2 Hm2]).
+(exists (TPair v1 v2); apply match_ty_i_pair; assumption).
+-
+(destruct (IHt1 k) as [v1 Hm1]).
+(exists v1; apply match_ty_i_union_1; assumption).
+-
+exists (TRef t).
+(apply match_ty_i__reflexive).
+constructor.
+Qed.
+Lemma sem_sub_k_i__trans : forall (k : nat) (t1 t2 t3 : ty), ||-[ k][t1]<= [t2] -> ||-[ k][t2]<= [t3] -> ||-[ k][t1]<= [t3].
+Proof.
+auto with DBBetaJulia.
+Qed.
+Lemma value_sem_sub_k_i_union__inv :
+  forall v : ty, value_type v -> forall (k : nat) (ta tb : ty), ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
+Proof.
+(intros v Hv k ta tb Hsem; unfold sem_sub_k_i in Hsem).
+(assert (Hm : |-[ k] v <$ v) by (apply match_ty_i__reflexive; assumption)).
+specialize (Hsem _ Hm).
+(apply match_ty_i_union__inv in Hsem).
+(destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hm'; apply match_ty_i__transitive_on_value_type with v; assumption).
+Qed.
 Lemma sem_sub_k_i_pair__inv :
   forall (t1 t2 t1' t2' : ty) (k : nat), ||-[ k][TPair t1 t2]<= [TPair t1' t2'] -> ||-[ k][t1]<= [t1'] /\ ||-[ k][t2]<= [t2'].
 Proof.
@@ -14,4 +43,5 @@ Proof.
   [ assert (Hmp : |-[ k] TPair v v' <$ TPair t1 t2) by (apply match_ty_i_pair; assumption)
   | assert (Hmp : |-[ k] TPair v' v <$ TPair t1 t2) by (apply match_ty_i_pair; assumption) ]; specialize (Hsem _ Hmp);
   apply match_ty_i_pair__inv in Hsem; destruct Hsem as [v1 [v2 [Heq [Hm1 Hm2]]]]; inversion Heq; subst; assumption).
+Qed.
 (* Failed. *)
