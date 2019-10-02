d8ea46f9c7ce3f8674c334652529fe1473ce464c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:36 -0700

    user 3, session 960, part 145

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -95,8 +95,11 @@ Function
    | _ =>
        let base := S (S base_m2) in
        let digit := x `mod` base in
-       exist (fun x => x < base) digit mod_S_lt
-       :: nat_to_le base_m2 (x / base)
+       exist (fun x => x < base) digit _ :: nat_to_le base_m2 (x / base)
    end.
-(* Auto-generated comment: Failed. *)
+Proof.
+-
+(intros; subst).
+(apply PeanoNat.Nat.div_lt; auto; try lia).
+(* Auto-generated comment: Succeeded. *)
 
