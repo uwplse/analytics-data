ed3fdf02058acb00bc8da39f2da9963bd6cf085a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:07 -0700

    user 7, session 224, part 42

--- diffs-annotated/7/user-7-session-224.v
+++ diffs-annotated/7/user-7-session-224.v
@@ -22,4 +22,23 @@ Lemma build_v_full :
     (forall (w' : nat) (t' : ty),
      |-[ w'] v' <$ t' -> (not_f_free_in_ty X' t' -> |-[ w'] v <$ t') /\ (f_free_in_ty X' t' -> exists w2, |-[ w2] v <$ [FX' := tx] t')).
 exists 0.
+admit.
+*
+(rewrite f_subst_union).
+(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
+  destruct IHt' as [IHt'a IHt'b]; split; intros HX').
+{
+(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
+(apply match_ty_union_1; auto).
+}
+{
+(destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
+{
+specialize (IHt'b HXt'1).
+(destruct IHt'b as [w2 IHt'b]).
+exists w2.
+(apply match_ty_union_1; assumption).
+}
+{
+exists S w.
 (* Failed. *)
