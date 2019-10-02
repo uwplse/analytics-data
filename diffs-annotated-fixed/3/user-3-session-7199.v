9870690e07ca743cba993fa74f49d8407aaa6f85
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:16:41 -0700

    user 3, session 7199, part 410

--- diffs-annotated-fixed/3/user-3-session-7199.v
+++ diffs-annotated-fixed/3/user-3-session-7199.v
@@ -365,7 +365,8 @@ Proof.
 (rewrite app_length in *; simpl in *).
 lia.
 }
-(destruct (a == length bs)).
+(destruct (a == length bs); subst).
 -
+autorewrite with upd.
 (* Auto-generated comment: Succeeded. *)
 
