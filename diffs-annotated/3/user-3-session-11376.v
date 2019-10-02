9c1cd2e987e75252ca54535595ef74c1569d3db7
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:00 -0700

    user 3, session 11376, part 0

---
+++ diffs-annotated/3/user-3-session-11376.v
@@ -0,0 +1,8 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqpOybRz"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Export LF.Basics.
