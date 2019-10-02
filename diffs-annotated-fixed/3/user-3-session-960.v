820050d96311985e9c0530632d4143afd6e5ff34
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:30 -0700

    user 3, session 960, part 34

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -72,7 +72,9 @@ Definition byte_nat := {x : nat | x < 256}.
 Fixpoint nat_to_le base (x : nat) {measure x : list {x : nat | x < S base} :=
   match x with
   | 0 => nil
-  | _ => exist _ (x mod S base) _ :: nat_to_le base x / S base
+  | _ => exist _ (x mod S base) _ :: nat_to_le base (x / S base)
   end.
-(* Auto-generated comment: Failed. *)
+Next Obligation.
+(simpl).
+(* Auto-generated comment: Succeeded. *)
 
