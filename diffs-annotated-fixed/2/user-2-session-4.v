06536280c9eb8de0ae84321d8bafcddafe592eec
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:23 -0700

    user 2, session 4, part 0

---
+++ diffs-annotated-fixed/2/user-2-session-4.v
@@ -0,0 +1,23 @@
+Redirect "/tmp/coqIwNt4T" Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Redirect "/tmp/coq8QvH6N" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Test Debug Analytics.
+Require Import Coq.Strings.String.
+Redirect "/tmp/coqsRY5JP" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Inductive term :=
+  | Nil : term
+  | Ident : string -> term
+  | Cons : term -> term -> term
+  | App : term -> term -> term.
+Redirect "/tmp/coqXt9OWv" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+(* Auto-generated comment: Succeeded. *)
+
