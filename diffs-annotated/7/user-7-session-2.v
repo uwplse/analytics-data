92a15c3d7fd9f10e478f97d0abf869ff02f61ee3
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:36 -0700

    user 7, session 2, part 33

--- diffs-annotated/7/user-7-session-2.v
+++ diffs-annotated/7/user-7-session-2.v
@@ -16,6 +16,6 @@ Open Scope btjr_scope.
 (apply sub_r_nf_union_l__inv in Hsub2; try assumption).
 Check unite_pairs_union_t.
 Check sub_r_nf_union_l__inv.
-(destruct (sub_r_nf_union_l__inv _ _ _ Hsub1 Hnf)).
-Check sub_r_nf_union_l__inv.
+(destruct (sub_r_nf_union_l__inv _ _ _ Hsub1 Hnf) as [Hsub11 Hsub12]).
+(constructor; [ apply SR_UnionR1 | apply SR_UnionR2 ]; tauto).
 (* Failed. *)
