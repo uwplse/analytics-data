d1d1cf80aaa606bbb687ebe7b951d5ea55b7f81c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:02 -0700

    user 1, session 32, part 0

---
+++ diffs-annotated-fixed/1/user-1-session-32.v
@@ -0,0 +1,31 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Import Lists.List.
+Require Import
+  Coq.Sorting.Permutation.
+Require Import
+  Coq.Sorting.Sorted.
+Require Import Orders.
+Require Import FunInd.
+Require Import Recdef.
+Require Import Extraction.
+Require Import
+  ExtrOcamlBasic.
+Require Import
+  Coq.Program.Equality.
+Require Import Math.
+#[local]
+Coercion is_true : bool >->
+ Sortclass.
+Module Sort.
+Parameter (t : Type).
+Parameter
+  (leb : t -> t -> bool).
+Notation "a <=? b" :=
+  (leb a b).
+Notation "! a " := 
+  (negb a) (at level 10).
+(* Auto-generated comment: Succeeded. *)
+
