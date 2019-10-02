6054bea2843033e6560370c319501eb39611412d
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:59 -0700

    user 7, session 19, part 80

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,4 +3,6 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Lemma match_ty_i_t_le_k__v_ke_t : forall (k : nat) (t : ty), | t | <= k -> forall v : ty, |-[ k] v <$ t -> | v | <= | t |.
+Proof.
+(induction k; induction t; intros Htk v Hm).
 (* Failed. *)
