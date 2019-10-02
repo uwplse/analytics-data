5577953235605bb03a6c36e20acb22696d64eef2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:07 -0700

    user 1, session 37, part 35

--- diffs-annotated-fixed/1/user-1-session-37.v
+++ diffs-annotated-fixed/1/user-1-session-37.v
@@ -304,6 +304,29 @@ Inductive GT : Type :=
 Definition SetST := Ensemble ST.
 Print Forall.
 Print Forall2.
-Fixpoint Gamma (G : GT) : SetST.
-(* Auto-generated comment: Succeeded. *)
+Fixpoint Gamma (G : GT) : SetST :=
+  match G with
+  | GDyn => Full_set _
+  | GInt => Singleton _ SInt
+  | GBool => Singleton _ SBool
+  | GFun G_1 G_2 => zipWith_ensembles SFun (Gamma G_1) (Gamma G_2)
+  | GRec l =>
+      fun X =>
+      exists l',
+        X = SRec l' /\
+        Forall2 (fun (S' : option ST) G' => True) l'
+          (map GammaPair l)
+  | _ => Empty_set _
+  end
+with GammaPair (x : option (Ann * GT)) :
+match x as m return (x = m -> SetST) with
+| None => fun _ => Empty_set _
+| Some P =>
+    fun H =>
+    match P with
+    | (R, G) => Gamma G
+    | (O, G) => Empty_set _
+    end
+end eq_refl.
+(* Auto-generated comment: Failed. *)
 
