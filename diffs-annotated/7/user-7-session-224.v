992e3fc9c6294513b416c59444bb402451a87ada
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:05 -0700

    user 7, session 224, part 16

--- diffs-annotated/7/user-7-session-224.v
+++ diffs-annotated/7/user-7-session-224.v
@@ -149,19 +149,4 @@ reflexivity.
 Check b_free_in_ty__b_free_in_b_subst_neq.
 (apply (b_free_in_ty__b_free_in_b_subst_neq i ti) in HX; try assumption).
 specialize (IHw _ _ Hwftx HX Hm).
-*
-(rewrite f_subst_union).
-(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
-  destruct IHt' as [IHt'a IHt'b]; split; intros HX').
-{
-(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
-(apply match_ty_union_1; auto).
-}
-{
-(destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
-{
-(apply match_ty_union_1; auto).
-}
-{
-(apply match_ty_union_1).
-(rewrite f_subst_not_b_free_in_ty; assumption).
+(* Failed. *)
