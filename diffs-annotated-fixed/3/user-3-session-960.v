78d88bf2bb35eb6e2618707e0a004e27d91443ec
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:28 -0700

    user 3, session 960, part 4

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -86,6 +86,6 @@ Print Ascii.Ascii.
 refine
  {|
  byte := Ascii.ascii;
- byte0 := Ascii.ascii false false false false false false false false |}.
-(* Auto-generated comment: Failed. *)
+ byte0 := Ascii.Ascii false false false false false false false false |}.
+(* Auto-generated comment: Succeeded. *)
 
