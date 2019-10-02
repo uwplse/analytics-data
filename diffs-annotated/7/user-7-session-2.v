e516ab90e9f783cf31a36536fcbb94f17839b67d
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:35 -0700

    user 7, session 2, part 15

--- diffs-annotated/7/user-7-session-2.v
+++ diffs-annotated/7/user-7-session-2.v
@@ -13,5 +13,6 @@ Require Import Coq.Bool.Bool.
 Close Scope btj_scope.
 Open Scope btjnf_scope.
 Open Scope btjr_scope.
-Check mk_nf_nf__equal.
+(assert (Hnf : InNF( t)) by (constructor; assumption)).
+(rewrite (mk_nf_nf__equal t Hnf) in Hsub).
 (* Failed. *)
