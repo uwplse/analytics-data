2ef2d61887510e048dc5414d14090613e74af4a1
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:03 -0700

    user 7, session 19, part 150

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,6 +3,9 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_pair; auto).
-(assert (Hv : value_type (TCName c)) by constructor; assert (Hm : |-[ 0] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption);
-  specialize (H 0 _ Hv); destruct H as [H _]; specialize (H Hm)).
+-
+(assert (H1 : forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ TCName c <-> |-[ k] v <$ t'1)).
+{
+(intros k v Hv).
+specialize (H k v Hv).
 (* Failed. *)
