b4fe127cf3c7902d49964fcbd82ec101d1c17697
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:49:03 -0700

    user 7, session 65, part 1

--- diffs-annotated-fixed/7/user-7-session-65.v
+++ diffs-annotated-fixed/7/user-7-session-65.v
@@ -138,7 +138,10 @@ Proof.
 (assert (Hma : |-[ k] TPair ta1 ta2 <$ TPair ta1 ta2) by (apply match_ty_value_type__reflexive; assumption)).
 (induction t2; intros Hsem; try (solve [ specialize (Hsem _ Hma); destruct k; simpl in Hsem; subst; constructor || contradiction ])).
 -
-clear IHt2_1 IHt2_2.
-(destruct (sem_sub_k_pair__inv _ _ _ _ _ Hdep Hsem') as [Hsem1 Hsem2]).
-(* Auto-generated comment: Failed. *)
+(destruct (sem_sub_k_pair__inv _ _ _ _ _ Hdep Hsem) as [Hsem1 Hsem2]).
+-
+(apply value_sem_sub_k_union__inv in Hsem; try assumption).
+(destruct Hsem as [Hsem| Hsem]; [ apply union_right_1 | apply union_right_2 ]; tauto).
+-
+(* Auto-generated comment: Succeeded. *)
 
