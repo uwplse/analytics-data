6cf116514895e61bbae04ebeecf6c1320f2fe6ca
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:43 -0700

    user 7, session 10, part 32

--- diffs-annotated/7/user-7-session-10.v
+++ diffs-annotated/7/user-7-session-10.v
@@ -14,14 +14,5 @@ Close Scope btj_scope.
 Open Scope btjnf_scope.
 Open Scope btjr_scope.
 (destruct IHHsub1 as [IHHsub11 IHHsub12]; try assumption).
-(destruct IHHsub2 as [IHHsub21 IHHsub22]; try assumption).
-(split; intros tx Hsub'; [ remember (TPair t1 t2) as ty eqn:Heqy  | remember (TPair t1' t2') as ty eqn:Heqy  ]; induction Hsub'; inversion Heqy;
-  subst; try (solve [ constructor; auto ])).
-+
-(apply IHHsub').
-(apply mk_nf_nf__equal; assumption).
-(apply mk_nf__in_nf).
--
-(destruct (in_nf_union__inv _ _ Hnfm1) as [Hnfm11 Hnfm12]).
-(destruct IHHsub1 as [IHHsub11 IHHsub12]).
+(destruct IHHsub1 as [IHHsub11 IHHsub12]; assumption).
 (* Failed. *)
