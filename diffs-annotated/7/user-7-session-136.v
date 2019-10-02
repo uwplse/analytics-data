dd54ac4391494565eca541f89340f459a94ac618
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:11 -0700

    user 7, session 136, part 2

--- diffs-annotated/7/user-7-session-136.v
+++ diffs-annotated/7/user-7-session-136.v
@@ -2,17 +2,3 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0280a.BaseDefs.
-Require Import BetaJulia.Sub0280a.BaseMatchProps.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Open Scope btjm.
-Lemma sem_sub__sem_eq : forall t t' : ty, ||- [t]<= [t'] -> ||- [t']<= [t] -> ||- [t]= [t'].
-Proof.
-(intros t t' Hsem1 Hsem2 k).
-(unfold sem_eq_k).
-(* Failed. *)
