eb313393c13d0bf0967b576668b4338fb6f8db2f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:59 -0700

    user 7, session 19, part 86

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,9 +2,8 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(induction k; induction t; intros Htk v Hm;
-  try match goal with
-      | Hm:|-[ ?k'] ?v <$ TCName _ |- _ => apply match_ty_i_cname__inv in Hm; subst; constructor
-      end).
-(destruct (max_inv_depth_le__components_le _ _ Htk) as [Htk1 Htk2]).
+(destruct (max_inv_depth_le__components_le _ _ _ Htk) as [Htk1 Htk2]).
+(apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+SearchPattern (Nat.max _ _ <= Nat.max _ _).
+(simpl; apply Nat.max_le_compat).
 (* Failed. *)
