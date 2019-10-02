e43d064d97b465c1d46f1e9b3775f51888c809df
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:47 -0700

    user 3, session 960, part 313

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -154,6 +154,103 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqFNw7z8"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Search -Ascii.ascii.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Definition ascii_to_bounded (a : Ascii.ascii) : {x | x < 256}.
+refine (exist _ (Ascii.nat_of_ascii a) _).
+(apply Ascii.nat_ascii_bounded).
+Defined.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq2e8LX6"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqZAxZFb"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Instance aModel : GoModel.
+Proof.
+refine
+ {|
+ byte := {x | x < 256};
+ byte0 := exist _ 0 _;
+ uint64_to_string := pretty.pretty_nat;
+ ascii_to_byte := ascii_to_bounded;
+ byte_to_ascii := bounded_to_ascii;
+ uint64_to_le := nat_to_le 254;
+ uint64_from_le := fun digits => Some (le_to_nat digits);
+ File := Z;
+ nilFile := (- 1)%Z;
+ Ptr := fun _ => nat;
+ nullptr := fun _ => 0 |}.
+(apply Nat.lt_0_succ).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqmN14LA"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Defined.
+Class GoModelWf (model : GoModel) :={uint64_to_string_inj :
+                                      forall x y,
+                                      uint64_to_string x = uint64_to_string y ->
+                                      x = y;
+                                     ascii_byte_bijection1 :
+                                      forall c,
+                                      byte_to_ascii (ascii_to_byte c) = c;
+                                     ascii_byte_bijection2 :
+                                      forall b,
+                                      ascii_to_byte (byte_to_ascii b) = b;
+                                     uint64_le_enc :
+                                      FixedLengthEncoder 8 uint64_to_le
+                                        uint64_from_le;
+                                     file_eqdec :> EqualDec File;
+                                     file_countable :> Countable File;
+                                     sigPtr_eq_dec :> EqualDec (sigT Ptr)}.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqwRsNEY"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqgoHMEH"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Instance aModel_wf : (GoModelWf aModel).
+Proof.
+econstructor.
+-
+(simpl).
+(apply pretty.pretty_nat_inj).
+-
+(simpl).
+(unfold bounded_to_ascii, ascii_to_bounded).
+(intros).
+(destruct c; simpl).
+(rewrite Ascii.ascii_nat_embedding; auto).
+-
+(simpl).
+(destruct b; simpl).
+(unfold ascii_to_bounded, bounded_to_ascii; simpl).
+(apply subset_eq_compat).
+(rewrite Ascii.nat_ascii_embedding; auto).
+-
+admit.
+-
+(simpl).
+typeclasses eauto.
+-
+(simpl).
+(unfold EqualDec).
+(intros; simpl).
+(decide equality; subst).
+(decide equality; subst).
+(decide equality; subst).
+(destruct x, y; simpl).
+(destruct (nat_eq_dec n n0); subst).
+left.
+Search -(existT _ _ = existT _ _) -projT2.
+unshelve (eapply eq_existT_curried).
 (* Auto-generated comment: Succeeded. *)
 
