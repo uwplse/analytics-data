d3e258bb4896d5b181237d09c34d2792ccc2f4f3
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:37 -0700

    user 7, session 83, part 9

--- diffs-annotated/7/user-7-session-83.v
+++ diffs-annotated/7/user-7-session-83.v
@@ -13,6 +13,15 @@ Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Close Scope btjm.
 Open Scope btjmi.
+Lemma sem_eq_k_i__trans : forall (k : nat) (t1 t2 t3 : ty), ||-[ k][t1]= [t2] -> ||-[ k][t2]= [t3] -> ||-[ k][t1]= [t3].
+Proof.
+(intros k t1 t2 t3 Hsem1 Hsem2).
+(unfold sem_eq_k in *).
+(intros v).
+specialize (Hsem1 v).
+specialize (Hsem2 v).
+tauto.
+Qed.
 Lemma match_ty_i_pair : forall (v1 v2 t1 t2 : ty) (k : nat), |-[ k] v1 <$ t1 -> |-[ k] v2 <$ t2 -> |-[ k] TPair v1 v2 <$ TPair t1 t2.
 Proof.
 (intros v1 v2 t1 t2 k Hm1 Hm2).
@@ -28,6 +37,108 @@ Proof.
 (intros v t1 t2 k Hm).
 (destruct k; destruct v; right; assumption).
 Qed.
+Lemma match_ty_i_cname__inv : forall (v : ty) (c : cname) (k : nat), |-[ k] v <$ TCName c -> v = TCName c.
+Proof.
+(intros v; induction v; try (solve [ intros c k Hm; destruct k; contradiction ])).
+(intros c0 k Hm).
+(destruct k; simpl in Hm; subst; reflexivity).
+Qed.
+Lemma match_ty_i_pair__inv :
+  forall (v t1 t2 : ty) (k : nat), |-[ k] v <$ TPair t1 t2 -> exists v1 v2 : ty, v = TPair v1 v2 /\ |-[ k] v1 <$ t1 /\ |-[ k] v2 <$ t2.
+Proof.
+(intros v; induction v; try (solve [ intros t1 t2 k Hm; destruct k; contradiction ])).
+(intros t1 t2 k Hm).
+exists v1,v2.
+(split; try reflexivity).
+(destruct k; simpl in Hm; assumption).
+Qed.
+Lemma match_ty_i_union__inv : forall (v t1 t2 : ty) (k : nat), |-[ k] v <$ TUnion t1 t2 -> |-[ k] v <$ t1 \/ |-[ k] v <$ t2.
+Proof.
+(intros v t1 t2 k Hm).
+(destruct k; destruct v; assumption).
+Qed.
+Lemma match_ty_i_ref__weak_inv : forall (v t : ty) (k : nat), |-[ k] v <$ TRef t -> exists t' : ty, v = TRef t'.
+Proof.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; contradiction ])).
+clear IHv.
+(intros t k).
+(intros Hm).
+exists v.
+reflexivity.
+Qed.
+Lemma match_ty_i_ref__inv : forall (v t : ty) (k : nat), |-[ S k] v <$ TRef t -> exists t' : ty, v = TRef t' /\ ||-[ k][t']= [t].
+Proof.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; contradiction ])).
+clear IHv.
+(intros t k Hm).
+(simpl in Hm).
+exists v.
+auto.
+Qed.
+Theorem match_ty_i__value_type_l : forall (v t : ty) (k : nat), |-[ k] v <$ t -> value_type v.
+Proof.
+(intros v t).
+generalize dependent v.
+(induction t; intros k v Hm).
+-
+(apply match_ty_i_cname__inv in Hm; subst).
+constructor.
+-
+(apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(constructor; [ eapply IHt1 | eapply IHt2 ]; eauto).
+-
+(apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto).
+-
+(apply match_ty_i_ref__weak_inv in Hm).
+(destruct Hm as [t' Heq]; subst).
+constructor.
+Qed.
+Lemma match_ty_i__reflexive : forall v : ty, value_type v -> forall k : nat, |-[ k] v <$ v.
+Proof.
+(intros v Hv; induction Hv; intros k).
+-
+(destruct k; reflexivity).
+-
+(apply match_ty_i_pair; auto).
+-
+(destruct k).
+constructor.
+(simpl).
+tauto.
+Qed.
+Lemma match_ty_i__transitive_on_value_type :
+  forall v1 v2 t3 : ty, value_type v2 -> forall k : nat, |-[ k] v1 <$ v2 -> |-[ k] v2 <$ t3 -> |-[ k] v1 <$ t3.
+Proof.
+(intros v1 v2 t3 Hv2).
+generalize dependent t3.
+generalize dependent v1.
+(induction Hv2).
+-
+(intros v1 t3 k Hm1 Hm2).
+(apply match_ty_i_cname__inv in Hm1; subst).
+assumption.
+-
+(intros v0 t3 k Hm1 Hm2).
+(apply match_ty_i_pair__inv in Hm1).
+(destruct Hm1 as [pv11 [pv12 [Heq [Hm11 Hmpv12]]]]; subst).
+(induction t3; try (solve [ destruct k; contradiction ])).
++
+(apply match_ty_i_pair__inv in Hm2).
+(destruct Hm2 as [pv21 [pv22 [Heq [Hm21 Hm22]]]]).
+(inversion Heq; subst).
+auto using match_ty_i_pair.
++
+(apply match_ty_i_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
+-
+(intros v1 t3 k Hm1 Hm2).
+(induction t3; try (solve [ destruct k; contradiction ])).
++
+(apply match_ty_i_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
++
+clear IHt3.
+(destruct k).
 (destruct v1; contradiction || constructor).
 (apply match_ty_i_ref__inv in Hm1).
 (destruct Hm1 as [tx [Heqx Hrefx]]; inversion Heqx; subst).
