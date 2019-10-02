2f0aaf9ceac8e003511c70a456ed0e99a5ad6993
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:40 -0700

    user 3, session 960, part 211

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -232,9 +232,9 @@ Proof.
 refine
  {|
  byte := {x | x < 256};
- byte0 := Ascii.Ascii false false false false false false false false;
+ byte0 := exist _ 0 _;
  uint64_to_string := pretty.pretty_nat;
  ascii_to_byte := ascii_to_bounded;
  byte_to_ascii := bounded_to_ascii |}.
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
