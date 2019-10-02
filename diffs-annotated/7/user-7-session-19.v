2b94918c7fdc5eba679ce7365fe8717ce67bc7b4
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:55 -0700

    user 7, session 19, part 4

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -107,21 +107,5 @@ Proof.
    end).
 -
 (intros ta; induction ta; intros tb; induction tb; intros Hsem; pose proof Hsem as Hsem'; unfold sem_sub_i, sem_sub_k_i in Hsem';
-  unfold sem_sub_deq, sem_sub_k_deq; intros k v Hv Hm1;
-  try
-   match goal with
-   | Hm1:(|-[ ?k] ?v <$ TCName ?c)%btjmdeq
-     |- _ =>
-         apply match_ty_deq_cname__inv in Hm1; subst; assert (Hm : (|-[ k] TCName c <$ TCName c)%btjmi) by (destruct k; reflexivity); specialize
-          (Hsem' _ _ Hv Hm)
-   end).
--
-(apply match_ty_i_cname__inv in Hsem').
-(inversion Hsem').
-(destruct k; reflexivity).
--
-(apply match_ty_i_pair__inv in Hsem').
-(destruct Hsem' as [v1 [v2 [Heq _]]]).
-(inversion Heq).
--
+  unfold sem_sub_deq, sem_sub_k_deq; intros k v Hv Hm1).
 (* Failed. *)
