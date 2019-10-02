beb72c49c8f7746104ff4c5eb8de27dd26f3538d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:16:15 -0700

    user 3, session 7199, part 60

--- diffs-annotated-fixed/3/user-3-session-7199.v
+++ diffs-annotated-fixed/3/user-3-session-7199.v
@@ -208,6 +208,6 @@ Proof.
 (descend; intuition eauto).
 (descend; intuition eauto).
 (unfold log_abstraction in H0; intuition).
-(pose proof (H1 (log_addr a)); intuition).
+(pose proof (H1 a); intuition).
 (* Auto-generated comment: Succeeded. *)
 
