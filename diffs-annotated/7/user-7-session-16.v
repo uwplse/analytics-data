28f677a65121406647b83b5b943b126581739be4
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:50 -0700

    user 7, session 16, part 48

--- diffs-annotated/7/user-7-session-16.v
+++ diffs-annotated/7/user-7-session-16.v
@@ -232,11 +232,6 @@ Ltac
    apply atom_sub_r_union__inv in Hcontra; tauto || constructor.
 right.
 (match goal with
- | |- ~ |- ?t1 << ?t2 =>
-       remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]);
-        subst
- end).
-(match goal with
- | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra
+ | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto
  end).
 (* Failed. *)
