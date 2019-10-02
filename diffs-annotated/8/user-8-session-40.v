86aa97d71fb437bf63321b00e3d8431ce3818434
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:00 -0700

    user 8, session 40, part 24

--- diffs-annotated/8/user-8-session-40.v
+++ diffs-annotated/8/user-8-session-40.v
@@ -27,20 +27,3 @@ autorewrite with proof_db.
 (simpl).
 (rewrite Nat.sub_diag).
 (rewrite super_I; easy).
-Hint Rewrite @kron_1_r' : M_db.
-Lemma X_spec :
-  forall b safe : bool,
-  denote_box safe (boxed_gate _X) (bool_to_matrix b) == bool_to_matrix (\194\172 b).
-Proof.
-(intros).
-vector_denote.
-(destruct b; unfold bool_to_ket; simpl; Msimpl; easy).
-Qed.
-Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqYJshKs"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Lemma init0_spec : forall safe, denote_box safe init0 (I (2 ^ 0)) = \226\136\1630\226\159\169\226\159\1680\226\136\163.
-Proof.
-(intros).
-matrix_denote.
-Msimpl.
