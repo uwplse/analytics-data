5cabe34a1b528df4bf4f9fccd99cd5390304f99a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:06 -0700

    user 7, session 19, part 205

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,12 +2,12 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(try (solve
-  [ match goal with
-    | |- | ?t1 | <= | ?t2 | =>
-          (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
-            (H _ Hv); destruct H as [H _]; specialize (H Hm); contradiction) ||
-            (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
-              (H _ Hv); destruct H as [_ H]; specialize (H Hm); contradiction)
-    end ])).
+(try
+  match goal with
+  | |- | ?t1 | <= | ?t2 | =>
+        (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+          (H _ Hv); destruct H as [H _]; specialize (H Hm); contradiction) ||
+          (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
+            (H _ Hv); destruct H as [_ H]; specialize (H Hm); contradiction)
+  end).
 (* Failed. *)
