aa7d32bcc7d68c38cca3f32eb5922db475845b87
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:45 -0700

    user 3, session 960, part 279

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -311,6 +311,6 @@ typeclasses eauto.
 (destruct (nat_eq_dec n n0); subst).
 left.
 Search -(existT _ _ = existT _ _).
-(apply eq_existT_curried).
-(* Auto-generated comment: Failed. *)
+(eapply eq_existT_curried).
+(* Auto-generated comment: Succeeded. *)
 
