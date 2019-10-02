477736b1e52456357c2d44348bef5987ebc4d761
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:47:07 -0700

    user 7, session 19, part 197

--- diffs-annotated-fixed/7/user-7-session-19.v
+++ diffs-annotated-fixed/7/user-7-session-19.v
@@ -114,6 +114,6 @@ idtac.
 specialize (H _ Hm).
 (apply match_ty_i_ref__inv in H).
 (destruct H as [tx [Heq Href]]; inversion Heq; subst).
-(intros v Hm; auto).
+(intros v Hmv; auto).
 (* Auto-generated comment: Failed. *)
 
