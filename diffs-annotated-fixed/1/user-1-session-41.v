661a5abaf3fdff6795a8b874d66257eaa489c556
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:15 -0700

    user 1, session 41, part 46

--- diffs-annotated-fixed/1/user-1-session-41.v
+++ diffs-annotated-fixed/1/user-1-session-41.v
@@ -561,6 +561,7 @@ all: (try (apply singleton_eq in H1; congruence)).
 Search -(Ensembles.In _ (Singleton _ _) _).
 all: (try specialize (H1 _ (In_singleton _ _ _))).
 Show Intros.
-all: (try specialize (H1 _ (In_singleton _))).
+all: (try specialize (H1 _ (In_singleton _ _))).
+all: (try congruence).
 (* Auto-generated comment: Succeeded. *)
 
