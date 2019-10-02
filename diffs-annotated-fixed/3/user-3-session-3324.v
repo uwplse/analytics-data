1bee5697bf17957c9ee19fb8ffc00fcab2dc22f2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:03:02 -0700

    user 3, session 3324, part 10

--- diffs-annotated-fixed/3/user-3-session-3324.v
+++ diffs-annotated-fixed/3/user-3-session-3324.v
@@ -140,6 +140,5 @@ step_proc.
 (destruct (lt_dec r0 (diskSize (stateDisk state)))).
 step_proc.
 (case_eq (diskGet (stateDisk state) (diskSize (stateDisk state) - 1)); intros).
-{
 (* Auto-generated comment: Succeeded. *)
 
