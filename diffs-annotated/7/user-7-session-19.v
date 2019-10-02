e56b4008a2f9dc6c7c83373434b635772d59ff94
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:57 -0700

    user 7, session 19, part 37

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,5 +3,6 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 specialize (Href v Hv).
-(split; intros Hm; tauto).
+(split; tauto).
+Qed.
 (* Failed. *)
