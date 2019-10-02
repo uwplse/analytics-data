2b6c32f845f21420ee7596c3b06a9a25d32c6ccc
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:27 -0700

    user 8, session 2, part 255

--- diffs-annotated/8/user-8-session-2.v
+++ diffs-annotated/8/user-8-session-2.v
@@ -50,4 +50,5 @@ Timeout 1 Print Grammar tactic.
 Timeout 1 About restore_dims.
 Timeout 1 About trans.
 Timeout 1 About ctrls_to_list.
-(simpl_rewrite @denote_unitary_transpose).
+(rewrite @denote_unitary_transpose).
+(* Failed. *)
