3568042114c6e006a9822ea38c90cf2aa17b37ea
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:05:02 -0700

    user 8, session 125, part 24

--- diffs-annotated-fixed/8/user-8-session-125.v
+++ diffs-annotated-fixed/8/user-8-session-125.v
@@ -39,6 +39,6 @@ Timeout 1 Print unitary_gate_unitary.
 matrix_denote.
 setoid_rewrite denote_unitary_transpose.
 (simpl in *; Msimpl).
-(repeat rewrite Mmult_assoc; try rewrite unitary_gate_unitary).
-(* Auto-generated comment: Succeeded. *)
+(repeat rewrite Mmult_assoc; rewrite unitary_gate_unitary).
+(* Auto-generated comment: Failed. *)
 
