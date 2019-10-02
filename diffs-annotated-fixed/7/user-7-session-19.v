e4e9f896d176f65c14bc3d5db1344c856f5e7e67
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:47:24 -0700

    user 7, session 19, part 345

--- diffs-annotated-fixed/7/user-7-session-19.v
+++ diffs-annotated-fixed/7/user-7-session-19.v
@@ -157,6 +157,6 @@ Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k
 Lemma sem_sub_k_i_pair__inv :
   forall (t1 t2 t1' t2' : ty) (k : nat), ||-[ k][TPair t1 t2]<= [TPair t1' t2'] -> ||-[ k][t1]<= [t1'] /\ ||-[ k][t2]<= [t2'].
 Proof.
-(intros t1 t2 t1' t2' k).
+(intros t1 t2 t1' t2' k H).
 (* Auto-generated comment: Failed. *)
 
