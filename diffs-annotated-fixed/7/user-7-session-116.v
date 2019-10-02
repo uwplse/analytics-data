7ac927081b14a66808406d266a4ecd98bbcd61b0
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:51:24 -0700

    user 7, session 116, part 33

--- diffs-annotated-fixed/7/user-7-session-116.v
+++ diffs-annotated-fixed/7/user-7-session-116.v
@@ -55,5 +55,11 @@ contradiction.
 (unfold fresh in Hfresh).
 (destruct (beq_idP X i); try reflexivity).
 subst.
+exfalso.
+(apply Hfresh).
+Search -IdSet.singleton.
+(apply IdSetFacts.singleton_2).
+reflexivity.
+Qed.
 (* Auto-generated comment: Failed. *)
 
