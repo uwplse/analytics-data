1e72763e8cc4c7ab35090c507ab0457c336cd156
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:06:02 -0700

    user 10, session 2, part 1

--- diffs-annotated/10/user-10-session-2.v
+++ diffs-annotated/10/user-10-session-2.v
@@ -1,3 +1,5 @@
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqWXn2UD" Print Ltac Signatures.
+Require Import Analytical.Analytics.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqBDrtIb" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
+Print Analytics.
