62eea810a3e2cb252c0252660652ad830cf83faa
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:05 -0700

    user 7, session 224, part 9

--- diffs-annotated/7/user-7-session-224.v
+++ diffs-annotated/7/user-7-session-224.v
@@ -149,8 +149,5 @@ reflexivity.
 Check b_free_in_ty__b_free_in_b_subst_neq.
 (apply (b_free_in_ty__b_free_in_b_subst_neq i ti) in HX; try assumption).
 specialize (IHw _ _ Hwftx HX Hm).
-(destruct IHw as [v' [Hm' IHw]]).
-exists v'.
-split.
-exists ti.
+(apply match_ty_exist).
 (* Failed. *)
