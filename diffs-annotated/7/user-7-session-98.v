bccae0411341081889b75b46a08fc86836ca6651
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:47 -0700

    user 7, session 98, part 27

--- diffs-annotated/7/user-7-session-98.v
+++ diffs-annotated/7/user-7-session-98.v
@@ -67,7 +67,23 @@ Lemma match_ty_ref__inv : forall (v t : ty) (k : nat), |-[ S k] v <$ TRef t -> e
 Proof.
 (intros v; induction v; try (solve [ intros t k Hm; destruct k; contradiction ])).
 clear IHv.
-Lemma not_sem_sub__refeXrefX_eYrefrefY : ~ ||- [TRef (TExist vX (TRef tX))]<= [TExist vY (TRef (TRef tY))].
-+
-Abort.
+(intros t k Hm).
+(simpl in Hm).
+exists v.
+auto.
+Qed.
+Lemma match_ty_exist__inv : forall (v : ty) (X : id) (t : ty) (k : nat), |-[ S k] v <$ TExist X t -> exists tx : ty, |-[ k] v <$ [X := tx] t.
+Proof.
+(intros v; induction v; intros X t k Hm; assumption).
+Qed.
+Theorem match_ty__value_type_l : forall (k : nat) (v t : ty), |-[ k] v <$ t -> value_type v.
+Proof.
+(induction k; intros v t; generalize dependent v; induction t; intros v Hm;
+  try (solve
+   [ apply match_ty_cname__inv in Hm; subst; constructor
+   | apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; constructor; [ eapply IHt1 | eapply IHt2 ]; eauto
+   | apply match_ty_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto
+   | apply match_ty_ref__weak_inv in Hm; destruct Hm as [t' Heq]; subst; constructor
+   | destruct v; contradiction ])).
+-
 (* Failed. *)
