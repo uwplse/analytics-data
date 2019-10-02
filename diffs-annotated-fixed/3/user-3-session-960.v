492fde9ffe9907ab51a60040dcd547d2228da0ce
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:28 -0700

    user 3, session 960, part 7

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -79,6 +79,7 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-Print string.
+Search -nat -string.
+Print pretty.pretty.
 (* Auto-generated comment: Succeeded. *)
 
