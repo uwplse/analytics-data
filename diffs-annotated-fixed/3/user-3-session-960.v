d76e6c1b092806f0b80171cba616a7bb93e6fc79
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:41 -0700

    user 3, session 960, part 218

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -237,6 +237,6 @@ refine
  ascii_to_byte := ascii_to_bounded;
  byte_to_ascii := bounded_to_ascii;
  uint64_to_le := nat_to_le 256;
- le_to_uint64 := fun digits => Some (le_to_nat digits) |}.
+ uint64_from_le := fun digits => Some (le_to_nat digits) |}.
 (* Auto-generated comment: Failed. *)
 
