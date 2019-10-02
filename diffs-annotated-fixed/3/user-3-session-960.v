9da502dab2a4693403ee304023e288d493b9f693
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:33 -0700

    user 3, session 960, part 97

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -135,6 +135,6 @@ Proof.
 (intros).
 (induction n as [n IHn] using lt_wf_ind).
 (destruct n; simpl).
-(rewrite nat_to_le_func).
-(* Auto-generated comment: Failed. *)
+Search -nat_to_le_func.
+(* Auto-generated comment: Succeeded. *)
 
