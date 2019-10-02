4bfbe980c120689aa4fe9c80bde094d3bc937142
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:01:51 -0700

    user 8, session 79, part 680

--- diffs-annotated-fixed/8/user-8-session-79.v
+++ diffs-annotated-fixed/8/user-8-session-79.v
@@ -1006,7 +1006,6 @@ restore_dims tensor_tac.
  end).
 2: {
 (simpl).
-(rewrite ctx_to_mat_list_length; try rewrite size_ntensor, Nat.mul_1_r;
-  easy reflexivity).
-(* Auto-generated comment: Failed. *)
+(rewrite ctx_to_mat_list_length; try rewrite size_ntensor, Nat.mul_1_r; easy).
+(* Auto-generated comment: Succeeded. *)
 
