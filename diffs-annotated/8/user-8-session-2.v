51fedb3ebfbe34b611003bb2bec0209e886a6194
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:15 -0700

    user 8, session 2, part 69

--- diffs-annotated/8/user-8-session-2.v
+++ diffs-annotated/8/user-8-session-2.v
@@ -47,6 +47,4 @@ Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Lemma ctrls_to_list_transpose : forall W lb li (u : Unitary W), fst (ctrls_to_list lb li u) = fst (ctrls_to_list lb li (trans u)).
 (induction W; intros lb li u; try (solve [ inversion u ])).
-(destruct (ctrls_to_list lb li (trans u)) as [[j' l'] v'] eqn:E').
-simpl_args.
-(* Failed. *)
+(simpl).
