390980375d285f44c5ec45bed90a043baefe6561
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:47:08 -0700

    user 7, session 19, part 213

--- diffs-annotated-fixed/7/user-7-session-19.v
+++ diffs-annotated-fixed/7/user-7-session-19.v
@@ -103,6 +103,12 @@ constructor.
 Qed.
 Lemma bbb : forall (k : nat) (t t' v : ty), |-[ k] v <$ t -> |-[ k] v <$ t' -> | t | <= | t' |.
 Proof.
-(induction k; induction t; induction t').
+(induction k; induction t; induction t'; intros v Hmt Hmt').
+Show 2.
+32: {
+idtac.
+(simpl).
+(apply le_n_S).
+(apply IHk).
 (* Auto-generated comment: Failed. *)
 
