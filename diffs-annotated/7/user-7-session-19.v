9ecf4d286cc5d9f9314dfa1ff815872ac4c456a5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:13 -0700

    user 7, session 19, part 343

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,7 +3,7 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Lemma sem_sub_k_i_pair__inv :
-  forall t1 t2 t1' t2' : ty,
-  forall k : nat, | TPair t1 t2 | <= k -> ||-[ k][TPair t1 t2]<= [TPair t1' t2'] -> ||-[ k][t1]<= [t1'] /\ ||-[ k][t2]<= [t2'].
+  forall (t1 t2 t1' t2' : ty) (k : nat),
+  | TPair t1 t2 | <= k -> ||-[ k][TPair t1 t2]<= [TPair t1' t2'] -> ||-[ k][t1]<= [t1'] /\ ||-[ k][t2]<= [t2'].
 Proof.
 (* Failed. *)
