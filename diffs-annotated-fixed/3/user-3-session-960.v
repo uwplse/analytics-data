2ff4f97b5334ffec3dec3de6ebe2c4b7202eb26e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:50 -0700

    user 3, session 960, part 363

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -301,7 +301,5 @@ Check repeat_length.
 (rewrite nat_from_le_zeros; auto).
 (rewrite nat_le_inverse; auto).
 (generalize dependent Datatypes.length (nat_to_le 254 x); intros).
-(assert (n <= 8)).
-lia.
 (* Auto-generated comment: Succeeded. *)
 
