e83a122a084ec3db4973d9bb2f59ae0ae39e0e43
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:47:51 -0700

    user 7, session 19, part 564

--- diffs-annotated-fixed/7/user-7-session-19.v
+++ diffs-annotated-fixed/7/user-7-session-19.v
@@ -404,6 +404,7 @@ Lemma value_sem_sub_i_union__inv : forall v : ty, value_type v -> forall ta tb :
 Proof.
 (intros v Hv ta tb Hsem; unfold sem_sub_i in Hsem).
 (assert (Hm : |-[ 0] v <$ v) by (apply match_ty_i__reflexive; assumption)).
+(assert (Hm : |-[ 0] v <$ v) by (apply match_ty_i__reflexive; assumption)).
 specialize (Hsem 0 _ Hm).
 (apply match_ty_i_union__inv in Hsem).
 (destruct Hsem; [ left | right ]).
