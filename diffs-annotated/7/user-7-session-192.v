d79a82fcd2017c431c2bf5a8acc3e2de9c0ab759
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:41 -0700

    user 7, session 192, part 4

--- diffs-annotated/7/user-7-session-192.v
+++ diffs-annotated/7/user-7-session-192.v
@@ -36,5 +36,6 @@ assumption.
 *
 (rewrite subst_union).
 (destruct (either_free_or_fresh_in_ty X t'1) as [HXt'1| HXt'1]; apply match_ty_union_1; auto).
-(destruct (either_free_or_fresh_in_ty X t'1) as [HXt'1| HXt'1]; apply match_ty_union_1; tauto).
+(destruct (either_free_or_fresh_in_ty X t'1) as [HXt'1| HXt'1]).
+(apply match_ty_union_1; tauto).
 (* Failed. *)
