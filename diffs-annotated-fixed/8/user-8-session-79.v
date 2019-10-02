d3b617e4c8f22d397e6d55befdb5c01e3e6f73ba
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:02:37 -0700

    user 8, session 79, part 911

--- diffs-annotated-fixed/8/user-8-session-79.v
+++ diffs-annotated-fixed/8/user-8-session-79.v
@@ -1039,6 +1039,7 @@ specialize inSeq_correct as IS.
 (simpl in IS).
 (repeat (rewrite IS; compile_typing compile_WT)).
 (repeat (rewrite IS; unfold compose_super; compile_typing compile_WT)).
-(unfold compose_super).
+rewrite_inPar'.
+rewrite_inPar'.
 (* Auto-generated comment: Succeeded. *)
 
