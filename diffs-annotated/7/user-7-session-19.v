c4e7c0a5a578d70af27c0100e3c21308b6c2608f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:12 -0700

    user 7, session 19, part 336

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,5 +2,6 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ H)).
+(pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ H) as Hsemu).
+(destruct Hsemu as [Hsemu1| Hsemu2]).
 (* Failed. *)
