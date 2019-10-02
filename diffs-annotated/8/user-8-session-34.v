28a2b453f755d062be9310beb5e64b0a6dcc93ba
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:56 -0700

    user 8, session 34, part 22

--- diffs-annotated/8/user-8-session-34.v
+++ diffs-annotated/8/user-8-session-34.v
@@ -25,3 +25,4 @@ Fact denote_compose :
   \206\14701 \226\169\181 \206\1470 \226\136\153 \206\1471 ->
   denote_circuit safe (compose c f) \206\1470 \206\1471' \207\129 =
   compose_super (denote_circuit safe (f (add_fresh_pat w \206\1471)) \206\1470 (add_fresh_state w \206\1471)) (denote_circuit safe c \206\14701 \206\147) \207\129.
+dependent induction TP.
