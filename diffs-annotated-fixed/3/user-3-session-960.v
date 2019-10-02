f1029acb74f25b444c47d17d328db98a19ef580a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:40 -0700

    user 3, session 960, part 207

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -204,20 +204,6 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Definition bounded_to_ascii (x : {x | x < 256}) : Ascii.ascii :=
-  match Byte.of_nat (proj1_sig x) with
-  | Some b => Ascii.ascii_of_byte b
-  | None => ascii0
-  end.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqhtsIcj"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqTyMpHa"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Check Ascii.ascii_of_nat.
-Print Ascii.ascii_of_nat.
-(* Auto-generated comment: Succeeded. *)
+  Ascii.nat_of_ascii (proj1_sig x).
+(* Auto-generated comment: Failed. *)
 
