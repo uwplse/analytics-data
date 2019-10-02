e5df776621ff8de272b5768d0ec9ca781c75223d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 10:16:40 -0700

    Fix comment

--- scripts/find-refactors.py
+++ scripts/find-refactors.py
@@ -134,7 +134,7 @@ for i in range(len(group_ends) - 1):
                 new = new_cumulative[curr_index]
                 f.write(new + "\n")
         if len(group_failures) > j and group_failures[j] is True:
-            f.write("(* Failed. *)\n")
+            f.write("(* Auto-generated comment: Failed. *)\n")
 
     # Now switch to use the new cumulative file
     old_cumulative = new_cumulative
