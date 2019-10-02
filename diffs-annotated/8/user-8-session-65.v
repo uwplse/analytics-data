a4035ea7c199bea5eec5f41f6893790a3b1134fa
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:19 -0700

    user 8, session 65, part 84

--- diffs-annotated/8/user-8-session-65.v
+++ diffs-annotated/8/user-8-session-65.v
@@ -948,7 +948,5 @@ Fact assert_at_spec_unsafe :
 Admitted.
 Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqqi4Q9u"
 Print Ltac Signatures.
-(repeat rewrite Mmult_assoc).
-Msimpl.
-(rewrite kron_mixed_product).
+setoid_rewrite kron_mixed_product.
 (* Failed. *)
