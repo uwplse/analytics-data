a6f95aaa940779bab69681aa19fd37da53384095
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:03:45 -0700

    user 8, session 79, part 1261

--- diffs-annotated-fixed/8/user-8-session-79.v
+++ diffs-annotated-fixed/8/user-8-session-79.v
@@ -1227,6 +1227,7 @@ rewrite_inPar'.
 restore_dims tensor_tac.
 (rewrite id_circ_spec).
 (rewrite assert0_spec).
+restore_dims tensor_tac.
 Msimpl.
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
