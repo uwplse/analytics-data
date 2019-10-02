d4b2a72c3934108e6c32a2121f8a747fdb98ab11
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:16 -0700

    user 1, session 41, part 51

--- diffs-annotated-fixed/1/user-1-session-41.v
+++ diffs-annotated-fixed/1/user-1-session-41.v
@@ -558,9 +558,7 @@ generalize dependent G'.
 -
 (intros; inversion H0; subst; eauto).
 all: (try (apply singleton_eq in H1; congruence)).
-Search -(Ensembles.In _ (Singleton _ _) _).
 all: (try specialize (H1 _ (In_singleton _ _ _))).
-Show Intros.
 all: (try specialize (H1 _ (In_singleton _ _))).
 all:
  (repeat
@@ -569,5 +567,19 @@ all:
    | H:_ \/ _ |- _ => inversion H; clear H
    end).
 all: (try congruence).
+-
+(intros; inversion H0; subst; eauto).
+all: (try (apply singleton_eq in H1; congruence)).
+all: (try specialize (H1 _ (In_singleton _ _ _))).
+all: (try specialize (H1 _ (In_singleton _ _))).
+all:
+ (repeat
+   match goal with
+   | H:exists _, _ |- _ => destruct H
+   | H:_ \/ _ |- _ => inversion H; clear H
+   end).
+all: (try congruence).
+-
+(intros; inversion H0; subst; eauto).
 (* Auto-generated comment: Succeeded. *)
 
