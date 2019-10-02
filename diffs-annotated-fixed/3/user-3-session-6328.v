1879cb145dbfcf4bc17740c560ab3019476f5bec
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:13:34 -0700

    user 3, session 6328, part 3

--- diffs-annotated-fixed/3/user-3-session-6328.v
+++ diffs-annotated-fixed/3/user-3-session-6328.v
@@ -82,37 +82,5 @@ Hint Resolve append_ok: core.
 Hint Resolve reset_ok: core.
 Hint Resolve recover_wipe: core.
 End LogAPI.
-Axiom (addr_to_block : addr -> proc block).
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqi4LWSV"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqrWwQPX"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Axiom (block_to_addr : block -> addr).
-Definition addr_to_block_spec State a :
-  Specification unit block unit State :=
-  fun (_ : unit) state =>
-  {|
-  pre := True;
-  post := fun r state' => state' = state /\ block_to_addr r = a;
-  recovered := fun _ state' => state' = state |}.
-Axiom
-  (addr_to_block_ok :
-     forall State a recover abstr,
-     proc_spec (@addr_to_block_spec State a) (addr_to_block a) recover abstr).
-Hint Resolve addr_to_block_ok: core.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqJDjLk4"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq6hCXzi"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
