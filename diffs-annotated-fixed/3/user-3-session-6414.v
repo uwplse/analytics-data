6528b29dff8e260448dc83639d8c90909e8334c4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:13:51 -0700

    user 3, session 6414, part 42

--- diffs-annotated-fixed/3/user-3-session-6414.v
+++ diffs-annotated-fixed/3/user-3-session-6414.v
@@ -110,18 +110,7 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqdgF84B"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Print nth.
-Definition log_abstraction (d : disk) (log : list block) : Prop :=
-  (exists b, diskGet d 0 =?= b /\ block_to_addr b = length log) /\
-  (forall a, a < length log -> diskGet d (log_addr a) =?= nth a log block0).
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqDHJ38c"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqEMvFKg"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-(* Auto-generated comment: Succeeded. *)
+Definition log_length_ok d log :=
+  exists b, diskGet d 0 =?= b /\ block_to_addr b = length log.
+(* Auto-generated comment: Failed. *)
 
