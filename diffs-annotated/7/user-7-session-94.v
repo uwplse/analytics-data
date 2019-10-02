1b4d7327b4fc50200ea1eae302ecc118c3f1db45
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:44 -0700

    user 7, session 94, part 13

--- diffs-annotated/7/user-7-session-94.v
+++ diffs-annotated/7/user-7-session-94.v
@@ -15,5 +15,26 @@ Proof.
 (intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
 Qed.
 Lemma match_ty_exist__inv : forall (v : ty) (X : id) (t : ty) (k : nat), |-[ S k] v <$ TExist X t -> exists tx : ty, |-[ k] v <$ [X := tx] t.
-(intros v; induction v; auto).
+(intros v; induction v; intros X t k Hm; assumption).
+Qed.
+Theorem match_ty__value_type_l : forall (v t : ty) (k : nat), |-[ k] v <$ t -> value_type v.
+Proof.
+(intros v t).
+generalize dependent v.
+(induction t; intros v k Hm).
+-
+(apply match_ty_cname__inv in Hm; subst).
+constructor.
+-
+(apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(constructor; [ eapply IHt1 | eapply IHt2 ]; eauto).
+-
+(apply match_ty_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto).
+-
+(apply match_ty_ref__weak_inv in Hm).
+(destruct Hm as [t' Heq]; subst).
+constructor.
+-
+(destruct k).
+contradiction.
 (* Failed. *)
