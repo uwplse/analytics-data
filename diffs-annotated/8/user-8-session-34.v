b3c6bf4acaf84dd8add737acfa52aecfe6d14a39
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:55 -0700

    user 8, session 34, part 4

--- diffs-annotated/8/user-8-session-34.v
+++ diffs-annotated/8/user-8-session-34.v
@@ -16,6 +16,23 @@ Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Proof.
 Timeout 1 About denote_circuit.
-Timeout 1 Print denote_circuit.
-Timeout 1 Print Ltac denote_circuit.
-(* Failed. *)
+Fact denote_compose :
+  forall safe w (c : Circuit w) (\206\147 : Ctx),
+  forall w' (f : Pat w -> Circuit w') (\206\1470 \206\1471 \206\1471' \206\14701 : Ctx) \207\129,
+  \206\147 \226\138\162 c :Circ ->
+  \206\1471 \226\138\162 f :Fun ->
+  \206\1471' \226\169\181 \206\1471 \226\136\153 \206\147 ->
+  \206\14701 \226\169\181 \206\1470 \226\136\153 \206\1471 ->
+  denote_circuit safe (compose c f) \206\1470 \206\1471' \207\129 =
+  compose_super (denote_circuit safe (f (add_fresh_pat w \206\1471)) \206\1470 (add_fresh_state w \206\1471)) (denote_circuit safe c \206\14701 \206\147) \207\129.
+Proof.
+(intros safe w c \206\147 TP).
+dependent induction TP.
+-
+(intros w' f \206\1470 \206\1471 \206\1471' \206\14701 WT pf_merge1 pf_merge2).
+(simpl).
+(unfold compose_super).
+(unfold denote_circuit).
+(simpl).
+(unfold pad).
+(rewrite (ctx_wtype_size w p \206\147) by easy).
