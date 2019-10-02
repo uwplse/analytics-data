3d74a300db074d27f3948732cc89db9fa5e385fb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:13:52 -0700

    user 3, session 6414, part 64

--- diffs-annotated-fixed/3/user-3-session-6414.v
+++ diffs-annotated-fixed/3/user-3-session-6414.v
@@ -146,10 +146,5 @@ Add Search Blacklist "Raw" "Proofs".
 (simpl; intuition).
 lia.
 Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqvd3z47"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
