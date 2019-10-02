d6d3f59430531c375d8b0328e1eb99d2c6b5e9ba
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:50 -0700

    user 3, session 960, part 372

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -5,14 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-From Coq Require Import ProofIrrelevance.
-From Coq Require Export String.
-From Coq Require Import Program.
-Timeout 1 Print LoadPath.
-From Coq Require Import FunInd Recdef.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0iIh4K"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
 (* Auto-generated comment: Succeeded. *)
 
