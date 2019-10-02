b09db2e30eaad49fbaac26ed45f77329494a4fd8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:18 -0700

    user 7, session 26, part 3

--- diffs-annotated/7/user-7-session-26.v
+++ diffs-annotated/7/user-7-session-26.v
@@ -23,4 +23,31 @@ Proof.
 (inversion H).
 (split; assumption).
 Qed.
+Lemma unite_pairs__preserves_nf : forall t1 t2 : ty, InNF( t1) -> InNF( t2) -> InNF( unite_pairs t1 t2).
+Proof.
+(intros ta tb Hnfa).
+generalize dependent tb.
+generalize dependent Hnfa.
+generalize dependent ta.
+(apply
+  (in_nf_mut (fun (t1 : ty) (Hat1 : atom_type t1) => forall t2, in_nf t2 -> InNF( unite_pairs t1 t2))
+     (fun (t1 : ty) (Hnf1 : in_nf t1) => forall t2, in_nf t2 -> InNF( unite_pairs t1 t2)));
+  try (solve
+   [ intros;
+      match goal with
+      | |- InNF( unite_pairs ?tx _) =>
+            remember tx as tz eqn:Heqtz ;
+             apply
+              (in_nf_mut (fun (t2 : ty) (_ : atom_type t2) => InNF( unite_pairs tz t2)) (fun (t2 : ty) (_ : in_nf t2) => InNF( unite_pairs tz t2)));
+             try (solve [ subst; intros; simpl; auto with DBBetaJulia ])
+      end ])).
+Qed.
+Lemma unite_pairs_t_union :
+  forall t t1 t2 : ty, ~ (exists ta tb, t = TUnion ta tb) -> unite_pairs t (TUnion t1 t2) = TUnion (unite_pairs t t1) (unite_pairs t t2).
+Proof.
+(intros t t1 t2 Hcontra).
+(destruct t; try (solve [ simpl; reflexivity ])).
+exfalso.
+(apply Hcontra).
+(eexists; reflexivity).
 (* Failed. *)
