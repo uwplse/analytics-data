ae5ae55d9c0f7befc1a0dff7e7f4ce7acb05fcd0
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:04 -0700

    user 7, session 120, part 3

--- diffs-annotated/7/user-7-session-120.v
+++ diffs-annotated/7/user-7-session-120.v
@@ -17,6 +17,6 @@ Proof.
 (intros w1).
 (specialize (Hsem1 w1); specialize (Hsem2 w1)).
 exists (Nat.max w21 w22).
-(intros v Hm).
-(apply match_ty_pair__inv in Hm; subst).
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]).
 (* Failed. *)
