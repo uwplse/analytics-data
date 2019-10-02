c91feae560a19e58a79dd43c1fbbc06a4a9f41ac
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:16:29 -0700

    user 3, session 7199, part 304

--- diffs-annotated-fixed/3/user-3-session-7199.v
+++ diffs-annotated-fixed/3/user-3-session-7199.v
@@ -395,9 +395,5 @@ step_proc.
 (unfold log_abstraction; intuition eauto).
 (unfold log_length_ok).
 (rewrite H).
-Admitted.
-Theorem reset_ok : proc_spec reset_spec reset recover abstr.
-Proof.
-Admitted.
 (* Auto-generated comment: Succeeded. *)
 
