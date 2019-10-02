f4d5d905c6041261e369cb2c81f1b392980090b3
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:18 -0700

    user 7, session 26, part 5

--- diffs-annotated/7/user-7-session-26.v
+++ diffs-annotated/7/user-7-session-26.v
@@ -9,7 +9,30 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-eexists.
-eexists.
+(apply Hcontra).
+(do 2 eexists).
 reflexivity.
+Qed.
+Ltac resolve_not_union := intros [tx [ty Hcontra]]; inversion Hcontra.
+Lemma unite_pairs_union_t : forall t1 t2 t' : ty, unite_pairs (TUnion t1 t2) t' = TUnion (unite_pairs t1 t') (unite_pairs t2 t').
+Proof.
+(intros t1 t2 t').
+(destruct t'; try (solve [ simpl; reflexivity ])).
+Qed.
+Lemma mk_nf_pair : forall t1 t2 : ty, MkNF( TPair t1 t2) = unite_pairs (MkNF( t1)) (MkNF( t2)).
+Proof.
+reflexivity.
+Qed.
+Lemma mk_nf_union : forall t1 t2 : ty, MkNF( TUnion t1 t2) = TUnion (MkNF( t1)) (MkNF( t2)).
+Proof.
+reflexivity.
+Qed.
+Lemma mk_nf_ref : forall t : ty, MkNF( TRef t) = TRef (MkNF( t)).
+Proof.
+reflexivity.
+Qed.
+Hint Resolve mk_nf_pair mk_nf_union mk_nf_ref: DBBetaJulia.
+Theorem mk_nf__in_nf : forall t : ty, InNF( MkNF( t)).
+Proof.
+(intros t; induction t; try (solve [ auto using unite_pairs__preserves_nf with DBBetaJulia ])).
 (* Failed. *)
