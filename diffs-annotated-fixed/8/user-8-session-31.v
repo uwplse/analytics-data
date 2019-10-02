7c8f0c35ca024dd3d9d7334c153e172fa5d549c2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:56:36 -0700

    user 8, session 31, part 70

--- diffs-annotated-fixed/8/user-8-session-31.v
+++ diffs-annotated-fixed/8/user-8-session-31.v
@@ -2762,5 +2762,9 @@ replace (size_ctx \206\147) with size_octx \206\147 by easy.
 *
 (unfold denote_circuit in IH).
 (rewrite Nat.add_sub).
+(rewrite <- size_octx_merge by easy).
+(rewrite <- pf_merge in *).
+(simpl).
+(eapply (IH p1); trivial).
 (* Auto-generated comment: Succeeded. *)
 
