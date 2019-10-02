f2099eae37547c2ff366107287855ea514421eb1
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:20 -0700

    user 7, session 159, part 19

--- diffs-annotated/7/user-7-session-159.v
+++ diffs-annotated/7/user-7-session-159.v
@@ -66,5 +66,7 @@ Lemma build_v_full :
   forall (X X' : id) (tx : ty) (w : nat) (t v : ty),
   |-[ w] v <$ [X := tx] t ->
   exists v' : ty, |-[ w] v' <$ [X := TVar X'] t /\ (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> |-[ w'] v <$ [X' := tx] t').
-(induction t'; intros Hm; try assumption).
+(induction t'; intros Hm; try assumption || (destruct w'; contradiction)).
++
+(apply match_ty_union__inv in Hm).
 (* Failed. *)
