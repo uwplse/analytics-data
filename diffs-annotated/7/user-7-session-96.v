e988053c38d104a63592cef9762e48643f66617d
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:45 -0700

    user 7, session 96, part 1

--- diffs-annotated/7/user-7-session-96.v
+++ diffs-annotated/7/user-7-session-96.v
@@ -52,18 +52,6 @@ clear IHv.
 exists v.
 auto.
 Qed.
-Lemma match_ty_exist__inv : forall (v : ty) (X : id) (t : ty) (k : nat), |-[ S k] v <$ TExist X t -> exists tx : ty, |-[ k] v <$ [X := tx] t.
+Lemma match_ty_exist__0_inv : forall (v : ty) (X : id) (t : ty), |-[ 0] v <$ TExist X t -> |-[ 0] v <$ t.
 Proof.
 (intros v; induction v; intros X t k Hm; assumption).
-Qed.
-Theorem match_ty__value_type_l : forall (k : nat) (v t : ty), |-[ k] v <$ t -> value_type v.
-Proof.
-(induction k; intros v t; generalize dependent v; induction t; intros v Hm;
-  try (solve
-   [ apply match_ty_cname__inv in Hm; subst; constructor
-   | apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; constructor; [ eapply IHt1 | eapply IHt2 ]; eauto
-   | apply match_ty_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto
-   | apply match_ty_ref__weak_inv in Hm; destruct Hm as [t' Heq]; subst; constructor
-   | destruct v; contradiction ])).
--
-(* Auto-generated comment: Failed. *)
