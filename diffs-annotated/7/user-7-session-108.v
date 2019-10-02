ab5b45935599cbafc8cf3b7e24d9778043667f0f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:58 -0700

    user 7, session 108, part 67

--- diffs-annotated/7/user-7-session-108.v
+++ diffs-annotated/7/user-7-session-108.v
@@ -231,12 +231,18 @@ subst.
 (destruct w').
 (inversion Hle).
 Search -id.
-subst.
+(rewrite Heq in Hcontra).
+(apply Hnotm).
+exists v.
+assumption.
++
 (simpl in Hcontra).
-(assert (Heq : beq_id i i = true)).
+(assert (Heq : beq_id X i = false)).
 {
 admit.
 }
-Search -beq_id_refl.
-(rewrite Heq).
+(rewrite Heq in Hcontra).
+(apply match_ty_exist__inv in Hcontra).
+(destruct Hcontra as [tx Hcontra]).
+(apply Hnotm).
 (* Failed. *)
