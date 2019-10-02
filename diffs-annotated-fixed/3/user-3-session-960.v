0ac1c74fe4b4b3018a7a5712191cb3f71a3cbf54
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:40 -0700

    user 3, session 960, part 208

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -204,6 +204,18 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Definition bounded_to_ascii (x : {x | x < 256}) : Ascii.ascii :=
-  Ascii.nat_of_ascii (proj1_sig x).
-(* Auto-generated comment: Failed. *)
+  Ascii.ascii_of_nat (proj1_sig x).
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqjMbEGo"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq7rBVPP"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Definition ascii_to_bounded (a : Ascii.ascii) : {x | x < 256}.
+refine (exist _ (Ascii.nat_of_ascii a) _).
+Search -Ascii.nat_of_ascii.
+(* Auto-generated comment: Succeeded. *)
 
