1c5bf2725d8e6f9f3a24d49b215ffafb7b0b6285
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:27 -0700

    user 2, session 5, part 45

--- diffs-annotated-fixed/2/user-2-session-5.v
+++ diffs-annotated-fixed/2/user-2-session-5.v
@@ -53,22 +53,8 @@ Check
 Redirect "/tmp/coqLRo0nb" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
-Fixpoint step (t : term) : term :=
-  match t with
-  | Nil => Nil
-  | Ident _ => Nil
-  | <a b> => if value a then <a (step b)> else <(step a) b>
-  | {f a} =>
-      if value f
-      then match f with
-           | [(Ident lam) (Ident arg) body] => body
-           | Ident prim => t
-           | _ => Nil
-           end
-      else {(step f) a}
-  end.
-Redirect "/tmp/coqYXWD7k" Print Ltac Signatures.
+Fixpoint subst (x : string) (u : term) (t : term) : term := t.
+Redirect "/tmp/coqVl2ZnQ" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
-Print step.
 (* Auto-generated comment: Succeeded. *)
 
