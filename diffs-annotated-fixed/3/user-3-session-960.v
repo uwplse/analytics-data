33f540d89aa3fcdd7c04824171499e9785133fc8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:43 -0700

    user 3, session 960, part 258

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -296,6 +296,11 @@ econstructor.
 (unfold ascii_to_bounded, bounded_to_ascii; simpl).
 Check Ascii.nat_ascii_embedding.
 (apply subset_eq_compat).
-(rewrite Ascii.nat_ascii_embedding).
+(rewrite Ascii.nat_ascii_embedding; auto).
+Print subset_eq_compat.
+-
+admit.
+-
+(hnf).
 (* Auto-generated comment: Succeeded. *)
 
