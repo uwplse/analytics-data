023494f5b9f53f8be91d5de92fbd39c5a99b9e8f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:46:22 -0700

    user 7, session 16, part 43

--- diffs-annotated-fixed/7/user-7-session-16.v
+++ diffs-annotated-fixed/7/user-7-session-16.v
@@ -379,12 +379,14 @@ Proof.
          destruct (cname_eq__decidable c1 c2);
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
-(split; intros t'; induction t'; try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt2_1 IHt2_2 ]);
+(split; intros t'; induction t'; try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 ]);
   try
    match goal with
    | |- Decidable.decidable (|- TCName ?c1 << TCName ?c2) =>
          destruct (cname_eq__decidable c1 c2);
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
++
+(right; solve_not_x_sub_r_y_full).
 (* Auto-generated comment: Failed. *)
 
