af2bbfcfdc29fe4e75995176e3c787f789ce4b65
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:59 -0700

    user 7, session 19, part 88

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,5 +3,5 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (destruct (max_inv_depth_le__components_le _ _ _ Htk) as [Htk1 Htk2]).
-(apply Nat.max_le_compat).
+(apply Nat.max_le_compat; auto).
 (* Failed. *)
