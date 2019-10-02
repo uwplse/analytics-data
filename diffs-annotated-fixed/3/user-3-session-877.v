2b904d6cc962729dedcffb27669c5a41d3e9d891
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:05 -0700

    user 3, session 877, part 5

--- diffs-annotated-fixed/3/user-3-session-877.v
+++ diffs-annotated-fixed/3/user-3-session-877.v
@@ -1366,6 +1366,6 @@ Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 iFrame.
-iDestruct "Hmbox" as "(Hwlock&Hlockinv&Hstatus)".
+(<ssreflect_plugin::ssrtclseq@0> iDestruct "Hmbox" as "[>Hmbox|Hmbox]" ; last  first).
 (* Auto-generated comment: Failed. *)
 
