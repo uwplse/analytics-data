b545b16e669bb35de245d7b805f073aa6ecadf8c
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:50 -0700

    user 7, session 205, part 2

--- diffs-annotated/7/user-7-session-205.v
+++ diffs-annotated/7/user-7-session-205.v
@@ -4,8 +4,15 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0282a.BaseDefs.
-Require Import BetaJulia.Sub0282a.BaseProps.
-Require Import BetaJulia.Sub0282a.BaseMatchProps.
 Require Import BetaJulia.Sub0282a.BaseSemSubProps.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Lemma build_v_full :
+  forall (X X' : id) (tx : ty) (w : nat) (t v : ty),
+  |-[ w] v <$ [BX := tx] t ->
+  exists v' : ty,
+    |-[ w] v' <$ [BX := TFVar X'] t /\
+    (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> (fresh_in_ty X' t' -> |-[ w'] v <$ t') /\ (free_in_ty X' t' -> |-[ w'] v <$ [FX' := tx] t')).
 (* Failed. *)
