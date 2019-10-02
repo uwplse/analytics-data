8b9f74d97427f63b450e0865de28d0d701676eb5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:04:45 -0700

    user 3, session 3788, part 40

--- diffs-annotated-fixed/3/user-3-session-3788.v
+++ diffs-annotated-fixed/3/user-3-session-3788.v
@@ -43,7 +43,7 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqtKERBA"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Notation "p" := (ProcMarker p) (at level 10, only printing).
+Notation "p" := (ProcMarker p) (at level 0, only printing).
 Theorem swapXY_ok :
   proc_spec
     (fun (_ : unit) state =>
