95c6cee6822fa8146abe0a51025d6252a8e24cb8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:07 -0700

    user 7, session 19, part 227

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -5,7 +5,6 @@ Add Search Blacklist "Private_" "_subproof".
 (match goal with
  | |- | ?t1 | <= | ?t2 | =>
        assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize (H _ Hm);
-        apply match_ty_i_union__inv in H; rewrite inv_depth_union; destruct H as [Hm1| Hm2];
-        [ apply Nat.le_trans with (| t1 |) | apply Nat.le_trans with (| t2 |) ]
+        apply match_ty_i_union__inv in H; rewrite inv_depth_union
  end).
 (* Failed. *)
