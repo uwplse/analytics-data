3f600a037755c42e69f032be4102d6fc02cba1b3
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:08 -0700

    user 8, session 53, part 6

--- diffs-annotated/8/user-8-session-53.v
+++ diffs-annotated/8/user-8-session-53.v
@@ -945,7 +945,4 @@ Admitted.
 Lemma assert_init_at_id :
   forall b m i, i < S m -> (assert_at b m i \194\183 init_at b m i \226\137\161 id_circ)%qc.
 Proof.
-(intros b m i Lt \207\129 safe).
-(simpl).
-(simpl_rewrite id_circ_spec; auto with wf_db).
-(* Failed. *)
+(simpl_rewrite id_circ_spec).
