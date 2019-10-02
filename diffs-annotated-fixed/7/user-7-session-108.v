695b509c8408bcbb8b9ac653922f9f7d76c82b8f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:50:53 -0700

    user 7, session 108, part 36

--- diffs-annotated-fixed/7/user-7-session-108.v
+++ diffs-annotated-fixed/7/user-7-session-108.v
@@ -256,6 +256,7 @@ Proof.
 -
 (left; exists (TCName c)).
 (apply match_ty_value_type__reflexive; constructor).
+(left; exists (TCName c); apply match_ty_cname).
 -
 admit.
 -
@@ -276,5 +277,16 @@ right.
 (left; exists (TEV i); apply match_ty_ev).
 -
 (left; exists (TCName c); apply match_ty_cname).
-(* Auto-generated comment: Failed. *)
+-
+admit.
+-
+admit.
+-
+(left; exists (TRef t)).
+(destruct k).
+reflexivity.
+(split; intros w1; exists w1; auto).
+-
+(destruct (IHt k) as [v Hm]).
+(* Auto-generated comment: Succeeded. *)
 
