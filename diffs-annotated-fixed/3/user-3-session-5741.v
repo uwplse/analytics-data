39369187216f1427eac080aa10c0acbc12570568
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:11:44 -0700

    user 3, session 5741, part 28

--- diffs-annotated-fixed/3/user-3-session-5741.v
+++ diffs-annotated-fixed/3/user-3-session-5741.v
@@ -602,5 +602,6 @@ Theorem identity_fn_applied_twice :
   forall f : bool -> bool,
   (forall x : bool, f x = x) -> forall b : bool, f (f b) = b.
 Proof.
+(intros f Hfix).
 (* Auto-generated comment: Succeeded. *)
 
