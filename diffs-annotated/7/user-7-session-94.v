9f5f50505cc0d7cde9f9451a1051a1bd1f515df7
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:43 -0700

    user 7, session 94, part 4

--- diffs-annotated/7/user-7-session-94.v
+++ diffs-annotated/7/user-7-session-94.v
@@ -14,21 +14,7 @@ Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
 Proof.
 (intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
 Qed.
-Lemma match_ty_cname__inv : forall (v : ty) (c : cname) (k : nat), |-[ k] v <$ TCName c -> v = TCName c.
-(intros v t).
-generalize dependent v.
-(induction t; intros v k Hm).
--
-(apply match_ty_cname__inv in Hm; subst).
-constructor.
--
-(apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(constructor; [ eapply IHt1 | eapply IHt2 ]; eauto).
--
-(apply match_ty_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto).
--
-(apply match_ty_ref__weak_inv in Hm).
-(destruct Hm as [t' Heq]; subst).
-constructor.
--
+Lemma match_ty_exist__inv : forall (v : ty) (X : id) (t : ty) (k : nat), |-[ S k] v <$ TExist X t -> exists tx : ty, |-[ k] v <$ [X := tx] t.
+Proof.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; contradiction ])).
 (* Failed. *)
