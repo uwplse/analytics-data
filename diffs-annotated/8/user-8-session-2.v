17a943a4a496b3d105140160e38b267a9ec70d2b
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:18 -0700

    user 8, session 2, part 115

--- diffs-annotated/8/user-8-session-2.v
+++ diffs-annotated/8/user-8-session-2.v
@@ -47,7 +47,3 @@ Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 (simpl; Msimpl).
 (dependent destruction u; simpl; Msimpl; reflexivity).
--
-(induction r; try reflexivity).
-(simpl).
-(destruct a).
