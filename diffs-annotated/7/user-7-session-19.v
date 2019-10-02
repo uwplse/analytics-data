845c7ed2b2e2bed2dd6d1ee7946c3fd64cf31c4c
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:13 -0700

    user 7, session 19, part 351

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,6 +3,5 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 exists (TRef t).
-(split; intros v Hm; [ destruct (match_ty_i_exists t2 k) as [v' Hm'] | destruct (match_ty_i_exists t1 k) as [v' Hm'] ]).
-(assert (Hmp : |-[ k] TPair v v' <$ TPair t1 t2) by (constructor; assumption)).
+(assert (Hmp : |-[ k] TPair v v' <$ TPair t1 t2)).
 (* Failed. *)
