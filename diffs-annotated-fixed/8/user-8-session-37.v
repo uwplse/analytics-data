c0d2f94c19ccea60a7f6fc20ce304e47a88dcd5d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:57:20 -0700

    user 8, session 37, part 78

--- diffs-annotated-fixed/8/user-8-session-37.v
+++ diffs-annotated-fixed/8/user-8-session-37.v
@@ -451,6 +451,5 @@ Proof.
 (intros W W' c \207\129 \207\129' safe T H D).
 (destruct safe; trivial).
 (unfold valid_ancillae_box in H).
-(rewrite H; assumption).
 (* Auto-generated comment: Succeeded. *)
 
