2ca97353a5b7ff6fc825a9f913a2051d770ac3a5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:06:07 -0700

    user 10, session 13, part 2

--- diffs-annotated/10/user-10-session-13.v
+++ diffs-annotated/10/user-10-session-13.v
@@ -5,5 +5,15 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-From Coq Require Import Stream.
-(* Failed. *)
+Timeout 1 Print LoadPath.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coq5lrxpQ" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+From Coq Require Import Streams.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqLohXfs" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+From ReductionEffect Require Import PrintingEffect.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqFkS8gC" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Check map.
