7bfe15de6d6c84897c74ec10062e7ee06ac03928
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:42 -0700

    user 7, session 192, part 29

--- diffs-annotated/7/user-7-session-192.v
+++ diffs-annotated/7/user-7-session-192.v
@@ -13,14 +13,8 @@ Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Lemma match_ty_subst_fresh : forall (X : id) (s : ty) (w : nat) (t v : ty), fresh_in_ty X t -> |-[ w] v <$ t -> |-[ w] v <$ [X := s] t.
-(intros X s; induction w; induction t; intros v HX Hm;
-  try (solve
-   [ rewrite subst_cname in *; assumption
-   | rewrite subst_pair; destruct (fresh_in_ty_pair__inv _ _ _ HX) as [HX1 HX2]; apply match_ty_pair__inv in Hm;
-      destruct Hm as [v1 [v2 [heq [Hm1 Hm2]]]]; subst; apply match_ty_pair; auto
-   | rewrite subst_union; destruct (fresh_in_ty_union__inv _ _ _ HX) as [HX1 HX2]; apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm];
-      [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto
-   | rewrite subst_ev in *; assumption ])).
+(apply match_ty_exist__0_inv in Hm; contradiction).
 -
-(apply match_ty_exist__0_inv in Hm).
+(unfold fresh_in_ty, fresh in HX).
+Search -IdSet.In.
 (* Failed. *)
