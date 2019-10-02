cf34e7a1bccc18573f725dc4726a2b40d2f548d9
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:46:46 -0700

    user 7, session 19, part 17

--- diffs-annotated-fixed/7/user-7-session-19.v
+++ diffs-annotated-fixed/7/user-7-session-19.v
@@ -55,6 +55,8 @@ Proof.
       | |- |-[ ?k'] ?v <$ TCName _ => apply match_ty_i_cname__inv in Hm; subst; reflexivity
       end).
 -
-(apply match_ty_i_pair__inv in Hm; subst).
+(apply match_ty_i_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+Search -match_ty_i__pair.
 (* Auto-generated comment: Failed. *)
 
