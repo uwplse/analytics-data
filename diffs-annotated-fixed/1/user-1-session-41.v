b5c14fe6587a30595ec9b4954a7b23219bf91bc3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:19 -0700

    user 1, session 41, part 98

--- diffs-annotated-fixed/1/user-1-session-41.v
+++ diffs-annotated-fixed/1/user-1-session-41.v
@@ -303,7 +303,7 @@ Inductive GT : Type :=
 Check fold_right.
 Fixpoint size_gt (G : GT) : nat :=
   match G with
-  | GFun G_1 G_2 => 1 + size_gt G_1 + size_ G_2
+  | GFun G_1 G_2 => 1 + size_gt G_1 + size_gt G_2
   | GRec l =>
       fold_right
         (fun x acc =>
