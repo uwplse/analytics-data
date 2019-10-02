474e540596936361140ebe16f9e9e952f9a87338
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:13:56 -0700

    user 3, session 6414, part 111

--- diffs-annotated-fixed/3/user-3-session-6414.v
+++ diffs-annotated-fixed/3/user-3-session-6414.v
@@ -208,10 +208,6 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Qed.
-Theorem get_ok : proc_spec get_spec get recover abstr.
-Proof.
-(unfold get; intros).
-(apply spec_abstraction_compose; simpl).
-step_proc.
+Hint Resolve get_len: core.
 (* Auto-generated comment: Succeeded. *)
 
