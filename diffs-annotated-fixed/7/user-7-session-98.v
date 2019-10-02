d0998ae40ed568e1184ed1366ecf4cb1a5267952
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:50:01 -0700

    user 7, session 98, part 23

--- diffs-annotated-fixed/7/user-7-session-98.v
+++ diffs-annotated-fixed/7/user-7-session-98.v
@@ -162,6 +162,17 @@ Proof.
 (apply match_ty_exist__0_inv in Hm).
 (destruct Hm as [Hv [tx Heqx]]; subst).
 (simpl in Hv).
-contradiction.
+(inversion Hv).
+-
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hmx]).
+(simpl in Hmx).
+(apply match_ty_union__inv in Hmx).
+(destruct Hmx as [Hmx| Hmx]).
++
+(apply match_ty_union_1).
+(apply match_ty_exist).
+exists tx.
+assumption.
 (* Auto-generated comment: Failed. *)
 
