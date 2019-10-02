8ceb0ba2b1e1485dc2ae5817417032c39c1ae245
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:26 -0700

    user 7, session 171, part 1

--- diffs-annotated/7/user-7-session-171.v
+++ diffs-annotated/7/user-7-session-171.v
@@ -68,10 +68,10 @@ Fixpoint subst (x : id) (s t : ty) :=
   | TUnion t1 t2 => TUnion ([x := s] t1) ([x := s] t2)
   | TExist y t' =>
       if IdSet.mem y (FV s)
-      then let z := gen_fresh (IDSet.union (FV s) (FV t')) in let tz := [y @ z] t' in TExist z (if beq_id x z then tz else [x := s] tz)
+      then let z := gen_fresh (IdSet.union (FV s) (FV t')) in let tz := [y @ z] t' in TExist z (if beq_id x z then tz else [x := s] tz)
       else TExist y (if beq_id x y then t' else [x := s] t')
   | TVar y => if beq_id x y then s else t
   | TEV y => t
   end
 where "'[' x ':=' s ']' t" := (subst x s t) : btjt_scope.
-(* Auto-generated comment: Failed. *)
+(* Failed. *)
