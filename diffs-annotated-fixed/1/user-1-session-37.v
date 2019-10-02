820ab7fd948512742382d2bc4eda0507d78d92bd
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:06 -0700

    user 1, session 37, part 10

--- diffs-annotated-fixed/1/user-1-session-37.v
+++ diffs-annotated-fixed/1/user-1-session-37.v
@@ -283,14 +283,13 @@ econstructor.
 (econstructor; eauto).
 Qed.
 End AGT_Spec.
+Require Import Coq.Lists.List.
 Module AGT_Bounded_Rows_Details.
-Search -unique.
-Search -uniqueness.
 Definition label := nat.
 Inductive ST : Type :=
   | SInt : ST
   | SBool : ST
   | SFun : ST -> ST -> ST
   | SRec : forall l : list (label * ST)%type, NoDup (map fst l) -> ST.
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
