9d1e662c906e485dbff516b74e528015ef46ad7d
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:10 -0700

    user 7, session 19, part 283

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -4,8 +4,5 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_union__inv in Hm2).
 (destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
-(assert (Hmt't : |-[ S k] TRef t' <$ TRef t)).
-(simpl).
-(intros v').
-split.
+(intros v'; split; intros Hm').
 (* Failed. *)
