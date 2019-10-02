ab03540ed03964ed646252ba543725cc0c84ac66
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:02:58 -0700

    user 8, session 79, part 1032

--- diffs-annotated-fixed/8/user-8-session-79.v
+++ diffs-annotated-fixed/8/user-8-session-79.v
@@ -1103,6 +1103,5 @@ rewrite_inPar'.
 (rewrite id_circ_spec).
 (rewrite strip_one_l_out_eq).
 (rewrite xorb_nb_b).
-rewrite_inPar.
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
