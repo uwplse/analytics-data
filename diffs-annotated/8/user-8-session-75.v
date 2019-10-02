90a1d8589117752b73e5d5e04fa294f6a7d57761
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:33 -0700

    user 8, session 75, part 7

--- diffs-annotated/8/user-8-session-75.v
+++ diffs-annotated/8/user-8-session-75.v
@@ -230,4 +230,3 @@ Timeout 1 Print LoadPath.
 Definition _R'_ (m : nat) := _R_ (2 * PI / INR (2 ^ m)).
 Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqLDAawB"
 Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
