8ee112a1ac0a1325a35c479be5ba1ba46e2866b1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:47:51 -0700

    user 7, session 19, part 567

--- diffs-annotated-fixed/7/user-7-session-19.v
+++ diffs-annotated-fixed/7/user-7-session-19.v
@@ -401,12 +401,6 @@ tauto.
 tauto.
 Admitted.
 Lemma value_sem_sub_i_union__inv : forall v : ty, value_type v -> forall ta tb : ty, ||- [v]<= [TUnion ta tb] -> ||- [v]<= [ta] \/ ||- [v]<= [tb].
-Proof.
-(intros v Hv ta tb Hsem; unfold sem_sub_i in Hsem).
-(assert (Hm : |-[ 0] v <$ v) by (apply match_ty_i__reflexive; assumption)).
-(assert (Hm : |-[ | TUnion ta tb |] v <$ v) by (apply match_ty_i__reflexive; assumption)).
-specialize (Hsem _ _ Hm).
-(apply match_ty_i_union__inv in Hsem).
-(destruct Hsem; [ left | right ]; unfold sem_sub_i; intros k v' Hm').
+Lemma match_ty_i__match_le_inv_depth : forall (k : nat) (t v : ty), |-[ k] v <$ t -> forall k' : nat, k' <= k -> |-[ k'] v <$ t.
 (* Auto-generated comment: Failed. *)
 
