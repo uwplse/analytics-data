51fead865c6bdf4e625596084212cd1e6d3e4706
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:02:01 -0700

    user 8, session 79, part 731

--- diffs-annotated-fixed/8/user-8-session-79.v
+++ diffs-annotated-fixed/8/user-8-session-79.v
@@ -1027,5 +1027,6 @@ restore_dims tensor_tac.
           rewrite size_ntensor in *; simpl in *; try rewrite Nat.mul_1_r in *)
  end; try (solve [ type_check ])).
 Set Printing All.
+Set Printing All.
 (* Auto-generated comment: Succeeded. *)
 
