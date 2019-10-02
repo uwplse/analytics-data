3f8bf3f18a6634a7a6377ae4e0052a8c9d59e9d6
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:44:01 -0700

    user 3, session 11371, part 12

--- diffs-annotated-fixed/3/user-3-session-11371.v
+++ diffs-annotated-fixed/3/user-3-session-11371.v
@@ -202,18 +202,5 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-Theorem mult_comm' :
-  forall m n : nat, m * n = n * m.
-Proof.
-(intros).
-(induction n as [| n' IHn']).
--
-(simpl).
-(rewrite mult_0_r).
-reflexivity.
--
-(simpl).
-(assert (H1 : m * S n' = m + m * n')).
-{
 (* Auto-generated comment: Succeeded. *)
 
