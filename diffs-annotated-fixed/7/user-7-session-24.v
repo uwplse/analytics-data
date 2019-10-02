d34c45183efc605ac0a5edb72ca29f29df905b26
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:48:20 -0700

    user 7, session 24, part 11

--- diffs-annotated-fixed/7/user-7-session-24.v
+++ diffs-annotated-fixed/7/user-7-session-24.v
@@ -23,6 +23,8 @@ Definition tflt := TCName NFlt.
 Definition tstr := TCName NStr.
 Definition tIntInt := TPair tint tint.
 Declare Scope btjt_scope.
+Delimit Scope btjt_scope with btjt.
+Open Scope btjt.
 Reserved Notation "'|' t '|'" (at level 20).
 Fixpoint inv_depth (t : ty) :=
   match t with
@@ -32,8 +34,6 @@ Fixpoint inv_depth (t : ty) :=
   | TRef t' => 1 + | t' |
   end
 where "'|' t '|'" := (inv_depth t) : btjt_scope.
-Delimit Scope btjt_scope with btjt.
-Open Scope btjt.
 Inductive value_type : ty -> Prop :=
   | VT_CName : forall cn, value_type (TCName cn)
   | VT_Pair : forall v1 v2, value_type v1 -> value_type v2 -> value_type (TPair v1 v2)
@@ -42,5 +42,19 @@ Hint Constructors value_type: DBBetaJulia.
 Declare Scope btjm_scope.
 Delimit Scope btjm_scope with btjm.
 Open Scope btjm.
+Reserved Notation "'|-[' k ']' v '<$' t" (at level 50).
+Reserved Notation "'||-[' k ']' '[' t1 ']' '=' '[' t2 ']'" (at level 45).
+Fixpoint match_ty (k : nat) :=
+  fix mty (v : ty) :=
+    fix mty' (t : ty) :=
+      match k, v, t with
+      | _, TCName c, TCName c' => c = c'
+      | _, TPair v1 v2, TPair t1 t2 => mty v1 t1 /\ mty v2 t2
+      | _, _, TUnion t1 t2 => mty' t1 \/ mty' t2
+      | S k, TRef t', TRef t => (inv_depth t <= k /\ inv_depth t' = inv_depth t) /\ (forall v, |-[ k] v <$ t' <-> |-[ k] v <$ t)
+      | _, _, _ => False
+      end
+where "|-[ k ']' v '<$' t" := (match_ty k v t) : btjm_scope.
+Definition sem_eq_k (k : nat) (t1 t2 : ty) := forall v : ty, |-[ k] v <$ t1 <-> |-[ k] v <$ t2.
 (* Auto-generated comment: Failed. *)
 
