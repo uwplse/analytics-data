370d0f2bd67155dee9a1c63a54e03fcf1d88e69d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:04:43 -0700

    user 3, session 3788, part 3

--- diffs-annotated-fixed/3/user-3-session-3788.v
+++ diffs-annotated-fixed/3/user-3-session-3788.v
@@ -35,6 +35,6 @@ monad_simpl.
 (eapply proc_spec_rx; [ solve [ eauto ] |  ]).
 Check proc_spec_rx.
 (cbn[pre post recovered]).
-step_proc.
-(* Auto-generated comment: Failed. *)
+(intros).
+(* Auto-generated comment: Succeeded. *)
 
