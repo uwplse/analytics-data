9dc52faa350fa3f736edf91c8399d63eba02c10c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:15 -0700

    user 1, session 41, part 48

--- diffs-annotated-fixed/1/user-1-session-41.v
+++ diffs-annotated-fixed/1/user-1-session-41.v
@@ -562,6 +562,9 @@ Search -(Ensembles.In _ (Singleton _ _) _).
 all: (try specialize (H1 _ (In_singleton _ _ _))).
 Show Intros.
 all: (try specialize (H1 _ (In_singleton _ _))).
-destruct_pairs.
+all: (repeat match goal with
+             | H:exists _, _ |- _ => destruct H
+             end).
+all: congruence.
 (* Auto-generated comment: Failed. *)
 
