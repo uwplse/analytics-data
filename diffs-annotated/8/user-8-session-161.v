3107b948569a563ed267fa3a0a7f4ddf3fea93e4
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:06:01 -0700

    user 8, session 161, part 0

---
+++ diffs-annotated/8/user-8-session-161.v
@@ -0,0 +1,12 @@
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqijezX9"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqRYLY5c"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Export QWIRE.Quantum.
