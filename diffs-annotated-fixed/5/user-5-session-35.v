3391749386728f9fb9ae41db538e7893cec6d932
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:45:18 -0700

    user 5, session 35, part 10

--- diffs-annotated-fixed/5/user-5-session-35.v
+++ diffs-annotated-fixed/5/user-5-session-35.v
@@ -212,12 +212,31 @@ Proof.
  end).
 intro x.
 (assert
-  (exists i,
-     eval L env
-       (Choose x (And (In (Var x) Ints) (Eq (Int 6) (Times (Var x) (Int 2))))) =
-     eval L env (Int i))).
+  (forall res,
+   eval L env
+     (Choose x (And (In (Var x) Ints) (Eq (Int 6) (Times (Var x) (Int 2))))) =
+   res -> res = eval L env (Int 3))).
 {
 (intros).
-(apply -> evalInInts; try reflexivity).
+(assert
+  (eval L
+     (extendEnv env x
+        (eval L env (Choose x (Eq (Int 6) (Times (Var x) (Int 2))))))
+     (Eq (Int 6) (Times (Var x) (Int 2))) = L.(eval) env (Bool true))).
+{
+(apply evalChoose).
+exists (eval L env (Int 3)).
+(erewrite evalBoolConst).
+apply -> evalEqTrue.
+(rewrite evalTimes with (i := 3%Z) (j := 2%Z)).
+-
+reflexivity.
+-
+(rewrite evalVar).
+(rewrite extendEnv_eq).
+(apply evalIntConst).
+-
+reflexivity.
+}
 (* Auto-generated comment: Succeeded. *)
 
