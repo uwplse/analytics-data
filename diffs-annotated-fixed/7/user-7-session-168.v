26d15aadcd86af5b0c01f4b7ea32f5d9cb55893b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:52:46 -0700

    user 7, session 168, part 1

--- diffs-annotated-fixed/7/user-7-session-168.v
+++ diffs-annotated-fixed/7/user-7-session-168.v
@@ -33,8 +33,5 @@ Proof.
 -
 (apply sem_sub__refl).
 -
-(apply sem_sub__trans with t2; assumption).
--
-(apply sem_sub_pair; assumption).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
