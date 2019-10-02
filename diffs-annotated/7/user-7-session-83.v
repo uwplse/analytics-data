f8b4861b0720394ef07f63bbfb3e435c5d214dea
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:38 -0700

    user 7, session 83, part 22

--- diffs-annotated/7/user-7-session-83.v
+++ diffs-annotated/7/user-7-session-83.v
@@ -5,10 +5,7 @@ Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0250a.BaseDefs.
-+
-(apply value_sem_sub_k_i_union__inv in Hsem; try assumption).
-(destruct Hsem as [Hsem| Hsem]; [ apply union_right_1 | apply union_right_2 ]; auto).
-+
+clear IHt2.
 (simpl in Hdep).
-(pose proof (le_S_n _ _ Hdep) as Hdep').
+(pose proof (le_S_n _ _ Hdep) as Hdt').
 (* Failed. *)
