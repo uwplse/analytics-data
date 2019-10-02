498d98750745196074102b0cec35178334826619
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:11:43 -0700

    user 3, session 5741, part 22

--- diffs-annotated-fixed/3/user-3-session-5741.v
+++ diffs-annotated-fixed/3/user-3-session-5741.v
@@ -566,16 +566,11 @@ reflexivity.
 Qed.
 Theorem andb_true_elim2 : forall b c : bool, andb b c = true -> c = true.
 Proof.
-(intros [] [] H).
+(intros b [] H).
 -
 reflexivity.
 -
 (destruct H).
 reflexivity.
--
-(destruct H).
-reflexivity.
--
-(destruct H).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
