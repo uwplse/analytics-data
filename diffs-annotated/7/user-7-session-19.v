ad5d10cc84a0ddc3aeea20a84cb2933cf898de7e
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:06 -0700

    user 7, session 19, part 213

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,7 +2,11 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Lemma bbb : forall (k : nat) (t t' v : ty), |-[ k] v <$ t -> |-[ k] v <$ t' -> | t | <= | t' |.
-Proof.
-(induction k; induction t; induction t').
+(induction k; induction t; induction t'; intros v Hmt Hmt').
+Show 2.
+32: {
+idtac.
+(simpl).
+(apply le_n_S).
+(apply IHk).
 (* Failed. *)
