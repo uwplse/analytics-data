8292709e735d700350ad42309a5c5da2adaf8fe5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:49 -0700

    user 7, session 203, part 10

--- diffs-annotated/7/user-7-session-203.v
+++ diffs-annotated/7/user-7-session-203.v
@@ -197,5 +197,7 @@ right.
 exists 0,(TCName c).
 (apply match_ty_cname).
 -
-(destruct IHt1 as [IHt1| [w1 [v1 Hm1]]]; destruct IHt2 as [IHt2| [w2 [v2 Hm2]]]).
+(destruct IHt1 as [IHt1| [w1 [v1 IHt1]]]; destruct IHt2 as [IHt2| [w2 [v2 IHt2]]]).
++
+(left; intros w v Hm; apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]).
 (* Failed. *)
