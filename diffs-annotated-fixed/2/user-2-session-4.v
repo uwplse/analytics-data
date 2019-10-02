deb815bda6d5ce711093427ec1ef166c4a755966
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:24 -0700

    user 2, session 4, part 2

--- diffs-annotated-fixed/2/user-2-session-4.v
+++ diffs-annotated-fixed/2/user-2-session-4.v
@@ -17,7 +17,7 @@ Inductive term :=
   | Ident : string -> term
   | Cons : term -> term -> term
   | App : term -> term -> term.
-Redirect "/tmp/coqJpJYaV" Print Ltac Signatures.
+Redirect "/tmp/coqn5gNxo" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 (* Auto-generated comment: Succeeded. *)
 
