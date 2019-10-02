1f236c5bd0fea0d6a787a28e0a7e43d614bf3a92
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:07 -0700

    user 7, session 122, part 9

--- diffs-annotated/7/user-7-session-122.v
+++ diffs-annotated/7/user-7-session-122.v
@@ -28,28 +28,6 @@ exists (Nat.max w21 w22).
 Qed.
 Lemma sem_sub_pair : forall t1 t2 t1' t2' : ty, ||- [t1]<= [t1'] -> ||- [t2]<= [t2'] -> ||- [TPair t1 t2]<= [TPair t1' t2'].
 Proof.
-(intros t1 t2 t1' t2' Hem1 Hsem2 k).
+(intros).
 (apply sem_sub_k_pair; auto).
-Qed.
-Lemma sem_sub_k_union : forall (k : nat) (t1 t2 t' : ty), ||-[ k][t1]<= [t'] -> ||-[ k][t2]<= [t'] -> ||-[ k][TUnion t1 t2]<= [t'].
-Proof.
-(intros k t1 t2 t' Hsem1 Hsem2).
-(intros w1).
-(specialize (Hsem1 w1); specialize (Hsem2 w1)).
-(destruct Hsem1 as [w21 Hsem1]; destruct Hsem2 as [w22 Hsem2]).
-exists (Nat.max w21 w22).
-(intros v Hm).
-(apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ specialize (Hsem1 _ Hm) | specialize (Hsem2 _ Hm) ]; [ apply match_ty_union_1 | apply match_ty_union_2 ];
-  eapply match_ty__ge_w; try eassumption).
-(destruct Hm as [Hm| Hm]; [ specialize (Hsem1 _ Hm) | specialize (Hsem2 _ Hm) ]; [ apply Hsem1 | apply Hsem2 ]; eapply match_ty__ge_w;
-  try eassumption).
-(destruct Hm as [Hm| Hm]; [ specialize (Hsem1 _ Hm) | specialize (Hsem2 _ Hm) ]; eapply match_ty__ge_w).
-(destruct Hm as [Hm| Hm]; [ specialize (Hsem1 _ Hm) | specialize (Hsem2 _ Hm) ]; eapply match_ty__ge_w; [ eapply Hsem1 | eapply Hsem2 ]).
-(destruct Hm as [Hm| Hm]; [ specialize (Hsem1 _ Hm) | specialize (Hsem2 _ Hm) ]; eapply match_ty__ge_w; try eassumption).
-(apply Nat.le_max_l).
-(apply Nat.le_max_r).
-Qed.
-Lemma sem_sub_union : forall t1 t2 t' : ty, ||- [t1]<= [t'] -> ||- [t2]<= [t'] -> ||- [TUnion t1 t2]<= [t'].
-Proof.
 (* Failed. *)
