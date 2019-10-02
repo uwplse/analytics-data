ca98bd31ae3332755f40979e9bbb364e82734739
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:04:59 -0700

    user 8, session 125, part 13

--- diffs-annotated-fixed/8/user-8-session-125.v
+++ diffs-annotated-fixed/8/user-8-session-125.v
@@ -39,8 +39,6 @@ Timeout 1 Print unitary_gate_unitary.
 matrix_denote.
 setoid_rewrite denote_unitary_transpose.
 (simpl in *; Msimpl).
-(repeat rewrite Mmult_assoc; try rewrite denote_unitary_unitary).
-Timeout 1 About denote_unitary_unitary.
-Timeout 1 Print Ltac denote_unitary_unitary.
+Search -denote_unitary.
 (* Auto-generated comment: Succeeded. *)
 
