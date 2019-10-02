08fdbf8531153cfeff5b032343658a20face84a8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:09 -0700

    user 7, session 19, part 265

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -5,5 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_union__inv in Hm2).
 (destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
 (apply match_ty_i_ref__inv in Hm2).
-(destruct Hm2 as [t'' [Heq' Href']]).
+(destruct Hm2 as [t'' [Heq' Href']]; inversion Heq'; subst).
 (* Failed. *)
