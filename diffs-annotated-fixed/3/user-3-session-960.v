c3c172704b03f387e951982f0afa6256c38cabff
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:37 -0700

    user 3, session 960, part 151

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -119,6 +119,25 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq1tRDll"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Check nat_to_le_equation.
+Fixpoint le_to_nat base (digits : list {x : nat | x < S (S base)}) : nat :=
+  match digits with
+  | nil => 0
+  | digit :: digits' => proj1_sig digit * base + le_to_nat digits'
+  end.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8Bga3l"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqq7lPKp"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Theorem nat_le_inverse base : forall n, le_to_nat (nat_to_le base n) = n.
+Proof.
+(intros).
+(induction n as [n IHn] using lt_wf_ind).
+(destruct n; simpl).
+(rewrite nat_to_le_equation; simpl).
 (* Auto-generated comment: Succeeded. *)
 
