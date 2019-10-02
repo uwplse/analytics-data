4f043da1472e3612b449bb59133c04106214f7b0
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:58 -0700

    user 7, session 19, part 63

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -4,15 +4,23 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (simpl).
 Lemma match_ty_i__inv_depth_stable :
-  forall (t : ty) (k k' : nat),
+  forall (k k' : nat) (t : ty),
   inv_depth t <= k -> inv_depth t <= k' -> forall v : ty, inv_depth v <= k -> inv_depth v <= k' -> |-[ k] v <$ t <-> |-[ k'] v <$ t.
 Proof.
-(induction t; intros k k' Htk Htk' v Hvk Hvk').
+(induction k; induction k').
+-
+tauto.
 -
 admit.
 -
 admit.
 -
+(induction t).
 admit.
--
+admit.
+admit.
++
+clear IHk'.
+(intros Htk Htk' v Hvk Hvk').
+(simpl in Htk).
 (* Failed. *)
