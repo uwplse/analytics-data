55d2c27af9701de0b5efa140b908fe6367a37661
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:55 -0700

    user 8, session 34, part 7

--- diffs-annotated/8/user-8-session-34.v
+++ diffs-annotated/8/user-8-session-34.v
@@ -26,4 +26,3 @@ Fact denote_compose :
   denote_circuit safe (compose c f) \206\1470 \206\1471' \207\129 =
   compose_super (denote_circuit safe (f (add_fresh_pat w \206\1471)) \206\1470 (add_fresh_state w \206\1471)) (denote_circuit safe c \206\14701 \206\147) \207\129.
 (unfold pad).
-(rewrite (ctx_wtype_size w p \206\147) by easy).
