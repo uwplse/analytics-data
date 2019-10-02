0faac8a566b56966cd42b477a5d1aaf37a46a21a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:16 -0700

    user 1, session 41, part 58

--- diffs-annotated-fixed/1/user-1-session-41.v
+++ diffs-annotated-fixed/1/user-1-session-41.v
@@ -565,5 +565,16 @@ specialize (H _ (In_singleton _ _)).
   | H:exists _, _ |- _ => destruct H
   | H:_ \/ _ |- _ => inversion H; clear H
   end).
+congruence.
+-
+specialize (H _ (In_singleton _ _)).
+(repeat
+  match goal with
+  | H:exists _, _ |- _ => destruct H
+  | H:_ \/ _ |- _ => inversion H; clear H
+  end).
+congruence.
+-
+specialize (H _ (In_singleton _ _)).
 (* Auto-generated comment: Succeeded. *)
 
