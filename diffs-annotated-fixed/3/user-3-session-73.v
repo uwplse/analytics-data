69ff1a474bcf7e4e7970484fb4f2a9fba0b73641
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:47 -0700

    user 3, session 73, part 3

--- diffs-annotated-fixed/3/user-3-session-73.v
+++ diffs-annotated-fixed/3/user-3-session-73.v
@@ -56,6 +56,15 @@ Lemma proc_rspec_refine_rec T R (p : proc C_Op T)
   _ <- absr; rexec c_sem p rec ---> v <- spec_aexec spec; _ <- absr; pure v.
 Proof.
 (intros Hprspec Habstr_pre).
-(intros sA sC b ([], (sTstart, (Hrd, Hexec)))).
+Admitted.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqd7tWFz"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqgXtIXj"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
