0e0f77415ec721b51e72613b897fb2eef11ef972
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:04 -0700

    user 7, session 224, part 3

--- diffs-annotated/7/user-7-session-224.v
+++ diffs-annotated/7/user-7-session-224.v
@@ -147,5 +147,6 @@ reflexivity.
 (rewrite b_subst_neq__permute in Hm; try assumption || auto).
 (apply b_free_in_ty_exist_neq__inv in HX; try assumption).
 Check b_free_in_ty__b_free_in_b_subst_neq.
-(apply (b_free_in_ty__b_free_in_b_subst_neq i ti) in HX).
+(apply (b_free_in_ty__b_free_in_b_subst_neq i ti) in HX; try assumption).
+specialize (IHw _ _ _ HX Hm).
 (* Failed. *)
