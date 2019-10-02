b0c112c778aa42099564af544f88640e4d144cf3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:49 -0700

    user 3, session 960, part 343

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -190,8 +190,11 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqN87KBx"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Theorem nat_from_le_zeros base_m2 digits zero_v n :
-  proj1_sig zero_v 0 ->
+  proj1_sig zero_v = 0 ->
   @nat_from_le base_m2 (digits ++ repeat zero_v n) =
   @nat_from_le base_m2 digits.
-(* Auto-generated comment: Failed. *)
+Proof.
+(intros).
+(induction digits; simpl).
+(* Auto-generated comment: Succeeded. *)
 
