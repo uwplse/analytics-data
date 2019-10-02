69e8464e21e30f3fa6106fb38960046e75b1f938
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:02 -0700

    user 7, session 116, part 20

--- diffs-annotated/7/user-7-session-116.v
+++ diffs-annotated/7/user-7-session-116.v
@@ -20,16 +20,6 @@ Proof.
 (unfold fresh in *).
 (split; intros Hcontra; [ apply (IdSetFacts.union_2 fvs2) in Hcontra | apply (IdSetFacts.union_3 fvs1) in Hcontra ]).
 Qed.
-Lemma subs_fresh : forall (X : id) (t : ty), fresh_in_ty X t -> forall s : ty, [X := s] t = t.
-Proof.
-(intros X t).
-(unfold fresh_in_ty in *).
-(simpl in Hfresh).
-(apply fresh_union__inv in Hfresh).
-(destruct Hfresh as [Hfresh1 Hfresh2]).
-(simpl).
-(rewrite IHt1; try assumption).
-(rewrite IHt2; try assumption).
-reflexivity.
+(induction t; intros Hfresh s; try (solve [ reflexivity ]); unfold fresh_in_ty in *; simpl in Hfresh).
 -
 (* Failed. *)
