4bd77f8672953868d4aed60e8c72e3c1e5bd097b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:04:43 -0700

    user 3, session 3788, part 7

--- diffs-annotated-fixed/3/user-3-session-3788.v
+++ diffs-annotated-fixed/3/user-3-session-3788.v
@@ -44,7 +44,7 @@ Proof.
 (unfold swapXY).
 monad_simpl.
 (match goal with
- | |- proc_spec _ ?p _ _ => pose proof (ProcMarker p)
+ | |- proc_spec _ ?p _ _ => pose proof (AProc p)
  end).
 (* Auto-generated comment: Succeeded. *)
 
