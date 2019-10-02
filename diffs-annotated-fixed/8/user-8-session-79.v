b92d1f4700f765f8450fc5e686d874d05e294bf5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:04:11 -0700

    user 8, session 79, part 1395

--- diffs-annotated-fixed/8/user-8-session-79.v
+++ diffs-annotated-fixed/8/user-8-session-79.v
@@ -1160,6 +1160,7 @@ clear IS.
 5: (erewrite denote_box_compat).
 6: (erewrite denote_box_compat).
 7: (erewrite kron_compat).
+8: reflexivity.
 9: (erewrite denote_box_compat).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
