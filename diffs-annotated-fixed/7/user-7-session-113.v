ff30fd548f4c2ad273a8c4c6ae640be4187bdf16
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:51:14 -0700

    user 7, session 113, part 2

--- diffs-annotated-fixed/7/user-7-session-113.v
+++ diffs-annotated-fixed/7/user-7-session-113.v
@@ -14,6 +14,6 @@ Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
 Proof.
 (intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
 Qed.
-Lemma subs_neq__permute : forall X Y : id, X <> Y -> forall t s1 s2 : ty, t = t.
+Lemma subs_neq__permute : forall X Y : id, X <> Y -> forall t s1 s2 : ty, [X := s1] ([Y := s2] t) = [Y := s2] ([X := s1] t).
 (* Auto-generated comment: Failed. *)
 
