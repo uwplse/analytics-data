01d96918e73082e9815833fedcbba15c0d432b72
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:43 -0700

    user 7, session 194, part 2

--- diffs-annotated/7/user-7-session-194.v
+++ diffs-annotated/7/user-7-session-194.v
@@ -4,14 +4,26 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0281a.BaseDefs.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
 Open Scope btjt.
 Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
 Proof.
 (intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
 Qed.
+Lemma f_free_in_ty__decidable : forall (X : id) (t : ty), Decidable.decidable (f_free_in_ty X t).
+Proof.
+(intros X t).
+(unfold f_free_in_ty).
+(apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
+Qed.
+Lemma b_free_in_ty__decidable : forall (X : id) (t : ty), Decidable.decidable (b_free_in_ty X t).
+Proof.
+(intros X t).
+(unfold f_free_in_ty).
+(apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
+Qed.
+Lemma f_free_in_ty__dec : forall (X : id) (t : ty), {f_free_in_ty X t} + {not_f_free_in_ty X t}.
+Proof.
+(intros X t).
+(unfold f_free_in_ty).
+(apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
 (* Failed. *)
