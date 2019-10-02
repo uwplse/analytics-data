a01f53eee6e708426110fd99d9cf22aee15f5f8a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:16:17 -0700

    user 3, session 7199, part 104

--- diffs-annotated-fixed/3/user-3-session-7199.v
+++ diffs-annotated-fixed/3/user-3-session-7199.v
@@ -307,6 +307,6 @@ Lemma firstn_one_more :
   S a <= length d -> firstn a d ++ [nth a d block0] = firstn (S a) d.
 Proof.
 (intros).
-Print firstn.
+(induction a).
 (* Auto-generated comment: Succeeded. *)
 
