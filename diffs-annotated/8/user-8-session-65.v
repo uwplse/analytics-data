3d9cf22f4ae56fa4c9d543be306fb7d730f36cad
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:14 -0700

    user 8, session 65, part 13

--- diffs-annotated/8/user-8-session-65.v
+++ diffs-annotated/8/user-8-session-65.v
@@ -948,5 +948,16 @@ Fact assert_at_spec_unsafe :
 Admitted.
 Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqqi4Q9u"
 Print Ltac Signatures.
-restore_dims.
+Timeout 1 About restore_dims.
+Timeout 1 Print restore_dims.
+Timeout 1 Print Ltac restore_dims.
+(match goal with
+ | |- ?A => let A' := restore_dims_rec tac A in
+            replace
+            A
+            with
+            A'
+ end).
+2: {
+unify_matrix_dims.
 (* Failed. *)
