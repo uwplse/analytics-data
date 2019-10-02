db366f898ab385b2d46fef7854c16fb963a123d2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:02:58 -0700

    user 3, session 3307, part 1

--- diffs-annotated-fixed/3/user-3-session-3307.v
+++ diffs-annotated-fixed/3/user-3-session-3307.v
@@ -5,17 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Export List.
-Require Export Lia.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqYb3Ku6"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqlUzZqI"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
 (* Auto-generated comment: Succeeded. *)
 
