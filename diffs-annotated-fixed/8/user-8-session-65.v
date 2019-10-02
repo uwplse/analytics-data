487bf59fcfe02c825f066a4307673bd52c2c0477
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:58:42 -0700

    user 8, session 65, part 175

--- diffs-annotated-fixed/8/user-8-session-65.v
+++ diffs-annotated-fixed/8/user-8-session-65.v
@@ -1107,6 +1107,14 @@ omega.
 -
 (apply H_c2; auto).
 Qed.
+Open Scope matrix_scope.
 Lemma denote_box_id_circ : forall b w \207\129, denote_box b (id_circ : Box w w) \207\129 == \207\129.
+Proof.
+(intros b w \207\129).
+(simpl).
+autounfold with den_db.
+(simpl).
+(rewrite add_fresh_split; simpl).
+(rewrite subst_pat_fresh_empty).
 (* Auto-generated comment: Succeeded. *)
 
