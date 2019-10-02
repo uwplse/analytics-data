cc0b6196d78b2a30097d48d5c4c0f9a0c35df2f5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:48:31 -0700

    user 7, session 29, part 3

--- diffs-annotated-fixed/7/user-7-session-29.v
+++ diffs-annotated-fixed/7/user-7-session-29.v
@@ -225,6 +225,6 @@ Lemma sem_eq_k__comm :
   forall (t1 t2 : ty) (k : nat), ||-[ k][t1]= [t2] -> ||-[ k][t2]= [t1].
 (intros t1 t2 k Hsem).
 (unfold sem_eq_k in *).
-auto.
+tauto.
 (* Auto-generated comment: Failed. *)
 
