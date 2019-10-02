5941f60a489562f0cd882530cb66ce57338ad913
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:22 -0700

    user 7, session 161, part 17

--- diffs-annotated/7/user-7-session-161.v
+++ diffs-annotated/7/user-7-session-161.v
@@ -78,8 +78,8 @@ assumption.
 assumption.
 Abort.
 Lemma subst_nested : forall (X Y : id) (t tx ty : ty), [X := tx] ([Y := ty] t) = [Y := [X := tx] ty] ([X := tx] t).
-(induction t; intros tx ty; try reflexivity).
+(rewrite IHt1, IHt2).
+reflexivity.
 -
 (repeat rewrite subst_pair).
-(rewrite IHt1).
-(* Failed. *)
+(rewrite IHt1, IHt2).
