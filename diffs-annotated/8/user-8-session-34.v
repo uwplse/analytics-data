1dc53bcd4647dde7e2d97882ea9429bf27092bb9
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:57 -0700

    user 8, session 34, part 39

--- diffs-annotated/8/user-8-session-34.v
+++ diffs-annotated/8/user-8-session-34.v
@@ -25,4 +25,6 @@ Fact denote_compose :
   \206\14701 \226\169\181 \206\1470 \226\136\153 \206\1471 ->
   denote_circuit safe (compose c f) \206\1470 \206\1471' \207\129 =
   compose_super (denote_circuit safe (f (add_fresh_pat w \206\1471)) \206\1470 (add_fresh_state w \206\1471)) (denote_circuit safe c \206\14701 \206\147) \207\129.
-replace (compose (gate g p1 f)) with gate g p1 (fun p2 => compose (f p2)) by auto.
+(intros WT pf_merge1 pf_merge2).
+replace (compose (gate g p1 f0) f) with gate g p1 (fun p2 => compose (f0 p2) h) by auto.
+(* Failed. *)
