c7cda0502171ed6b5039a457a2eff776dc641648
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:11:00 -0700

    user 3, session 5526, part 6

--- diffs-annotated-fixed/3/user-3-session-5526.v
+++ diffs-annotated-fixed/3/user-3-session-5526.v
@@ -191,7 +191,23 @@ invert_abstraction.
 {
 (eexists; eauto).
 }
+(step_proc; intuition idtac).
 {
+(eexists; eauto).
+}
+(step_proc; intuition idtac).
++
+replace (diskSize (stateDisk state) - 1) with diskSize s in * by lia.
+(exists s; repeat split; auto).
+(destruct (stateBadBlock state == diskSize s)).
+*
+(rewrite disk_oob_eq by lia; simpl; auto).
+*
+(rewrite <- Hremap by eauto; auto).
++
+(eexists; eauto).
+-
+invert_abstraction.
 (step_proc; intuition idtac).
 (* Auto-generated comment: Succeeded. *)
 
