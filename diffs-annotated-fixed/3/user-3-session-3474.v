4a0e42a9295d093771cf9d569df17fa15a3c71a4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:03:32 -0700

    user 3, session 3474, part 4

--- diffs-annotated-fixed/3/user-3-session-3474.v
+++ diffs-annotated-fixed/3/user-3-session-3474.v
@@ -5,7 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Import Helpers.Helpers.
-Require Import Proc.
 (* Auto-generated comment: Succeeded. *)
 
