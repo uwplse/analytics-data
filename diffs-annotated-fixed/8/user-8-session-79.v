080891bf96b3c6aa1197be4158a17e1368628ed0
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:01:20 -0700

    user 8, session 79, part 528

--- diffs-annotated-fixed/8/user-8-session-79.v
+++ diffs-annotated-fixed/8/user-8-session-79.v
@@ -1012,6 +1012,5 @@ Timeout 1 Print Ltac rewrite_inPar.
  end; try (solve [ type_check ])).
 show_dimensions.
 (simpl in *; rewrite size_ntensor in *; try rewrite Nat.mul_1_r in *).
-(rewrite IP).
 (* Auto-generated comment: Succeeded. *)
 
