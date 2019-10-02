502cba7f2842711f0ee5b10d1fdefcc59cfa8170
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:30 -0700

    user 5, session 36, part 10

--- diffs-annotated/5/user-5-session-36.v
+++ diffs-annotated/5/user-5-session-36.v
@@ -259,5 +259,5 @@ clear H.
 (rewrite evalAnd in H0).
 (destruct (evalIn _ _ _)).
 (match goal with
- | H:eval ?L ?env (If (In ?x ?S) _ _) |- _ => destruct (evalIn L env x S)
+ | H:eval ?L ?env (If (In ?x ?S) _ _) |- _ => idtac
  end).
