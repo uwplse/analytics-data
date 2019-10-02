20d32f6dbaac14517b852681e158405e4e69e610
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:37 -0700

    user 7, session 2, part 39

--- diffs-annotated/7/user-7-session-2.v
+++ diffs-annotated/7/user-7-session-2.v
@@ -16,6 +16,48 @@ Open Scope btjr_scope.
 (apply sub_r_nf_union_l__inv in Hsub2; try assumption).
 Check unite_pairs_union_t.
 Check sub_r_nf_union_l__inv.
-constructor.
-auto.
+Lemma sub_r__mk_nf_sub_r : forall t t' : ty, |- t << t' -> |- MkNF( t) << MkNF( t').
+Proof.
+(intros t t' Hsub; induction Hsub; try (solve [ simpl; constructor ])).
+-
+(simpl).
+(apply unite_pairs_of_nf__preserves_sub_r; assumption || apply mk_nf__in_nf).
+-
+(simpl).
+(constructor; assumption).
+-
+(apply SR_UnionR1; assumption).
+-
+(apply SR_UnionR2; assumption).
+-
+(simpl).
+(constructor; assumption).
+-
+(rewrite <- mk_nf__idempotent).
+assumption.
+Qed.
+Lemma sub_r__reflexive : forall t : ty, |- t << t.
+Proof.
+(apply sub_r__rflxv).
+Qed.
+Lemma sub_r__transitive : forall t1 t2 t3 : ty, |- t1 << t2 -> |- t2 << t3 -> |- t1 << t3.
+Proof.
+(intros t1 t2 t3 Hsub1).
+generalize dependent t3.
+(induction Hsub1; intros t3 Hsub2).
+-
+assumption.
+-
+(remember (TPair t1' t2') as tm eqn:Heq ).
+(induction Hsub2; try (solve [ inversion Heq | constructor ])).
++
+(inversion Heq; subst).
+(constructor; auto with DBBetaJulia).
++
+(apply SR_UnionR1; tauto).
++
+(apply SR_UnionR2; tauto).
++
+subst.
+clear IHHsub2.
 (* Failed. *)
