23a6eb75485b5016e5c8a8022147fad587f2314d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:49:51 -0700

    user 7, session 94, part 6

--- diffs-annotated-fixed/7/user-7-session-94.v
+++ diffs-annotated-fixed/7/user-7-session-94.v
@@ -54,6 +54,8 @@ auto.
 Qed.
 Lemma match_ty_exist__inv : forall (v : ty) (X : id) (t : ty) (k : nat), |-[ S k] v <$ TExist X t -> exists tx : ty, |-[ k] v <$ [X := tx] t.
 Proof.
-(intros v; induction v; try (solve [ intros X t k Hm; destruct k; contradiction ])).
+(intros v; induction v).
+-
+(intros X t k Hm).
 (* Auto-generated comment: Failed. *)
 
