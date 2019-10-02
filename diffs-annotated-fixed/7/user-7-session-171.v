abb9724b58d2e09cee2cbbd2bb1782b921462be7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:52:52 -0700

    user 7, session 171, part 17

--- diffs-annotated-fixed/7/user-7-session-171.v
+++ diffs-annotated-fixed/7/user-7-session-171.v
@@ -101,10 +101,6 @@ Fixpoint subst (x : id) (s t : ty) {measure size t :=
   end
 where "'[' x ':=' s ']' t" := (subst x s t) : btjt_scope.
 Next Obligation.
-(simpl).
-Search -S.
-Search -"+".
-(rewrite <- Nat.add_1_r).
 Omega.omega.
 (* Auto-generated comment: Failed. *)
 
