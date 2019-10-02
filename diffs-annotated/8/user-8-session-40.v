7cf966297410cf2a9d96017c68de212b2882f242
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:00 -0700

    user 8, session 40, part 26

--- diffs-annotated/8/user-8-session-40.v
+++ diffs-annotated/8/user-8-session-40.v
@@ -27,25 +27,5 @@ autorewrite with proof_db.
 (simpl).
 (rewrite Nat.sub_diag).
 (rewrite super_I; easy).
-Lemma init0_spec : forall safe, denote_box safe init0 (I (2 ^ 0)) == \226\136\1630\226\159\169\226\159\1680\226\136\163.
-Proof.
-(intros).
 matrix_denote.
 Msimpl.
-reflexivity.
-Qed.
-Lemma init1_spec : forall safe, denote_box safe init1 (I (2 ^ 0)) == \226\136\1631\226\159\169\226\159\1681\226\136\163.
-Proof.
-(intros).
-matrix_denote.
-Msimpl.
-reflexivity.
-Qed.
-Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqeKDbME"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Lemma assert0_spec : forall safe, denote_box safe assert0 \226\136\1630\226\159\169\226\159\1680\226\136\163 == I 1.
-Proof.
-(destruct safe).
--
-matrix_denote.
