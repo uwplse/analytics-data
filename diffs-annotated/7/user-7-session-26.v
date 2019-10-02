82e6abe8fef61d81a524775c5a762658112cfe13
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:19 -0700

    user 7, session 26, part 20

--- diffs-annotated/7/user-7-session-26.v
+++ diffs-annotated/7/user-7-session-26.v
@@ -113,5 +113,5 @@ Proof.
 (split; [ eapply Nat.max_lub_l | eapply Nat.max_lub_r ]; eassumption).
 Qed.
 Lemma inv_depth_mk_nf : forall t : ty, | MkNF( t) | = | t |.
-(split; tauto).
+(split; auto).
 (* Failed. *)
