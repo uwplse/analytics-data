fc08ac073cb40b674e91736b5056c9e283c90e6b
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:05 -0700

    user 7, session 19, part 201

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,5 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(induction k; induction t; induction t'; intros H; try solve constructor).
+(induction k; induction t; induction t'; intros H; try (solve [ simpl; constructor ])).
 (* Failed. *)
