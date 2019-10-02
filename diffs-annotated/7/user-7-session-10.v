ed4391c03259a228fda5462c11e6c48ac2dcf2a3
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:43 -0700

    user 7, session 10, part 26

--- diffs-annotated/7/user-7-session-10.v
+++ diffs-annotated/7/user-7-session-10.v
@@ -13,14 +13,7 @@ Require Import Coq.Bool.Bool.
 Close Scope btj_scope.
 Open Scope btjnf_scope.
 Open Scope btjr_scope.
-Lemma sub_r_nf__trans :
-  forall tm1 tm2 : ty,
-  |- tm1 << tm2 -> InNF( tm1) -> InNF( tm2) -> (forall tl : ty, |- tl << tm1 -> |- tl << tm2) /\ (forall tr : ty, |- tm2 << tr -> |- tm1 << tr).
-Proof.
-(intros tm1 tm2 Hsub).
-(induction Hsub; intros Hnfm1 Hnfm2).
--
-tauto.
--
-(destruct (in_nf_pair__inv _ _ Hnfm1) as [Hnfmx Hnfmy]).
+(destruct (in_nf_pair__inv _ _ Hnfm1) as [Hnfm11 Hnfm12]).
+(destruct (in_nf_pair__inv _ _ Hnfm2) as [Hnfm21 Hnfm22]).
+(destruct IHHsub1 as [IHHsub11 IHHsub12]).
 (* Failed. *)
