975b18cf38593e0fcc9d5194e7fb9fb3f7e99198
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:15 -0700

    user 7, session 19, part 380

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -5,13 +5,7 @@ Add Search Blacklist "Private_" "_subproof".
 exists (TRef t).
 (split; intros v Hm; assert (Hmu : |-[ k] v <$ TUnion t1 t2) by (apply match_ty_i_union_1; assumption) || (apply match_ty_i_union_2; assumption);
   apply Hsem; assumption).
-(induction k; induction t; induction t'; intros Hnft Hsem; try (solve [ simpl; constructor ]);
-  try (solve
-   [ match goal with
-     | Hsem:||-[ ?k][?t1]<= [?t2]
-       |- | ?t1 | <= | ?t2 | =>
-           assert (Hv : value_type t1) by constructor; assert (Hm : |-[ k] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
-            (Hsem _ Hm); contradiction
-     end ])).
+-
 (assert (Hv : value_type (TCName c)) by constructor).
+(pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ H) as Hsemu).
 (* Failed. *)
