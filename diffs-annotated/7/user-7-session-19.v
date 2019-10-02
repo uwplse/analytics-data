c292ca87824d9da630b71a37096d69fe2493ee82
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:11 -0700

    user 7, session 19, part 315

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -4,5 +4,8 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_union__inv in Hm2).
 (destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
-(induction k; intros v Hv; induction Hv; intros ta tb Hsem; unfold sem_sub_k_i in Hsem).
+Lemma value_sem_sub_k_union__value_sem_sub_k_component :
+  forall v : ty, value_type v -> forall (k : nat) (ta tb : ty), ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
+Proof.
+(intros v Hv; induction Hv; intros k ta tb Hsem; unfold sem_sub_k_i in Hsem).
 (* Failed. *)
