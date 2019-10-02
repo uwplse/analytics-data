949130ee24f07c7e0e38a536be4dd4e267c16f58
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:51:32 -0700

    user 7, session 122, part 2

--- diffs-annotated-fixed/7/user-7-session-122.v
+++ diffs-annotated-fixed/7/user-7-session-122.v
@@ -37,7 +37,6 @@ Proof.
 (intros w1).
 (specialize (Hsem1 w1); specialize (Hsem2 w1)).
 (destruct Hsem1 as [w21 Hsem1]; destruct Hsem2 as [w22 Hsem2]).
-(destruct Hm as [Hm| Hm]; [ specialize (Hsem1 _ Hm) | specialize (Hsem2 _ Hm) ]; [ apply match_ty_union_1 | apply match_ty_union_2 ];
-  eapply match_ty__ge_w; try eassumption).
+(destruct Hm as [Hm| Hm]; [ specialize (Hsem1 _ Hm) | specialize (Hsem2 _ Hm) ]).
 (* Auto-generated comment: Failed. *)
 
