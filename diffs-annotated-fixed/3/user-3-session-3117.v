f2272cad045020556bf7a4d6dc22e118e5da0d43
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:02:24 -0700

    user 3, session 3117, part 14

--- diffs-annotated-fixed/3/user-3-session-3117.v
+++ diffs-annotated-fixed/3/user-3-session-3117.v
@@ -119,6 +119,6 @@ Proof.
 (destruct a; simpl; intros).
 Search -"-" -(_ -> _ < _).
 (apply lt_O_minus_lt).
-Print "<".
-(* Auto-generated comment: Succeeded. *)
+constructor.
+(* Auto-generated comment: Failed. *)
 
