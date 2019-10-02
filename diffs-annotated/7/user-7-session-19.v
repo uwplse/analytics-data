d320839e8115a35ef3db38c40f570f24983bb94e
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:05 -0700

    user 7, session 19, part 193

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,40 +2,22 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(intros v; induction v; try (solve [ intros t k Hm; destruct k; simpl in Hm; contradiction ])).
-clear IHv.
-(intros t k).
-(intros Hm).
-exists v.
-reflexivity.
-Qed.
-Lemma match_ty_i_ref__inv :
-  forall v t : ty, forall k : nat, |-[ S k] v <$ TRef t -> exists t' : ty, v = TRef t' /\ (forall v' : ty, |-[ k] v' <$ t' <-> |-[ k] v' <$ t).
-Proof.
-(intros v; induction v; try (solve [ intros t k Hm; destruct k; simpl in Hm; contradiction ])).
-clear IHv.
-(intros t k).
-(intros Hm).
-(pose proof Hm as Href).
-(simpl in Href).
-exists v.
-split.
-reflexivity.
-(intros v').
-specialize (Href v').
-(destruct Href; split; assumption).
-Qed.
-Lemma match_ty_i__value_type : forall (t : ty) (k : nat) (v : ty), |-[ k] v <$ t -> value_type v.
-Proof.
-(induction t; intros k v Hm).
--
-(apply match_ty_i_cname__inv in Hm; subst).
-constructor.
--
-(apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(constructor; [ eapply IHt1 | eapply IHt2 ]; eauto).
--
-(apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto).
--
 (apply match_ty_i_ref__weak_inv in Hm).
+(destruct Hm as [t' Heq]; subst).
+constructor.
+Qed.
+Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
+Proof.
+(induction k; induction t; induction t'; intros H).
+32: {
+idtac.
+(simpl).
+(apply le_n_S).
+(apply IHk).
+(assert (Hv : value_type (TRef t)) by constructor).
+(assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; constructor)).
+specialize (H _ Hm).
+(apply match_ty_i_ref__inv in H).
+(destruct H as [tx [Heq Href]]; inversion Heq; subst).
+auto.
 (* Failed. *)
