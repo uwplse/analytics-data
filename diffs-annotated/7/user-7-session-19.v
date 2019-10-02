8d43406e4bcaf7b9cf3549c921a4353f5a0ded43
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:04 -0700

    user 7, session 19, part 183

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,13 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Lemma match_ty_i__value_type : forall (t : ty) (k : nat) (v : ty), |-[ k] v <$ t -> value_type v.
-Proof.
-(induction t; intros k v Hm).
--
-(apply match_ty_i_cname__inv in Hm; subst).
-constructor.
--
-(apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(constructor; auto).
+(constructor; [ apply IHt1 | apply IHt2 ]; auto).
 (* Failed. *)
