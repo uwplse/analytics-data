518ef141449fdfbbe043eea0ce584bc5f66489ee
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:45:52 -0700

    user 7, session 2, part 148

--- diffs-annotated-fixed/7/user-7-session-2.v
+++ diffs-annotated-fixed/7/user-7-session-2.v
@@ -240,6 +240,8 @@ split.
 Abort.
 Lemma sub_r_nf__transitive : forall t1 t2 t3 : ty, |- t1 << t2 -> InNF( t1) -> InNF( t2) -> |- t2 << t3 -> |- t1 << t3.
 Proof.
+Lemma sub_r_nf__transitive : forall t1 t2 t3 : ty, |- t1 << t2 -> InNF( t1) -> InNF( t2) -> |- t2 << t3 -> |- t1 << t3.
+Proof.
 (intros t1 t2 t3 Hsub1).
 generalize dependent t3.
 (induction Hsub1; intros t3 Hnf1 Hnf2 Hsub2; try (solve [ assumption ])).
@@ -278,52 +280,6 @@ generalize dependent t3.
 (inversion H0; subst).
 (remember (TRef t') as tx eqn:Heqx ).
 (induction Hsub2; inversion Heqx; subst).
-+
-(apply SR_UnionR1; tauto).
-+
-(apply SR_UnionR2; tauto).
-+
-Abort.
-Lemma weird_trans : forall t1 t2 t3 : ty, InNF( t1) -> InNF( t2) -> InNF( t3) -> |- t1 << t2 -> |- t2 << t3 -> |- t1 << t3.
-Proof.
-Abort.
-Lemma sub_r__mk_nf_sub_r : forall t t' : ty, |- t << t' -> |- MkNF( t) << MkNF( t').
-Proof.
-(intros t t' Hsub; induction Hsub; try (solve [ simpl; constructor ])).
--
-(simpl).
-(apply unite_pairs_of_nf__preserves_sub_r; assumption || apply mk_nf__in_nf).
--
-(simpl).
-(constructor; assumption).
--
-(apply SR_UnionR1; assumption).
--
-(apply SR_UnionR2; assumption).
--
-(simpl).
-(constructor; assumption).
--
-(rewrite <- mk_nf__idempotent).
-assumption.
-Qed.
-Lemma sub_r__reflexive : forall t : ty, |- t << t.
-Proof.
-(apply sub_r__rflxv).
-Qed.
-Lemma sub_r__transitive : forall t1 t2 t3 : ty, |- t1 << t2 -> |- t2 << t3 -> |- t1 << t3.
-Proof.
-(intros t1 t2 t3 Hsub1).
-generalize dependent t3.
-(induction Hsub1; intros t3 Hsub2).
--
-assumption.
--
-(remember (TPair t1' t2') as tm eqn:Heq ).
-(induction Hsub2; try (solve [ inversion Heq | constructor ])).
-+
-(inversion Heq; subst).
-(constructor; auto with DBBetaJulia).
 +
 (apply SR_UnionR1; tauto).
 +
