605aa632487fa3d1bee5c868b9efc4de799b30bb
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:16 -0700

    user 7, session 150, part 1

--- diffs-annotated/7/user-7-session-150.v
+++ diffs-annotated/7/user-7-session-150.v
@@ -34,5 +34,39 @@ Proof.
 (apply fresh_union__inv in Hfresh).
 assumption.
 Qed.
-Lemma fresh_in_ty_ref__inv : forall (X : id) (t : ty), fresh_in_ty X (TRef t) -> fresh_in_ty X t.
-(* Auto-generated comment: Failed. *)
+Lemma subs_fresh_in_ty : forall (X : id) (t : ty), fresh_in_ty X t -> forall s : ty, [X := s] t = t.
+(intros X t).
+(induction t; intros Hfresh s; try (solve [ reflexivity ]); unfold fresh_in_ty in *; simpl in Hfresh; simpl).
+-
+(apply fresh_union__inv in Hfresh).
+(destruct Hfresh as [Hfresh1 Hfresh2]).
+(rewrite IHt1; try assumption).
+(rewrite IHt2; try assumption).
+reflexivity.
+-
+(apply fresh_union__inv in Hfresh).
+(destruct Hfresh as [Hfresh1 Hfresh2]).
+(rewrite IHt1; try assumption).
+(rewrite IHt2; try assumption).
+reflexivity.
+-
+(destruct (beq_idP X i); try reflexivity).
+(rewrite IHt).
+reflexivity.
+(unfold fresh in *).
+(intros Hcontra).
+(apply Hfresh).
+(apply IdSetFacts.remove_2; try assumption).
+(intros Heq).
+subst.
+contradiction.
+-
+(unfold fresh in Hfresh).
+(destruct (beq_idP X i); try reflexivity).
+subst.
+exfalso.
+(apply Hfresh).
+(apply IdSetFacts.singleton_2).
+reflexivity.
+Qed.
+(* Failed. *)
