07263e1272e6741da8425a020a2027a686925ba3
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:04 -0700

    user 7, session 19, part 181

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -4,4 +4,7 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Lemma match_ty_i__value_type : forall (t : ty) (k : nat) (v : ty), |-[ k] v <$ t -> value_type v.
 Proof.
+(induction t; intros k v Hm).
+-
+Abort.
 (* Failed. *)
