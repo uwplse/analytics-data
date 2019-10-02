33490e7dca825ccb599f30d54bb5f26763ff7177
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:45 -0700

    user 7, session 199, part 2

--- diffs-annotated/7/user-7-session-199.v
+++ diffs-annotated/7/user-7-session-199.v
@@ -152,18 +152,9 @@ assumption.
 Qed.
 Lemma match_ty__match_ty_f_subst_int : forall (X : id) (w : nat) (t v : ty), |-[ w] v <$ t -> exists v' : ty, |-[ w] v' <$ [BX := tint] t.
 Proof.
-(intros X; induction w; induction t; intros v;
-  try (solve
-   [ intros Hm; exists v; assumption
-   | intros Hm; apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; destruct (IHt1 _ Hm1) as [v1' Hm1'];
-      destruct (IHt2 _ Hm2) as [v2' Hm2']; exists (TPair v1' v2'); rewrite b_subst_pair; apply match_ty_pair; assumption
-   | intros Hm; apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm]; [ destruct (IHt1 _ Hm) as [v' Hm'] | destruct (IHt2 _ Hm) as [v' Hm'] ];
-      exists v'; rewrite b_subst_union; [ apply match_ty_union_1 | apply match_ty_union_2 ]; assumption
-   | intros Hm; destruct (beq_idP X i);
-      [ subst; rewrite subst_var_eq; exists tint; reflexivity | rewrite subst_var_neq; try assumption; exists v; assumption ] ])).
--
 (intros Hm).
 (apply match_ty_exist__0_inv in Hm; contradiction).
 -
 (intros Hm).
+(simpl).
 (* Failed. *)
