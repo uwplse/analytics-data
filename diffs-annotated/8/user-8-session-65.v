696f112637c0aa3aecedf8c65cee6a43c17b541a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:15 -0700

    user 8, session 65, part 20

--- diffs-annotated/8/user-8-session-65.v
+++ diffs-annotated/8/user-8-session-65.v
@@ -949,4 +949,5 @@ Admitted.
 Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqqi4Q9u"
 Print Ltac Signatures.
 Timeout 1 About restore_dims.
-(apply f_equal_gen; trivial).
+(do 2 apply f_equal_gen; trivial).
+(* Failed. *)
