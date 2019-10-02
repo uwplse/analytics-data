63e2fa900f480aaa3191d83002fa44b7353b87ca
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:36 -0700

    user 3, session 960, part 133

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -94,7 +94,7 @@ Function
    | _ =>
        let base := base_m2 in
        let digit := x `mod` base in
-       exist (fun x => x < base) digit _ :: nat_to_le base (x / base)
+       exist (fun x => x < base) digit _ :: nat_to_le base_m2 (x / base)
    end.
 (* Auto-generated comment: Failed. *)
 
