ae82b89f70e22a704257f1c4542487d58739405c
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:07 -0700

    user 7, session 19, part 240

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,7 +2,7 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(induction k; intros v Hv; induction Hv; intros ta tb Hsem).
 6: {
-idtac Abort.
+idtac.
+(unfold sem_sub_i_k).
 (* Failed. *)
