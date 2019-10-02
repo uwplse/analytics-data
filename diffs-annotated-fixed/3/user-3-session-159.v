f7af95ed23a2cf7cdae48d6a9f745660f22a8a4f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:51:05 -0700

    user 3, session 159, part 3

--- diffs-annotated-fixed/3/user-3-session-159.v
+++ diffs-annotated-fixed/3/user-3-session-159.v
@@ -6,34 +6,38 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Require Import Examples.StatDb.Impl.
-Require Import Spec.Hoare.
-Require Import Spec.HoareTactics.
-Require Import Spec.AbstractionSpec.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqXI6axi"
+From Transitions Require Import NonError.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4Wt8Ur"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0BLEGv"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqgwakKd"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Require Import Spec.Hoare.
+Require Import Spec.HoareTactics.
+Require Import Spec.AbstractionSpec.
 Definition absr : relation DB.l.(State) Var.l.(State) unit :=
   fun l res =>
   match res with
   | Val s _ => fst s = fold_right plus 0 l /\ snd s = length l
   | Err _ _ => False
   end.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqiQZm2s"
+Instance absr_non_error : (NonError absr).
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq6gXHSd"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4uHC6k"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqdPtyvh"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-Instance absr_non_error : (NonError absr).
+Proof.
+(hnf).
 (* Auto-generated comment: Succeeded. *)
 
