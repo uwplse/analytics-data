52ed2068391c1173348525fc2bcb43fa052851ef
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:07 -0700

    user 7, session 122, part 10

--- diffs-annotated/7/user-7-session-122.v
+++ diffs-annotated/7/user-7-session-122.v
@@ -26,8 +26,25 @@ exists (Nat.max w21 w22).
 (apply Nat.le_max_l).
 (apply Nat.le_max_r).
 Qed.
-Lemma sem_sub_pair : forall t1 t2 t1' t2' : ty, ||- [t1]<= [t1'] -> ||- [t2]<= [t2'] -> ||- [TPair t1 t2]<= [TPair t1' t2'].
-Proof.
-(intros).
+(intros t1 t2 t1' t2' Hem1 Hsem2 k).
 (apply sem_sub_k_pair; auto).
+Qed.
+Lemma sem_sub_k_union : forall (k : nat) (t1 t2 t' : ty), ||-[ k][t1]<= [t'] -> ||-[ k][t2]<= [t'] -> ||-[ k][TUnion t1 t2]<= [t'].
+Proof.
+(intros k t1 t2 t' Hsem1 Hsem2).
+(intros w1).
+(specialize (Hsem1 w1); specialize (Hsem2 w1)).
+(destruct Hsem1 as [w21 Hsem1]; destruct Hsem2 as [w22 Hsem2]).
+exists (Nat.max w21 w22).
+(intros v Hm).
+(apply match_ty_union__inv in Hm).
+(destruct Hm as [Hm| Hm]; [ specialize (Hsem1 _ Hm) | specialize (Hsem2 _ Hm) ]; eapply match_ty__ge_w; try eassumption).
+(apply Nat.le_max_l).
+(apply Nat.le_max_r).
+Qed.
+Lemma sem_sub_union : forall t1 t2 t' : ty, ||- [t1]<= [t'] -> ||- [t2]<= [t'] -> ||- [TUnion t1 t2]<= [t'].
+Proof.
+(intros t1 t2 t' Hem1 Hsem2 k).
+(apply sem_sub_k_union; auto).
+Qed.
 (* Failed. *)
