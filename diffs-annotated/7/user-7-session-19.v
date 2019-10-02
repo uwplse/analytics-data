26e46b1a8a80c329a6fc1d6d9ec1c350361eaf2b
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:03 -0700

    user 7, session 19, part 155

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,4 +3,11 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_pair; auto).
+-
+(assert (IH1 : forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ TCName c <-> |-[ k] v <$ t'1)).
+{
+(intros k v Hv).
+specialize (H k v Hv).
+(destruct H as [H1 H2]).
+(split; intros Hm).
 (* Failed. *)
