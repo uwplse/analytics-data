ff773596352e155c9302582fcfb4996779b7adad
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:35 -0700

    user 7, session 183, part 3

--- diffs-annotated/7/user-7-session-183.v
+++ diffs-annotated/7/user-7-session-183.v
@@ -92,5 +92,5 @@ specialize (Hmem HY).
 reflexivity.
 Qed.
 Lemma subst_exist : forall (X : id) (s : ty) (Y : id) (t : ty), exists (Z : id) (tz : ty), [X := s] TExist X t = TExist Z tz.
-Proof Lemma subst_id : forall (X : id) (t : ty), [X := TVar X] t = t.
+Proof.
 (* Failed. *)
