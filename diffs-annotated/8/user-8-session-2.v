eb6a9858abb540beab18e24ed0bec15ef7e10d44
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:18 -0700

    user 8, session 2, part 116

--- diffs-annotated/8/user-8-session-2.v
+++ diffs-annotated/8/user-8-session-2.v
@@ -47,3 +47,20 @@ Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 (simpl; Msimpl).
 (dependent destruction u; simpl; Msimpl; reflexivity).
+(simpl).
+(destruct a; Msimpl; rewrite IHr; reflexivity).
+-
+(simpl).
+(destruct a; Msimpl; rewrite IHl; reflexivity).
+Qed.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqoQ4Vef"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Lemma denote_ctrls_transpose :
+  forall W (n : nat) (u : Unitary W) li,
+  denote_ctrls n (trans u) li = (denote_ctrls n u li) \226\128\160.
+Proof.
+(intros).
+(unfold denote_ctrls).
+(destruct (ctrls_to_list (repeat false n) li u) as [[j l] v] eqn:E).
+(apply ctrls_to_list_transpose in E).
