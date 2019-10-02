6d75478b00e6d93ebe07bd27263e5fc303925939
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:05:03 -0700

    user 8, session 125, part 28

--- diffs-annotated-fixed/8/user-8-session-125.v
+++ diffs-annotated-fixed/8/user-8-session-125.v
@@ -41,6 +41,6 @@ setoid_rewrite denote_unitary_transpose.
 (simpl in *; Msimpl).
 (repeat rewrite Mmult_assoc).
 restore_dims.
-(simpl_rewrite unitary_gate_unitary).
+setoid_rewrite unitary_gate_unitary.
 (* Auto-generated comment: Failed. *)
 
