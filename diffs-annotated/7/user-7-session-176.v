ba5f53f15112115c5d74eee1c1db492b5c47cd11
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:31 -0700

    user 7, session 176, part 31

--- diffs-annotated/7/user-7-session-176.v
+++ diffs-annotated/7/user-7-session-176.v
@@ -143,5 +143,5 @@ Lemma match_ty__match_ty_subst_int : forall (X : id) (w : nat) (t v : ty), |-[ w
 Lemma match_ty__subst_neq_permute :
   forall (X Y : id) (sx sy : ty) (w : nat) (v t : ty), X <> Y -> |-[ w] v <$ [Y := sy] ([X := sx] t) <-> |-[ w] v <$ [X := sx] ([Y := sy] t).
 (split; repeat rewrite subst_pair; intros Hm; apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(destruct (IHt1 _ Hm1) as [IHt1 _]; destruct (IHt2 _ Hm2) as [IHt2 _]; exists (TPair v1' v2'); rewrite subst_pair; apply match_ty_pair; assumption).
+(destruct (IHt1 v1) as [IHt1 _]; destruct (IHt2 v2) as [IHt2 _]).
 (* Failed. *)
