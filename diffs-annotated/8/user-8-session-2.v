018ea46c85e8d27dc87a4b2424fa7f9442605c36
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:24 -0700

    user 8, session 2, part 202

--- diffs-annotated/8/user-8-session-2.v
+++ diffs-annotated/8/user-8-session-2.v
@@ -53,5 +53,5 @@ Timeout 1 About ctrls_to_list.
 Lemma denote_ctrls_transpose :
   forall W (n : nat) (u : Unitary W) li,
   (forall x, In x li -> x < n)%nat -> (length li = \226\159\166 W \226\159\167)%nat -> denote_ctrls n (trans u) li == (denote_ctrls n u li) \226\128\160.
-specialize (ctrls_to_list_spec _ _ _ _ _ _ H H0).
+specialize (ctrls_to_list_spec _ _ _ _ _ _ _ H H0).
 (* Failed. *)
