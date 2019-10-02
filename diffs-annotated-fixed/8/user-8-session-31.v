26b6f592690f5405e53da682edc3c48b02e92677
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:56:25 -0700

    user 8, session 31, part 10

--- diffs-annotated-fixed/8/user-8-session-31.v
+++ diffs-annotated-fixed/8/user-8-session-31.v
@@ -2188,8 +2188,8 @@ Proof.
 (simpl; fold_denotation).
 Timeout 1 About process_gate.
 Timeout 1 Print process_gate.
-replace (process_gate g p1 \206\147) with
- (process_gate_pat g p1 \206\147, process_gate_state g p1 \206\147)
- by (symmetry; apply surjective_pairing).
+Timeout 1 About process_gate_state.
+Timeout 1 Print process_gate_state.
+Timeout 1 Print Ltac process_gate_state.
 (* Auto-generated comment: Failed. *)
 
