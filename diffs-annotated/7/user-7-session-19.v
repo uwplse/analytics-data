b993ff8fde324ec0ba96d8b6da1b0fe289faabed
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:01 -0700

    user 7, session 19, part 126

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,11 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Lemma match_ty_i__reflexive : forall v : ty, value_type v -> forall k : nat, |-[ k] v <$ v.
-Proof.
-(intros v Hv; induction Hv; intros k).
--
-(destruct k; reflexivity).
--
-(apply match_ty_i_pair; auto).
+(apply match_ty_i_pair; tauto).
 (* Failed. *)
