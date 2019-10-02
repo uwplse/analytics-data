a668b1abe29f89da3f7b2a59dc82d5fa03b9088e
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:50 -0700

    user 7, session 16, part 50

--- diffs-annotated/7/user-7-session-16.v
+++ diffs-annotated/7/user-7-session-16.v
@@ -221,17 +221,41 @@ tauto.
 Ltac
  solve_not_x_sub_r_y_full :=
   match goal with
-  | |- ~ |- ?tx << ?ty =>
-        remember tx as t eqn:Heq1 ; remember ty as t' eqn:Heq2 ; intros Hcontra; induction Hcontra; try (solve [ inversion Heq1 | inversion Heq2 ]);
-         subst; auto
-  end.
+  | |- ~ |- ?t1 << ?t2 =>
+        remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra;
+         try (solve [ inversion Heqx | inversion Heqy ]); subst
+  end; match goal with
+       | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto
+       end.
 Ltac
  solve_atom_sub_r_union__decidable IHt2_1 IHt2_2 :=
   destruct IHt2_1 as [IH1| IH1]; destruct IHt2_2 as [IH2| IH2];
    try (solve [ left; apply SR_UnionR1; assumption | left; apply SR_UnionR2; assumption ]); right; intros Hcontra;
    apply atom_sub_r_union__inv in Hcontra; tauto || constructor.
+Lemma nf_sub_r__decidable2 :
+  forall t : ty, InNF( t) -> (forall t' : ty, Decidable.decidable (|- t << t')) /\ (forall t' : ty, Decidable.decidable (|- t' << t)).
+Proof.
+(apply
+  (in_nf_mut
+     (fun (t : ty) (Hat : atom_type t) => (forall t' : ty, Decidable.decidable (|- t << t')) /\ (forall t' : ty, Decidable.decidable (|- t' << t)))
+     (fun (t : ty) (Hnf : in_nf t) => (forall t' : ty, Decidable.decidable (|- t << t')) /\ (forall t' : ty, Decidable.decidable (|- t' << t))))).
+-
+(intros c).
+(split; intros t'; induction t'; try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 ]);
+  try
+   match goal with
+   | |- Decidable.decidable (|- TCName ?c1 << TCName ?c2) =>
+         destruct (cname_eq__decidable c1 c2);
+          [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
+   end).
++
 right.
 (match goal with
- | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra
+ | |- ~ |- ?t1 << ?t2 =>
+       remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]);
+        subst
+ end).
+(match goal with
+ | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto
  end).
 (* Failed. *)
