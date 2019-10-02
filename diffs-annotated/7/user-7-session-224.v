2d8c8de4e7d469adc2c609fe9621e95df916b8bd
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:06 -0700

    user 7, session 224, part 35

--- diffs-annotated/7/user-7-session-224.v
+++ diffs-annotated/7/user-7-session-224.v
@@ -22,28 +22,6 @@ Lemma build_v_full :
     (forall (w' : nat) (t' : ty),
      |-[ w'] v' <$ t' -> (not_f_free_in_ty X' t' -> |-[ w'] v <$ t') /\ (f_free_in_ty X' t' -> exists w2, |-[ w2] v <$ [FX' := tx] t')).
 exists 0.
-(apply match_ty_union_2).
-(rewrite f_subst_not_b_free_in_ty; auto).
-}
-}
-*
-(destruct (beq_idP X' i0)).
-{
-subst.
-(split; intros Hi0).
+(split; intros HX').
 {
 (unfold not_f_free_in_ty, not_free in Hi0).
-(simpl in Hi0).
-exfalso.
-(apply Hi0).
-(apply IdSetFacts.singleton_2).
-reflexivity.
-}
-{
-exists 0.
-(rewrite f_subst_fvar_eq).
-assumption.
-}
-}
-{
-(* Failed. *)
