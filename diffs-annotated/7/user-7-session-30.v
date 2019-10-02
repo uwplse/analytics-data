b8b550f1b8ca3b908b4b3cc49051a76c2207ae80
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:20 -0700

    user 7, session 30, part 3

--- diffs-annotated/7/user-7-session-30.v
+++ diffs-annotated/7/user-7-session-30.v
@@ -25,4 +25,22 @@ Proof.
 exists v1,v2.
 (split; try reflexivity).
 (simpl in Hm).
+Lemma match_ty_union__inv : forall (v t1 t2 : ty) (k : nat), |-[ k] v <$ TUnion t1 t2 -> |-[ k] v <$ t1 \/ |-[ k] v <$ t2.
+Proof.
+(intros v t1 t2 k Hm).
+(destruct k; destruct v; assumption).
+Qed.
+Lemma match_ty_ref__inv :
+  forall (v t : ty) (k : nat),
+  |-[ S k] v <$ TRef t -> exists t' : ty, v = TRef t' /\ (inv_depth t <= k /\ inv_depth t' = inv_depth t) /\ ||-[ k][t']= [t].
+Proof.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; contradiction ])).
+clear IHv.
+(intros t k Hm).
+(simpl in Hm).
+(exists v; auto).
+Qed.
+Lemma match_ty__value_type : forall (v t : ty) (k : nat), |-[ k] v <$ t -> value_type v.
+Proof.
+(induction v; induction t).
 (* Failed. *)
