891eb30781b0a16fd4af9be7f63c7ba85f12845f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:54:37 -0700

    user 7, session 218, part 6

--- diffs-annotated-fixed/7/user-7-session-218.v
+++ diffs-annotated-fixed/7/user-7-session-218.v
@@ -326,6 +326,7 @@ Proof.
 reflexivity.
 Qed.
 Lemma f_subst_bvar_eq : forall (X : id) (s : ty) (Y : id), [FX := s] TBVar Y = TBVar Y.
+Lemma f_subst_bvar : forall (X : id) (s : ty) (Y : id), [FX := s] TBVar Y = TBVar Y.
 Proof.
 (intros).
 (simpl).
@@ -457,5 +458,16 @@ admit.
 subst.
 (rewrite b_subst_bvar_eq).
 (rewrite f_subst_bvar).
+(rewrite b_subst_bvar_eq).
+reflexivity.
++
+(rewrite b_subst_bvar_neq; try assumption).
+(rewrite f_subst_bvar).
+(rewrite b_subst_bvar_neq; try assumption).
+reflexivity.
+-
+(destruct (beq_idP X i)).
++
+subst.
 (* Auto-generated comment: Failed. *)
 
