bfbad153786647acd9016aad2c4eb1156784bc2d
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:02 -0700

    user 7, session 19, part 131

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -5,4 +5,9 @@ Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_pair; auto).
 Lemma match_ty_i_eq__inv_depth_eq :
   forall t t' : ty, (forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
+Proof.
+(induction t; induction t'; intros H; try reflexivity).
+(match goal with
+ | |- | ?t1 | = | ?t2 | => assert (Hv : value_type t1) by constructor
+ end).
 (* Failed. *)
