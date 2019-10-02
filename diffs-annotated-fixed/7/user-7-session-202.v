cfcaf2a13ce9fa7cc4d7e69ba04450be414afeab
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:54:07 -0700

    user 7, session 202, part 34

--- diffs-annotated-fixed/7/user-7-session-202.v
+++ diffs-annotated-fixed/7/user-7-session-202.v
@@ -132,11 +132,15 @@ Proof.
 Lemma b_subst_wf_ty : forall (X : id) (t : ty), not_b_free_in_ty X t -> forall s : ty, [BX := s] t = t.
 Proof.
 (intros X t).
-(induction t; intros HX s;
-  try (solve
-   [ reflexivity
-   | try destruct (not_b_free_in_ty_pair__inv _ _ HX) as [HX1 HX2]; try destruct (not_b_free_in_ty_union__inv _ _ HX) as [HX1 HX2]; simpl;
-      rewrite IHt1; try assumption; rewrite IHt2; try assumption; reflexivity ])).
-(destruct (not_b_free_in_ty_pair__inv _ _ _ HX) as [HX1 HX2]).
+-
+(destruct (beq_idP X i); try (subst; rewrite b_subst_exist_eq; reflexivity)).
+(rewrite b_subst_exist_neq; try assumption).
+(rewrite IHt).
+reflexivity.
+admit.
+-
+(destruct (beq_idP X i)).
++
+subst.
 (* Auto-generated comment: Failed. *)
 
