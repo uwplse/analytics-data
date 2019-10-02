0acd824328333d8a3e53b455c1d0ceb087142ae4
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:47 -0700

    user 7, session 98, part 33

--- diffs-annotated/7/user-7-session-98.v
+++ diffs-annotated/7/user-7-session-98.v
@@ -2,22 +2,6 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(intros v; induction v; intros X t Hm; assumption).
-Qed.
-Lemma match_ty_exist__inv : forall (v : ty) (X : id) (t : ty) (k : nat), |-[ S k] v <$ TExist X t -> exists tx : ty, |-[ k] v <$ [X := tx] t.
-Proof.
-(intros v; induction v; intros X t k Hm; assumption).
-Qed.
-Theorem match_ty__value_type_l : forall (k : nat) (v t : ty), |-[ k] v <$ t -> value_type v.
-Proof.
-(induction k; intros v t; generalize dependent v; induction t; intros v Hm;
-  try (solve
-   [ apply match_ty_cname__inv in Hm; subst; constructor
-   | apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; constructor; [ eapply IHt1 | eapply IHt2 ]; eauto
-   | apply match_ty_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto
-   | apply match_ty_ref__weak_inv in Hm; destruct Hm as [t' Heq]; subst; constructor
-   | destruct v; contradiction ])).
+auto.
 -
-(apply match_ty_exist__0_inv in Hm).
-tauto.
-(* Failed. *)
+(apply match_ty_exist__inv in Hm).
