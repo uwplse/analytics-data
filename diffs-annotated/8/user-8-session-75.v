0fb6e2242dd3aa977856999bdf06072c441df963
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:33 -0700

    user 8, session 75, part 3

--- diffs-annotated/8/user-8-session-75.v
+++ diffs-annotated/8/user-8-session-75.v
@@ -224,5 +224,8 @@ type_check.
 Qed.
 Timeout 1 Print PI.
 Timeout 1 Print Ltac PI.
-Definition _R'_ (m : nat) := _R_ (2 * Complex.PI / INR (2 ^ m)).
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqd1Bw0O"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Definition _R'_ (m : nat) := _R_ (2 * Reals.PI / INR (2 ^ m)).
 (* Failed. *)
