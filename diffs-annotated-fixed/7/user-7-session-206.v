aa45e1b302d49905b33ef770436a620c55dd12e1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:54:18 -0700

    user 7, session 206, part 31

--- diffs-annotated-fixed/7/user-7-session-206.v
+++ diffs-annotated-fixed/7/user-7-session-206.v
@@ -63,6 +63,6 @@ Search -Morphisms.Proper.
 Search -Morphisms.Proper.
 Check Hfact.
 Print Morphisms.Proper.
-specialize (Hfact _ _ _ Ht).
+specialize (Hfact _ _ _ (FBV t) Ht).
 (* Auto-generated comment: Failed. *)
 
