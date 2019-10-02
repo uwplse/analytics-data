bb48cb745178c048aa0761b6a661b0a37d38be3e
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:55 -0700

    user 7, session 108, part 11

--- diffs-annotated/7/user-7-session-108.v
+++ diffs-annotated/7/user-7-session-108.v
@@ -172,6 +172,62 @@ contradiction.
 (apply match_ty_exist__inv in Hm).
 (destruct Hm as [tx Hmx]).
 Abort.
+Lemma match_ty__ge_w : forall (w : nat) (t : ty) (k : nat) (v : ty), |-[ k, w] v <$ t -> forall w' : nat, w <= w' -> |-[ k, w'] v <$ t.
+Proof.
+(induction w; induction t; intros k v Hm w' Hle).
+-
+(apply match_ty_cname__inv in Hm).
+subst.
+(apply match_ty_cname).
+-
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_pair; [ eapply IHt1 | eapply IHt2 ]; eauto).
+-
+(apply match_ty_union__inv in Hm).
+(destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; eauto).
+-
+(destruct k).
++
+(apply match_ty_ref__weak_inv in Hm).
+(destruct Hm as [t' Heq]; subst).
+(destruct w'; constructor).
++
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [t' [Heq Href]]; subst).
+(destruct w'; assumption).
+-
+(apply match_ty_exist__0_inv in Hm; contradiction).
+-
+(apply match_ty_var__inv in Hm; subst).
+(apply match_ty_var).
+-
+(apply match_ty_ev__inv in Hm; subst).
+(apply match_ty_ev).
+-
+(apply match_ty_cname__inv in Hm).
+subst.
+(apply match_ty_cname).
+-
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_pair; [ eapply IHt1 | eapply IHt2 ]; eauto).
+-
+(apply match_ty_union__inv in Hm).
+(destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; eauto).
+-
+(destruct k).
++
+(apply match_ty_ref__weak_inv in Hm).
+(destruct Hm as [t' Heq]; subst).
+(destruct w'; constructor).
++
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [t' [Heq Href]]; subst).
+(destruct w'; assumption).
+-
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hmx]).
 (destruct w').
 (inversion Hle).
 (apply match_ty_exist).
@@ -196,4 +252,5 @@ exists 0,(TCName c).
 (destruct (IHt1 k) as [w1 [v1 Hm1]]).
 (destruct (IHt2 k) as [w2 [v2 Hm2]]).
 exists (Nat.max w1 w2),(TPair v1 v2).
+(apply match_ty_pair; apply match_ty__ge_w).
 (* Failed. *)
