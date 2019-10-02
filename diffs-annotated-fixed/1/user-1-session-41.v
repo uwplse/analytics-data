b46fdb2218d93fe50a706b585253d677001b27ea
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:17 -0700

    user 1, session 41, part 69

--- diffs-annotated-fixed/1/user-1-session-41.v
+++ diffs-annotated-fixed/1/user-1-session-41.v
@@ -621,6 +621,14 @@ all:
 all: congruence.
 -
 (inversion H1).
-specialize (H _ H5).
+specialize (H _ H4).
+specialize (H2 _ H4).
+(repeat
+  match goal with
+  | H:exists _, _ |- _ => destruct H
+  | H:_ \/ _ |- _ => inversion H; clear H
+  end).
+congruence.
+-
 (* Auto-generated comment: Succeeded. *)
 
