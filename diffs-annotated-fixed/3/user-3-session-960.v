17c015115b0b1aa774db1ae753b3e12f72130308
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:33 -0700

    user 3, session 960, part 90

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -78,6 +78,15 @@ list {x : nat | x < S (S base)} :=
       let digit := x mod S (S base) in
       exist _ digit _ :: nat_to_le base (x / S (S base))
   end.
+#[program]
+Fixpoint nat_to_le base (x : nat) {measure x lt} :
+list {x : nat | x < S (S base)} :=
+  match x with
+  | 0 => nil
+  | _ =>
+      let digit := x mod S (S base) in
+      exist _ digit _ :: nat_to_le base (x / S (S base))
+  end.
 Next Obligation of nat_to_le_func.
 Proof.
 (apply PeanoNat.Nat.mod_upper_bound; auto).
@@ -97,30 +106,30 @@ Proof.
 (apply (wf_projected lt projT2); auto).
 (apply lt_wf).
 Qed.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqtKFEKU"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqhwf9lL"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4gF8CO"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqNJr2rI"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Print projT1.
-Print sig.
 Fixpoint le_to_nat base (digits : list {x : nat | x < S (S base)}) : nat :=
   match digits with
   | nil => 0
   | digit :: digits' => proj1_sig digit * base + le_to_nat digits'
   end.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqA2kjC4"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0fxDJv"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqofvrZu"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqP3FOAC"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
+Instance aModel : GoModel.
+Proof.
 (* Auto-generated comment: Succeeded. *)
 
