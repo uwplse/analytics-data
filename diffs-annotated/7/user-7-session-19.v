2bfafe24011f47270ceac533764a2b33da42edd8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:02 -0700

    user 7, session 19, part 129

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,5 +3,5 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_pair; auto).
-(induction t; induction t'; intros H).
+(induction t; induction t'; intros H; try reflexivity).
 (* Failed. *)
