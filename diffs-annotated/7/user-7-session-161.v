e4238c8848bf48c71da81438095fe0323c385aaf
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:22 -0700

    user 7, session 161, part 15

--- diffs-annotated/7/user-7-session-161.v
+++ diffs-annotated/7/user-7-session-161.v
@@ -78,5 +78,8 @@ assumption.
 assumption.
 Abort.
 Lemma subst_nested : forall (X Y : id) (t tx ty : ty), [X := tx] ([Y := ty] t) = [Y := [X := tx] ty] ([X := tx] t).
+Lemma subst_nested : forall (X Y : id) (t tx ty : ty), [X := tx] ([Y := ty] t) = [Y := [X := tx] ty] ([X := tx] t).
 Proof.
+(intros X Y t).
+(induction t).
 (* Failed. *)
