1672ccfd9cca488b5c28dbb8909e8b4895b78cb4
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:24 -0700

    user 7, session 162, part 11

--- diffs-annotated/7/user-7-session-162.v
+++ diffs-annotated/7/user-7-session-162.v
@@ -45,16 +45,5 @@ Fixpoint FV (t : ty) : id_set :=
 Definition fresh (X : id) (fvs : id_set) := ~ IdSet.In X fvs.
 Axiom (gen_fresh__fresh : forall fvs : id_set, fresh (gen_fresh fvs) fvs).
 Definition get_fresh_in_ty (t : ty) := gen_fresh (FV t).
-Reserved Notation "'[' x ':=' s ']' t" (at level 30).
-Fixpoint subst (x : id) (s t : ty) :=
-  match t with
-  | TCName _ => t
-  | TPair t1 t2 => TPair ([x := s] t1) ([x := s] t2)
-  | TUnion t1 t2 => TUnion ([x := s] t1) ([x := s] t2)
-  | TExist y t' =>
-      let s' := if IdSet.mem y (FV s) then [y := TVar (gen_fresh (FV s))] s else s in TExist y (if beq_id x y then t' else [x := s'] t')
-  | TVar y => if beq_id x y then s else t
-  | TEV y => t
-  end
-where "'[' x ':=' s ']' t" := (subst x s t) : btjt_scope.
+Reserved Notation "'[' x '->' y ']' t" (at level 30).
 (* Failed. *)
