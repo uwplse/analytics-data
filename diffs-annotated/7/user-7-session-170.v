88682fbc987206ed2512e32ba23f6f049e2d3d44
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:26 -0700

    user 7, session 170, part 0

---
+++ diffs-annotated/7/user-7-session-170.v
@@ -0,0 +1,72 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0281a.BaseDefs.
+Require Import BetaJulia.Sub0281a.BaseProps.
+Require Import BetaJulia.Sub0281a.BaseMatchProps.
+Require Import BetaJulia.Sub0281a.BaseSemSubProps.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Lemma subst_nested :
+  forall (X Y : id) (tX tY : ty), X <> Y -> fresh_in_ty Y tX -> forall t : ty, [X := tX] ([Y := tY] t) = [Y := [X := tX] tY] ([X := tX] t).
+Proof.
+(intros X Y tX tY Hneq HYfresh t).
+(induction t; try reflexivity).
+-
+(repeat rewrite subst_pair).
+(rewrite IHt1, IHt2).
+reflexivity.
+-
+(repeat rewrite subst_union).
+(rewrite IHt1, IHt2).
+reflexivity.
+-
+(destruct (beq_idP Y i) as [HY| HY]).
++
+subst.
+(destruct (beq_idP X i) as [HX| HX]).
+*
+subst.
+contradiction.
+*
+(rewrite subst_exist_eq).
+Search -IdSet.In.
+Abort.
+Lemma build_v_full :
+  forall (X X' : id) (w : nat) (t v : ty) (tx : ty),
+  |-[ w] v <$ [X := tx] t ->
+  exists v' : ty, |-[ w] v' <$ [X := TVar X'] t /\ (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> |-[ w'] v <$ [X' := tx] t').
+Proof.
+(intros X X').
+(induction w; induction t; intros v tx Hm).
+-
+exists v.
+split.
+assumption.
+(apply match_ty_cname__inv in Hm; subst).
+(induction w'; induction t'; intros Hm; try assumption || contradiction).
++
+(rewrite subst_union).
+(apply match_ty_union__inv in Hm).
+(destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
++
+(rewrite subst_union).
+(apply match_ty_union__inv in Hm).
+(destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
++
+(destruct (beq_idP X' i) as [Hbeq| Hbeq]).
+*
+subst.
+(rewrite subst_exist_eq).
+assumption.
+*
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [ti Hm]).
+(rewrite (subst_exist_neq _ _ _ _ Hbeq)).
+exists ([X' := tx] ti).
+(* Auto-generated comment: Failed. *)
