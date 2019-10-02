065d14cd4b6a1e4435507df1053be0818f629041
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:58 -0700

    user 7, session 19, part 58

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,6 +2,6 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(destruct v).
-contradiction.
+(destruct v; try contradiction).
+(inversion Hle).
 (* Failed. *)
