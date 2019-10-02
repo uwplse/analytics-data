7abfabc0219dc563102e192ff450bfa394bc5a6a
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:24 -0700

    user 7, session 162, part 13

--- diffs-annotated/7/user-7-session-162.v
+++ diffs-annotated/7/user-7-session-162.v
@@ -49,8 +49,8 @@ Reserved Notation "'[' x '@' y ']' t" (at level 30).
 Fixpoint rename (x y : id) (t : ty) :=
   match t with
   | TCName _ => t
-  | TPair t1 t2 => TPair ([x @ s] t1) ([x @ s] t2)
-  | TUnion t1 t2 => TUnion ([x @ s] t1) ([x @ s] t2)
+  | TPair t1 t2 => TPair ([x @ y] t1) ([x @ y] t2)
+  | TUnion t1 t2 => TUnion ([x @ y] t1) ([x @ y] t2)
   | TExist z t' => TExist z (if beq_id x z then t' else [x @ y] t')
   | TVar z => if beq_id x z then s else t
   | TEV z => t
