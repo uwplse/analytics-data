df4015b3e5f46925ad2c7b21d2db6fff458f3bf5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:54:28 -0700

    user 7, session 214, part 12

--- diffs-annotated-fixed/7/user-7-session-214.v
+++ diffs-annotated-fixed/7/user-7-session-214.v
@@ -260,6 +260,7 @@ Proof.
 -
 (rewrite b_subst_pair).
 (unfold f_free_in_ty, free).
-(destruct (f_free_in_ty_pair__inv _ _ _ HX) as [HXt| HXt]).
+(apply f_free_in_ty_pair).
+(apply f_free_in_ty_pair__inv).
 (* Auto-generated comment: Failed. *)
 
