74ea93a0c052d68b070409104bb539d2ef67243f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:15 -0700

    user 7, session 147, part 2

--- diffs-annotated/7/user-7-session-147.v
+++ diffs-annotated/7/user-7-session-147.v
@@ -160,4 +160,45 @@ exists w1.
 (apply match_ty_exist__inv in Hm).
 (destruct Hm as [tx Hm]).
 (simpl in Hm).
-(* Failed. *)
+(rewrite subs_fresh_in_ty in Hm; try assumption).
+(eapply match_ty__ge_w).
+eassumption.
+(repeat constructor).
+Qed.
+Lemma sem_sub_k_exist_fresh_r : forall (k : nat) (X : id) (t : ty), fresh_in_ty X t -> ||-[ k][t]<= [TExist X t].
+Proof.
+(intros k X t Hfresh).
+(intros w1).
+exists (S w1).
+(intros v Hm).
+(apply match_ty_exist).
+exists (TEV X).
+(rewrite subs_fresh_in_ty; assumption).
+Qed.
+Lemma sem_sub_exist_fresh_l : forall (X : id) (t : ty), fresh_in_ty X t -> ||- [TExist X t]<= [t].
+Proof.
+(intros X t Hfresh k).
+(apply sem_sub_k_exist_fresh_l).
+assumption.
+Qed.
+Lemma sem_sub_k_exist_pair : forall (k : nat) (X : id) (t1 t2 : ty), ||-[ k][TExist X (TPair t1 t2)]<= [TPair (TExist X t1) (TExist X t2)].
+Proof.
+(intros k X t1 t2 w1).
+exists w1.
+(intros v Hm).
+(destruct w1).
+-
+(apply match_ty_exist__0_inv in Hm; contradiction).
+-
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hm]).
+(simpl in Hm).
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_pair; apply match_ty_exist; exists tx; assumption).
+Qed.
+Lemma sem_sub_exist_pair : forall (X : id) (t1 t2 : ty), ||- [TExist X (TPair t1 t2)]<= [TPair (TExist X t1) (TExist X t2)].
+Proof.
+(intros X t1 t2 k).
+(apply sem_sub_k_exist_pair).
+Qed.
