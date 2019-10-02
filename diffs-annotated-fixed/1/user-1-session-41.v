3192ce7650473396e1d8e50d13ef21bd5b776194
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:21 -0700

    user 1, session 41, part 128

--- diffs-annotated-fixed/1/user-1-session-41.v
+++ diffs-annotated-fixed/1/user-1-session-41.v
@@ -374,7 +374,13 @@ Function
        eq (GRow tl1, GRow tl2)
    | (GRow (Some None :: tl1), GRow (Some None :: tl2)) =>
        eq (GRow tl1, GRow tl2)
+   | (GRow (None :: tl1), GRow []) => eq (GRow tl1, GRow [])
+   | (GRow [], GRow (None :: tl1)) => eq (GRow [], GRow tl1)
    | _ => False
    end.
+all: (intros; subst; simpl; eauto with math).
+all: (try destruct hd1; try destruct hd2; simpl; eauto with math).
+Defined.
+Theorem eq_refl : forall A, reflexive _ (eq (A:=A)).
 (* Auto-generated comment: Succeeded. *)
 
