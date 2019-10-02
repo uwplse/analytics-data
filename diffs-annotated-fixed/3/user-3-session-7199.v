e039ae99303fd7af04e875c166b0f59e2e1902b8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:16:17 -0700

    user 3, session 7199, part 103

--- diffs-annotated-fixed/3/user-3-session-7199.v
+++ diffs-annotated-fixed/3/user-3-session-7199.v
@@ -303,10 +303,10 @@ Theorem get_upto_ok a :
     (get_upto a) recover abstr.
 Opaque firstn.
 Lemma firstn_one_more :
-  forall (a : nat) (state : list block),
-  S a <= length state ->
-  firstn a state ++ [nth a state block0] = firstn (S a) state.
+  forall (a : nat) (d : list block),
+  S a <= length d -> firstn a d ++ [nth a d block0] = firstn (S a) d.
 Proof.
-(intros a state H).
+(intros).
+Print firstn.
 (* Auto-generated comment: Succeeded. *)
 
