b1544497fb1b9a16914380ab6602e2bb032d6d34
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:45:25 -0700

    user 5, session 36, part 47

--- diffs-annotated-fixed/5/user-5-session-36.v
+++ diffs-annotated-fixed/5/user-5-session-36.v
@@ -268,6 +268,6 @@ clear H.
 (rewrite <- evalEqTrue in H0).
 (rewrite evalVar in H).
 (rewrite extendEnv_eq in H).
-(assert (x0 = 3%Z)).
+(assert (forall z, 6 = z * 2 -> z = 3) by admit).
 (* Auto-generated comment: Succeeded. *)
 
