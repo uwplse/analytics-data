bacca6538150995da2e53c077f2c26a79730f644
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:40 -0700

    user 7, session 9, part 0

---
+++ diffs-annotated/7/user-7-session-9.v
@@ -0,0 +1,29 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0250b.BaseDefs.
+Require Import BetaJulia.Sub0250b.BaseProps.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Close Scope btj_scope.
+Open Scope btjnf_scope.
+Open Scope btjr_scope.
+Lemma atom_sub_r_union__inv : forall t t1' t2' : ty, |- t << TUnion t1' t2' -> atom_type t -> |- t << t1' \/ |- t << t2'.
+Proof.
+(intros t t1' t2' Hsub).
+(remember (TUnion t1' t2') as t' eqn:Heq ).
+(induction Hsub; intros Hat; try (solve [ inversion Heq | inversion Hat ])).
+-
+(inversion Heq; subst).
+(left; assumption).
+-
+(inversion Heq; subst).
+(right; assumption).
+-
+(inversion Heq; subst).
+(* Auto-generated comment: Failed. *)
