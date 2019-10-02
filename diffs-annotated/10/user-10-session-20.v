5772b6de513ffbaa24937bbc88035ac839dc2ee5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:06:09 -0700

    user 10, session 20, part 2

--- diffs-annotated/10/user-10-session-20.v
+++ diffs-annotated/10/user-10-session-20.v
@@ -19,6 +19,4 @@ Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqAWfqgM" Print Ltac
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Anomaly ""Assert_failure printing/ppconstr.ml:399:14"." Please report at http://coq.inria.fr/bugs/.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqTOxbsM" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Eval compute in hd (fib2 0 1).
+Eval compute in Str_nth 10 (fib2 0 1).
