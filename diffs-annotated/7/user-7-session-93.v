8c89ce8e9ab9869b4452c8d72cd3cf4db1cc225e
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:43 -0700

    user 7, session 93, part 12

--- diffs-annotated/7/user-7-session-93.v
+++ diffs-annotated/7/user-7-session-93.v
@@ -13,10 +13,47 @@ Inductive cname : Type :=
   | NInt : _
   | NFlt : _
   | NStr : _.
+Inductive ty : Type :=
+  | TCName : cname -> ty
+  | TPair : ty -> ty -> ty
+  | TUnion : ty -> ty -> ty
+  | TRef : ty -> ty
+  | TExist : id -> ty -> ty
+  | TVar : id -> ty.
+Definition tint := TCName NInt.
+Definition tflt := TCName NFlt.
+Definition tstr := TCName NStr.
+Definition tIntInt := TPair tint tint.
+Definition vX := Id 1.
 Definition vY := Id 2.
 Definition vZ := Id 3.
 Definition tX := TVar vX.
 Definition tY := TVar vY.
 Definition teXX := TExist vX tX.
 Definition tyXRefX := TExist vX (TRef tX).
+Declare Scope btjt_scope.
+Delimit Scope btjt_scope with btjt.
+Open Scope btjt.
+Reserved Notation "'|' t '|'" (at level 20).
+Fixpoint inv_depth (t : ty) :=
+  match t with
+  | TCName _ => 0
+  | TPair t1 t2 => Nat.max (| t1 |) (| t2 |)
+  | TUnion t1 t2 => Nat.max (| t1 |) (| t2 |)
+  | TRef t' => 1 + | t' |
+  | TExist _ t' => | t' |
+  | TVar _ => 0
+  end
+where "'|' t '|'" := (inv_depth t) : btjt_scope.
+Reserved Notation "'[' x ':=' s ']' t" (at level 30).
+Fixpoint subst (x : id) (s t : ty) :=
+  match t with
+  | TCName _ => t
+  | TPair t1 t2 => TPair (subst x s t1) (subst x s t2)
+  | TUnion t1 t2 => TUnion (subst x s t1) (subst x s t2)
+  | TRef t' => TRef (subst x s t')
+  | TExist y t' => TExist y (if beq_id x y then t' else subst x s t')
+  | TVar y => if beq_id x y then s else t
+  end
+where "'[' x ':=' s ']' t" := (subst x s t) : btjt_scope.
 (* Failed. *)
