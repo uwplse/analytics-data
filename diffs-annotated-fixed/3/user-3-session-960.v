5fb9d89c198425b35d6e075daf143d25eb5dd0a0
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:34 -0700

    user 3, session 960, part 100

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -89,29 +89,49 @@ list {x : nat | x < S (S base)} :=
   end.
 Next Obligation of nat_to_le_func.
 Proof.
+Next Obligation of nat_to_le_func.
+Proof.
 (apply PeanoNat.Nat.mod_upper_bound; auto).
-Qed.
+Defined.
 Next Obligation of nat_to_le_func.
 Proof.
 subst digit.
 (apply PeanoNat.Nat.div_lt; auto; try lia).
-Qed.
+Defined.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqR5t07S"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq1sUxE7"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 Next Obligation of nat_to_le_func.
 Proof.
 lia.
-Qed.
+Defined.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqLYnJFy"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqrXfjTc"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 Next Obligation of nat_to_le_func.
 Proof.
 (unfold MR).
 (apply (wf_projected lt projT2); auto).
 (apply lt_wf).
-Qed.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqhwf9lL"
+Defined.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqVkQJro"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqNJr2rI"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqJwp8ky"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
@@ -120,21 +140,12 @@ Fixpoint le_to_nat base (digits : list {x : nat | x < S (S base)}) : nat :=
   | nil => 0
   | digit :: digits' => proj1_sig digit * base + le_to_nat digits'
   end.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0fxDJv"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqP3FOAC"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Print FixedLengthEncoder.
 Theorem nat_le_inverse base : forall n, le_to_nat (nat_to_le base n) = n.
 Proof.
 (intros).
 (induction n as [n IHn] using lt_wf_ind).
 (destruct n; simpl).
 Search -"nat_to_le".
+Instance aModel : GoModel.
 (* Auto-generated comment: Succeeded. *)
 
