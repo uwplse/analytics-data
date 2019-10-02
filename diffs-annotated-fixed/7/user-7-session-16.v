c52a191a9a5fc0bddfa5fe6091f5d90100efc1cc
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:46:18 -0700

    user 7, session 16, part 11

--- diffs-annotated-fixed/7/user-7-session-16.v
+++ diffs-annotated-fixed/7/user-7-session-16.v
@@ -110,6 +110,6 @@ Proof.
 (intros c1 c2 Hsub).
 (remember (TCName c1) as t1 eqn:Heq1 ).
 (remember (TCName c2) as t2 eqn:Heq2 ).
-(induction Hsub; try (solve [ inversion Heq1; inversion Heq2 ])).
+(induction Hsub; try inversion Heq1; inversion Heq2; subst).
 (* Auto-generated comment: Failed. *)
 
