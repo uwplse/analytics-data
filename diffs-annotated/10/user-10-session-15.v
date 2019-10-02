be372445215197d149be7614970eb35c8df88bc1
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:06:08 -0700

    user 10, session 15, part 0

---
+++ diffs-annotated/10/user-10-session-15.v
@@ -0,0 +1,18 @@
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqVccw9q"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+From ReductionEffect Require Import PrintingEffect.
+Eval compute in (fun f x => f (f (f x))) (fun x => S (print_id x)) 0.
+Eval cbn in (fun f x => f (f (f x))) print_id 0.
+Eval hnf in (fun f x => f (f (f x))) print_id 0.
+Eval simpl in (fun f x => f (f (f x))) (fun x => print_id (1 + x) + 1) 0.
+Anomaly ""Assert_failure printing/ppconstr.ml:399:14"." Please report at http://coq.inria.fr/bugs/.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqcuNkuE" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+From Coq Require Import Lists NArith.
+(* Auto-generated comment: Failed. *)
