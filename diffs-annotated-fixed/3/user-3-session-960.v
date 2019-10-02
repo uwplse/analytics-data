5f3881d357f54ab0021d75afc85f8071a6de9bb4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:37 -0700

    user 3, session 960, part 163

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -147,6 +147,6 @@ auto.
 (assert (base_m2 = S (S base_m2) - 2) by lia).
 (generalize dependent S (S base_m2); intros base **; subst).
 (assert (0 < S n) by lia).
-(generalize dependent S n; clear n; intros m).
+(generalize dependent S n; clear n; intros n).
 (* Auto-generated comment: Succeeded. *)
 
