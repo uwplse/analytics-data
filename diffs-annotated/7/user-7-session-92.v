e0c291e576c89e34ab0ca35adc7b11e762ecc105
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:42 -0700

    user 7, session 92, part 0

---
+++ diffs-annotated/7/user-7-session-92.v
@@ -0,0 +1,18 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0250b.BaseDefs.
+Require Import BetaJulia.Sub0250b.BaseProps.
+Require Import BetaJulia.Sub0250b.DeclSubProps.
+Require Import BetaJulia.Sub0250b.RedSubProps.
+Require Import BetaJulia.BasicTactics.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Close Scope btjd_scope.
+Open Scope btjr_scope.
+Theorem sub_r__sound : forall t1 t2 : ty, |- t1 << t2 -> (|- t1 << t2)%btj.
