94491ae1c123ca2be2b56f9d4ff1a3d681e825b5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:53 -0700

    user 7, session 105, part 2

--- diffs-annotated/7/user-7-session-105.v
+++ diffs-annotated/7/user-7-session-105.v
@@ -35,3 +35,13 @@ Proof.
 (destruct k, v; assumption).
 Qed.
 Lemma match_ty_var : forall (X : cname) (k w : nat), |-[ k, w] TEV X <$ TVar X.
+Lemma match_ty_var : forall (X : id) (k w : nat), |-[ k, w] TEV X <$ TVar X.
+Proof.
+(intros X k w).
+(destruct k, w; reflexivity).
+Qed.
+Lemma match_ty_ev : forall (X : id) (k w : nat), |-[ k, w] TEV X <$ TEV X.
+Proof.
+(intros X k w).
+(destruct k, w; reflexivity).
+Qed.
