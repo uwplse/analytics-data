71a0584cf13d32f6bb5cc9f08d138acaa32667e5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:29 -0700

    user 8, session 72, part 17

--- diffs-annotated/8/user-8-session-72.v
+++ diffs-annotated/8/user-8-session-72.v
@@ -1234,3 +1234,4 @@ constructor.
 (intros \207\129 TB).
 (repeat rewrite inSeq_correct; try apply inSeq_WT; try apply unitary_at1_WT;
   try apply source_symmetric_WT; trivial).
+(unfold compose_super).
