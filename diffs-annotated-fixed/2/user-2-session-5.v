fdb57d13e2ecdaec61d6d2b4284e3bc0655690b7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:29 -0700

    user 2, session 5, part 79

--- diffs-annotated-fixed/2/user-2-session-5.v
+++ diffs-annotated-fixed/2/user-2-session-5.v
@@ -157,10 +157,5 @@ Fixpoint trace (n : nat) : term -> list term :=
   end.
 Redirect "/tmp/coqMDtlae" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
-Example t1 := {"if" [{"cons?" <<[] []> <[] []>>} {"fst" <<[] []> []>} []]}.
-Redirect "/tmp/coq5O2xER" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Eval vm_compute in trace 10 t1.
-Eval vm_compute in multistep 10 t1.
 (* Auto-generated comment: Succeeded. *)
 
