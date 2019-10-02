abdf8d7af9eeed78732512dd7b2029b4796556fb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:47 -0700

    user 3, session 73, part 4

--- diffs-annotated-fixed/3/user-3-session-73.v
+++ diffs-annotated-fixed/3/user-3-session-73.v
@@ -13,58 +13,6 @@ Require Import Layer.
 Section Abstraction.
 Context (AState CState : Type).
 Context (absr : relation AState CState unit).
-Definition refine_spec T R (spec : Specification T R AState) :
-  AState -> Specification T R CState :=
-  fun s cs =>
-  {|
-  pre := absr s (Val cs tt) /\ (spec s).(pre);
-  post := fun cs' r =>
-          exists s', absr s' (Val cs' tt) /\ (spec s).(post) s' r;
-  alternate := fun cs' r =>
-               exists s', absr s' (Val cs' tt) /\ (spec s).(alternate) s' r |}.
-Context A_Op C_Op `(a_sem : Dynamics A_Op AState)
- `(c_sem : Dynamics C_Op CState).
-Import RelationNotations.
-Lemma proc_rspec_refine_exec T R (p : proc C_Op T) 
-  (rec : proc C_Op R) spec :
-  (forall t, proc_rspec c_sem p rec (refine_spec spec t)) ->
-  (forall sA sC, absr sA (Val sC tt) -> (spec sA).(pre)) ->
-  _ <- absr; exec c_sem p ---> v <- spec_exec spec; _ <- absr; pure v.
-Proof.
-(intros Hprspec Habstr_pre).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqKwOLDP"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
-Admitted.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq44j751"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqrIRuKB"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Lemma proc_rspec_refine_rec T R (p : proc C_Op T) 
-  (rec : proc C_Op R) spec :
-  (forall t, proc_rspec c_sem p rec (refine_spec spec t)) ->
-  (forall sA sC, absr sA (Val sC tt) -> (spec sA).(pre)) ->
-  _ <- absr; rexec c_sem p rec ---> v <- spec_aexec spec; _ <- absr; pure v.
-Proof.
-(intros Hprspec Habstr_pre).
-Admitted.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqd7tWFz"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqgXtIXj"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+Context (absr_ok : NonError absr).
 (* Auto-generated comment: Succeeded. *)
 
