07486a1636850c07d882728d7cd0d9cc2b49914e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:13:51 -0700

    user 3, session 6414, part 36

--- diffs-annotated-fixed/3/user-3-session-6414.v
+++ diffs-annotated-fixed/3/user-3-session-6414.v
@@ -60,14 +60,58 @@ Definition init : proc InitResult :=
   (if lt_dec size 1
    then Ret InitFailed
    else len0 <- addr_to_block 0; _ <- d.write len_addr len0; Ret Initialized).
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqsUb5xv"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqBorB5Y"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqK4sBjD"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0sXoFi"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-(* Auto-generated comment: Succeeded. *)
+Definition get_len : proc addr := b <- d.read len_addr; Ret (block_to_addr b).
+Definition get_at (a : addr) : proc block := d.read (log_addr a).
+Fixpoint get_upto (a : addr) : proc (list block) :=
+  match a with
+  | 0 => Ret []
+  | S a => b <- get_at a; bs <- get_upto a; Ret (bs ++ [b])
+  end.
+Definition get : proc (list block) := len <- get_len; get_upto len.
+Fixpoint append_at (a : addr) (bs : list block) : 
+proc unit :=
+  match bs with
+  | [] => Ret tt
+  | b :: bs => _ <- d.write (log_addr a) b; append_at (S a) bs
+  end.
+Definition append (bs : list block) : proc bool :=
+  size <- d.size;
+  len <- get_len;
+  (if le_dec (1 + len + length bs) size
+   then _ <- append_at len bs; Ret true
+   else Ret false).
+Definition reset : proc unit :=
+  len0 <- addr_to_block 0; d.write len_addr len0.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqQyZbpq"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqsBpUMY"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Definition recover : proc unit := Ret tt.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoMkHI8"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqdgF84B"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Definition log_abstraction (d : disk) (log : list block) : Prop :=
+  diskGet d 0 =?= length log /\
+  (forall a, a < length log -> diskGet d (log_addr a) =?= nth log a).
+(* Auto-generated comment: Failed. *)
 
