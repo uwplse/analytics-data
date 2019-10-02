8bf3ff593889fb2b6c87b63919cb1493ba322c77
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:38 -0700

    user 7, session 83, part 23

--- diffs-annotated/7/user-7-session-83.v
+++ diffs-annotated/7/user-7-session-83.v
@@ -5,7 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0250a.BaseDefs.
-clear IHt2.
-(simpl in Hdep).
-(pose proof (le_S_n _ _ Hdep) as Hdt').
+(simpl in Hdt).
 (* Failed. *)
