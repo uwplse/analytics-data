439f9dcf56516db417c17c8fcdf5a0e03ab253b7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:48:17 -0700

    user 7, session 23, part 3

--- diffs-annotated-fixed/7/user-7-session-23.v
+++ diffs-annotated-fixed/7/user-7-session-23.v
@@ -82,6 +82,8 @@ Proof.
 Qed.
 Theorem sem_sub_i__sem_sub_deq : forall t1 t2 : ty, (||- [t1]<= [t2])%btjmi -> ||- [t1]<= [t2].
 Proof.
-(intros Hsemi).
+(intros t1 t2 Hsemi).
+(apply sub_d__semantic_sound).
+(apply sub_d__sem_sub_i).
 (* Auto-generated comment: Failed. *)
 
