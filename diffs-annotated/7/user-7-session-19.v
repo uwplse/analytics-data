5c17552970f2db280e6de199d01a5ebfb3310a67
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:11 -0700

    user 7, session 19, part 314

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -4,13 +4,5 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_union__inv in Hm2).
 (destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
-(induction k; intros v Hv; induction Hv; intros ta tb Hsem; unfold sem_sub_k_i in Hsem;
-  try
-   match goal with
-   | Hsem:forall v', value_type v' -> |-[ ?k] v' <$ ?v -> _
-     |- _ =>
-         assert (Hvv : value_type v) by (constructor; assumption); assert (Hmv : |-[ k] v <$ v) by (apply match_ty_i__reflexive; assumption);
-          specialize (Hsem _ Hvv Hmv); apply match_ty_i_union__inv in Hsem; destruct Hsem; [ left | right ]; unfold sem_sub_k_i; 
-          intros v' Hv' Hm'; apply match_ty_i__transitive_on_value_type with v; assumption
-   end).
+(induction k; intros v Hv; induction Hv; intros ta tb Hsem; unfold sem_sub_k_i in Hsem).
 (* Failed. *)
