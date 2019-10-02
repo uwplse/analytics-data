a0a71494c3d05fd2eabcc8085108a582a9bd66b1
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:13 -0700

    user 7, session 19, part 342

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,7 +2,8 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(destruct Hsemu as [Hsemu| Hsemu]; [ apply Nat.le_trans with (| t'1 |) | apply Nat.le_trans with (| t'2 |) ];
-  tauto || apply Max.le_max_l || apply Max.le_max_r).
-admit.
+Lemma sem_sub_k_i_pair__inv :
+  forall t1 t2 t1' t2' : ty,
+  forall k : nat, | TPair t1 t2 | <= k -> ||-[ k][TPair t1 t2]<= [TPair t1' t2'] -> ||-[ k][t1]<= [t1'] /\ ||-[ k][t2]<= [t2'].
+Proof.
 (* Failed. *)
