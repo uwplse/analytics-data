3c82ff8c1e4df8171b828f21c0eea94875a1e3c3
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:38 -0700

    user 7, session 187, part 2

--- diffs-annotated/7/user-7-session-187.v
+++ diffs-annotated/7/user-7-session-187.v
@@ -44,5 +44,7 @@ Proof.
 (apply (IdSetFacts.remove_2 Hneq') in Hcontra).
 contradiction.
 Qed.
-Lemma free_in_ty_union__inv : forall (X : id) (t1 t2 : ty), free_in_ty X (TUnion t1 t2) -> free_in_ty t1 \/ free_in_ty t2.
+Lemma free_in_ty_union__inv : forall (X : id) (t1 t2 : ty), free_in_ty X (TUnion t1 t2) -> free_in_ty X t1 \/ free_in_ty X t2.
+(intros X t1 t2 HX).
+(unfold fresh_in_ty, fresh in *).
 (* Failed. *)
