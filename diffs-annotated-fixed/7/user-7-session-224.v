da28af57da10b6287d7e35c45a42b84ec2a23127
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:54:51 -0700

    user 7, session 224, part 28

--- diffs-annotated-fixed/7/user-7-session-224.v
+++ diffs-annotated-fixed/7/user-7-session-224.v
@@ -34,5 +34,12 @@ admit.
 +
 subst.
 (rewrite b_subst_exist_eq in Hm).
+(apply match_ty_exist__0_inv in Hm).
+contradiction.
++
+subst.
+(rewrite b_subst_exist_neq in Hm; try assumption).
+(apply match_ty_exist__0_inv in Hm).
+contradiction.
 (* Auto-generated comment: Failed. *)
 
