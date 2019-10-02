0f869309e88946839bb8159b13ffc9f909365547
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:52:45 -0700

    user 7, session 165, part 9

--- diffs-annotated-fixed/7/user-7-session-165.v
+++ diffs-annotated-fixed/7/user-7-session-165.v
@@ -87,6 +87,8 @@ reflexivity.
 reflexivity.
 -
 (destruct (beq_idP X i); try reflexivity).
-(rewrite IHt).
+Search -IdSet.mem.
+(destruct (IdSet.mem i (IdSet.singleton X))).
++
 (* Auto-generated comment: Failed. *)
 
