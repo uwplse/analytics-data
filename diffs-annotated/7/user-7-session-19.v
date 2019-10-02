f7f97b9356335d1082cd9399f012b66c67f198d3
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:58 -0700

    user 7, session 19, part 64

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,24 +3,6 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (simpl).
-Lemma match_ty_i__inv_depth_stable :
-  forall (k k' : nat) (t : ty),
-  inv_depth t <= k -> inv_depth t <= k' -> forall v : ty, inv_depth v <= k -> inv_depth v <= k' -> |-[ k] v <$ t <-> |-[ k'] v <$ t.
-Proof.
-(induction k; induction k').
--
-tauto.
--
-admit.
--
-admit.
--
-(induction t).
-admit.
-admit.
-admit.
-+
-clear IHk'.
-(intros Htk Htk' v Hvk Hvk').
-(simpl in Htk).
+(simpl in Htk, Htk').
+(apply le_S_n in Htk).
 (* Failed. *)
