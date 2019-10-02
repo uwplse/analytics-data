337c107894496fc1688c277de3a81bcad16ad79d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:48:57 -0700

    user 7, session 58, part 6

--- diffs-annotated-fixed/7/user-7-session-58.v
+++ diffs-annotated-fixed/7/user-7-session-58.v
@@ -16,6 +16,9 @@ Lemma value_sem_sub_k_i_union__inv :
   forall v : ty, value_type v -> forall (k : nat) (ta tb : ty), ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
 Proof.
 (intros v Hv k ta tb Hsem).
-(pose proof (match_ty_value_type_r v Hv k)).
+(destruct (match_ty_value_type_r v Hv k) as [Hcontra| Hm]).
+-
+(left; intros v' Hm').
+contradiction.
 (* Auto-generated comment: Failed. *)
 
