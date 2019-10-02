c8380a8a4acaac99e1977b3593f2d3e6133d34b5
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:38 -0700

    user 7, session 83, part 26

--- diffs-annotated/7/user-7-session-83.v
+++ diffs-annotated/7/user-7-session-83.v
@@ -6,11 +6,5 @@ Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0250a.BaseDefs.
 (apply match_ty_i_ref__inv in Hsem).
-(destruct Hsem as [t' [Heqt' Href]]).
-(inversion Heqt'; subst).
-clear Heqt'.
-constructor.
-*
-(apply IHk; try assumption).
-(apply sem_eq_k_i__sem_sub_k in Href).
+(apply sem_eq_k_i__sem_sub_k_i in Href).
 (* Failed. *)
