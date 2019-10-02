c0f12401d637bad6ff964ffd9370f27af69ca924
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:06:15 -0700

    user 11, session 14, part 13

--- diffs-annotated/11/user-11-session-14.v
+++ diffs-annotated/11/user-11-session-14.v
@@ -4,26 +4,3 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-From Coq Require Import ExtrOcamlIntConv.
-From ExtLib Require Import Applicative StateMonad Monad.
-From ITree Require Import Exception Nondeterminism ITree.
-From SimpleIO Require Import IO_Random SimpleIO.
-From DeepWeb Require Import CryptoLib KvsLib.
-Redirect "/tmp/coq16819_Ty" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Redirect "/tmp/coq16819xdB" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Redirect "/tmp/coq16819-nH" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Redirect "/tmp/coq16819LyN" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Redirect "/tmp/coq16819Y8T" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Import ApplicativeNotation FunctorNotation ListNotations MonadNotation SumNotations.
-Open Scope sum_scope.
-Open Scope monad_scope.
