c255332e20602ef8ae9e9e1b1c0593d6d4efe959
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:16:19 -0700

    user 3, session 7199, part 128

--- diffs-annotated-fixed/3/user-3-session-7199.v
+++ diffs-annotated-fixed/3/user-3-session-7199.v
@@ -358,6 +358,7 @@ Proof.
 (unfold get; intros).
 step_proc.
 (eapply proc_spec_weaken; eauto).
-(unfold spec_impl; simpl; intros).
+(unfold spec_impl; simpl; intuition).
+(descend; intuition eauto).
 (* Auto-generated comment: Succeeded. *)
 
