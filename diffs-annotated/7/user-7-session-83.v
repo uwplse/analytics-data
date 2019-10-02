0a41720a2e9f9ce4da573887d6e4c6dff77a04e8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:38 -0700

    user 7, session 83, part 21

--- diffs-annotated/7/user-7-session-83.v
+++ diffs-annotated/7/user-7-session-83.v
@@ -6,4 +6,9 @@ Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0250a.BaseDefs.
 +
+(apply value_sem_sub_k_i_union__inv in Hsem; try assumption).
+(destruct Hsem as [Hsem| Hsem]; [ apply union_right_1 | apply union_right_2 ]; auto).
++
+(simpl in Hdep).
+(pose proof (le_S_n _ _ Hdep) as Hdep').
 (* Failed. *)
