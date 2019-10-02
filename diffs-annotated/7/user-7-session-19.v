ba25701b85c947fc91769703bf964595dfdd9fe7
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:01 -0700

    user 7, session 19, part 123

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,18 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-clear IHt'.
-(simpl).
-(apply f_equal).
-(apply IHt).
-(intros k v Hv).
-(assert (Hmt : |-[ S k] TRef t <$ TRef t) by (simpl; tauto)).
-specialize (H (S k) (TRef t)).
-(destruct H as [H _]).
-constructor.
-specialize (H Hmt).
-(apply match_ty_i_ref__inv in H).
-(destruct H as [tx [Heq Href]]).
-(inversion Heq; subst).
-auto.
+tauto.
 (* Failed. *)
