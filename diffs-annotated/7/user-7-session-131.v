2dc98cd22b0bb82f927f89e5d5c49ce65af524f8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:10 -0700

    user 7, session 131, part 2

--- diffs-annotated/7/user-7-session-131.v
+++ diffs-annotated/7/user-7-session-131.v
@@ -35,4 +35,15 @@ Proof.
 (intros k w1).
 exists w1.
 (intros v Hm).
-(* Failed. *)
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_union__inv in Hm1).
+(destruct Hm1; [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto using match_ty_pair).
+-
+(intros k w1).
+exists w1.
+(intros v Hm).
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto using match_ty_pair).
+-
