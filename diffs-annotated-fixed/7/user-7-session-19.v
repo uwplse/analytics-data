ac2967216a1797ab385c5a33b1aa6a0d4c190b7b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:47:08 -0700

    user 7, session 19, part 211

--- diffs-annotated-fixed/7/user-7-session-19.v
+++ diffs-annotated-fixed/7/user-7-session-19.v
@@ -101,6 +101,20 @@ constructor.
 (destruct Hm as [t' Heq]; subst).
 constructor.
 Qed.
-Lemma aaa : forall (k : nat) (t t' : ty), forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t' -> | t | <= | t' |.
+Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
+Proof.
+(induction k; induction t; induction t'; intros H; try (solve [ simpl; constructor ]);
+  try (solve
+   [ match goal with
+     | |- | ?t1 | <= | ?t2 | =>
+           (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+             (H _ Hm); contradiction) ||
+             (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
+               (H _ Hm); contradiction)
+     end ])).
+(match goal with
+ | |- | ?t1 | <= | ?t2 | =>
+       assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize (H _ Hm)
+ end).
 (* Auto-generated comment: Failed. *)
 
