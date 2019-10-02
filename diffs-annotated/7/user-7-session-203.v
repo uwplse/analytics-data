c99b733a012fd280fa0e505c3315b3ee93eb025f
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:49 -0700

    user 7, session 203, part 6

--- diffs-annotated/7/user-7-session-203.v
+++ diffs-annotated/7/user-7-session-203.v
@@ -189,33 +189,9 @@ assumption.
 Search -IdSet.Empty.
 (apply IdSetProps.empty_is_empty_2).
 Search -IdSet.Equal.
-(apply IdSetProps.equal_refl).
-Qed.
-Lemma match_ty__exists_w_v : forall t : ty, exists (w : nat) (v : ty), |-[ w] v <$ t.
+Definition ty_empty (t : ty) := forall (w : nat) (v : ty), |-[ w] v <$ t -> False.
+Definition ty_not_empty (t : ty) := exists (w : nat) (v : ty), |-[ w] v <$ t.
+Lemma match_ty__prop : forall t : ty, ty_empty t \/ ty_not_empty t.
 Proof.
 (induction t).
--
-exists 0,(TCName c).
-(apply match_ty_cname).
--
-(destruct (IHt1) as [w1 [v1 Hm1]]).
-(destruct (IHt2) as [w2 [v2 Hm2]]).
-exists (Nat.max w1 w2),(TPair v1 v2).
-(apply match_ty_pair; eapply match_ty__ge_w; try eassumption).
-(apply Nat.le_max_l).
-(apply Nat.le_max_r).
--
-(destruct (IHt1) as [w [v Hm]]).
-exists w,v.
-(apply match_ty_union_1; assumption).
--
-(apply (match_ty__match_ty_f_subst_int i) in Hm).
-(destruct Hm as [v' Hm]).
-exists (S w),v'.
-(apply match_ty_exist).
-exists tint.
-split.
-admit.
-assumption.
--
 (* Failed. *)
