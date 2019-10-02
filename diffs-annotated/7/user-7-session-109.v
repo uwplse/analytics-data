2e99262ff1f611638cdcd71e659d9e955957525d
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:59 -0700

    user 7, session 109, part 3

--- diffs-annotated/7/user-7-session-109.v
+++ diffs-annotated/7/user-7-session-109.v
@@ -47,6 +47,55 @@ Fixpoint subst (x : id) (s t : ty) :=
   | TEV y => t
   end
 where "'[' x ':=' s ']' t" := (subst x s t) : btjt_scope.
-Print id_set.
+Print IdSet.remove.
 Print IdSet.
+Fixpoint FV (t : ty) : id_set :=
+  match t with
+  | TCName _ => IdSet.empty
+  | TPair t1 t2 => IdSet.union (FV t1) (FV t2)
+  | TUnion t1 t2 => IdSet.union (FV t1) (FV t2)
+  | TRef t' => FV t'
+  | TExist y t' => IdSet.remove y (FV t')
+  | TVar y => IdSet.singleton y
+  | TEV _ => IdSet.empty
+  end.
+Inductive value_type : ty -> Prop :=
+  | VT_CName : forall cn, value_type (TCName cn)
+  | VT_Pair : forall v1 v2, value_type v1 -> value_type v2 -> value_type (TPair v1 v2)
+  | VT_Ref : forall t, value_type (TRef t)
+  | VT_EV : forall X : id, value_type (TEV X).
+Hint Constructors value_type: DBBetaJulia.
+Declare Scope btjm_scope.
+Delimit Scope btjm_scope with btjm.
+Open Scope btjm.
+Reserved Notation "'|-[' k ',' w ']' v '<$' t" (at level 40).
+Fixpoint match_ty (k : nat) :=
+  fix mtyw (w : nat) :=
+    fix mtyv (v : ty) :=
+      fix mtyt (t : ty) :=
+        match k, w, v, t with
+        | _, _, TCName c, TCName c' => c = c'
+        | _, _, TPair v1 v2, TPair t1 t2 => mtyv v1 t1 /\ mtyv v2 t2
+        | _, _, _, TUnion t1 t2 => mtyt t1 \/ mtyt t2
+        | 0, _, TRef t', TRef t => True
+        | S k, _, TRef t', TRef t =>
+            (forall w1, exists w2, forall v, |-[ k, w1] v <$ t' -> |-[ k, w2] v <$ t) /\
+            (forall w1, exists w2, forall v, |-[ k, w1] v <$ t -> |-[ k, w2] v <$ t')
+        | _, S w, v, TExist X t' => exists tx, mtyw w v ([X := tx] t')
+        | _, _, TEV X, TVar X' => X = X'
+        | _, _, TEV X, TEV X' => X = X'
+        | _, _, _, _ => False
+        end
+where "'|-[' k ',' w ']' v '<$' t" := (match_ty k w v t) : btjm_scope.
+Definition sem_sub_k_w (k w1 w2 : nat) (t1 t2 : ty) := forall v : ty, |-[ k, w1] v <$ t1 -> |-[ k, w2] v <$ t2.
+Notation "'||-[' k ',' w1 ',' w2 ']' '[' t1 ']' '<=' '[' t2 ']'" := (sem_sub_k_w k w1 w2 t1 t2) (at level 45) : btjm_scope.
+Definition sem_sub_k (k : nat) (t1 t2 : ty) := forall w1 : nat, exists w2 : nat, ||-[ k, w1, w2][t1]<= [t2].
+Notation "'||-[' k ']' '[' t1 ']' '<=' '[' t2 ']'" := (sem_sub_k k t1 t2) (at level 47) : btjm_scope.
+Definition sem_eq_k (k : nat) (t1 t2 : ty) := ||-[ k][t1]<= [t2] /\ ||-[ k][t2]<= [t1].
+Notation "'||-[' k ']' '[' t1 ']' '=' '[' t2 ']'" := (sem_eq_k k t1 t2) (at level 47) : btjm_scope.
+Definition sem_sub (t1 t2 : ty) := forall k : nat, ||-[ k][t1]<= [t2].
+Notation "'||-' '[' t1 ']' '<=' '[' t2 ']'" := (sem_sub t1 t2) (at level 50) : btjm_scope.
+Definition sem_eq (t1 t2 : ty) := forall k : nat, ||-[ k][t1]= [t2].
+Notation "'||-' '[' t1 ']' '=' '[' t2 ']'" := (sem_eq t1 t2) (at level 50) : btjm_scope.
+Hint Unfold sem_sub_k_w sem_sub_k sem_eq_k sem_sub sem_eq: DBBetaJulia.
 (* Failed. *)
