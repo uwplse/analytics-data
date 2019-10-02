9a9036313f18a57e2c0d8229b32347e091b96652
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:44 -0700

    user 3, session 960, part 262

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -305,6 +305,9 @@ admit.
 typeclasses eauto.
 -
 (simpl).
+(unfold EqualDec).
 decide equality.
-(* Auto-generated comment: Failed. *)
+decide equality.
+decide equality.
+(* Auto-generated comment: Succeeded. *)
 
