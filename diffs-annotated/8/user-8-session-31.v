c0f5450ec199245f314044006405835cb6c38d61
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:45 -0700

    user 8, session 31, part 7

--- diffs-annotated/8/user-8-session-31.v
+++ diffs-annotated/8/user-8-session-31.v
@@ -2185,5 +2185,7 @@ Lemma denote_gate_circuit :
 Proof.
 Timeout 1 About process_gate.
 Timeout 1 Print process_gate.
-Timeout 1 Print Ltac process_gate.
+replace (process_gate g p1 \206\147) with
+ (process_gate_pat g p1 \206\147, process_gate_state g p1 \206\147)
+ by (symmetry; apply surjective_pairing).
 (* Failed. *)
