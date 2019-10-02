83dd359c36415f70bfab175093b4ac4002e52fbc
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:13:54 -0700

    user 3, session 6414, part 84

--- diffs-annotated-fixed/3/user-3-session-6414.v
+++ diffs-annotated-fixed/3/user-3-session-6414.v
@@ -136,7 +136,6 @@ Proof.
 (unfold get_len; intros).
 (apply spec_abstraction_compose).
 step_proc.
-(destruct a' as [_ bs]).
-intuition.
+(destruct a' as [_ bs]; simpl in *).
 (* Auto-generated comment: Succeeded. *)
 
