1338f7ff18e6f9dceb808333c8b971c63a23f7ae
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:37 -0700

    user 7, session 83, part 8

--- diffs-annotated/7/user-7-session-83.v
+++ diffs-annotated/7/user-7-session-83.v
@@ -28,42 +28,9 @@ Proof.
 (intros v t1 t2 k Hm).
 (destruct k; destruct v; right; assumption).
 Qed.
-Lemma match_ty_i__transitive_on_value_type :
-  forall v1 v2 t3 : ty, value_type v2 -> forall k : nat, |-[ k] v1 <$ v2 -> |-[ k] v2 <$ t3 -> |-[ k] v1 <$ t3.
-Proof.
-(intros v1 v2 t3 Hv2).
-generalize dependent t3.
-generalize dependent v1.
-(induction Hv2).
--
-(intros v1 t3 k Hm1 Hm2).
-(apply match_ty_i_cname__inv in Hm1; subst).
-assumption.
--
-(intros v0 t3 k Hm1 Hm2).
-(apply match_ty_i_pair__inv in Hm1).
-(destruct Hm1 as [pv11 [pv12 [Heq [Hm11 Hmpv12]]]]; subst).
-(induction t3; try (solve [ destruct k; contradiction ])).
-+
-(apply match_ty_i_pair__inv in Hm2).
-(destruct Hm2 as [pv21 [pv22 [Heq [Hm21 Hm22]]]]).
-(inversion Heq; subst).
-auto using match_ty_i_pair.
-+
-(apply match_ty_i_union__inv in Hm2).
-(destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
--
-(intros v1 t3 k Hm1 Hm2).
-(induction t3; try (solve [ destruct k; contradiction ])).
-+
-(apply match_ty_i_union__inv in Hm2).
-(destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
-+
-clear IHt3.
-(destruct k).
 (destruct v1; contradiction || constructor).
 (apply match_ty_i_ref__inv in Hm1).
 (destruct Hm1 as [tx [Heqx Hrefx]]; inversion Heqx; subst).
-(apply match_ty_i_ref__inv in Hm2).
-(destruct Hm2 as [ty [Heqy Hrefy]]; inversion Heqy; subst).
+(simpl in Hm2).
+(apply sem_eq_i__trans with t).
 (* Failed. *)
