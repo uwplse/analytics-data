7280cb03647f7aa9341fff8f7df211e89cec1625
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:52:08 -0700

    user 7, session 148, part 6

--- diffs-annotated-fixed/7/user-7-session-148.v
+++ diffs-annotated-fixed/7/user-7-session-148.v
@@ -91,7 +91,6 @@ tauto.
 (simpl in *).
 (induction t').
 +
-(intros k).
-Check match_ty__exists_w_v.
+(intros k; destruct (match_ty__exists_w_v (TPair ([X := TVar X'] t1) ([X := TVar X'] t2)) k) as [w [v Hm]]).
 (* Auto-generated comment: Failed. *)
 
