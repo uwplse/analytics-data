32ec281d3c94e78e177b18d82307fc03d72181cf
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 16:05:15 -0700

    user 8, session 125, part 107

--- diffs-annotated-fixed/8/user-8-session-125.v
+++ diffs-annotated-fixed/8/user-8-session-125.v
@@ -40,7 +40,5 @@ Proof.
 matrix_denote.
 setoid_rewrite denote_unitary_transpose.
 (simpl in *; Msimpl).
-(repeat rewrite Mmult_assoc).
-(simpl_rewrite @unitary_gate_unitary).
 (* Auto-generated comment: Succeeded. *)
 
