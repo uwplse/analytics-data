6716c5060016415e5a6c034de8a8d6141a36e11c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:46 -0700

    user 3, session 960, part 300

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -309,6 +309,6 @@ typeclasses eauto.
 (intros; simpl).
 Search -sigT -"dec".
 Check Eqdep_dec.inj_pair2_eq_dec.
-(destruct (Eqdep_dec.inj_pair2_eq_dec _ _ _ x y)).
+(apply (Eqdep_dec.inj_pair2_eq_dec _ _ _ x y)).
 (* Auto-generated comment: Failed. *)
 
