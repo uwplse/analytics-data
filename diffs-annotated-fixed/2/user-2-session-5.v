fd2e0ebd224ae907d02d8d494d9a54b855d71497
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:29 -0700

    user 2, session 5, part 68

--- diffs-annotated-fixed/2/user-2-session-5.v
+++ diffs-annotated-fixed/2/user-2-session-5.v
@@ -13,5 +13,21 @@ Redirect "/tmp/coq4iI7Dj" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Open Scope string_scope.
-(* Auto-generated comment: Failed. *)
+Inductive term :=
+  | Nil : term
+  | Ident : string -> term
+  | Symb : string -> term
+  | Cons : term -> term -> term
+  | App : term -> term -> term.
+Redirect "/tmp/coq84xhGL" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Definition oneArgCbvPrimitive (name : string) : bool :=
+  if
+   List.find (String.eqb name)
+     ("fst" :: "snd" :: "fun" :: "arg" :: "nil?" :: "app?" :: "cons?" :: nil)
+  then true
+  else false.
+Redirect "/tmp/coqi2dfar" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+(* Auto-generated comment: Succeeded. *)
 
