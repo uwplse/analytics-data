ba7df419f33a850c70b74b3e80ddc03d200d1955
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:39 -0700

    user 7, session 6, part 0

---
+++ diffs-annotated/7/user-7-session-6.v
@@ -0,0 +1,7 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import Analytical.Analytics.
+(* Auto-generated comment: Failed. *)
