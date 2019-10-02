d5969321281a606bc5cf5a3b228f1d8914c01502
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:53 -0700

    user 7, session 105, part 1

--- diffs-annotated/7/user-7-session-105.v
+++ diffs-annotated/7/user-7-session-105.v
@@ -10,5 +10,28 @@ Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Open Scope btjm.
-Lemma match_ty_cname : forall (c : cname) (k w : nat), |-[ k, w] TCName <$ TCName.
-(* Auto-generated comment: Failed. *)
+Lemma match_ty_cname : forall (c : cname) (k w : nat), |-[ k, w] TCName c <$ TCName c.
+(intros c k w).
+(destruct k, w; reflexivity).
+Qed.
+Lemma match_ty_pair : forall (v1 v2 t1 t2 : ty) (k w : nat), |-[ k, w] v1 <$ t1 -> |-[ k, w] v2 <$ t2 -> |-[ k, w] TPair v1 v2 <$ TPair t1 t2.
+Proof.
+(intros v1 v2 t1 t2 k w Hm1 Hm2).
+(destruct k, w; split; assumption).
+Qed.
+Lemma match_ty_union_1 : forall (v t1 t2 : ty) (k w : nat), |-[ k, w] v <$ t1 -> |-[ k, w] v <$ TUnion t1 t2.
+Proof.
+(intros v t1 t2 k w Hm).
+(destruct k, w, v; left; assumption).
+Qed.
+Lemma match_ty_union_2 : forall (v t1 t2 : ty) (k w : nat), |-[ k, w] v <$ t2 -> |-[ k, w] v <$ TUnion t1 t2.
+Proof.
+(intros v t1 t2 k w Hm).
+(destruct k, w, v; right; assumption).
+Qed.
+Lemma match_ty_exist : forall (v : ty) (X : id) (t : ty) (k w : nat), (exists tx : ty, |-[ k, w] v <$ [X := tx] t) -> |-[ k, S w] v <$ TExist X t.
+Proof.
+(intros v X t k w Hex).
+(destruct k, v; assumption).
+Qed.
+Lemma match_ty_var : forall (X : cname) (k w : nat), |-[ k, w] TEV X <$ TVar X.
