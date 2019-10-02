b55ebed235b002e49e0cf28c43713309fb958872
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:55 -0700

    user 8, session 34, part 3

--- diffs-annotated/8/user-8-session-34.v
+++ diffs-annotated/8/user-8-session-34.v
@@ -15,13 +15,7 @@ Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqRfcFt8" Print Ltac
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Proof.
-Fact denote_compose :
-  forall safe w (c : Circuit w) (\206\147 : Ctx) \207\129,
-  \206\147 \226\138\162 c :Circ ->
-  forall w' (f : Pat w -> Circuit w') (\206\1470 \206\1471 \206\1471' \206\14701 : Ctx),
-  \206\1471 \226\138\162 f :Fun ->
-  \206\1471' \226\169\181 \206\1471 \226\136\153 \206\147 ->
-  \206\14701 \226\169\181 \206\1470 \226\136\153 \206\1471 ->
-  denote_circuit safe (compose c f) \206\1470 \206\1471' \207\129 =
-  compose_super (denote_circuit safe (f (add_fresh_pat w \206\1471)) \206\1470 (add_fresh_state w \206\1471)) (denote_circuit safe c \206\14701 \206\147) \207\129.
+Timeout 1 About denote_circuit.
+Timeout 1 Print denote_circuit.
+Timeout 1 Print Ltac denote_circuit.
 (* Failed. *)
