4cefd9a2416bbae9263f4323370f6734142829f1
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:39 -0700

    user 8, session 14, part 45

--- diffs-annotated/8/user-8-session-14.v
+++ diffs-annotated/8/user-8-session-14.v
@@ -1214,6 +1214,8 @@ Lemma apply_new0_correct : forall n, WF_Superoperator (@apply_new0 n).
 Proof.
 (intros n \207\129 M\207\129).
 (unfold apply_new0, super).
+gen \207\129.
+(rewrite <- (Nat.mul_1_r (2 ^ n)%nat)).
 (repeat rewrite Nat.pow_add_r).
 (intros).
 Msimpl.
