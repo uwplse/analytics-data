6c43a81c4cc1463b8ef0117af8c33f47e383dcb8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:57:52 -0700

    user 8, session 53, part 8

--- diffs-annotated-fixed/8/user-8-session-53.v
+++ diffs-annotated-fixed/8/user-8-session-53.v
@@ -947,5 +947,8 @@ Lemma assert_init_at_id :
 (intros b m i Lt \207\129 safe).
 (simpl).
 (simpl_rewrite id_circ_spec).
+Timeout 1 About id_circ_spec.
+Timeout 1 Print id_circ_spec.
+Timeout 1 Print Ltac id_circ_spec.
 (* Auto-generated comment: Succeeded. *)
 
