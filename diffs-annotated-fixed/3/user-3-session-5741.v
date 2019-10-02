9ff0fda3374c3550d212b39f3bdace10bb47a0e1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:11:43 -0700

    user 3, session 5741, part 15

--- diffs-annotated-fixed/3/user-3-session-5741.v
+++ diffs-annotated-fixed/3/user-3-session-5741.v
@@ -427,13 +427,6 @@ Qed.
 Theorem mult_S_1 : forall n m : nat, m = S n -> m * (1 + n) = m * m.
 Proof.
 (intros n m H).
-(rewrite H).
-reflexivity.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqUBWrf3"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+(simpl).
 (* Auto-generated comment: Succeeded. *)
 
