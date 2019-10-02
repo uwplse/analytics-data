2111e155a5a7b1a3117a0d04acca96fb512e41b4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:33 -0700

    user 3, session 960, part 95

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -130,8 +130,10 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Print FixedLengthEncoder.
-Theorem nat_le_inverse base : forall x, le_to_nat (nat_to_le base x) = x.
+Theorem nat_le_inverse base : forall n, le_to_nat (nat_to_le base n) = n.
 Proof.
 (intros).
+(induction n as [n IHn] using lt_wf_ind).
+(destruct n).
 (* Auto-generated comment: Succeeded. *)
 
