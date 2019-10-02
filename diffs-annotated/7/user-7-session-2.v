ab1a0834e74b6814fc89c99bbe8956caf3fd2f28
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:35 -0700

    user 7, session 2, part 9

--- diffs-annotated/7/user-7-session-2.v
+++ diffs-annotated/7/user-7-session-2.v
@@ -13,17 +13,4 @@ Require Import Coq.Bool.Bool.
 Close Scope btj_scope.
 Open Scope btjnf_scope.
 Open Scope btjr_scope.
-Lemma atom_sub_r_union__sub_r_component : forall t t1' t2' : ty, |- t << TUnion t1' t2' -> atom_type t -> |- t << t1' \/ |- t << t2'.
-Proof.
-(intros t t1' t2' Hsub).
-(remember (TUnion t1' t2') as t' eqn:Heq ).
-(induction Hsub; intros Hat; try (solve [ inversion Heq | inversion Hat ])).
--
-(inversion Heq; subst).
-(left; assumption).
--
-(inversion Heq; subst).
-(right; assumption).
--
-(inversion Heq; subst).
-(rewrite (mk_nf_atomty__equal _ Hat) in *).
+(* Failed. *)
