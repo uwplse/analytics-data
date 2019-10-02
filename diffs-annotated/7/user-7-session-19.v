ee92667010dddd5d4166a1a5c67c307093cf06a8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:08 -0700

    user 7, session 19, part 262

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -3,5 +3,9 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 (apply match_ty_i_union__inv in Hm2).
-(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
+(destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
++
+clear IHt3.
+(destruct k).
+(destruct v1; try contradiction).
 (* Failed. *)
