7c5c72433d0567c4ac718fcdb89cccd8c66f7f26
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:08 -0700

    user 7, session 19, part 255

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,12 +2,4 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-auto using match_ty_i_pair.
-+
-(apply match_ty_i_union__inv in Hm2).
-(destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
--
-(intros v1 v3 k Hm1 Hm2).
-(destruct k).
-(destruct v1; inversion Hm1).
-(apply match_ty_i_ref__inv in Hm1).
+(* Failed. *)
