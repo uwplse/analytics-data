747e58dd14f3b1eca3565bed6418f615ba5a88d5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:48:23 -0700

    user 7, session 24, part 38

--- diffs-annotated-fixed/7/user-7-session-24.v
+++ diffs-annotated-fixed/7/user-7-session-24.v
@@ -141,6 +141,6 @@ Inductive sub_d : ty -> ty -> Prop :=
 Hint Constructors sub_d: DBBetaJulia.
 Lemma union_right_1 : forall t t1 t2 : ty, |- t << t1 -> |- t << TUnion t1 t2.
 Proof.
-(intros; eapply SD_Trans; eauto).
+(intros t t1 t2 H).
 (* Auto-generated comment: Failed. *)
 
