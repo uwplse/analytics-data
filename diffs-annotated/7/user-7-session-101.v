6019095ff08cf483600b55aca939c1823b9f6b81
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:50 -0700

    user 7, session 101, part 2

--- diffs-annotated/7/user-7-session-101.v
+++ diffs-annotated/7/user-7-session-101.v
@@ -23,5 +23,10 @@ exists 1.
 (apply match_ty_ref__weak_inv in Hm).
 (destruct Hm as [t' Heq]; subst).
 (simpl).
+exists tint.
 constructor.
+-
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [t' [Heq Href]]; subst).
+(simpl).
 (* Failed. *)
