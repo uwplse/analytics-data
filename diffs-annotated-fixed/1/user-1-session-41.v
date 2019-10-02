f8633dc8e893796bd3eabd1813c65bf6a38eef70
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:19 -0700

    user 1, session 41, part 99

--- diffs-annotated-fixed/1/user-1-session-41.v
+++ diffs-annotated-fixed/1/user-1-session-41.v
@@ -308,10 +308,10 @@ Fixpoint size_gt (G : GT) : nat :=
       fold_right
         (fun x acc =>
          match x with
-         | Some (_, G) => size - gt G
+         | Some (_, G) => size_gt G
          | _ => 0
          end + acc) 1 l
   | _ => 0
   end.
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
