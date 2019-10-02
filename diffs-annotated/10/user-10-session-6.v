dcc9892a443c1aae901f6bcccbbea90179a31d95
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:06:03 -0700

    user 10, session 6, part 1

--- diffs-annotated/10/user-10-session-6.v
+++ diffs-annotated/10/user-10-session-6.v
@@ -8,4 +8,11 @@ Add Search Blacklist "Private_" "_subproof".
 From ExtLib Require Import Functor Monad.
 From SimpleIO Require Import IO_Random.
 From DeepWeb Require Import Crypto4 IO_Test.
-(* Auto-generated comment: Failed. *)
+Definition run_tester : io_unit :=
+  IO.unsafe_run
+    (ORandom.self_init tt;; run_test (multi_test (exec_test test_crypto))).
+Separate Extraction run_tester.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqQJpnIk"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
