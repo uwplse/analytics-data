4819406193f554cad19bb79414b18bebed2fad9b
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:49 -0700

    user 7, session 203, part 2

--- diffs-annotated/7/user-7-session-203.v
+++ diffs-annotated/7/user-7-session-203.v
@@ -183,5 +183,32 @@ exists v'.
 exists tx.
 split.
 assumption.
-(rewrite b_subst_neq__permute in IHw).
+(rewrite b_subst_neq__permute in IHw; try assumption).
+(unfold wf_ty).
+(simpl).
+Search -IdSet.Empty.
+(apply IdSetProps.empty_is_empty_2).
+Search -IdSet.Equal.
+(apply IdSetProps.equal_refl).
+Qed.
+Lemma match_ty__exists_w_v : forall t : ty, exists (w : nat) (v : ty), |-[ w] v <$ t.
+Proof.
+(induction t).
+-
+exists 0,(TCName c).
+(apply match_ty_cname).
+-
+(destruct (IHt1) as [w1 [v1 Hm1]]).
+(destruct (IHt2) as [w2 [v2 Hm2]]).
+exists (Nat.max w1 w2),(TPair v1 v2).
+(apply match_ty_pair; eapply match_ty__ge_w; try eassumption).
+(apply Nat.le_max_l).
+(apply Nat.le_max_r).
+-
+(destruct (IHt1) as [w [v Hm]]).
+exists w,v.
+(apply match_ty_union_1; assumption).
+-
+(destruct IHt as [w [v Hm]]).
+(apply match_ty__match_ty_f_subst_int in Hm).
 (* Failed. *)
