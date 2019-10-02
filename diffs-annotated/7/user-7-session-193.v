07b0a186577ce526775201931b7bbeedd8d0ad1b
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:43 -0700

    user 7, session 193, part 0

---
+++ diffs-annotated/7/user-7-session-193.v
@@ -0,0 +1,48 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Require Import Recdef.
+Create HintDb DBBetaJulia.
+Declare Scope btjt_scope.
+Delimit Scope btjt_scope with btjt.
+Open Scope btjt.
+Inductive cname : Type :=
+  | NInt : _
+  | NFlt : _
+  | NStr : _.
+Inductive ty : Type :=
+  | TCName : cname -> ty
+  | TPair : ty -> ty -> ty
+  | TUnion : ty -> ty -> ty
+  | TExist : id -> ty -> ty
+  | TBVar : id -> ty
+  | TFVar : id -> ty
+  | TEV : id -> ty.
+Definition tint := TCName NInt.
+Definition tflt := TCName NFlt.
+Definition tstr := TCName NStr.
+Definition tIntInt := TPair tint tint.
+Definition vX := Id 1.
+Definition vY := Id 2.
+Definition vZ := Id 3.
+Definition tX := TBVar vX.
+Definition tY := TBVar vY.
+Definition teXX := TExist vX tX.
+Fixpoint FFV (t : ty) : id_set :=
+  match t with
+  | TCName _ => IdSet.empty
+  | TPair t1 t2 => IdSet.union (FV t1) (FV t2)
+  | TUnion t1 t2 => IdSet.union (FV t1) (FV t2)
+  | TExist y t' => IdSet.remove y (FV t')
+  | TBVar _ => IdSet.empty
+  | TFVar y => IdSet.singleton y
+  | TEV _ => IdSet.empty
+  end.
+(* Auto-generated comment: Failed. *)
