ce6c88710714ed787722eb658821883c6221a1b0
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:15 -0700

    user 7, session 19, part 383

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -8,14 +8,16 @@ exists (TRef t).
 (induction k; induction t; induction t'; intros Hnft Hsem; try (solve [ simpl; constructor ]);
   try (solve
    [ match goal with
-     | Hsem:||-[ ?k][?t1]<= [?t2]
-       |- | ?t1 | <= | ?t2 | =>
-           assert (Hv : value_type t1) by constructor; assert (Hm : |-[ k] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+     | Hsem:||-[ ?k][?t]<= [?t']
+       |- | ?t | <= | ?t' | =>
+           assert (Hv : value_type t) by constructor; assert (Hm : |-[ k] t <$ t) by (apply match_ty_i__reflexive; assumption); specialize
             (Hsem _ Hm); contradiction
+     | Hsem:||-[ ?k][?t]<= [TUnion ?t'1 ?t'2]
+       |- | ?t | <= _ =>
+           assert (Hv : value_type t) by constructor; pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ Hsem) as Hsemu;
+            destruct Hsemu as [Hsemu| Hsemu]; [ apply Nat.le_trans with (| t'1 |) | apply Nat.le_trans with (| t'2 |) ];
+            tauto || apply Max.le_max_l || apply Max.le_max_r
      end ])).
 -
-(assert (Hv : value_type (TCName c)) by constructor).
-(pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ Hsem) as Hsemu).
-(destruct Hsemu as [Hsemu| Hsemu]; [ apply Nat.le_trans with (| t'1 |) | apply Nat.le_trans with (| t'2 |) ];
-  tauto || apply Max.le_max_l || apply Max.le_max_r).
+(destruct (match_ty_i_exists t1) as [v1 Hm1]; destruct (match_ty_i_exists t2) as [v2 Hm2]).
 (* Failed. *)
