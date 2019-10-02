9bb0556f84d648215a47c3d055ad60bd33f0e7ad
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:51 -0700

    user 7, session 206, part 2

--- diffs-annotated/7/user-7-session-206.v
+++ diffs-annotated/7/user-7-session-206.v
@@ -128,6 +128,9 @@ Proof.
 (rewrite IHt).
 reflexivity.
 (unfold not_b_free_in_ty, not_free in *).
-(simpl in HX).
-(intros Hcontra admit).
+(intros Hcontra).
+(apply HX).
+Search -IdSet.remove.
+(apply IdSetFacts.remove_iff).
+tauto.
 (* Failed. *)
