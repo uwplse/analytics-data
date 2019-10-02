79de037765f5ebfdc132609ff20c4f63f520cfd2
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:49 -0700

    user 7, session 16, part 41

--- diffs-annotated/7/user-7-session-16.v
+++ diffs-annotated/7/user-7-session-16.v
@@ -230,13 +230,11 @@ Ltac
   destruct IHt2_1 as [IH1| IH1]; destruct IHt2_2 as [IH2| IH2];
    try (solve [ left; apply SR_UnionR1; assumption | left; apply SR_UnionR2; assumption ]); right; intros Hcontra;
    apply atom_sub_r_union__inv in Hcontra; tauto || constructor.
-(split; intros t'; induction t';
+(split; intros t'; induction t'; try (solve [ right; solve_not_x_sub_r_y_full ]);
   try
    match goal with
    | |- Decidable.decidable (|- TCName ?c1 << TCName ?c2) =>
          destruct (cname_eq__decidable c1 c2);
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
-+
-(right; solve_not_x_sub_r_y_full).
 (* Failed. *)
