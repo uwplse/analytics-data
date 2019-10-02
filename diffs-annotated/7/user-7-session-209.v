d250e238891c7357d4b0af4a87cb951804f39e61
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:54 -0700

    user 7, session 209, part 1

--- diffs-annotated/7/user-7-session-209.v
+++ diffs-annotated/7/user-7-session-209.v
@@ -43,5 +43,45 @@ assumption.
 }
 {
 (apply match_ty_union_1).
-(rewrite f_subst_not_b_free_in_ty).
-(* Auto-generated comment: Failed. *)
+(apply match_ty_union_2; auto).
+*
+(destruct (f_free_in_ty__dec X' t'2) as [HXt'2| HXt'2]).
+{
+(apply match_ty_union_2; auto).
+}
+{
+(apply match_ty_union_2).
+(rewrite f_subst_not_b_free_in_ty; assumption).
+}
++
+(rewrite f_subst_union).
+(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
+  destruct IHt' as [IHt'a IHt'b]; split; intros HX').
+*
+(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
+(apply match_ty_union_1; auto).
+*
+(destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
+{
+(apply match_ty_union_1; auto).
+}
+{
+(apply match_ty_union_1).
+(rewrite f_subst_not_b_free_in_ty; assumption).
+}
+*
+(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
+(apply match_ty_union_2; auto).
+*
+(destruct (f_free_in_ty__dec X' t'2) as [HXt'2| HXt'2]).
+{
+(apply match_ty_union_2; auto).
+}
+{
+(apply match_ty_union_2).
+(rewrite f_subst_not_b_free_in_ty; assumption).
+}
++
+(apply match_ty_exist__inv in Hm').
+(destruct Hm' as [ti Hm']).
+(* Failed. *)
