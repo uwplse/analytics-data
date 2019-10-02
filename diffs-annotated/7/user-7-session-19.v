87c3e349b6e9160c80a2032c46a95e0dda88aca1
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:15 -0700

    user 7, session 19, part 388

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -5,5 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 exists (TRef t).
 (split; intros v Hm; assert (Hmu : |-[ k] v <$ TUnion t1 t2) by (apply match_ty_i_union_1; assumption) || (apply match_ty_i_union_2; assumption);
   apply Hsem; assumption).
-(destruct (match_ty_i_exists t1 0) as [v1 Hm1]; destruct (match_ty_i_exists t2 0) as [v2 Hm2]).
+(inversion Hnft; subst).
 (* Failed. *)
