f6b3c5b1ef1a489e1c2799d579ba4541f8a0b6d8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:48:21 -0700

    user 7, session 24, part 23

--- diffs-annotated-fixed/7/user-7-session-24.v
+++ diffs-annotated-fixed/7/user-7-session-24.v
@@ -63,8 +63,68 @@ Notation "'||-' '[' t1 ']' '<=' '[' t2 ']'" := (sem_sub t1 t2) (at level 50) : b
 Definition sem_eq (t1 t2 : ty) := forall k : nat, ||-[ k][t1]= [t2].
 Notation "'||-' '[' t1 ']' '=' '[' t2 ']'" := (sem_eq t1 t2) (at level 50) : btjm_scope.
 Hint Unfold sem_sub_k sem_eq_k sem_sub sem_eq: DBBetaJulia.
-Example ex : ||- [tint]= [tint].
+Inductive atom_type : ty -> Prop :=
+  | AT_CName : forall c : cname, atom_type (TCName c)
+  | AT_Pair : forall ta1 ta2 : ty, atom_type ta1 -> atom_type ta2 -> atom_type (TPair ta1 ta2)
+  | AT_Ref : forall t : ty, in_nf t -> atom_type (TRef t)
+with in_nf : ty -> Prop :=
+  | NF_Atom : forall ta : ty, atom_type ta -> in_nf ta
+  | NF_Union : forall t1 t2 : ty, in_nf t1 -> in_nf t2 -> in_nf (TUnion t1 t2).
+Scheme atom_type_mut := Induction for atom_type Sort Prop
+  with in_nf_mut := Induction for in_nf Sort Prop.
+Declare Scope btjnf_scope.
+Delimit Scope btjnf_scope with btjnf.
+Open Scope btjnf.
+Notation "'InNF(' t ')'" := (in_nf t) (at level 30) : btjnf_scope.
+Hint Constructors atom_type in_nf: DBBetaJulia.
+Example innf_1 : InNF( tint).
 Proof.
-(unfold sem_eq).
+(repeat constructor).
+Qed.
+Example innf_2 : InNF( TPair tint tstr).
+Proof.
+(repeat constructor).
+Qed.
+Example innf_3 : InNF( TUnion (TPair tint tstr) tint).
+Proof.
+(apply NF_Union; repeat constructor).
+Qed.
+Example innf_4 : InNF( TPair tint (TUnion tint tstr)) -> False.
+Proof.
+(intros Hcontra; inversion Hcontra).
+(inversion H).
+(inversion H4).
+Qed.
+Example innf_5 : InNF( TRef (TUnion tint tstr)).
+Proof.
+(apply NF_Atom).
+(apply AT_Ref).
+(solve [ repeat constructor ]).
+Qed.
+Example innf_6 : InNF( TRef (TPair tint (TUnion tint tstr))) -> False.
+Proof.
+(intros Hcontra).
+(inversion Hcontra; subst).
+(inversion H; subst).
+(apply innf_4; assumption).
+Qed.
+Fixpoint unite_pairs (t1 : ty) :=
+  fix unprs (t2 : ty) :=
+    match t1, t2 with
+    | TUnion t11 t12, _ => TUnion (unite_pairs t11 t2) (unite_pairs t12 t2)
+    | _, TUnion t21 t22 => TUnion (unprs t21) (unprs t22)
+    | _, _ => TPair t1 t2
+    end.
+Fixpoint mk_nf (t : ty) :=
+  match t with
+  | TCName n => t
+  | TPair t1 t2 => let t1' := mk_nf t1 in let t2' := mk_nf t2 in unite_pairs t1' t2'
+  | TUnion t1 t2 => TUnion (mk_nf t1) (mk_nf t2)
+  | TRef t' => TRef (mk_nf t')
+  end.
+Notation "'MkNF(' t ')'" := (mk_nf t) (at level 30) : btjnf_scope.
+Declare Scope btjd_scope.
+Delimit Scope btjd_scope with btjd.
+Open Scope btjd.
 (* Auto-generated comment: Failed. *)
 
