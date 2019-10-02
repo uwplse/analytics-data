dc53eaa8205d6d0ed6659797e8b0c564955738a0
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:51:44 -0700

    user 3, session 314, part 26

--- diffs-annotated-fixed/3/user-3-session-314.v
+++ diffs-annotated-fixed/3/user-3-session-314.v
@@ -259,6 +259,12 @@ Proof.
 (hnf in H; propositional).
 (destruct H0; propositional).
 (apply crash_step_simp in H; auto).
-(apply crash_step_simp in H; auto).
-(* Auto-generated comment: Failed. *)
+(apply crash_step_simp in H0; auto).
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqWIRPze"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+(* Auto-generated comment: Succeeded. *)
 
