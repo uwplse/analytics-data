043eaf0f5725ebc98bb8e67637306f63882fbd38
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:01:56 -0700

    user 7, session 19, part 20

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -11,5 +11,10 @@ Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Open Scope btjmi_scope.
-(apply match_ty_i_pair; tauto).
+(induction k; intros v t; generalize dependent v; induction t; intros v Hm k' Hle;
+  try
+   match goal with
+   | |- |-[ ?k'] ?v <$ TCName _ => apply match_ty_i_cname__inv in Hm; subst; reflexivity
+   | |- |-[ ?k'] ?v <$ TPair _ _ => apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; apply match_ty_i_pair; tauto
+   end).
 (* Failed. *)
