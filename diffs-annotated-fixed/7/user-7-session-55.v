35a0a8834c27965fb9d3af0ae68697406bffb3f9
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:48:53 -0700

    user 7, session 55, part 5

--- diffs-annotated-fixed/7/user-7-session-55.v
+++ diffs-annotated-fixed/7/user-7-session-55.v
@@ -16,6 +16,7 @@ Lemma value_sem_sub_k_i_union__inv :
   forall v : ty, value_type v -> forall (k : nat) (ta tb : ty), ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
 Proof.
 (intros v Hv k ta tb Hsem; unfold sem_sub_k in Hsem).
-(assert (Hdep : | v | <= k) by apply match_ty__inv_depth_l_le_index with (TUnion ta tb)).
-(* Auto-generated comment: Failed. *)
+(assert (Hdep : | v | <= k)).
+(apply match_ty__inv_depth_l_le_index with (TUnion ta tb)).
+(* Auto-generated comment: Succeeded. *)
 
