7af20f0eaaa51f32a0e94d740848e7cfd0d6dcc4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:16:17 -0700

    user 3, session 7199, part 101

--- diffs-annotated-fixed/3/user-3-session-7199.v
+++ diffs-annotated-fixed/3/user-3-session-7199.v
@@ -302,27 +302,12 @@ Theorem get_upto_ok a :
      recovered := fun _ state' => state' = state |}) 
     (get_upto a) recover abstr.
 Opaque firstn.
-Opaque firstn.
-Theorem get_upto_ok a :
-  proc_spec
-    (fun (_ : unit) state =>
-     {|
-     pre := a <= length state;
-     post := fun r state' => state' = state /\ r = firstn a state;
-     recovered := fun _ state' => state' = state |}) 
-    (get_upto a) recover abstr.
+Lemma firstn_one_more :
+  forall (a : nat) (state : list block),
+  S a <= length state ->
+  firstn a state ++ [nth a state block0] = firstn (S a) state.
 Proof.
-(induction a; simpl).
--
-step_proc.
--
-step_proc.
-step_proc.
-intuition eauto.
-{
-lia.
-}
-step_proc.
-Timeout 1 Show.
+(intros a state H).
+(simpl).
 (* Auto-generated comment: Succeeded. *)
 
