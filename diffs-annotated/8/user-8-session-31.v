17a0abf31529cd0e4636b2124a39b686dd291d5e
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:46 -0700

    user 8, session 31, part 13

--- diffs-annotated/8/user-8-session-31.v
+++ diffs-annotated/8/user-8-session-31.v
@@ -2172,3 +2172,21 @@ easy.
 dependent destruction TP.
 easy.
 Qed.
+Lemma denote_gate_circuit :
+  forall {w1} {w2} {w'} (safe : bool) (g : Gate w1 w2) p1 
+    (f : Pat w2 -> Circuit w') (\206\1470 \206\147 \206\1471 \206\1472 : Ctx),
+  \206\147 \226\169\181 \206\1471 \226\136\153 \206\1472 ->
+  \206\1471 \226\138\162 p1 :Pat ->
+  denote_circuit safe (gate g p1 f) \206\1470 \206\147 =
+  compose_super
+    (denote_circuit safe (f (process_gate_pat g p1 \206\147)) \206\1470
+       (process_gate_state g p1 \206\147))
+    (apply_gate safe g (pat_to_list (subst_pat \206\147 p1))).
+Proof.
+(intros).
+(unfold denote_circuit).
+(simpl; fold_denotation).
+replace (process_gate g p1 \206\147) with
+ pair (process_gate_pat g p1 \206\147) (process_gate_state g p1 \206\147)
+ by (symmetry; apply surjective_pairing).
+(* Failed. *)
