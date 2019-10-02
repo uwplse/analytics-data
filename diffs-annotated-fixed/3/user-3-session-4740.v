4ca2172a01bdf185a388ac7fb724389872784e5b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:08:10 -0700

    user 3, session 4740, part 0

---
+++ diffs-annotated-fixed/3/user-3-session-4740.v
@@ -0,0 +1,20 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqUJOOPh"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq98GU8h"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8DKAxw"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Notation "a \226\137\161 b" := (a = b).
+(* Auto-generated comment: Failed. *)
+
