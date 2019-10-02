0c6e2cf8ac508088ccbadb2d1364292b61bb01a6
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:04 -0700

    user 7, session 19, part 171

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,5 +2,6 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-(assert (Hv : value_type (TRef t))).
+(assert (Hv : value_type (TRef t)) by constructor).
+(assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; constructor)).
 (* Failed. *)
