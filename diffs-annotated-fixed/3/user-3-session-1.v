e12b9b63a635c4ae19ce9bba6ddf2c160c5efbb3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:50:32 -0700

    user 3, session 1, part 15

--- diffs-annotated-fixed/3/user-3-session-1.v
+++ diffs-annotated-fixed/3/user-3-session-1.v
@@ -465,11 +465,6 @@ Proof.
 (unfold spec_exec in H1).
 (destruct ret; simpl in *; firstorder).
 Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect
-"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqwsUJVO"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+(destruct ret; firstoder).
 (* Auto-generated comment: Succeeded. *)
 
