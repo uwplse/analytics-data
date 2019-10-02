30f46231030b92f76f15c002b322a559850c7855
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:16:30 -0700

    user 3, session 7199, part 312

--- diffs-annotated-fixed/3/user-3-session-7199.v
+++ diffs-annotated-fixed/3/user-3-session-7199.v
@@ -393,6 +393,5 @@ step_proc.
 (unfold log_size_ok; autorewrite with list; auto).
 (exists bs; intuition eauto).
 (unfold log_abstraction; intuition eauto).
-(unfold log_length_ok).
 (* Auto-generated comment: Succeeded. *)
 
