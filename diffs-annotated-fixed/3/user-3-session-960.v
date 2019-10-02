7b54002aafb121d960a980b8466477707632c403
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:31 -0700

    user 3, session 960, part 48

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -80,23 +80,35 @@ Proof.
 (apply PeanoNat.Nat.mod_upper_bound; auto).
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqNd84Xb"
+#[program]
+Fixpoint nat_to_le base (x : nat) {measure x : list {x : nat | x < S base} :=
+  match x with
+  | 0 => nil
+  | _ =>
+      let digit := x mod S base in
+      exist _ digit _ :: nat_to_le base ((x - digit) / S base)
+  end.
+Next Obligation.
+Proof.
+(apply PeanoNat.Nat.mod_upper_bound; auto).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqkAmYiA"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Qed.
-Next Obligation.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqsXaK0u"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqTZy0we"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqfRsBS8"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8035nf"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
+Next Obligation.
 Proof.
-Search -"div" -"<".
 (apply PeanoNat.Nat.div_lt).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
