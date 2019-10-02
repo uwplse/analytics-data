37c882cb3d66cd5dd0648f5479fca4473cc1c09c
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:01 -0700

    user 7, session 219, part 8

--- diffs-annotated/7/user-7-session-219.v
+++ diffs-annotated/7/user-7-session-219.v
@@ -110,4 +110,41 @@ assumption.
 (intros w' t' Hm').
 (split; intros HX').
 assumption.
+Abort.
+Open Scope btjm.
+Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
+Proof.
+(intros t1 t2 Hsub).
+(induction Hsub).
+-
+(apply sem_sub__refl).
+-
+(apply sem_sub__trans with t2; assumption).
+-
+(apply sem_sub_pair; assumption).
+-
+(apply sem_sub_union; assumption).
+-
+(apply sem_sub_union_1).
+(apply sem_sub__refl).
+-
+(apply sem_sub_union_2).
+(apply sem_sub__refl).
+-
+(intros w1).
+exists w1.
+(intros v Hm).
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_union__inv in Hm1).
+(destruct Hm1; [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto using match_ty_pair).
+-
+(intros w1).
+exists w1.
+(intros v Hm).
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto using match_ty_pair).
+-
 (* Failed. *)
