016328383a9275593d52f04a7d741baa257bebf8
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:07 -0700

    user 7, session 19, part 239

--- diffs-annotated/7/user-7-session-19.v
+++ diffs-annotated/7/user-7-session-19.v
@@ -2,8 +2,7 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Lemma value_sem_sub_k_union__value_sem_sub_k_component :
-  forall k : nat, forall v : ty, value_type v -> forall ta tb : ty, ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
-Proof.
-(induction k; intros v Hv; induction Hv; intros Hsem).
+(induction k; intros v Hv; induction Hv; intros ta tb Hsem).
+6: {
+idtac Abort.
 (* Failed. *)
