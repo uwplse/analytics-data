c171e71b5403a918a51d8c67737e1a0421fedadb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:11:30 -0700

    user 3, session 5682, part 0

---
+++ diffs-annotated-fixed/3/user-3-session-5682.v
@@ -0,0 +1,12 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqLLv3hB"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Export List.
+Require Export Lia Arith.
+From POCS Require Export Helpers.Helpers.
+(* Auto-generated comment: Succeeded. *)
+
