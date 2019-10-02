bae1d9f1734ad25618858f727e9f73553245cb47
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:09:39 -0700

    user 3, session 5111, part 20

--- diffs-annotated-fixed/3/user-3-session-5111.v
+++ diffs-annotated-fixed/3/user-3-session-5111.v
@@ -195,6 +195,15 @@ Proof.
 (destruct a'; simpl in *; intuition idtac).
 {
 (destruct (a == r)).
-}
-(* Auto-generated comment: Failed. *)
+Admitted.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqkiR9n0"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqUKQE2O"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+(* Auto-generated comment: Succeeded. *)
 
