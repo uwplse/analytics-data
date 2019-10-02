924bff802769ce3f524cdae871efd5bb13e4ae46
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:22 -0700

    user 7, session 41, part 8

--- diffs-annotated/7/user-7-session-41.v
+++ diffs-annotated/7/user-7-session-41.v
@@ -3,5 +3,3 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
-Require Import BetaJulia.Sub0250a.MatchProps.
-(* Failed. *)
