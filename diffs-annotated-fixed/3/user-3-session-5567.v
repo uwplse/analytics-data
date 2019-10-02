6aedc9d898d4503ea5de39c7d319d6cc32eb340b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:11:08 -0700

    user 3, session 5567, part 7

--- diffs-annotated-fixed/3/user-3-session-5567.v
+++ diffs-annotated-fixed/3/user-3-session-5567.v
@@ -171,6 +171,8 @@ Proof.
 step.
 (destruct r; step).
 {
+(destruct r; step).
+-
 (descend; intuition eauto).
 {
 (destruct (lt_dec a (diskSize a'))).
@@ -181,6 +183,13 @@ simplify.
 }
 step.
 (destruct r; intuition eauto; simplify).
-}
+-
+(destruct r; step).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqpDheCv"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
