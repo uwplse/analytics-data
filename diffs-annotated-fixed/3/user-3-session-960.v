6c8e684b3a5e3b7e6f9ccdaa594177c53716596d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:32 -0700

    user 3, session 960, part 78

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -104,6 +104,7 @@ Next Obligation of nat_to_le_func.
 Proof.
 (unfold MR).
 Search -wf.
-(apply (wf_projected lt projT2)).
-(* Auto-generated comment: Succeeded. *)
+(apply (wf_projected lt projT2); auto).
+(apply wf_lt).
+(* Auto-generated comment: Failed. *)
 
