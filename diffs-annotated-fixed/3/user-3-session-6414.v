f64b21b6681b6024ab4136f00ac22b2e58283c44
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:13:49 -0700

    user 3, session 6414, part 5

--- diffs-annotated-fixed/3/user-3-session-6414.v
+++ diffs-annotated-fixed/3/user-3-session-6414.v
@@ -54,6 +54,6 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq1ktiSB"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Definition abstr : Abstraction State := True.
+Definition abstr : Abstraction State := fun _ _ => True.
 (* Auto-generated comment: Failed. *)
 
