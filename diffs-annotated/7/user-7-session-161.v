56179eb2455dfa6932377efc57b41518ad85a8ab
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:22 -0700

    user 7, session 161, part 8

--- diffs-annotated/7/user-7-session-161.v
+++ diffs-annotated/7/user-7-session-161.v
@@ -104,11 +104,4 @@ assumption.
 subst.
 (rewrite subst_exist_eq).
 assumption.
-(apply match_ty_exist__inv in Hm).
-(destruct Hm as [ti Hm]).
-specialize (IHw' _ Hm).
-(rewrite subst_neq__permute in IHw').
-exists ti.
-assumption.
-assumption.
 (* Failed. *)
