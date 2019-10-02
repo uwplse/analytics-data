9d7f4a0f2a239e80fda3a653ce77286d68b91b62
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:53:42 -0700

    user 7, session 190, part 8

--- diffs-annotated-fixed/7/user-7-session-190.v
+++ diffs-annotated-fixed/7/user-7-session-190.v
@@ -37,6 +37,5 @@ assumption.
 (rewrite subst_union).
 (destruct (free_in_ty__decidable X t'1) as [HXt'1| HXt'1]).
 (apply match_ty_union_1; auto).
-(exfalso; eapply not__free_and_fresh_in_ty; eassumption).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
