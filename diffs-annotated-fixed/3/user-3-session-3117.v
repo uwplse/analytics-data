25b0c73e0a454f353abb6ab4ef530c1468ffc838
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:02:24 -0700

    user 3, session 3117, part 11

--- diffs-annotated-fixed/3/user-3-session-3117.v
+++ diffs-annotated-fixed/3/user-3-session-3117.v
@@ -118,6 +118,5 @@ Theorem nat_ascii_bounded : forall a, Ascii.nat_of_ascii a < 256.
 Proof.
 (destruct a; simpl; intros).
 Search -"-" -"<".
-(apply Nat.sub_lt).
 (* Auto-generated comment: Succeeded. *)
 
