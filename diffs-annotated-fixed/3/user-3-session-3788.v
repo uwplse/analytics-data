4308f454317c3c2f2404a7b78e7070ea487ea384
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:04:44 -0700

    user 3, session 3788, part 21

--- diffs-annotated-fixed/3/user-3-session-3788.v
+++ diffs-annotated-fixed/3/user-3-session-3788.v
@@ -32,34 +32,8 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoPhort"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Definition change_marker `{m : @ProcMarker T p} {T'} 
-  (p' : proc T') : ProcMarker p' := AProc p'.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqfBlXZI"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4luqFX"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Theorem swapXY_ok :
-  proc_spec
-    (fun (_ : unit) state =>
-     {|
-     pre := True;
-     post := fun r state' =>
-             state' = mkState (StateY state) (StateX state) (StateZ state);
-     recovered := fun _ state' => True |}) swapXY vars.recover vars.abstr.
-Proof.
-(match goal with
- | |- proc_spec _ ?p _ _ => pose proof (AProc p) as Hbefore
- end).
-(unfold swapXY).
-monad_simpl.
-About change_marker.
-(match goal with
- | |- proc_spec _ ?p _ _ => apply (change_marker p) in Hbefore
- end).
-(* Auto-generated comment: Succeeded. *)
+Definition change_marker {T'} (p' : proc T') :
+  forall `{m : @ProcMarker T p}, ProcMarker p' := 
+  AProc p'.
+(* Auto-generated comment: Failed. *)
 
