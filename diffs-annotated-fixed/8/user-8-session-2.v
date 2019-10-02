4daf33717793a4d85f44824c75567b6a1d3e9da4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:55:34 -0700

    user 8, session 2, part 169

--- diffs-annotated-fixed/8/user-8-session-2.v
+++ diffs-annotated-fixed/8/user-8-session-2.v
@@ -140,6 +140,6 @@ Opaque firstn.
 Opaque rev.
 Opaque skipn.
 (simpl).
-(rewrite ctrl_list_to_unitary_transpose).
+(rewrite <- ctrl_list_to_unitary_transpose).
 (* Auto-generated comment: Failed. *)
 
