9bd7f42687a79f9debf61a581e121d9b36b97647
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:08 -0700

    user 7, session 228, part 2

--- diffs-annotated/7/user-7-session-228.v
+++ diffs-annotated/7/user-7-session-228.v
@@ -85,5 +85,21 @@ clear Hm'.
 (apply not_f_free_in_ty_fvar__inv in HX').
 contradiction.
 *
-(simpl).
++
+admit.
++
+(rewrite f_subst_union).
+(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
+  destruct IHt' as [IHt'a IHt'b]; split; intros HX').
+*
+(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
+(apply match_ty_union_1; auto).
+*
+(destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
+{
+(apply match_ty_union_1; auto).
+}
+{
+(apply match_ty_union_1; rewrite f_subst_not_b_free_in_ty; auto).
+}
 (* Failed. *)
