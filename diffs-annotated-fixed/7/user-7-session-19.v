8a076045b8d938f7b8a12f52a1eac5a89cc145e4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:46:58 -0700

    user 7, session 19, part 122

--- diffs-annotated-fixed/7/user-7-session-19.v
+++ diffs-annotated-fixed/7/user-7-session-19.v
@@ -103,9 +103,15 @@ clear IHt'.
 (simpl).
 (apply f_equal).
 (apply IHt).
-(intros k v).
+(intros k v Hv).
 (assert (Hmt : |-[ S k] TRef t <$ TRef t) by (simpl; tauto)).
 specialize (H (S k) (TRef t)).
 (destruct H as [H _]).
+constructor.
+specialize (H Hmt).
+(apply match_ty_i_ref__inv in H).
+(destruct H as [tx [Heq Href]]).
+(inversion Heq; subst).
+auto.
 (* Auto-generated comment: Failed. *)
 
