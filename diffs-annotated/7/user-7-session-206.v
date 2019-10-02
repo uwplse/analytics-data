eb68a6a0e560946ed07e7872cc388d3d328b96f9
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:03:51 -0700

    user 7, session 206, part 10

--- diffs-annotated/7/user-7-session-206.v
+++ diffs-annotated/7/user-7-session-206.v
@@ -34,16 +34,14 @@ Proof.
 (intros X t).
 (apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
 Qed.
-Lemma not_free_union__inv : forall (X : id) (fvs1 fvs2 : id_set), not_free X (IdSet.union fvs1 fvs2) -> not_free X fvs1 /\ not_free X fvs2.
-Proof.
-(intros X fvs1 fvs2 H).
-(unfold not_free in *).
-(split; intros Hcontra; [ apply (IdSetFacts.union_2 fvs2) in Hcontra | apply (IdSetFacts.union_3 fvs1) in Hcontra ]; contradiction).
-Qed.
-Ltac
- solve_not_free_union fvname := intros X t1 t2 Hfresh; unfold fvname in *; simpl in Hfresh; simpl; apply not_free_union__inv in Hfresh; assumption.
-Lemma wf_ty__not_b_free_in_ty : forall (t : ty) (X : id), wf_ty t -> not_b_free_in_ty X t.
-Proof.
-(unfold wf_ty in *).
-(unfold not_b_free_in_ty, not_free).
+(unfold wf_ty, not_b_free_in_ty, not_free).
+(intros t X Ht Hcontra).
+(apply IdSetProps.empty_is_empty_1 in Ht).
+Search -IdSet.empty.
+Search -IdSet.Equal.
+Search -IdSet.In.
+(pose proof IdSetFacts.In_m).
+Search -Morphisms.Proper.
+Search -Morphisms.respectful.
+(destruct (IdSetFacts.empty_iff Y) as [H _]).
 (* Failed. *)
