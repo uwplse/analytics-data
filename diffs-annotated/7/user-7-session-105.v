40cb65a9a54358e7dca8ba04cf77f79a1e30bf11
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:02:52 -0700

    user 7, session 105, part 0

---
+++ diffs-annotated/7/user-7-session-105.v
@@ -0,0 +1,14 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Add LoadPath "../..".
+Require Import BetaJulia.BasicPLDefs.Identifier.
+Require Import BetaJulia.Sub0280a.BaseDefs.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Open Scope btjm.
+Lemma match_ty_cname : forall (c : cname) (k w : nat), |-[ k, w] TCName <$ TCName.
+(* Auto-generated comment: Failed. *)
