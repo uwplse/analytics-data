75c1af733504f2e0320b1d94567a14ba9a8e976c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 14:54:39 -0700

    user 3, session 960, part 193

--- diffs-annotated-fixed/3/user-3-session-960.v
+++ diffs-annotated-fixed/3/user-3-session-960.v
@@ -167,15 +167,36 @@ auto.
 (assert (0 < S n) by lia).
 (generalize dependent S n; clear n; intros n **).
 (destruct (lt_dec n base)).
-+
-admit.
-+
 (rewrite IHn).
 {
 (rewrite (PeanoNat.Nat.div_mod n base)  at 3 by lia).
 lia.
 }
-Search -"div" -"lt".
 (apply Nat.div_lt; lia).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqQBdBtX"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqicZbpe"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqlBjurg"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Instance aModel : GoModel.
+Proof.
+refine
+ {|
+ byte := Ascii.ascii;
+ byte0 := Ascii.Ascii false false false false false false false false;
+ uint64_to_string := pretty.pretty_nat;
+ ascii_to_byte := id;
+ byte_to_ascii := id |}.
 (* Auto-generated comment: Succeeded. *)
 
