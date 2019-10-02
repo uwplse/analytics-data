1665c27d79ce0642ec71d06ea8a5479834f88543
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:59 -0700

    user 8, session 40, part 8

--- diffs-annotated/8/user-8-session-40.v
+++ diffs-annotated/8/user-8-session-40.v
@@ -26,8 +26,4 @@ autorewrite with proof_db.
 (unfold pad).
 (simpl).
 (rewrite Nat.sub_diag).
-(rewrite kron_1_r').
-(rewrite subst_pat_fresh_empty).
-(rewrite denote_pat_fresh_id).
 (rewrite super_I; auto).
-Qed.
