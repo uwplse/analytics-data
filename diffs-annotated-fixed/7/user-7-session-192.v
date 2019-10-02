232ddbc283cb40f9e78e781ab5bc871871d9cd8a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:53:49 -0700

    user 7, session 192, part 54

--- diffs-annotated-fixed/7/user-7-session-192.v
+++ diffs-annotated-fixed/7/user-7-session-192.v
@@ -56,6 +56,12 @@ Lemma build_v_full :
   exists v' : ty,
     |-[ w] v' <$ [X := TVar X'] t /\
     (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> (fresh_in_ty X' t' -> |-[ w'] v <$ t') /\ (free_in_ty X' t' -> |-[ w'] v <$ [X' := tx] t')).
+Lemma build_v_full :
+  forall (X X' : id) (tx : ty) (w : nat) (t v : ty),
+  |-[ w] v <$ [X := tx] t ->
+  exists v' : ty,
+    |-[ w] v' <$ [X := TVar X'] t /\
+    (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> (fresh_in_ty X' t' -> |-[ w'] v <$ t') /\ (free_in_ty X' t' -> |-[ w'] v <$ [X' := tx] t')).
 Proof.
 (intros X X' tx).
 (induction w; induction t; intros v Hm).
@@ -88,30 +94,5 @@ admit.
 (destruct Hm' as [ti Hm']).
 specialize (IHw' _ Hm').
 (destruct IHw' as [IHw'a IHw'b]).
-(destruct (either_free_or_fresh_in_ty X' ([i := ti] t'))).
-*
-specialize (IHw'b H).
-(split; intros HX').
-{
-(apply match_ty_exist).
-exists ti.
-assumption.
-}
-{
-(destruct (beq_idP X' i)).
-{
-subst.
-(unfold free_in_ty in HX').
-(simpl in HX').
-admit.
-}
-{
-(rewrite subst_equation).
-Search -beq_id.
-(assert (Hbeq : beq_id X' i = false) by (apply beq_id_false_iff; assumption)).
-(rewrite Hbeq).
-(destruct (IdSet.mem i (FV tx))).
-{
-(remember (gen_fresh (IdSet.union (FV tx) (IdSet.add X' (FV t')))) as z).
 (* Auto-generated comment: Failed. *)
 
