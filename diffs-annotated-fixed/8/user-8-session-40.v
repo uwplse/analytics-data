32fe326912f6967a0123aa6c1b0710721cb521c9
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:57:27 -0700

    user 8, session 40, part 36

--- diffs-annotated-fixed/8/user-8-session-40.v
+++ diffs-annotated-fixed/8/user-8-session-40.v
@@ -71,5 +71,15 @@ Proof.
 (destruct safe).
 -
 matrix_denote.
+Msimpl.
+lma.
+-
+matrix_denote.
+Msimpl.
+lma.
+Qed.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqXm3oVN"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
 (* Auto-generated comment: Succeeded. *)
 
