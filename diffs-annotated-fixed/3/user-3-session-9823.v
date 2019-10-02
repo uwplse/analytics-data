4eec30c9a65e2d2d4eec449ed7e490f7e4a48e6c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:33:07 -0700

    user 3, session 9823, part 15

--- diffs-annotated-fixed/3/user-3-session-9823.v
+++ diffs-annotated-fixed/3/user-3-session-9823.v
@@ -192,6 +192,7 @@ replace (diskSize (stateDisk state) - 1) with diskSize s in * by lia.
 (rewrite <- Hremap by eauto; auto).
 -
 invert_abstraction.
+(step_proc; intuition eauto).
 (step_proc; intuition).
 (* Auto-generated comment: Succeeded. *)
 
