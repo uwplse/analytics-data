3502a3501b07f01cecbb74f0f779230bb3f0f7ba
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:04:28 -0700

    user 8, session 2, part 273

--- diffs-annotated/8/user-8-session-2.v
+++ diffs-annotated/8/user-8-session-2.v
@@ -51,27 +51,5 @@ Timeout 1 About restore_dims.
 Timeout 1 About trans.
 Timeout 1 About ctrls_to_list.
 (simpl_rewrite @denote_unitary_transpose).
-Axiom
-  (operator_sum_decomposition : forall {m} {n} (l : list (Matrix m n)), outer_sum l == I n <-> WF_Superoperator (operator_sum l)).
-Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqZu7SSe" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Lemma discard_superoperator :
-  forall (n i j : nat) (\207\129 : Square (2 ^ n)),
-  (i * j * 2 = 2 ^ n)%nat ->
-  Mixed_State \207\129 -> Mixed_State (I i \226\138\151 \226\159\1680\226\136\163 \226\138\151 I j \195\151 \207\129 \195\151 (I i \226\138\151 \226\136\1630\226\159\169 \226\138\151 I j) .+ I i \226\138\151 \226\159\1681\226\136\163 \226\138\151 I j \195\151 \207\129 \195\151 (I i \226\138\151 \226\136\1631\226\159\169 \226\138\151 I j)).
-Proof.
-(intros n i j \207\129 E M).
-(destruct (operator_sum_decomposition [I i \226\138\151 \226\159\1680\226\136\163 \226\138\151 I j; I i \226\138\151 \226\159\1681\226\136\163 \226\138\151 I j]) as [WFS _]).
-(assert (OS : outer_sum [I i \226\138\151 \226\159\1680\226\136\163 \226\138\151 I j; I i \226\138\151 \226\159\1681\226\136\163 \226\138\151 I j] == I (i * 2 * j))).
-{
-(unfold outer_sum).
-(simpl).
-Msimpl.
-(rewrite <- kron_plus_dist_r, <- kron_plus_dist_l).
-mat_replace \226\136\1630\226\159\169\226\159\1680\226\136\163 .+ \226\136\1631\226\159\169\226\159\1681\226\136\163 with I 2 by lma.
-(repeat rewrite id_kron).
-easy.
-}
-specialize (WFS OS \207\129).
-(unfold operator_sum, Splus, SZero, super, WF_Superoperator in WFS).
 (simpl in WFS).
+autorewrite with M_db in WFS.
