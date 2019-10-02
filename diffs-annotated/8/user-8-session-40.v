0b8529ce4ac09396d2700522811f50874f3b9c16
Talia Ringer <tringer@cs.washington.edu>
2019-10-01 17:05:00 -0700

    user 8, session 40, part 17

--- diffs-annotated/8/user-8-session-40.v
+++ diffs-annotated/8/user-8-session-40.v
@@ -27,5 +27,11 @@ autorewrite with proof_db.
 (simpl).
 (rewrite Nat.sub_diag).
 (rewrite super_I; easy).
-Hint Rewrite kron_1_r'.
-(* Failed. *)
+Hint Rewrite @kron_1_r'.
+Lemma X_spec :
+  forall b safe : bool,
+  denote_box safe (boxed_gate _X) (bool_to_matrix b) == bool_to_matrix (\194\172 b).
+Proof.
+(intros).
+vector_denote.
+(destruct b; unfold bool_to_ket; simpl; Msimpl).
