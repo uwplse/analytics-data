72312a6a0e3c5f1cb865ab1cbcecf8c51c0c580d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:54:49 -0700

    user 7, session 224, part 13

--- diffs-annotated-fixed/7/user-7-session-224.v
+++ diffs-annotated-fixed/7/user-7-session-224.v
@@ -167,5 +167,6 @@ subst.
 exists (TEV X').
 split.
 reflexivity.
+(induction w'; induction t'; intros Hm'; try (solve [ destruct v; contradiction || tauto ])).
 (* Auto-generated comment: Failed. *)
 
