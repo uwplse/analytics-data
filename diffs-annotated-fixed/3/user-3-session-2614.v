13de11c909482df1463dfabf6c9814e7a8d429c3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:00:17 -0700

    user 3, session 2614, part 15

--- diffs-annotated-fixed/3/user-3-session-2614.v
+++ diffs-annotated-fixed/3/user-3-session-2614.v
@@ -8,5 +8,16 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import FunInd Recdef.
 From Coq Require Import PeanoNat.
 From Coq Require Import Arith.
-(* Auto-generated comment: Failed. *)
+From Coq Require Import ProofIrrelevance.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq3LrJrG"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqkRD83W"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+(* Auto-generated comment: Succeeded. *)
 
