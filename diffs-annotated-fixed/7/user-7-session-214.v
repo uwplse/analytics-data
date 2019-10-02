1ed68c7bfe9b86d4dbd5bfb7ca22932bf7fd979d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 15:54:27 -0700

    user 7, session 214, part 8

--- diffs-annotated-fixed/7/user-7-session-214.v
+++ diffs-annotated-fixed/7/user-7-session-214.v
@@ -107,7 +107,6 @@ Proof.
 (simpl).
 (intros X t1 t2 H).
 Search -IdSet.In.
-(destruct H as [H| H]).
-(apply IdSetFacts.union_2).
+(destruct H as [H| H]; [ apply IdSetFacts.union_2 | apply IdSetFacts.union_3 ]).
 (* Auto-generated comment: Failed. *)
 
