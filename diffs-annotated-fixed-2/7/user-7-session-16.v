8dc3ba62029cf093eecd0d313a7d59843977215f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:13 -0700

    user 7, session 16, part 36

--- diffs-annotated-fixed-2/7/user-7-session-16.v
+++ diffs-annotated-fixed-2/7/user-7-session-16.v
@@ -370,6 +370,6 @@ Proof.
      (fun (t : ty) (Hnf : in_nf t) => (forall t' : ty, Decidable.decidable (|- t << t')) /\ (forall t' : ty, Decidable.decidable (|- t' << t))))).
 -
 (intros c).
-(split; intros t'; induction t').
+(split; intros t'; induction t'; try (solve [ right; solve_not_x_sub_r_y_full ])).
 (* Auto-generated comment: Failed. *)
 
