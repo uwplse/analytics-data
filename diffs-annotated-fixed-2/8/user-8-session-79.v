ba0b370d13c53ec07713ea7428931f8b0a2685b0
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:11 -0700

    user 8, session 79, part 34

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -604,6 +604,7 @@ Timeout 1 Print Ltac Msimpl_light.
 Timeout 1 About Matrix.Msimpl_light.
 Timeout 1 Print Matrix.Msimpl_light.
 Timeout 1 Print Ltac Matrix.Msimpl_light.
-(progress Msimpl).
+(progress restore_dims).
+Msimpl.
 (* Auto-generated comment: Succeeded. *)
 
