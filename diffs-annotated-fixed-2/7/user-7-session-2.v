6259bce472c9a28bcd3db1ac876aa9f3238a711a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:57 -0700

    user 7, session 2, part 147

--- diffs-annotated-fixed-2/7/user-7-session-2.v
+++ diffs-annotated-fixed-2/7/user-7-session-2.v
@@ -235,5 +235,97 @@ specialize (IHHsub11_2 Hsub122 _ Hsub21_2 Hsub222).
 (apply sub_r_union_l__inv in Hsub22).
 split.
 (apply SR_UnionR1; tauto).
+Abort.
+Lemma sub_r_nf__transitive : forall t1 t2 t3 : ty, |- t1 << t2 -> InNF( t1) -> InNF( t2) -> |- t2 << t3 -> |- t1 << t3.
+Proof.
+(intros t1 t2 t3 Hsub1).
+generalize dependent t3.
+(induction Hsub1; intros t3 Hnf1 Hnf2 Hsub2; try (solve [ assumption ])).
+-
+(inversion Hnf1; subst).
+(inversion Hnf2; subst).
+(inversion H; inversion H0; subst).
+(remember (TPair t1' t2') as tx eqn:Heqx ; induction Hsub2; inversion Heqx; subst).
++
+(constructor; [ apply IHHsub1_1 | apply IHHsub1_2 ]; try assumption || constructor; assumption).
++
+(apply SR_UnionR1; auto).
++
+(apply SR_UnionR2; auto).
++
+(apply IHHsub2).
+(apply mk_nf_nf__equal; assumption).
+(apply mk_nf__in_nf).
+(rewrite mk_nf_nf__equal; assumption).
+-
+(destruct (union_in_nf__components_in_nf _ _ Hnf1) as [Hnfa1 Hnfa2]).
+(constructor; [ apply IHHsub1_1 | apply IHHsub1_2 ]; assumption).
+-
+(destruct (union_in_nf__components_in_nf _ _ Hnf2) as [Hnfa1 Hnfa2]).
+(apply IHHsub1; try assumption).
+(pose proof (sub_r_nf_union_l__inv _ _ _ Hsub2 Hnf2); tauto).
+-
+(destruct (union_in_nf__components_in_nf _ _ Hnf2) as [Hnfa1 Hnfa2]).
+(apply IHHsub1; try assumption).
+(pose proof (sub_r_nf_union_l__inv _ _ _ Hsub2 Hnf2); tauto).
+-
+(remember (TRef t') as tx eqn:Heqx ).
+(inversion Hnf1; subst).
+(inversion Hnf2; subst).
+(inversion H; subst).
+(inversion H0; subst).
+(remember (TRef t') as tx eqn:Heqx ).
+(induction Hsub2; inversion Heqx; subst).
++
+(apply SR_UnionR1; tauto).
++
+(apply SR_UnionR2; tauto).
++
+Abort.
+Lemma weird_trans : forall t1 t2 t3 : ty, InNF( t1) -> InNF( t2) -> InNF( t3) -> |- t1 << t2 -> |- t2 << t3 -> |- t1 << t3.
+Proof.
+Abort.
+Lemma sub_r__mk_nf_sub_r : forall t t' : ty, |- t << t' -> |- MkNF( t) << MkNF( t').
+Proof.
+(intros t t' Hsub; induction Hsub; try (solve [ simpl; constructor ])).
+-
+(simpl).
+(apply unite_pairs_of_nf__preserves_sub_r; assumption || apply mk_nf__in_nf).
+-
+(simpl).
+(constructor; assumption).
+-
+(apply SR_UnionR1; assumption).
+-
+(apply SR_UnionR2; assumption).
+-
+(simpl).
+(constructor; assumption).
+-
+(rewrite <- mk_nf__idempotent).
+assumption.
+Qed.
+Lemma sub_r__reflexive : forall t : ty, |- t << t.
+Proof.
+(apply sub_r__rflxv).
+Qed.
+Lemma sub_r__transitive : forall t1 t2 t3 : ty, |- t1 << t2 -> |- t2 << t3 -> |- t1 << t3.
+Proof.
+(intros t1 t2 t3 Hsub1).
+generalize dependent t3.
+(induction Hsub1; intros t3 Hsub2).
+-
+assumption.
+-
+(remember (TPair t1' t2') as tm eqn:Heq ).
+(induction Hsub2; try (solve [ inversion Heq | constructor ])).
++
+(inversion Heq; subst).
+(constructor; auto with DBBetaJulia).
++
+(apply SR_UnionR1; tauto).
++
+(apply SR_UnionR2; tauto).
++
 (* Auto-generated comment: Failed. *)
 
