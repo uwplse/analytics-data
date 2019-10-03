9f332193987b5a30ad495062e7c6f281378d3722
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:38 -0700

    user 7, session 19, part 257

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -102,32 +102,11 @@ constructor.
 constructor.
 Qed.
 Lemma match_ty_i__transitive_on_value_type :
-  forall v1 v2 v3 : ty, value_type v2 -> forall k : nat, |-[ k] v1 <$ v2 -> |-[ k] v2 <$ v3 -> |-[ k] v1 <$ v3.
+  forall v1 v2 t3 : ty, value_type v2 -> forall k : nat, |-[ k] v1 <$ v2 -> |-[ k] v2 <$ t3 -> |-[ k] v1 <$ t3.
 Proof.
-(intros v1 v2 v3 Hv2).
-generalize dependent v3.
+(intros v1 v2 t3 Hv2).
+generalize dependent t3.
 generalize dependent v1.
-(induction Hv2).
--
-(intros v1 v3 k Hm1 Hm2).
-(apply match_ty_i_cname__inv in Hm1; subst).
-assumption.
--
-(intros v0 v3 k Hm1 Hm2).
-(apply match_ty_i_pair__inv in Hm1).
-(destruct Hm1 as [pv11 [pv12 [Heq [Hm11 Hmpv12]]]]; subst).
-(induction v3; try (solve [ destruct k; simpl in Hm2; contradiction ])).
-+
-(apply match_ty_i_pair__inv in Hm2).
-(destruct Hm2 as [pv21 [pv22 [Heq [Hm21 Hm22]]]]).
-(inversion Heq; subst).
-auto using match_ty_i_pair.
-+
-(apply match_ty_i_union__inv in Hm2).
-(destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
--
-(intros v1 v3 k Hm1 Hm2).
-(destruct k).
-(destruct v1; try contradiction).
+(induction Hv2; intros v1 t3 k Hm1 Hm2).
 (* Auto-generated comment: Failed. *)
 
