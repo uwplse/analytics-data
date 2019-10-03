4360cf6b3c0dfcd5e5463ca838bdaee52224ed16
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:24:42 -0700

    user 8, session 65, part 254

--- diffs-annotated-fixed-2/8/user-8-session-65.v
+++ diffs-annotated-fixed-2/8/user-8-session-65.v
@@ -1250,5 +1250,6 @@ constructor.
 (intros \207\129 TB).
 (repeat simpl_rewrite inSeq_correct; try apply inSeq_WT; try apply unitary_at1_WT;
   try apply source_symmetric_WT; trivial).
+(unfold compose_super).
 (* Auto-generated comment: Succeeded. *)
 
