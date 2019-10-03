e0c748c76f169d00346a409a7021261b2fcba4d6
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:43 -0700

    user 8, session 79, part 396

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -891,5 +891,16 @@ Fact Toffoli_at_spec :
   (\226\159\166 Toffoli_at n x y z \226\159\167) (\226\168\130 li) =
   \226\168\130 update_at li z (bool_to_matrix ((b1 && b2) \226\138\149 b3)).
 Admitted.
+Ltac
+ rewrite_inPar' :=
+  match goal with
+  | |-
+    context [ (@denote_box true ?W ?W' (@inPar ?W1 ?W1' ?W2 ?W2' ?f ?g))
+                (@kron ?m ?n ?o ?p ?\207\1291 ?\207\1292) ] =>
+        let IP := fresh "IP" in
+        specialize (inPar_correct W1 W1' W2 W2' f g true \207\1291 \207\1292) as IP;
+         rewrite size_ntensor in *; simpl in *; try rewrite Nat.mul_1_r in *;
+         rewrite IP
+  end; try (solve [ type_check ]).
 (* Auto-generated comment: Failed. *)
 
