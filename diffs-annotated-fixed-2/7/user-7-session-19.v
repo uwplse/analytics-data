db2221cd8252cdea999603ca97c725b5ed56cbdf
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:25 -0700

    user 7, session 19, part 26

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -74,6 +74,6 @@ Proof.
          apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto
    end).
 -
-(destruct v; contradiction).
+(destruct v; simpl in Hm; contradiction).
 (* Auto-generated comment: Failed. *)
 
