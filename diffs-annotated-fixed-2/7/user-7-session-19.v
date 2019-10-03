a08c2048738149b524d1cf1e9bf473963c1c85ed
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:45 -0700

    user 7, session 19, part 381

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -196,6 +196,8 @@ Proof.
      end ])).
 -
 (assert (Hv : value_type (TCName c)) by constructor).
-(pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ H) as Hsemu).
+(pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ Hsem) as Hsemu).
+(destruct Hsemu as [Hsemu| Hsemu]; [ apply Nat.le_trans with (| t'1 |) | apply Nat.le_trans with (| t'2 |) ];
+  tauto || apply Max.le_max_l || apply Max.le_max_r).
 (* Auto-generated comment: Failed. *)
 
