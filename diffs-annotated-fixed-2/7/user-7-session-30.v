fcdb8c8f7400aa4f6165a753cae775020f178e5e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:22 -0700

    user 7, session 30, part 32

--- diffs-annotated-fixed-2/7/user-7-session-30.v
+++ diffs-annotated-fixed-2/7/user-7-session-30.v
@@ -129,6 +129,6 @@ generalize dependent v.
 (intros v k Hm).
 (apply match_ty_pair__inv in Hm).
 (destruct Hm as [v1' [v2' [Heqp [Hm1 Hm2]]]]; subst).
-(simpl; apply Nat.max_lub; eauto).
+(simpl; apply Nat.max_lub; [ eapply IHv'1 | eapply IHv'2 ]; eassumption).
 (* Auto-generated comment: Failed. *)
 
