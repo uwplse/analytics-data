d940b80de1bba517841c5fec8017c4c014e4eac4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:22 -0700

    user 7, session 157, part 16

--- diffs-annotated-fixed-2/7/user-7-session-157.v
+++ diffs-annotated-fixed-2/7/user-7-session-157.v
@@ -17,8 +17,7 @@ Proof.
 (intros X X' tx).
 (induction w; induction t; intros v Hm).
 -
-exists (TCName c).
-(apply match_ty_cname).
+(exists v; assumption).
 -
 (rewrite subst_pair in *).
 (apply match_ty_pair__inv in Hm).
@@ -45,13 +44,12 @@ reflexivity.
 +
 exists v.
 (simpl in *).
-Check beq_id_false_iff.
-Print "<->".
-Print "/\".
 (destruct (beq_id_false_iff X i) as [_ Hid]).
 specialize (Hid n).
 (rewrite Hid in *).
 assumption.
+-
+(exists v; assumption).
 -
 (* Auto-generated comment: Failed. *)
 
