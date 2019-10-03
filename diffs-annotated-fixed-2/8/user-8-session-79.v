217633ec5dab4ce55d77ff739bdbb1a9bfe2ddf1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:36 -0700

    user 8, session 79, part 325

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -925,6 +925,7 @@ Timeout 1 Print Ltac restore_dims.
             A'
  end).
 2: {
-(apply f_equal_gen).
+(apply f_equal_gen; trivial).
+(apply f_equal_gen; trivial).
 (* Auto-generated comment: Succeeded. *)
 
