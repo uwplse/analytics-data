366dd8f2efae5daf6c6d98871d956c1ca0ef8fb8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:57 -0700

    user 7, session 2, part 134

--- diffs-annotated-fixed-2/7/user-7-session-2.v
+++ diffs-annotated-fixed-2/7/user-7-session-2.v
@@ -229,6 +229,7 @@ Proof.
 *
 clear Heqx IHHsub21_1 IHHsub21_2.
 (intros Hsub22).
-(apply sub_r_pair__inv in Hsub22).
+(apply sub_r_pair__inv in Hsub22; destruct Hsub22 as [Hsub221 Hsub222]).
+(split; constructor; auto).
 (* Auto-generated comment: Failed. *)
 
