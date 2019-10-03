81f6d7f3346506fd839f343e85a79e39ac571bad
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:26:14 -0700

    user 8, session 79, part 735

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -1007,5 +1007,23 @@ reflexivity.
 (apply ctx_lookup_exists; easy).
 -
 Opaque NTensor.
+(simpl in *).
+restore_dims tensor_tac.
+specialize inSeq_correct as IS.
+(simpl in IS).
+(repeat (rewrite IS; compile_typing compile_WT)).
+(unfold compose_super).
+restore_dims tensor_tac.
+(match goal with
+ | |-
+   context [ (@denote_box true ?W ?W' (@inPar ?W1 ?W1' ?W2 ?W2' ?f ?g))
+               (@kron ?m ?n ?o ?p ?\207\1291 ?\207\1292) ] =>
+       idtac f;
+        (let IP := fresh "IP" in
+         specialize (inPar_correct W1 W1' W2 W2' f g true \207\1291 \207\1292) as IP; simpl in *;
+          rewrite size_ntensor in *; simpl in *; try rewrite Nat.mul_1_r in *)
+ end; try (solve [ type_check ])).
+Set Printing All.
+(simpl_rewrite IP).
 (* Auto-generated comment: Succeeded. *)
 
