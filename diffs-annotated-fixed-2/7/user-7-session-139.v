c8e63eb9674ccd35ca21487e587a9abebeecddef
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:07 -0700

    user 7, session 139, part 17

--- diffs-annotated-fixed-2/7/user-7-session-139.v
+++ diffs-annotated-fixed-2/7/user-7-session-139.v
@@ -59,6 +59,6 @@ Proof.
 (unfold fresh_in_ty, fresh).
 (simpl).
 Search -IdSet.empty.
-(apply IdSetFacts.empty_iff).
+(intros Hcontra; apply IdSetFacts.empty_iff).
 (* Auto-generated comment: Failed. *)
 
