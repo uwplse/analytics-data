aae3742643be8fe82ae297b037efef6e0b293890
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:56 -0700

    user 7, session 88, part 49

--- diffs-annotated-fixed-2/7/user-7-session-88.v
+++ diffs-annotated-fixed-2/7/user-7-session-88.v
@@ -453,6 +453,9 @@ Proof.
 (inversion Hnft; subst).
 (inversion H; subst).
 (simpl in Hdep).
-(apply IHk; try assumption).
+(assert (Hv : value_type (TRef t)) by constructor).
+(assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; constructor)).
+specialize (Hsem _ Hm).
+(simpl in Hsem).
 (* Auto-generated comment: Failed. *)
 
