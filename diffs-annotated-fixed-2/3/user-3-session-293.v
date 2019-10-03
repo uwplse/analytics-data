2a55417ed954130d8a9d6b8ae16e66d854bb983d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:11:32 -0700

    user 3, session 293, part 0

---
+++ diffs-annotated-fixed-2/3/user-3-session-293.v
@@ -0,0 +1,12 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqi6gkwo"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+From Coq Require Import List.
+From Transitions Require Import NonError.
+From RecoveryRefinement Require Export Lib.
+(* Auto-generated comment: Succeeded. *)
+
