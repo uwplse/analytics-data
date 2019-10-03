61f99be6540154abc034cfd55fac979aa1c6f2e4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:12:13 -0700

    user 3, session 9904, part 96

--- diffs-annotated-fixed-2/3/user-3-session-9904.v
+++ diffs-annotated-fixed-2/3/user-3-session-9904.v
@@ -148,7 +148,7 @@ step_proc.
 (match goal with
  | H:pre (match ?a with
           | (x, y) => _
-          end _) |- _ => idtac H y a
+          end _) |- _ => destruct a as [x y]
  end).
 (* Auto-generated comment: Succeeded. *)
 
