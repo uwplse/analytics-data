50f706ee7c88f79ce26ff8c191e6b3a79dc7404d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:11 -0700

    user 1, session 37, part 18

--- diffs-annotated-fixed-2/1/user-1-session-37.v
+++ diffs-annotated-fixed-2/1/user-1-session-37.v
@@ -305,6 +305,6 @@ Definition R (e : evidence) : SetST2 :=
   fun pair =>
   let (T1, T2) := pair in
   static_pred T1 T2 /\
-  In _ (Gamma (fst e)) T1 /\ In _ (Gamma (snd e)) T2.
+  Ensembles.In _ (Gamma (fst e)) T1 /\ In _ (Gamma (snd e)) T2.
 (* Auto-generated comment: Failed. *)
 
