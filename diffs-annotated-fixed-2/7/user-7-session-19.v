3e656d3c3d91d3a958950858082c6b9b9b98a9e7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:58 -0700

    user 7, session 19, part 596

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -403,7 +403,6 @@ constructor.
 {
 subst.
 (simpl).
-SearchPattern (_ <= Nat.max _ _).
 (apply Nat.le_max_l).
 }
 (assert (Hdeple2 : | tb | <= kmax)).
@@ -412,5 +411,62 @@ subst.
 (simpl).
 (apply Nat.le_max_r).
 }
+(unfold sem_sub_i in Hsem).
+(assert (Hm : |-[ kmax] v <$ v) by (apply match_ty_i__reflexive; assumption)).
+specialize (Hsem _ _ Hm).
+(apply match_ty_i_union__inv in Hsem).
+(destruct Hsem; [ left | right ]; unfold sem_sub_i; intros k v' Hm').
+Abort.
+Open Scope btjmdeq_scope.
+Lemma match_ty_deq_cname__inv : forall (v : ty) (c : cname), forall k : nat, |-[ k] v <$ TCName c -> v = TCName c.
+Proof.
+(intros v; induction v; try (solve [ intros c k Hm; destruct k; simpl in Hm; contradiction ])).
+(intros c0 k Hm).
+(destruct k; simpl in Hm; subst; reflexivity).
+Qed.
+Lemma match_ty_deq_pair__inv :
+  forall v t1 t2 : ty, forall k : nat, |-[ k] v <$ TPair t1 t2 -> exists v1 v2 : ty, v = TPair v1 v2 /\ |-[ k] v1 <$ t1 /\ |-[ k] v2 <$ t2.
+Proof.
+(intros v; induction v; try (solve [ intros t1 t2 k Hm; destruct k; simpl in Hm; contradiction ])).
+(intros t1 t2 k Hm).
+exists v1,v2.
+(split; try reflexivity).
+(destruct k; simpl in Hm; assumption).
+Qed.
+Lemma match_ty_deq_union__inv : forall v t1 t2 : ty, forall k : nat, |-[ k] v <$ TUnion t1 t2 -> |-[ k] v <$ t1 \/ |-[ k] v <$ t2.
+Proof.
+(intros v t1 t2 k Hm).
+(destruct k; destruct v; assumption).
+Qed.
+Lemma match_ty_deq_ref__inv :
+  forall v t : ty,
+  forall k : nat,
+  |-[ S k] v <$ TRef t ->
+  exists t' : ty, v = TRef t' /\ inv_depth t <= k /\ inv_depth t' = inv_depth t /\ (forall v' : ty, |-[ k] v' <$ t' <-> |-[ k] v' <$ t).
+Proof.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; simpl in Hm; contradiction ])).
+clear IHv.
+(intros t k).
+(intros Hm).
+(simpl in Hm).
+(destruct Hm as [Hdept [Hdepv Href]]).
+exists v.
+split.
+reflexivity.
+split.
+assumption.
+split.
+assumption.
+(intros v').
+specialize (Href v').
+(destruct Href; split; assumption).
+Qed.
+Close Scope btjmdeq_scope.
+Theorem sem_sub_i__sem_sub_deq : forall t1 t2 : ty, (||- [t1]<= [t2])%btjmi -> (||- [t1]<= [t2])%btjmdeq.
+Proof.
+(intros ta; induction ta; intros tb; induction tb; intros Hsem).
+16: {
+idtac.
+clear IHta.
 (* Auto-generated comment: Failed. *)
 
