7569d637ac63060e5f2ba37f0dd262d734c625a3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:10 -0700

    user 7, session 192, part 65

--- diffs-annotated-fixed-2/7/user-7-session-192.v
+++ diffs-annotated-fixed-2/7/user-7-session-192.v
@@ -143,5 +143,8 @@ assumption.
 (rewrite subst_equation).
 (assert (Hbeq : beq_id X' i = false) by (apply beq_id_false_iff; assumption)).
 (rewrite Hbeq).
+(destruct (IdSet.mem i (FV tx)) eqn:Hmem).
+{
+(remember (gen_fresh (IdSet.union (FV tx) (IdSet.add X' (FV t')))) as z).
 (* Auto-generated comment: Failed. *)
 
