657859800aec8f0a7de146710f3e95d7e02569fc
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:51 -0700

    user 7, session 2, part 47

--- diffs-annotated-fixed-2/7/user-7-session-2.v
+++ diffs-annotated-fixed-2/7/user-7-session-2.v
@@ -161,12 +161,5 @@ generalize dependent t3.
 (inversion Hnf2; subst).
 (inversion H; subst).
 (inversion H0; subst).
-(remember (TRef t') as tx eqn:Heqx ).
-(induction Hsub2; inversion Heqx; subst).
-+
-(apply SR_UnionR1; tauto).
-+
-(apply SR_UnionR2; tauto).
-+
 (* Auto-generated comment: Failed. *)
 
