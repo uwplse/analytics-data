61bab4b14d2ce17ef7db7f7fb6b9cace5da926c9
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:14 -0700

    user 7, session 16, part 63

--- diffs-annotated-fixed-2/7/user-7-session-16.v
+++ diffs-annotated-fixed-2/7/user-7-session-16.v
@@ -370,27 +370,23 @@ Ltac
    try (solve [ left; apply SR_UnionR1; assumption | left; apply SR_UnionR2; assumption ]); right; intros Hcontra;
    apply atom_sub_r_union__inv in Hcontra; tauto || constructor.
 Lemma nf_sub_r__decidable2 :
-  forall t : ty, InNF( t) -> (forall t' : ty, Decidable.decidable (|- t << t')) /\ (forall t' : ty, Decidable.decidable (|- t' << t)).
+  forall t : ty,
+  InNF( t) -> (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t)).
 Proof.
 (apply
   (in_nf_mut
-     (fun (t : ty) (Hat : atom_type t) => (forall t' : ty, Decidable.decidable (|- t << t')) /\ (forall t' : ty, Decidable.decidable (|- t' << t)))
-     (fun (t : ty) (Hnf : in_nf t) => (forall t' : ty, Decidable.decidable (|- t << t')) /\ (forall t' : ty, Decidable.decidable (|- t' << t))))).
+     (fun (t : ty) (Hat : atom_type t) =>
+      (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t)))
+     (fun (t : ty) (Hnf : in_nf t) =>
+      (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t))))).
 -
 (intros c).
-(split; intros t'; induction t'; try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 ]);
+(split; intros t'; induction t'; intros Hnf'; try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 ]);
   try
    match goal with
    | |- Decidable.decidable (|- TCName ?c1 << TCName ?c2) =>
          destruct (cname_eq__decidable c1 c2);
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
-+
-right.
-(intros Hcontra).
-(apply sub_r__mk_nf_sub_r1 in Hcontra).
-(remember (MkNF( TPair t'1 t'2)) as tx eqn:Heqx ; remember (TCName c) as ty eqn:Heqy ).
-(induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]); subst).
-Show 2.
 (* Auto-generated comment: Failed. *)
 
