1eea6fe5b0591a93d1cb11d885d7b3a00086e93f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:26:38 -0700

    user 8, session 79, part 993

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -1039,6 +1039,5 @@ mat_replace \226\136\1631\226\159\169\226\159\1681\226\136\163 with bool_to_matr
 2: (erewrite denote_box_compat).
 3: (erewrite denote_box_compat).
 4: (erewrite kron_compat).
-6: {
 (* Auto-generated comment: Succeeded. *)
 
