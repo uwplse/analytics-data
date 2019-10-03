17728ec736cd6b90ab9cbaf650503bfb3d49afdb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:25 -0700

    user 8, session 79, part 200

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -654,6 +654,6 @@ Proof.
 (intros).
 (simpl).
 specialize (kron_1_l (\226\168\130 l2)) as KL.
-(rewrite kron_1_l_inv).
-(* Auto-generated comment: Failed. *)
+(rewrite <- kron_1_l_inv).
+(* Auto-generated comment: Succeeded. *)
 
