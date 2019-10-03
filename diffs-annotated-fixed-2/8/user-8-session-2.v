51bdb9bf1d7136039b3dc7851c9c504c57b0f699
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:02 -0700

    user 8, session 2, part 154

--- diffs-annotated-fixed-2/8/user-8-session-2.v
+++ diffs-annotated-fixed-2/8/user-8-session-2.v
@@ -135,6 +135,6 @@ Search -fst -snd.
 (rewrite (surjective_pairing (ctrls_to_list (repeat false n) li u))).
 (rewrite <- ctrls_to_list_transpose_fst).
 (destruct (ctrls_to_list (repeat false n) li u) as [[j l] v] eqn:E).
-(rewrite <- ctrl_list_to_unitary_transpose).
-(* Auto-generated comment: Failed. *)
+(simpl).
+(* Auto-generated comment: Succeeded. *)
 
