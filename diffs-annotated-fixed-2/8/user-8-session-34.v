0ab08175cc893b10498d284f57f00d40ccee63f7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:47 -0700

    user 8, session 34, part 54

--- diffs-annotated-fixed-2/8/user-8-session-34.v
+++ diffs-annotated-fixed-2/8/user-8-session-34.v
@@ -85,9 +85,6 @@ replace (size_wtype W1) with \226\159\166 \206\1471 \226\159\167.
 replace (size_wtype W2) with \226\159\166 \206\1472 \226\159\167.
 specialize denote_compose as DC.
 (unfold denote_circuit in DC).
-(erewrite DC with (\206\1471 := [])).
-(simpl).
-(unfold compose_super).
-(rewrite H2, H3).
-(* Auto-generated comment: Succeeded. *)
+(erewrite DC with (\206\1471 := []) (\206\14701 := \226\136\133)).
+(* Auto-generated comment: Failed. *)
 
