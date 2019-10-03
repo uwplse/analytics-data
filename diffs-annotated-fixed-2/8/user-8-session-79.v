45be0b2cfafd5f9baf3c841ee96efb8c88f81f2f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:48 -0700

    user 8, session 79, part 451

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -964,6 +964,6 @@ Timeout 1 Print Ltac rewrite_inPar''.
         rewrite size_ntensor in *; simpl in *; try rewrite Nat.mul_1_r in *
  end; try (solve [ type_check ])).
 show_dimensions.
-injection L1.
+injection L1 as L3.
 (* Auto-generated comment: Succeeded. *)
 
