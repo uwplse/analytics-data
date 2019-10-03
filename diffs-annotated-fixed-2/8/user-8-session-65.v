2a45b429da0b6f3068d13e25dd724f60947c7d21
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:24:33 -0700

    user 8, session 65, part 160

--- diffs-annotated-fixed-2/8/user-8-session-65.v
+++ diffs-annotated-fixed-2/8/user-8-session-65.v
@@ -981,7 +981,6 @@ Msimpl.
 mat_replace \226\159\1680\226\136\163 \195\151 \226\136\1631\226\159\169 with @Zero 1 1 by lma.
 mat_replace \226\159\1681\226\136\163 \195\151 \226\136\1631\226\159\169 with I 1 by lma.
 Msimpl.
-Timeout 1 About id_kron.
-Timeout 1 Print id_kron.
-(* Auto-generated comment: Succeeded. *)
+(rewrite id_kron' by (apply Nat.pow_nonzero; lia)).
+(* Auto-generated comment: Failed. *)
 
