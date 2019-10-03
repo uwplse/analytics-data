14d20af3b3abcf5e18d207ab976c54bc6f0acdb1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:55 -0700

    user 7, session 2, part 107

--- diffs-annotated-fixed-2/7/user-7-session-2.v
+++ diffs-annotated-fixed-2/7/user-7-session-2.v
@@ -127,13 +127,28 @@ Qed.
 Lemma sub_r_unite_pairs_l__inv :
   forall t1 t2 t1' t2' : ty, |- unite_pairs t1 t2 << TPair t1' t2' -> InNF( t1) -> InNF( t2) -> |- t1 << t1' /\ |- t2 << t2'.
 Proof.
-(intros t1; induction t1; intros t2; induction t2).
-(intros t1' t2' Hsub; intros Hnf1 Hnf2).
+(intros t1; induction t1; intros t2; induction t2; intros t1' t2' Hsub; intros Hnf1 Hnf2).
 -
 (match goal with
  | Hsub:|- ?t1 << ?t2
    |- _ => remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; induction Hsub; inversion Heqx; inversion Heqy; subst; tauto
  end).
 -
+(simpl in Hsub).
+(remember (TPair (TCName c) (TPair t2_1 t2_2)) as tx eqn:Heqx ).
+(remember (TPair t1' t2') as ty eqn:Heqy ).
+(induction Hsub; inversion Heqx; inversion Heqy; subst).
+tauto.
+(inversion Hnf1; subst).
+(inversion Hnf2; subst).
+(assert (Hnf : InNF( TPair (TCName c) (TPair t2_1 t2_2))) by (do 2 constructor; assumption)).
+(rewrite (mk_nf_nf__equal _ Hnf) in IHHsub).
+tauto.
+-
+(rewrite unite_pairs_t_union in Hsub; try resolve_not_union).
+(apply sub_r_nf_union_l__inv in Hsub).
+(destruct Hsub as [Hsub1 Hsub2]).
+specialize (IHt2_1 _ _ Hsub1).
+specialize (IHt2_2 _ _ Hsub2).
 (* Auto-generated comment: Failed. *)
 
