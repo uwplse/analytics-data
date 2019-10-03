86a5481ff59ea9bd550731de6ef0f56fcf6f032c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:18:42 -0700

    user 3, session 960, part 69

--- diffs-annotated-fixed-2/3/user-3-session-960.v
+++ diffs-annotated-fixed-2/3/user-3-session-960.v
@@ -70,7 +70,7 @@ Search -"endian".
 Opaque Nat.modulo Nat.div.
 #[local]Obligation Tactic := (intros; simpl; subst).
 #[program]
-Fixpoint nat_to_le base (x : nat) {measure x :
+Fixpoint nat_to_le base (x : nat) {measure x lt} :
 list {x : nat | x < S (S base)} :=
   match x with
   | 0 => nil
@@ -78,31 +78,5 @@ list {x : nat | x < S (S base)} :=
       let digit := x mod S (S base) in
       exist _ digit _ :: nat_to_le base (x / S (S base))
   end.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqpvVnr7"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqtn7sMw"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
-Next Obligation of nat_to_le_func.
-Proof.
-(apply PeanoNat.Nat.mod_upper_bound; auto).
-Qed.
-Next Obligation of nat_to_le_func.
-Proof.
-subst digit.
-(apply PeanoNat.Nat.div_lt; auto; try lia).
-Qed.
-Next Obligation of nat_to_le_func.
-Proof.
-lia.
-Qed.
-Next Obligation of nat_to_le_func.
-Proof.
-Search -wf.
 (* Auto-generated comment: Succeeded. *)
 
