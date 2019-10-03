f25d8b0bf07ffe9482ff48901ce80eed047cb462
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:28 -0700

    user 7, session 212, part 9

--- diffs-annotated-fixed-2/7/user-7-session-212.v
+++ diffs-annotated-fixed-2/7/user-7-session-212.v
@@ -94,6 +94,7 @@ assumption.
 Qed.
 Lemma f_free_in_ty_pair__inv : forall (X : id) (t1 t2 : ty), f_free_in_ty X (TPair t1 t2) -> f_free_in_ty X t1 \/ f_free_in_ty X t2.
 Proof.
+(intros X t1 t2).
 (unfold f_free_in_ty).
 (simpl).
 (apply free_union__inv).
