df48fd429760cb08eac541a7f4c2b7d90f0ab7bf
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:55 -0700

    user 7, session 88, part 31

--- diffs-annotated-fixed-2/7/user-7-session-88.v
+++ diffs-annotated-fixed-2/7/user-7-session-88.v
@@ -423,7 +423,8 @@ Proof.
 -
 ((assert (Hv : value_type (TCName c)) by constructor; pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ Hsem) as Hsemu;
    destruct Hsemu as [Hsemu| Hsemu];
-   [ destruct Hdep as [Hdept| Hdept']; try destruct (max_inv_depth_le__inv _ _ _ Hdept') as [Hdept'1 Hdept'2]; apply Nat.le_trans with (| t'1 |)
+   [ destruct Hdep as [Hdept| Hdept']; try destruct (max_inv_depth_le__inv _ _ _ Hdept') as [Hdept'1 Hdept'2]; apply Nat.le_trans with (| t'1 |);
+      tauto || apply Max.le_max_l
    |  ]) + destruct Hdep as [Hdept| Hdept']).
 (* Auto-generated comment: Failed. *)
 
