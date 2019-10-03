8060c45345a3f8b9b0d133329251cac8767e1333
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:51 -0700

    user 8, session 37, part 22

--- diffs-annotated-fixed-2/8/user-8-session-37.v
+++ diffs-annotated-fixed-2/8/user-8-session-37.v
@@ -61,6 +61,5 @@ replace (gate g p c) with compose (gate g p (fun p' => output p')) c by auto.
 dependent destruction H'.
 (destruct \206\1471 as [| \206\1471]; try invalid_contradiction).
 (erewrite denote_compose with (\206\1471 := []); trivial).
-Focus 3.
 (* Auto-generated comment: Succeeded. *)
 
