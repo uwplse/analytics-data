a4053652e4100798c282da6f75b26969716742bb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:24 -0700

    user 8, session 79, part 186

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -594,6 +594,10 @@ Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqRShYoE"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Lemma mat_equiv_equiv' : @mat_equiv = mat_equiv'.
-Proof.
+reflexivity.
+Qed.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqlTTxBw"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
 (* Auto-generated comment: Succeeded. *)
 
