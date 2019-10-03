aef52eafad9c299a74d947ed880c006780260a83
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:44:58 -0700

    user 3, session 3788, part 18

--- diffs-annotated-fixed-2/3/user-3-session-3788.v
+++ diffs-annotated-fixed-2/3/user-3-session-3788.v
@@ -32,28 +32,7 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoPhort"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Definition change_marker `{m : @ProcMarker T p} {T'} 
+Definition change_marker `{m : ProcMarker p} {T'} 
   (p' : proc T') : ProcMarker p' := AProc p'.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqXrGjSU"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqpGp7Ob"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Theorem swapXY_ok :
-  proc_spec
-    (fun (_ : unit) state =>
-     {|
-     pre := True;
-     post := fun r state' =>
-             state' = mkState (StateY state) (StateX state) (StateZ state);
-     recovered := fun _ state' => True |}) swapXY vars.recover vars.abstr.
-Proof.
-(match goal with
- | |- proc_spec _ ?p _ _ => pose proof (AProc p) as Hbefore
- end).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
