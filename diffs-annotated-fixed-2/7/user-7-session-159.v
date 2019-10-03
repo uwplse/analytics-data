810558baabd4ab42f09d9d14e51c87ec6c7ebe6b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:24 -0700

    user 7, session 159, part 6

--- diffs-annotated-fixed-2/7/user-7-session-159.v
+++ diffs-annotated-fixed-2/7/user-7-session-159.v
@@ -68,6 +68,9 @@ subst.
 (apply match_ty_exist__inv in Hm).
 (destruct Hm as [ti Hm]).
 (rewrite subst_neq__permute in Hm).
-symmetry in Hbeq.
+specialize (IHw _ _ Hm).
+(destruct IHw as [v' Hm']).
+exists v'.
+(apply match_ty_exist).
 (* Auto-generated comment: Failed. *)
 
