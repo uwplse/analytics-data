42817094c3834616f2047039ea4075b3ed2669ff
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:19:14 -0700

    user 3, session 960, part 270

--- diffs-annotated-fixed-2/3/user-3-session-960.v
+++ diffs-annotated-fixed-2/3/user-3-session-960.v
@@ -276,6 +276,6 @@ typeclasses eauto.
 (unfold EqualDec).
 (intros; simpl).
 (destruct x, y; simpl).
-(destruct (nat_eq_dec n n0)).
+(destruct (nat_eq_dec n n0); subst).
 (* Auto-generated comment: Succeeded. *)
 
