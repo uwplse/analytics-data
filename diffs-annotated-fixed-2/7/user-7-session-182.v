f258e7f4d4a80b411476cfeb356cee11a1910594
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:57 -0700

    user 7, session 182, part 19

--- diffs-annotated-fixed-2/7/user-7-session-182.v
+++ diffs-annotated-fixed-2/7/user-7-session-182.v
@@ -81,6 +81,7 @@ assumption.
 (destruct Hm as [ti Hm]).
 (destruct (IdSetProps.In_dec i (FV tx)) as [Hin| Hin]).
 {
-(pose proof (IdSetFacts.mem_1 Hin) as Hmem).
+(rewrite subst_exist_neq).
+Print fresh_in_ty.
 (* Auto-generated comment: Failed. *)
 
