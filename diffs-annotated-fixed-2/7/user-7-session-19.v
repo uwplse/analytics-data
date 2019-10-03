de405ca2513cc527c37f018dc35abe9d2a83b6c5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:46 -0700

    user 7, session 19, part 405

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -200,7 +200,9 @@ Proof.
             destruct Hsemu as [Hsemu| Hsemu]; [ apply Nat.le_trans with (| t'1 |) | apply Nat.le_trans with (| t'2 |) ];
             tauto || apply Max.le_max_l || apply Max.le_max_r
      | Hsem:||-[ ?k][TPair ?t1 ?t2]<= [?t']
-       |- _ <= | ?t' | => assert (Hvp : value_type (TPair t1 t2)) by (apply in_nf_pair__value_type; assumption)
+       |- _ <= | ?t' | =>
+           assert (Hvp : value_type (TPair t1 t2)) by (apply in_nf_pair__value_type; assumption);
+            assert (Hmp : |-[ k] TPair t1 t2 <$ TPair t1 t2) by (apply match_ty_i__reflexive; assumption)
      end ])).
 (* Auto-generated comment: Failed. *)
 
