d5b29816190cc4b90bdfcb4380c50c0c6962da04
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:24:27 -0700

    user 8, session 65, part 89

--- diffs-annotated-fixed-2/8/user-8-session-65.v
+++ diffs-annotated-fixed-2/8/user-8-session-65.v
@@ -972,8 +972,6 @@ restore_dims try rewrite size_ntensor; unify_pows_two; simpl; try lia.
 (destruct b; simpl; Msimpl).
 (repeat rewrite Mmult_assoc).
 Msimpl.
-(match goal with
- | |- context [ ?A \226\138\151 ?B \195\151 (?C \226\138\151 ?D) ] => setoid_rewrite (kron_mixed_product A B C D)
- end).
-(* Auto-generated comment: Failed. *)
+show_dimensions.
+(* Auto-generated comment: Succeeded. *)
 
