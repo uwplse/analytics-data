11a18744c4612e4afec63f231dd727fb6fd7ba79
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:55 -0700

    user 8, session 37, part 73

--- diffs-annotated-fixed-2/8/user-8-session-37.v
+++ diffs-annotated-fixed-2/8/user-8-session-37.v
@@ -436,6 +436,5 @@ Lemma valid_denote_true :
 Proof.
 (intros W W' c \207\129 \207\129' safe T H D).
 (destruct safe; trivial).
-(rewrite <- H; assumption).
 (* Auto-generated comment: Succeeded. *)
 
