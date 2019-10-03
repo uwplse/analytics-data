0b62a67181dc78cbe7f036a90a8ca1c0be0104db
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:42 -0700

    user 7, session 19, part 330

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -147,6 +147,8 @@ Lemma value_sem_sub_k_i_union__inv :
 Proof.
 (intros v Hv k ta tb Hsem; unfold sem_sub_k_i in Hsem).
 (assert (Hm : |-[ k] v <$ v) by (apply match_ty_i__reflexive; assumption)).
-specialize (Hsem _ Hv Hm).
-(* Auto-generated comment: Succeeded. *)
+specialize (Hsem _ Hm).
+(apply match_ty_i_union__inv in Hsem).
+(destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hv' Hm'; apply match_ty_i__transitive_on_value_type with v; assumption).
+(* Auto-generated comment: Failed. *)
 
