f8a82b4eab2bce93ce4424a61db0fd72ef79386e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:53 -0700

    user 7, session 19, part 518

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -270,6 +270,6 @@ Proof.
 SearchPattern (_ <= _ -> _ <= _ -> _ = _).
 Check le_unique.
 SearchPattern (_ <= _ -> _ = _).
-(apply Nat.le_antisymm).
+(apply Nat.le_antisymm; assumption).
 (* Auto-generated comment: Failed. *)
 
