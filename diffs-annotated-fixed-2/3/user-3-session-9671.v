155cc156cef081a12a539ec944ea50c7af42a748
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:11:22 -0700

    user 3, session 9671, part 34

--- diffs-annotated-fixed-2/3/user-3-session-9671.v
+++ diffs-annotated-fixed-2/3/user-3-session-9671.v
@@ -247,17 +247,6 @@ step.
 (pose proof (H3 a); intuition).
 (assert (log_addr a < diskSize state)).
 {
-eauto.
-}
-eq_values.
-auto.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect
-"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqn7pS5Y"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Qed.
+eauto using log_size_bounds.
 (* Auto-generated comment: Succeeded. *)
 
