0da79586ed74c03b71227b5414d665bca71ef7d8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:28 -0700

    user 7, session 104, part 86

--- diffs-annotated-fixed-2/7/user-7-session-104.v
+++ diffs-annotated-fixed-2/7/user-7-session-104.v
@@ -60,7 +60,7 @@ clear Hm.
 (inversion Heq; subst).
 clear Heq.
 (destruct Hcontra as [Hcontra _]).
-(assert (Hm : |-[ 1, 0] tint <$ tint) by (apply match_ty_value_type__reflexive; constructor)).
-Check Hcontra.
+(assert (Hm : |-[ 0, 0] tint <$ tint) by (apply match_ty_value_type__reflexive; constructor)).
+specialize (Hcontra _ Hm).
 (* Auto-generated comment: Failed. *)
 
