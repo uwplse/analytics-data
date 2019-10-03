e8e4f8e9bd7c24bdc1a5e7e80250465f8ff9aa46
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:29 -0700

    user 7, session 214, part 3

--- diffs-annotated-fixed-2/7/user-7-session-214.v
+++ diffs-annotated-fixed-2/7/user-7-session-214.v
@@ -248,6 +248,6 @@ Proof.
 (induction t; try (solve [ simpl; assumption ])).
 -
 (rewrite b_subst_pair).
-(destruct (f_free_in_ty_pair__inv _ _ _ HX) as [HXt| HXt]).
+(unfold f_free_in_ty).
 (* Auto-generated comment: Failed. *)
 
