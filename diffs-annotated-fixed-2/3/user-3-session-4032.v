6edd08d0504e793d0711c3a9c44587308f6d42c5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:47:27 -0700

    user 3, session 4032, part 9

--- diffs-annotated-fixed-2/3/user-3-session-4032.v
+++ diffs-annotated-fixed-2/3/user-3-session-4032.v
@@ -319,5 +319,12 @@ Timeout 1 Print LoadPath.
 Check file_truncate.
 Check file_set_sz.
 Anomaly "No printing rule found for "If _ _ else _"." Please report at http://coq.inria.fr/bugs/.
-(* Auto-generated comment: Failed. *)
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqYYZafd" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq6ls6Tj" SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+(* Auto-generated comment: Succeeded. *)
 
