3018ba47c5d85062a4c000804441aa475d6f7391
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:03 -0700

    user 7, session 19, part 676

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -469,16 +469,14 @@ Lemma pair_sem_sub_k_i__sub_d :
   forall k : nat,
   forall ta1 ta2 : ty,
   atom_type (TPair ta1 ta2) ->
-  | TPair ta1 ta2 | <= k ->
   (forall tb1 : ty, ||-[ k][ta1]<= [tb1] -> |- ta1 << tb1) ->
   (forall tb2 : ty, ||-[ k][ta2]<= [tb2] -> |- ta2 << tb2) -> forall t2 : ty, ||-[ k][TPair ta1 ta2]<= [t2] -> |- TPair ta1 ta2 << t2.
 Proof.
-(intros k ta1 ta2 Hat Hdep IH1 IH2).
+(intros k ta1 ta2 Hat IH1 IH2).
 (pose proof (atom_type__value_type _ Hat) as Hva).
 (assert (Hma : |-[ k] TPair ta1 ta2 <$ TPair ta1 ta2) by (apply match_ty_i__reflexive; assumption)).
 (induction t2; intros Hsem; try (solve [ specialize (Hsem _ Hma); destruct k; simpl in Hsem; subst; constructor || contradiction ])).
 -
-Check sem_sub_k_i_pair__inv.
 (destruct (sem_sub_k_i_pair__inv _ _ _ _ _ Hsem) as [Hsem1 Hsem2]).
 (constructor; [ apply IH1 | apply IH2 ]; tauto).
 -
