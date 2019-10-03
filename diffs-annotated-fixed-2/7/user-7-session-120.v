db518be886750122ed7dedb72a65f7e847c84f90
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:55 -0700

    user 7, session 120, part 3

--- diffs-annotated-fixed-2/7/user-7-session-120.v
+++ diffs-annotated-fixed-2/7/user-7-session-120.v
@@ -19,6 +19,7 @@ Proof.
 (destruct Hsem1 as [w21 Hsem1]; destruct Hsem2 as [w22 Hsem2]).
 exists (Nat.max w21 w22).
 (intros v Hm).
-(apply match_ty_pair__inv in Hm; subst).
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]).
 (* Auto-generated comment: Failed. *)
 
