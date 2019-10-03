ac783b0934f8e1bf2977f9f4e8e4ca536278d56f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:27:51 -0700

    user 8, session 125, part 129

--- diffs-annotated-fixed-2/8/user-8-session-125.v
+++ diffs-annotated-fixed-2/8/user-8-session-125.v
@@ -84,5 +84,12 @@ Msimpl.
 specialize (denote_ctrls_unitary W (\226\159\166 W \226\159\167) U li) as inv.
 replace (size_wtype W1 + size_wtype W2)%nat with \226\159\166 W \226\159\167 by (subst; easy).
 (unfold apply_U, apply_unitary, super).
-(* Auto-generated comment: Succeeded. *)
+(destruct W; try (solve [ inversion HeqW ])).
+(rewrite denote_ctrls_transpose).
+(remember (denote_ctrls (\226\159\166 W3 \226\138\151 W4 \226\159\167) U li) as A).
+(remember (swap_list (\226\159\166 W3 \226\138\151 W4 \226\159\167) li) as S).
+(rewrite <- (Mmult_assoc _ (A \195\151 \207\129) _)).
+(rewrite <- (Mmult_assoc _ A \207\129)).
+(rewrite inv).
+(* Auto-generated comment: Failed. *)
 
