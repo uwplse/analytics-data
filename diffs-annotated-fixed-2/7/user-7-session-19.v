accba1a44052c31605dc54a438fce97d5d27a57c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:27 -0700

    user 7, session 19, part 73

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -117,6 +117,7 @@ clear IHk' IHt.
 (simpl in Htk, Htk').
 (apply le_S_n in Htk).
 (apply le_S_n in Htk').
-(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v Hv; specialize (Href v Hv)).
+(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v Hv; specialize (Href v Hv); specialize
+  (IHk k' t')).
 (* Auto-generated comment: Failed. *)
 
