2bb75512ef842e9733235474f58206cdd401058d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:26 -0700

    user 7, session 104, part 51

--- diffs-annotated-fixed-2/7/user-7-session-104.v
+++ diffs-annotated-fixed-2/7/user-7-session-104.v
@@ -63,20 +63,5 @@ Lemma not__ref_t_match_ty_t : forall (k : nat) (t : ty), | t | <= k -> forall w 
 Proof.
 (induction k).
 -
-(induction t).
-+
-(intros Hdep w Hcontra).
-(apply match_ty_cname__inv in Hcontra).
-(inversion Hcontra).
-+
-(intros Hdep w Hcontra).
-(apply match_ty_pair__inv in Hcontra).
-(destruct Hcontra as [v1 [v2 [Heq _]]]).
-(inversion Heq).
-+
-(intros Hdep w Hcontra).
-(apply match_ty_union__inv in Hcontra).
-(destruct Hcontra as [Hcontra| Hcontra]).
-*
 (* Auto-generated comment: Failed. *)
 
