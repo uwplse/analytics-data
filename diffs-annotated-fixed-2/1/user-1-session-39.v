deda0bcd8a65ee016b58e0d44384caeba66f3052
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:17 -0700

    user 1, session 39, part 0

---
+++ diffs-annotated-fixed-2/1/user-1-session-39.v
@@ -0,0 +1,11 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Set Bullet Behavior
+  "Strict Subproofs".
+Require Import
+  Coq.Sets.Powerset_facts.
+Require Import library.
+(* Auto-generated comment: Failed. *)
+
