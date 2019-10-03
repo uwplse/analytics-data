082554e72ab45436997e8017dcc943165050132c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:27 -0700

    user 7, session 104, part 76

--- diffs-annotated-fixed-2/7/user-7-session-104.v
+++ diffs-annotated-fixed-2/7/user-7-session-104.v
@@ -53,6 +53,7 @@ Proof.
 specialize (Hcontra 1 0).
 (destruct Hcontra as [w2 Hcontra]).
 (assert (Hm : |-[ 1, 0] TRef tint <$ TRef tint) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Hcontra Hm).
+specialize (Hcontra _ Hm).
+(apply macth_ty_cname__inv in hcontra).
 (* Auto-generated comment: Failed. *)
 
