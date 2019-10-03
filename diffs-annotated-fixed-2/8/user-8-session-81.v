f049e1116b555b86c8f693e1b5f5823c89f61832
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:27:26 -0700

    user 8, session 81, part 10

--- diffs-annotated-fixed-2/8/user-8-session-81.v
+++ diffs-annotated-fixed-2/8/user-8-session-81.v
@@ -727,6 +727,6 @@ Timeout 1 Print Grammar tactic.
 (repeat rewrite Nat.mul_1_l).
 (rewrite (kron_1_r A)).
 (rewrite (kron_1_r B)).
-(apply H).
-(* Auto-generated comment: Failed. *)
+(repeat rewrite <- Nat.mul_1_l).
+(* Auto-generated comment: Succeeded. *)
 
