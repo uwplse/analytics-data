765ffce5a062f6010f5ae56cd5bcf0c367fb3466
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:03 -0700

    user 8, session 2, part 163

--- diffs-annotated-fixed-2/8/user-8-session-2.v
+++ diffs-annotated-fixed-2/8/user-8-session-2.v
@@ -136,5 +136,6 @@ Search -fst -snd.
 (rewrite <- ctrls_to_list_transpose_fst).
 (destruct (ctrls_to_list (repeat false n) li u) as [[j l] v] eqn:E).
 Opaque rev.
+(simpl).
 (* Auto-generated comment: Succeeded. *)
 
