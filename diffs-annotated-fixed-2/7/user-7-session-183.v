6c7f6e4bdd87687272e78ae7473c98ca62787cb8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:59 -0700

    user 7, session 183, part 7

--- diffs-annotated-fixed-2/7/user-7-session-183.v
+++ diffs-annotated-fixed-2/7/user-7-session-183.v
@@ -98,6 +98,10 @@ Proof.
 -
 subst.
 exists Y,t.
-(rewrite subst_exist_eq).
+(apply subst_exist_eq).
+-
+(destruct (IdSetProps.In_dec Y (FV s)) as [Hin| Hin]).
++
+(rewrite subst_equation).
 (* Auto-generated comment: Failed. *)
 
