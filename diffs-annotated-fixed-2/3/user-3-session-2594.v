8beb0deef19f22a8c0cffc852b8dfc682443a53a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:31:44 -0700

    user 3, session 2594, part 4

--- diffs-annotated-fixed-2/3/user-3-session-2594.v
+++ diffs-annotated-fixed-2/3/user-3-session-2594.v
@@ -6,5 +6,19 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import FunInd Recdef.
-(* Auto-generated comment: Failed. *)
+From Coq Require Import PeanoNat.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqPFg0QW"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqbXPA9y"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+From Armada Require Import Goose.Machine.
+Opaque Nat.modulo Nat.div.
+#[local]Obligation Tactic := (intros; simpl; subst).
+(* Auto-generated comment: Succeeded. *)
 
