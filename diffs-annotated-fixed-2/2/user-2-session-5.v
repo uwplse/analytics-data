8a317142a78c4ea1a031913429963889a56e2c4e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:49 -0700

    user 2, session 5, part 46

--- diffs-annotated-fixed-2/2/user-2-session-5.v
+++ diffs-annotated-fixed-2/2/user-2-session-5.v
@@ -53,8 +53,5 @@ Check
 Redirect "/tmp/coqLRo0nb" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
-Fixpoint subst (x : string) (u : term) (t : term) : term := t.
-Redirect "/tmp/coqVl2ZnQ" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
