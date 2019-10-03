a297fa03e494ace37b8167eab9507aadf0d72e0c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:50 -0700

    user 2, session 5, part 52

--- diffs-annotated-fixed-2/2/user-2-session-5.v
+++ diffs-annotated-fixed-2/2/user-2-session-5.v
@@ -51,35 +51,5 @@ Check
   [<Nil <Nil "hi">> (Cons (Ident "1") (Ident "2")) (Ident "a")
   {(Ident "myfun") (Ident "somArg")}].
 Fixpoint subst (x : string) (u : term) (t : term) : term := t.
-Fixpoint step (t : term) : term :=
-  match t with
-  | Nil => Nil
-  | Ident _ => Nil
-  | <a b> => if value a then <a (step b)> else <(step a) b>
-  | {f a} =>
-      if value f
-      then
-       match f with
-       | [(Ident lam) (Ident x) body] => if value a then subst x a body else {f (step a)}
-       | Ident prim =>
-           if String.eqb prim "if"
-           then
-            match a with
-            | [cond bThen bElse] =>
-                if value cond
-                then match cond with
-                     | Nil => bElse
-                     | _ => bThen
-                     end
-                else {"if" [(step cond) bThen bElse]}
-            | _ => Nil
-            end
-           else Nil
-       | _ => Nil
-       end
-      else {(step f) a}
-  end.
-Redirect "/tmp/coqio6rsl" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
 (* Auto-generated comment: Succeeded. *)
 
