75f18e835ed9b704f919268278d4cf25a0e3108b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:56 -0700

    user 3, session 1, part 4

--- diffs-annotated-fixed-2/3/user-3-session-1.v
+++ diffs-annotated-fixed-2/3/user-3-session-1.v
@@ -454,5 +454,7 @@ Theorem spec_exec_impl `(p_hspec : Specification T unit State)
   spec_exec p_rspec ---> spec_exec p_hspec.
 Proof.
 firstorder.
-(* Auto-generated comment: Failed. *)
+(intros).
+(apply impl_spec_exec).
+(* Auto-generated comment: Succeeded. *)
 
