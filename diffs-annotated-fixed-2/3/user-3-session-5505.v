c9e3b7c197f160b4a903ea8978fcfb83eeb73460
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:58:13 -0700

    user 3, session 5505, part 4

--- diffs-annotated-fixed-2/3/user-3-session-5505.v
+++ diffs-annotated-fixed-2/3/user-3-session-5505.v
@@ -70,16 +70,5 @@ intuition eauto.
 (simpl write).
 step_proc.
 intuition eauto.
-+
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqEM7AbS"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqyjemv2"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
 (* Auto-generated comment: Succeeded. *)
 
