9dbcdb56e24ca2f9b27e452c2d6f6ad53029ab52
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:31 -0700

    user 8, session 31, part 47

--- diffs-annotated-fixed-2/8/user-8-session-31.v
+++ diffs-annotated-fixed-2/8/user-8-session-31.v
@@ -2759,6 +2759,5 @@ replace (size_ctx \206\147) with size_octx \206\147 by easy.
 (rewrite size_octx_merge by easy).
 (simpl_rewrite (octx_wtype_size W p1 \206\1471 t)).
 (apply compose_super_correct).
-*
 (* Auto-generated comment: Succeeded. *)
 
