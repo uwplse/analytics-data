c8c55794f30f265a9272288285f059a1206ad19d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:12 -0700

    user 7, session 194, part 9

--- diffs-annotated-fixed-2/7/user-7-session-194.v
+++ diffs-annotated-fixed-2/7/user-7-session-194.v
@@ -40,19 +40,14 @@ Proof.
 (unfold not_free in *).
 (split; intros Hcontra; [ apply (IdSetFacts.union_2 fvs2) in Hcontra | apply (IdSetFacts.union_3 fvs1) in Hcontra ]; contradiction).
 Qed.
+Ltac solve_not_free fvname := intros X t1 t2 Hfresh; unfold fvname in *; simpl in Hfresh; simpl; apply not_free_union__inv in Hfresh; assumption.
 Lemma not_f_free_in_ty_pair__inv : forall (X : id) (t1 t2 : ty), not_f_free_in_ty X (TPair t1 t2) -> not_f_free_in_ty X t1 /\ not_f_free_in_ty X t2.
 Proof.
-(intros X t1 t2 Hfresh).
-(unfold not_f_free_in_ty in *; simpl in Hfresh; simpl).
-(apply not_free_union__inv in Hfresh).
-assumption.
+(solve_not_free not_f_free_in_ty).
 Qed.
 Lemma not_b_free_in_ty_pair__inv : forall (X : id) (t1 t2 : ty), not_b_free_in_ty X (TPair t1 t2) -> not_b_free_in_ty X t1 /\ not_b_free_in_ty X t2.
 Proof.
-(intros X t1 t2 Hfresh).
-(unfold not_b_free_in_ty in *; simpl in Hfresh; simpl).
-(apply not_free_union__inv in Hfresh).
-assumption.
+(solve_not_free not_b_free_in_ty).
 Qed.
 (* Auto-generated comment: Failed. *)
 
