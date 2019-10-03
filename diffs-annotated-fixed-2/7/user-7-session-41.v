c01e6dd1fc761919da54b519d4272c2cc91280c7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:24 -0700

    user 7, session 41, part 1

--- diffs-annotated-fixed-2/7/user-7-session-41.v
+++ diffs-annotated-fixed-2/7/user-7-session-41.v
@@ -11,6 +11,5 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
