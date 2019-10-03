16801d9f9d79869ed4c5bfc836a9c1925117c778
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:28:10 -0700

    user 8, session 158, part 7

--- diffs-annotated-fixed-2/8/user-8-session-158.v
+++ diffs-annotated-fixed-2/8/user-8-session-158.v
@@ -335,5 +335,10 @@ Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 3: {
+econstructor.
+3: eauto.
+3: eauto.
+2: monoid.
+validate.
 (* Auto-generated comment: Succeeded. *)
 
