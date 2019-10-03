334e1e618a77906b7734ba1b83a2e522644ef7a2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:17 -0700

    user 7, session 152, part 2

--- diffs-annotated-fixed-2/7/user-7-session-152.v
+++ diffs-annotated-fixed-2/7/user-7-session-152.v
@@ -59,7 +59,7 @@ exists w2.
 Qed.
 Lemma sem_sub_k_union_2 : forall t t1' t2' : ty, ||- [t]<= [t2'] -> ||- [t]<= [TUnion t1' t2'].
 Proof.
-(intros t1' t2' Hsem).
+(intros w1 t1' t2' Hsem).
 (intros w1).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
