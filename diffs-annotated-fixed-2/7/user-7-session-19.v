9bdb33d750ac3d7d27762e077ffed61f9a9b9cd1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:55 -0700

    user 7, session 19, part 555

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -378,5 +378,6 @@ clear IHk' IHt.
 *
 (assert (Hdepeq : | t' | = | t |) by apply (sem_eq_k_i__inv_depth_eq_2 _ _ _ Htk Href)).
 (pose proof Htk as Ht'k; pose proof Htk' as Ht'k'; rewrite <- Hdepeq in Ht'k, Ht'k'; pose proof (IHk k' t Htk Htk' v) as Ht).
+(pose proof (IHk k' t' Ht'k Ht'k' v) as Ht').
 (* Auto-generated comment: Failed. *)
 
