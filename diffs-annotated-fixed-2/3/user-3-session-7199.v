38472f23b590346fd1d43c7a621d0887adf6251e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:41 -0700

    user 3, session 7199, part 117

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -319,6 +319,10 @@ Lemma firstn_one_more :
 Proof.
 (intros).
 generalize dependent a.
-(induction d; simpl in *).
+(induction d; simpl; intros).
+-
+(exfalso; lia).
+-
+(simpl).
 (* Auto-generated comment: Succeeded. *)
 
