3cb2be07d922a328f40ad92ef0ddae76dfd477c2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:54 -0700

    user 7, session 19, part 534

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -271,7 +271,7 @@ Proof.
             destruct (sem_sub_k_union_l__inv _ _ _ _ Hsem) as [HSem1 Hsem2]; destruct (in_nf_union__inv _ _ Hnft) as [Hnft1 Hnft2];
             rewrite inv_depth_union; apply Nat.max_lub; auto
      end ])).
-(destruct (max_inv_depth_le__components_le _ _ _ Hdept') as [Hdept'1 Hdept'2]; destruct (sem_sub_k_union_l__inv _ _ _ _ Hsem) as [HSem1 Hsem2];
-  destruct (in_nf_union__inv _ _ Hnft) as [Hnft1 Hnft2]).
+(destruct (sem_sub_k_union_l__inv _ _ _ _ Hsem) as [HSem1 Hsem2]; destruct (in_nf_union__inv _ _ Hnft) as [Hnft1 Hnft2]).
+(rewrite inv_depth_union; apply Nat.max_lub; auto).
 (* Auto-generated comment: Failed. *)
 
