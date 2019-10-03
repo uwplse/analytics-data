75c02e4fa0b29663510401c78c1b52edc1fc9a61
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:10 -0700

    user 7, session 98, part 65

--- diffs-annotated-fixed-2/7/user-7-session-98.v
+++ diffs-annotated-fixed-2/7/user-7-session-98.v
@@ -165,6 +165,6 @@ clear Hm.
 specialize (Hsem' (TRef tx)).
 (destruct Hsem' as [Hsem'1 Hsem'2]).
 specialize (Hsem'2 Hm).
-(simpl in Hsem'2).
+(apply match_ty_exist_inv in Hsem'2).
 (* Auto-generated comment: Failed. *)
 
