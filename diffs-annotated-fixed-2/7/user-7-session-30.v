8029231d90e71c2b392ccd1ce29f060bf9e9a2b8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:22 -0700

    user 7, session 30, part 29

--- diffs-annotated-fixed-2/7/user-7-session-30.v
+++ diffs-annotated-fixed-2/7/user-7-session-30.v
@@ -97,5 +97,16 @@ split.
 (inversion Hm).
 -
 (intros t; induction t; intros k Hm; try (solve [ destruct k; contradiction | solve_match_ty__inv_depth_l__union_r IHt1 IHt2 ])).
++
+clear IHt.
+(destruct k).
+contradiction.
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [t' [Heqt' [[Hdept Hdept'] _]]]).
+(inversion Heqt'; subst).
+(simpl).
+(rewrite Hdept').
+(split; apply le_n_S; assumption || constructor).
+Qed.
 (* Auto-generated comment: Failed. *)
 
