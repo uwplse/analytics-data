8ccdddb0e80d126f62d890d18f8796f8f75d5e6a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:49 -0700

    user 7, session 116, part 6

--- diffs-annotated-fixed-2/7/user-7-session-116.v
+++ diffs-annotated-fixed-2/7/user-7-session-116.v
@@ -23,6 +23,6 @@ SearchPattern (IdSet.In (IdSet.union _ _)).
 Search -IdSet.union.
 Check IdSetFacts.union_2.
 Check IdSetFacts.union_2.
-(apply (IdSetFacts.union_2 fvs1 fvs2) in Hcontra).
+(apply (IdSetFacts.union_2 fvs2) in Hcontra).
 (* Auto-generated comment: Failed. *)
 
