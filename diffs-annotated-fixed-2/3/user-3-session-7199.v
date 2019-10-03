12ea02867f1b9f5e2909a142f1c4aa03f04078ea
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:48 -0700

    user 3, session 7199, part 223

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -288,6 +288,8 @@ Proof.
 (unfold log_size_ok, log_contents_ok; intros).
 (specialize (H0 a); intuition).
 (unfold log_addr in *).
+(rewrite disk_oob_eq; simpl; auto).
+autorewrite with upd.
 lia.
 (* Auto-generated comment: Failed. *)
 
