4e30511633d1291f859b3ed500ace2dd1e5b3c0f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:36 -0700

    user 7, session 64, part 5

--- diffs-annotated-fixed-2/7/user-7-session-64.v
+++ diffs-annotated-fixed-2/7/user-7-session-64.v
@@ -40,6 +40,11 @@ Check value_type_matching_ty__exists.
 (destruct (value_type_matching_ty__exists t2 k Hdep2) as [pv2 [Hpval2 Hpv2]]).
 (split; intros v Hm).
 -
-(assert (Hvp : value_type (TPair v pv2)) by (constructor; assumption)).
+(assert (Hmp : |-[ k] TPair v pv2 <$ TPair t1 t2) by (apply match_ty_pair; assumption)).
+specialize (Hsem _ Hmp).
+(apply match_ty_pair__inv in Hsem).
+(destruct Hsem as [v1 [v2 [Heq [Hm1 Hm2]]]]).
+(inversion Heq; subst).
+assumption.
 (* Auto-generated comment: Failed. *)
 
