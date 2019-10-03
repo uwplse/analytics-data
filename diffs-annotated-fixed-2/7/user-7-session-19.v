93a554487c7c1c7306e689f6b40808915c0ce08a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:52 -0700

    user 7, session 19, part 505

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -250,6 +250,7 @@ Proof.
 (intros k t t' Hdept Hsem).
 (rewrite <- inv_depth_mk_nf).
 (apply sem_sub_k_i_nf__inv_depth_le with k).
-(apply mk_nf__in_f).
+(apply mk_nf__in_nf).
+(rewrite inv_depth_mk_nf; assumotion).
 (* Auto-generated comment: Failed. *)
 
