bcd40447384d8914267f7caea19f86af10f4905e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:15 -0700

    user 8, session 14, part 52

--- diffs-annotated-fixed-2/8/user-8-session-14.v
+++ diffs-annotated-fixed-2/8/user-8-session-14.v
@@ -1220,6 +1220,7 @@ gen \207\129.
 (intros).
 (rewrite Nat.mul_1_r).
 Msimpl.
-(apply init0_end_superoperator).
+(eapply init0_end_superoperator; trivial).
+easy.
 (* Auto-generated comment: Failed. *)
 
