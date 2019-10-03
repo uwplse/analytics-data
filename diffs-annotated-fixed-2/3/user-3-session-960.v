b26353aa81fe8408722a5babce2e73a906f1c238
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:19:32 -0700

    user 3, session 960, part 372

--- diffs-annotated-fixed-2/3/user-3-session-960.v
+++ diffs-annotated-fixed-2/3/user-3-session-960.v
@@ -5,8 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-From Coq Require Import ProofIrrelevance.
-From Coq Require Export String.
-Timeout 1 Print LoadPath.
 (* Auto-generated comment: Succeeded. *)
 
