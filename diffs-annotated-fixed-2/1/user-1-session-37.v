64b075778029d19eb3801b813e4c5bb1e2a602ea
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:15 -0700

    user 1, session 37, part 47

--- diffs-annotated-fixed-2/1/user-1-session-37.v
+++ diffs-annotated-fixed-2/1/user-1-session-37.v
@@ -330,7 +330,12 @@ Fixpoint Gamma (G : GT) : SetST :=
                      | None => False
                      | Some T => Ensembles.In _ (Gamma G) T
                      end
-                 | (O, G) => Empty_set _
+                 | (O, G) =>
+                     fun OS =>
+                     match OS with
+                     | None => True
+                     | Some T => Ensembles.In _ (Gamma G) T
+                     end
                  end)) l)
   | _ => Empty_set _
   end.
