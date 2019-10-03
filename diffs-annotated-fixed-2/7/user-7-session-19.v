b933b5f57d06b619c30126b5d3f7515131b9944c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:00 -0700

    user 7, session 19, part 634

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -447,6 +447,7 @@ constructor.
 (apply IHk).
 (apply mk_nf__in_nf).
 (rewrite inv_depth_mk_nf).
-assumption.
+Check sem_eq_k_i__inv_depth_eq_1.
+(pose proof (sem_eq_k_i__inv_depth_eq_1 _ _ _ Hdep' Href)).
 (* Auto-generated comment: Failed. *)
 
