ccdf4b122b4b58859b28eb40e62dd860496c9b44
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:58 -0700

    user 3, session 7199, part 385

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -480,6 +480,10 @@ intuition.
 (descend; intuition eauto).
 }
 (eapply proc_spec_weaken; eauto).
-(unfold spec_impl).
-(* Auto-generated comment: Succeeded. *)
+(unfold spec_impl; simpl; intuition).
+(descend; intuition eauto).
+{
+(descend; intuition eauto).
+(apply log_abstraction_nil).
+(* Auto-generated comment: Failed. *)
 
