9ab43f680afbf4a32c9ba834e01cea82e862d9cc
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:02 -0700

    user 7, session 10, part 18

--- diffs-annotated-fixed-2/7/user-7-session-10.v
+++ diffs-annotated-fixed-2/7/user-7-session-10.v
@@ -126,19 +126,11 @@ Proof.
            remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ;
             assert (Hnf : InNF( t1)) by (subst; apply unite_pairs__preserves_nf; assumption); induction Hsub; inversion Heqx; 
             inversion Heqy; subst; tauto || (rewrite (mk_nf_nf__equal _ Hnf) in IHHsub; tauto)
+     | Hsub:|- unite_pairs _ (TUnion _ _) << TPair _ _
+       |- _ =>
+           rewrite unite_pairs_t_union in Hsub; try resolve_not_union; destruct (in_nf_union__inv _ _ Hnf2) as [Hnf21 Hnf22];
+            apply sub_r_union_l__inv in Hsub; destruct Hsub as [Hsub1 Hsub2]; specialize (IHt2_1 _ _ Hsub1 Hnf1 Hnf21); specialize
+            (IHt2_2 _ _ Hsub2 Hnf1 Hnf22); split; tauto || constructor; tauto
      end ])).
--
-(rewrite unite_pairs_t_union in Hsub; try resolve_not_union; destruct (in_nf_union__inv _ _ Hnf2) as [Hnf21 Hnf22];
-  apply sub_r_union_l__inv in Hsub; destruct Hsub as [Hsub1 Hsub2]).
-(specialize (IHt2_1 _ _ Hsub1 Hnf1 Hnf21); specialize (IHt2_2 _ _ Hsub2 Hnf1 Hnf22)).
-(split; tauto || constructor; tauto).
--
-(match goal with
- | Hsub:|- unite_pairs _ (TUnion _ _) << TPair _ _
-   |- _ =>
-       rewrite unite_pairs_t_union in Hsub; try resolve_not_union; destruct (in_nf_union__inv _ _ Hnf2) as [Hnf21 Hnf22];
-        apply sub_r_union_l__inv in Hsub; destruct Hsub as [Hsub1 Hsub2]; specialize (IHt2_1 _ _ Hsub1 Hnf1 Hnf21); specialize
-        (IHt2_2 _ _ Hsub2 Hnf1 Hnf22); split; tauto || constructor; tauto
- end).
 (* Auto-generated comment: Failed. *)
 
