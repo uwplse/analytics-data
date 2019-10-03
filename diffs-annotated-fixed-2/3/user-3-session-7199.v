8913234e8cc924a8b0fb41fa907b8a37b820a87a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:49 -0700

    user 3, session 7199, part 247

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -335,6 +335,5 @@ intuition eauto.
 step_proc.
 (intuition eauto; autorewrite with upd; auto).
 (apply log_contents_ok_unchanged; eauto).
-(eapply proc_spec_weaken; eauto).
 (* Auto-generated comment: Succeeded. *)
 
