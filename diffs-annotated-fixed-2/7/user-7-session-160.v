1464815894bb16d89ebf23346f42a4b786f82fa7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:27 -0700

    user 7, session 160, part 7

--- diffs-annotated-fixed-2/7/user-7-session-160.v
+++ diffs-annotated-fixed-2/7/user-7-session-160.v
@@ -45,6 +45,6 @@ Search -IdSet.remove.
 Check IdSetFacts.remove_2.
 Check IdSetFacts.remove_2.
 (assert (Hneq' : X' <> X) by auto).
-(apply (IdSetFacts.remove_2 _ _ Hneq') in Hcontra).
+(apply (IdSetFacts.remove_2 _ Hneq') in Hcontra).
 (* Auto-generated comment: Failed. *)
 
