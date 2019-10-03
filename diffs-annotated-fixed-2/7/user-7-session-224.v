7d9b6e6590e56ed55142ee378dd9a7afdb0f264e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:45 -0700

    user 7, session 224, part 62

--- diffs-annotated-fixed-2/7/user-7-session-224.v
+++ diffs-annotated-fixed-2/7/user-7-session-224.v
@@ -79,6 +79,7 @@ exists w2.
 (apply match_ty_exist__inv in Hm).
 (destruct Hm as [tx [Hwftx Hm]]).
 (rewrite b_subst_not_b_free_in_ty in Hm; try assumption).
-tauto.
+auto.
+}
 (* Auto-generated comment: Failed. *)
 
