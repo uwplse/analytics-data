a9c046ea40f68aed7edc0c86bff7ffdd55e7a2c8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:31 -0700

    user 8, session 79, part 263

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -650,8 +650,13 @@ Lemma big_kron_append :
 Proof.
 (induction l1).
 -
-show_dimensions.
 (intros).
 (simpl).
+(rewrite <- kron_1_l_inv).
+reflexivity.
+-
+(intros).
+(simpl).
+(erewrite IHl1; auto).
 (* Auto-generated comment: Succeeded. *)
 
