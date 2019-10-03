636f29d9239fc1287ae67f64e398f50ffb05e4c3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:04 -0700

    user 7, session 19, part 704

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -380,6 +380,6 @@ Qed.
 Lemma match_ty_i_nf : forall (t : ty) (k : nat), ||-[ k][t]= [MkNF( t)].
 Proof.
 (intros t k).
-(apply match_ty_i_nf').
+(pose proof (match_ty_i_nf' k t) as H).
 (* Auto-generated comment: Failed. *)
 
