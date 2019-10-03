dc411a34bbe8885bb183d4b9198669592b41fc58
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:32 -0700

    user 7, session 106, part 5

--- diffs-annotated-fixed-2/7/user-7-session-106.v
+++ diffs-annotated-fixed-2/7/user-7-session-106.v
@@ -181,7 +181,6 @@ subst.
 (apply match_ty_cname).
 -
 (apply match_ty_pair__inv in Hm).
-(destruct Hm as [v1 v2 [Heq [Hmq Hm2]]]; subst).
 (destruct Hm as [v1 [v2 [Heq [Hmq Hm2]]]]; subst).
 (* Auto-generated comment: Failed. *)
 
