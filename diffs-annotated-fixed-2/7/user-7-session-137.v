7c742e312d7de13873536d4224ca9cda29289c65
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:06 -0700

    user 7, session 137, part 10

--- diffs-annotated-fixed-2/7/user-7-session-137.v
+++ diffs-annotated-fixed-2/7/user-7-session-137.v
@@ -15,13 +15,8 @@ Require Import Coq.Bool.Bool.
 Lemma sem_sub_k_exist_fresh_l : forall (k : nat) (X : id) (t : ty), fresh_in_ty X t -> ||-[ k][TExist X t]<= [t].
 Proof.
 (intros k X t).
-(induction t; intros Hfresh).
+(induction t; intros Hfresh; intros w1; exists w1; intros v Hm; destruct w1; try (solve [ apply match_ty_exist__0_inv in Hm; contradiction ])).
 -
-(intros w1).
-exists w1.
-(intros v Hm).
-(destruct w1).
-(apply match_ty_exist__0_inv in Hm; contradiction).
 (apply match_ty_exist__inv in Hm).
 (destruct Hm as [tx Hm]).
 (simpl in Hm).
@@ -29,5 +24,7 @@ exists w1.
 eassumption.
 (repeat constructor).
 -
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hm]).
 (* Auto-generated comment: Failed. *)
 
