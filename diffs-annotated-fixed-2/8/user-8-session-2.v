a28ddc309b940c13e716017c6b6dc398f5373c62
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:57 -0700

    user 8, session 2, part 91

--- diffs-annotated-fixed-2/8/user-8-session-2.v
+++ diffs-annotated-fixed-2/8/user-8-session-2.v
@@ -102,5 +102,5 @@ specialize (IHW2 lb li u).
 (destruct (ctrls_to_list lb li (trans u)) as [[j' l'] v'] eqn:E').
 (simpl in *).
 (apply IHW2).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
