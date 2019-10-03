312672356efaa64df8168cdc6e9f8f277166e6cd
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:27:12 -0700

    user 8, session 79, part 1330

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -1138,7 +1138,7 @@ specialize inSeq_correct as IS.
 (repeat (rewrite IS; unfold compose_super; compile_typing compile_WT)).
 clear IS.
 (apply subset_classical_merge in H as [S1 S2]).
+(repeat rewrite_inPar).
 rewrite_inPar.
-rewrite_inPar.
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
