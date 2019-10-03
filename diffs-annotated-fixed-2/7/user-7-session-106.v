8b0d5a2ad24b9d8ef08cf6de0625120c0f59d8cd
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:32 -0700

    user 7, session 106, part 14

--- diffs-annotated-fixed-2/7/user-7-session-106.v
+++ diffs-annotated-fixed-2/7/user-7-session-106.v
@@ -186,5 +186,8 @@ subst.
 -
 (apply match_ty_union__inv in Hm).
 (destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; eauto).
-(* Auto-generated comment: Failed. *)
+-
+(destruct k).
++
+(* Auto-generated comment: Succeeded. *)
 
