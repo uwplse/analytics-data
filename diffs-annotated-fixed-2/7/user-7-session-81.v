934f19da342515cb0f54163755c66a931fdd2686
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:45 -0700

    user 7, session 81, part 9

--- diffs-annotated-fixed-2/7/user-7-session-81.v
+++ diffs-annotated-fixed-2/7/user-7-session-81.v
@@ -94,6 +94,9 @@ Proof.
 (unfold sem_sub in Hsem).
 (split; unfold sem_sub; intros k v Hm).
 -
-(destruct (value_type_matching_ty__exists k t2) as [pv2 Hpv2]).
+Check value_type_matching_ty__exists.
+(destruct (value_type_matching_ty__exists t2 k) as [pv2 Hpv2]).
+(remember (Nat.max k (| t2 |)) as kmax).
+(assert (Hmp : |-[ kmax] TPair v pv2 <$ TPair t1 t2)).
 (* Auto-generated comment: Failed. *)
 
