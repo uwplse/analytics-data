41b6a896782fb828e4b7c4503228c1abbc8f6894
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:50 -0700

    user 2, session 5, part 55

--- diffs-annotated-fixed-2/2/user-2-session-5.v
+++ diffs-annotated-fixed-2/2/user-2-session-5.v
@@ -135,6 +135,5 @@ Fixpoint step (t : term) : term :=
 Redirect "/tmp/coqfAE0Rj" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
-Print step.
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
