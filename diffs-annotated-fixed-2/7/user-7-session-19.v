a3cb81e0b3667321b0bf3994cbb07961676849eb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:42 -0700

    user 7, session 19, part 329

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -148,20 +148,5 @@ Proof.
 (intros v Hv k ta tb Hsem; unfold sem_sub_k_i in Hsem).
 (assert (Hm : |-[ k] v <$ v) by (apply match_ty_i__reflexive; assumption)).
 specialize (Hsem _ Hv Hm).
-(apply match_ty_i_union__inv in Hsem).
-(destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hv' Hm'; apply match_ty_i__transitive_on_value_type with v; assumption).
-Qed.
-Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
-Proof.
-(induction k; induction t; induction t'; intros H; try (solve [ simpl; constructor ]);
-  try (solve
-   [ match goal with
-     | |- | ?t1 | <= | ?t2 | =>
-           (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
-             (H _ Hm); contradiction) ||
-             (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
-               (H _ Hm); contradiction)
-     end ])).
-(assert (Hv : value_type (TCName c)) by constructor).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
