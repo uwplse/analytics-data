4639b4047b34ece903ba2c50054b56c3d66a47ff
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:03 -0700

    user 7, session 19, part 687

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -337,29 +337,6 @@ Proof.
 Qed.
 Lemma match_ty_i_nf' : forall k : nat, forall v t : ty, |-[ k] v <$ t <-> |-[ k] v <$ MkNF( t).
 Proof.
-(induction k; intros v t; generalize dependent v; induction t; intros v; split; intros Hm; try (solve [ simpl; assumption ]);
-  try
-   match goal with
-   | Hm:|-[ _] ?v <$ TPair ?t1 ?t2
-     |- |-[ _] ?v <$ MkNF( TPair ?t1 ?t2) =>
-         apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; rewrite mk_nf_pair; apply match_ty_i__pair_unite_pairs;
-          [ apply IHt1 | apply IHt2 ]; assumption
-   | Hm:|-[ _] ?v <$ MkNF( TPair ?t1 ?t2)
-     |- |-[ _] ?v <$ TPair ?t1 ?t2 =>
-         rewrite mk_nf_pair in Hm; apply match_ty_i__unite_pairs_pair in Hm; apply match_ty_i__pair__inv in Hm;
-          destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; apply match_ty_i_pair; [ apply IHt1 | apply IHt2 ]; assumption
-   end;
-  try
-   match goal with
-   | Hm:|-[ _] ?v <$ TUnion ?t1 ?t2
-     |- |-[ _] ?v <$ MkNF( TUnion ?t1 ?t2) =>
-         rewrite mk_nf_union; apply match_ty_i_union__inv in Hm; destruct Hm as [Hm| Hm]; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ];
-          [ apply IHt1 | apply IHt2 ]; assumption
-   | Hm:|-[ _] ?v <$ MkNF( TUnion ?t1 ?t2)
-     |- |-[ _] ?v <$ TUnion ?t1 ?t2 =>
-         rewrite mk_nf_union in Hm; apply match_ty_i_union__inv in Hm; destruct Hm as [Hm| Hm];
-          [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; [ apply IHt1 | apply IHt2 ]; assumption
-   end; try (solve [ destruct v; contradiction ])).
--
+(induction k; intros v t; generalize dependent v; induction t; intros v; split; intros Hm; try (solve [ simpl; assumption ])).
 (* Auto-generated comment: Failed. *)
 
