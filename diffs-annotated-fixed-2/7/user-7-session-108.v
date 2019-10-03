170fc822db2db77ad450573a70da29e870951b89
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:36 -0700

    user 7, session 108, part 44

--- diffs-annotated-fixed-2/7/user-7-session-108.v
+++ diffs-annotated-fixed-2/7/user-7-session-108.v
@@ -254,8 +254,10 @@ Proof.
 (apply match_ty_pair__inv in Hcontra).
 (destruct Hcontra as [v1 [v2 [Heq [Hm1 Hm2]]]]).
 subst.
-(assert (Hcontra : ~ (exists v : ty, |-[ k, w] v <$ t1))).
+(assert (Hcontra : ~ (exists v1 : ty, |-[ k, w] v1 <$ t1) \/ ~ (exists v2 : ty, |-[ k, w] v2 <$ t2))).
 {
-(intros Hcontra).
+(assert (Hcontra' : ~ ((exists v1 : ty, |-[ k, w] v1 <$ t1) /\ (exists v2 : ty, |-[ k, w] v2 <$ t2)))).
+{
+(intros [Hcontra'1 hcontra'2]).
 (* Auto-generated comment: Failed. *)
 
