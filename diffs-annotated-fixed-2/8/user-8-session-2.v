9696ce8eee2e52320b66e6f6aa23c5d4b76bb1a5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:05 -0700

    user 8, session 2, part 182

--- diffs-annotated-fixed-2/8/user-8-session-2.v
+++ diffs-annotated-fixed-2/8/user-8-session-2.v
@@ -148,6 +148,6 @@ Timeout 1 Print Ltac restore_dims.
             A'
  end).
 (rewrite <- ctrl_list_to_unitary_transpose).
-(rewrite ctrl_list_to_unitary_transpose).
-(* Auto-generated comment: Succeeded. *)
+(apply ctrls_to_list_transpose in E).
+(* Auto-generated comment: Failed. *)
 
