69aa475852332dbb320d05a7c05a3082cba0d1ba
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:45:00 -0700

    user 3, session 3788, part 32

--- diffs-annotated-fixed-2/3/user-3-session-3788.v
+++ diffs-annotated-fixed-2/3/user-3-session-3788.v
@@ -43,22 +43,6 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqtKERBA"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Theorem swapXY_ok :
-  proc_spec
-    (fun (_ : unit) state =>
-     {|
-     pre := True;
-     post := fun r state' =>
-             state' = mkState (StateY state) (StateX state) (StateZ state);
-     recovered := fun _ state' => True |}) swapXY vars.recover vars.abstr.
-Proof.
-(match goal with
- | |- proc_spec _ ?p _ _ => pose proof (AProc p) as Hbefore
- end).
-(unfold swapXY).
-monad_simpl.
-(match goal with
- | |- proc_spec _ ?p _ _ => apply (change_marker p) in Hbefore
- end).
-(* Auto-generated comment: Succeeded. *)
+Notation "'proc:' p" := (ProcMarker p) (only printing).
+(* Auto-generated comment: Failed. *)
 
