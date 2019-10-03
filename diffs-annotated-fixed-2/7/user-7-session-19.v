2d5c4e54c7a8fc10ab3e4d76b88df0673d3548c7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:42 -0700

    user 7, session 19, part 326

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -163,6 +163,6 @@ Proof.
                (H _ Hm); contradiction)
      end ])).
 (assert (Hv : value_type (TCName c)) by constructor).
-(pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ H)).
+(pose proof (value_sem_sub_k_i_union__inv _ Hv)).
 (* Auto-generated comment: Failed. *)
 
