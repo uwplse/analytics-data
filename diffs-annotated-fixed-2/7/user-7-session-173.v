df46c79824dad27bd923a70d4c12ea81dda4b8bb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:42 -0700

    user 7, session 173, part 50

--- diffs-annotated-fixed-2/7/user-7-session-173.v
+++ diffs-annotated-fixed-2/7/user-7-session-173.v
@@ -85,8 +85,10 @@ assumption.
 -
 (destruct (beq_idP x i); reflexivity).
 Qed.
+Check lt.
+Definition lt_size (t1 t2 : ty) := lt (size t1) (size t2).
 Function
- subst (x : id) (s t : ty) {wf size t} : ty :=
+ subst (x : id) (s t : ty) {wf lt_size t} : ty :=
    match t with
    | TCName _ => t
    | TPair t1 t2 => TPair (subst x s t1) (subst x s t2)
@@ -98,5 +100,6 @@ Function
    | TVar y => if beq_id x y then s else t
    | TEV y => t
    end.
-(* Auto-generated comment: Failed. *)
+Print All.
+(* Auto-generated comment: Succeeded. *)
 
