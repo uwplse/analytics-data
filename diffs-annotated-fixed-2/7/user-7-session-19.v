e59391c5a99f05feddaea00acdf2779406134a2b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:38 -0700

    user 7, session 19, part 258

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -107,6 +107,28 @@ Proof.
 (intros v1 v2 t3 Hv2).
 generalize dependent t3.
 generalize dependent v1.
-(induction Hv2; intros v1 t3 k Hm1 Hm2).
-(* Auto-generated comment: Failed. *)
+(induction Hv2).
+-
+(intros v1 t3 k Hm1 Hm2).
+(apply match_ty_i_cname__inv in Hm1; subst).
+assumption.
+-
+(intros v0 t3 k Hm1 Hm2).
+(apply match_ty_i_pair__inv in Hm1).
+(destruct Hm1 as [pv11 [pv12 [Heq [Hm11 Hmpv12]]]]; subst).
+(induction t3; try (solve [ destruct k; simpl in Hm2; contradiction ])).
++
+(apply match_ty_i_pair__inv in Hm2).
+(destruct Hm2 as [pv21 [pv22 [Heq [Hm21 Hm22]]]]).
+(inversion Heq; subst).
+auto using match_ty_i_pair.
++
+(apply match_ty_i_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; tauto).
+-
+(intros v1 t3 k Hm1 Hm2).
+(destruct k).
+(destruct v1; try contradiction).
+(apply match_ty_i_ref__inv in Hm1).
+(* Auto-generated comment: Succeeded. *)
 
