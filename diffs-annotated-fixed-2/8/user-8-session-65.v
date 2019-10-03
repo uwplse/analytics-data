7989c1aedf71ecc2dca8f7ea69939371425b8bb9
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:24:26 -0700

    user 8, session 65, part 76

--- diffs-annotated-fixed-2/8/user-8-session-65.v
+++ diffs-annotated-fixed-2/8/user-8-session-65.v
@@ -968,6 +968,5 @@ gen \207\129.
 (rewrite Nat.mul_1_r).
 (intros \207\129).
 remember_differences.
-restore_dims try rewrite size_ntensor; unify_pows_two; simpl; try lia.
 (* Auto-generated comment: Succeeded. *)
 
