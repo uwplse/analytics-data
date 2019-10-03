827ea38946e0fa951d53ca2c3094117bb8765525
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:10:27 -0700

    user 3, session 159, part 2

--- diffs-annotated-fixed-2/3/user-3-session-159.v
+++ diffs-annotated-fixed-2/3/user-3-session-159.v
@@ -9,7 +9,31 @@ Require Import Examples.StatDb.Impl.
 Require Import Spec.Hoare.
 Require Import Spec.HoareTactics.
 Require Import Spec.AbstractionSpec.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqXI6axi"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0BLEGv"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 Definition absr : relation DB.l.(State) Var.l.(State) unit :=
-  fun l s _ => fst s = fold_right plus 0 l /\ snd s = length l.
+  fun l res =>
+  match res with
+  | Val s _ => fst s = fold_right plus 0 l /\ snd s = length l
+  | Err _ _ => False
+  end.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqiQZm2s"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq4uHC6k"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Instance absr_non_error : (NonError absr).
 (* Auto-generated comment: Succeeded. *)
 
