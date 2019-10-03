88e651e5d8fc980c5395b014a150f84983fcce08
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:35 -0700

    user 7, session 61, part 5

--- diffs-annotated-fixed-2/7/user-7-session-61.v
+++ diffs-annotated-fixed-2/7/user-7-session-61.v
@@ -135,6 +135,6 @@ Lemma pair_sem_sub_k__sub_d :
   (forall tb2 : ty, ||-[ k][ta2]<= [tb2] -> |- ta2 << tb2) -> forall t2 : ty, ||-[ k][TPair ta1 ta2]<= [t2] -> |- TPair ta1 ta2 << t2.
 Proof.
 (intros k ta1 ta2 Hat Hdep IH1 IH2).
-(assert (Hva : value_type (TPair ta1 ta2)) by (apply atom_type__value_type; assumption)).
+(assert (Hva : value_type (TPair ta1 ta2)) by atom_type__value_type _ hat).
 (* Auto-generated comment: Failed. *)
 
