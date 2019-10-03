b383eabd646243c3985206615d5b8971487290f4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:25 -0700

    user 7, session 41, part 4

--- diffs-annotated-fixed-2/7/user-7-session-41.v
+++ diffs-annotated-fixed-2/7/user-7-session-41.v
@@ -13,5 +13,13 @@ Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Open Scope btjm.
 Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
+Proof.
+(intros t1 t2 Hsub).
+(unfold sem_sub).
+(induction Hsub; intros k v Hm).
+-
+assumption.
+-
+auto.
 (* Auto-generated comment: Failed. *)
 
