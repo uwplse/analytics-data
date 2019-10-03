ca8e3aede0b9bc17dbe750c0ceea91628b8e8a6b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:26:18 -0700

    user 8, session 79, part 769

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -1022,7 +1022,7 @@ restore_dims tensor_tac.
          specialize (inPar_correct W1 W1' W2 W2' f g true \207\1291 \207\1292) as IP; simpl in *;
           rewrite size_ntensor in *; simpl in *; try rewrite Nat.mul_1_r in *)
  end; try (solve [ type_check ])).
-show_dimensions.
 restore_dims tensor_tac.
+(rewrite IP).
 (* Auto-generated comment: Succeeded. *)
 
