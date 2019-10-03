fab70195eff3db953cac8fa1938fb88cee3cfa36
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:12:13 -0700

    user 3, session 9904, part 85

--- diffs-annotated-fixed-2/3/user-3-session-9904.v
+++ diffs-annotated-fixed-2/3/user-3-session-9904.v
@@ -132,6 +132,12 @@ intuition.
 congruence.
 }
 (simpl).
-(unfold statdb_abstraction in *).
+(unfold statdb_abstraction in *; intuition).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoBERUr"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
