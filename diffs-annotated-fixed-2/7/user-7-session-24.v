ce2f50b3c68f05c9e860a7a8f511f047dfafce1a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:11 -0700

    user 7, session 24, part 6

--- diffs-annotated-fixed-2/7/user-7-session-24.v
+++ diffs-annotated-fixed-2/7/user-7-session-24.v
@@ -33,10 +33,28 @@ Fixpoint inv_depth (t : ty) :=
   end
 where "'|' t '|'" := (inv_depth t) : btjt_scope.
 Delimit Scope btjt_scope with btjt.
+Open Scope btjt_scope.
 Inductive value_type : ty -> Prop :=
   | VT_CName : forall cn, value_type (TCName cn)
   | VT_Pair : forall v1 v2, value_type v1 -> value_type v2 -> value_type (TPair v1 v2)
   | VT_Ref : forall t, value_type (TRef t).
 Hint Constructors value_type: DBBetaJulia.
+Declare Scope btjm_scope.
+Reserved Notation "'|-[' k ']' v '<$' t" (at level 50).
+Reserved Notation "'||-[' k ']' '[' t1 ']' '=' '[' t2 ']'" (at level 45).
+Fixpoint match_ty (k : nat) :=
+  fix mty (v : ty) :=
+    fix mty' (t : ty) :=
+      match k, v, t with
+      | _, TCName c, TCName c' => c = c'
+      | _, TPair v1 v2, TPair t1 t2 => mty v1 t1 /\ mty v2 t2
+      | _, _, TUnion t1 t2 => mty' t1 \/ mty' t2
+      | S k, TRef t', TRef t => (inv_depth t <= k /\ inv_depth t' = inv_depth t) /\ (forall v, |-[ k] v <$ t' <-> |-[ k] v <$ t)
+      | _, _, _ => False
+      end
+where "|-[ k ']' v '<$' t" := (match_ty k v t) : btjm_scope
+with sem_eq_k (k : nat) (t1 t2 : ty) :=
+  forall v : ty, |-[ k] v <$ t1 <-> |-[ k] v <$ t2
+where "'||-[' k ']' '[' t1 ']' '=' '[' t2 ']'" := (sem_eq_k k t1 t2) : btjm_scope.
 (* Auto-generated comment: Failed. *)
 
