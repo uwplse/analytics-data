1a543417e8552f3e614ad5e79c28ebab11bc657f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:32 -0700

    user 7, session 216, part 19

--- diffs-annotated-fixed-2/7/user-7-session-216.v
+++ diffs-annotated-fixed-2/7/user-7-session-216.v
@@ -413,6 +413,11 @@ tauto.
 Search -IdSet.Empty.
 Search -IdSet.Empty.
 Search -IdSet.remove.
+Search -IdSet.In.
+(destruct (IdSetProps.In_dec i (FBV t))).
 admit.
+admit.
+-
+(simpl).
 (* Auto-generated comment: Failed. *)
 
