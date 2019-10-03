5180d45990f72151b5d985941938d7bfa578fa6b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:54 -0700

    user 8, session 2, part 47

--- diffs-annotated-fixed-2/8/user-8-session-2.v
+++ diffs-annotated-fixed-2/8/user-8-session-2.v
@@ -58,5 +58,8 @@ dependent destruction u.
 +
 (simpl).
 (destruct li as [| k li]; trivial).
+(destruct (ctrls_to_list lb li u) as [[j l] v] eqn:E).
+(destruct (ctrls_to_list lb li (trans u)) as [[j' l'] v'] eqn:E').
+(simpl).
 (* Auto-generated comment: Succeeded. *)
 
