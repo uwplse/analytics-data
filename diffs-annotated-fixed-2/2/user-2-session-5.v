4e1a2f49b5d41508423bee30512255c7a88c3b8d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:49 -0700

    user 2, session 5, part 43

--- diffs-annotated-fixed-2/2/user-2-session-5.v
+++ diffs-annotated-fixed-2/2/user-2-session-5.v
@@ -33,7 +33,35 @@ Fixpoint value (t : term) : bool :=
   | Cons a b => value a && value b
   | App f a => false
   end.
-Redirect "/tmp/coqSxNVO1" Print Ltac Signatures.
+Redirect "/tmp/coqACDvw1" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
+Module TermNotations.
+Declare Scope coucou_scope.
+Notation "{ f a }" := (App f a) (f  at level 0, a  at level 0) : coucou_scope.
+Notation "< a b >" := (Cons a b) (format "< a  b >", a  at level 0, b  at level 0) : coucou_scope.
+Notation "[ ]" := Nil (format "[ ]") : coucou_scope.
+Notation "[ x ]" := (Cons x Nil) : coucou_scope.
+Notation "[ x y .. z ]" := (Cons x (Cons y .. (Cons z Nil) ..))
+  (x  at level 0, y  at level 0, z  at level 0) : coucou_scope.
+Coercion Ident : string >-> term.
+End TermNotations.
+Import TermNotations.
+Open Scope coucou_scope.
+Check
+  [<Nil <Nil "hi">> (Cons (Ident "1") (Ident "2")) (Ident "a")
+  {(Ident "myfun") (Ident "somArg")}].
+Redirect "/tmp/coqLRo0nb" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Fixpoint step (t : term) : term :=
+  match t with
+  | Nil => Nil
+  | Ident _ => Nil
+  | <a b> => if value a then <a (step b)> else <(step a) b>
+  | _ => t
+  end.
+Redirect "/tmp/coqMMj85L" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Print step.
 (* Auto-generated comment: Succeeded. *)
 
