ef21dd2b65e88ecc202e8270c7d82bcdb68488a4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:46 -0700

    user 8, session 34, part 35

--- diffs-annotated-fixed-2/8/user-8-session-34.v
+++ diffs-annotated-fixed-2/8/user-8-session-34.v
@@ -47,5 +47,6 @@ replace (size_ctx \206\1471') with size_octx \206\1471' by easy.
 admit.
 -
 (intros WT pf_merge1 pf_merge2).
-(* Auto-generated comment: Succeeded. *)
+replace (compose (gate g p1 f) h) with gate g p1 (fun p2 => compose (f p2) h) by auto.
+(* Auto-generated comment: Failed. *)
 
