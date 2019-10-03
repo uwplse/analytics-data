a2dd13c5a14e6764129dd06fb2f1a673fad0df9d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:34 -0700

    user 7, session 165, part 4

--- diffs-annotated-fixed-2/7/user-7-session-165.v
+++ diffs-annotated-fixed-2/7/user-7-session-165.v
@@ -83,8 +83,5 @@ Proof.
    [ apply fresh_union__inv in Hfresh; destruct Hfresh as [Hfresh1 Hfresh2]; rewrite IHt1; try assumption; rewrite IHt2; try assumption;
       reflexivity ])).
 -
-(destruct (beq_idP X i); try reflexivity).
-(destruct (IdSet.mem i (FV s))).
-+
 (* Auto-generated comment: Failed. *)
 
