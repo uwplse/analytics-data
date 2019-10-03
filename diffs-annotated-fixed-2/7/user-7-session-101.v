3615b372643c6f600516c016359fa72022bc6350
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:16 -0700

    user 7, session 101, part 32

--- diffs-annotated-fixed-2/7/user-7-session-101.v
+++ diffs-annotated-fixed-2/7/user-7-session-101.v
@@ -94,6 +94,7 @@ clear Hm.
 (inversion Href).
 -
 specialize (Hcontra 2).
-(assert (Hm : |-[ 0, 2] TRef (TExist vX (TRef tX)) <$ TRef (TExist vX (TRef tX))) by (apply match_ty_value_type__reflexive; constructor)).
-(* Auto-generated comment: Failed. *)
+(assert (Hm : |-[ w, 2] TRef (TExist vX (TRef tX)) <$ TRef (TExist vX (TRef tX))) by (apply match_ty_value_type__reflexive; constructor)).
+specialize (Hcontra _ Hm).
+(* Auto-generated comment: Succeeded. *)
 
