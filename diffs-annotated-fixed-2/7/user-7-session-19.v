c702f09f6dcb24fbb96271ab064597ac8c28be99
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:34 -0700

    user 7, session 19, part 193

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -98,5 +98,22 @@ constructor.
 (apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto).
 -
 (apply match_ty_i_ref__weak_inv in Hm).
+(destruct Hm as [t' Heq]; subst).
+constructor.
+Qed.
+Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
+Proof.
+(induction k; induction t; induction t'; intros H).
+32: {
+idtac.
+(simpl).
+(apply le_n_S).
+(apply IHk).
+(assert (Hv : value_type (TRef t)) by constructor).
+(assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; constructor)).
+specialize (H _ Hm).
+(apply match_ty_i_ref__inv in H).
+(destruct H as [tx [Heq Href]]; inversion Heq; subst).
+auto.
 (* Auto-generated comment: Failed. *)
 
