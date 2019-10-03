5b6e7a9d40a974f4fba7ac6fa630ab5fd06c9273
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:11 -0700

    user 7, session 99, part 14

--- diffs-annotated-fixed-2/7/user-7-session-99.v
+++ diffs-annotated-fixed-2/7/user-7-session-99.v
@@ -80,5 +80,11 @@ Notation "'||-[' w ',' k ']' '[' t1 ']' '=' '[' t2 ']'" := (sem_eq_w_k w k t1 t2
 Definition sem_sub_w (w : nat) (t1 t2 : ty) := forall k : nat, ||-[ w, k][t1]<= [t2].
 Notation "'||-[' w ']' '[' t1 ']' '<=' '[' t2 ']'" := (sem_sub_w w t1 t2) (at level 45) : btjm_scope.
 Definition sem_eq_w (w : nat) (t1 t2 : ty) := forall k : nat, ||-[ w, k][t1]= [t2].
+Notation "'||-[' w ']' '[' t1 ']' '=' '[' t2 ']'" := (sem_eq_w w t1 t2) (at level 45) : btjm_scope.
+Definition sem_sub (t1 t2 : ty) := exists w : nat, ||-[ w][t1]<= [t2].
+Notation "'||-' '[' t1 ']' '<=' '[' t2 ']'" := (sem_sub t1 t2) (at level 50) : btjm_scope.
+Definition sem_eq (t1 t2 : ty) := exists w : nat, ||-[ w][t1]= [t2].
+Notation "'||-' '[' t1 ']' '=' '[' t2 ']'" := (sem_eq t1 t2) (at level 50) : btjm_scope.
+Hint Unfold sem_sub_k sem_eq_k sem_sub sem_eq: DBBetaJulia.
 (* Auto-generated comment: Failed. *)
 
