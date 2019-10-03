5d728ed5a17c2d1e43c5defcaed74c2d498c51be
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:52 -0700

    user 7, session 19, part 508

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -252,7 +252,7 @@ Proof.
 (apply sem_sub_k_i_nf__inv_depth_le with k).
 (apply mk_nf__in_nf).
 (rewrite inv_depth_mk_nf; assumption).
-(apply sem_sub_k__i__trans with t).
+(apply sem_sub_k__i__trans with t; try assumption).
 (pose proof (match_ty_i_nf k t)).
 (* Auto-generated comment: Failed. *)
 
