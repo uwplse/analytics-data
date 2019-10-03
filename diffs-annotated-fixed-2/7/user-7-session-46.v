c6baa73bf570191ddef451e345dfcb0a38774f3b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:27 -0700

    user 7, session 46, part 3

--- diffs-annotated-fixed-2/7/user-7-session-46.v
+++ diffs-annotated-fixed-2/7/user-7-session-46.v
@@ -62,6 +62,6 @@ Proof.
 (intros v; induction v; try (solve [ intros t k Hm; destruct k; contradiction ])).
 clear IHv.
 (intros t k).
-(intros Hm).
+(simpl).
 (* Auto-generated comment: Failed. *)
 
