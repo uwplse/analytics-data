1e8a9cc1ed270b93702abce83d648ff5e8ab4b39
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:27 -0700

    user 7, session 19, part 66

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -116,6 +116,8 @@ admit.
 clear IHk'.
 (intros Htk Htk' v Hvk Hvk').
 (simpl in Htk, Htk').
+(apply le_S_n in Htk).
 (apply le_S_n in Htk').
+(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]).
 (* Auto-generated comment: Failed. *)
 
