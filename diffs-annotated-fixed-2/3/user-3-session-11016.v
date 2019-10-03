acc0e65c17749b00528dfeefaa9a648ed8c72073
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:15:59 -0700

    user 3, session 11016, part 21

--- diffs-annotated-fixed-2/3/user-3-session-11016.v
+++ diffs-annotated-fixed-2/3/user-3-session-11016.v
@@ -256,5 +256,10 @@ Example test_oddmembers : oddmembers [0; 1; 0; 2; 3; 0; 0] = [1; 3].
 Proof.
 reflexivity.
 Qed.
+Definition countoddmembers (l : natlist) : nat :=
+  match l with
+  | [ ] => 0
+  | h :: t => (if oddb h then 1 else 0) + countoddmembers t
+  end.
 (* Auto-generated comment: Failed. *)
 
