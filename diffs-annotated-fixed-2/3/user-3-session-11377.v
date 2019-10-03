a917186de197b11d017453e6d10090e355775576
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:05 -0700

    user 3, session 11377, part 4

--- diffs-annotated-fixed-2/3/user-3-session-11377.v
+++ diffs-annotated-fixed-2/3/user-3-session-11377.v
@@ -190,11 +190,11 @@ reflexivity.
 (rewrite plus_assoc).
 reflexivity.
 Qed.
-Theorem mult_n_Sm : forall m n, n * S m = m + n * m.
+Theorem mult_n_Sm : forall m n, n * S m = n + n * m.
 Proof.
 (induction n as [| n' IHn']).
 -
 (simpl).
 (rewrite <- plus_n_O).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
