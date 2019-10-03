d44a3d9e48e0a3a6255021cbaca954de012878ec
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:04:01 -0700

    user 3, session 7199, part 435

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -436,8 +436,13 @@ Lemma log_abstraction_commit :
   log_size_ok d' (bs ++ bs') ->
   log_contents_ok d' (bs ++ bs') ->
   forall len_b : block,
-  diskGet d' len_addr =?= b ->
+  diskGet d' len_addr =?= len_b ->
   block_to_addr len_b = length bs + length bs' ->
   log_abstraction d' (bs ++ bs').
+Proof.
+(intros).
+(unfold log_abstraction; intuition).
+(unfold log_length_ok in *; intros).
+eq_values.
 (* Auto-generated comment: Failed. *)
 
