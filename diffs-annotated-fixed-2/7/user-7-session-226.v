19f5f1a7809709883e416d94f93df87d728e4489
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:46 -0700

    user 7, session 226, part 2

--- diffs-annotated-fixed-2/7/user-7-session-226.v
+++ diffs-annotated-fixed-2/7/user-7-session-226.v
@@ -60,7 +60,9 @@ reflexivity.
 *
 (destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
 {
-specialize (IHt'b HXt'1).
-(destruct IHt'b as [w2 IHt'b]).
+(apply match_ty_union_1; auto).
+}
+{
+(rewrite f_subst_not_b_free_in_ty).
 (* Auto-generated comment: Failed. *)
 
