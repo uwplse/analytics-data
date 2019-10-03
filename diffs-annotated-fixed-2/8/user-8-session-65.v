5272e576d5a74223aaf5328d64977a84524bd557
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:24:39 -0700

    user 8, session 65, part 229

--- diffs-annotated-fixed-2/8/user-8-session-65.v
+++ diffs-annotated-fixed-2/8/user-8-session-65.v
@@ -1247,5 +1247,8 @@ constructor.
 (inversion g0).
 +
 (unfold valid_ancillae_box).
+(intros \207\129 TB).
+(repeat simpl_rewrite inSeq_correct; try apply inSeq_WT; try apply unitary_at1_WT;
+  try apply source_symmetric_WT; trivial).
 (* Auto-generated comment: Succeeded. *)
 
