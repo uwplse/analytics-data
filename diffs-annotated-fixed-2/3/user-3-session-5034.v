f4421db11d457ce1a2ad0442e4c037fb039533e0
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:56:38 -0700

    user 3, session 5034, part 6

--- diffs-annotated-fixed-2/3/user-3-session-5034.v
+++ diffs-annotated-fixed-2/3/user-3-session-5034.v
@@ -115,6 +115,5 @@ step_proc.
 (destruct a'; simpl in *; intuition; subst; eauto).
 (step_proc; intuition; subst; eauto).
 {
-(step_proc; intuition; subst; eauto).
 (* Auto-generated comment: Succeeded. *)
 
