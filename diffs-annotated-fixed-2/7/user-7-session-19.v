e20af9cfbab47a97cb40c95012caf442671a588c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:37 -0700

    user 7, session 19, part 246

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -108,11 +108,10 @@ Proof.
 6: {
 idtac.
 (unfold sem_sub_k_i in Hsem).
-(assert (Hv : value_type (TRef t)) by constructor).
-Search -match_ty_i.
-(assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption)).
-(pose proof (Hsem _ Hv Hm) as Hmu).
-(apply match_ty_i_union__inv in Hmu).
-(destruct Hmu as [Hmu1| Hmu2]; [ left | right ]).
+(assert (Hvref : value_type (TRef t)) by constructor).
+(assert (Hmref : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption)).
+(pose proof (Hsem _ Hvref Hmref) as Hm).
+(apply match_ty_i_union__inv in Hm).
+(destruct Hm as [Hm1| Hm2]; [ left | right ]).
 (* Auto-generated comment: Failed. *)
 
