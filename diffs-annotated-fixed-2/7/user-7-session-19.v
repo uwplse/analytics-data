d8ad0e387cd456de63b08a73dcec8ade418426b4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:31 -0700

    user 7, session 19, part 145

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -105,6 +105,12 @@ Qed.
 Lemma match_ty_i_eq__inv_depth_eq :
   forall t t' : ty, (forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
 Proof.
-(induction t; induction t'; intros H; try reflexivity).
+(induction t; induction t'; intros H; try reflexivity;
+  try (solve
+   [ match goal with
+     | |- | ?t1 | = | ?t2 | =>
+           assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+            (H 0 _ Hv); destruct H as [H _]; specialize (H Hm); contradiction
+     end ])).
 (* Auto-generated comment: Failed. *)
 
