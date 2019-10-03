33e4fdfaf11955b81a19061df56adf6460570e65
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:56:38 -0700

    user 3, session 5034, part 0

---
+++ diffs-annotated-fixed-2/3/user-3-session-5034.v
@@ -0,0 +1,120 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqMcpdIP"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Import POCS.
+Require Import LogAPI.
+Require Import Common.OneDiskAPI.
+Import ListNotations.
+Axiom (addr_to_block : addr -> proc block).
+Axiom (block_to_addr : block -> addr).
+Definition addr_to_block_spec State a :
+  Specification unit block unit State :=
+  fun (_ : unit) state =>
+  {|
+  pre := True;
+  post := fun r state' => state' = state /\ block_to_addr r = a;
+  recovered := fun _ state' => state' = state |}.
+Axiom
+  (addr_to_block_ok :
+     forall State a recover abstr,
+     proc_spec (@addr_to_block_spec State a) (addr_to_block a) recover abstr).
+Hint Resolve addr_to_block_ok: core.
+Module Log (d: OneDiskAPI)<: LogAPI.
+Fixpoint get_helper (start : addr) (count : addr) : 
+proc (list block) :=
+  match count with
+  | O => Ret nil
+  | S count' =>
+      b <- d.read start; l <- get_helper (start + 1) count'; Ret ([b] ++ l)
+  end.
+Definition get : proc (list block) :=
+  len <- d.read 0; r <- get_helper 1 (block_to_addr len); Ret r.
+Fixpoint append_helper (start : addr) (l : list block) : 
+proc unit :=
+  match l with
+  | nil => Ret tt
+  | b :: l' =>
+      _ <- d.write start b; _ <- append_helper (start + 1) l'; Ret tt
+  end.
+Definition append (l : list block) : proc bool :=
+  maxlen <- d.size;
+  curlenb <- d.read 0;
+  (let curlen := block_to_addr curlenb in
+   let newlen := curlen + length l in
+   if gt_dec (newlen + 1) maxlen
+   then Ret false
+   else
+    _ <- append_helper (curlen + 1) l;
+    newlenb <- addr_to_block newlen; _ <- d.write 0 newlenb; Ret true).
+Definition reset : proc unit :=
+  newlenb <- addr_to_block 0; _ <- d.write 0 newlenb; Ret tt.
+Definition init' : proc InitResult :=
+  len <- d.size;
+  (if len == 0
+   then Ret InitFailed
+   else newlenb <- addr_to_block 0; _ <- d.write 0 newlenb; Ret Initialized).
+Definition init := then_init d.init init'.
+Definition recover : proc unit := d.recover.
+Definition disk_matches_list (ds : OneDiskAPI.State) 
+  (start : addr) (l : list block) : Prop :=
+  diskGets ds start (length l) =??= l.
+Definition log_abstraction (ds : OneDiskAPI.State) 
+  (ls : LogAPI.State) : Prop :=
+  diskSize ds > length ls /\
+  (exists lblk, diskGet ds 0 =?= lblk /\ block_to_addr lblk = length ls) /\
+  disk_matches_list ds 1 ls.
+Definition abstr : Abstraction LogAPI.State :=
+  abstraction_compose d.abstr {| abstraction := log_abstraction |}.
+Notation "d [ a |-> b ]" := (diskUpd d a b) (at level 8, left associativity).
+Notation "d [ a |=> bs ]" := (diskUpds d a bs)
+  (at level 8, left associativity).
+Opaque diskGet.
+Theorem log_abstraction_nonempty :
+  forall disk l, log_abstraction disk l -> diskSize disk <> 0.
+Proof.
+firstorder.
+Qed.
+Hint Resolve log_abstraction_nonempty: core.
+Theorem log_abstraction_init :
+  forall disk b,
+  block_to_addr b = 0 ->
+  diskSize disk <> 0 -> log_abstraction disk [0 |-> b] nil.
+Proof.
+(unfold log_abstraction; intros).
+(autorewrite with upd; simpl; intuition).
+-
+lia.
+-
+(exists b; intuition).
+-
+(unfold disk_matches_list; simpl; auto).
+Qed.
+Hint Resolve log_abstraction_init: core.
+Theorem init_ok : init_abstraction init recover abstr inited_any.
+Proof.
+(eapply then_init_compose; eauto).
+step_proc.
+(destruct (r == 0)).
+-
+step_proc.
+-
+step_proc.
+step_proc.
+step_proc.
+(exists nil; intuition).
+Qed.
+Theorem reset_ok : proc_spec reset_spec reset recover abstr.
+Proof.
+(unfold reset).
+(apply spec_abstraction_compose; simpl).
+step_proc.
+(destruct a'; simpl in *; intuition; subst; eauto).
+-
+(step_proc; intuition; subst; eauto).
+(step_proc; intuition; subst; eauto).
+(* Auto-generated comment: Succeeded. *)
+
