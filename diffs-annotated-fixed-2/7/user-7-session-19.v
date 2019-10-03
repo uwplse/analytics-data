17885b664cd486bc720f34bc128500704b9041f6
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:52 -0700

    user 7, session 19, part 502

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -243,5 +243,11 @@ Proof.
 Admitted.
 Lemma sem_sub_k__i__trans : forall (k : nat) (t1 t2 t3 : ty), ||-[ k][t1]<= [t2] -> ||-[ k][t2]<= [t3] -> ||-[ k][t1]<= [t3].
 Proof.
+auto with DBBetaJulia.
+Qed.
+Lemma sem_sub_k_nf__inv_depth_le : forall (k : nat) (t t' : ty), | t | <= k -> ||-[ k][t]<= [t'] -> | t | <= | t' |.
+Proof.
+(intros k t t' Hdept Hsem).
+(rewrite <- inv_depth_mk_nf).
 (* Auto-generated comment: Failed. *)
 
