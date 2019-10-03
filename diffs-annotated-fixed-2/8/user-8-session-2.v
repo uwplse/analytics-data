44b2cbec01fea5ab3e40fb93de0db74d77870ffb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:02 -0700

    user 8, session 2, part 156

--- diffs-annotated-fixed-2/8/user-8-session-2.v
+++ diffs-annotated-fixed-2/8/user-8-session-2.v
@@ -136,6 +136,5 @@ Search -fst -snd.
 (rewrite <- ctrls_to_list_transpose_fst).
 (destruct (ctrls_to_list (repeat false n) li u) as [[j l] v] eqn:E).
 (simpl).
-(rewrite <- ctrl_list_to_unitary_transpose).
 (* Auto-generated comment: Succeeded. *)
 
