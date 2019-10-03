5eb846bd29eaf999ce03a2bda1a4ce19e489b342
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:38 -0700

    user 7, session 69, part 4

--- diffs-annotated-fixed-2/7/user-7-session-69.v
+++ diffs-annotated-fixed-2/7/user-7-session-69.v
@@ -326,5 +326,16 @@ clear IHt.
 split.
 tauto.
 (eapply sem_eq_k__trans; eauto).
+-
+clear IHt.
+(rewrite mk_nf_ref in Hm).
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [t' [Heq [[Hdept Hdept'] Href]]]; subst).
+(rewrite inv_depth_mk_nf in Hdept, Hdept').
+split.
+tauto.
+(eapply sem_eq_k__trans; eauto).
+(apply sem_eq_k__comm).
+auto.
 (* Auto-generated comment: Failed. *)
 
