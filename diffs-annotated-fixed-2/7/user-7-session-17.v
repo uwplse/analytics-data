ffcc9ddf36320a278231105613b24af1bc87d983
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:19 -0700

    user 7, session 17, part 1

--- diffs-annotated-fixed-2/7/user-7-session-17.v
+++ diffs-annotated-fixed-2/7/user-7-session-17.v
@@ -49,6 +49,14 @@ Theorem sub_r__decidable : forall t1 t2 : ty, Decidable.decidable (|- t1 << t2).
 Proof.
 (intros t1 t2).
 (assert (Hnf1 : InNF( MkNF( t1))) by apply mk_nf__in_nf).
-(pose proof (nf_sub_r__decidable2 _ Hnf1)).
+(destruct (nf_sub_r__decidable2 _ Hnf1) as [Hdec _]).
+(assert (Hnf2 : InNF( MkNF( t2))) by apply mk_nf__in_nf).
+specialize (Hdec _ Hnf2).
+(destruct Hdec as [Hdec| Hdec]).
+-
+(left; apply mk_nf_sub_r__sub_r; assumption).
+-
+(right; intros Hcontra).
+(apply mk_nf_sub_r__sub_r in Hcontra).
 (* Auto-generated comment: Failed. *)
 
