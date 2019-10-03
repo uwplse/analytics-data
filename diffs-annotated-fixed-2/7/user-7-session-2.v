db578a80c40fd921dbf3c257ecb42a9ae4aefd40
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:57 -0700

    user 7, session 2, part 143

--- diffs-annotated-fixed-2/7/user-7-session-2.v
+++ diffs-annotated-fixed-2/7/user-7-session-2.v
@@ -219,6 +219,9 @@ Proof.
 (remember (TUnion t1 t2) as t eqn:Heqt ).
 (induction Hsub; inversion Heqt; subst).
 tauto.
-(split; [ apply SR_UnionR1 | apply SR_UnionR2 ]; tauto).
+(split; apply SR_UnionR1; tauto).
+(split; apply SR_UnionR2; tauto).
+(simpl in Hsub).
+(apply sub_r_nf_union_l__inv in Hsub).
 (* Auto-generated comment: Failed. *)
 
