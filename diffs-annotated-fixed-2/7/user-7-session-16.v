9fa62450d6a6a1ed0b302bc3029fadaafb568272
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:15 -0700

    user 7, session 16, part 67

--- diffs-annotated-fixed-2/7/user-7-session-16.v
+++ diffs-annotated-fixed-2/7/user-7-session-16.v
@@ -366,7 +366,7 @@ Ltac
        end.
 Ltac
  solve_atom_sub_r_union__decidable IHt2_1 IHt2_2 :=
-  destruct IHt2_1 as [IH1| IH1]; destruct IHt2_2 as [IH2| IH2];
+  destruct IHt2_1 as [IH1| IH1]; try assumption; destruct IHt2_2 as [IH2| IH2]; try assumption;
    try (solve [ left; apply SR_UnionR1; assumption | left; apply SR_UnionR2; assumption ]); right; intros Hcontra;
    apply atom_sub_r_union__inv in Hcontra; tauto || constructor.
 Lemma nf_sub_r__decidable2 :
@@ -381,7 +381,6 @@ Proof.
       (forall t' : ty, InNF( t') -> Decidable.decidable (|- t << t')) /\ (forall t' : ty, InNF( t') -> Decidable.decidable (|- t' << t))))).
 -
 (intros c).
-Check in_nf_union__inv.
 (split; intros t'; induction t'; intros Hnf'; try destruct (in_nf_union__inv _ _ Hnf') as [Hnf'1 Hnf'2];
   try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 ]);
   try
@@ -390,5 +389,12 @@ Check in_nf_union__inv.
          destruct (cname_eq__decidable c1 c2);
           [ subst; left; constructor | right; intros Hcontra; apply sub_r_cname__inv in Hcontra; contradiction ]
    end).
++
+right.
+(match goal with
+ | |- ~ |- ?t1 << ?t2 =>
+       remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]);
+        subst
+ end).
 (* Auto-generated comment: Failed. *)
 
