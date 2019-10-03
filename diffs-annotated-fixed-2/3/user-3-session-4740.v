4e1c90bd5aef47b95d13a223dabf899a09d8017c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:55:15 -0700

    user 3, session 4740, part 28

--- diffs-annotated-fixed-2/3/user-3-session-4740.v
+++ diffs-annotated-fixed-2/3/user-3-session-4740.v
@@ -15,31 +15,33 @@ Timeout 1 Print LoadPath.
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Notation "a === b" := (a = b) (at level 100, format "a '/' ===  b").
+Notation "a === b" := (a = b) (at level 100, format "a  '/' ===  b").
 Definition aReallyBigIdentifier := 3.
 Redirect
-"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqLIXWpS"
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoMo5u2"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
 Redirect
-"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqrNt0PZ"
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqbfhrAa"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Definition anotherLongName := 10.
+Check 3 === 3.
 Redirect
-"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqIn9zkk"
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqHn7OBC"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
 Redirect
-"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqFdQVWS"
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqxKlg3n"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Check 3=== 3.
+Check 1 + 2 + aReallyBigIdentifier + 4 + anotherLongName
+  === 6 + anotherLongName + 8 + 9.
 (* Auto-generated comment: Succeeded. *)
 
