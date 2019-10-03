b01533765db915ac75f2bf89a0d40d94171cefa4
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:58 -0700

    user 3, session 7199, part 386

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -484,6 +484,6 @@ intuition.
 (descend; intuition eauto).
 {
 (descend; intuition eauto).
-(apply log_abstraction_nil).
-(* Auto-generated comment: Failed. *)
+(eapply log_abstraction_nil).
+(* Auto-generated comment: Succeeded. *)
 
