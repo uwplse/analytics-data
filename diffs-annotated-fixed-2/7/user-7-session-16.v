553fdd0509f14fdd90ee08234143761180784589
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:14 -0700

    user 7, session 16, part 56

--- diffs-annotated-fixed-2/7/user-7-session-16.v
+++ diffs-annotated-fixed-2/7/user-7-session-16.v
@@ -340,6 +340,8 @@ Qed.
 Lemma sub_r__mk_nf_sub_r1 : forall t t' : ty, |- t << t' -> |- MkNF( t) << t'.
 Proof.
 (intros t t' Hsub).
-(apply sub_r__transitive with t).
+(apply sub_r__transitive with t; try assumption).
+(apply mk_nf__sub_r1).
+Qed.
 (* Auto-generated comment: Failed. *)
 
