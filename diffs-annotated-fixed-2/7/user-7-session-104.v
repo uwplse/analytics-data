a1518427673421898c63cf7691852da9a2447054
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:24 -0700

    user 7, session 104, part 8

--- diffs-annotated-fixed-2/7/user-7-session-104.v
+++ diffs-annotated-fixed-2/7/user-7-session-104.v
@@ -50,7 +50,9 @@ Qed.
 Lemma not_sem_sub__refeXrefX_eYrefrefY : ~ ||- [TRef (TExist vX (TRef tX))]<= [TExist vY (TRef (TRef tY))].
 Proof.
 (intros Hcontra).
-specialize (Hcontra 2).
-(simpl in Hcontra).
-(* Auto-generated comment: Failed. *)
+specialize (Hcontra 2 1).
+(destruct Hcontra as [w Hcontra]).
+(assert (Hm : |-[ 2, 1] TRef (TExist vX (TRef tX)) <$ TRef (TExist vX (TRef tX))) by (apply match_ty_value_type__reflexive; constructor)).
+specialize (Hcontra _ _ Hm).
+(* Auto-generated comment: Succeeded. *)
 
