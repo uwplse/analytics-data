c2dfd60e191237c2a3ec3da63dae70ae6e3a1882
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:53 -0700

    user 7, session 2, part 69

--- diffs-annotated-fixed-2/7/user-7-session-2.v
+++ diffs-annotated-fixed-2/7/user-7-session-2.v
@@ -143,6 +143,7 @@ Proof.
 (induction Hsub21; inversion Heqx; subst).
 *
 clear Heqx IHHsub12_1 IHHsub12_2 IHHsub21_1 IHHsub21_2.
-(intros Hsub22; inversion Hsub12; subst).
+(intros Hsub22).
+(inversion Hsub12; subst).
 (* Auto-generated comment: Failed. *)
 
