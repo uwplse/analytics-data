eb1f9f7c376679eedcad21455e5e8c7214be82f7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:44 -0700

    user 7, session 110, part 23

--- diffs-annotated-fixed-2/7/user-7-session-110.v
+++ diffs-annotated-fixed-2/7/user-7-session-110.v
@@ -194,5 +194,13 @@ auto using match_ty_pair.
 (apply match_ty_union__inv in Hm2).
 (destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
 +
+(apply match_ty_pair__inv in Hm2).
+(destruct Hm2 as [pv21 [pv22 [Heq [Hmpv21 Hm22]]]]).
+(inversion Heq; subst).
+auto using match_ty_pair.
++
+(apply match_ty_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
++
 (* Auto-generated comment: Failed. *)
 
