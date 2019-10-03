3e87355fed397275a3d0a0098113b956bec6291f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:15 -0700

    user 7, session 16, part 81

--- diffs-annotated-fixed-2/7/user-7-session-16.v
+++ diffs-annotated-fixed-2/7/user-7-session-16.v
@@ -400,6 +400,14 @@ Proof.
   end).
 -
 (intros ta1 ta2 Hat1 IHta1 Hat2 IHta2).
-(assert (Hnf : InNF( TPair ta1 ta2)) by (do 2 cnstructor; assumption)).
+(assert (Hnf : InNF( TPair ta1 ta2)) by (do 2 constructor; assumption)).
+(split; intros t'; induction t'; intros Hnf';
+  try
+   match goal with
+   | Hnf':InNF( TUnion _ _) |- _ => destruct (in_nf_union__inv _ _ Hnf') as [Hnf'1 Hnf'2]
+   | Hnf':InNF( TPair _ _) |- _ => destruct (in_nf_pair__inv _ _ Hnf') as [Hnf'1 Hnf'2]
+   end; try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 | solve_union_sub_r__decidable IHt'1 IHt'2 ])).
++
+(destruct (IHta1 t2_1) as [IH1| IH1]; destruct (IHta2 t2_2) as [IH2| IH2]).
 (* Auto-generated comment: Failed. *)
 
