4b66dc0c9036467016f81f90fdee4f1c938561ee
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:26:11 -0700

    user 8, session 79, part 695

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -1009,15 +1009,6 @@ reflexivity.
 (simpl in *).
 specialize inSeq_correct as IS.
 (simpl in IS).
-(repeat (rewrite IS; compile_typing compile_WT)).
-(unfold compose_super).
-Timeout 1 About rewrite_inPar.
-Timeout 1 Print rewrite_inPar.
-Timeout 1 Print Ltac rewrite_inPar.
-(match goal with
- | |-
-   context [ (@denote_box true ?W ?W' (@inPar ?W1 ?W1' ?W2 ?W2' ?f ?g))
-               (@kron ?m ?n ?o ?p ?\207\1291 ?\207\1292) ] => idtac f
- end).
+(rewrite IS).
 (* Auto-generated comment: Succeeded. *)
 
