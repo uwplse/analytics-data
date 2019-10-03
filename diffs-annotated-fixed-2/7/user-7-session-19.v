22ab9c17a10a0916453ba69647943c7979717b37
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:44 -0700

    user 7, session 19, part 372

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -191,7 +191,7 @@ Proof.
  | Hsem:||-[ ?k][?t1]<= [?t2]
    |- | ?t1 | <= | ?t2 | =>
        assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
-        (Hsem _ Hm)
+        (Hsem _ Hm); contradiction
  end).
 (* Auto-generated comment: Failed. *)
 
