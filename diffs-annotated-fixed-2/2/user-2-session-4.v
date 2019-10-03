a338f14ca07e4c822032ebcff6ba8c424f089d07
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:41 -0700

    user 2, session 4, part 3

--- diffs-annotated-fixed-2/2/user-2-session-4.v
+++ diffs-annotated-fixed-2/2/user-2-session-4.v
@@ -7,17 +7,5 @@ Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Test Debug Analytics.
-Require Import Coq.Strings.String.
-Redirect "/tmp/coqsRY5JP" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Inductive term :=
-  | Nil : term
-  | Ident : string -> term
-  | Cons : term -> term -> term
-  | App : term -> term -> term.
-Redirect "/tmp/coqn5gNxo" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
 (* Auto-generated comment: Succeeded. *)
 
