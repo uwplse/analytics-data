28ccb4e1cd2d61b232e27430c572775665d10097
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:26 -0700

    user 7, session 159, part 40

--- diffs-annotated-fixed-2/7/user-7-session-159.v
+++ diffs-annotated-fixed-2/7/user-7-session-159.v
@@ -94,6 +94,5 @@ assumption.
 +
 (rewrite subst_union).
 (apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
 (* Auto-generated comment: Failed. *)
 
