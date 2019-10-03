a7d0a28418189b7a2b2aa94e591706a0f29ba651
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:08 -0700

    user 1, session 34, part 1

--- diffs-annotated-fixed-2/1/user-1-session-34.v
+++ diffs-annotated-fixed-2/1/user-1-session-34.v
@@ -2,7 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Export Arith.
-Require Export Lia.
 (* Auto-generated comment: Succeeded. *)
 
