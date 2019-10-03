6ad5a236a9c510a23634cf9be2cc552c06e60426
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:49 -0700

    user 7, session 2, part 13

--- diffs-annotated-fixed-2/7/user-7-session-2.v
+++ diffs-annotated-fixed-2/7/user-7-session-2.v
@@ -28,6 +28,6 @@ Proof.
 subst.
 Search -mk_nf.
 (assert (Hnf : InNF( MkNF( t))) by apply mk_nf__in_nf).
-(rewrite (mk_nf_nf__equal t Hnf) in Hsub).
+(rewrite (mk_nf_nf__equal (MkNF( t)) Hnf) in Hsub).
 (* Auto-generated comment: Failed. *)
 
