b0d4cbb34790add59f86df011e681d3eec6f5140
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:24:21 -0700

    user 8, session 65, part 25

--- diffs-annotated-fixed-2/8/user-8-session-65.v
+++ diffs-annotated-fixed-2/8/user-8-session-65.v
@@ -962,23 +962,6 @@ Proof.
 (destruct safe).
 -
 (rewrite (assert_at_spec_safe b m i); [  | omega ]).
-gen \207\129.
-(rewrite size_ntensor).
-(simpl).
-(rewrite Nat.mul_1_r).
-(intros \207\129).
-Timeout 1 About restore_dims.
-Timeout 1 Print restore_dims.
-Timeout 1 Print Ltac restore_dims.
-(match goal with
- | |- ?A => let A' := restore_dims_rec tac A in
-            replace
-            A
-            with
-            A'
- end).
-2: {
-(do 4 (apply f_equal_gen; trivial)).
-(rewrite size_ntensor).
-(* Auto-generated comment: Succeeded. *)
+restore_dims.
+(* Auto-generated comment: Failed. *)
 
