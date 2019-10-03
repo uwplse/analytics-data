c1f6375ff817566a9d8d347d9b834eeae0c44e5b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:38 -0700

    user 7, session 19, part 261

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -129,6 +129,7 @@ auto using match_ty_i_pair.
 (intros v1 t3 k Hm1 Hm2).
 (induction t3; try (solve [ destruct k; simpl in Hm2; contradiction ])).
 +
-(apply match_ty_union__inv in Hm2).
+(apply match_ty_i_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
 (* Auto-generated comment: Failed. *)
 
