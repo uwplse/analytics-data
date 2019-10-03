4a77a8a9ac080f16b14dda54be5a03a4ad8f1df0
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:37:54 -0700

    user 3, session 3117, part 45

--- diffs-annotated-fixed-2/3/user-3-session-3117.v
+++ diffs-annotated-fixed-2/3/user-3-session-3117.v
@@ -114,10 +114,7 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Check Ascii.nat_ascii_bounded.
+Theorem N_ascii_bounded : forall a, (Ascii.N_of_ascii a < 256)%N.
 Theorem nat_ascii_bounded : forall a, Ascii.nat_of_ascii a < 256.
-Proof.
-(intro a; unfold Ascii.nat_of_ascii).
-(change_no_check 256 with (N.to_nat 256)).
-(rewrite <- Nat.compare_lt_iff, <- N2Nat.inj_compare, N.compare_lt_iff).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
