b27b33001fd1a7492bf4e9d341328936fb532242
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:18 -0700

    user 7, session 153, part 6

--- diffs-annotated-fixed-2/7/user-7-session-153.v
+++ diffs-annotated-fixed-2/7/user-7-session-153.v
@@ -12,6 +12,9 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-Lemma build_v : forall (X X' : id) (w : nat) (tx v t : ty), |-[ w] v <$ [X := tx] t -> exists v' : ty, |-[ w] v <$ [X := X'] t.
+Lemma build_v : forall (X X' : id) (w : nat) (tx v t : ty), |-[ w] v <$ [X := tx] t -> exists v' : ty, |-[ w] v <$ [X := TVar X'] t.
+Proof.
+(intros X X' w tx v t).
+(induction t'; intros Hm).
 (* Auto-generated comment: Failed. *)
 
