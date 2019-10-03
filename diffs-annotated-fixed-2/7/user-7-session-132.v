db287add683955a09b359387e9822a8c7768a7bb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:03 -0700

    user 7, session 132, part 5

--- diffs-annotated-fixed-2/7/user-7-session-132.v
+++ diffs-annotated-fixed-2/7/user-7-session-132.v
@@ -94,5 +94,7 @@ exists w1.
 (destruct Hm as [tx Heq]; subst).
 (destruct w1; simpl; tauto).
 -
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [tx [Heq Href]]; subst).
 (* Auto-generated comment: Failed. *)
 
