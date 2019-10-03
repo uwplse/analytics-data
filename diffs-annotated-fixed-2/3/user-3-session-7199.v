347cd20736253f2c8f1a112272272474a9284f4b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:37 -0700

    user 3, session 7199, part 44

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -149,8 +149,7 @@ Theorem get_at_ok a :
     (fun (_ : unit) state =>
      {|
      pre := a < length state;
-     post := fun r state' =>
-             state' = state /\ diskGet state (log_addr a) = Some r;
+     post := fun r state' => state' = state /\ diskGet state a = Some r;
      recovered := fun _ state' => state' = state |}) 
     (get_at a) recover abstr.
 Proof.
@@ -163,6 +162,6 @@ Proof.
 (descend; intuition eauto).
 (descend; intuition eauto).
 (apply disk_inbounds_exists in H; intuition eauto).
-(rewrite H in *).
+(unfold log_abstraction in H0).
 (* Auto-generated comment: Succeeded. *)
 
