b549972427e23675d8bd1a73562a51a7af57b83e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:54 -0700

    user 7, session 19, part 540

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -316,7 +316,7 @@ Lemma sem_sub_k_i__inv_depth_le_2 : forall (k : nat) (t t' : ty), | t' | <= k ->
 Proof.
 (intros k t t' Hdept' Hsem).
 (rewrite <- inv_depth_mk_nf).
-(apply sem_sub_k_i_nf__inv_depth_le_1 with k).
+(apply sem_sub_k_i_nf__inv_depth_le_2 with k).
 (apply mk_nf__in_nf).
 (rewrite inv_depth_mk_nf; assumption).
 (* Auto-generated comment: Failed. *)
