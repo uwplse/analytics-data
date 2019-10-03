c8ae1fc234fe5dda3311a05b3dbde5564db436b6
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:24 -0700

    user 7, session 19, part 13

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -51,5 +51,7 @@ Qed.
 Lemma match_ty_i_k__match_le_k : forall (k : nat) (v t : ty), |-[ k] v <$ t -> forall k' : nat, k' <= k -> |-[ k] v <$ t.
 Proof.
 (induction k; intros v t; generalize dependent v; induction t; intros v Hm k' Hle).
+-
+(apply match_ty_i_cname__inv in Hm).
 (* Auto-generated comment: Failed. *)
 
