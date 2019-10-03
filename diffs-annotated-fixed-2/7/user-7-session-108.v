36e6dd32faeb711e0093015c88a22bbc5686c355
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:36 -0700

    user 7, session 108, part 56

--- diffs-annotated-fixed-2/7/user-7-session-108.v
+++ diffs-annotated-fixed-2/7/user-7-session-108.v
@@ -271,6 +271,8 @@ admit.
 admit.
 -
 (apply Hnotm).
-(exists (TRef t); apply match_ty__reflexive; constructor).
+exists (TRef t).
+(apply match_ty_value_type__reflexive; constructor).
+-
 (* Auto-generated comment: Failed. *)
 
