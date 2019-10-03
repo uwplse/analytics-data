4f82920412622128461cb50f062504248f5501c5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:52 -0700

    user 7, session 19, part 500

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -239,6 +239,11 @@ tauto.
 Qed.
 Lemma match_ty_nf : forall (k : nat) (t : ty), ||-[ k][t]= [MkNF( t)].
 Proof.
-(induction k; induction t; intros v; split; intros Hm).
+(induction k; induction t; intros v; split; intros Hm; try (solve [ simpl; assumption ])).
+Admitted.
+Lemma sem_sub_k__i__trans : forall (k : nat) (t1 t2 t3 : ty), ||-[ k][t1]<= [t2] -> ||-[ k][t2]<= [t3] -> ||-[ k][t1]<= [t3].
+Proof.
+auto with DBBetaJulia.
+Qed.
 (* Auto-generated comment: Failed. *)
 
