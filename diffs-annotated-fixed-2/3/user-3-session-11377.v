ac51c2159fb63b4dc25edb900462c8b6646928e3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:05 -0700

    user 3, session 11377, part 6

--- diffs-annotated-fixed-2/3/user-3-session-11377.v
+++ diffs-annotated-fixed-2/3/user-3-session-11377.v
@@ -199,6 +199,27 @@ reflexivity.
 -
 (simpl).
 (rewrite IHn').
+(rewrite plus_swap).
 reflexivity.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqHU0aZp"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
+Theorem mult_comm : forall m n : nat, m * n = n * m.
+Proof.
+(intros m n).
+(induction m as [| m' IHm']).
+-
+(simpl).
+(rewrite <- mult_n_O).
+reflexivity.
+-
+(simpl).
+(rewrite IHm').
+(rewrite <- mult_n_Sm).
+(rewrite plus_comm).
 (* Auto-generated comment: Failed. *)
 
