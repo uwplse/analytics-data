79b9cf8f42d12d55192a92ad15b18b95b4a1cff7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:01 -0700

    user 7, session 93, part 13

--- diffs-annotated-fixed-2/7/user-7-session-93.v
+++ diffs-annotated-fixed-2/7/user-7-session-93.v
@@ -49,7 +49,7 @@ Reserved Notation "'[' x ':=' s ']' t" (at level 30).
 Fixpoint subst (x : id) (s t : ty) :=
   match t with
   | TCName _ => t
-  | TPair t1 t2 => TPair (subst x s t1) (subst x s t2)
+  | TPair t1 t2 => TPair ([x := s] t1) (subst x s t2)
   | TUnion t1 t2 => TUnion (subst x s t1) (subst x s t2)
   | TRef t' => TRef (subst x s t')
   | TExist y t' => TExist y (if beq_id x y then t' else subst x s t')
