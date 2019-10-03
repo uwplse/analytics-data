80cdf1137c298441f208f6bc94eb8c65130739cd
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:20 -0700

    user 1, session 41, part 1

--- diffs-annotated-fixed-2/1/user-1-session-41.v
+++ diffs-annotated-fixed-2/1/user-1-session-41.v
@@ -365,27 +365,7 @@ Fixpoint Gamma (G : GT) : SetST :=
                         end
                     end))) l)
   end.
-Inductive Alpha : SetST -> GT -> Prop :=
-  | alpha_int : Alpha (Singleton _ SInt) GInt
-  | alpha_bool : Alpha (Singleton _ SBool) GBool
-  | alpha_fun :
-      forall S G_1 G_2,
-      Inhabited _ S ->
-      (forall X,
-       Ensembles.In _ S X -> exists S_1 S_2, X = SFun S_1 S_2) ->
-      Alpha
-        (SetPMap S
-           (fun S =>
-            match S with
-            | SFun S_1 S_2 => Some S_1
-            | _ => None
-            end)) G_1 ->
-      Alpha
-        (SetPMap S
-           (fun S =>
-            match S with
-            | SFun S_1 S_2 => Some S_2
-            | _ => None
-            end)) G_2 -> Alpha S (GFun G_1 G_2).
+Search -Ensemble -(list _).
+Search -(Ensemble (list _)).
 (* Auto-generated comment: Succeeded. *)
 
