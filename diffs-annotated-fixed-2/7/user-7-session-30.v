016c0e0ba3aa14a4cc89a4186c2f93baa6590b63
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:20 -0700

    user 7, session 30, part 2

--- diffs-annotated-fixed-2/7/user-7-session-30.v
+++ diffs-annotated-fixed-2/7/user-7-session-30.v
@@ -15,6 +15,15 @@ Lemma match_ty_cname__inv : forall (v : ty) (c : cname) (k : nat), |-[ k] v <$ T
 Proof.
 (intros v; induction v; try (solve [ intros c k Hm; destruct k; contradiction ])).
 (intros c0 k Hm).
-(destruct k; reflexivity).
+(destruct k; simpl in Hm; subst; reflexivity).
+Qed.
+Lemma match_ty_pair__inv :
+  forall (v t1 t2 : ty) (k : nat), |-[ k] v <$ TPair t1 t2 -> exists v1 v2 : ty, v = TPair v1 v2 /\ |-[ k] v1 <$ t1 /\ |-[ k] v2 <$ t2.
+Proof.
+(intros v; induction v; try (solve [ intros t1 t2 k Hm; destruct k; contradiction ])).
+(intros t1 t2 k Hm).
+exists v1,v2.
+(split; try reflexivity).
+(simpl in Hm).
 (* Auto-generated comment: Failed. *)
 
