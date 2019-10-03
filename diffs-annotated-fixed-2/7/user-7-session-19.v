0036d6dbe633e58ae95c79ce9e182b6d0dac09cf
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:42 -0700

    user 7, session 19, part 324

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -162,11 +162,5 @@ Proof.
              (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
                (H _ Hm); contradiction)
      end ])).
-(match goal with
- | |- | ?t1 | <= | ?t2 | =>
-       assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize (H _ Hm);
-        apply match_ty_i_union__inv in H; rewrite inv_depth_union; destruct H as [Hm1| Hm2];
-        [ apply Nat.le_trans with (| t'1 |) | apply Nat.le_trans with (| t'2 |) ]; try apply Max.le_max_l || apply Max.le_max_r
- end).
 (* Auto-generated comment: Failed. *)
 
