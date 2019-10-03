cb6c75b2454bb381730fab1e4eefca7da50e9ebf
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:03 -0700

    user 7, session 19, part 685

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -502,5 +502,39 @@ Proof.
          intros t1 t2 Hnf1 IH1 Hnf2 IH2 Hdep; destruct (max_inv_depth_le__components_le _ _ _ Hdep) as [Hdep1 Hdep2]; intros t' Hsem;
           apply sem_sub_k_union_l__inv in Hsem; destruct Hsem as [Hsem1 Hsem2]; constructor; auto
    end).
+-
+(intros t Hnft _ Hdep).
+(inversion Hdep).
+-
+(intros t Hnft _ Hdep t2).
+(assert (Hva : value_type (TRef t)) by constructor).
+(assert (Hma : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption)).
+(induction t2; intros Hsem; try (solve [ specialize (Hsem _ Hma); contradiction ])).
++
+(apply value_sem_sub_k_i_union__inv in Hsem; try assumption).
+(destruct Hsem as [Hsem| Hsem]; [ apply union_right_1 | apply union_right_2 ]; auto).
++
+(simpl in Hdep).
+(pose proof (le_S_n _ _ Hdep) as Hdep').
+(pose proof Hsem as Hsem').
+(unfold sem_sub_k_i in Hsem).
+specialize (Hsem _ Hma).
+(apply match_ty_i_ref__inv in Hsem).
+(destruct Hsem as [t' [Heqt' Href]]).
+(inversion Heqt'; subst).
+clear Heqt'.
+constructor.
+{
+(apply IHk; try assumption).
+(destruct (sem_eq_k_i__sem_sub_k_i _ _ _ Href); assumption).
+}
+(apply SD_Trans with (MkNF( t2))).
+(apply mk_nf__sub_d2; assumption).
+{
+(apply IHk).
+(apply mk_nf__in_nf).
+(rewrite inv_depth_mk_nf).
+(pose proof (sem_eq_k_i__inv_depth_eq_1 _ _ _ Hdep' Href) as Hdepeq).
+(rewrite <- Hdepeq; assumption).
 (* Auto-generated comment: Failed. *)
 
