8dd3d357442cf9b8e6e56fec8aaffa50cc865002
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:13 -0700

    user 1, session 37, part 30

--- diffs-annotated-fixed-2/1/user-1-session-37.v
+++ diffs-annotated-fixed-2/1/user-1-session-37.v
@@ -301,6 +301,14 @@ Inductive GT : Type :=
   | GRec : list (option (Ann * GT)) -> GT
   | GRow : list (option (option (Ann * GT))) -> GT.
 Definition SetST := Ensemble ST.
-Search -Ensembles.
-(* Auto-generated comment: Failed. *)
+Search -Ensemble.
+Fixpoint Gamma (G : GT) : SetST :=
+  match G with
+  | GDyn => Full_set _
+  | GInt => Singleton _ SInt
+  | GBool => Singleton _ SBool
+  | GFun G_1 G_2 => zipWith_ensembles SFun (Gamma G_1) (Gamma G_2)
+  | _ => Empty_set _
+  end.
+(* Auto-generated comment: Succeeded. *)
 
