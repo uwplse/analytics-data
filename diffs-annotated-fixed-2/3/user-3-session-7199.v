d2e6d43484edaf28766bf45efac9c38bb05d27eb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:04:02 -0700

    user 3, session 7199, part 460

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -450,6 +450,6 @@ Proof.
 (unfold log_abstraction; intuition).
 -
 (unfold log_length_ok in *; intros).
-autorewrite with upd in *.
+(rewrite diskUpd_eq in H2).
 (* Auto-generated comment: Succeeded. *)
 
