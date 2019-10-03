ef9a0ab0db3d942062005725a381eba1f20140bb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:55 -0700

    user 7, session 19, part 553

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -374,8 +374,7 @@ clear IHk' IHt.
 (simpl in Htk, Htk').
 (apply le_S_n in Htk).
 (apply le_S_n in Htk').
-(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v; pose proof (Href v) as Hrefv;
-  assert (Hdepeq : | t' | = | t |) by apply (sem_eq_k_i__inv_depth_eq_2 _ _ _ Htk Href); pose proof Htk as Ht'k; pose proof Htk' as Ht'k';
-  rewrite <- Hdepeq in Ht'k, Ht'k'; pose proof (IHk k' t Htk Htk' v) as Ht).
+(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v; pose proof (Href v) as Hrefv).
+(assert (Hdepeq : | t' | = | t |) by apply (sem_eq_k_i__inv_depth_eq_2 _ _ _ Htk Href)).
 (* Auto-generated comment: Failed. *)
 
