1addba3c5d6e495c8df0a468978f60b0d3f491b2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:48 -0700

    user 7, session 19, part 430

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -194,6 +194,10 @@ Proof.
 (induction k; induction t; induction t'; intros Hnft Hsem; try (solve [ simpl; constructor ]);
   try (solve
    [ match goal with
+     | Hsem:||-[ ?k][?t]<= [?t']
+       |- | ?t | <= | ?t' | =>
+           assert (Hv : value_type t) by constructor; assert (Hm : |-[ k] t <$ t) by (apply match_ty_i__reflexive; assumption); specialize
+            (Hsem _ Hm); contradiction
      | Hsem:||-[ ?k][TPair ?t1 ?t2]<= [TUnion ?t'1 ?t'2]
        |- _ =>
            assert (Hv : value_type (TPair t1 t2)) by (apply in_nf_pair__value_type; assumption);
@@ -212,13 +216,6 @@ Proof.
        |- _ =>
            destruct (sem_sub_k_union_l__inv _ _ _ _ Hsem) as [HSem1 Hsem2]; destruct (in_nf_union__inv _ _ Hnft) as [Hnft1 Hnft2];
             rewrite inv_depth_union; apply Nat.max_lub; auto
-     end ]);
-  try
-   match goal with
-   | Hsem:||-[ ?k][?t]<= [?t']
-     |- | ?t | <= | ?t' | =>
-         assert (Hv : value_type t) by constructor; assert (Hm : |-[ k] t <$ t) by (apply match_ty_i__reflexive; assumption); specialize
-          (Hsem _ Hm); contradiction
-   end).
+     end ])).
 (* Auto-generated comment: Failed. *)
 
