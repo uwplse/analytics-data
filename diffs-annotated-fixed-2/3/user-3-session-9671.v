f9da9dbe493e124530bc15434b32afd8272b8443
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:11:21 -0700

    user 3, session 9671, part 6

--- diffs-annotated-fixed-2/3/user-3-session-9671.v
+++ diffs-annotated-fixed-2/3/user-3-session-9671.v
@@ -465,9 +465,7 @@ step.
 (destruct a' as [[] bs]; simpl in *).
 intuition eauto.
 step.
-(exists bs; intuition).
+(exists bs; intuition eauto).
 {
-eauto.
-}
 (* Auto-generated comment: Succeeded. *)
 
