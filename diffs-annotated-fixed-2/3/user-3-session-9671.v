9c155d812ff23e320ccb2c10951d83923262aea1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:11:22 -0700

    user 3, session 9671, part 25

--- diffs-annotated-fixed-2/3/user-3-session-9671.v
+++ diffs-annotated-fixed-2/3/user-3-session-9671.v
@@ -528,5 +528,17 @@ intuition eauto.
 }
 {
 (exists []; intuition eauto).
+(eapply log_abstraction_nil; eauto).
+(rewrite diskUpd_eq; eauto).
+}
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqgWxRc5"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
+End Log.
 (* Auto-generated comment: Succeeded. *)
 
