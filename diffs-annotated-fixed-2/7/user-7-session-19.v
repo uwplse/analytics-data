f405f98a46f096a93c036302c928a79bda6af410
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:59 -0700

    user 7, session 19, part 611

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -406,6 +406,11 @@ clear IHt.
 (destruct k).
 (destruct k'; inversion Hle).
 assumption.
+(apply match_ty_i_ref__inv in Hm).
+(destruct Hm as [t' [Heq Href]]; subst).
 (destruct k').
+constructor.
+(apply le_S_n in Hle).
+(simpl).
 (* Auto-generated comment: Failed. *)
 
