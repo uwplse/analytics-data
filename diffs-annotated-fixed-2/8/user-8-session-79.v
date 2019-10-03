8aa500b8aff2737752ec5bd897e56c2bf861c99a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:32 -0700

    user 8, session 79, part 284

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -676,6 +676,6 @@ Timeout 1 About length_app.
 (apply f_equal_gen; trivial).
 (repeat rewrite app_length).
 unify_pows_two.
-(rewrite Nat.add_pow_r).
-(* Auto-generated comment: Failed. *)
+(rewrite Nat.pow_add_r).
+(* Auto-generated comment: Succeeded. *)
 
