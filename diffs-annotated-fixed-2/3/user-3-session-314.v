d562d6c7a3973a0bad669f9983191dba7f793be8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:11:42 -0700

    user 3, session 314, part 1

--- diffs-annotated-fixed-2/3/user-3-session-314.v
+++ diffs-annotated-fixed-2/3/user-3-session-314.v
@@ -83,6 +83,17 @@ Lemma read_op_ok :
 Proof.
 (intros).
 (eapply op_spec_sound).
-Qed.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq5kkA0H"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqWDv50N"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
 (* Auto-generated comment: Succeeded. *)
 
