765d8a732ca9efb1006db8d08a2aa0d296047e6b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:16 -0700

    user 7, session 101, part 23

--- diffs-annotated-fixed-2/7/user-7-session-101.v
+++ diffs-annotated-fixed-2/7/user-7-session-101.v
@@ -94,6 +94,7 @@ clear Hm.
 (inversion Href).
 -
 (apply match_ty_exist__inv in Hcontra).
-(destruct Hcontra as [t' Hcontra]).
+(destruct Hcontra as [t Hcontra]).
+(apply match_ty_ref__inv in Hcontra).
 (* Auto-generated comment: Failed. *)
 
