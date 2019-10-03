c7147127b90c85054e80db4a3f7243502e5884d5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:12:12 -0700

    user 3, session 9904, part 77

--- diffs-annotated-fixed-2/3/user-3-session-9904.v
+++ diffs-annotated-fixed-2/3/user-3-session-9904.v
@@ -99,32 +99,6 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Qed.
-Theorem mean_ok : proc_spec mean_spec mean recover abstr.
-Proof.
-(unfold mean).
-(intros).
-(apply spec_abstraction_compose; simpl).
-step_proc.
-(destruct a'; simpl in *; intuition idtac).
-(destruct (r == 0)).
--
-(step_proc; intuition).
-(exists s; intuition).
-left.
-intuition.
-(unfold statdb_abstraction in *).
-(destruct s; intuition).
-(simpl in *).
-lia.
--
-(step_proc; intuition).
-(step_proc; intuition).
-(destruct s; intuition).
-+
-exfalso.
-(unfold statdb_abstraction in *; simpl in *).
-intuition.
-+
-(exists (n0 :: s); intuition).
-(* Auto-generated comment: Succeeded. *)
+Opaque Nat.di.
+(* Auto-generated comment: Failed. *)
 
