8a44bd4d4b74af91755820d21df5d4b4eab5fc5c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:16:00 -0700

    user 3, session 11016, part 42

--- diffs-annotated-fixed-2/3/user-3-session-11016.v
+++ diffs-annotated-fixed-2/3/user-3-session-11016.v
@@ -540,12 +540,6 @@ Theorem rev_involutive : forall l : natlist, rev (rev l) = l.
 Proof.
 (induction l; simpl; auto).
 (rewrite rev_app_distr; simpl).
-(rewrite IHl; auto).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqRREFch"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+auto.
 (* Auto-generated comment: Succeeded. *)
 
