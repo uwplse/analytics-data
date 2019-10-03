d2b51d6739f20ed991334d0bce820e035bf83839
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:12 -0700

    user 8, session 79, part 39

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -597,12 +597,5 @@ Proof.
 (intros).
 (simpl).
 Set Printing All.
-(remember (length l2) as L).
-Timeout 1 About Msimpl_light.
-Timeout 1 Print Msimpl_light.
-Timeout 1 Print Ltac Msimpl_light.
-Timeout 1 About Matrix.Msimpl_light.
-Timeout 1 Print Matrix.Msimpl_light.
-Timeout 1 Print Ltac Matrix.Msimpl_light.
 (* Auto-generated comment: Succeeded. *)
 
