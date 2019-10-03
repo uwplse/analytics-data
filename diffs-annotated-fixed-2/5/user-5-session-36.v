5faddb8c03b98f7466a80f99e26af41ebd380087
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:43 -0700

    user 5, session 36, part 11

--- diffs-annotated-fixed-2/5/user-5-session-36.v
+++ diffs-annotated-fixed-2/5/user-5-session-36.v
@@ -258,7 +258,7 @@ reflexivity.
 clear H.
 (rewrite evalAnd in H0).
 (match goal with
- | H:eval ?L ?env (If (In ?x ?S) _ _) |- _ => idtac
+ | H:eval ?L ?env (If (In ?x ?S) _ _) = _ |- _ => idtac
  end).
 (* Auto-generated comment: Succeeded. *)
 
