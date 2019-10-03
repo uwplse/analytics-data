9674ed1d7eba344a178345b5bc22f89a1d1c6830
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:47 -0700

    user 7, session 19, part 415

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -217,6 +217,6 @@ Proof.
 (destruct (sem_sub_k_union_l__inv _ _ _ _ Hsem) as [HSem1 Hsem2]).
 (destruct (in_nf_union__inv _ _ Hnft) as [Hnft1 Hnft2]).
 SearchPattern (Nat.max _ _ <= _).
-(simpl).
+(apply Nat.max_lub).
 (* Auto-generated comment: Failed. *)
 
