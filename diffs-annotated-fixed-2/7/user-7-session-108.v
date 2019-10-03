ace369908f56aff49861d445a8912477b975fa08
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:36 -0700

    user 7, session 108, part 60

--- diffs-annotated-fixed-2/7/user-7-session-108.v
+++ diffs-annotated-fixed-2/7/user-7-session-108.v
@@ -278,7 +278,22 @@ exists (TRef t).
 admit.
 -
 (apply Hnotm).
-exists (TEV X).
+exists (TEV i).
 (destruct k; reflexivity).
+-
+(apply Hnotm).
+exists (TEV i).
+(destruct k; reflexivity).
+-
+admit.
+-
+admit.
+-
+admit.
+-
+admit.
+-
+(destruct Hcontra as [v Hcontra]).
+(destruct (dec_id X i)).
 (* Auto-generated comment: Failed. *)
 
