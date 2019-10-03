e2958e1cb12a27d4f3e38024cb0271e1def8f554
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:50 -0700

    user 7, session 176, part 26

--- diffs-annotated-fixed-2/7/user-7-session-176.v
+++ diffs-annotated-fixed-2/7/user-7-session-176.v
@@ -144,8 +144,6 @@ Lemma match_ty__subst_neq_permute :
 Proof.
 (intros X Y sx sy w).
 (induction w; intros v t HXY; generalize dependent t; induction t; try (solve [ split; intros Hm; assumption ])).
-(split; repeat rewrite subst_pair; intros Hm; apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst;
-  destruct (IHt1 _ Hm1) as [v1' Hm1']; destruct (IHt2 _ Hm2) as [v2' Hm2']; exists (TPair v1' v2'); rewrite subst_pair; 
-  apply match_ty_pair; assumption).
+(split; repeat rewrite subst_pair; intros Hm; apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
 (* Auto-generated comment: Failed. *)
 
