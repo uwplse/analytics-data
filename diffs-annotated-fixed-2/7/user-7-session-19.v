775546d34862963034c9e87ce1b714086db950aa
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:56 -0700

    user 7, session 19, part 563

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -388,122 +388,12 @@ tauto.
 (pose proof (IHk k' t' Ht'k Ht'k' v) as Ht').
 tauto.
 Admitted.
-Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
+Lemma value_sem_sub_i_union__inv : forall v : ty, value_type v -> forall ta tb : ty, ||- [v]<= [TUnion ta tb] -> ||- [v]<= [ta] \/ ||- [v]<= [tb].
 Proof.
-(induction k; induction t; induction t'; intros H; try (solve [ simpl; constructor ]);
-  try (solve
-   [ match goal with
-     | |- | ?t1 | <= | ?t2 | =>
-           (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
-             (H _ Hm); contradiction) ||
-             (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
-               (H _ Hm); contradiction)
-     end ])).
-Abort.
-Lemma match_ty_i_t_le_k__v_ke_t : forall (k : nat) (t : ty), | t | <= k -> forall v : ty, |-[ k] v <$ t -> | v | <= | t |.
-Proof.
-(induction k; induction t; intros Htk v Hm;
-  try
-   match goal with
-   | Hm:|-[ ?k'] ?v <$ TCName _ |- _ => apply match_ty_i_cname__inv in Hm; subst; constructor
-   | H:|-[ ?k'] ?v <$ TPair _ _
-     |- _ =>
-         destruct (max_inv_depth_le__components_le _ _ _ Htk) as [Htk1 Htk2]; apply match_ty_i_pair__inv in Hm;
-          destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; apply Nat.max_le_compat; auto
-   | H:|-[ ?k'] ?v <$ TUnion _ _
-     |- _ =>
-         destruct (max_inv_depth_le__components_le _ _ _ Htk) as [Htk1 Htk2]; apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2];
-          [ apply Nat.le_trans with (| t1 |) | apply Nat.le_trans with (| t2 |) ]; auto; apply Max.le_max_l || apply Max.le_max_r
-   end).
--
-(destruct v; try contradiction).
-(inversion Htk).
--
-clear IHt.
-(simpl in Htk).
-(apply le_S_n in Htk).
-(apply match_ty_i_ref__inv in Hm).
-(destruct Hm as [t' [Heq Href]]; subst).
-Abort.
-Lemma match_ty_i_eq__inv_depth_eq :
-  forall t t' : ty, (forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
-Proof.
-(induction t; induction t'; intros H; try reflexivity;
-  try (solve
-   [ match goal with
-     | |- | ?t1 | = | ?t2 | =>
-           (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
-             (H 0 _ Hv); destruct H as [H _]; specialize (H Hm); contradiction) ||
-             (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
-               (H 0 _ Hv); destruct H as [_ H]; specialize (H Hm); contradiction)
-     end ])).
-9: {
-idtac.
-clear IHt'.
-(simpl).
-(apply f_equal).
-(apply IHt).
-(intros k v Hv).
-(assert (Hmt : |-[ S k] TRef t <$ TRef t) by (simpl; tauto)).
-specialize (H (S k) (TRef t)).
-(destruct H as [H _]).
-constructor.
-specialize (H Hmt).
-(apply match_ty_i_ref__inv in H).
-(destruct H as [tx [Heq Href]]).
-(inversion Heq; subst).
-auto.
-}
-Abort.
-Open Scope btjmdeq_scope.
-Lemma match_ty_deq_cname__inv : forall (v : ty) (c : cname), forall k : nat, |-[ k] v <$ TCName c -> v = TCName c.
-Proof.
-(intros v; induction v; try (solve [ intros c k Hm; destruct k; simpl in Hm; contradiction ])).
-(intros c0 k Hm).
-(destruct k; simpl in Hm; subst; reflexivity).
-Qed.
-Lemma match_ty_deq_pair__inv :
-  forall v t1 t2 : ty, forall k : nat, |-[ k] v <$ TPair t1 t2 -> exists v1 v2 : ty, v = TPair v1 v2 /\ |-[ k] v1 <$ t1 /\ |-[ k] v2 <$ t2.
-Proof.
-(intros v; induction v; try (solve [ intros t1 t2 k Hm; destruct k; simpl in Hm; contradiction ])).
-(intros t1 t2 k Hm).
-exists v1,v2.
-(split; try reflexivity).
-(destruct k; simpl in Hm; assumption).
-Qed.
-Lemma match_ty_deq_union__inv : forall v t1 t2 : ty, forall k : nat, |-[ k] v <$ TUnion t1 t2 -> |-[ k] v <$ t1 \/ |-[ k] v <$ t2.
-Proof.
-(intros v t1 t2 k Hm).
-(destruct k; destruct v; assumption).
-Qed.
-Lemma match_ty_deq_ref__inv :
-  forall v t : ty,
-  forall k : nat,
-  |-[ S k] v <$ TRef t ->
-  exists t' : ty, v = TRef t' /\ inv_depth t <= k /\ inv_depth t' = inv_depth t /\ (forall v' : ty, |-[ k] v' <$ t' <-> |-[ k] v' <$ t).
-Proof.
-(intros v; induction v; try (solve [ intros t k Hm; destruct k; simpl in Hm; contradiction ])).
-clear IHv.
-(intros t k).
-(intros Hm).
-(simpl in Hm).
-(destruct Hm as [Hdept [Hdepv Href]]).
-exists v.
-split.
-reflexivity.
-split.
-assumption.
-split.
-assumption.
-(intros v').
-specialize (Href v').
-(destruct Href; split; assumption).
-Qed.
-Close Scope btjmdeq_scope.
-Theorem sem_sub_i__sem_sub_deq : forall t1 t2 : ty, (||- [t1]<= [t2])%btjmi -> (||- [t1]<= [t2])%btjmdeq.
-Proof.
-(intros ta; induction ta; intros tb; induction tb; intros Hsem).
-16: {
-idtac.
+(intros v Hv ta tb Hsem; unfold sem_sub_i in Hsem).
+(assert (Hm : |-[ 0] v <$ v) by (apply match_ty_i__reflexive; assumption)).
+specialize (Hsem 0 _ Hm).
+(apply match_ty_i_union__inv in Hsem).
+(destruct Hsem; [ left | right ]).
 (* Auto-generated comment: Failed. *)
 
