191beb7ef787fa99267cb19ca9c4c09a3f0c16ea
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:44 -0700

    user 8, session 34, part 17

--- diffs-annotated-fixed-2/8/user-8-session-34.v
+++ diffs-annotated-fixed-2/8/user-8-session-34.v
@@ -32,6 +32,5 @@ dependent induction TP.
 (intros w' f \206\1470 \206\1471 \206\1471' \206\14701 WT pf_merge1 pf_merge2).
 (simpl).
 (unfold compose_super).
-(unfold denote_circuit).
 (* Auto-generated comment: Succeeded. *)
 
