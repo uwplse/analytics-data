9f0087c98070c5bd9e36df6f33961629326e164a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:26 -0700

    user 8, session 79, part 207

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -620,6 +620,8 @@ Qed.
 Goal _ forall m n (A B : Matrix m n), A == B -> I 1 \226\138\151 A \226\138\151 I 1 == I 1 \226\138\151 B \226\138\151 I 1.
 Proof.
 (intros).
-(rewrite <- kron_1_l_inv).
+(rewrite kron_1_l).
+(rewrite kron_1_l).
+(rewrite kron_1_r).
 (* Auto-generated comment: Failed. *)
 
