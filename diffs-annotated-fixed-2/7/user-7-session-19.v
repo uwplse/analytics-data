ccc321e8fed2eace8d62b909069ee7fd38556c56
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:49 -0700

    user 7, session 19, part 457

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -199,7 +199,6 @@ Lemma sem_sub_i_ref__inv : forall t t' : ty, ||- [TRef t]<= [TRef t'] -> ||- [t]
 Proof.
 (intros t t' Hsem).
 (split; intros k; specialize (Hsem (S k)); assert (Hvref : value_type (TRef t)) by constructor;
-  assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hm)).
-(simpl in Hsem).
+  assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hm); simpl in Hsem).
 (* Auto-generated comment: Failed. *)
 
