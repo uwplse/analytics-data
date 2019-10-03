5c675b3c99fbd46abd0502db037f97df41bcc63a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:33 -0700

    user 7, session 19, part 171

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -85,6 +85,7 @@ idtac.
 (simpl).
 (apply le_n_S).
 (apply IHk).
-(assert (Hv : value_type (TRef t))).
+(assert (Hv : value_type (TRef t)) by constructor).
+(assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; constructor)).
 (* Auto-generated comment: Failed. *)
 
