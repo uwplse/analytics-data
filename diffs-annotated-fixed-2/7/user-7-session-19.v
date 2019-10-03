13bc8b5996fca85a39708a84d7fec328e0215688
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:39 -0700

    user 7, session 19, part 278

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -154,5 +154,8 @@ idtac.
 (pose proof (Hsem _ Hvref Hmref) as Hmu).
 (apply match_ty_i_union__inv in Hmu).
 (destruct Hmu as [Hmu1| Hmu2]; [ left | right ]; intros v Hv Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst).
+(assert (Hmt't : |-[ S k] TRef t' <$ TRef t)).
+(simpl).
+assumption.
 (* Auto-generated comment: Failed. *)
 
