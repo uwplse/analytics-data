4a54357bb99d35da1cb94a2837c7c88147330fcf
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:03 -0700

    user 7, session 19, part 673

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -497,6 +497,8 @@ Proof.
            | |- context [ |- TCName _ << _ ] => apply cname_sem_sub_k_i__sub_d
            end).
 -
-(intros ta1 ta2 Hat1 IH1 Hat2 IH2).
+(intros ta1 ta2 Hat1 IH1 Hat2 IH2 Hdep).
+(assert (Hat : atom_type (TPair ta1 ta2)) by (constructor; assumption)).
+(apply pair_sem_sub_k_i__sub_d).
 (* Auto-generated comment: Failed. *)
 
