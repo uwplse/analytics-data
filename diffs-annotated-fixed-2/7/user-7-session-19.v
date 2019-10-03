07bae57f88ca54994c105d8a1627df36127494ca
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:30 -0700

    user 7, session 19, part 121

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -89,7 +89,8 @@ clear IHt.
 (apply match_ty_i_ref__inv in Hm).
 (destruct Hm as [t' [Heq Href]]; subst).
 Abort.
-Lemma match_ty_i_eq__inv_depth_eq : forall t t' : ty, (forall (k : nat) (v : ty), |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
+Lemma match_ty_i_eq__inv_depth_eq :
+  forall t t' : ty, (forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
 Proof.
 (induction t; induction t'; intros H).
 reflexivity.
@@ -103,10 +104,5 @@ clear IHt'.
 (assert (Hmt : |-[ S k] TRef t <$ TRef t) by (simpl; tauto)).
 specialize (H (S k) (TRef t)).
 (destruct H as [H _]).
-specialize (H Hmt).
-(apply match_ty_i_ref__inv in H).
-(destruct H as [tx [Heq Href]]).
-(inversion Heq; subst).
-auto.
 (* Auto-generated comment: Failed. *)
 
