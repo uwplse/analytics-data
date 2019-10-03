78ffb18220ca33bbdf4e261ab06104ef65b71963
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:54 -0700

    user 7, session 88, part 22

--- diffs-annotated-fixed-2/7/user-7-session-88.v
+++ diffs-annotated-fixed-2/7/user-7-session-88.v
@@ -431,6 +431,7 @@ specialize (IHt'2 Hnft).
 *
 Check left.
 Print "\/".
-specialize (IHt'1 (or_introl Hdept)).
+(apply Nat.le_trans with (| t'1 |)).
+tauto.
 (* Auto-generated comment: Failed. *)
 
