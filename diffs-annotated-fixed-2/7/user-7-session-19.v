11479f647215d9b96d0596c3533186085d0b4ce1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:48 -0700

    user 7, session 19, part 433

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -311,7 +311,51 @@ clear IHk' IHt.
 (simpl in Htk, Htk').
 (apply le_S_n in Htk).
 (apply le_S_n in Htk').
-(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v Hv; specialize (Href v Hv);
-  simpl in Hvk, Hvk'; apply le_S_n in Hvk; apply le_S_n in Hvk').
-(* Auto-generated comment: Failed. *)
+(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v; specialize (Href v); simpl in Hvk, Hvk';
+  apply le_S_n in Hvk; apply le_S_n in Hvk').
+Abort.
+Open Scope btjmdeq_scope.
+Lemma match_ty_deq_cname__inv : forall (v : ty) (c : cname), forall k : nat, |-[ k] v <$ TCName c -> v = TCName c.
+Proof.
+(intros v; induction v; try (solve [ intros c k Hm; destruct k; simpl in Hm; contradiction ])).
+(intros c0 k Hm).
+(destruct k; simpl in Hm; subst; reflexivity).
+Qed.
+Lemma match_ty_deq_pair__inv :
+  forall v t1 t2 : ty, forall k : nat, |-[ k] v <$ TPair t1 t2 -> exists v1 v2 : ty, v = TPair v1 v2 /\ |-[ k] v1 <$ t1 /\ |-[ k] v2 <$ t2.
+Proof.
+(intros v; induction v; try (solve [ intros t1 t2 k Hm; destruct k; simpl in Hm; contradiction ])).
+(intros t1 t2 k Hm).
+exists v1,v2.
+(split; try reflexivity).
+(destruct k; simpl in Hm; assumption).
+Qed.
+Lemma match_ty_deq_union__inv : forall v t1 t2 : ty, forall k : nat, |-[ k] v <$ TUnion t1 t2 -> |-[ k] v <$ t1 \/ |-[ k] v <$ t2.
+Proof.
+(intros v t1 t2 k Hm).
+(destruct k; destruct v; assumption).
+Qed.
+Lemma match_ty_deq_ref__inv :
+  forall v t : ty,
+  forall k : nat,
+  |-[ S k] v <$ TRef t ->
+  exists t' : ty,
+    v = TRef t' /\ inv_depth t <= k /\ inv_depth t' = inv_depth t /\ (forall v' : ty, value_type v' -> |-[ k] v' <$ t' <-> |-[ k] v' <$ t).
+Proof.
+(intros v; induction v; try (solve [ intros t k Hm; destruct k; simpl in Hm; contradiction ])).
+clear IHv.
+(intros t k).
+(intros Hm).
+(simpl in Hm).
+(destruct Hm as [Hdept [Hdepv Href]]).
+exists v.
+split.
+reflexivity.
+split.
+assumption.
+split.
+assumption.
+(intros v' Hv').
+specialize (Href v' Hv').
+(* Auto-generated comment: Succeeded. *)
 
