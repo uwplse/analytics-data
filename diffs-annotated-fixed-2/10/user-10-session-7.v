074ef6daa3ffa2d711be8d99db8e735566bd6a3a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:28:21 -0700

    user 10, session 7, part 39

--- diffs-annotated-fixed-2/10/user-10-session-7.v
+++ diffs-annotated-fixed-2/10/user-10-session-7.v
@@ -264,7 +264,7 @@ CoFixpoint match_event {X} (e0 : networkE X) (x0 : X) (t : itree tE unit) : itre
   end.
 Definition match_event_list {X} : networkE X -> X -> list (itree tE unit) -> list (itree tE unit) :=
   compose pfmap \226\136\152 match_event.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqRrqKTJ" Print Ltac Signatures.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqBOZR0N" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 (* Auto-generated comment: Succeeded. *)
 
