329bb67b377b862355b61428533478578a39fa1b
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:49 -0700

    user 7, session 83, part 41

--- diffs-annotated-fixed-2/7/user-7-session-83.v
+++ diffs-annotated-fixed-2/7/user-7-session-83.v
@@ -285,7 +285,15 @@ clear IHt.
 (destruct Hm as [t' [Heq Href]]; subst).
 (simpl).
 Check sem_eq_k_i__trans.
-(eapply sem_eq_k_i__trans).
-eassumption.
+(eapply sem_eq_k_i__trans; eauto).
+-
+clear IHt.
+(rewrite mk_nf_ref in Hm).
+(apply match_ty_i_ref__inv in Hm).
+(destruct Hm as [t' [Heq Href]]; subst).
+(simpl).
+(eapply sem_eq_k_i__trans; eauto).
+(apply sem_eq_k_i__comm).
+assumption.
 (* Auto-generated comment: Failed. *)
 
