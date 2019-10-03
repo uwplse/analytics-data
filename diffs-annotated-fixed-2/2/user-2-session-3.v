fae57923f9511a2a547c857a9ae17d98d8b1d5ed
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:40 -0700

    user 2, session 3, part 1

--- diffs-annotated-fixed-2/2/user-2-session-3.v
+++ diffs-annotated-fixed-2/2/user-2-session-3.v
@@ -7,6 +7,16 @@ Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Import Coq.String.String.
-(* Auto-generated comment: Failed. *)
+Redirect "/tmp/coqBdydvw" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Require Import Coq.Strings.String.
+Redirect "/tmp/coqBL0Wgk" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Require Import Analytical.Analytics.
+Redirect "/tmp/coqP4bYp5" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+(* Auto-generated comment: Succeeded. *)
 
