c780d925ab8c91b001cf4da25f28ba679a4e8073
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:27:20 -0700

    user 8, session 79, part 1412

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -1172,5 +1172,7 @@ reflexivity.
 listify_kron.
 (simpl_rewrite (CNOT_at_spec \226\140\136 b2 | f \226\140\137 (\226\140\136 b1 | f \226\140\137 \226\138\149 t) (2 + \226\159\166 \206\147 \226\159\167) 1 0); trivial;
   try omega).
+(simpl).
+reflexivity.
 (* Auto-generated comment: Succeeded. *)
 
