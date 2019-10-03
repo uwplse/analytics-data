26f14a1aeba96627c168cae3272cee693215c6d0
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:48 -0700

    user 3, session 7199, part 220

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -286,6 +286,7 @@ Theorem log_contents_ok_unchanged d bs a0 b :
   log_addr a0 >= length bs -> log_contents_ok (diskUpd d a0 b) bs.
 Proof.
 (unfold log_contents_ok; intros).
-(specialize (H a); intuition).
+(specialize (H0 a); intuition).
+lia.
 (* Auto-generated comment: Failed. *)
 
