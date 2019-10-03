3ab4e5b499a0dc97372b3fb27babdf26f36d644a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:41 -0700

    user 7, session 77, part 4

--- diffs-annotated-fixed-2/7/user-7-session-77.v
+++ diffs-annotated-fixed-2/7/user-7-session-77.v
@@ -59,6 +59,9 @@ Qed.
 Lemma mk_nf__sem_sub_l : forall t : ty, ||- [MkNF( t)]<= [t].
 Proof.
 Check sem_eq__sem_sub.
+(intros t).
 (apply sem_eq__sem_sub).
-(* Auto-generated comment: Failed. *)
+(apply mk_nf__sem_eq).
+Qed.
+(* Auto-generated comment: Succeeded. *)
 
