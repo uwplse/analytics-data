1dbc89c95539b382f1fd6205808eb3654a6123a5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:01 -0700

    user 7, session 128, part 13

--- diffs-annotated-fixed-2/7/user-7-session-128.v
+++ diffs-annotated-fixed-2/7/user-7-session-128.v
@@ -91,6 +91,6 @@ Proof.
 -
 (assert (Hmp : |-[ k, w1'] TPair v v2 <$ TPair t1 t2)).
 {
-(apply match_ty_pair; apply match_ty__ge_w).
+(apply match_ty_pair; eapply match_ty__ge_w; try eassumption).
 (* Auto-generated comment: Failed. *)
 
