5698082b18c8084fd6079f755c97df9ebc147fb7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:04:04 -0700

    user 3, session 7199, part 478

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -430,6 +430,6 @@ Theorem log_contents_ok_len_change d bs b :
   log_contents_ok d bs -> log_contents_ok (diskUpd d len_addr b) bs.
 Proof.
 (unfold log_size_ok, log_contents_ok, len_addr; intros).
-(destruct (a == a0); subst; autorewrite with upd; auto).
-(* Auto-generated comment: Failed. *)
+(destruct (0 == log_addr a); try lia).
+(* Auto-generated comment: Succeeded. *)
 
