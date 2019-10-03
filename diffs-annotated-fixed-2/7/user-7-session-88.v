02a78aa48fed00af1fa551eab53eedc202ee3a92
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:57 -0700

    user 7, session 88, part 62

--- diffs-annotated-fixed-2/7/user-7-session-88.v
+++ diffs-annotated-fixed-2/7/user-7-session-88.v
@@ -415,6 +415,6 @@ tauto.
 Qed.
 Lemma sem_sub_k_i_nf__inv_depth_le_1 : forall (k : nat) (t t' : ty), InNF( t) -> | t | <= k -> ||-[ k][t]<= [t'] -> | t | <= | t' |.
 Proof.
-(apply sem_sub_k_i_nf__inv_depth_le with k; tauto).
+(apply sem_sub_k_i_nf__inv_depth_le; tauto).
 (* Auto-generated comment: Failed. *)
 
