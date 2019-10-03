6aadd232b0768963ad360abe8f5d137fd58a8145
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:51 -0700

    user 7, session 116, part 47

--- diffs-annotated-fixed-2/7/user-7-session-116.v
+++ diffs-annotated-fixed-2/7/user-7-session-116.v
@@ -100,10 +100,9 @@ assumption.
 assumption.
 +
 (simpl).
-Search -beq_id.
 (rewrite (false_beq_id _ _ n)).
 (rewrite (false_beq_id _ _ n0)).
 reflexivity.
 Qed.
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
