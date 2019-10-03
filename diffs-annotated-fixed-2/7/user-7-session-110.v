f5761e4729dd59e2ee4c9b90aa4a9149b4db5d78
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:43 -0700

    user 7, session 110, part 8

--- diffs-annotated-fixed-2/7/user-7-session-110.v
+++ diffs-annotated-fixed-2/7/user-7-session-110.v
@@ -129,6 +129,20 @@ Proof.
    | |- |-[ _, _] _ <$ TCName _ => apply match_ty_cname__inv in Hm; subst; apply match_ty_cname
    | |- |-[ _, _] _ <$ TPair _ _ =>
          apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; apply match_ty_pair; [ eapply IHt1 | eapply IHt2 ]; eauto
+   | |- |-[ _, _] _ <$ TUnion _ _ =>
+         apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; eauto
    end).
+-
+(destruct k).
++
+(apply match_ty_ref__weak_inv in Hm).
+(destruct Hm as [t' Heq]; subst).
+(destruct w'; constructor).
++
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [t' [Heq Href]]; subst).
+(destruct w'; assumption).
+-
+(apply match_ty_exist__0_inv in Hm; contradiction).
 (* Auto-generated comment: Failed. *)
 
