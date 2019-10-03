8e4e815b9bc8b66225020dc66ae9f29fb28d099c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:40 -0700

    user 7, session 19, part 293

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -146,16 +146,5 @@ Lemma value_sem_sub_k_union__value_sem_sub_k_component :
   forall k : nat, forall v : ty, value_type v -> forall ta tb : ty, ||-[ k][v]<= [TUnion ta tb] -> ||-[ k][v]<= [ta] \/ ||-[ k][v]<= [tb].
 Proof.
 (induction k; intros v Hv; induction Hv; intros ta tb Hsem).
-6: {
-idtac.
-(unfold sem_sub_k_i in Hsem).
-(assert (Hvref : value_type (TRef t)) by constructor).
-(assert (Hmref : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption)).
-(pose proof (Hsem _ Hvref Hmref) as Hmu).
-(apply match_ty_i_union__inv in Hmu).
-(destruct Hmu as [Hmu1| Hmu2]; [ left | right ]; intros v Hv Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst;
-  assert (Hmt't : |-[ S k] TRef t' <$ TRef t) by (intros v'; split; intros Hm'; specialize (Href v'); tauto);
-  apply match_ty_i__transitive_on_value_type with (TRef t); assumption).
-}
 (* Auto-generated comment: Failed. *)
 
