ed7e2706e8616d1eb3afff8544ede97b3e5925d5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:40 -0700

    user 7, session 108, part 121

--- diffs-annotated-fixed-2/7/user-7-session-108.v
+++ diffs-annotated-fixed-2/7/user-7-session-108.v
@@ -131,18 +131,5 @@ Qed.
 Lemma sem_sub__pair_exist_distr_1 : forall (X : id) (t1 t2 : ty), ||- [TPair (TExist X t1) t2]<= [TExist X (TPair t1 t2)].
 Proof.
 (intros X t1 t2 k w1).
-exists w1.
-(intros v Hm).
-(apply match_ty_pair__inv in Hm).
-(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(destruct w1).
--
-(apply match_ty_exist__0_inv in Hm1).
-contradiction.
--
-(apply match_ty_exist).
-(apply match_ty_exist__inv in Hm1).
-(destruct Hm1 as [tx Hmx]).
-exists tx.
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
