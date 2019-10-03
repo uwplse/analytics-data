012686730af629786d235b478333f7a908451319
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:35 -0700

    user 7, session 63, part 3

--- diffs-annotated-fixed-2/7/user-7-session-63.v
+++ diffs-annotated-fixed-2/7/user-7-session-63.v
@@ -233,6 +233,7 @@ constructor.
 +
 (destruct k; reflexivity).
 -
-(destruct (max_inv_depth_le__inv _ _ _ Hdep)).
+(destruct (max_inv_depth_le__inv _ _ _ Hdep) as [Hdep1 Hdep2]).
+(destruct IHt1 as [v1 [Hv1 Hm1]]).
 (* Auto-generated comment: Failed. *)
 
