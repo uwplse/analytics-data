d362b454f36281a1cafadbc82f462dc800f9831a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:35 -0700

    user 8, session 79, part 316

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -909,6 +909,6 @@ gen n.
 (simpl in *).
 clear L1 M1 Lt.
 (rewrite strip_one_l_in_eq).
-setoid_rewrite  <- (kron_1_l (\226\168\130 l2)) at 1.
+setoid_rewrite  <- (kron_1_l (\226\168\130 l2)).
 (* Auto-generated comment: Failed. *)
 
