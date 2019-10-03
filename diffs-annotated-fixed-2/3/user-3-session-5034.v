90ae0b4fef144b649e14fa83db30045af031fa04
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:56:38 -0700

    user 3, session 5034, part 3

--- diffs-annotated-fixed-2/3/user-3-session-5034.v
+++ diffs-annotated-fixed-2/3/user-3-session-5034.v
@@ -114,15 +114,6 @@ Proof.
 step_proc.
 (destruct a'; simpl in *; intuition; subst; eauto).
 -
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqc7YM9a"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq8HKFdP"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
+(step_proc; intuition; subst; eauto).
 (* Auto-generated comment: Succeeded. *)
 
