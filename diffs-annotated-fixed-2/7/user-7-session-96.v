2d095cc152fa47c8e57fa1609eb29643be67e5b2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:05 -0700

    user 7, session 96, part 5

--- diffs-annotated-fixed-2/7/user-7-session-96.v
+++ diffs-annotated-fixed-2/7/user-7-session-96.v
@@ -80,6 +80,8 @@ Qed.
 Lemma sem_sub__refint_eXrefX : ||- [TRef tint]<= [TExist vX (TRef tX)].
 Proof.
 (intros k; induction k; intros v Hm).
--
+2: {
+idtac.
+(simpl).
 (* Auto-generated comment: Failed. *)
 
