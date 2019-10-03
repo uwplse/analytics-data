7ff3d5988156a2ad440fa67da24fd7ff9cd50c02
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:13 -0700

    user 7, session 16, part 47

--- diffs-annotated-fixed-2/7/user-7-session-16.v
+++ diffs-annotated-fixed-2/7/user-7-session-16.v
@@ -381,7 +381,11 @@ Proof.
 right.
 (match goal with
  | |- ~ |- ?t1 << ?t2 =>
-       remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ])
+       remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; intros Hcontra; induction Hcontra; try (solve [ inversion Heqx | inversion Heqy ]);
+        subst
+ end).
+(match goal with
+ | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra
  end).
 (* Auto-generated comment: Failed. *)
 
