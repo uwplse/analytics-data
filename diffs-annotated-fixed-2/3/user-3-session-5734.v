8d50caec937de321f2d8356138f70dbbda8b7db6
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:58:58 -0700

    user 3, session 5734, part 1

--- diffs-annotated-fixed-2/3/user-3-session-5734.v
+++ diffs-annotated-fixed-2/3/user-3-session-5734.v
@@ -5,16 +5,17 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-From Coq Require extraction.Extraction.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqI3mC2x"
+Require Coq.extraction.Extraction.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqHRtkWp"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqm91Ids"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqemwNHE"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
+Extraction Language OCaml.
 (* Auto-generated comment: Succeeded. *)
 
