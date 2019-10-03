6676951f644886d55a5910206f9919146c2f192f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:33 -0700

    user 7, session 19, part 172

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -87,5 +87,8 @@ idtac.
 (apply IHk).
 (assert (Hv : value_type (TRef t)) by constructor).
 (assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; constructor)).
+specialize (H _ Hm).
+(apply match_ty_i_ref__inv in H).
+(destruct H as [t' [Heq Href]]; subst).
 (* Auto-generated comment: Failed. *)
 
