9751823f39e61f441108431a879a97dce8960c2d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:27 -0700

    user 7, session 209, part 2

--- diffs-annotated-fixed-2/7/user-7-session-209.v
+++ diffs-annotated-fixed-2/7/user-7-session-209.v
@@ -87,6 +87,9 @@ assumption.
 }
 +
 (apply match_ty_exist__inv in Hm').
-(destruct Hm' as [ti Hm']).
+(destruct Hm' as [ti [Hwf Hm']]).
+(split; intros HX').
+*
+exists ti'.
 (* Auto-generated comment: Failed. *)
 
