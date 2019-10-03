046cc68edb173b953615bd8600db7eac6889f4d7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:54 -0700

    user 3, session 7199, part 328

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -381,6 +381,7 @@ Theorem log_abstraction_preserved d bs d' :
   diskGet d' len_addr = diskGet d len_addr ->
   diskSize d' = diskSize d -> log_contents_ok d' bs -> log_abstraction d' bs.
 Proof.
-(unfold log_abstraction, log_length_ok, log_size_ok; intros).
-(* Auto-generated comment: Succeeded. *)
+(unfold log_abstraction, log_length_ok, log_size_ok; intuition).
+congruence.
+(* Auto-generated comment: Failed. *)
 
