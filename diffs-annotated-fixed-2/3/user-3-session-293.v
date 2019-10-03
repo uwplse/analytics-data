cce4ab262bc757142791c40339a657fca69ff9b7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:11:33 -0700

    user 3, session 293, part 3

--- diffs-annotated-fixed-2/3/user-3-session-293.v
+++ diffs-annotated-fixed-2/3/user-3-session-293.v
@@ -58,5 +58,12 @@ Instance var_crash_step_nonerror :
 Proof.
 (simpl).
 typeclasses eauto.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqbheUZM"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
