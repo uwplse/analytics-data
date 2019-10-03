9de873db59ccc33ba538ec8eb4d526c0ac6b72bf
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:50 -0700

    user 7, session 83, part 49

--- diffs-annotated-fixed-2/7/user-7-session-83.v
+++ diffs-annotated-fixed-2/7/user-7-session-83.v
@@ -338,7 +338,6 @@ Proof.
             destruct (in_nf_union__inv _ _ Hnft) as [Hnft1 Hnft2]; rewrite inv_depth_union; apply Nat.max_lub; auto
      end ])).
 Check match_ty_i__reflexive.
--
-(inversion Hdept; subst).
+Check in_nf_pair__value_type.
 (* Auto-generated comment: Failed. *)
 
