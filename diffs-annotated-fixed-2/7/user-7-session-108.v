7c97cdae9421e40a32faa055f484cea338fdcba0
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:37 -0700

    user 7, session 108, part 75

--- diffs-annotated-fixed-2/7/user-7-session-108.v
+++ diffs-annotated-fixed-2/7/user-7-session-108.v
@@ -293,19 +293,6 @@ admit.
 -
 admit.
 -
-(assert (Hnotm' : ~ (exists v tx, |-[ k, w] v <$ [i := tx] t))).
-{
-(intros [v [tx Hm]]).
-(assert (Hme : |-[ k, S w] v <$ TExist i t)).
-{
-(apply match_ty_exist).
-exists tx.
-assumption.
-}
-(apply Hnotm).
-exists v.
-assumption.
-}
 (destruct Hcontra as [v Hcontra]).
 (destruct (beq_idP X i)).
 +
@@ -328,5 +315,8 @@ admit.
 (rewrite Heq in Hcontra).
 (apply match_ty_exist__inv in Hcontra).
 (destruct Hcontra as [tx Hcontra]).
+(assert (Hnotm' : ~ (exists v, |-[ k, w] v <$ [i := tx] t))).
+{
+(intros [v Hm]).
 (* Auto-generated comment: Failed. *)
 
