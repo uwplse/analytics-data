a4d82eb61d4ebb5c7d68e4fe213d9caeddf2fd97
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:38 -0700

    user 7, session 68, part 4

--- diffs-annotated-fixed-2/7/user-7-session-68.v
+++ diffs-annotated-fixed-2/7/user-7-session-68.v
@@ -195,6 +195,12 @@ constructor.
 (apply IHk; try assumption).
 (apply sem_eq_k__sem_sub_k in Href).
 tauto.
-}
-(* Auto-generated comment: Failed. *)
+*
+(apply SD_Trans with (MkNF( t2))).
+(apply mk_nf__sub_d_r; assumption).
+(apply IHk).
+(apply mk_nf__in_nf).
+(rewrite inv_depth_mk_nf).
+assumption.
+(* Auto-generated comment: Succeeded. *)
 
