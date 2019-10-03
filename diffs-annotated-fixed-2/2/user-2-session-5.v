5b230234ed968edfc9d69db40688fd41b127ce46
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:53 -0700

    user 2, session 5, part 75

--- diffs-annotated-fixed-2/2/user-2-session-5.v
+++ diffs-annotated-fixed-2/2/user-2-session-5.v
@@ -56,10 +56,7 @@ End TermNotations.
 Import TermNotations.
 Open Scope coucou_scope.
 Check
-  [<Nil <Nil # "hi">> (Cons (Ident "1") (Ident "2")) (Ident "a")
+  [<# hello <Nil # "hi">> (Cons (Ident "1") (Ident "2")) (Ident "a")
   {(Ident "myfun") (Ident "somArg")}].
-Redirect "/tmp/coqCUd7Pb" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
