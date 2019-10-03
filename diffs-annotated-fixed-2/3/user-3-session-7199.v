3c71ab66b6c6aeec65519bb89b5b107722b04259
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:04:03 -0700

    user 3, session 7199, part 463

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -450,7 +450,6 @@ Proof.
 (assert (len_addr < diskSize d') by eauto).
 (unfold log_abstraction; intuition).
 -
-(unfold log_length_ok in *; intros).
-autorewrite with upd in *.
+(unfold log_length_ok in *; intros; autorewrite with upd in *).
 (* Auto-generated comment: Succeeded. *)
 
