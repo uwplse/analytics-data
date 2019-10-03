b7535717d03b4d307dd2d598b621962768aed946
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:56 -0700

    user 7, session 19, part 561

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -382,6 +382,6 @@ clear IHk' IHt.
 (pose proof (IHk k' t' Ht'k Ht'k' v) as Ht').
 tauto.
 *
-(assert (Hdepeq : | t' | = | t |) by apply (sem_eq_k_i__inv_depth_eq_1 _ _ _ Htk Href)).
+(assert (Hdepeq : | t' | = | t |)).
 (* Auto-generated comment: Failed. *)
 
