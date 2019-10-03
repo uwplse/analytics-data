e4f5c2580fef0432f49c6a187b03d4a7d2040b2c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:02 -0700

    user 7, session 186, part 16

--- diffs-annotated-fixed-2/7/user-7-session-186.v
+++ diffs-annotated-fixed-2/7/user-7-session-186.v
@@ -31,5 +31,6 @@ assumption.
 (apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
   destruct IHt' as [IHt'a IHt'b]; split).
 (intros HX'; destruct (fresh_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
+(apply match_ty_union_1; auto).
 (* Auto-generated comment: Failed. *)
 
