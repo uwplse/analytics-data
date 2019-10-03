95d78c5252a01f2208117ece7fe9a212400fd9c2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:22 -0700

    user 7, session 103, part 30

--- diffs-annotated-fixed-2/7/user-7-session-103.v
+++ diffs-annotated-fixed-2/7/user-7-session-103.v
@@ -56,6 +56,6 @@ Proof.
 (intros v t k w Hm).
 (destruct k, w, v; simpl in Hm; try contradiction).
 -
-(repeat unfold match_ty in Hm).
+(fold match_ty in Hm).
 (* Auto-generated comment: Failed. *)
 
