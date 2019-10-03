b315a5982d3e7d4e9be8f475e41d451355aafce3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:58 -0700

    user 3, session 7199, part 389

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -473,6 +473,6 @@ Theorem abstr_length_sz_bound d bs :
   log_size_ok d bs -> len_addr < diskSize d.
 Proof.
 (unfold log_size_ok).
-lia.
-(* Auto-generated comment: Failed. *)
+(intros; lia).
+(* Auto-generated comment: Succeeded. *)
 
