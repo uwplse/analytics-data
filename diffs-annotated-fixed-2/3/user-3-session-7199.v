f4a333a84de9d177a96ec6c79b1faacac03ffb89
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:47 -0700

    user 3, session 7199, part 210

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -287,6 +287,6 @@ Proof.
 (unfold log_contents_ok; intros).
 (specialize (H a); intuition).
 (unfold log_addr in *).
-(destruct (a0 == S a); try lia).
+(destruct (a0 == S a); subst; try lia).
 (* Auto-generated comment: Succeeded. *)
 
