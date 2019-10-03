01def185fbcd229d956414346931aba994f967fe
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:04 -0700

    user 7, session 19, part 693

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -360,9 +360,6 @@ Proof.
          rewrite mk_nf_union in Hm; apply match_ty_i_union__inv in Hm; destruct Hm as [Hm| Hm];
           [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; [ apply IHt1 | apply IHt2 ]; assumption
    end; try (solve [ destruct v; contradiction ])).
-(rewrite mk_nf_ref).
-(apply match_ty_i_ref__weak_inv in Hm).
-(destruct Hm as [t' Heq]; subst).
-constructor.
+(rewrite mk_nf_ref in *).
 (* Auto-generated comment: Failed. *)
 
