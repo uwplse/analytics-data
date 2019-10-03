f10b9487258e1e8f022109c8e45fce65abfc4a45
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:46 -0700

    user 7, session 81, part 10

--- diffs-annotated-fixed-2/7/user-7-session-81.v
+++ diffs-annotated-fixed-2/7/user-7-session-81.v
@@ -97,6 +97,5 @@ Proof.
 Check value_type_matching_ty__exists.
 (destruct (value_type_matching_ty__exists t2 k) as [pv2 Hpv2]).
 (remember (Nat.max k (| t2 |)) as kmax).
-(assert (Hmp : |-[ kmax] TPair v pv2 <$ TPair t1 t2)).
 (* Auto-generated comment: Failed. *)
 
