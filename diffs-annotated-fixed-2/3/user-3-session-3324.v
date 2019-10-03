a7ea8e680070ea5ed32cb31b79edac89d68f0379
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:39:40 -0700

    user 3, session 3324, part 8

--- diffs-annotated-fixed-2/3/user-3-session-3324.v
+++ diffs-annotated-fixed-2/3/user-3-session-3324.v
@@ -142,16 +142,5 @@ step_proc.
 (case_eq (diskGet (stateDisk state) (diskSize (stateDisk state) - 1)); intros).
 {
 exists (diskUpd (diskShrink (stateDisk state)) (stateBadBlock state) b).
-(unfold inited_any).
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqVPbCeu"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqu1n37z"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
 (* Auto-generated comment: Succeeded. *)
 
