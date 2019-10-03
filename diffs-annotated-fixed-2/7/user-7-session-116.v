6dab0a3d11af6f975453b67b3e210e5184a856c8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:50 -0700

    user 7, session 116, part 20

--- diffs-annotated-fixed-2/7/user-7-session-116.v
+++ diffs-annotated-fixed-2/7/user-7-session-116.v
@@ -23,16 +23,7 @@ Qed.
 Lemma subs_fresh : forall (X : id) (t : ty), fresh_in_ty X t -> forall s : ty, [X := s] t = t.
 Proof.
 (intros X t).
-(induction t; intros Hfresh s; try (solve [ reflexivity ])).
--
-(unfold fresh_in_ty in *).
-(simpl in Hfresh).
-(apply fresh_union__inv in Hfresh).
-(destruct Hfresh as [Hfresh1 Hfresh2]).
-(simpl).
-(rewrite IHt1; try assumption).
-(rewrite IHt2; try assumption).
-reflexivity.
+(induction t; intros Hfresh s; try (solve [ reflexivity ]); unfold fresh_in_ty in *; simpl in Hfresh).
 -
 (* Auto-generated comment: Failed. *)
 
