76e731b9ce0166e1243ebf82943138601259c840
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:18 -0700

    user 7, session 16, part 134

--- diffs-annotated-fixed-2/7/user-7-session-16.v
+++ diffs-annotated-fixed-2/7/user-7-session-16.v
@@ -462,6 +462,7 @@ tauto.
   (IH22 _ Hnf'); try (solve [ solve_union_sub_r__decidable IH11 IH21 ])).
 +
 (destruct IH12 as [IH12| IH12]; destruct IH22 as [IH22| IH22]; try (solve [ left; constructor; assumption ])).
-(right; intros Hcontra; destruct (sub_r_union_l__inv _ _ _ Hcontra) as [Hsub1 Hsub2]).
+Check sub_r_union_l__inv.
+(right; intros Hcontra).
 (* Auto-generated comment: Failed. *)
 
