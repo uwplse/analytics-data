9ee590a27062cd310a424c3ea4d8d75910c89c33
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:45 -0700

    user 7, session 174, part 28

--- diffs-annotated-fixed-2/7/user-7-session-174.v
+++ diffs-annotated-fixed-2/7/user-7-session-174.v
@@ -86,13 +86,14 @@ assumption.
 (destruct (beq_idP x i); reflexivity).
 Qed.
 Definition lt_size (t1 t2 : ty) := lt (size t1) (size t2).
+Definition foo (t : ty) := TExist vX t.
 Function
  subst (x : id) (s t : ty) {wf fun t1 t2 : ty => size t1 < size t2 t} : ty :=
    match t with
    | TCName _ => t
    | TPair t1 t2 => TPair (subst x s t1) (subst x s t2)
    | TUnion t1 t2 => TUnion (subst x s t1) (subst x s t2)
-   | TExist y t' => t
+   | TExist y t' => foo (subst x s t')
    | TVar y => if beq_id x y then s else t
    | TEV y => t
    end.
@@ -117,9 +118,6 @@ Omega.omega.
 (simpl).
 Omega.omega.
 -
-(apply (well_founded_lt_compat ty size)).
 (intros).
-tauto.
-Defined.
 (* Auto-generated comment: Failed. *)
 
