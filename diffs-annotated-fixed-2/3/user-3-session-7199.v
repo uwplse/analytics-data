6219fbebc4f7f4e2056cd0b7128822f680297abe
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:48 -0700

    user 3, session 7199, part 230

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -288,7 +288,7 @@ Proof.
 (unfold log_size_ok, log_contents_ok; intros).
 Check diskUpd_oob_eq.
 (rewrite diskUpd_oob_noop; eauto).
-(unfold log_addr in *).
-(unfold diskSize in *).
+(unfold log_addr, diskSize in *).
+lia.
 (* Auto-generated comment: Succeeded. *)
 
