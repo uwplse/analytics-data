d3110dc3a4b7f39162bd68335cbfe1ffbfee76c2
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:26 -0700

    user 7, session 104, part 44

--- diffs-annotated-fixed-2/7/user-7-session-104.v
+++ diffs-annotated-fixed-2/7/user-7-session-104.v
@@ -59,8 +59,6 @@ Fixpoint inv_depth (t : ty) :=
   | TEV _ => 0
   end
 where "'|' t '|'" := (inv_depth t) : btjt_scope.
-Lemma not__ref_t_match_ty_t : forall (k : nat) (t : ty), | t | <= k -> forall w : nat, ~ |-[ S k, w] TRef t <$ t.
-Proof.
-(induction k).
+Lemma not__ref_t_match_ty_t : forall (k : nat) (t : ty), | t | < k -> forall w : nat, ~ |-[ k, w] TRef t <$ t.
 (* Auto-generated comment: Failed. *)
 
