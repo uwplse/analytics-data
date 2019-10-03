c1daafa96373deea11793429cebad4df37f5c98e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:04 -0700

    user 7, session 10, part 50

--- diffs-annotated-fixed-2/7/user-7-session-10.v
+++ diffs-annotated-fixed-2/7/user-7-session-10.v
@@ -179,40 +179,7 @@ Proof.
 -
 tauto.
 -
-(destruct (in_nf_pair__inv _ _ Hnfm1) as [Hnfm11 Hnfm12]).
-(destruct (in_nf_pair__inv _ _ Hnfm2) as [Hnfm21 Hnfm22]).
 (destruct IHHsub1 as [IHHsub11 IHHsub12]; try assumption).
 (destruct IHHsub2 as [IHHsub21 IHHsub22]; try assumption).
-(split; intros tx Hsub'; [ remember (TPair t1 t2) as ty eqn:Heqy  | remember (TPair t1' t2') as ty eqn:Heqy  ]; induction Hsub'; inversion Heqy;
-  subst; try (solve [ constructor; auto ])).
-+
-(apply IHHsub').
-(apply mk_nf_nf__equal; assumption).
-(apply mk_nf__in_nf).
--
-(destruct (in_nf_union__inv _ _ Hnfm1) as [Hnfm11 Hnfm12]).
-(destruct IHHsub1 as [IHHsub11 IHHsub12]; try assumption).
-(destruct IHHsub2 as [IHHsub21 IHHsub22]; try assumption).
-(split; intros tx Hsub').
-+
-(remember (TUnion t1 t2) as ty eqn:Heqy ).
-(induction Hsub'; inversion Heqy; subst; try (solve [ (constructor; tauto) || auto ])).
-+
-(constructor; auto).
--
-(destruct (in_nf_union__inv _ _ Hnfm2) as [Hnfm21 Hnfm22]).
-(destruct IHHsub as [IHHsub1 IHHsub2]; try assumption).
-(split; intros tx Hsub'; try (solve [ constructor; auto ])).
-+
-(apply sub_r_union_l__inv in Hsub').
-(destruct Hsub'; auto).
--
-(destruct (in_nf_union__inv _ _ Hnfm2) as [Hnfm21 Hnfm22]).
-(destruct IHHsub as [IHHsub1 IHHsub2]; try assumption).
-(split; intros tx Hsub'; try (solve [ constructor; auto ])).
-+
-(apply sub_r_union_l__inv in Hsub').
-(destruct Hsub'; auto).
--
 (* Auto-generated comment: Failed. *)
 
