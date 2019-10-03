b4f0e4adc40afda08b7d4f5a086473e80e7d8bd9
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:11:51 -0700

    user 3, session 9823, part 12

--- diffs-annotated-fixed-2/3/user-3-session-9823.v
+++ diffs-annotated-fixed-2/3/user-3-session-9823.v
@@ -174,6 +174,11 @@ Proof.
 (destruct (a == r)).
 -
 invert_abstraction.
-(step_proc; intuition).
+(step_proc; intuition eauto).
+(step_proc; intuition eauto).
+(step_proc; intuition eauto).
+replace (diskSize (stateDisk state) - 1) with diskSize s in * by lia.
+(exists s; repeat split; auto).
+(destruct (stateBadBlock state == diskSize s)).
 (* Auto-generated comment: Succeeded. *)
 
