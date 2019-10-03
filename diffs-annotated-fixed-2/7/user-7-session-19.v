82c68c65c2a5ffd06640457241ab4b6c45b52766
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:30 -0700

    user 7, session 19, part 129

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -105,6 +105,6 @@ Qed.
 Lemma match_ty_i_eq__inv_depth_eq :
   forall t t' : ty, (forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
 Proof.
-(induction t; induction t'; intros H).
+(induction t; induction t'; intros H; try reflexivity).
 (* Auto-generated comment: Failed. *)
 
