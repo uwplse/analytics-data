e581cc47192df8e9b48757c29111ad74c1afb1f1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:46 -0700

    user 8, session 34, part 45

--- diffs-annotated-fixed-2/8/user-8-session-34.v
+++ diffs-annotated-fixed-2/8/user-8-session-34.v
@@ -49,6 +49,42 @@ admit.
 (intros WT pf_merge1 pf_merge2).
 replace (compose (gate g p1 f0) f) with gate g p1 (fun p2 => compose (f0 p2) f) by auto.
 (repeat rewrite denote_gate_circuit; fold_denotation).
-(set (p2 := process_gate_pat g p1 \206\1473)).
-(* Auto-generated comment: Failed. *)
+(set (p2 := process_gate_pat g p1 \206\147)).
+(set (\206\1473'' := process_gate_state g p1 \206\147)).
+admit.
+-
+admit.
+Admitted.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqKdN89S" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Theorem inSeq_correct :
+  forall W1 W2 W3 (g : Box W2 W3) (f : Box W1 W2) (safe : bool) \207\129,
+  Typed_Box g -> Typed_Box f -> denote_box safe (inSeq f g) \207\129 == compose_super (denote_box safe g) (denote_box safe f) \207\129.
+Proof.
+(intros W1 W2 W3 g f safe types_g types_f \207\129).
+(autounfold with den_db; simpl).
+(destruct f as [f]).
+(destruct g as [g]).
+(autounfold with den_db; simpl).
+(destruct (add_fresh W1 []) as [p1 \206\1471] eqn:E1).
+(simpl).
+(destruct (add_fresh W2 []) as [p2 \206\1472] eqn:E2).
+(simpl).
+(rewrite add_fresh_split in E1, E2).
+(inversion E1).
+(inversion E2).
+(assert (S1 : \226\159\166 \206\1471 \226\159\167 = \226\159\166 W1 \226\159\167)).
+(rewrite <- H1).
+(rewrite size_fresh_ctx; auto).
+(assert (S2 : \226\159\166 \206\1472 \226\159\167 = \226\159\166 W2 \226\159\167)).
+(rewrite <- H3).
+(rewrite size_fresh_ctx; auto).
+(rewrite H0, H1, H2, H3).
+replace 0%nat with \226\159\166 [] : Ctx \226\159\167 : nat by auto.
+replace (size_wtype W1) with \226\159\166 \206\1471 \226\159\167.
+replace (size_wtype W2) with \226\159\166 \206\1472 \226\159\167.
+specialize denote_compose as DC.
+(unfold denote_circuit in DC).
+(rewrite DC with (\206\1471 := [])).
+(* Auto-generated comment: Succeeded. *)
 
