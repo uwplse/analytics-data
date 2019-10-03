82c96c9146c1ee77bb530098e17546f67aeebc12
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:16:01 -0700

    user 3, session 11016, part 54

--- diffs-annotated-fixed-2/3/user-3-session-11016.v
+++ diffs-annotated-fixed-2/3/user-3-session-11016.v
@@ -613,6 +613,7 @@ Qed.
 Theorem beq_natlist_refl : forall l : natlist, true = beq_natlist l l.
 Proof.
 (induction l; simpl; auto).
-(rewrite IHl; simpl).
+(rewrite <- IHl; simpl).
+Search -andb -true.
 (* Auto-generated comment: Succeeded. *)
 
