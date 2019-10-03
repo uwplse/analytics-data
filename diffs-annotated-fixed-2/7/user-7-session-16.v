61f2e46f19196dbc0d32b5d325ba223dc0800fa6
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:17 -0700

    user 7, session 16, part 101

--- diffs-annotated-fixed-2/7/user-7-session-16.v
+++ diffs-annotated-fixed-2/7/user-7-session-16.v
@@ -434,5 +434,9 @@ Proof.
    | Hnf':InNF( TPair _ _) |- _ => destruct (in_nf_pair__inv _ _ Hnf') as [Hnf'1 Hnf'2]
    end; try (solve [ right; solve_not_x_sub_r_y_full | solve_atom_sub_r_union__decidable IHt'1 IHt'2 | solve_union_sub_r__decidable IHt'1 IHt'2 ])).
 +
+(right; solve_not_x_sub_r_y_full).
+(rewrite mk_nf_nf__equal).
+reflexivity.
+(do 2 constructor).
 (* Auto-generated comment: Failed. *)
 
