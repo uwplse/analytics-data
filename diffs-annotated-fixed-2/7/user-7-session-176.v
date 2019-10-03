17001d66d79c0f8b270ebdf7c052363af2aaed5c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:52 -0700

    user 7, session 176, part 68

--- diffs-annotated-fixed-2/7/user-7-session-176.v
+++ diffs-annotated-fixed-2/7/user-7-session-176.v
@@ -172,6 +172,11 @@ Proof.
 -
 split.
 +
-(destruct (beq_idP X i) eqn:Heq).
+(destruct (beq_idP X i)).
+*
+subst.
+(rewrite subst_var_eq).
+(destruct (beq_idP Y i)).
+subst.
 (* Auto-generated comment: Failed. *)
 
