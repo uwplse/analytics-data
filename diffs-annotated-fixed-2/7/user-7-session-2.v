ab02cdd394600194469932847123e3495af6bf9f
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:53 -0700

    user 7, session 2, part 82

--- diffs-annotated-fixed-2/7/user-7-session-2.v
+++ diffs-annotated-fixed-2/7/user-7-session-2.v
@@ -126,6 +126,8 @@ Proof.
 Qed.
 Lemma sub_r_unite_pairs_l__inv : forall t1 t2 t1' t2' : ty, |- unite_pairs t1 t2 << TPair t1' t2' -> |- t1 << t1' /\ |- t2 << t2'.
 Proof.
-(intros t1; induction t1; intros t2; induction t2).
+(intros t1; induction t1; intros t2; induction t2; intros t1' t2' Hsub).
+(simpl in Hsub).
+(inversion Hsub).
 (* Auto-generated comment: Failed. *)
 
