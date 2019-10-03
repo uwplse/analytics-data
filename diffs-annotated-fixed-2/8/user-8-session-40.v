d37fde6f3ab000e2752c567a66690b70b3e31b13
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:23:59 -0700

    user 8, session 40, part 15

--- diffs-annotated-fixed-2/8/user-8-session-40.v
+++ diffs-annotated-fixed-2/8/user-8-session-40.v
@@ -40,6 +40,6 @@ Lemma X_spec :
 Proof.
 (intros).
 vector_denote.
-(destruct b; unfold bool_to_ket; simpl; Msimpl; easy).
-(* Auto-generated comment: Failed. *)
+(destruct b; unfold bool_to_ket; simpl; Msimpl).
+(* Auto-generated comment: Succeeded. *)
 
