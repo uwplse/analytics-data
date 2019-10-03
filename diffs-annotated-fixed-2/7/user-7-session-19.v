6fb3c224c245d520ee6cb05fdbd0cc180b154af1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:24 -0700

    user 7, session 19, part 5

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -98,6 +98,13 @@ Close Scope btjmdeq_scope.
 Theorem sem_sub_i__sem_sub_deq : forall t1 t2 : ty, (||- [t1]<= [t2])%btjmi -> (||- [t1]<= [t2])%btjmdeq.
 Proof.
 (intros ta; induction ta; intros tb; induction tb; intros Hsem; pose proof Hsem as Hsem'; unfold sem_sub_i, sem_sub_k_i in Hsem';
-  unfold sem_sub_deq, sem_sub_k_deq; intros k v Hv Hm1).
+  unfold sem_sub_deq, sem_sub_k_deq; intros k v Hv Hm1;
+  try
+   match goal with
+   | Hm1:(|-[ ?k] ?v <$ TCName ?c)%btjmdeq
+     |- _ =>
+         apply match_ty_deq_cname__inv in Hm1; subst; assert (Hm : (|-[ k] TCName c <$ TCName c)%btjmi) by (destruct k; reflexivity); specialize
+          (Hsem' _ _ Hv Hm)
+   end).
 (* Auto-generated comment: Failed. *)
 
