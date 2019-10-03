8b5cc72a47c65995daaf775b10c1532237c4fd41
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:51 -0700

    user 7, session 19, part 483

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -248,7 +248,8 @@ clear IHk' IHt.
 (simpl in Htk, Htk').
 (apply le_S_n in Htk).
 (apply le_S_n in Htk').
-(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v; specialize (Href v)).
+(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v; pose proof (Href v) as Hrefv).
 (pose proof (IHk k' t Htk Htk' v) as Ht).
+(pose proof (IHk k' t' Htk Htk' v) as Ht').
 (* Auto-generated comment: Failed. *)
 
