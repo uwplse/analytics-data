a3e513bc7127f909cc5986fe11c0ba9d9569f3d9
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:26:01 -0700

    user 8, session 79, part 596

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -1004,9 +1004,7 @@ restore_dims tensor_tac.
    end).
 (match goal with
  | |- context [ @kron ?a ?b ?c ?d ?A (\226\168\130 ?li) ] => mat_replace
-   @kron a b c d A (\226\168\130 li) with \226\168\130 (A :: li) by
-   simpl; Msimpl; rewrite ctx_to_mat_list_length;
-    try rewrite size_ntensor, Nat.mul_1_r; easy
+   @kron a b c d A (\226\168\130 li) with \226\168\130 (A :: li)
  end).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
