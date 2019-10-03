18cb9e658244db0c68745c8d4f0e671c9c171cc7
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:44 -0700

    user 7, session 174, part 17

--- diffs-annotated-fixed-2/7/user-7-session-174.v
+++ diffs-annotated-fixed-2/7/user-7-session-174.v
@@ -134,9 +134,6 @@ Omega.omega.
 Check lt_wf.
 Print lt_wf.
 (unfold well_founded).
-(intros t; induction t; constructor; intros k Hk).
-+
-(inversion Hk; subst).
-Search -Acc.
+(intros t; induction t; constructor; induction t'; intros H).
 (* Auto-generated comment: Failed. *)
 
