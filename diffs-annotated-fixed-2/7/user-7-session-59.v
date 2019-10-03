2f69964c5cbbdc5c3839780301460e7756aab0aa
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:33 -0700

    user 7, session 59, part 2

--- diffs-annotated-fixed-2/7/user-7-session-59.v
+++ diffs-annotated-fixed-2/7/user-7-session-59.v
@@ -190,6 +190,13 @@ auto using match_ty_pair.
 (apply match_ty_union__inv in Hm2).
 (destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
 +
-clear IHv3.
-(* Auto-generated comment: Failed. *)
+clear IHt3.
+(apply match_ty_ref__inv in Hm2).
+(destruct Hm2 as [t'' [Heq' [[Hdepv3 Hdept''] Href']]]).
+(inversion Heq'; subst).
+(simpl).
+(rewrite Hdept').
+(split; tauto || eapply sem_eq_k__trans; eassumption).
+Qed.
+(* Auto-generated comment: Succeeded. *)
 
