7882dd1a0db852ec78c99a76b01ee7299f1ceef5
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:05 -0700

    user 7, session 10, part 66

--- diffs-annotated-fixed-2/7/user-7-session-10.v
+++ diffs-annotated-fixed-2/7/user-7-session-10.v
@@ -242,39 +242,6 @@ tauto.
 (apply SR_NormalForm).
 (assert (Hsub : |- TPair t1 t2 << TPair t1' t2') by (constructor; assumption)).
 (apply sub_r__mk_nf_sub_r in Hsub).
-(pose proof (mk_nf__in_nf (TPair t1 t2)) as Hnf1).
-(pose proof (mk_nf__in_nf (TPair t1' t2')) as Hnf2).
-(pose proof (sub_r_nf__trans2 _ _ Hsub Hnf1 Hnf2) as Htrans).
-(destruct Htrans as [_ Htrans]).
-(apply Htrans; assumption).
--
-(destruct IHHsub1 as [IHHsub11 IHHsub12]).
-(destruct IHHsub2 as [IHHsub21 IHHsub22]).
-(split; intros tx Hsub'; try (solve [ constructor; auto ])).
-+
-(remember (TUnion t1 t2) as ty eqn:Heqy ).
-(induction Hsub'; inversion Heqy; subst; try (solve [ (constructor; tauto) || auto ])).
--
-(destruct IHHsub as [IHHsub1 IHHsub2]).
-(split; intros tx Hsub'; try (solve [ constructor; auto ])).
-+
-(apply sub_r_union_l__inv in Hsub').
-(destruct Hsub'; auto).
--
-(destruct IHHsub as [IHHsub1 IHHsub2]; try assumption).
-(split; intros tx Hsub'; try (solve [ constructor; auto ])).
-+
-(apply sub_r_union_l__inv in Hsub').
-(destruct Hsub'; auto).
--
-(destruct IHHsub1 as [IHHsub11 IHHsub12]).
-(destruct IHHsub2 as [IHHsub21 IHHsub22]).
-(split; intros tx Hsub'; [ remember (TRef t) as ty eqn:Heqy  | remember (TRef t') as ty eqn:Heqy  ]; induction Hsub'; inversion Heqy; subst;
-  try (solve [ constructor; auto ])).
-+
-(apply SR_NormalForm).
-(assert (Hsub : |- TRef t << TRef t') by (constructor; assumption)).
-(apply sub_r__mk_nf_sub_r in Hsub).
 (apply sub_r_nf__trans2 with (MkNF( TRef t')); assumption || apply mk_nf__in_nf).
 (* Auto-generated comment: Failed. *)
 
