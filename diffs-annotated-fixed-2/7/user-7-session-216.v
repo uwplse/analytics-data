b74bdc54e70dfa280cf93c0f9fba1bc8957e1dda
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:31 -0700

    user 7, session 216, part 1

--- diffs-annotated-fixed-2/7/user-7-session-216.v
+++ diffs-annotated-fixed-2/7/user-7-session-216.v
@@ -359,6 +359,7 @@ Lemma wf_ty__wf_ty_f_subst : forall (X : id) (s t : ty), wf_ty s -> wf_ty t -> w
 Proof.
 (intros X s t Hwfs).
 generalize dependent t.
-(induction t; intros Hwft).
+(induction t; intros Hwft; try (solve [ simpl; assumption ])).
+-
 (* Auto-generated comment: Failed. *)
 
