0b94b56c974ff5a4b32f59eaf9c0a7f15b833cb3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:57 -0700

    user 7, session 2, part 142

--- diffs-annotated-fixed-2/7/user-7-session-2.v
+++ diffs-annotated-fixed-2/7/user-7-session-2.v
@@ -217,6 +217,8 @@ Lemma sub_r_union_l__inv : forall t1 t2 t' : ty, |- TUnion t1 t2 << t' -> |- t1 
 Proof.
 (intros t1 t2 t' Hsub).
 (remember (TUnion t1 t2) as t eqn:Heqt ).
-(induction Hsub; inversion Heqt; subst; try tauto).
+(induction Hsub; inversion Heqt; subst).
+tauto.
+(split; [ apply SR_UnionR1 | apply SR_UnionR2 ]; tauto).
 (* Auto-generated comment: Failed. *)
 
