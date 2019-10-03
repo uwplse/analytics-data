c0b9175879845ec8f3e4cd24f6942e3009ce4e28
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:45 -0700

    user 2, session 5, part 13

--- diffs-annotated-fixed-2/2/user-2-session-5.v
+++ diffs-annotated-fixed-2/2/user-2-session-5.v
@@ -35,5 +35,6 @@ Notation "[ x y .. z ]" := (Cons x (Cons y .. (Cons z Nil) ..))
 End TermNotations.
 Import TermNotations.
 Open Scope coucou_scope.
+Check [Nil Nil Nil Nil].
 (* Auto-generated comment: Succeeded. *)
 
