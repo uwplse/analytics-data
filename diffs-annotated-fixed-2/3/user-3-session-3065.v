df3afa9c64670a572587425e7f936818efa3e27a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:37:08 -0700

    user 3, session 3065, part 2

--- diffs-annotated-fixed-2/3/user-3-session-3065.v
+++ diffs-annotated-fixed-2/3/user-3-session-3065.v
@@ -368,6 +368,17 @@ iDestruct "Hltmps" as % Hltmps.
 (assert (tmps = tmps \226\136\150 list_to_set (take 0 ltmps)) as ->).
 {
 by rewrite difference_empty_L.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqgRajTr"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoI3N8W"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
 }
+iDestruct (slice_mapsto_len with "Hs") as % ->.
 (* Auto-generated comment: Succeeded. *)
 
