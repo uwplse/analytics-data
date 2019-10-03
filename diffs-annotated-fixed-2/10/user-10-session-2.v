18ec9d66c825a45f3f3283900a4dcf7f7d319f55
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:28:15 -0700

    user 10, session 2, part 0

---
+++ diffs-annotated-fixed-2/10/user-10-session-2.v
@@ -0,0 +1,5 @@
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqWXn2UD" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+(* Auto-generated comment: Succeeded. *)
+
