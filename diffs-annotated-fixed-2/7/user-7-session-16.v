4bdc6cc308cf371917246591daccd3e8e7600b5c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:15 -0700

    user 7, session 16, part 69

--- diffs-annotated-fixed-2/7/user-7-session-16.v
+++ diffs-annotated-fixed-2/7/user-7-session-16.v
@@ -401,7 +401,7 @@ right.
         subst
  end).
 (match goal with
- | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto
+ | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto || apply mk_nf_nf__equal
  end).
 (* Auto-generated comment: Failed. *)
 
