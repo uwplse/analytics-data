c24651ff5eb7392732ef6dc4183565bc6cd9b541
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:32 -0700

    user 7, session 19, part 160

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -114,12 +114,47 @@ Proof.
              (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
                (H 0 _ Hv); destruct H as [_ H]; specialize (H Hm); contradiction)
      end ])).
-2: {
+Show 3.
+9: {
 idtac.
-clear IHt'1 IHt'2.
+clear IHt'.
 (simpl).
-(assert (Heq1 : | t1 | = | t'1 |)).
-{
-(apply IHt1).
+(apply f_equal).
+(apply IHt).
+(intros k v Hv).
+(assert (Hmt : |-[ S k] TRef t <$ TRef t) by (simpl; tauto)).
+specialize (H (S k) (TRef t)).
+(destruct H as [H _]).
+constructor.
+specialize (H Hmt).
+(apply match_ty_i_ref__inv in H).
+(destruct H as [tx [Heq Href]]).
+(inversion Heq; subst).
+auto.
+}
+Abort.
+Lemma match_ty_i__inv_depth_stable :
+  forall (k k' : nat) (t : ty),
+  inv_depth t <= k -> inv_depth t <= k' -> forall v : ty, inv_depth v <= k -> inv_depth v <= k' -> |-[ k] v <$ t <-> |-[ k'] v <$ t.
+Proof.
+(induction k; induction k').
+-
+tauto.
+-
+admit.
+-
+admit.
+-
+(induction t).
+admit.
+admit.
+admit.
++
+clear IHk' IHt.
+(intros Htk Htk' v Hvk Hvk').
+(simpl in Htk, Htk').
+(apply le_S_n in Htk).
+(apply le_S_n in Htk').
+(split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v Hv; specialize (Href v Hv)).
 (* Auto-generated comment: Failed. *)
 
