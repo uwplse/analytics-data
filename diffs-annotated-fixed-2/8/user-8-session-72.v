815472510a3386471186a2384caa94c14e85ad55
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:24:55 -0700

    user 8, session 72, part 34

--- diffs-annotated-fixed-2/8/user-8-session-72.v
+++ diffs-annotated-fixed-2/8/user-8-session-72.v
@@ -1250,6 +1250,5 @@ reflexivity.
   try apply unitary_at1_WT; try apply source_symmetric_WT; trivial).
 (apply source_symmetric_WT in H).
 (unfold valid_ancillae_box in IHsource_symmetric).
-(rewrite IHsource_symmetric; trivial).
 (* Auto-generated comment: Succeeded. *)
 
