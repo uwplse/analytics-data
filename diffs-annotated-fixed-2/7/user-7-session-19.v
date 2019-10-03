a1a8376e51f17e609be22dd182fe5babe40adb03
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:45 -0700

    user 7, session 19, part 385

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -201,6 +201,6 @@ Proof.
      end ])).
 -
 (destruct (match_ty_i_exists t1 0) as [v1 Hm1]; destruct (match_ty_i_exists t2 0) as [v2 Hm2]).
-(assert (Hmp : |-[ 0] TPair v1 v2 <$ TPir t1 t2) by (apply match_ty_i_pair; assumption)).
+(assert (Hmp : |-[ 0] TPair v1 v2 <$ TPair t1 t2) by (apply match_ty_i_pair; assumption)).
 (* Auto-generated comment: Failed. *)
 
