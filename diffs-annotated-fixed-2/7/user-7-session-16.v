268983a920604ffa6fabd7f427239d4d1f3cec64
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:19 -0700

    user 7, session 16, part 138

--- diffs-annotated-fixed-2/7/user-7-session-16.v
+++ diffs-annotated-fixed-2/7/user-7-session-16.v
@@ -458,9 +458,9 @@ Proof.
 tauto.
 -
 (intros t1 t2 Hnf1 [IH11 IH12] Hnf2 [IH21 IH22]).
-(split; intros t'; induction t'; intros Hnf'; specialize (IH11 _ Hnf'); specialize (IH12 _ Hnf'); specialize (IH21 _ Hnf'); specialize
-  (IH22 _ Hnf'); try (solve [ solve_union_sub_r__decidable IH11 IH21 | solve_atom_sub_r_union__decidable IH12 IH22 ])).
+(split; intros t'; induction t'; intros Hnf'; try (inversion Hnf'; subst); specialize (IH11 _ Hnf'); specialize (IH12 _ Hnf'); specialize
+  (IH21 _ Hnf'); specialize (IH22 _ Hnf'); try (solve [ solve_union_sub_r__decidable IH11 IH21 | solve_atom_sub_r_union__decidable IH12 IH22 ])).
 +
-(destruct IH12 as [IH12| IH12]; destruct IH22 as [IH22| IH22]; try (solve [ left; constructor; assumption ])).
+(destruct IHt'1 as [IH1| IH1]; destruct IHt'2 as [IH2| IH2]).
 (* Auto-generated comment: Failed. *)
 
