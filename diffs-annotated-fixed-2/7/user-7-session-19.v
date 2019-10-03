590483f738eb9fdcd4caf46fa4889990305baba9
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:56 -0700

    user 7, session 19, part 573

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -390,10 +390,6 @@ tauto.
 Admitted.
 Lemma match_ty_i__match_le_inv_depth : forall (k : nat) (t v : ty), |-[ k] v <$ t -> forall k' : nat, k' <= k -> |-[ k'] v <$ t.
 Proof.
-(induction k; induction t).
-4: {
-idtac.
-(intros Hm k' Hle).
-(inversion Hle; subst).
+(induction k; induction t; intros v).
 (* Auto-generated comment: Failed. *)
 
