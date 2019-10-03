afa7d2510b9fb8801c693ec1a5d4f0e699f177fd
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:27:05 -0700

    user 8, session 79, part 1259

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -1195,6 +1195,6 @@ rewrite_inPar'.
 (rewrite id_circ_spec).
 (rewrite strip_one_l_out_eq).
 rewrite_inPar'.
-(rewrite assert0_spec).
+(rewrite id_circ_spec).
 (* Auto-generated comment: Failed. *)
 
