1f4292bd6872bd08e5b5b1521bb6e3c02c505e50
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:27 -0700

    user 7, session 160, part 8

--- diffs-annotated-fixed-2/7/user-7-session-160.v
+++ diffs-annotated-fixed-2/7/user-7-session-160.v
@@ -45,6 +45,8 @@ Search -IdSet.remove.
 Check IdSetFacts.remove_2.
 Check IdSetFacts.remove_2.
 (assert (Hneq' : X' <> X) by auto).
-(apply (IdSetFacts.remove_2 _ Hneq') in Hcontra).
-(* Auto-generated comment: Failed. *)
+(apply (IdSetFacts.remove_2 Hneq') in Hcontra).
+contradiction.
+Qed.
+(* Auto-generated comment: Succeeded. *)
 
