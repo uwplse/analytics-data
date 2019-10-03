c438d4b845d4964fa2df5482c6ee9ce937657ffb
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:50 -0700

    user 7, session 2, part 29

--- diffs-annotated-fixed-2/7/user-7-session-2.v
+++ diffs-annotated-fixed-2/7/user-7-session-2.v
@@ -67,7 +67,7 @@ Check unite_pairs_union_t.
 -
 (intros Hnf1' Hnf2 Hn2' Hsub1 Hsub2).
 (rewrite (unite_pairs_union_t t1 t0 t2)).
-(apply sub_r_nf_union_l__inv in Hsub1; try assumption).
-constructor.
+Check sub_r_nf_union_l__inv.
+(destruct (sub_r_nf_union_l__inv _ _ _ Hsub1) as [Hsub11| Hsub12]).
 (* Auto-generated comment: Failed. *)
 
