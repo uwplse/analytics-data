e38b441b64703dfac1609712f386295f9f060a55
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:04:00 -0700

    user 3, session 7199, part 426

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -459,6 +459,28 @@ step_proc.
 intuition.
 {
 (exists (bs ++ v); intuition).
+rename state into d.
 rename state0 into d'.
+rename v into bs'.
+rename r0 into len_b.
+Timeout 1 Show.
+Timeout 1 Show Intros.
+Timeout 1
+(repeat
+  match goal with
+  | company_coq_hyp__:_
+    |- _ =>
+        clear dependent company_coq_hyp__;
+         (let dummy := H0 in
+          let dummy := H5 in
+          let dummy := H3 in
+          let dummy := H2 in
+          idtac)
+  end;
+  repeat
+   match goal with
+   | H:_ |- _ => generalize dependent H; try (generalize dependent H; fail 1)
+   end).
+Timeout 1 Show Intros.
 (* Auto-generated comment: Succeeded. *)
 
