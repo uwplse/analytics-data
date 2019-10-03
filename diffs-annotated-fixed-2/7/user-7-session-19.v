746ab1b6b1a04d7032c90674b1b102917f8003f0
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:44 -0700

    user 7, session 19, part 369

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -188,7 +188,8 @@ Lemma sem_sub_k_i_nf__inv_depth_le : forall (k : nat) (t t' : ty), InNF( t) -> |
 Proof.
 (induction k; induction t; induction t'; intros Hnft Hsem; try (solve [ simpl; constructor ])).
 (match goal with
- | Hsem:||-[ ?k][?t1]<= [?t2] |- | ?t1 | <= | ?t2 | => assert (Hv : value_type t1) by constructor
+ | Hsem:||-[ ?k][?t1]<= [?t2]
+   |- | ?t1 | <= | ?t2 | => assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption)
  end).
 (* Auto-generated comment: Failed. *)
 
