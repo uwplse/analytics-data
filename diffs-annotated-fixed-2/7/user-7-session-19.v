a8b78ea2baea8c4c2189df5f9eb88250b8aa4acc
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:00 -0700

    user 7, session 19, part 631

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -427,6 +427,10 @@ admit.
 (apply value_sem_sub_k_i_union__inv in Hsem; try assumption).
 (destruct Hsem as [Hsem| Hsem]; [ apply union_right_1 | apply union_right_2 ]; auto).
 +
-(simpl in Hdt).
+(simpl in Hdep).
+(pose proof (le_S_n _ _ Hdep) as Hdep').
+(pose proof Hsem as Hsem').
+(unfold sem_sub_k_i in Hsem).
+specialize (Hsem _ Hva Hma).
 (* Auto-generated comment: Failed. *)
 
