f99bdbec32bc728913a5f6ff770137233e915c15
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:09 -0700

    user 7, session 14, part 1

--- diffs-annotated-fixed-2/7/user-7-session-14.v
+++ diffs-annotated-fixed-2/7/user-7-session-14.v
@@ -280,5 +280,6 @@ Lemma mk_nf_sub_r__sub_r : forall t t' : ty, |- MkNF( t) << MkNF( t') -> |- t <<
 Proof.
 (intros t t' Hsub).
 (apply SR_NormalForm).
+(apply sub_r__transitive with (MkNF( t'))).
 (* Auto-generated comment: Failed. *)
 
