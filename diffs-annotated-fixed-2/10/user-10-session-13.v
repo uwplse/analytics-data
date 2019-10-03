aceb4a68c8e36d6829728e3529ab71c57981848d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:28:25 -0700

    user 10, session 13, part 10

--- diffs-annotated-fixed-2/10/user-10-session-13.v
+++ diffs-annotated-fixed-2/10/user-10-session-13.v
@@ -8,6 +8,23 @@ Add Search Blacklist "Private_" "_subproof".
 Timeout 1 Print LoadPath.
 Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coq5lrxpQ" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
-From Coq Require Import NArith Lists Streams.
-(* Auto-generated comment: Failed. *)
+From Coq Require Import NArith List Streams.
+From ReductionEffect Require Import PrintingEffect.
+CoFixpoint fib (x y : N) : Stream N := Cons y (fib y (x + y)).
+Eval compute in Str_nth 10 (map print_id (fib 0 1)).
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqcRXKe3" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Import ListNotations.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqinpWed" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Fixpoint fib' (fuel : nat) (x y : N) : list N := match fuel with
+                                                 | O => []
+                                                 | S fuel => fib' fuel y (x + y)
+                                                 end.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coq7AlQ5i" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Eval compute in fib' 10 0 1.
+(* Auto-generated comment: Succeeded. *)
 
