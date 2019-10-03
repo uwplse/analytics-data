f6e2bc227e334bd5ca2bc5a89558f0e0ceb9ed93
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:52 -0700

    user 3, session 7199, part 288

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -384,6 +384,6 @@ destruct matches.
 step_proc.
 (descend; intuition eauto).
 (unfold log_size_ok; autorewrite with list; auto).
-(descend; intuition eauto).
+(exists bs; eauto).
 (* Auto-generated comment: Succeeded. *)
 
