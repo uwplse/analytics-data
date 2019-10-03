bcb5cd396c4c5e6c25084d6bcc19efb960d1399a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:09 -0700

    user 7, session 141, part 1

--- diffs-annotated-fixed-2/7/user-7-session-141.v
+++ diffs-annotated-fixed-2/7/user-7-session-141.v
@@ -78,7 +78,7 @@ specialize (IHt1 _ _ Hfresh1 Hsem1).
 specialize (IHt2 _ _ Hfresh2 Hsem2).
 (apply sem_sub__trans with (TPair (TExist X t1) (TExist X t2))).
 2: {
-idtac.
-(apply sem_sub_pair).
+(apply sem_sub_pair; assumption).
+}
 (* Auto-generated comment: Failed. *)
 
