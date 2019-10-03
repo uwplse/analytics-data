b35078d42ddf539cc52a695f1ee0f2105bb592fe
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:44 -0700

    user 7, session 19, part 371

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -189,9 +189,9 @@ Proof.
 (induction k; induction t; induction t'; intros Hnft Hsem; try (solve [ simpl; constructor ])).
 (match goal with
  | Hsem:||-[ ?k][?t1]<= [?t2]
-   |- | ?t1 | <= | ?t2 | => assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption)
+   |- | ?t1 | <= | ?t2 | =>
+       assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+        (Hsem _ Hm)
  end).
-Search -sem_sub_k_i.
-Print sem_sub_k_i.
 (* Auto-generated comment: Failed. *)
 
