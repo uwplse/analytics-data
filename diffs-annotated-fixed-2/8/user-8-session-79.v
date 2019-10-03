3f48ecaa341a526687fb47e77021c3cf00b0e5fa
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:25:18 -0700

    user 8, session 79, part 121

--- diffs-annotated-fixed-2/8/user-8-session-79.v
+++ diffs-annotated-fixed-2/8/user-8-session-79.v
@@ -595,6 +595,19 @@ Proof.
 (rewrite kron_1_l).
 (rewrite kron_1_l).
 Msimpl.
+restore_dims.
 (apply H).
+Qed.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coq0uS6IR"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Lemma big_kron_append :
+  forall m n (l1 l2 : list (Matrix m n)), \226\168\130 (l1 ++ l2) == (\226\168\130 l1) \226\138\151 (\226\168\130 l2).
+Proof.
+(induction l1).
+-
+(intros).
+(simpl).
+specialize (kron_1_l (\226\168\130 l2)) as KL.
 (* Auto-generated comment: Failed. *)
 
