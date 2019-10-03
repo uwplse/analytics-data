2e56849a295f7a142e4b82f34fa1f5491e5ccab8
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:20:21 -0700

    user 7, session 103, part 23

--- diffs-annotated-fixed-2/7/user-7-session-103.v
+++ diffs-annotated-fixed-2/7/user-7-session-103.v
@@ -39,10 +39,10 @@ Lemma match_ty_pair__inv :
   forall (v t1 t2 : ty) (k w : nat), |-[ k, w] v <$ TPair t1 t2 -> exists v1 v2 : ty, v = TPair v1 v2 /\ |-[ k, w] v1 <$ t1 /\ |-[ k, w] v2 <$ t2.
 Proof.
 (intros v t1 t2 k w Hm).
-(destruct k, w, v; simpl in Hm; try contradiction).
-exists v1,v2.
-split.
-reflexivity.
-tauto.
+(destruct k, w, v; simpl in Hm; contradiction || (exists v1,v2; split; [ reflexivity | tauto ])).
+Qed.
+Lemma match_ty_union__inv : forall (v t1 t2 : ty) (k w : nat), |-[ k, w] v <$ TUnion t1 t2 -> |-[ k, w] v <$ t1 \/ |-[ k, w] v <$ t2.
+Proof.
+(intros v t1 t2 k w Hm).
 (* Auto-generated comment: Failed. *)
 
