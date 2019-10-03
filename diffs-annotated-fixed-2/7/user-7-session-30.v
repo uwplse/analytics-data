954af1076c9a47a0af1f65b4148ba53d5724e54a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:21 -0700

    user 7, session 30, part 4

--- diffs-annotated-fixed-2/7/user-7-session-30.v
+++ diffs-annotated-fixed-2/7/user-7-session-30.v
@@ -43,6 +43,6 @@ clear IHv.
 Qed.
 Lemma match_ty__value_type : forall (v t : ty) (k : nat), |-[ k] v <$ t -> value_type v.
 Proof.
-(induction v; induction t).
+(induction v; induction t; try (solve [ auto with DBBetaJulia ])).
 (* Auto-generated comment: Failed. *)
 
