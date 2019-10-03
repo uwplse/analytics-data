07e67eaefd0868c4003c99d3495e9af00a3daae1
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:56 -0700

    user 3, session 7199, part 363

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -393,9 +393,47 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Hint Resolve append_at_ok: core.
-Theorem log_abstraction_preserved d bs d' bs' :
+Theorem log_abstraction_preserved d bs d' :
   log_abstraction d bs ->
   diskGet d' len_addr = diskGet d len_addr ->
   diskSize d' = diskSize d -> log_contents_ok d' bs -> log_abstraction d' bs.
+Proof.
+(unfold log_abstraction, log_length_ok, log_size_ok; intuition).
+-
+replace (diskGet d' len_addr) in *.
+auto.
+-
+congruence.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqQxkvg1"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
+Theorem append_ok :
+  forall v, proc_spec (append_spec v) (append v) recover abstr.
+Proof.
+(unfold append; intros).
+(apply spec_abstraction_compose).
+step_proc.
+(destruct a' as [[] bs]; simpl in *).
+intuition eauto.
+step_proc.
+(descend; intuition eauto).
+destruct matches.
+-
+step_proc.
+(descend; intuition eauto).
+{
+(unfold log_size_ok; autorewrite with list; auto).
+}
+{
+(exists bs; intuition eauto using log_abstraction_preserved).
+}
+step_proc.
+intuition.
+{
+eauto using log_abstraction_preserved.
 (* Auto-generated comment: Succeeded. *)
 
