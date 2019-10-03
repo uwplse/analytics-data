46a091ca0e57805fe57946a718b70eb8dbfceb70
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:42 -0700

    user 5, session 36, part 9

--- diffs-annotated-fixed-2/5/user-5-session-36.v
+++ diffs-annotated-fixed-2/5/user-5-session-36.v
@@ -257,6 +257,8 @@ reflexivity.
 (rewrite H in H0).
 clear H.
 (rewrite evalAnd in H0).
-(destruct (evalIn _ _ _)).
+(match goal with
+ | H:eval ?L ?env (If (In ?x ?S) _ _) |- _ => destruct (evalIn L env x S)
+ end).
 (* Auto-generated comment: Succeeded. *)
 
