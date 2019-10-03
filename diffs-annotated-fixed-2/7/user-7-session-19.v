57e964751442873dd5209efd631fd83fec399283
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:33 -0700

    user 7, session 19, part 169

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -77,8 +77,14 @@ reflexivity.
 specialize (Href v' Hv').
 (destruct Href; split; assumption).
 Qed.
-Lemma aaa : forall (k : nat) (t t' v : ty), |-[ k] v <$ t -> |-[ k] v <$ t' -> | t | <= | t' |.
+Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
 Proof.
-(induction k).
+(induction k; induction t; induction t'; intros H).
+32: {
+idtac.
+(simpl).
+(apply le_n_S).
+(apply IHk).
+(assert (Hv : value_type (TRef t)) by (apply match_ty_i__reflexive; constructor)).
 (* Auto-generated comment: Failed. *)
 
