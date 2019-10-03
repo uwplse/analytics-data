22b80011920ddc8dc048bf15162f7ae4b19bf7fc
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:08:20 -0700

    user 1, session 41, part 8

--- diffs-annotated-fixed-2/1/user-1-session-41.v
+++ diffs-annotated-fixed-2/1/user-1-session-41.v
@@ -411,7 +411,7 @@ Inductive Alpha : SetST -> GT -> Prop :=
             match S with
             | SRec (hd :: tl) => hd
             | _ => None
-            end)) hd -> Alpha S (GRec ((R, hd) :: tl))
+            end)) hd -> Alpha S (GRec (Some (R, hd) :: tl))
   | alpha_row_mt :
       forall S,
       Inhabited _ S ->
@@ -427,5 +427,5 @@ Inductive Alpha : SetST -> GT -> Prop :=
                | SRec l => nth n l None
                | _ => None
                end)) GDyn) -> Alpha S (GRow []).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
