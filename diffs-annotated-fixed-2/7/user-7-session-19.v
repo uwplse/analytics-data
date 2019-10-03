13495e081195d26bf01f1507300bf1da6f2ea879
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:00 -0700

    user 7, session 19, part 624

--- diffs-annotated-fixed-2/7/user-7-session-19.v
+++ diffs-annotated-fixed-2/7/user-7-session-19.v
@@ -400,6 +400,7 @@ Proof.
 -
 (intros c k Hdep t2).
 (assert (Hva : value_type (TCName c)) by constructor).
-(assert (Hma : |-[ k] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption)).
-(* Auto-generated comment: Succeeded. *)
+(assert (Hma : |-[ 0] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption)).
+(induction t2; intros Hsem; try (solve [ specialize (Hsem _ Hma); destruct k; simpl in Hsem; subst; constructor || contradiction ])).
+(* Auto-generated comment: Failed. *)
 
