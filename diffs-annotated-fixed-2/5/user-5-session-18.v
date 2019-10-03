1267227b4568a153e5aa57de35e442027e8c260c
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:30 -0700

    user 5, session 18, part 3

--- diffs-annotated-fixed-2/5/user-5-session-18.v
+++ diffs-annotated-fixed-2/5/user-5-session-18.v
@@ -11,10 +11,18 @@ Parameter (vTrue : Value).
 Parameter (vFalse : Value).
 Axiom (trueAndFalseDistinct : vTrue <> vFalse).
 Definition Environment := Identifier -> Value.
+Definition extendEnv (env : Environment) (var : Identifier)
+  (newValue : Value) : Environment :=
+  fun id => if id_eq_dec id var then newValue else env id.
 Parameter (eval : Environment -> Term -> Value).
 Axiom (evalEqTrue : forall env term, eval env (Eq term term) = vTrue).
 Axiom
   (evalEqFalse : forall env t1 t2, t1 <> t2 -> eval env (Eq t1 t2) = vFalse).
 Axiom (evalVar : forall env id, eval env (Var id) = env id).
+Axiom
+  (evalChoose :
+     forall env id body,
+     (exists value, eval (extendEnv env id value) body = vTrue) ->
+     exists out, eval env (Eq (Choose id body) out) = vTrue).
 (* Auto-generated comment: Succeeded. *)
 
