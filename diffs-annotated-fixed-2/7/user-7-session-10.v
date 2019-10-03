36c232fb3efe228aad3e956e90be1d79de513aef
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:03 -0700

    user 7, session 10, part 30

--- diffs-annotated-fixed-2/7/user-7-session-10.v
+++ diffs-annotated-fixed-2/7/user-7-session-10.v
@@ -183,7 +183,6 @@ tauto.
 (destruct (in_nf_pair__inv _ _ Hnfm2) as [Hnfm21 Hnfm22]).
 (destruct IHHsub1 as [IHHsub11 IHHsub12]; try assumption).
 (split; intros tx Hsub'; [ remember (TPair t1 t2) as ty eqn:Heqy  | remember (TPair t1' t2') as ty eqn:Heqy  ]; induction Hsub'; inversion Heqy;
-  subst).
-+
+  subst; try (solve [ constructor; auto ])).
 (* Auto-generated comment: Failed. *)
 
