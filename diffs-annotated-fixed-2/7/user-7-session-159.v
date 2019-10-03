6acb33418c04d6bba7357dd04a634c822067c3f9
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:25 -0700

    user 7, session 159, part 11

--- diffs-annotated-fixed-2/7/user-7-session-159.v
+++ diffs-annotated-fixed-2/7/user-7-session-159.v
@@ -70,6 +70,23 @@ subst.
 (rewrite subst_neq__permute in Hm).
 specialize (IHw _ _ Hm).
 (destruct IHw as [v' Hm']).
-(rewrite subst_neq__permute in Hm).
+(rewrite subst_neq__permute in Hm').
+exists v'.
+(apply match_ty_exist).
+exists ti.
+assumption.
+assumption.
+Abort.
+Lemma build_v_full :
+  forall (X X' : id) (tx : ty) (w : nat) (t v : ty),
+  |-[ w] v <$ [X := tx] t ->
+  exists v' : ty, |-[ w] v' <$ [X := TVar X'] t /\ (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> |-[ w'] v <$ [X' := tx] t').
+Proof.
+(intros X X' tx).
+(induction w; induction t; intros v Hm).
+-
+exists v.
+split.
+assumption.
 (* Auto-generated comment: Failed. *)
 
