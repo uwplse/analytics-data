68b02921422a46ea1eab6b5a8734974f95579827
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:15:59 -0700

    user 3, session 11016, part 28

--- diffs-annotated-fixed-2/3/user-3-session-11016.v
+++ diffs-annotated-fixed-2/3/user-3-session-11016.v
@@ -362,14 +362,22 @@ Proof.
 reflexivity.
 Qed.
 Definition add (v : nat) (s : bag) : bag := v :: s.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq3379k5"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq89QAr9"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqdjcOCO"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqydWFWT"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-(* Auto-generated comment: Succeeded. *)
+Example test_add1 : count 1 (add 1 [1; 4; 1]) = 3.
+Proof.
+reflexivity.
+Qed.
+Example test_add2 : count 5 (add 1 [1; 4; 1]) = 0.
+Proof.
+reflexivity.
+Qed.
+(* Auto-generated comment: Failed. *)
 
