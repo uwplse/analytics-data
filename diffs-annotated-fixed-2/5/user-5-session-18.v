abebb88840dbf8f5ff87954d3d48329225ae3656
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:17:30 -0700

    user 5, session 18, part 9

--- diffs-annotated-fixed-2/5/user-5-session-18.v
+++ diffs-annotated-fixed-2/5/user-5-session-18.v
@@ -13,5 +13,39 @@ Inductive Term : Set :=
 Definition extendEnv {Value} (env : Identifier -> Value) 
   (var : Identifier) (newValue : Value) : Identifier -> Value :=
   fun id => if id_eq_dec id var then newValue else env id.
+Record EpsilonLogic :=
+ mkLogic {Value : Type;
+          vTrue : Value;
+          vFalse : Value;
+          trueAndFalseDistinct : vTrue <> vFalse;
+          eval : (Identifier -> Value) -> Term -> Value;
+          evalVar : forall env id, eval env (Var id) = env id;
+          evalIntConst :
+           forall env1 env2 i, eval env1 (Int i) = eval env2 (Int i);
+          evalIntInj :
+           forall env i j, i <> j -> eval env (Int i) <> eval env (Int j);
+          evalEqTrue :
+           forall env a b,
+           eval env a = eval env b -> eval env (Eq a b) = vTrue;
+          evalEqFalse :
+           forall env a b,
+           eval env a <> eval env b -> eval env (Eq a b) = vFalse;
+          evalChoose :
+           forall env x P,
+           (exists value, eval (extendEnv env x value) P = vTrue) ->
+           exists out, eval env (Eq (Choose x P) out) = vTrue;
+          evalChooseDet :
+           forall env x P Q,
+           eval env P = vTrue <-> eval env Q = vTrue ->
+           eval env (Choose x P) = eval env (Choose x Q)}.
+Fixpoint simplify (t : Term) : Term :=
+  match t with
+  | Var x => Var x
+  | Eq a b => Eq (simplify a) (simplify b)
+  | Choose x P => Choose x (simplify P)
+  end.
+Theorem simplify_correct :
+  forall t : Term,
+  EpsilonLogic.(isTheorem) t <-> EpsilonLogic.(isTheorem) (simplify t).
 (* Auto-generated comment: Succeeded. *)
 
