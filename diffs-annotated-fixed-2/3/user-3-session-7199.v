3702278773d038c45871f615643655aa381b1f0d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:03:46 -0700

    user 3, session 7199, part 202

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -305,5 +305,15 @@ intuition eauto.
 -
 step_proc.
 (intuition eauto; autorewrite with upd; auto).
+Admitted.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqXydXNX"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq0isTIA"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
