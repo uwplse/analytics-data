cb08c4f1556a32d18c26a30a05008cab3dd2893d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:04:01 -0700

    user 3, session 7199, part 432

--- diffs-annotated-fixed-2/3/user-3-session-7199.v
+++ diffs-annotated-fixed-2/3/user-3-session-7199.v
@@ -441,6 +441,7 @@ Lemma log_abstraction_commit :
   log_abstraction d' (bs ++ bs').
 Proof.
 (intros).
-(unfold log_abstraction in *; intuition).
+(unfold log_abstraction; intuition).
+(unfold log_length_ok; intros).
 (* Auto-generated comment: Succeeded. *)
 
