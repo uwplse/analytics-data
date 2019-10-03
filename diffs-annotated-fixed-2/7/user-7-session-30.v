a334a4008879893cfc770c8473cdb0bb8ceb72e3
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:19:21 -0700

    user 7, session 30, part 13

--- diffs-annotated-fixed-2/7/user-7-session-30.v
+++ diffs-annotated-fixed-2/7/user-7-session-30.v
@@ -52,6 +52,6 @@ constructor.
 -
 (apply match_ty_pair__inv in Hm).
 (destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(constructor; auto).
+(constructor; eauto).
 (* Auto-generated comment: Failed. *)
 
