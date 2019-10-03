68399393ca2a066c961f7c760fc0da077ebc7e0a
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:44:56 -0700

    user 3, session 3788, part 0

---
+++ diffs-annotated-fixed-2/3/user-3-session-3788.v
@@ -0,0 +1,36 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqtUxvQR"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Import POCS.
+Require Import Lab0.HeapVariablesAPI.
+Module HeapExamples (vars: VarsAPI).
+Definition swapXY : proc unit :=
+  x <- vars.read VarX;
+  y <- vars.read VarY; _ <- vars.write VarX y; _ <- vars.write VarY x; Ret tt.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqMe8vtG"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqLQslKK"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Theorem swapXY_ok :
+  proc_spec
+    (fun (_ : unit) state =>
+     {|
+     pre := True;
+     post := fun r state' =>
+             state' = mkState (StateY state) (StateX state) (StateZ state);
+     recovered := fun _ state' => True |}) swapXY vars.recover vars.abstr.
+Proof.
+(unfold swapXY).
+monad_simpl.
+(* Auto-generated comment: Succeeded. *)
+
