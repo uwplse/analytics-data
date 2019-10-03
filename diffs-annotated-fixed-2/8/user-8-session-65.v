94448a92f7e5c90c547f0354288ce490226453aa
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:24:23 -0700

    user 8, session 65, part 43

--- diffs-annotated-fixed-2/8/user-8-session-65.v
+++ diffs-annotated-fixed-2/8/user-8-session-65.v
@@ -979,5 +979,6 @@ gen \207\129.
 (apply f_equal_gen; trivial).
 (rewrite size_ntensor; unify_pows_two; simpl; try lia).
 (rewrite size_ntensor; unify_pows_two; simpl; try lia).
+(unify_matrix_dims idtac).
 (* Auto-generated comment: Succeeded. *)
 
