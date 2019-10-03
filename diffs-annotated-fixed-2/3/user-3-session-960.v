1275dd00b78981793edf21862441655f56a7f557
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 17:18:42 -0700

    user 3, session 960, part 70

--- diffs-annotated-fixed-2/3/user-3-session-960.v
+++ diffs-annotated-fixed-2/3/user-3-session-960.v
@@ -76,7 +76,8 @@ list {x : nat | x < S (S base)} :=
   | 0 => nil
   | _ =>
       let digit := x mod S (S base) in
-      exist _ digit _ :: nat_to_le base (x / S (S base))
+      exist _ digit (PeanoNat.Nat.mod_upper_bound _)
+      :: nat_to_le base (x / S (S base))
   end.
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
