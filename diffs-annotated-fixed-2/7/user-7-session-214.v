2f479c3b9a867154dc00314fd3db5d14f1153a7e
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:30 -0700

    user 7, session 214, part 13

--- diffs-annotated-fixed-2/7/user-7-session-214.v
+++ diffs-annotated-fixed-2/7/user-7-session-214.v
@@ -260,6 +260,18 @@ Proof.
 -
 (rewrite b_subst_pair).
 (apply f_free_in_ty_pair).
-(apply f_free_in_ty_pair__inv).
+(apply f_free_in_ty_pair__inv in HX).
+tauto.
+-
+(rewrite b_subst_union).
+(apply f_free_in_ty_union).
+(apply f_free_in_ty_union__inv in HX).
+tauto.
+-
+(apply f_free_in_ty_exist__inv in HX).
+specialize (IHt HX).
+(destruct (beq_idP Y i)).
++
+(subst; simpl).
 (* Auto-generated comment: Failed. *)
 
