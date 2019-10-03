6259b67764cd36c476fe328cd808a5ec6abba8f9
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:18:03 -0700

    user 7, session 10, part 26

--- diffs-annotated-fixed-2/7/user-7-session-10.v
+++ diffs-annotated-fixed-2/7/user-7-session-10.v
@@ -179,6 +179,8 @@ Proof.
 -
 tauto.
 -
-(destruct (in_nf_pair__inv _ _ Hnfm1) as [Hnfmx Hnfmy]).
+(destruct (in_nf_pair__inv _ _ Hnfm1) as [Hnfm11 Hnfm12]).
+(destruct (in_nf_pair__inv _ _ Hnfm2) as [Hnfm21 Hnfm22]).
+(destruct IHHsub1 as [IHHsub11 IHHsub12]).
 (* Auto-generated comment: Failed. *)
 
