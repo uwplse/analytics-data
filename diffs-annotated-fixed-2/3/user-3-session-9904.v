7385738986e801e06269ccfb0743f486eae3c251
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:12:13 -0700

    user 3, session 9904, part 86

--- diffs-annotated-fixed-2/3/user-3-session-9904.v
+++ diffs-annotated-fixed-2/3/user-3-session-9904.v
@@ -131,13 +131,22 @@ intuition.
 {
 congruence.
 }
-(simpl).
 (unfold statdb_abstraction in *; intuition).
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoBERUr"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqyMkn2P"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
+Qed.
+Theorem recover_wipe : rec_wipe recover abstr no_crash.
+Proof.
+(unfold rec_wipe).
+(intros).
+(apply spec_abstraction_compose; simpl).
+(step_proc; intros).
+{
+eauto.
+}
 (* Auto-generated comment: Succeeded. *)
 
