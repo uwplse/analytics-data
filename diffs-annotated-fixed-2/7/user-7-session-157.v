0c14553c5b3182b760b8e14071d989c69326e59d
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:21:22 -0700

    user 7, session 157, part 14

--- diffs-annotated-fixed-2/7/user-7-session-157.v
+++ diffs-annotated-fixed-2/7/user-7-session-157.v
@@ -48,6 +48,6 @@ exists v.
 Check beq_id_false_iff.
 Print "<->".
 Print "/\".
-(destruct (beq_id_false_iff X i) as [H1 H2]).
+(destruct (beq_id_false_iff X i) as [_ Hbeq]).
 (* Auto-generated comment: Failed. *)
 
