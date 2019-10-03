1fe4528dc91d7aa502c304235ef1afe47809ad40
Talia Ringer <tringer@cs.washington.edu>
2019-10-02 18:22:04 -0700

    user 7, session 189, part 4

--- diffs-annotated-fixed-2/7/user-7-session-189.v
+++ diffs-annotated-fixed-2/7/user-7-session-189.v
@@ -21,7 +21,9 @@ Proof.
 Search -IdSet.In.
 (apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
 Qed.
-Lemma fresh_in_ty__decidable : forall (X : id) (t : ty), Decidable.decidable (fresh_in_ty X t).
+Lemma not_fresh_and_free_in_ty : forall (X : id) (t : ty), free_in_ty X t -> fresh_in_ty X t -> False.
 Proof.
-(* Auto-generated comment: Failed. *)
+(unfold free_in_ty, fresh_in_ty, fresh).
+contradiction.
+(* Auto-generated comment: Succeeded. *)
 
