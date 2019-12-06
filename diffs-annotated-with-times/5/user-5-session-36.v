7a1b43eb9d8a0e440f60c63eec4ba8d06056d9b5
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:46 -0800

    user 5, session 36, part 45

--- diffs-annotated-with-times/5/user-5-session-36.v
+++ diffs-annotated-with-times/5/user-5-session-36.v
@@ -266,9 +266,12 @@ clear H.
 (destruct H).
 (erewrite evalBoolConst in H0).
 (rewrite <- evalEqTrue in H0).
+(rewrite evalVar in H).
+(rewrite extendEnv_eq in H).
 (assert (x0 = 3%Z)).
 {
+(erewrite evalTimes in H0).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-01 10:51:48.340000.*)
+(* Auto-generated comment: At 2019-09-01 10:52:48.240000.*)
 
