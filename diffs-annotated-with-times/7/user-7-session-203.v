b4f5ad946d8918fd460416605b825e743d22b8a0
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:56 -0800

    user 7, session 203, part 9

--- diffs-annotated-with-times/7/user-7-session-203.v
+++ diffs-annotated-with-times/7/user-7-session-203.v
@@ -205,5 +205,5 @@ exists 0,(TCName c).
 (destruct IHt1 as [IHt1| [w1 [v1 Hm1]]]; destruct IHt2 as [IHt2| [w2 [v2 Hm2]]]).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 07:49:04.520000.*)
+(* Auto-generated comment: At 2019-09-04 07:49:48.200000.*)
 
