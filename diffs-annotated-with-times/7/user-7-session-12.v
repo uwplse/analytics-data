18fcb20c59ecd2c967c62aba3f1eeae0c43dfc85
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:58 -0800

    user 7, session 12, part 21

--- diffs-annotated-with-times/7/user-7-session-12.v
+++ diffs-annotated-with-times/7/user-7-session-12.v
@@ -307,9 +307,8 @@ Qed.
 Lemma mk_nf__distr11 : forall t11 t12 t2 : ty, |- MkNF( TPair (TUnion t11 t12) t2) << MkNF( TUnion (TPair t11 t2) (TPair t12 t2)).
 Proof.
 (intros t11 t12 t2).
-(repeat rewrite mk_nf_union).
-(rewrite mk_nf_pair).
+(rewrite mk_nf_union).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-09 07:44:40.540000.*)
+(* Auto-generated comment: At 2019-08-09 07:44:46.990000.*)
 
