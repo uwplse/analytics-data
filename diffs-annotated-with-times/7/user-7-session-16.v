6e83761986e47e9bcdf389fd05ec910427bf9c36
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:00 -0800

    user 7, session 16, part 49

--- diffs-annotated-with-times/7/user-7-session-16.v
+++ diffs-annotated-with-times/7/user-7-session-16.v
@@ -385,9 +385,9 @@ right.
         subst
  end).
 (match goal with
- | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto
+ | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra
  end).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-09 11:48:06.590000.*)
+(* Auto-generated comment: At 2019-08-09 11:48:32.700000.*)
 
