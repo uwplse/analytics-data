952dcd67aa98596d314bde3f20e48dd5cf832d1f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:04 -0800

    user 7, session 104, part 58

--- diffs-annotated-with-times/7/user-7-session-104.v
+++ diffs-annotated-with-times/7/user-7-session-104.v
@@ -65,8 +65,8 @@ Proof.
 (induction k; induction t; intros Hdep Hm; try (solve [ apply match_ty_cname__inv in Hm; subst; constructor ])).
 -
 (apply match_ty_pair__inv in Hm).
-(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 13:13:30.550000.*)
+(* Auto-generated comment: At 2019-08-20 13:14:26.400000.*)
 
