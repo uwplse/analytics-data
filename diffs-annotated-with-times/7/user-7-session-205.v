673c0695bb21649ac5c5905bc7f69b2556b19c0f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:57 -0800

    user 7, session 205, part 2

--- diffs-annotated-with-times/7/user-7-session-205.v
+++ diffs-annotated-with-times/7/user-7-session-205.v
@@ -8,7 +8,17 @@ Require Import BetaJulia.Sub0282a.BaseDefs.
 Require Import BetaJulia.Sub0282a.BaseProps.
 Require Import BetaJulia.Sub0282a.BaseMatchProps.
 Require Import BetaJulia.Sub0282a.BaseSemSubProps.
+Require Import Coq.Lists.List.
+Import ListNotations.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Bool.Bool.
+Lemma build_v_full :
+  forall (X X' : id) (tx : ty) (w : nat) (t v : ty),
+  |-[ w] v <$ [BX := tx] t ->
+  exists v' : ty,
+    |-[ w] v' <$ [BX := TFVar X'] t /\
+    (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> (fresh_in_ty X' t' -> |-[ w'] v <$ t') /\ (free_in_ty X' t' -> |-[ w'] v <$ [FX' := tx] t')).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 08:07:54.310000.*)
+(* Auto-generated comment: At 2019-09-04 08:08:02.570000.*)
 
