36272d4e1a8d9c8dcb6fc6dbe40d8a884a2ebe23
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:10 -0800

    user 3, session 7199, part 206

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -280,13 +280,13 @@ step_proc.
 (descend; intuition eauto).
 (rewrite firstn_all; auto).
 Qed.
-Theorem log_contents_ok_unchanged d bs a b :
+Theorem log_contents_ok_unchanged d bs a0 b :
   log_contents_ok d bs ->
-  log_addr a >= length bs -> log_contents_ok (diskUpd d a b) bs.
+  log_addr a0 >= length bs -> log_contents_ok (diskUpd d a0 b) bs.
 Proof.
 (unfold log_contents_ok; intros).
 specialize (H a).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 11:45:41.650000.*)
+(* Auto-generated comment: At 2019-09-04 11:46:06.040000.*)
 
