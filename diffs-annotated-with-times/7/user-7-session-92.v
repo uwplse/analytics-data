9090c685ec407f3bd484442a464ba83a6743733b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:52 -0800

    user 7, session 92, part 5

--- diffs-annotated-with-times/7/user-7-session-92.v
+++ diffs-annotated-with-times/7/user-7-session-92.v
@@ -13,37 +13,5 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-Theorem sub_r__sound : forall t1 t2 : ty, (|- t1 << t2)%btjr -> (|- t1 << t2)%btjd.
-Proof.
-(intros t1 t2 Hsub; induction Hsub; try (solve [ constructor; assumption ])).
--
-(apply union_right_1; assumption).
--
-(apply union_right_2; assumption).
--
-(apply SD_Trans with (MkNF( t))).
-(apply mk_nf__sub_d_r).
-assumption.
-Qed.
-Theorem sub_r__complete : forall t1 t2 : ty, (|- t1 << t2)%btjd -> (|- t1 << t2)%btjr.
-Proof.
-(intros t1 t2 Hsub; induction Hsub; try (solve [ constructor; assumption ])).
--
-(apply sub_r__reflexive).
--
-(apply sub_r__transitive with t2; assumption).
--
-(apply SR_UnionR1; apply sub_r__reflexive).
--
-(apply SR_UnionR2; apply sub_r__reflexive).
--
-(apply mk_nf_sub_r__sub_r).
-(apply mk_nf__distr11).
--
-(apply mk_nf_sub_r__sub_r).
-(apply mk_nf__distr21).
-Qed.
-(* Auto-generated comment: Failed. *)
-
-(* Auto-generated comment: At 2019-08-18 07:46:53.180000.*)
+(* Auto-generated comment: Succeeded. *)
 
