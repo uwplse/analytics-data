2b9b642c74495100dd941881bdfee0ff4a075fd8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:25 -0800

    user 7, session 148, part 13

--- diffs-annotated-with-times/7/user-7-session-148.v
+++ diffs-annotated-with-times/7/user-7-session-148.v
@@ -102,8 +102,7 @@ clear IHt'1 IHt'2.
 (apply sem_sub_pair__inv in Hsem).
 (destruct Hsem as [Hsem1 Hsem2]).
 (destruct (fresh_in_ty_pair__inv _ _ _ Hfresh') as [Hfresh1 Hfresh2]).
-specialize (IHt1 _ _ Hfresh1 Hsem1).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-29 07:45:15.390000.*)
+(* Auto-generated comment: At 2019-08-29 07:45:43.310000.*)
 
