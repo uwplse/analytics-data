ad3bfc66157d7b643fdd5cd08a459daed453c205
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:00 -0800

    user 7, session 102, part 4

--- diffs-annotated-with-times/7/user-7-session-102.v
+++ diffs-annotated-with-times/7/user-7-session-102.v
@@ -69,7 +69,7 @@ Fixpoint match_ty (k : nat) :=
         | S k, _, TRef t', TRef t =>
             (forall w1, exists w2, forall v, |-[ k, w1] v <$ t' -> |-[ k, w2] v <$ t) /\
             (forall w1, exists w2, forall v, |-[ k, w1] v <$ t -> |-[ k, w2] v <$ t')
-        | _, S w', v, TExist X t' => exists tx, mtyw w' v ([X := tx] t')
+        | _, S w, v, TExist X t' => exists tx, mtyw w v ([X := tx] t')
         | _, _, TEV X, TVar X' => X = X'
         | _, _, TEV X, TEV X' => X = X'
         | _, _, _, _ => False
@@ -77,5 +77,5 @@ Fixpoint match_ty (k : nat) :=
 where "'|-[' k ',' w ']' v '<$' t" := (match_ty w k v t) : btjm_scope.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 11:11:51.210000.*)
+(* Auto-generated comment: At 2019-08-20 11:12:00.620000.*)
 
