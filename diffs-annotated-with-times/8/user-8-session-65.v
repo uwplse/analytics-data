10b387787f6a5ddc8fd99a1af1de88e4cada0580
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:12 -0800

    user 8, session 65, part 86

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -973,9 +973,9 @@ restore_dims try rewrite size_ntensor; unify_pows_two; simpl; try lia.
 (repeat rewrite Mmult_assoc).
 Msimpl.
 (match goal with
- | |- ?A \226\138\151 ?B \195\151 (?C \226\138\151 ?D) => idtac A; idtac B; idtac C; idtac D
+ | |- context [ ?A \226\138\151 ?B \195\151 (?C \226\138\151 ?D) ] => idtac A; idtac B; idtac C; idtac D
  end).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-13 13:54:01.590000.*)
+(* Auto-generated comment: At 2019-08-13 13:54:02.020000.*)
 
