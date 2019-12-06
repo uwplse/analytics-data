8ab374ba662e50c69b081af247b2d580217b22ca
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:57 -0800

    user 8, session 79, part 444

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -956,12 +956,15 @@ Timeout 1 About rewrite_inPar''.
 Timeout 1 Print rewrite_inPar''.
 Timeout 1 Print Ltac rewrite_inPar''.
 (match goal with
- | |- context [ denote_box true (inPar ?f ?g) (kron ?\207\1291 ?\207\1292) ] =>
+ | |-
+   context [ (@denote_box true ?W ?W' (@inPar ?W1 ?W1' ?W2 ?W2' ?f ?g))
+               (@kron ?m ?n ?o ?p ?\207\1291 ?\207\1292) ] =>
        let IP := fresh "IP" in
        specialize (inPar_correct W1 W1' W2 W2' f g true \207\1291 \207\1292) as IP;
         rewrite size_ntensor in *; simpl in *; try rewrite Nat.mul_1_r in *
- end).
+ end; try (solve [ type_check ])).
+(rewrite IP).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-14 15:29:16.880000.*)
+(* Auto-generated comment: At 2019-08-14 15:29:36.930000.*)
 
