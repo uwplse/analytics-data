cebbff187b94591f3c282710bed6aefd0d5c60f4
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:24 -0800

    user 8, session 65, part 263

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -1250,8 +1250,6 @@ constructor.
 (intros \207\129 TB).
 (repeat simpl_rewrite inSeq_correct; try apply inSeq_WT; try apply unitary_at1_WT;
   try apply source_symmetric_WT; trivial).
-Timeout 1 About inSeq_correct.
-Timeout 1 Print inSeq_correct.
-Timeout 1 Print Ltac inSeq_correct.
+(unfold compose_super).
 (* Auto-generated comment: Succeeded. *)
 
