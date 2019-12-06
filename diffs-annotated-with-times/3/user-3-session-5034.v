944eb2049cafc6ce73c66a170031339ad26f4e9f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:35:19 -0800

    user 3, session 5034, part 23

--- diffs-annotated-with-times/3/user-3-session-5034.v
+++ diffs-annotated-with-times/3/user-3-session-5034.v
@@ -387,7 +387,16 @@ step_proc.
 (step_proc; intuition; subst; eauto).
 -
 (step_proc; intuition; subst; eauto).
+{
+(step_proc; intuition; subst; eauto).
+{
+(step_proc; intuition; subst; eauto).
+{
+(step_proc; intuition; subst; eauto).
+*
+(exists (s ++ v); simpl; intuition).
+(apply log_abstraction_post_commit; auto).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-03 10:59:37.700000.*)
+(* Auto-generated comment: At 2019-09-03 10:59:40.310000.*)
 
