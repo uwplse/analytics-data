32745a97abd424ae6704974d885c23601e8cd90e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:10 -0800

    user 8, session 65, part 13

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -967,8 +967,19 @@ gen \207\129.
 (simpl).
 (rewrite Nat.mul_1_r).
 (intros \207\129).
-restore_dims.
+Timeout 1 About restore_dims.
+Timeout 1 Print restore_dims.
+Timeout 1 Print Ltac restore_dims.
+(match goal with
+ | |- ?A => let A' := restore_dims_rec tac A in
+            replace
+            A
+            with
+            A'
+ end).
+2: {
+unify_matrix_dims.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 10:42:55.920000.*)
+(* Auto-generated comment: At 2019-08-13 10:42:56.740000.*)
 
