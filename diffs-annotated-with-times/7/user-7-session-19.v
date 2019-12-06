1ae90672a2f407cd793977c02292889939f1cfdb
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:12 -0800

    user 7, session 19, part 208

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -108,11 +108,11 @@ Proof.
   match goal with
   | |- | ?t1 | <= | ?t2 | =>
         (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
-          (H _ Hm)) ||
+          (H _ Hm); contradiction) ||
           (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
             (H _ Hv); destruct H as [_ H]; specialize (H Hm); contradiction)
   end).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 13:24:28.040000.*)
+(* Auto-generated comment: At 2019-08-12 13:24:53.030000.*)
 
