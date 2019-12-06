5829bb49ea79be29f405e5eb466a4f6fc6b5097e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:16 -0800

    user 8, session 2, part 152

--- diffs-annotated-with-times/8/user-8-session-2.v
+++ diffs-annotated-with-times/8/user-8-session-2.v
@@ -136,8 +136,8 @@ Search -fst -snd.
 (rewrite <- ctrls_to_list_transpose_fst).
 (destruct (ctrls_to_list (repeat false n) li u) as [[j l] v] eqn:E).
 (simpl).
-(rewrite ctrl_list_to_unitary_transpose).
-(* Auto-generated comment: Failed. *)
+(rewrite <- ctrl_list_to_unitary_transpose).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-09 12:27:42.690000.*)
+(* Auto-generated comment: At 2019-08-09 12:29:16.500000.*)
 
