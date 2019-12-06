75d68b2cb66bbf018f51c551073a26e62752dc52
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:21:17 -0800

    user 3, session 960, part 40

--- diffs-annotated-with-times/3/user-3-session-960.v
+++ diffs-annotated-with-times/3/user-3-session-960.v
@@ -79,8 +79,14 @@ Proof.
 (intros).
 (simpl).
 Search -Nat.modulo -"<".
-(apply PeanoNat.Nat.mod_upper_bound).
+(apply PeanoNat.Nat.mod_upper_bound; auto).
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqPcKfTK"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-16 05:33:55.870000.*)
+(* Auto-generated comment: At 2019-08-16 05:34:23.570000.*)
 
