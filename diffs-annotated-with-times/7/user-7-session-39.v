519b4f45783146c489f6bbd1e215cfbc2df514b5
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:33 -0800

    user 7, session 39, part 3

--- diffs-annotated-with-times/7/user-7-session-39.v
+++ diffs-annotated-with-times/7/user-7-session-39.v
@@ -2,11 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0250a.BaseDefs.
-Require Import BetaJulia.Sub0250a.AltMatchDefs.
 (* Auto-generated comment: Succeeded. *)
-
-(* Auto-generated comment: At 2019-08-16 06:38:41.110000.*)
 
