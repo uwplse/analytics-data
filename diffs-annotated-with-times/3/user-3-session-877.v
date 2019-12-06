a1a633cd239c97672759246183def0b5a62ec6f7
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:20:57 -0800

    user 3, session 877, part 8

--- diffs-annotated-with-times/3/user-3-session-877.v
+++ diffs-annotated-with-times/3/user-3-session-877.v
@@ -1366,8 +1366,9 @@ Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 iFrame.
-(cbn[MailboxStatusInterp]).
+(cbn[MailboxStatusInterp fst]).
+(<ssreflect_plugin::ssrtclseq@0> iDestruct "Hmbox" as "[Hmbox|Hmbox]" ; last  first).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-14 13:18:51.380000.*)
+(* Auto-generated comment: At 2019-08-14 13:19:27.340000.*)
 
