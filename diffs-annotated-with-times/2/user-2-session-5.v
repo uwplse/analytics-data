f7d8f1afd65427cd33a64628af8a9981733d8dc6
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:41 -0800

    user 2, session 5, part 36

--- diffs-annotated-with-times/2/user-2-session-5.v
+++ diffs-annotated-with-times/2/user-2-session-5.v
@@ -19,36 +19,7 @@ Inductive term :=
   | App : term -> term -> term.
 Redirect "/tmp/coqmW6Ht8" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
-Fixpoint value (t : term) : bool :=
-  match t with
-  | Nil => true
-  | Ident _ => true
-  | Cons a b => value a && value b
-  | App f a => false
-  end.
-Redirect "/tmp/coqhYSoRC" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Module TermNotations.
-Declare Scope coucou_scope.
-Notation "{ f a }" := (App f a) (f  at level 0, a  at level 0) : coucou_scope.
-Notation "< a b >" := (Cons a b) (format "< a  b >", a  at level 0, b  at level 0) : coucou_scope.
-Notation "[ ]" := Nil (format "[ ]") : coucou_scope.
-Notation "[ x ]" := (Cons x Nil) : coucou_scope.
-Notation "[ x y .. z ]" := (Cons x (Cons y .. (Cons z Nil) ..))
-  (x  at level 0, y  at level 0, z  at level 0) : coucou_scope.
-Coercion Ident : string >-> term.
-End TermNotations.
-Import TermNotations.
-Redirect "/tmp/coqhUNJDd" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Open Scope coucou_scope.
-Open Scope string_scope.
-Check
-  [<Nil <Nil "hi">> (Cons (Ident "1") (Ident "2")) (Ident "a")
-  {(Ident "myfun") (Ident "somArg")}].
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-17 14:41:05.650000.*)
+(* Auto-generated comment: At 2019-08-17 14:44:18.310000.*)
 
