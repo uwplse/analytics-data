db311d98f0e8070854ef05f791a6cb6139310a98
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:56 -0800

    user 7, session 203, part 11

--- diffs-annotated-with-times/7/user-7-session-203.v
+++ diffs-annotated-with-times/7/user-7-session-203.v
@@ -204,8 +204,8 @@ exists 0,(TCName c).
 -
 (destruct IHt1 as [IHt1| [w1 [v1 IHt1]]]; destruct IHt2 as [IHt2| [w2 [v2 IHt2]]]).
 +
-(left; intros w v Hm; apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]).
+(left; intros w v Hm; apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; contradiction).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 07:50:01.820000.*)
+(* Auto-generated comment: At 2019-09-04 07:50:06.730000.*)
 
