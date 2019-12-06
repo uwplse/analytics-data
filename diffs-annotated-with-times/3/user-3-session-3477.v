5268e276a457a03cebba0c2e1a1d4db1dd8ad294
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:29:18 -0800

    user 3, session 3477, part 6

--- diffs-annotated-with-times/3/user-3-session-3477.v
+++ diffs-annotated-with-times/3/user-3-session-3477.v
@@ -271,7 +271,18 @@ clear H.
 (descend; intuition eauto).
 -
 (unfold proc_spec; simpl; intros).
+(destruct matches; subst; eauto).
+(eexists; intuition eauto).
+(inv_rexec; inv_exec).
+congruence.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqKSBnLc"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Qed.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-29 07:16:42.380000.*)
+(* Auto-generated comment: At 2019-08-29 07:16:42.620000.*)
 
