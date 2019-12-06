e438662a63a9a5250b51ebc41429db0eac556fee
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:31 -0800

    user 1, session 29, part 1

--- diffs-annotated-with-times/1/user-1-session-29.v
+++ diffs-annotated-with-times/1/user-1-session-29.v
@@ -2,8 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Import Analytical.Analytics.
 (* Auto-generated comment: Succeeded. *)
-
-(* Auto-generated comment: At 2019-08-09 15:55:54.010000.*)
 
