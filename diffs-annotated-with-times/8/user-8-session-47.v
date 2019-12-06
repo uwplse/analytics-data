0f2a317909a26b4e8153561a4a22aa577da53efa
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:57 -0800

    user 8, session 47, part 2

--- diffs-annotated-with-times/8/user-8-session-47.v
+++ diffs-annotated-with-times/8/user-8-session-47.v
@@ -625,8 +625,12 @@ Proof.
 (induction n; intros).
 -
 (simpl).
-Msimpl.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqkgd1Jh"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+(rewrite (kron_1_r u)).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-12 15:03:15.480000.*)
+(* Auto-generated comment: At 2019-08-12 15:04:23.440000.*)
 
