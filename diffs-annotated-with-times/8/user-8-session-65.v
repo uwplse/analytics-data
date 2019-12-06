260e4c8e6896b7b1ef00d72f6af2f95068003e2e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:13 -0800

    user 8, session 65, part 113

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -969,6 +969,6 @@ gen \207\129.
 (rewrite Nat.mul_1_r).
 (rewrite Lt).
 (rewrite Nat.pow_add_r, <- (Nat.mul_1_r i)).
-(intros \207\129 intros \207\129).
-(* Auto-generated comment: Failed. *)
+(intros \207\129).
+(* Auto-generated comment: Succeeded. *)
 
