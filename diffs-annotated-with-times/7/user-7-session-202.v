a2bc17b0562f18406d3c289936492c41333736cc
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:55 -0800

    user 7, session 202, part 18

--- diffs-annotated-with-times/7/user-7-session-202.v
+++ diffs-annotated-with-times/7/user-7-session-202.v
@@ -133,8 +133,8 @@ Proof.
    | try destruct (wf_ty_pair__inv _ _ Hwf) as [wf1 wf2]; try destruct (wf_ty_union__inv _ _ Hwf) as [wf1 wf2]; simpl; rewrite IHt1; try assumption;
       rewrite IHt2; try assumption; reflexivity ])).
 -
-(destruct (beq_idP X i); try reflexivity).
+(destruct (beq_idP X i); try subst; simpl; reflexivity).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-03 09:57:28.350000.*)
+(* Auto-generated comment: At 2019-09-03 09:57:30.740000.*)
 
