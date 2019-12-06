e286643c394d9fb1900de324ba7e54935e29d3c0
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:03 -0800

    user 7, session 104, part 9

--- diffs-annotated-with-times/7/user-7-session-104.v
+++ diffs-annotated-with-times/7/user-7-session-104.v
@@ -53,8 +53,8 @@ Proof.
 specialize (Hcontra 2 1).
 (destruct Hcontra as [w Hcontra]).
 (assert (Hm : |-[ 2, 1] TRef (TExist vX (TRef tX)) <$ TRef (TExist vX (TRef tX))) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Hcontra _ _ Hm).
-(* Auto-generated comment: Succeeded. *)
+(simpl in Hcontra).
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 12:15:46.370000.*)
+(* Auto-generated comment: At 2019-08-20 12:15:50.940000.*)
 
