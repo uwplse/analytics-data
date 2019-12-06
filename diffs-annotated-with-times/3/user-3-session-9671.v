a74e1fd22e5d6b4a72917a6767846eff64f8809d
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:49:37 -0800

    user 3, session 9671, part 28

--- diffs-annotated-with-times/3/user-3-session-9671.v
+++ diffs-annotated-with-times/3/user-3-session-9671.v
@@ -209,8 +209,9 @@ Proof.
 (apply spec_abstraction_compose).
 step.
 (destruct a' as [[] bs]; simpl in *; intuition eauto).
+step.
 (exists bs; intuition eauto).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-05 17:35:14.570000.*)
+(* Auto-generated comment: At 2019-09-05 17:35:56.210000.*)
 
