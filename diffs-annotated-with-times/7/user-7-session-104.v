08cb295a0ebd2cc87410667a037038e78427cd9a
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:05 -0800

    user 7, session 104, part 105

--- diffs-annotated-with-times/7/user-7-session-104.v
+++ diffs-annotated-with-times/7/user-7-session-104.v
@@ -172,16 +172,10 @@ contradiction.
 (apply match_ty_exist__inv in Hm).
 (destruct Hm as [tx Hmx]).
 Abort.
-Lemma ty__empty_or_matching_ty_exist : forall (t : ty) (k : nat), exists (w : nat) (v : ty), |-[ k, w] v <$ t.
+Lemma match_ty__match_ge_world : forall (w k : nat) (t v : ty), |-[ k, w] v <$ t -> forall w' : nat, w <= w' -> |-[ k, w'] v <$ t.
 Proof.
-(induction t; intros k).
--
-exists 0,(TCName c).
-(apply match_ty_value_type__reflexive; constructor).
--
-(destruct (IHt1 k) as [w1 [v1 Hm1]]).
-(destruct (IHt2 k) as [w2 [v2 Hm2]]).
+(induction w; intros k t; generalize k; induction t).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-21 07:50:13.210000.*)
+(* Auto-generated comment: At 2019-08-21 07:51:35.150000.*)
 
