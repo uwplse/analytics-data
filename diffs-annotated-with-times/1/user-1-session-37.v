28532702c76a085ba646a2d21fbac3947c082023
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:34 -0800

    user 1, session 37, part 57

--- diffs-annotated-with-times/1/user-1-session-37.v
+++ diffs-annotated-with-times/1/user-1-session-37.v
@@ -380,26 +380,5 @@ Print SetPMap.
 Print Forall.
 Search -Ensemble.
 Print SetPMap.
-Inductive Alpha : SetST -> GT -> Prop :=
-  | alpha_int : Alpha (Singleton _ SInt) GInt
-  | alpha_bool : Alpha (Singleton _ SBool) GBool
-  | alpha_fun :
-      forall S G_1 G_2,
-      (forall X,
-       Ensembles.In _ S X -> exists S_1 S_2, X = SFun S_1 S_2) ->
-      Alpha
-        (SetPMap S
-           (fun S =>
-            match S with
-            | SFun S_1 S_2 => Some S_1
-            | _ => None
-            end)) G_1 ->
-      Alpha
-        (SetPMap S
-           (fun S =>
-            match S with
-            | SFun S_1 S_2 => Some S_2
-            | _ => None
-            end)) G_2 -> Alpha S (GFun G_1 G_2).
 (* Auto-generated comment: Succeeded. *)
 
