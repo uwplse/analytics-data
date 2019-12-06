47612c0f2e5a863779b9e0178adce7c799c6dcaf
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:08 -0800

    user 7, session 224, part 66

--- diffs-annotated-with-times/7/user-7-session-224.v
+++ diffs-annotated-with-times/7/user-7-session-224.v
@@ -22,7 +22,11 @@ Lemma build_v_full :
     (forall w' : nat,
      exists w2 : nat,
        forall t' : ty, |-[ w'] v' <$ t' -> (not_f_free_in_ty X' t' -> |-[ w'] v <$ t') /\ (f_free_in_ty X' t' -> |-[ w2] v <$ [FX' := tx] t')).
+Proof.
+(intros X X' tx).
+(induction w; induction t; intros v Hwftx HX Hm;
+  try (solve [ unfold b_free_in_ty, free in HX; simpl in HX; rewrite IdSetFacts.empty_iff in HX; contradiction ])).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-06 09:14:37.930000.*)
+(* Auto-generated comment: At 2019-09-06 09:15:00.810000.*)
 
