03ad2692153b78bf2e167232c141560243ca0c1e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:52 -0800

    user 7, session 2, part 103

--- diffs-annotated-with-times/7/user-7-session-2.v
+++ diffs-annotated-with-times/7/user-7-session-2.v
@@ -129,11 +129,7 @@ Lemma sub_r_unite_pairs_l__inv :
 Proof.
 (intros t1; induction t1; intros t2; induction t2).
 -
-(match goal with
- | Hsub:|- ?t1 << ?t2
-   |- _ => remember t1 as tx eqn:Heqx ; remember t2 as ty eqn:Heqy ; induction Hsub; inversion Heqx; inversion Heqy; subst; tauto
- end).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-07 12:19:47.360000.*)
+(* Auto-generated comment: At 2019-08-07 12:20:07.160000.*)
 
