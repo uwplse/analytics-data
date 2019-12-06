e1e78c92d5a1873afd70367b3186e67bfe00c8c0
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:06 -0800

    user 3, session 7199, part 87

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -263,28 +263,9 @@ intuition eauto.
 (eexists; intuition eauto).
 Qed.
 Hint Resolve get_len_ok: core.
-Theorem get_at_ok a :
-  proc_spec
-    (fun (_ : unit) state =>
-     {|
-     pre := a < length state;
-     post := fun r state' => state' = state /\ nth a state block0 = r;
-     recovered := fun _ state' => state' = state |}) 
-    (get_at a) recover abstr.
-Proof.
-(unfold get_at; intros).
-(apply spec_abstraction_compose).
-(simpl).
-(eapply proc_spec_weaken; eauto).
-(unfold spec_impl; intros).
-(destruct a0 as [_ bs]; simpl in *; intuition eauto).
-(descend; intuition eauto).
-(descend; intuition eauto).
-(unfold log_abstraction in H0; intuition).
-(pose proof (H3 a); intuition).
-(assert (v = nth a bs block0)).
-(eapply diskGet_eq_values; eauto).
-(* Auto-generated comment: Succeeded. *)
+Theorem log_size_bound d bs :
+  log_size_ok d bs -> a < length bs -> log_addr a < diskSize d.
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 11:07:54.480000.*)
+(* Auto-generated comment: At 2019-09-04 11:07:58.170000.*)
 
