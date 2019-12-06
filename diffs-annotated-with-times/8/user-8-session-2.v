4bab9b57845ac6ce9dc27ca6517c9bb805a707a1
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:16 -0800

    user 8, session 2, part 146

--- diffs-annotated-with-times/8/user-8-session-2.v
+++ diffs-annotated-with-times/8/user-8-session-2.v
@@ -137,5 +137,5 @@ Search -fst -snd.
 (simpl).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-09 12:18:20.820000.*)
+(* Auto-generated comment: At 2019-08-09 12:18:29.490000.*)
 
