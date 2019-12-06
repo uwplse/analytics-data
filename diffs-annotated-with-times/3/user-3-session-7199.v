121c3506a4a6cf5a3112971e51a227d9f208428a
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:09 -0800

    user 3, session 7199, part 173

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -375,8 +375,11 @@ Theorem append_ok :
   forall v, proc_spec (append_spec v) (append v) recover abstr.
 Proof.
 (unfold append; intros).
+(apply spec_abstraction_compose).
 step_proc.
-(* Auto-generated comment: Failed. *)
+(destruct a' as [[] bs]; simpl in *).
+intuition eauto.
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 11:31:29.170000.*)
+(* Auto-generated comment: At 2019-09-04 11:31:54.940000.*)
 
