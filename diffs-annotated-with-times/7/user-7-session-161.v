8c6f4e448654974309a1feb506a78dccac35dfa2
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:33 -0800

    user 7, session 161, part 42

--- diffs-annotated-with-times/7/user-7-session-161.v
+++ diffs-annotated-with-times/7/user-7-session-161.v
@@ -91,26 +91,7 @@ reflexivity.
 (rewrite IHt1, IHt2).
 reflexivity.
 -
-(destruct (beq_idP Y i) as [HY| HY]).
-+
-subst.
-(destruct (beq_idP X i) as [HX| HX]).
-*
-subst.
-contradiction.
-*
-(rewrite subst_exist_eq).
-(rewrite (subst_exist_neq _ _ _ _ HX)).
-(rewrite subst_exist_eq).
-reflexivity.
-+
-(destruct (beq_idP X i) as [HX| HX]).
-*
-subst.
-(rewrite (subst_exist_neq _ _ _ _ HY)).
-(repeat rewrite subst_exist_eq).
-(rewrite (subst_exist_neq _ _ _ _ HY)).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-29 12:30:55.290000.*)
+(* Auto-generated comment: At 2019-08-29 13:23:31.490000.*)
 
