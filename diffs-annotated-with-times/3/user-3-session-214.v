3528a9c48181f9a376cf5a63ad06cc8887d07e52
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:18:23 -0800

    user 3, session 214, part 4

--- diffs-annotated-with-times/3/user-3-session-214.v
+++ diffs-annotated-with-times/3/user-3-session-214.v
@@ -82,7 +82,8 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqKp0FVe"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
+Instance nonError_gets  `(f : A -> T): (NonError (gets f)).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-07 14:05:25.020000.*)
+(* Auto-generated comment: At 2019-08-07 14:05:29.160000.*)
 
