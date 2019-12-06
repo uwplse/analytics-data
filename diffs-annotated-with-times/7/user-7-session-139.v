6aba6444fc0bca1e79a01cb66cf4b469b9d7e83c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:22 -0800

    user 7, session 139, part 25

--- diffs-annotated-with-times/7/user-7-session-139.v
+++ diffs-annotated-with-times/7/user-7-session-139.v
@@ -65,16 +65,9 @@ tauto.
 -
 (intros t' X' Hfresh Hsem).
 (simpl in *).
-(induction t').
-+
-(intros k).
-(destruct (match_ty__exists_w_v (TPair ([X := TVar X'] t1) ([X := TVar X'] t2)) k) as [w [v Hm]]).
-Check Hsem.
-specialize (Hsem k w).
-(destruct Hsem as [w2 Hsem]).
-specialize (Hsem _ Hm).
-(apply match_ty_pair__inv in Hm).
+(induction t'; intros k; destruct (match_ty__exists_w_v (TPair ([X := TVar X'] t1) ([X := TVar X'] t2)) k) as [w [v Hm]]; specialize (Hsem k w);
+  destruct Hsem as [w2 Hsem]; specialize (Hsem _ Hm); apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 09:11:25.790000.*)
+(* Auto-generated comment: At 2019-08-27 09:11:37.790000.*)
 
