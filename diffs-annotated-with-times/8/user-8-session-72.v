46891c84b01262eb3c61259b0932f742838dd54d
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:32 -0800

    user 8, session 72, part 31

--- diffs-annotated-with-times/8/user-8-session-72.v
+++ diffs-annotated-with-times/8/user-8-session-72.v
@@ -1242,5 +1242,6 @@ Timeout 1 Print ancilla_free_box_valid.
 specialize @ancilla_free_box_valid as AV.
 (unfold valid_ancillae_box in AV).
 (rewrite AV; [  | apply ancilla_free_X_at | type_check ]).
+(rewrite AV).
 (* Auto-generated comment: Succeeded. *)
 
