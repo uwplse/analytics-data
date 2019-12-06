9af49686ec92e6b826917f304f43ea5f948183ab
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:50:22 -0800

    user 3, session 9904, part 37

--- diffs-annotated-with-times/3/user-3-session-9904.v
+++ diffs-annotated-with-times/3/user-3-session-9904.v
@@ -119,8 +119,7 @@ lia.
 -
 (step_proc; intuition).
 (step_proc; intuition).
-(exists s; intuition).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-06 05:40:35.020000.*)
+(* Auto-generated comment: At 2019-09-06 05:40:40.710000.*)
 
