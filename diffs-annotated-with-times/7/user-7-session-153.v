591b1ec11b44bedccfbed32cf3ed5cd6fcd23505
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:27 -0800

    user 7, session 153, part 14

--- diffs-annotated-with-times/7/user-7-session-153.v
+++ diffs-annotated-with-times/7/user-7-session-153.v
@@ -12,28 +12,17 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-Lemma build_v : forall (X X' : id) (w : nat) (tx t v : ty), |-[ w] v <$ [X := tx] t -> exists v' : ty, |-[ w] v' <$ [X := TVar X'] t.
+Lemma build_v : forall (X X' : id) (tx : ty) (w : nat) (t v : ty), |-[ w] v <$ [X := tx] t -> exists v' : ty, |-[ w] v' <$ [X := TVar X'] t.
 Proof.
-(intros X X' w tx t).
-(induction t; intros v Hm).
+(intros X X' tx).
+(induction w; induction t; intros v Hm).
 -
 exists (TCName c).
 (apply match_ty_cname).
 -
 (simpl in *).
 (apply match_ty_pair__inv in Hm).
-(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(destruct (IHt1 _ Hm1) as [v1' Hm1']).
-(destruct (IHt2 _ Hm2) as [v2' Hm2']).
-exists (TPair v1' v2').
-(apply match_ty_pair; assumption).
--
-(simpl in *).
-(apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ destruct (IHt1 _ Hm) as [v' Hm'] | destruct (IHt2 _ Hm) as [v' Hm'] ]; exists v';
-  [ apply match_ty_union_1 | apply match_ty_union_2 ]; assumption).
--
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-29 08:54:40.280000.*)
+(* Auto-generated comment: At 2019-08-29 08:55:09.460000.*)
 
