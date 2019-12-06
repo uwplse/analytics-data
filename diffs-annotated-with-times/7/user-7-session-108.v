0235aeb1ec0d4d0b0e1869335e44e01be917c340
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:09 -0800

    user 7, session 108, part 103

--- diffs-annotated-with-times/7/user-7-session-108.v
+++ diffs-annotated-with-times/7/user-7-session-108.v
@@ -244,14 +244,15 @@ Abort.
 Lemma aaa : forall (k : nat) (t t' : ty), | t | <= k -> ||-[ k][t]= [t'] -> | t | = | t' |.
 Proof.
 (induction k; induction t; induction t'; intros Hdep Hsem; try reflexivity).
--
-(destruct Hsem as [Hsem _]).
-specialize (Hsem 0).
-(destruct Hsem as [w2 Hsem]).
-(assert (Hm : |-[ 0, 0] TCName c <$ TCName c) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Hsem _ Hm).
-(destruct w2; simpl in Hsem; contradiction).
+(try
+  match goal with
+  | Hsem:||-[ ?k][?t]<= [?t']
+    |- | ?t | = | ?t' | =>
+        destruct Hsem as [Hsem _]; specialize (Hsem 0); destruct Hsem as [w2 Hsem];
+         assert (Hm : |-[ 0, 0] t <$ t) by (apply match_ty_value_type__reflexive; constructor); specialize (Hsem _ Hm); 
+         destruct w2; simpl in Hsem; contradiction
+  end).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-21 13:08:01.140000.*)
+(* Auto-generated comment: At 2019-08-21 13:11:56.430000.*)
 
