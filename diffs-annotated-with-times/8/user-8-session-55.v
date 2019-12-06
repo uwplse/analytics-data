f7d9ea089acc31e59149b41e7e701a23908b1463
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:03 -0800

    user 8, session 55, part 0

---
+++ diffs-annotated-with-times/8/user-8-session-55.v
@@ -0,0 +1,13 @@
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqVQNm49"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Import Psatz.
+Require Import Reals.
+(* Auto-generated comment: Succeeded. *)
+
+(* Auto-generated comment: At 2019-08-12 20:29:54.240000.*)
+
