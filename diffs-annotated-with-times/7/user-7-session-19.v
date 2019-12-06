d3d7898479e6acf4720dbd183d8d25e642bb0bdc
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:09 -0800

    user 7, session 19, part 87

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -73,8 +73,8 @@ Proof.
 (destruct (max_inv_depth_le__components_le _ _ _ Htk) as [Htk1 Htk2]).
 (apply match_ty_i_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
 SearchPattern (Nat.max _ _ <= Nat.max _ _).
-(simpl; apply Nat.max_le_compat).
+(apply Nat.max_le_compat).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 11:24:39.710000.*)
+(* Auto-generated comment: At 2019-08-12 11:24:49.770000.*)
 
