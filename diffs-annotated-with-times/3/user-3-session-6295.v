732e699a79a6055c0be284edf7ed19d26e1f20ff
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:39:20 -0800

    user 3, session 6295, part 0

---
+++ diffs-annotated-with-times/3/user-3-session-6295.v
@@ -0,0 +1,12 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqUco7Dt"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Module Log (d: OneDiskAPI)<: LogAPI.
+(* Auto-generated comment: Failed. *)
+
+(* Auto-generated comment: At 2019-09-04 08:44:33.670000.*)
+
