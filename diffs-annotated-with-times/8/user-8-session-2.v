f0e3d5847e8aacd1bad1671c80d7a2016e603a99
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:12 -0800

    user 8, session 2, part 16

--- diffs-annotated-with-times/8/user-8-session-2.v
+++ diffs-annotated-with-times/8/user-8-session-2.v
@@ -50,9 +50,11 @@ Proof.
 (induction W; intros lb li u; try (solve [ inversion u ])).
 -
 (destruct li as [| k li]).
-(rewrite ctrls_to_list_empty in *).
-(rewrite ctrls_to_list_empty).
+(repeat rewrite ctrls_to_list_empty).
+reflexivity.
+(inversion u; subst).
+(simpl).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-08 12:50:22.680000.*)
+(* Auto-generated comment: At 2019-08-08 12:50:31.070000.*)
 
