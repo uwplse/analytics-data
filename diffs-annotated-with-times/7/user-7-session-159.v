3fc2d4a0c4ead9d0ca7bc5a818f2a6a973b9731d
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:30 -0800

    user 7, session 159, part 26

--- diffs-annotated-with-times/7/user-7-session-159.v
+++ diffs-annotated-with-times/7/user-7-session-159.v
@@ -99,10 +99,17 @@ assumption.
 (apply match_ty_union__inv in Hm).
 (destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
 +
-(destruct (beq_idP X i) as [Hbeq| Hbeq]).
+(destruct (beq_idP X' i) as [Hbeq| Hbeq]).
 *
 subst.
+(rewrite subst_exist_eq).
+assumption.
+*
+(rewrite (subst_exist_neq _ _ _ _ Hbeq)).
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [ti Hm]).
+(rewrite subst_neq__permute in Hm).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-29 09:37:17.370000.*)
+(* Auto-generated comment: At 2019-08-29 09:38:22.860000.*)
 
