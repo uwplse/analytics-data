721f28e3750a5cc682f1fdb6379829f2999e48ed
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:45 -0800

    user 5, session 36, part 13

--- diffs-annotated-with-times/5/user-5-session-36.v
+++ diffs-annotated-with-times/5/user-5-session-36.v
@@ -261,8 +261,13 @@ clear H.
  | H:eval ?L ?env (If (In ?x ?S) _ _) = _ |- _ => destruct (evalIn L env x S)
  end).
 -
-(rewrite evalIfTrue in H0).
+(rewrite evalIfTrue in H0; admit).
+-
+exfalso.
+(rewrite evalIfFalse in H0).
++
+(apply evalBoolInj).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-01 10:34:40.070000.*)
+(* Auto-generated comment: At 2019-09-01 10:35:25.030000.*)
 
