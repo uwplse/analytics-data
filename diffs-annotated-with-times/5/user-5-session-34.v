2863cafbf677cde9684b03198302b33dd654aefa
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:44 -0800

    user 5, session 34, part 3

--- diffs-annotated-with-times/5/user-5-session-34.v
+++ diffs-annotated-with-times/5/user-5-session-34.v
@@ -128,8 +128,13 @@ Proof.
 (intros).
 (destruct (L.(value_eq_dec) (L.(eval) env t1) (L.(eval) env t2))).
 -
-left.
+apply -> L.(evalEqTrue).
+assumption.
+-
+apply -> L.(evalEqFalse).
+assumption.
+Qed.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-01 08:56:26.190000.*)
+(* Auto-generated comment: At 2019-09-01 08:57:08.310000.*)
 
