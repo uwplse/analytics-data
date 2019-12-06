5bb1386670001bbf03a5caaa59aa91b29170d5eb
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:18 -0800

    user 7, session 19, part 456

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -200,8 +200,8 @@ Proof.
 (intros t t' Hsem).
 (split; intros k; specialize (Hsem (S k)); assert (Hvref : value_type (TRef t)) by constructor;
   assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hm)).
-(simpl in Hsem').
+(simpl in Hsem).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 08:15:51.510000.*)
+(* Auto-generated comment: At 2019-08-13 08:16:01.850000.*)
 
