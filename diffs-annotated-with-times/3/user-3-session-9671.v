ee97415fc2415ea8251c5c0a5ce9fce2f7ffb9f4
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:49:37 -0800

    user 3, session 9671, part 19

--- diffs-annotated-with-times/3/user-3-session-9671.v
+++ diffs-annotated-with-times/3/user-3-session-9671.v
@@ -520,11 +520,15 @@ intuition.
 (exists bs; intuition eauto).
 }
 step.
-intuition.
+intuition eauto.
 {
-eauto.
+(exists []; intuition eauto).
+(eapply log_abstraction_nil; eauto).
+(rewrite diskUpd_eq; eauto).
 }
+{
+(exists []; intuition eauto).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-05 17:34:00.160000.*)
+(* Auto-generated comment: At 2019-09-05 17:34:04.760000.*)
 
