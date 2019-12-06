6bc76ccaa59454ee20aee190773d43e82350304d
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:10 -0800

    user 7, session 19, part 124

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -108,8 +108,9 @@ specialize (H Hmt).
 (apply match_ty_i_ref__inv in H).
 (destruct H as [tx [Heq Href]]).
 (inversion Heq; subst).
-tauto.
+auto.
+}
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 12:09:35.090000.*)
+(* Auto-generated comment: At 2019-08-12 12:15:40.980000.*)
 
