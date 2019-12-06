4b2d3df722ea6f2187a6fbd67de8684f0c857c2c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:54 -0800

    user 8, session 79, part 366

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -923,8 +923,8 @@ restore_dims
 2: {
 restore_dims
  simpl; try rewrite size_ntensor; try rewrite L2; simpl; unify_pows_two; lia.
-(rewrite (kron_1_l_inv (\226\168\130 l2))).
-(* Auto-generated comment: Succeeded. *)
+(rewrite <- (kron_1_l (\226\168\130 l2))).
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-14 14:27:16.770000.*)
+(* Auto-generated comment: At 2019-08-14 14:28:00.070000.*)
 
