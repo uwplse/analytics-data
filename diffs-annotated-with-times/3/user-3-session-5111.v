8ec369984d0d3b641c2a7ca6323f8f55bda07332
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:35:33 -0800

    user 3, session 5111, part 12

--- diffs-annotated-with-times/3/user-3-session-5111.v
+++ diffs-annotated-with-times/3/user-3-session-5111.v
@@ -199,7 +199,9 @@ Proof.
 (apply spec_abstraction_compose; simpl).
 (step_proc; intros).
 (destruct a'; simpl in *; intuition idtac).
+{
+(destruct (a == r)).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-03 11:03:00.820000.*)
+(* Auto-generated comment: At 2019-09-03 11:03:09.390000.*)
 
