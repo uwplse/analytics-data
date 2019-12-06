3a7200a8731c5e4577035274bb3b0a45ccc333b6
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:28:13 -0800

    user 3, session 3117, part 50

--- diffs-annotated-with-times/3/user-3-session-3117.v
+++ diffs-annotated-with-times/3/user-3-session-3117.v
@@ -116,15 +116,8 @@ Unset Search Output Name Only.
 Check Ascii.nat_ascii_bounded.
 Theorem N_ascii_bounded : forall a, (Ascii.N_of_ascii a < 256)%N.
 Proof.
-(destruct a as [[| ] [| ] [| ] [| ] [| ] [| ] [| ] [| ]]; vm_compute;
-  reflexivity).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqrQYZbi"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+(destruct a as [[| ] [| ] [| ] [| ] [| ] [| ] [| ] [| ]]; vm_compute).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-16 10:11:24.150000.*)
+(* Auto-generated comment: At 2019-08-16 10:11:48.280000.*)
 
