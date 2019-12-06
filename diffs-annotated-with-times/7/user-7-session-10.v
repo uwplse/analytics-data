b31862ca113c5246bc4c82c58bf73323257d831e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:56 -0800

    user 7, session 10, part 38

--- diffs-annotated-with-times/7/user-7-session-10.v
+++ diffs-annotated-with-times/7/user-7-session-10.v
@@ -196,8 +196,8 @@ tauto.
 (split; intros tx Hsub').
 +
 (remember (TUnion t1 t2) as ty eqn:Heqy ).
-(induction Hsub'; inversion Heqy; subst; try (solve [ auto || constructor; tauto ])).
+(induction Hsub'; inversion Heqy; subst; try (solve [ constructor; tauto || auto ])).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-08 13:40:44.830000.*)
+(* Auto-generated comment: At 2019-08-08 13:41:04.700000.*)
 
