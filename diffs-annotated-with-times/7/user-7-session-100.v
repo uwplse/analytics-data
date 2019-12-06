b8d36451944763b8bb270c1a27a0eea69d9e1b1b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:57 -0800

    user 7, session 100, part 14

--- diffs-annotated-with-times/7/user-7-session-100.v
+++ diffs-annotated-with-times/7/user-7-session-100.v
@@ -82,8 +82,23 @@ Qed.
 Lemma match_ty_var__inv : forall (v : ty) (X : id) (w k : nat), |-[ w, k] v <$ TVar X -> v = TEV X.
 Proof.
 (intros v; induction v; try (solve [ intros X w k Hm; destruct w; destruct k; contradiction ])).
+(intros X w k Hm).
+(destruct w; destruct k; simpl in Hm; subst; reflexivity).
+Qed.
+Theorem match_ty__value_type_l : forall (w k : nat) (v t : ty), |-[ w, k] v <$ t -> value_type v.
+Proof.
+(induction w; induction k; intros v t; generalize dependent v; induction t; intros v Hm;
+  try (solve
+   [ apply match_ty_cname__inv in Hm; subst; constructor
+   | apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; constructor; [ eapply IHt1 | eapply IHt2 ]; eauto
+   | apply match_ty_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto
+   | apply match_ty_ref__weak_inv in Hm; destruct Hm as [t' Heq]; subst; constructor
+   | apply match_ty_var__inv in Hm; assumption ])).
+-
+(apply match_ty_exist__0_inv in Hm).
+auto.
 -
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 08:03:02.760000.*)
+(* Auto-generated comment: At 2019-08-20 08:03:27.690000.*)
 
