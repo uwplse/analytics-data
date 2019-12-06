7dfc438f7ffe5a4e834d696e3246dd791810ff33
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:47 -0800

    user 8, session 34, part 30

--- diffs-annotated-with-times/8/user-8-session-34.v
+++ diffs-annotated-with-times/8/user-8-session-34.v
@@ -29,6 +29,7 @@ Proof.
 (intros safe w c \206\147 w' f \206\1470 \206\1471 \206\1471' \206\14701 \207\129 TP).
 dependent induction TP.
 -
+(intros WT pf_merge1 pf_merge2).
 (simpl).
 (unfold compose_super).
 (unfold denote_circuit).
@@ -38,7 +39,15 @@ dependent induction TP.
 (rewrite Nat.add_sub).
 (rewrite size_fresh_ctx).
 (destruct pf_merge1 as [V1 M1]).
+replace (size_ctx \206\1471') with size_octx \206\1471' by easy.
+(rewrite M1 in *).
+(rewrite size_octx_merge by easy).
+(simpl).
+(rewrite (ctx_wtype_size w p \206\147 t)).
+admit.
+-
+(intros w' h \206\1473 \206\1472 \206\1473' \206\14703 WT pf_merge1 pf_merge2).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-10 12:30:39.410000.*)
+(* Auto-generated comment: At 2019-08-10 12:31:01.530000.*)
 
