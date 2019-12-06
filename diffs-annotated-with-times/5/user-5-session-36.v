c303d57588c81306cf930f806d117f852f14fc3f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:45 -0800

    user 5, session 36, part 23

--- diffs-annotated-with-times/5/user-5-session-36.v
+++ diffs-annotated-with-times/5/user-5-session-36.v
@@ -265,8 +265,12 @@ clear H.
 +
 (apply evalInInts in H).
 (destruct H).
+(erewrite evalBoolConst in H0).
 (rewrite <- evalEqTrue in H0).
+admit.
++
+reflexivity.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-01 10:41:30.600000.*)
+(* Auto-generated comment: At 2019-09-01 10:42:13.020000.*)
 
