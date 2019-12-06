4604ac22df39b0fff019bd56ea96fe98f997122d
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:41 -0800

    user 7, session 174, part 58

--- diffs-annotated-with-times/7/user-7-session-174.v
+++ diffs-annotated-with-times/7/user-7-session-174.v
@@ -101,24 +101,7 @@ Function
    end.
 Proof.
 all: (try (intros; simpl; Omega.omega)).
--
-(intros).
-(simpl).
-(rewrite rename__size).
-Omega.omega.
--
-(apply (well_founded_lt_compat ty size)).
-(intros).
-tauto.
-Defined.
-Notation "'[' x ':=' s ']' t" := (subst x s t) (at level 30) : btjt_scope.
-Lemma triv : forall (X : id) (s : ty) (t1 t2 : ty), [X := s] TPair t1 t2 = TPair ([X := s] t1) ([X := s] t2).
-Proof.
-(intros X s t1 t2).
-Check subst_equation.
-(apply subst_equation).
-Qed.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-30 06:28:04.880000.*)
+(* Auto-generated comment: At 2019-08-30 06:28:05.380000.*)
 
