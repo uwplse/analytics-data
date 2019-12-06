f1f76f3ee6911fab4fd251b3b32024c9d002a586
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:34 -0800

    user 7, session 44, part 3

--- diffs-annotated-with-times/7/user-7-session-44.v
+++ diffs-annotated-with-times/7/user-7-session-44.v
@@ -204,8 +204,9 @@ Qed.
 Lemma sem_sub_k__sem_eq_k : forall (k : nat) (t1 t2 : ty), ||-[ k][t1]<= [t2] -> ||-[ k][t2]<= [t1] -> ||-[ k][t1]= [t2].
 Proof.
 (intros k t1 t2 Hsem1 Hsem2).
-auto with DBBetaJulia.
+(unfold sem_sub_k in *).
+split.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-16 06:58:26.340000.*)
+(* Auto-generated comment: At 2019-08-16 06:58:49.220000.*)
 
