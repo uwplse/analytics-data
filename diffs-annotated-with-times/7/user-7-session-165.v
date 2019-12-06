bb1a7ceb00d1b7f67d6721d1a3d692ba7319f3fc
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:34 -0800

    user 7, session 165, part 5

--- diffs-annotated-with-times/7/user-7-session-165.v
+++ diffs-annotated-with-times/7/user-7-session-165.v
@@ -75,15 +75,19 @@ Proof.
 (rewrite <- beq_id_refl).
 reflexivity.
 Qed.
-Lemma subst_fresh_in_ty : forall (X : id) (t : ty), fresh_in_ty X t -> forall s : ty, [X := s] t = t.
+Lemma subst_neq__permute :
+  forall X Y : id, X <> Y -> forall t s1 s2 : ty, fresh_in_ty X s2 -> fresh_in_ty Y s1 -> [X := s1] ([Y := s2] t) = [Y := s2] ([X := s1] t).
 Proof.
-(intros X t).
-(induction t; intros Hfresh s; try (solve [ reflexivity ]); unfold fresh_in_ty in *; simpl in Hfresh; simpl;
-  try (solve
-   [ apply fresh_union__inv in Hfresh; destruct Hfresh as [Hfresh1 Hfresh2]; rewrite IHt1; try assumption; rewrite IHt2; try assumption;
-      reflexivity ])).
+(intros X Y Hneq t).
+(induction t; intros s1 s2 HXs2 HYs1;
+  try (solve [ simpl; reflexivity | simpl; rewrite IHt1; try assumption; rewrite IHt2; try assumption; reflexivity ])).
 -
-(* Auto-generated comment: Failed. *)
+(simpl).
+(destruct (beq_idP X i)).
++
+subst.
+(destruct (beq_idP Y i); reflexivity).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-29 13:51:50.830000.*)
+(* Auto-generated comment: At 2019-08-29 13:53:50.200000.*)
 
