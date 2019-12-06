45bfff963ff74b55f11f94d7afea8c655910c845
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:49 -0800

    user 8, session 79, part 218

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -623,8 +623,8 @@ Proof.
 (rewrite kron_1_l).
 (rewrite kron_1_l).
 restore_dims.
-(rewrite kron_1_r).
+(rewrite <- kron_1_r_inv).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-14 11:52:32.060000.*)
+(* Auto-generated comment: At 2019-08-14 11:52:37.180000.*)
 
