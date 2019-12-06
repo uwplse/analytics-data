2d53ee1af668f74ffe486687a4f1045573cd7196
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:16 -0800

    user 7, session 123, part 4

--- diffs-annotated-with-times/7/user-7-session-123.v
+++ diffs-annotated-with-times/7/user-7-session-123.v
@@ -2,36 +2,6 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0280a.BaseDefs.
-Require Import BetaJulia.Sub0280a.BaseProps.
-Require Import BetaJulia.Sub0280a.BaseMatchProps.
-Require Import BetaJulia.Sub0280a.BaseSemSubProps.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Open Scope btjm.
-Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
-Proof.
-(intros t1 t2 Hsub).
-(induction Hsub).
--
-(apply sem_sub__refl).
--
-(apply sem_sub__trans with t2; assumption).
--
-(apply sem_sub_pair; assumption).
--
-(apply sem_sub_union; assumption).
--
-(apply sem_sub_union_1).
-(apply sem_sub__refl).
--
-(apply sem_sub_union_2).
-(apply sem_sub__refl).
--
 (* Auto-generated comment: Succeeded. *)
 
 (* Auto-generated comment: At 2019-08-27 07:31:16.670000.*)
