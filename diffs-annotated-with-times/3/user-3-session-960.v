a0e2c29a1448830dd719a4e0156737f779663aae
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:21:23 -0800

    user 3, session 960, part 293

--- diffs-annotated-with-times/3/user-3-session-960.v
+++ diffs-annotated-with-times/3/user-3-session-960.v
@@ -275,8 +275,8 @@ typeclasses eauto.
 (simpl).
 (unfold EqualDec).
 (intros; simpl).
-Search -"{_ : _ & _}".
-(* Auto-generated comment: Failed. *)
+Search -"{_ & _}".
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-16 07:09:30.470000.*)
+(* Auto-generated comment: At 2019-08-16 07:09:41.280000.*)
 
