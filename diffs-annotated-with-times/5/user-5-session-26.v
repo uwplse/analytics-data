92a89d64b72f00945bcca89ea8b293128bfc6ba4
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:42 -0800

    user 5, session 26, part 5

--- diffs-annotated-with-times/5/user-5-session-26.v
+++ diffs-annotated-with-times/5/user-5-session-26.v
@@ -1,9 +1,7 @@
 Goal _ ~ (forall a b, a /\ b).
 intro H.
 specialize H with False False.
-intuition.
-Qed.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-28 12:44:33.220000.*)
+(* Auto-generated comment: At 2019-08-28 12:44:43.370000.*)
 
