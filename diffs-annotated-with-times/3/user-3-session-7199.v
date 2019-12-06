8e0477c604852e9b77a0023b3750b26d8b30d683
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:17 -0800

    user 3, session 7199, part 476

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -425,10 +425,12 @@ Proof.
 (intros; lia).
 Qed.
 Hint Resolve abstr_length_sz_bound: core.
-Theorem log_contents_ok_len_change d bs a0 b :
+Theorem log_contents_ok_len_change d bs b :
   log_size_ok d bs ->
   log_contents_ok d bs -> log_contents_ok (diskUpd d len_addr b) bs.
-(* Auto-generated comment: Failed. *)
+Proof.
+(unfold log_size_ok, log_contents_ok; intros).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 12:53:46.690000.*)
+(* Auto-generated comment: At 2019-09-04 12:53:55.390000.*)
 
