ea978cba21b398c37fc70c73282a8842c48d7a9b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:30 -0800

    user 7, session 26, part 25

--- diffs-annotated-with-times/7/user-7-session-26.v
+++ diffs-annotated-with-times/7/user-7-session-26.v
@@ -192,8 +192,15 @@ Proof.
 (unfold sem_eq_k in *).
 (intros v).
 specialize (Hsem1 v).
-specialize (Hsem v).
+specialize (Hsem2 v).
+tauto.
+Qed.
+Search -and.
+Lemma sem_eq_k__sem_sub_k : forall (k : nat) (t1 t2 : ty), ||-[ k][t1]= [t2] -> ||-[ k][t1]<= [t2] /\ ||-[ k][t2]<= [t1].
+Proof.
+(intros k t1 t2 Hsem).
+(unfold sem_sub_k; intros v Hm).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-15 07:03:00.760000.*)
+(* Auto-generated comment: At 2019-08-15 07:04:19.800000.*)
 
