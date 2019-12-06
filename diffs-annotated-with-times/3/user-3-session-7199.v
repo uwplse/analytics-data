d147d60ac3a253f17034747494eca87497fe07d1
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:06 -0800

    user 3, session 7199, part 77

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -186,8 +186,8 @@ eauto using log_length_ok_nil.
 (unfold log_size_ok).
 (destruct d; simpl in *; [  | lia ]).
 (assert (diskGet nil 0 = None)).
-(apply diskGet_oob_eq).
+Check diskGet_oob_eq.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 11:05:42.470000.*)
+(* Auto-generated comment: At 2019-09-04 11:05:45.620000.*)
 
