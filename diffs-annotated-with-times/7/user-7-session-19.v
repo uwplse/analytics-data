98c39f35aaf0c8640dfd00a6116916f8d5fde557
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:19 -0800

    user 7, session 19, part 517

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -268,8 +268,10 @@ Proof.
 (pose proof (sem_sub_k_i__inv_depth_le _ _ _ Hdept H1)).
 (pose proof (sem_sub_k_i__inv_depth_le _ _ _ Hdept' H2)).
 SearchPattern (_ <= _ -> _ <= _ -> _ = _).
-(apply le_unique).
+Check le_unique.
+SearchPattern (_ <= _ -> _ = _).
+(apply Nat.le_antisymm).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 09:21:43.590000.*)
+(* Auto-generated comment: At 2019-08-13 09:28:14.100000.*)
 
