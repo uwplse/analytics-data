31f6bf36bad67bfabfdb52221d6540eb68580552
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:21 -0800

    user 7, session 137, part 1

--- diffs-annotated-with-times/7/user-7-session-137.v
+++ diffs-annotated-with-times/7/user-7-session-137.v
@@ -12,46 +12,15 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
-Open Scope btjm.
-Theorem sub_d__semantic_sound : forall t1 t2 : ty, |- t1 << t2 -> ||- [t1]<= [t2].
+Lemma sem_sub_fresh_var__sem_sub_exist :
+  forall (X : id) (t t' : ty) (X' : id), fresh_in_ty X' t' -> ||- [[X := TVar X'] t]<= [t'] -> ||- [TExist X t]<= [t'].
 Proof.
-(intros t1 t2 Hsub).
-(induction Hsub).
+(intros X t).
+(induction t).
 -
-(apply sem_sub__refl).
--
-(apply sem_sub__trans with t2; assumption).
--
-(apply sem_sub_pair; assumption).
--
-(apply sem_sub_union; assumption).
--
-(apply sem_sub_union_1).
-(apply sem_sub__refl).
--
-(apply sem_sub_union_2).
-(apply sem_sub__refl).
--
-(intros k w1).
-exists w1.
-(intros v Hm).
-(apply match_ty_pair__inv in Hm).
-(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(apply match_ty_union__inv in Hm1).
-(destruct Hm1; [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto using match_ty_pair).
--
-(intros k w1).
-exists w1.
-(intros v Hm).
-(apply match_ty_pair__inv in Hm).
-(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(apply match_ty_union__inv in Hm2).
-(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto using match_ty_pair).
--
-(apply sem_sub_ref).
-(apply sem_sub__sem_eq; assumption).
--
+(intros t' X' Hfresh Hsem).
+(simpl in *).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 08:33:06.090000.*)
+(* Auto-generated comment: At 2019-08-27 08:37:12.780000.*)
 
