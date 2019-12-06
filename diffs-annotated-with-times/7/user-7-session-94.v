50d0fa0a59cd95ee552e2faae111823bc87256ac
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:53 -0800

    user 7, session 94, part 27

--- diffs-annotated-with-times/7/user-7-session-94.v
+++ diffs-annotated-with-times/7/user-7-session-94.v
@@ -67,8 +67,8 @@ Proof.
    | destruct v; contradiction ])).
 -
 (apply match_ty_exist__inv in Hm).
-eauto.
+(apply IHk).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-19 09:08:53.230000.*)
+(* Auto-generated comment: At 2019-08-19 09:08:56.720000.*)
 
