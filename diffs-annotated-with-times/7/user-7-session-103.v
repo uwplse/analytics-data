5d94aa4868d44aedd257804bfc3cc6b0d9b8961f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:02 -0800

    user 7, session 103, part 48

--- diffs-annotated-with-times/7/user-7-session-103.v
+++ diffs-annotated-with-times/7/user-7-session-103.v
@@ -103,9 +103,12 @@ Proof.
 (apply match_ty_pair; auto).
 -
 (destruct k, w; try (solve [ simpl; tauto ])).
++
 (simpl).
 split.
+*
+(intros w1; exists w1; tauto).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 12:05:40.910000.*)
+(* Auto-generated comment: At 2019-08-20 12:05:41.590000.*)
 
