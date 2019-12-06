7f83ed9bf2781f791231940f222939db9cc5d0c9
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:24 -0800

    user 8, session 65, part 278

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -1245,5 +1245,9 @@ Timeout 1 Print denote_box.
 Lemma denote_box_compat :
   forall (safe : bool) (W1 W2 : WType) (c : Box W1 W2) \207\129 \207\129',
   \207\129 == \207\129' -> denote_box safe c \207\129 == denote_box safe c \207\129'.
+Proof.
+(intros).
+(induction c).
+(simpl).
 (* Auto-generated comment: Succeeded. *)
 
