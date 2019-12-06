2f742eb7ae5d3f974b3e10673e2947c3564d6a4d
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:56 -0800

    user 8, session 79, part 427

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -952,8 +952,8 @@ replace (length l1 + length l2) with n by omega.
 (rewrite H0, L2).
 (simpl).
 hide_dimensions.
-rewrite_inPar''.
-(* Auto-generated comment: Failed. *)
+rewrite_inPar'.
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-14 15:24:40.270000.*)
+(* Auto-generated comment: At 2019-08-14 15:24:40.500000.*)
 
