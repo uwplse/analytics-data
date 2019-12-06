7ab50bb405332c957f211effc70c8b000e34fc23
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:31 -0800

    user 1, session 30, part 0

---
+++ diffs-annotated-with-times/1/user-1-session-30.v
@@ -0,0 +1,18 @@
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Import Lists.List.
+Require Import Coq.Sorting.Permutation.
+Require Import Coq.Sorting.Sorted.
+Require Import Orders.
+Require Import FunInd.
+Require Import Recdef.
+Require Import Extraction.
+Require Import ExtrOcamlBasic.
+Require Import Coq.Program.Equality.
+Require Import Math.
+(* Auto-generated comment: Failed. *)
+
+(* Auto-generated comment: At 2019-08-26 23:25:00.320000.*)
+
