9f4d2082fe57f9d7a9a98e58c01bba5491e6b39b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:19 -0800

    user 7, session 19, part 507

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -237,7 +237,7 @@ specialize (Hsem _ Hm).
 specialize (Hsem v').
 tauto.
 Qed.
-Lemma match_ty_nf : forall (k : nat) (t : ty), ||-[ k][t]= [MkNF( t)].
+Lemma match_ty_i_nf : forall (k : nat) (t : ty), ||-[ k][t]= [MkNF( t)].
 Proof.
 (induction k; induction t; intros v; split; intros Hm; try (solve [ simpl; assumption ])).
 Admitted.
@@ -253,7 +253,8 @@ Proof.
 (apply mk_nf__in_nf).
 (rewrite inv_depth_mk_nf; assumption).
 (apply sem_sub_k__i__trans with t).
+(pose proof (match_ty_i_nf k t)).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 09:10:34.020000.*)
+(* Auto-generated comment: At 2019-08-13 09:10:56.980000.*)
 
