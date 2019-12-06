9d1dfea8e640dab86a3d60ec45ba4da278523c80
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:28:50 -0800

    user 3, session 3324, part 35

--- diffs-annotated-with-times/3/user-3-session-3324.v
+++ diffs-annotated-with-times/3/user-3-session-3324.v
@@ -145,12 +145,11 @@ exists (diskUpd (diskShrink (stateDisk state)) (stateBadBlock state) b).
 (unfold inited_any; intuition idtac).
 (constructor; intuition idtac; auto; intros; autorewrite with upd in *;
   intuition idtac).
-all: (rewrite ?diskUpd_neq by omega).
 all: (rewrite ?diskShrink_preserves; auto).
 all: (rewrite ?diskShrink_size; try omega).
 all: (rewrite ?diskUpd_eq; auto).
 all: (rewrite ?diskShrink_size; omega).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-16 10:40:35.770000.*)
+(* Auto-generated comment: At 2019-08-16 10:40:47.290000.*)
 
