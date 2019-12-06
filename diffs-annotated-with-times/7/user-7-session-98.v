c6abd13aa5b34b3409ba6f5b61295b0ee91e4cb8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:56 -0800

    user 7, session 98, part 61

--- diffs-annotated-with-times/7/user-7-session-98.v
+++ diffs-annotated-with-times/7/user-7-session-98.v
@@ -162,8 +162,11 @@ clear Heq.
 clear Heq'.
 clear Hm.
 (assert (Hm : |-[ 1] TRef tx <$ TRef tx) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Hsem' _ Hm).
+specialize (Hsem' (TRef tx)).
+(destruct Hsem' as [Hsem'1 Hsem'2]).
+specialize (Hsem'2 Hm).
+(simpl in Hsem'2).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-19 13:27:30.230000.*)
+(* Auto-generated comment: At 2019-08-19 13:28:17.100000.*)
 
