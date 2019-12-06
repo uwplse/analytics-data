8b4ec61698b865c9ab90a7416c9022dc0dacaef4
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:59 -0800

    user 7, session 208, part 5

--- diffs-annotated-with-times/7/user-7-session-208.v
+++ diffs-annotated-with-times/7/user-7-session-208.v
@@ -74,8 +74,9 @@ Proof.
 Qed.
 Lemma not_f_free_in_ty_exist__inv : forall (X Y : id) (t : ty), not_f_free_in_ty X (TExist Y t) -> not_f_free_in_ty X t.
 Proof.
+(unfold not_f_free_in_ty, not_free).
 (intros X Y t).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 09:01:28.510000.*)
+(* Auto-generated comment: At 2019-09-04 09:01:38.930000.*)
 
