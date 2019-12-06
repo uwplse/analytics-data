17bdd101c3857b80f393a1fb75167541963b0697
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:14 -0800

    user 7, session 116, part 14

--- diffs-annotated-with-times/7/user-7-session-116.v
+++ diffs-annotated-with-times/7/user-7-session-116.v
@@ -30,8 +30,8 @@ Proof.
 (simpl in Hfresh).
 (apply fresh_union__inv in Hfresh).
 (destruct Hfresh as [Hfresh1 Hfresh2]).
-reflexivity.
+ereflexivity.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 06:46:08.440000.*)
+(* Auto-generated comment: At 2019-08-27 06:46:10.180000.*)
 
