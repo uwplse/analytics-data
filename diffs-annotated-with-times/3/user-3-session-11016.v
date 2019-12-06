21e863bd5b289b6f2831c88a5b792c88b4540f4b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:54:08 -0800

    user 3, session 11016, part 10

--- diffs-annotated-with-times/3/user-3-session-11016.v
+++ diffs-annotated-with-times/3/user-3-session-11016.v
@@ -258,5 +258,5 @@ reflexivity.
 Qed.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-06 10:01:22.320000.*)
+(* Auto-generated comment: At 2019-09-06 10:01:22.520000.*)
 
