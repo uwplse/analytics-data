159a31f54c02f5863fe967b8b225e2f7242a87f6
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:04 -0800

    user 7, session 104, part 55

--- diffs-annotated-with-times/7/user-7-session-104.v
+++ diffs-annotated-with-times/7/user-7-session-104.v
@@ -62,8 +62,8 @@ where "'|' t '|'" := (inv_depth t) : btjt_scope.
 Lemma match_ty__inv_depth : forall (w k : nat) (v t : ty), | v | <= k -> |-[ k, w] v <$ t -> | v | <= | t |.
 Proof.
 (intros w k).
-(induction k).
+(induction k; induction t).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 13:07:44.280000.*)
+(* Auto-generated comment: At 2019-08-20 13:07:47.980000.*)
 
