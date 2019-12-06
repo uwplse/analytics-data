923adedd470411173a1afed2512fad20cf3946e3
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:51 -0800

    user 7, session 194, part 5

--- diffs-annotated-with-times/7/user-7-session-194.v
+++ diffs-annotated-with-times/7/user-7-session-194.v
@@ -17,28 +17,31 @@ Qed.
 Lemma f_free_in_ty__decidable : forall (X : id) (t : ty), Decidable.decidable (f_free_in_ty X t).
 Proof.
 (intros X t).
-(unfold f_free_in_ty).
 (apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
 Qed.
 Lemma b_free_in_ty__decidable : forall (X : id) (t : ty), Decidable.decidable (b_free_in_ty X t).
 Proof.
 (intros X t).
-(unfold f_free_in_ty).
 (apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
 Qed.
 Lemma f_free_in_ty__dec : forall (X : id) (t : ty), f_free_in_ty X t \/ not_f_free_in_ty X t.
 Proof.
 (intros X t).
-(unfold f_free_in_ty).
 (apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
 Qed.
 Lemma b_free_in_ty__dec : forall (X : id) (t : ty), b_free_in_ty X t \/ not_b_free_in_ty X t.
 Proof.
 (intros X t).
-(unfold b_free_in_ty).
 (apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
 Qed.
+Lemma not_free_union__inv : forall (X : id) (fvs1 fvs2 : id_set), not_free X (IdSet.union fvs1 fvs2) -> not_free X fvs1 /\ not_free X fvs2.
+Proof.
+(intros X fvs1 fvs2 H).
+(unfold not_free in *).
+(split; intros Hcontra; [ apply (IdSetFacts.union_2 fvs2) in Hcontra | apply (IdSetFacts.union_3 fvs1) in Hcontra ]; contradiction).
+Qed.
+Lemma not_free_in_ty_pair__inv : forall (X : id) (t1 t2 : ty), not_free_in_ty X (TPair t1 t2) -> not_free_in_ty X t1 /\ not_free_in_ty X t2.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-03 09:11:05.800000.*)
+(* Auto-generated comment: At 2019-09-03 09:11:11.540000.*)
 
