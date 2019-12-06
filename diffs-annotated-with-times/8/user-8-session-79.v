d48464e1b1981d7c1c272c82325f6e5bf3b9fd69
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:46 -0800

    user 8, session 79, part 116

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -593,7 +593,9 @@ Goal _ forall m n (A B : Matrix m n), A == B -> I 1 \226\138\151 A \226\138\151 
 Proof.
 (intros).
 (rewrite kron_1_l).
-(* Auto-generated comment: Succeeded. *)
+(rewrite kron_1_l).
+setoid_rewrite kron_1_r.
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 17:51:10.870000.*)
+(* Auto-generated comment: At 2019-08-13 17:55:31.810000.*)
 
