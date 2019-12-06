cfca8058c73127fe4342bc6b633032814440d569
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:11 -0800

    user 3, session 7199, part 263

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -345,8 +345,9 @@ step_proc.
 (rewrite app_length; simpl; lia).
 +
 (unfold log_size_ok in *; simpl in *).
-(rewrite app_length in *; simpl).
+(rewrite ?app_length in *; simpl).
+lia.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 12:01:21.160000.*)
+(* Auto-generated comment: At 2019-09-04 12:01:45.250000.*)
 
