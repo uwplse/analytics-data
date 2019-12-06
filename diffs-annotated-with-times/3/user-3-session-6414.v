7d9a46222c324c208746e6b4e6ed59cde0ed28fe
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:39:42 -0800

    user 3, session 6414, part 2

--- diffs-annotated-with-times/3/user-3-session-6414.v
+++ diffs-annotated-with-times/3/user-3-session-6414.v
@@ -20,17 +20,7 @@ Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 Module Log (d: OneDiskAPI)<: LogAPI.
-Definition init : proc InitResult := Ret Initialized.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq9tVkPi"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq5hDUtn"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 09:56:32.310000.*)
+(* Auto-generated comment: At 2019-09-04 09:56:50.210000.*)
 
