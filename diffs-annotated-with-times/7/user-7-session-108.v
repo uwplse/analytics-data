77c7e4ae8f8ae8a9732470109f0f5a76bdadfa93
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:07 -0800

    user 7, session 108, part 1

--- diffs-annotated-with-times/7/user-7-session-108.v
+++ diffs-annotated-with-times/7/user-7-session-108.v
@@ -189,8 +189,10 @@ subst.
 -
 (destruct k).
 +
-(apply match_ty_ref__weak_inv in Hm; subst).
+(apply match_ty_ref__weak_inv in Hm).
+(destruct Hm as [t' Heq]; subst).
+(simpl).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-21 09:26:25.150000.*)
+(* Auto-generated comment: At 2019-08-21 09:26:48.700000.*)
 
