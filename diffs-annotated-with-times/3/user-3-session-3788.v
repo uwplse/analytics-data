8bcee0ea7e5961954a9e4f31eb721920c8cede29
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:30:31 -0800

    user 3, session 3788, part 27

--- diffs-annotated-with-times/3/user-3-session-3788.v
+++ diffs-annotated-with-times/3/user-3-session-3788.v
@@ -32,9 +32,9 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoPhort"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Definition change_marker {T'} (p' : proc T') `{!ProcMarker p} :
+Definition change_marker {T'} (p' : proc T') `{ProcMarker p} :
   ProcMarker p' := AProc p'.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-29 08:55:31.950000.*)
+(* Auto-generated comment: At 2019-08-29 08:55:33.300000.*)
 
