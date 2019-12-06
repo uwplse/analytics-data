2f15191feb101df765bed4f3740454998cc5761c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:39:44 -0800

    user 3, session 6414, part 95

--- diffs-annotated-with-times/3/user-3-session-6414.v
+++ diffs-annotated-with-times/3/user-3-session-6414.v
@@ -139,8 +139,25 @@ step_proc.
 step_proc.
 intuition eauto.
 (eexists; intuition eauto).
-(destruct H0).
+Timeout 1 Show.
+Timeout 1 Show.
+Timeout 1 Show Intros.
+Timeout 1
+(repeat
+  match goal with
+  | company_coq_hyp__:_
+    |- _ =>
+        clear dependent company_coq_hyp__;
+         (let dummy := H1 in
+          let dummy := H0 in
+          idtac)
+  end;
+  repeat
+   match goal with
+   | H:_ |- _ => generalize dependent H; try (generalize dependent H; fail 1)
+   end).
+Timeout 1 Show Intros.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 10:35:23.100000.*)
+(* Auto-generated comment: At 2019-09-04 10:35:24.390000.*)
 
