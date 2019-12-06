f76aee8be6c58899990d5c045664e60db9a54c98
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:40 -0800

    user 7, session 174, part 30

--- diffs-annotated-with-times/7/user-7-session-174.v
+++ diffs-annotated-with-times/7/user-7-session-174.v
@@ -86,43 +86,8 @@ assumption.
 (destruct (beq_idP x i); reflexivity).
 Qed.
 Definition lt_size (t1 t2 : ty) := lt (size t1) (size t2).
-Definition foo (t : ty) := TExist vX t.
-Function
- subst (x : id) (s t : ty) {wf fun t1 t2 : ty => size t1 < size t2 t} : ty :=
-   match t with
-   | TCName _ => t
-   | TPair t1 t2 => TPair (subst x s t1) (subst x s t2)
-   | TUnion t1 t2 => TUnion (subst x s t1) (subst x s t2)
-   | TExist y t' => foo (subst x s t')
-   | TVar y => if beq_id x y then s else t
-   | TEV y => t
-   end.
--
-(intros).
-(simpl).
-Omega.omega.
--
-(intros).
-(simpl).
-Omega.omega.
--
-(intros).
-(simpl).
-Omega.omega.
--
-(intros).
-(simpl).
-Omega.omega.
--
-(intros).
-(simpl).
-Omega.omega.
--
-(apply (well_founded_lt_compat ty size)).
-(intros).
-tauto.
-Defined.
+Definition mk_subst_exist (x : id) (y : id) (t ts : ty) := TExist y (if beq_id x y then t else ts).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-30 06:11:29.630000.*)
+(* Auto-generated comment: At 2019-08-30 06:13:07.370000.*)
 
