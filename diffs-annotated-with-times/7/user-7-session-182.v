8ac661fc2767e862277b3155ac5e35b3223eb69a
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:44 -0800

    user 7, session 182, part 6

--- diffs-annotated-with-times/7/user-7-session-182.v
+++ diffs-annotated-with-times/7/user-7-session-182.v
@@ -35,8 +35,16 @@ subst.
 contradiction.
 *
 (rewrite subst_exist_eq).
-(rewrite subst_exist_neq).
+(rewrite subst_exist_neq; try assumption).
+(rewrite subst_exist_eq).
+reflexivity.
++
+(destruct (beq_idP X i) as [HX| HX]).
+*
+subst.
+(repeat rewrite subst_exist_eq).
+Search -idSet.mem.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-02 06:48:10.660000.*)
+(* Auto-generated comment: At 2019-09-02 06:51:50.500000.*)
 
