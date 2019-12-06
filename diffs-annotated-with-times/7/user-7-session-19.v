b86f6543f435c42359f71d9060ae6b9301bb091e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:13 -0800

    user 7, session 19, part 282

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -156,8 +156,9 @@ idtac.
 (destruct Hmu as [Hmu1| Hmu2]; [ left | right ]; intros v Hv Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst).
 (assert (Hmt't : |-[ S k] TRef t' <$ TRef t)).
 (simpl).
-(intros v'; auto).
+(intros v').
+split.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 06:39:22.950000.*)
+(* Auto-generated comment: At 2019-08-13 06:40:17.330000.*)
 
