6f232e60179220674d0d6ca8cbfd06812f1c53a9
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:06 -0800

    user 3, session 7199, part 75

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -185,8 +185,9 @@ eauto using log_length_ok_nil.
 -
 (unfold log_size_ok).
 (destruct d; simpl in *; [  | lia ]).
-Search -diskGet -"inbounds".
+Search -diskGet -diskSize.
+(assert (diskGet nil 0 = None)).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 11:03:39.670000.*)
+(* Auto-generated comment: At 2019-09-04 11:05:41.260000.*)
 
