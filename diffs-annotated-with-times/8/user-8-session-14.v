c805e406a7ae05664d2d8df970bc907b7fb87622
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:23 -0800

    user 8, session 14, part 25

--- diffs-annotated-with-times/8/user-8-session-14.v
+++ diffs-annotated-with-times/8/user-8-session-14.v
@@ -1217,8 +1217,5 @@ Proof.
 Set Printing All.
 gen \207\129.
 subst.
-(repeat rewrite Nat.pow_add_r).
 (* Auto-generated comment: Succeeded. *)
-
-(* Auto-generated comment: At 2019-08-09 13:53:42.220000.*)
 
