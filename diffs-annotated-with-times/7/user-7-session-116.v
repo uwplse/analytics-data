81153215d532a8c02ad398e9c38a7df3ec0a15f1
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:13 -0800

    user 7, session 116, part 5

--- diffs-annotated-with-times/7/user-7-session-116.v
+++ diffs-annotated-with-times/7/user-7-session-116.v
@@ -22,8 +22,9 @@ Proof.
 SearchPattern (IdSet.In (IdSet.union _ _)).
 Search -IdSet.union.
 Check IdSetFacts.union_2.
-(apply (IdSetFacts.union_2 fvs1 fvs2 X) in Hcontra).
+Check IdSetFacts.union_2.
+(apply (IdSetFacts.union_2 fvs1 fvs2) in Hcontra).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 06:42:53.320000.*)
+(* Auto-generated comment: At 2019-08-27 06:43:09.600000.*)
 
