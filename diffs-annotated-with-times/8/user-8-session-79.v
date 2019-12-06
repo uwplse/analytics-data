9ad9007ec5411d9d8f257353899235a24b408040
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:46 -0800

    user 8, session 79, part 130

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -603,11 +603,11 @@ Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Lemma new_morphism :
   forall {m} {n} (A : Matrix m n),
-  Morphisms.Proper (Morphisms.respectful mat_equiv (flip impl)) 
-    (mat_equiv A) Lemma big_kron_append
-  :
-  forall m n (l1 l2 : list (Matrix m n)), \226\168\130 (l1 ++ l2) == (\226\168\130 l1) \226\138\151 (\226\168\130 l2).
-(* Auto-generated comment: Failed. *)
+  Morphisms.Proper (Morphisms.respectful mat_equiv (flip impl)) (mat_equiv A).
+Proof.
+(intros).
+(simpl).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-13 18:13:07.250000.*)
+(* Auto-generated comment: At 2019-08-13 18:13:22.770000.*)
 
