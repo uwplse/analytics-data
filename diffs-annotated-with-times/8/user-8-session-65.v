dd6a78d3e0d41a53d3dc12af5130271ca7e4ee7e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:09 -0800

    user 8, session 65, part 1

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -954,8 +954,7 @@ Proof.
 (simpl_rewrite id_circ_spec).
 (simpl_rewrite inSeq_correct; [  | apply assert_at_WT | apply init_at_WT ]).
 (unfold compose_super).
-(rewrite size_ntensor, Nat.mul_1_r in M).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-13 10:31:00.740000.*)
+(* Auto-generated comment: At 2019-08-13 10:31:51.610000.*)
 
