7cdeadde342b994ce1406f15297e5e31281874bb
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:13 -0800

    user 7, session 113, part 35

--- diffs-annotated-with-times/7/user-7-session-113.v
+++ diffs-annotated-with-times/7/user-7-session-113.v
@@ -2,26 +2,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Add LoadPath "../..".
-Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0280a.BaseDefs.
-Require Import Coq.Lists.List.
-Import ListNotations.
-Require Import Coq.Arith.Arith.
-Require Import Coq.Bool.Bool.
-Open Scope btjt.
-Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
-Proof.
-(intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
-Qed.
-Lemma subs_not_in_FV : forall (X : id) (t : ty), not_fresh_in_ty X t -> forall s : ty, [X := s] t = t.
-Proof.
-(intros X t).
-(induction t; intros Hnfresh s; try (solve [ reflexivity ])).
--
-(simpl).
-(unfold not_fresh_in_ty in *).
 (* Auto-generated comment: Succeeded. *)
-
-(* Auto-generated comment: At 2019-08-27 06:29:06.680000.*)
 
