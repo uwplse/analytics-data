b55049ebf367298a77c98b73e827c4ddd0b004f8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:34 -0800

    user 3, session 11481, part 0

---
+++ diffs-annotated-with-times/3/user-3-session-11481.v
@@ -0,0 +1,27 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqHmbRVM"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Import POCS.
+Require Import ThreeVariablesAPI.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqzFGxqr"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect
+"/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqhJIZ3A"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+Module Exercises (vars: VarsAPI).
+Check vars.read.
+(* Auto-generated comment: Succeeded. *)
+
+(* Auto-generated comment: At 2019-09-09 13:39:47.310000.*)
+
