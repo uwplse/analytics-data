6864e8785216d8321c87780c231ea3257d04b3b3
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:02 -0800

    user 7, session 216, part 6

--- diffs-annotated-with-times/7/user-7-session-216.v
+++ diffs-annotated-with-times/7/user-7-session-216.v
@@ -359,8 +359,8 @@ Lemma union_empty__inv : forall s1 s2, IdSet.Empty (IdSet.union s1 s2) -> IdSet.
 Proof.
 (intros s1 s2 H).
 Check IdSetProps.empty_union_1.
-(pose proof (IdSetProps.empty_union_1 H) as H1).
+(pose proof (IdSetProps.empty_union_1 _ H) as H1).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 09:49:02.710000.*)
+(* Auto-generated comment: At 2019-09-04 09:49:13.030000.*)
 
