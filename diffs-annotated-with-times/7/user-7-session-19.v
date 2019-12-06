f43eb1e559c55956db19217502b2fd59d7c6030d
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:13 -0800

    user 7, session 19, part 264

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -135,8 +135,9 @@ auto using match_ty_i_pair.
 clear IHt3.
 (destruct k).
 (destruct v1; contradiction || constructor).
-(apply match_ty_ref__inv in Hm2).
+(apply match_ty_i_ref__inv in Hm2).
+(destruct Hm2 as [t'' [Heq' Href']]).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 06:31:38.870000.*)
+(* Auto-generated comment: At 2019-08-13 06:32:13.180000.*)
 
