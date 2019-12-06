9c9e9ec10aadee901b1a9e2712842f50ec03e877
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:12 -0800

    user 7, session 112, part 7

--- diffs-annotated-with-times/7/user-7-session-112.v
+++ diffs-annotated-with-times/7/user-7-session-112.v
@@ -176,8 +176,23 @@ Proof.
 -
 (intros v1 t3 k w1 w2 Hm1 Hm2).
 (apply match_ty_cname__inv in Hm1; subst).
-auto.
+admit.
+-
+(intros v0 t3 k w1 w2 Hm1 Hm2).
+(apply match_ty_pair__inv in Hm1).
+(destruct Hm1 as [pv11 [pv12 [Heq [Hmpv11 Hmpv12]]]]; subst).
+(induction t3; try (solve [ destruct k, w2; contradiction ])).
++
+(apply match_ty_pair__inv in Hm2).
+(destruct Hm2 as [pv21 [pv22 [Heq [Hmpv21 Hm22]]]]).
+(inversion Heq; subst).
+auto using match_ty_pair.
++
+(apply match_ty_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
++
+(apply match_ty_exist__inv in Hm2).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-26 08:28:57.340000.*)
+(* Auto-generated comment: At 2019-08-26 08:29:31.740000.*)
 
