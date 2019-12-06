4283ce6222996c65583facbc62c2d2fa5098f8eb
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:57 -0800

    user 8, session 79, part 437

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -961,9 +961,8 @@ Timeout 1 Print Ltac restore_dims.
 (apply f_equal_gen; trivial).
 (apply f_equal_gen; trivial).
 (apply f_equal_gen; trivial).
-(simpl; try rewrite size_ntensor; try rewrite app_length; try rewrite L2; simpl;
-  unify_pows_two).
+(simpl; try rewrite size_ntensor; try rewrite app_length; simpl; unify_pows_two).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-14 15:26:48.120000.*)
+(* Auto-generated comment: At 2019-08-14 15:26:51.360000.*)
 
