a4a06dc8b985c8a0910e86641246ad7c5dd72d25
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:18:19 -0800

    user 3, session 197, part 8

--- diffs-annotated-with-times/3/user-3-session-197.v
+++ diffs-annotated-with-times/3/user-3-session-197.v
@@ -8,5 +8,5 @@ Add Search Blacklist "Private_" "_subproof".
 From Coq Require Import List.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-07 14:02:47.910000.*)
+(* Auto-generated comment: At 2019-08-07 14:02:49.880000.*)
 
