7dc4e94a7586b10a32e1fffa97205828d5a031e4
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:13 -0800

    user 7, session 19, part 249

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -111,9 +111,8 @@ idtac.
 (assert (Hvref : value_type (TRef t)) by constructor).
 (assert (Hmref : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; assumption)).
 (pose proof (Hsem _ Hvref Hmref) as Hm).
-(apply match_ty_i_union__inv in Hm).
-(destruct Hmu as [Hmu1| Hmu2]; [ left | right ]; intros v Hv Hm).
+(apply match_ty_i_union__inv in Hmu).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 13:55:13.570000.*)
+(* Auto-generated comment: At 2019-08-12 13:55:34.650000.*)
 
