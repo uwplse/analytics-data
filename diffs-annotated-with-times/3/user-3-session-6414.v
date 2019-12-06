f1980660e9114d7839e77612981e0084ed284142
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:39:43 -0800

    user 3, session 6414, part 57

--- diffs-annotated-with-times/3/user-3-session-6414.v
+++ diffs-annotated-with-times/3/user-3-session-6414.v
@@ -134,14 +134,14 @@ Theorem log_abstraction_nil d b :
 Proof.
 (unfold log_abstraction; intros).
 (intuition; eauto using log_length_ok_nil).
-(simpl in *; lia).
+(exfalso; simpl in *; lia).
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqK5CR8r"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqZ7TKRM"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 10:26:45.530000.*)
+(* Auto-generated comment: At 2019-09-04 10:26:57.400000.*)
 
