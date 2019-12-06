167464d8c7148e1da4ad3ee5721464ef7e9b0d01
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:54 -0800

    user 7, session 9, part 6

--- diffs-annotated-with-times/7/user-7-session-9.v
+++ diffs-annotated-with-times/7/user-7-session-9.v
@@ -51,8 +51,8 @@ Proof.
 *
 (intros Hnf2'; intros Hsub1 Hsub2).
 (rewrite unite_pairs_atom_union; try assumption).
-(apply sub_r_union_l__inv in Hsub2; try assumption).
+(apply sub_r_union_l__inv in Hsub2; assumption).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-08 13:03:10.610000.*)
+(* Auto-generated comment: At 2019-08-08 13:04:37.770000.*)
 
