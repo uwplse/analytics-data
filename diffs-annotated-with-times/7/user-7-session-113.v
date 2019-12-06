15d93c81ffaa452f0675bc89eac17e989f2d585e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:12 -0800

    user 7, session 113, part 15

--- diffs-annotated-with-times/7/user-7-session-113.v
+++ diffs-annotated-with-times/7/user-7-session-113.v
@@ -17,19 +17,9 @@ Qed.
 Lemma subs_neq__permute : forall X Y : id, X <> Y -> forall t s1 s2 : ty, [X := s1] ([Y := s2] t) = [Y := s2] ([X := s1] t).
 Proof.
 (intros X Y Hneq t).
-(induction t; intros s1 s2; try (solve [ simpl; reflexivity ]); try (specialize (IHt1 s1 s2); specialize (IHt2 s1 s2)); try specialize (IHt s1 s2)).
--
-(simpl).
-(rewrite IHt1).
-(rewrite IHt2).
-reflexivity.
--
-(simpl).
-(rewrite IHt1).
-(rewrite IHt2).
-reflexivity.
--
+(induction t; intros s1 s2;
+  try (solve [ simpl; reflexivity | specialize (IHt1 s1 s2); specialize (IHt2 s1 s2); simpl; rewrite IHt1; rewrite IHt2; reflexivity ])).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 06:10:46.040000.*)
+(* Auto-generated comment: At 2019-08-27 06:11:31.550000.*)
 
