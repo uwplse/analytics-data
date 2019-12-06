54d2398ac8d270e861183e94cc37d76f0f557293
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:43 -0800

    user 8, session 79, part 30

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -607,5 +607,5 @@ Timeout 1 Print Ltac Matrix.Msimpl_light.
 Msimpl.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-13 16:02:50.970000.*)
+(* Auto-generated comment: At 2019-08-13 16:06:22.890000.*)
 
