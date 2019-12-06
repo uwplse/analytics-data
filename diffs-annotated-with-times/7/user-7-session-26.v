c373d64e2e66c27a3c4dec150a3e8a154811f075
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:29 -0800

    user 7, session 26, part 14

--- diffs-annotated-with-times/7/user-7-session-26.v
+++ diffs-annotated-with-times/7/user-7-session-26.v
@@ -182,8 +182,7 @@ Proof.
 auto with DBBetaJulia.
 (intros k t1 t2 Hsem).
 (unfold sem_eq_k in *).
-tauto.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-15 07:01:47.390000.*)
+(* Auto-generated comment: At 2019-08-15 07:01:47.850000.*)
 
