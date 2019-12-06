22cd6bd685b859026ad2ee808d48f3d14e886115
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:09 -0800

    user 7, session 108, part 90

--- diffs-annotated-with-times/7/user-7-session-108.v
+++ diffs-annotated-with-times/7/user-7-session-108.v
@@ -402,25 +402,8 @@ eauto.
 -
 (left; exists (TEV i); apply match_ty_ev).
 Admitted.
-Lemma not_sem_eq__reft_t : forall (t : ty) (k : nat), ty_not_empty_k t k -> ~ ||-[ S k][t]<= [TRef t].
-Proof.
-(induction t; intros k Ht Hcontra).
--
-specialize (Hcontra 0).
-(destruct Hcontra as [w Hcontra]).
-(assert (Hm : |-[ S k, 0] TCName c <$ TCName c) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Hcontra _ Hm).
-clear Hm.
-(apply match_ty_ref__inv in Hcontra).
-(destruct Hcontra as [t' [Hcontra _]]).
-(inversion Hcontra).
--
-(destruct Ht as [w1 [v Hm]]).
-specialize (Hcontra w1).
-(destruct Hcontra as [w2 Hcontra]).
-Check Hcontra.
-specialize (Hcontra v Hm).
+Lemma not_sem_eq__reft_t : forall (t : ty) (k : nat), ty_not_empty_k t (S k) -> ~ ||-[ S k][t]<= [TRef t].
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-21 12:28:52.410000.*)
+(* Auto-generated comment: At 2019-08-21 12:29:45.680000.*)
 
