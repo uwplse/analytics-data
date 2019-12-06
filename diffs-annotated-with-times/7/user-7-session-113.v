94de2d90e05d78bf12e3093cc4be4ab9a21f3475
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:12 -0800

    user 7, session 113, part 25

--- diffs-annotated-with-times/7/user-7-session-113.v
+++ diffs-annotated-with-times/7/user-7-session-113.v
@@ -14,39 +14,11 @@ Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
 Proof.
 (intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
 Qed.
-Lemma subs_neq__permute : forall X Y : id, X <> Y -> forall t s1 s2 : ty, [X := s1] ([Y := s2] t) = [Y := s2] ([X := s1] t).
+Lemma subs_not_in_FV : forall (X : id) (t : ty), ~ fresh_in_ty X t -> forall s : ty, [X := s] t = t.
 Proof.
-(intros X Y Hneq t).
-(induction t; intros s1 s2; try (solve [ simpl; reflexivity | simpl; rewrite IHt1; rewrite IHt2; reflexivity ])).
--
-(simpl).
-(rewrite IHt).
-reflexivity.
--
-(simpl).
-(destruct (beq_idP X i)).
-+
-subst.
-(destruct (beq_idP Y i); reflexivity).
-+
-(destruct (beq_idP Y i)).
-*
-subst.
-reflexivity.
-*
-(rewrite IHt).
-reflexivity.
--
-(simpl).
-(destruct (beq_idP X i); destruct (beq_idP Y i); subst).
-+
-contradiction.
-+
-(simpl).
-Search -beq_id.
-(rewrite <- beq_id_refl).
-reflexivity.
+(intros X t).
+(induction t; intros Hnfresh s).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 06:16:53.460000.*)
+(* Auto-generated comment: At 2019-08-27 06:17:21.130000.*)
 
