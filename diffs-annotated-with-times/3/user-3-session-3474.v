5a5e19998c325ca634f01561563618be30b1a786
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:29:17 -0800

    user 3, session 3474, part 2

--- diffs-annotated-with-times/3/user-3-session-3474.v
+++ diffs-annotated-with-times/3/user-3-session-3474.v
@@ -18,7 +18,16 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
+Ltac
+ monad_simpl :=
+  repeat
+   match goal with
+   | |- proc_spec _ (Bind (Ret _) _) _ _ =>
+         eapply spec_exec_equiv; [ apply monad_left_id |  ]
+   | |- proc_spec _ (Bind (Bind _ _) _) _ _ =>
+         eapply spec_exec_equiv; [ apply monad_assoc |  ]
+   end.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-29 07:13:27.250000.*)
+(* Auto-generated comment: At 2019-08-29 07:13:29.130000.*)
 
