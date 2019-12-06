d0ee43c5c8250f73c73b9fbca8cf4ad76561d669
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:15 -0800

    user 7, session 19, part 357

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -174,8 +174,8 @@ Proof.
 (unfold sem_sub_k_i in Hsem).
 (split; intros v Hm; [ destruct (match_ty_i_exists t2 k) as [v' Hm'] | destruct (match_ty_i_exists t1 k) as [v' Hm'] ];
   [ assert (Hmp : |-[ k] TPair v v' <$ TPair t1 t2) by (apply match_ty_i_pair; assumption)
-  | assert (Hmp : |-[ k] TPair v' v <$ TPair t1 t2) by (apply match_ty_i_pair; assumption) ]).
+  | assert (Hmp : |-[ k] TPair v' v <$ TPair t1 t2) by (apply match_ty_i_pair; assumption) ]; specialize (Hsem _ Hmp)).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 07:13:12.240000.*)
+(* Auto-generated comment: At 2019-08-13 07:13:24.400000.*)
 
