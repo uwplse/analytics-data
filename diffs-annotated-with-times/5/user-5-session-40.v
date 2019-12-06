e82f887f917b37332da14f2f9211dc234ad77613
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:47 -0800

    user 5, session 40, part 0

---
+++ diffs-annotated-with-times/5/user-5-session-40.v
@@ -0,0 +1,8 @@
+Require Import String.
+Require Import ZArith.
+Require Import List.
+Import ListNotations.
+(* Auto-generated comment: Succeeded. *)
+
+(* Auto-generated comment: At 2019-09-01 11:44:42.750000.*)
+
