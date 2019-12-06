82fbe2253f74667271c935ee5f058d8d2afcffc8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:37:34 -0800

    user 3, session 5741, part 37

--- diffs-annotated-with-times/3/user-3-session-5741.v
+++ diffs-annotated-with-times/3/user-3-session-5741.v
@@ -628,14 +628,9 @@ reflexivity.
 Qed.
 Theorem andb_eq_orb : forall b c : bool, andb b c = orb b c -> b = c.
 Proof.
-(intros [] []; simpl; congruence).
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqWA3MG2"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
+(intros [] c).
+(simpl).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 06:55:10.240000.*)
+(* Auto-generated comment: At 2019-09-04 06:55:23.480000.*)
 
