a56ab8a36eea14f4424b775840a5cf12cea94e9f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:17 -0800

    user 7, session 124, part 4

--- diffs-annotated-with-times/7/user-7-session-124.v
+++ diffs-annotated-with-times/7/user-7-session-124.v
@@ -85,9 +85,9 @@ Lemma sem_sub_k_pair__inv :
   forall (k : nat) (t1 t2 t1' t2' : ty), ||-[ k][TPair t1 t2]<= [TPair t1' t2'] -> ||-[ k][t1]<= [t1'] /\ ||-[ k][t2]<= [t2'].
 Proof.
 (intros k t1 t2 t1' t2' Hsem).
-(split; intros w1; specialize (Hsem w1); destruct Hsem as [w2 Hsem]).
+(split; intros w1; specialize (Hsem w1); destruct Hsem as [w2 Hsem]; exists w2; intros v Hm).
 -
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 07:32:51.420000.*)
+(* Auto-generated comment: At 2019-08-27 07:41:24.180000.*)
 
