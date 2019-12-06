76d3e5b0309eb30ecfa69d31456235365f18338e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:08 -0800

    user 7, session 108, part 43

--- diffs-annotated-with-times/7/user-7-session-108.v
+++ diffs-annotated-with-times/7/user-7-session-108.v
@@ -254,8 +254,10 @@ Proof.
 (apply match_ty_pair__inv in Hcontra).
 (destruct Hcontra as [v1 [v2 [Heq [Hm1 Hm2]]]]).
 subst.
-(apply IHt1).
+(assert (Hcontra : ~ (exists v : ty, |-[ k, w] v <$ t1))).
+{
+(intros Hcontra).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-21 10:05:00.380000.*)
+(* Auto-generated comment: At 2019-08-21 10:08:04.400000.*)
 
