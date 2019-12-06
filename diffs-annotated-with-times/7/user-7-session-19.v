2fe4b28d6b890ef565d456c4da1dc6144fb53157
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:25 -0800

    user 7, session 19, part 722

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -655,9 +655,9 @@ Proof.
 (apply sem_sub__trans with t1).
 Search -mk_nf.
 (pose proof (SemSubProps.mk_nf__sem_eq t1)).
-(intros k v).
-specialize (H k).
+(intros k v Hv).
+specialize (H k v).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-14 06:41:39.030000.*)
+(* Auto-generated comment: At 2019-08-14 06:41:53.440000.*)
 
