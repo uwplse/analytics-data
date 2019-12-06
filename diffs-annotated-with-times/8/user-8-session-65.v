cdc86e082e6d3c185e18102f0e91995926a3c507
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:10 -0800

    user 8, session 65, part 25

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -962,25 +962,8 @@ Proof.
 (destruct safe).
 -
 (rewrite (assert_at_spec_safe b m i); [  | omega ]).
-gen \207\129.
-(rewrite size_ntensor).
-(simpl).
-(rewrite Nat.mul_1_r).
-(intros \207\129).
-Timeout 1 About restore_dims.
-Timeout 1 Print restore_dims.
-Timeout 1 Print Ltac restore_dims.
-(match goal with
- | |- ?A => let A' := restore_dims_rec tac A in
-            replace
-            A
-            with
-            A'
- end).
-2: {
-(do 4 (apply f_equal_gen; trivial)).
-(rewrite size_ntensor).
-(* Auto-generated comment: Succeeded. *)
+restore_dims.
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 11:05:09.270000.*)
+(* Auto-generated comment: At 2019-08-13 11:08:15.370000.*)
 
