1b186e2a72f5cf70c3d6f6af8a5fa4e03fb139e8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:51:56 -0800

    user 3, session 10411, part 7

--- diffs-annotated-with-times/3/user-3-session-10411.v
+++ diffs-annotated-with-times/3/user-3-session-10411.v
@@ -111,8 +111,8 @@ inv_exec.
  | Hexec:exec (rx _) _ _
    |- _ => eapply RExec in Hexec; eapply H4 in Hexec; eauto
  end).
-(simpl).
+(simpl; eauto).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-06 06:41:21.620000.*)
+(* Auto-generated comment: At 2019-09-06 06:41:24.550000.*)
 
