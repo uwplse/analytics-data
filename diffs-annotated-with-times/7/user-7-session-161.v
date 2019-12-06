c76450007e1b90cbed113e9e6bad35ae9b2739c4
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:32 -0800

    user 7, session 161, part 38

--- diffs-annotated-with-times/7/user-7-session-161.v
+++ diffs-annotated-with-times/7/user-7-session-161.v
@@ -77,9 +77,10 @@ exists ti.
 assumption.
 assumption.
 Abort.
-Lemma subst_nested : forall (X Y : id) (tX tY : ty), X <> Y -> forall t : ty, [X := tX] ([Y := tY] t) = [Y := [X := tX] tY] ([X := tX] t).
+Lemma subst_nested :
+  forall (X Y : id) (tX tY : ty), X <> Y -> fresh_in_ty Y tX -> forall t : ty, [X := tX] ([Y := tY] t) = [Y := [X := tX] tY] ([X := tX] t).
 Proof.
-(intros X Y tX tY Hneq t).
+(intros X Y tX tY Hneq HY t).
 (induction t; try reflexivity).
 -
 (repeat rewrite subst_pair).
@@ -91,25 +92,7 @@ reflexivity.
 reflexivity.
 -
 (destruct (beq_idP Y i) as [HY| HY]).
-+
-subst.
-(destruct (beq_idP X i) as [HX| HX]).
-*
-subst.
-contradiction.
-*
-(rewrite subst_exist_eq).
-(rewrite (subst_exist_neq _ _ _ _ HX)).
-(rewrite subst_exist_eq).
-reflexivity.
-+
-(destruct (beq_idP X i) as [HX| HX]).
-*
-subst.
-(rewrite (subst_exist_neq _ _ _ _ HY)).
-(repeat rewrite subst_exist_eq).
-(rewrite (subst_exist_neq _ _ _ _ HY)).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-29 12:16:36.720000.*)
+(* Auto-generated comment: At 2019-08-29 12:19:24.340000.*)
 
