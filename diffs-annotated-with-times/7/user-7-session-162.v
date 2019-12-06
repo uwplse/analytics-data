e3d9d505d3daea4b2346b084f80684ebc314e328
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:33 -0800

    user 7, session 162, part 13

--- diffs-annotated-with-times/7/user-7-session-162.v
+++ diffs-annotated-with-times/7/user-7-session-162.v
@@ -53,8 +53,8 @@ Reserved Notation "'[' x '@' y ']' t" (at level 30).
 Fixpoint rename (x y : id) (t : ty) :=
   match t with
   | TCName _ => t
-  | TPair t1 t2 => TPair ([x @ s] t1) ([x @ s] t2)
-  | TUnion t1 t2 => TUnion ([x @ s] t1) ([x @ s] t2)
+  | TPair t1 t2 => TPair ([x @ y] t1) ([x @ y] t2)
+  | TUnion t1 t2 => TUnion ([x @ y] t1) ([x @ y] t2)
   | TExist z t' => TExist z (if beq_id x z then t' else [x @ y] t')
   | TVar z => if beq_id x z then s else t
   | TEV z => t
@@ -62,5 +62,5 @@ Fixpoint rename (x y : id) (t : ty) :=
 where "'[' x '@' y ']' t" := (rename x y t) : btjt_scope.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-29 13:35:17.940000.*)
+(* Auto-generated comment: At 2019-08-29 13:35:19.850000.*)
 
