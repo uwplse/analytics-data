bfd1ccff075064daf7a7e8814e7a4aae04d45365
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:08 -0800

    user 7, session 108, part 70

--- diffs-annotated-with-times/7/user-7-session-108.v
+++ diffs-annotated-with-times/7/user-7-session-108.v
@@ -293,8 +293,10 @@ admit.
 -
 admit.
 -
-(assert (Hnotm' : ~ (exists v, tx, |-[ k, w] v <$ [i := tx] t))).
+(assert (Hnotm' : ~ (exists v tx, |-[ k, w] v <$ [i := tx] t))).
+{
+(intros Hcontra).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-21 10:43:06.270000.*)
+(* Auto-generated comment: At 2019-08-21 10:44:16.480000.*)
 
