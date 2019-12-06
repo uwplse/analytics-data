90f3abfbe804e40aea3ca58b205e98bb08444e32
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:50:05 -0800

    user 3, session 9823, part 11

--- diffs-annotated-with-times/3/user-3-session-9823.v
+++ diffs-annotated-with-times/3/user-3-session-9823.v
@@ -170,8 +170,12 @@ Proof.
 (intros).
 (apply spec_abstraction_compose; simpl).
 (step_proc; intros).
-(destruct a'; simpl in *; intuition).
+(destruct a'; simpl in *; intuition eauto).
+(destruct (a == r)).
+-
+invert_abstraction.
+(step_proc; intuition).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-06 05:22:03.240000.*)
+(* Auto-generated comment: At 2019-09-06 05:22:09.180000.*)
 
