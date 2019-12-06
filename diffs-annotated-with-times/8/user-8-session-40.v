35989e186c6dd4fa17da4505e2d9d2929579452b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:52 -0800

    user 8, session 40, part 3

--- diffs-annotated-with-times/8/user-8-session-40.v
+++ diffs-annotated-with-times/8/user-8-session-40.v
@@ -26,8 +26,8 @@ autorewrite with proof_db.
 (unfold pad).
 (simpl).
 (rewrite Nat.sub_diag).
-(rewrite kron_1_r).
-(* Auto-generated comment: Failed. *)
+(rewrite kron_1_r').
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-12 14:24:16.980000.*)
+(* Auto-generated comment: At 2019-08-12 14:24:23.930000.*)
 
