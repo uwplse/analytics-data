0d736f0a79700f01382a38052c248761869999eb
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:55 -0800

    user 7, session 98, part 11

--- diffs-annotated-with-times/7/user-7-session-98.v
+++ diffs-annotated-with-times/7/user-7-session-98.v
@@ -130,10 +130,12 @@ Proof.
 (apply match_ty_exist__0_inv in Hm).
 (destruct Hm as [Hv [tx Heqx]]; subst).
 (simpl in *).
-split.
-assumption.
-(exists tx; reflexivity).
+(split; eauto).
+-
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hmx]).
+(simpl).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-19 09:55:36.950000.*)
+(* Auto-generated comment: At 2019-08-19 09:55:54.430000.*)
 
