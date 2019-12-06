941d146163356000813777e9b0165cbb71c6e9c1
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:22 -0800

    user 7, session 139, part 4

--- diffs-annotated-with-times/7/user-7-session-139.v
+++ diffs-annotated-with-times/7/user-7-session-139.v
@@ -14,47 +14,17 @@ Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Lemma sem_sub_k_exist_fresh_l : forall (k : nat) (X : id) (t : ty), fresh_in_ty X t -> ||-[ k][TExist X t]<= [t].
 Proof.
-(intros k X t).
-(induction t; intros Hfresh; intros w1; exists w1; intros v Hm; destruct w1; try (solve [ apply match_ty_exist__0_inv in Hm; contradiction ])).
--
+(intros k X t Hfresh).
+(intros w1).
+exists w1.
+(intros v Hm).
+(destruct w1).
+(apply match_ty_exist__0_inv in Hm; contradiction).
 (apply match_ty_exist__inv in Hm).
 (destruct Hm as [tx Hm]).
 (simpl in Hm).
-(eapply match_ty__ge_w).
-eassumption.
-(repeat constructor).
--
-(apply match_ty_exist__inv in Hm).
-(destruct Hm as [tx Hm]).
-(simpl in Hm).
-(destruct (fresh_in_ty_pair__inv X t1 t2 Hfresh) as [Hfresh1 Hfresh2]).
-Check subs_fresh_in_ty.
-(rewrite (subs_fresh_in_ty _ _ Hfresh1) in Hm).
-(rewrite (subs_fresh_in_ty _ _ Hfresh2) in Hm).
-(eapply match_ty__ge_w).
-eassumption.
-(repeat constructor).
--
-(apply match_ty_exist__inv in Hm).
-(destruct Hm as [tx Hm]).
-(simpl in Hm).
-(destruct (fresh_in_ty_pair__inv X t1 t2 Hfresh) as [Hfresh1 Hfresh2]).
-(rewrite (subs_fresh_in_ty _ _ Hfresh1) in Hm).
-(rewrite (subs_fresh_in_ty _ _ Hfresh2) in Hm).
-(eapply match_ty__ge_w).
-eassumption.
-(repeat constructor).
--
-(apply match_ty_exist__inv in Hm).
-(destruct Hm as [tx Hm]).
-(simpl in Hm).
-(apply fresh_in_ty_ref__inv in Hfresh).
-(rewrite (subs_fresh_in_ty _ _ Hfresh) in Hm).
-(eapply match_ty__ge_w).
-eassumption.
-(repeat constructor).
--
+(rewrite subs_fresh_in_ty in Hm).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 08:54:50.900000.*)
+(* Auto-generated comment: At 2019-08-27 08:54:53.830000.*)
 
