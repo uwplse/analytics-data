28a39451e32f978149b9c20e35b601734a04b46a
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:26:29 -0800

    user 3, session 2698, part 9

--- diffs-annotated-with-times/3/user-3-session-2698.v
+++ diffs-annotated-with-times/3/user-3-session-2698.v
@@ -147,6 +147,7 @@ econstructor.
 (rewrite Ascii.nat_ascii_embedding; auto).
 -
 (simpl; constructor).
++
 (unfold nat64_to_le; intros).
 (match goal with
  | H:context [ nat_le_dec ?n ?m ]
@@ -155,7 +156,9 @@ econstructor.
 (inversion H; subst).
 (rewrite app_length, repeat_length).
 lia.
++
+(simpl).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-16 07:47:54.820000.*)
+(* Auto-generated comment: At 2019-08-16 07:48:04.530000.*)
 
