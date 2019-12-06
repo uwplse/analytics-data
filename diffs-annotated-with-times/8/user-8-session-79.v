5f036813897f8925f3a5fd6d82421ae70f288ca7
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:48 -0800

    user 8, session 79, part 182

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -592,10 +592,8 @@ Qed.
 Lemma kron_1_l_inv : forall {m} {n} (A : Matrix m n), A == I 1 \226\138\151 A.
 Proof.
 (intros).
-(rewrite <- (Nat.mul_1_l m), <- (Nat.mul_1_l n)).
-symmetry.
-(rewrite kron_1_l).
+Set Printing All.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-14 11:42:10.120000.*)
+(* Auto-generated comment: At 2019-08-14 11:42:16.540000.*)
 
