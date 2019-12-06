f523c39ebfc0733ba6241d1c8577c08216ac9698
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:31 -0800

    user 8, session 72, part 26

--- diffs-annotated-with-times/8/user-8-session-72.v
+++ diffs-annotated-with-times/8/user-8-session-72.v
@@ -1239,6 +1239,8 @@ constructor.
 (rewrite IHsource_symmetric; trivial).
 Timeout 1 About ancilla_free_box_valid.
 Timeout 1 Print ancilla_free_box_valid.
-Timeout 1 Print Ltac ancilla_free_box_valid.
+specialize @ancilla_free_box_valid as AV.
+(unfold valid_ancillae_box in AV).
+(rewrite ancilla_free_box_valid; [  | apply ancilla_free_X_at | type_check ]).
 (* Auto-generated comment: Failed. *)
 
