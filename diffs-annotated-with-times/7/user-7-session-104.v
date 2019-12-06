ef975a9d5b57df647c2d0e0bbb7d36f149de115b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:04 -0800

    user 7, session 104, part 65

--- diffs-annotated-with-times/7/user-7-session-104.v
+++ diffs-annotated-with-times/7/user-7-session-104.v
@@ -86,9 +86,9 @@ constructor.
 (apply Nat.max_le_compat; [ apply IHt1 | apply IHt2 ]; assumption).
 -
 (apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ apply Nat.le_trans with (| t1 |) | apply Nat.le_trans with (| t2 |) ]).
-auto.
+(destruct Hm as [Hm| Hm]; [ apply Nat.le_trans with (| t1 |) | apply Nat.le_trans with (| t2 |) ]; auto).
+(apply Nat.max_lub_l).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 13:18:29.160000.*)
+(* Auto-generated comment: At 2019-08-20 13:19:46.520000.*)
 
