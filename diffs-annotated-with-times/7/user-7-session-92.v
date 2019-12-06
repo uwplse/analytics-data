693bca17dcf84c4d52f7595d070a5e412e2eb2f8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:52 -0800

    user 7, session 92, part 1

--- diffs-annotated-with-times/7/user-7-session-92.v
+++ diffs-annotated-with-times/7/user-7-session-92.v
@@ -15,8 +15,17 @@ Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Close Scope btjd_scope.
 Open Scope btjr_scope.
-Theorem sub_r__sound : forall t1 t2 : ty, |- t1 << t2 -> (|- t1 << t2)%btj.
-(* Auto-generated comment: Succeeded. *)
+Theorem sub_r__sound : forall t1 t2 : ty, |- t1 << t2 -> (|- t1 << t2)%btjd.
+Proof.
+(intros t1 t2 Hsub; induction Hsub; try (solve [ constructor; assumption ])).
+-
+(apply union_right_1; assumption).
+-
+(apply union_right_2; assumption).
+-
+(apply SD_Trans with (MkNF( t))).
+(apply mk_nf__sub_d2).
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-18 07:43:18.610000.*)
+(* Auto-generated comment: At 2019-08-18 07:43:25.030000.*)
 
