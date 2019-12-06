e7d3818a087addee571cf2a7af7ef0bcf3a5c111
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:02 -0800

    user 7, session 104, part 1

--- diffs-annotated-with-times/7/user-7-session-104.v
+++ diffs-annotated-with-times/7/user-7-session-104.v
@@ -4,8 +4,8 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Add LoadPath "../..".
 Require Import BetaJulia.BasicPLDefs.Identifier.
-Require Import BetaJulia.Sub0270a.BaseDefs.
-Require Import BetaJulia.Sub0270a.BaseMatchProps.
+Require Import BetaJulia.Sub0280a.BaseDefs.
+Require Import BetaJulia.Sub0280a.BaseMatchProps.
 Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Arith.Arith.
@@ -15,7 +15,15 @@ Lemma cname_eq__decidable : forall n1 n2 : cname, Decidable.decidable (n1 = n2).
 Proof.
 (intros n1 n2; destruct n1; destruct n2; (left; reflexivity) || (right; intros H; inversion H)).
 Qed.
+Definition sem_sub (t1 t2 : ty) := forall k : nat, ||-[ k][t1]<= [t2].
+Notation "'||-' '[' t1 ']' '<=' '[' t2 ']'" := (sem_sub t1 t2) (at level 50) : btjm_scope.
+Lemma sem_sub__refint_eXrefX : ||- [TRef tint]<= [TExist vX (TRef tX)].
+Proof.
+(intros k).
+(destruct k; intros w1; exists 1).
+-
+(intros v Hm).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 12:08:16.420000.*)
+(* Auto-generated comment: At 2019-08-20 12:10:45.450000.*)
 
