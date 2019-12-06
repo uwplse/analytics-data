50f0d6305a6f5ab00b3878fb5dafb239ef89aa07
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:32 -0800

    user 7, session 161, part 16

--- diffs-annotated-with-times/7/user-7-session-161.v
+++ diffs-annotated-with-times/7/user-7-session-161.v
@@ -80,8 +80,11 @@ Abort.
 Lemma subst_nested : forall (X Y : id) (t tx ty : ty), [X := tx] ([Y := ty] t) = [Y := [X := tx] ty] ([X := tx] t).
 Proof.
 (intros X Y t).
-(induction t).
+(induction t; intros tx ty; try reflexivity).
+-
+(repeat rewrite subst_pair).
+(rewrite IHt1).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-29 11:57:22.540000.*)
+(* Auto-generated comment: At 2019-08-29 11:58:09.550000.*)
 
