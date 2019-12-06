88831cd564e0fa8685cf0d37a2941d105220dfd5
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:41 -0800

    user 5, session 19, part 29

--- diffs-annotated-with-times/5/user-5-session-19.v
+++ diffs-annotated-with-times/5/user-5-session-19.v
@@ -102,7 +102,8 @@ Fixpoint identity (t : Term) : Term :=
   end.
 Theorem eval_eq_true_or_false :
   forall (L : EpsilonLogic) env (t1 t2 : Term),
-  L.(eval) env (Eq t1 t2) = L.(vTrue) \/ L.(eval) env (Eq t1 t2) = L.(vFalse).
+  L.(eval) env (Eq t1 t2) = L.(eval) env (Bool true) \/
+  L.(eval) env (Eq t1 t2) = L.(eval) env (Bool false).
 Proof.
 (intros).
 (destruct (L.(value_eq_dec) (L.(eval) env t1) (L.(eval) env t2)) eqn:E).
@@ -117,5 +118,5 @@ assumption.
 Qed.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-13 09:31:16.520000.*)
+(* Auto-generated comment: At 2019-08-13 09:31:50.330000.*)
 
