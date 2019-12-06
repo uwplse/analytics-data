ef2d7877e9b8f8c98666354e1ba874010bfefa01
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:52 -0800

    user 7, session 196, part 3

--- diffs-annotated-with-times/7/user-7-session-196.v
+++ diffs-annotated-with-times/7/user-7-session-196.v
@@ -73,8 +73,9 @@ Qed.
 Lemma b_subst_union : forall (X : id) (s t1 t2 : ty), [BX := s] TUnion t1 t2 = TUnion ([BX := s] t1) ([BX := s] t2).
 Proof.
 (intros).
+(simpl).
 reflexivity.
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-03 09:31:17.900000.*)
+(* Auto-generated comment: At 2019-09-03 09:31:19.460000.*)
 
