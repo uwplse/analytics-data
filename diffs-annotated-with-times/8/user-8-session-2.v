025ba3558470a31a5ae95c1a78ce6de2fa8f76fa
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:15 -0800

    user 8, session 2, part 110

--- diffs-annotated-with-times/8/user-8-session-2.v
+++ diffs-annotated-with-times/8/user-8-session-2.v
@@ -128,8 +128,7 @@ reflexivity.
 -
 (simpl).
 (destruct a).
-+
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-09 11:21:30.950000.*)
+(* Auto-generated comment: At 2019-08-09 11:21:47.710000.*)
 
