d5c8faea1a0d5e874c05b2a168bf0e46e3809dbc
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:44 -0800

    user 7, session 180, part 1

--- diffs-annotated-with-times/7/user-7-session-180.v
+++ diffs-annotated-with-times/7/user-7-session-180.v
@@ -7,8 +7,7 @@ Require Import BetaJulia.BasicPLDefs.Identifier.
 Require Import BetaJulia.Sub0281a.BaseDefs.
 Require Import BetaJulia.Sub0281a.BaseProps.
 Require Import BetaJulia.Sub0281a.BaseMatchProps.
-Require Import BetaJulia.Sub0281a.BaseSemSubProps.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-02 06:43:44.120000.*)
+(* Auto-generated comment: At 2019-09-02 06:43:54.650000.*)
 
