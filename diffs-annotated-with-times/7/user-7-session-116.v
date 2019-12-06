ed1c4f02ff565b077ef44b7fd39eea744873a16e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:14 -0800

    user 7, session 116, part 22

--- diffs-annotated-with-times/7/user-7-session-116.v
+++ diffs-annotated-with-times/7/user-7-session-116.v
@@ -27,11 +27,18 @@ Proof.
 -
 (apply fresh_union__inv in Hfresh).
 (destruct Hfresh as [Hfresh1 Hfresh2]).
-(simpl).
 (rewrite IHt1; try assumption).
 (rewrite IHt2; try assumption).
 reflexivity.
+-
+(apply fresh_union__inv in Hfresh).
+(destruct Hfresh as [Hfresh1 Hfresh2]).
+(rewrite IHt1; try assumption).
+(rewrite IHt2; try assumption).
+reflexivity.
+-
+(rewrite Iht; try assumption).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 06:48:33.470000.*)
+(* Auto-generated comment: At 2019-08-27 06:48:57.470000.*)
 
