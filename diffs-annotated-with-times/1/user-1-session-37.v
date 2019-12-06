98da87a629b43b113c59384cb11f677d9f98d4a3
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:34 -0800

    user 1, session 37, part 44

--- diffs-annotated-with-times/1/user-1-session-37.v
+++ diffs-annotated-with-times/1/user-1-session-37.v
@@ -306,6 +306,7 @@ Print Forall.
 Print Forall2.
 Print map.
 Search -option.
+Print option_map.
 Fixpoint Gamma (G : GT) : SetST :=
   match G with
   | GDyn => Full_set _
@@ -317,13 +318,14 @@ Fixpoint Gamma (G : GT) : SetST :=
       exists l',
         X = SRec l' /\
         Forall2 (fun (S' : option ST) G' => True) l'
-          (option_map
-             (fun pair =>
-              match pair with
-              | (R, G) => Gamma G
-              | (O, G) => Empty_set _
-              end) l)
+          (map
+             (option_map
+                (fun pair =>
+                 match pair with
+                 | (R, G) => Gamma G
+                 | (O, G) => Empty_set _
+                 end)) l)
   | _ => Empty_set _
   end.
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
