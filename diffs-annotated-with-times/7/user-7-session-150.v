6d064791d4a262b3dbe3edcd4335ab7688cf047f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:26 -0800

    user 7, session 150, part 3

--- diffs-annotated-with-times/7/user-7-session-150.v
+++ diffs-annotated-with-times/7/user-7-session-150.v
@@ -39,8 +39,90 @@ Proof.
 (intros X t).
 (induction t; intros Hfresh s; try (solve [ reflexivity ]); unfold fresh_in_ty in *; simpl in Hfresh; simpl;
   try (solve
-   [ apply fresh_union__inv in Hfresh; destruct Hfresh as [Hfresh1 Hfresh2]; rewrite IHt1; try assumption; rewrite IHt2; try assumption ])).
-(* Auto-generated comment: Failed. *)
+   [ apply fresh_union__inv in Hfresh; destruct Hfresh as [Hfresh1 Hfresh2]; rewrite IHt1; try assumption; rewrite IHt2; try assumption;
+      reflexivity ])).
+-
+(destruct (beq_idP X i); try reflexivity).
+(rewrite IHt).
+reflexivity.
+(unfold fresh in *).
+(intros Hcontra).
+(apply Hfresh).
+(apply IdSetFacts.remove_2; try assumption).
+(intros Heq).
+subst.
+contradiction.
+-
+(unfold fresh in Hfresh).
+(destruct (beq_idP X i); try reflexivity).
+subst.
+exfalso.
+(apply Hfresh).
+(apply IdSetFacts.singleton_2).
+reflexivity.
+Qed.
+Lemma subs_neq__permute :
+  forall X Y : id, X <> Y -> forall t s1 s2 : ty, fresh_in_ty X s2 -> fresh_in_ty Y s1 -> [X := s1] ([Y := s2] t) = [Y := s2] ([X := s1] t).
+Proof.
+(intros X Y Hneq t).
+(induction t; intros s1 s2 HXs2 HYs1;
+  try (solve [ simpl; reflexivity | simpl; rewrite IHt1; try assumption; rewrite IHt2; try assumption; reflexivity ])).
+-
+(simpl).
+(destruct (beq_idP X i)).
++
+subst.
+(destruct (beq_idP Y i); reflexivity).
++
+(destruct (beq_idP Y i)).
+*
+subst.
+reflexivity.
+*
+(rewrite IHt; try assumption).
+reflexivity.
+-
+(simpl; destruct (beq_idP X i); destruct (beq_idP Y i); subst).
++
+contradiction.
++
+(simpl).
+(rewrite <- beq_id_refl).
+symmetry.
+(apply subs_fresh_in_ty).
+assumption.
++
+(simpl).
+(rewrite <- beq_id_refl).
+(apply subs_fresh_in_ty).
+assumption.
++
+(simpl).
+(rewrite (false_beq_id _ _ n)).
+(rewrite (false_beq_id _ _ n0)).
+reflexivity.
+Qed.
+Lemma subs_id : forall (X : id) (t : ty), [X := TVar X] t = t.
+Proof.
+(intros X t; induction t; simpl; try reflexivity).
+-
+(rewrite IHt1).
+(rewrite IHt2).
+reflexivity.
+-
+(rewrite IHt1).
+(rewrite IHt2).
+reflexivity.
+-
+(destruct (beq_idP X i); try reflexivity).
+(rewrite IHt).
+reflexivity.
+-
+(destruct (beq_idP X i); try reflexivity).
+subst.
+reflexivity.
+Qed.
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-29 08:11:18.120000.*)
+(* Auto-generated comment: At 2019-08-29 08:12:07.920000.*)
 
