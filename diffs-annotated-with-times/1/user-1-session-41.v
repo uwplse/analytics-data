85c997583dd0ac2bf1e78b5d61c5cbcc5ce649ea
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:38 -0800

    user 1, session 41, part 88

--- diffs-annotated-with-times/1/user-1-session-41.v
+++ diffs-annotated-with-times/1/user-1-session-41.v
@@ -758,6 +758,5 @@ subst.
 -
 (intros; inversion H0; subst; eauto).
 all: (try (apply singleton_eq in H1; congruence)).
-all: (try specialize (H1 _ (In_singleton _ _))).
 (* Auto-generated comment: Succeeded. *)
 
