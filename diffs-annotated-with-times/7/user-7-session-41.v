9a8fd95a879890eef1bdcd3a8f24532c05d7011e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:33 -0800

    user 7, session 41, part 12

--- diffs-annotated-with-times/7/user-7-session-41.v
+++ diffs-annotated-with-times/7/user-7-session-41.v
@@ -26,8 +26,33 @@ auto.
 -
 (apply match_ty_pair__inv in Hm).
 (destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(unfold sem_sub_k in *).
 auto using match_ty_pair.
-(* Auto-generated comment: Failed. *)
+-
+(apply match_ty_union__inv in Hm).
+(destruct Hm; [ apply IHHsub1 | apply IHHsub2 ]; assumption).
+-
+(apply match_ty_union_1; assumption).
+-
+(apply match_ty_union_2; assumption).
+-
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_union__inv in Hm1).
+(destruct Hm1; [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto using match_ty_pair).
+-
+(apply match_ty_pair__inv in Hm).
+(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
+(apply match_ty_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; auto using match_ty_pair).
+-
+(destruct k).
+(destruct v; contradiction).
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [tx [Heq [[Hdept Hdeptx] Href]]]; subst).
+(simpl).
+split.
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-16 06:46:23.080000.*)
+(* Auto-generated comment: At 2019-08-16 06:46:46.230000.*)
 
