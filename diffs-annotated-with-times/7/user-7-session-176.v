1307dbc6ab0c8b4e0bca9a26abf75f939ce8be31
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:43 -0800

    user 7, session 176, part 63

--- diffs-annotated-with-times/7/user-7-session-176.v
+++ diffs-annotated-with-times/7/user-7-session-176.v
@@ -153,9 +153,12 @@ Proof.
       try (solve [ apply match_ty_union_1; auto | apply match_ty_union_2; auto ]) ])).
 -
 (split; intros Hm).
++
 (rewrite (subst_equation X sx) in Hm).
 (destruct (IdSet.mem i (FV sx))).
+(unfold mk_subst_exist in Hm).
+(rewrite (subst_equation Y sy) in Hm).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-30 07:25:02.750000.*)
+(* Auto-generated comment: At 2019-08-30 07:25:09.410000.*)
 
