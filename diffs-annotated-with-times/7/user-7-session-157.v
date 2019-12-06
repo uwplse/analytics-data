2c97ce62133330764388f386f4b72c799f581568
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:28 -0800

    user 7, session 157, part 1

--- diffs-annotated-with-times/7/user-7-session-157.v
+++ diffs-annotated-with-times/7/user-7-session-157.v
@@ -14,7 +14,16 @@ Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Lemma build_v : forall (X X' : id) (tx : ty) (w : nat) (t v : ty), |-[ w] v <$ [X := tx] t -> exists v' : ty, |-[ w] v' <$ [X := TVar X'] t.
 Proof.
-(* Auto-generated comment: Failed. *)
+(intros X X' tx).
+(induction w; induction t; intros v Hm).
+-
+exists (TCName c).
+(apply match_ty_cname).
+-
+(simpl in Hm).
+(simpl).
+(apply match_ty_pair__inv in Hm).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-29 09:00:15.150000.*)
+(* Auto-generated comment: At 2019-08-29 09:00:26.210000.*)
 
