62cf89237465661e57c00bc96ada8db72ed3fe2c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:56 -0800

    user 3, session 73, part 6

--- diffs-annotated-with-times/3/user-3-session-73.v
+++ diffs-annotated-with-times/3/user-3-session-73.v
@@ -7,13 +7,7 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Require Import Spec.Proc.
 Require Import Spec.Hoare.
-From Transitions Require Import Relations Rewriting.
-Require Import Abstraction.
-Require Import Layer.
-Section Abstraction.
-Context (AState CState : Type).
-Context (absr : relation AState CState unit).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-07 13:54:02.390000.*)
+(* Auto-generated comment: At 2019-08-07 13:54:02.890000.*)
 
