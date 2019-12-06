43d16229845c400403b25a2c7d0927a7ab01d4c6
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:07 -0800

    user 7, session 19, part 10

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -48,11 +48,10 @@ reflexivity.
 specialize (Href v' Hv').
 (destruct Href; split; assumption).
 Qed.
-Lemma value_sem_sub_i_union__inv : forall v : ty, value_type v -> forall ta tb : ty, ||- [v]<= [TUnion ta tb] -> ||- [v]<= [ta] \/ ||- [v]<= [tb].
+Lemma match_ty_i_k__match_le_k : forall (k : nat) (v t : ty), |-[ k] v <$ t -> forall k' : nat, k' <= k -> |-[ k] v <$ t.
 Proof.
-(intros v Hv; induction Hv).
--
+Abort.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 09:33:20.410000.*)
+(* Auto-generated comment: At 2019-08-12 09:35:16.440000.*)
 
