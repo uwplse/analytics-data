f42d57b902cee0646d2dcc78d6bfd17b8781e9d9
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:40 -0800

    user 2, session 5, part 15

--- diffs-annotated-with-times/2/user-2-session-5.v
+++ diffs-annotated-with-times/2/user-2-session-5.v
@@ -34,8 +34,7 @@ Notation "[ x y .. z ]" := (Cons x (Cons y .. (Cons z Nil) ..))
   (x  at level 0, y  at level 0, z  at level 0) : coucou_scope.
 End TermNotations.
 Import TermNotations.
-Open Scope coucou_scope.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-17 14:33:01.910000.*)
+(* Auto-generated comment: At 2019-08-17 14:33:02.220000.*)
 
