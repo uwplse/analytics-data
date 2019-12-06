57a769cf5b8735cb394032a9c3be8cca5775b2d1
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:50:23 -0800

    user 3, session 9904, part 88

--- diffs-annotated-with-times/3/user-3-session-9904.v
+++ diffs-annotated-with-times/3/user-3-session-9904.v
@@ -144,10 +144,8 @@ Proof.
 (unfold rec_wipe).
 (intros).
 (apply spec_abstraction_compose; simpl).
-(step_proc; intros).
-{
-eauto.
+(step_proc; intuition).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-06 05:44:37.570000.*)
+(* Auto-generated comment: At 2019-09-06 05:44:41.770000.*)
 
