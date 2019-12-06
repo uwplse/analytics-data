cef12e30d1f8ebe2b5d74cd275e8c3a8d1f05175
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:51:56 -0800

    user 3, session 10411, part 16

--- diffs-annotated-with-times/3/user-3-session-10411.v
+++ diffs-annotated-with-times/3/user-3-session-10411.v
@@ -312,7 +312,8 @@ clear H.
 (eapply H0 in H2; eauto).
 (destruct matches in *; safe_intuition repeat deex; eauto).
 (descend; intuition eauto).
+intuition eauto.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-06 06:42:00.140000.*)
+(* Auto-generated comment: At 2019-09-06 06:42:23.690000.*)
 
