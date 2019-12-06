64fade36d67e70db69d46cfffd3d39f3eece0310
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:00 -0800

    user 7, session 16, part 31

--- diffs-annotated-with-times/7/user-7-session-16.v
+++ diffs-annotated-with-times/7/user-7-session-16.v
@@ -404,8 +404,8 @@ contradiction.
 (solve_atom_sub_r_union__decidable IHt2_1 IHt2_2; assumption).
 +
 (right; solve_not_x_sub_r_y_full).
-(apply IHHcontra; try tauto).
+(apply IHHcontra; try auto).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-09 11:33:37.710000.*)
+(* Auto-generated comment: At 2019-08-09 11:33:47.450000.*)
 
