8c61aefb5f44dca2d446ee5e58de428ece9ae5d0
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:57 -0800

    user 7, session 100, part 1

--- diffs-annotated-with-times/7/user-7-session-100.v
+++ diffs-annotated-with-times/7/user-7-session-100.v
@@ -17,8 +17,23 @@ Qed.
 Lemma match_ty_pair : forall (v1 v2 t1 t2 : ty) (w k : nat), |-[ w, k] v1 <$ t1 -> |-[ w, k] v2 <$ t2 -> |-[ w, k] TPair v1 v2 <$ TPair t1 t2.
 Proof.
 (intros v1 v2 t1 t2 w k Hm1 Hm2).
-(destruct k; split; assumption).
+(destruct w; destruct k; split; assumption).
+Qed.
+Lemma match_ty_union_1 : forall (v t1 t2 : ty) (w k : nat), |-[ w, k] v <$ t1 -> |-[ w, k] v <$ TUnion t1 t2.
+Proof.
+(intros v t1 t2 w k Hm).
+(destruct w; destruct k; destruct v; left; assumption).
+Qed.
+Lemma match_ty_union_2 : forall (v t1 t2 : ty) (w k : nat), |-[ w, k] v <$ t2 -> |-[ w, k] v <$ TUnion t1 t2.
+Proof.
+(intros v t1 t2 w k Hm).
+(destruct w; destruct k; destruct v; right; assumption).
+Qed.
+Lemma match_ty_exist : forall (v : ty) (X : id) (t : ty) (w k : nat), (exists tx : ty, |-[ w, k] v <$ [X := tx] t) -> |-[ S w, k] v <$ TExist X t.
+Proof.
+(intros v X t w k Hex).
+(destruct v; assumption).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 07:54:21.230000.*)
+(* Auto-generated comment: At 2019-08-20 07:55:37.820000.*)
 
