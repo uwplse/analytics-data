1f26524648389f7355cdf0da36466194cc242882
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:13 -0800

    user 3, session 7199, part 321

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -407,8 +407,8 @@ congruence.
 }
 }
 step_proc.
-intuition.
+intuition eauto.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 12:11:14.210000.*)
+(* Auto-generated comment: At 2019-09-04 12:11:15.410000.*)
 
