66d920f5ab557b61c8356012c564fc72fd15b695
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:23 -0800

    user 7, session 19, part 675

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -499,8 +499,8 @@ Proof.
 -
 (intros ta1 ta2 Hat1 IH1 Hat2 IH2 Hdep).
 (assert (Hat : atom_type (TPair ta1 ta2)) by (constructor; assumption)).
-(apply pair_sem_sub_k_i__sub_d; assumption).
+(apply pair_sem_sub_k_i__sub_d; try assumption).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 13:40:20.620000.*)
+(* Auto-generated comment: At 2019-08-13 13:41:07.390000.*)
 
