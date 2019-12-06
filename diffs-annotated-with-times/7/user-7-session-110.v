fbe58b17cc1afe32550c7a9fc4b9d1e2346c29ff
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:10 -0800

    user 7, session 110, part 9

--- diffs-annotated-with-times/7/user-7-session-110.v
+++ diffs-annotated-with-times/7/user-7-session-110.v
@@ -131,6 +131,8 @@ Proof.
          apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst; apply match_ty_pair; [ eapply IHt1 | eapply IHt2 ]; eauto
    | |- |-[ _, _] _ <$ TUnion _ _ =>
          apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; eauto
+   | |- |-[ _, _] _ <$ TVar _ => apply match_ty_var__inv in Hm; subst; apply match_ty_var
+   | |- |-[ _, _] _ <$ TEV _ => apply match_ty_ev__inv in Hm; subst; apply match_ty_ev
    end).
 -
 (destruct k).
@@ -144,7 +146,28 @@ Proof.
 (destruct w'; assumption).
 -
 (apply match_ty_exist__0_inv in Hm; contradiction).
+-
+(destruct k).
++
+(apply match_ty_ref__weak_inv in Hm).
+(destruct Hm as [t' Heq]; subst).
+(destruct w'; constructor).
++
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [t' [Heq Href]]; subst).
+(destruct w'; assumption).
+-
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hmx]).
+(destruct w').
+(inversion Hle).
+(apply match_ty_exist).
+exists tx.
+(apply IHw).
+assumption.
+(apply le_S_n; assumption).
+-
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-26 07:52:19.090000.*)
+(* Auto-generated comment: At 2019-08-26 07:53:43.770000.*)
 
