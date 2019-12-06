a7a8d6313b36382763c878512d826fbccb42968c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:29 -0800

    user 7, session 157, part 27

--- diffs-annotated-with-times/7/user-7-session-157.v
+++ diffs-annotated-with-times/7/user-7-session-157.v
@@ -60,16 +60,14 @@ exists (TPair v1' v2').
 (destruct Hm as [Hm| Hm]; [ destruct (IHt1 _ Hm) as [v' Hm'] | destruct (IHt2 _ Hm) as [v' Hm'] ]; exists v';
   [ apply match_ty_union_1 | apply match_ty_union_2 ]; assumption).
 -
-(destruct (beq_idP X i)).
+(destruct (beq_idP X i) as [Hbeq| Hbeq]).
 +
 subst.
 (simpl in *).
 (rewrite <- beq_id_refl in *).
 (exists v; assumption).
 +
-(destruct (beq_id_false_iff X i) as [_ Hid]).
-specialize (Hid n).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-29 09:11:49.410000.*)
+(* Auto-generated comment: At 2019-08-29 09:12:13.010000.*)
 
