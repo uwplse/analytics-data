21208dc3681e88eccfc4ce91c57d9cc60831a176
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:57 -0800

    user 7, session 203, part 25

--- diffs-annotated-with-times/7/user-7-session-203.v
+++ diffs-annotated-with-times/7/user-7-session-203.v
@@ -210,13 +210,13 @@ exists (Nat.max w1 w2),(TPair v1 v2).
 (apply Nat.le_max_l).
 (apply Nat.le_max_r).
 -
-(destruct IHt1 as [IHt1| [w1 [v1 IHt1]]]; destruct IHt2 as [IHt2| [w2 [v2 IHt2]]]).
-+
+(destruct IHt1 as [IHt1| [w1 [v1 IHt1]]]; destruct IHt2 as [IHt2| [w2 [v2 IHt2]]];
+  try (solve [ right; eexists; eexists; (apply match_ty_union_1; eassumption) || (apply match_ty_union_2; eassumption) ])).
 (left; intros w v Hm; apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm]; exfalso; eauto).
+-
+(destruct IHt as [IHt| [w [v Hm]]]).
 +
-(right; eexists; eexists).
-((apply match_ty_union_1; eassumption) || (apply match_ty_union_2; eassumption)).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 07:56:21.130000.*)
+(* Auto-generated comment: At 2019-09-04 07:56:29.750000.*)
 
