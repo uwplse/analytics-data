6782cddf457aa8db571ba801e8f613c4dec36749
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:31:39 -0800

    user 3, session 4175, part 2

--- diffs-annotated-with-times/3/user-3-session-4175.v
+++ diffs-annotated-with-times/3/user-3-session-4175.v
@@ -5,8 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-From Coq Require Extraction.
 (* Auto-generated comment: Succeeded. *)
-
-(* Auto-generated comment: At 2019-08-30 05:07:07.990000.*)
 
