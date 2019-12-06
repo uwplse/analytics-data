65743e0e77f6843c7d295b8ec6b9dd0c6e3a4991
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:41 -0800

    user 2, session 5, part 62

--- diffs-annotated-with-times/2/user-2-session-5.v
+++ diffs-annotated-with-times/2/user-2-session-5.v
@@ -142,7 +142,12 @@ Fixpoint multistep (n : nat) : term -> term :=
   end.
 Redirect "/tmp/coq1McSmJ" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
+Fixpoint trace (n : nat) : term -> list term :=
+  match n with
+  | O => nil
+  | S m => fun t => t :: trace m (step t)
+  end.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-17 15:56:03.270000.*)
+(* Auto-generated comment: At 2019-08-17 15:57:09.620000.*)
 
