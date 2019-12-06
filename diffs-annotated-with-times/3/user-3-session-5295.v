a55b185500a696b2bc1955f8c782c4d8cf8a8217
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:36:09 -0800

    user 3, session 5295, part 10

--- diffs-annotated-with-times/3/user-3-session-5295.v
+++ diffs-annotated-with-times/3/user-3-session-5295.v
@@ -241,8 +241,7 @@ Proof.
 (apply le_eq_or_S_le in H1; intuition subst).
 +
 (destruct (lt_dec a' (diskSize d_0)); autorewrite with upd).
-(assert (a' < diskSize d_1) by congruence; autorewrite with upd; auto).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-03 11:12:25.560000.*)
+(* Auto-generated comment: At 2019-09-03 11:12:30.350000.*)
 
