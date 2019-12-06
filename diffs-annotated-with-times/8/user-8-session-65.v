8339b045103f4fff52a72b5cebe70df7c35d07ed
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:18 -0800

    user 8, session 65, part 199

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -1246,13 +1246,5 @@ constructor.
 -
 (inversion g0).
 +
-(unfold valid_ancillae_box).
-(intros \207\129 TB).
-(repeat simpl_rewrite inSeq_correct; try apply inSeq_WT; try apply unitary_at1_WT;
-  try apply source_symmetric_WT; trivial).
-(unfold compose_super).
-(apply source_symmetric_WT in H).
-(unfold valid_ancillae_box in IHsource_symmetric).
-(rewrite IHsource_symmetric; trivial).
 (* Auto-generated comment: Succeeded. *)
 
