8862607aab78a7a11023051897393707c185c333
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:21:23 -0800

    user 3, session 960, part 283

--- diffs-annotated-with-times/3/user-3-session-960.v
+++ diffs-annotated-with-times/3/user-3-session-960.v
@@ -278,8 +278,8 @@ typeclasses eauto.
 (destruct x, y; simpl).
 (destruct (nat_eq_dec n n0); subst).
 left.
-Search -(existT _ _ = existT _ _) -proj2T.
-(* Auto-generated comment: Failed. *)
+Search -(existT _ _ = existT _ _) -projT2.
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-16 07:08:31.820000.*)
+(* Auto-generated comment: At 2019-08-16 07:08:35.080000.*)
 
