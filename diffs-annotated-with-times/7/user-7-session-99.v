1f4dc1ec58d3a8f4e756e0d8d612e8586e37a7b6
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:56 -0800

    user 7, session 99, part 3

--- diffs-annotated-with-times/7/user-7-session-99.v
+++ diffs-annotated-with-times/7/user-7-session-99.v
@@ -51,8 +51,29 @@ Inductive value_type : ty -> Prop :=
   | VT_CName : forall cn, value_type (TCName cn)
   | VT_Pair : forall v1 v2, value_type v1 -> value_type v2 -> value_type (TPair v1 v2)
   | VT_Ref : forall t, value_type (TRef t)
-  | VT_EV : forall X, value_type TEV X.
-(* Auto-generated comment: Succeeded. *)
+  | VT_EV : forall X : id, value_type (TEV X).
+Hint Constructors value_type: DBBetaJulia.
+Declare Scope btjm_scope.
+Delimit Scope btjm_scope with btjm.
+Open Scope btjm.
+Reserved Notation "'|-[' w ',' k ']' v '<$' t" (at level 40).
+Fixpoint match_ty (w : nat) :=
+  fix mtyk (k : nat) :=
+    fix mty (v : ty) :=
+      fix mty' (t : ty) :=
+        match w, k, v, t with
+        | _, _, TCName c, TCName c' => c = c'
+        | _, _, TPair v1 v2, TPair t1 t2 => mty v1 t1 /\ mty v2 t2
+        | _, _, _, TUnion t1 t2 => mty' t1 \/ mty' t2
+        | _, 0, TRef t', TRef t => True
+        | _, S k, TRef t', TRef t => forall v, |-[ w, k] v <$ t' <-> |-[ w, k] v <$ t
+        | 0, _, v, TExist _ t' => mty' t'
+        | S w, _, v, TExist X t' => exists tx, |-[ w, k] v <$ [X := tx] t'
+        | _, _, v, TVar X => TEV X
+        | _, _, _, _ => False
+        end
+where "'|-[' w ',' k ']' v '<$' t" := (match_ty w k v t) : btjm_scope.
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 07:40:19.650000.*)
+(* Auto-generated comment: At 2019-08-20 07:40:23.560000.*)
 
