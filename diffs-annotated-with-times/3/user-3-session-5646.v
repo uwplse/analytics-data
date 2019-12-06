cd77214cbd17a82d5642113f0fe1a046621ec8c2
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:37:14 -0800

    user 3, session 5646, part 2

--- diffs-annotated-with-times/3/user-3-session-5646.v
+++ diffs-annotated-with-times/3/user-3-session-5646.v
@@ -5,8 +5,5 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Require Import POCS.
 (* Auto-generated comment: Succeeded. *)
-
-(* Auto-generated comment: At 2019-09-03 13:41:33.180000.*)
 
