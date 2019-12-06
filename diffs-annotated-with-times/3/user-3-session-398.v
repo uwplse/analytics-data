604df3a60378dc723ff01a9fa51259678772f927
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:19:06 -0800

    user 3, session 398, part 1

--- diffs-annotated-with-times/3/user-3-session-398.v
+++ diffs-annotated-with-times/3/user-3-session-398.v
@@ -1,5 +1,3 @@
-Require Import Analytical.Analytical.
-(* Auto-generated comment: Failed. *)
-
-(* Auto-generated comment: At 2019-08-08 08:29:27.270000.*)
+Require Import Analytical.Analytics.
+(* Auto-generated comment: Succeeded. *)
 
