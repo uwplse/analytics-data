b7067a95c957f1c623301f6f2f6c53a42a766ac8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:46 -0800

    user 7, session 184, part 9

--- diffs-annotated-with-times/7/user-7-session-184.v
+++ diffs-annotated-with-times/7/user-7-session-184.v
@@ -89,8 +89,9 @@ split.
 (apply match_ty_union__inv in Hm').
 (destruct Hm' as [Hm'| Hm']; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
 +
-admit.
+(apply match_ty_exist__inv in Hm').
+(destruct Hm' as [tx Hm']).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-02 07:46:58.420000.*)
+(* Auto-generated comment: At 2019-09-02 07:47:37.090000.*)
 
