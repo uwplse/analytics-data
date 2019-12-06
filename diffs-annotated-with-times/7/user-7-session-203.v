38497c69782420b32179e49f898dad86464990bb
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:57 -0800

    user 7, session 203, part 24

--- diffs-annotated-with-times/7/user-7-session-203.v
+++ diffs-annotated-with-times/7/user-7-session-203.v
@@ -215,8 +215,8 @@ exists (Nat.max w1 w2),(TPair v1 v2).
 (left; intros w v Hm; apply match_ty_union__inv in Hm; destruct Hm as [Hm| Hm]; exfalso; eauto).
 +
 (right; eexists; eexists).
-((apply match_ty_union_1; assumption) || (apply match_ty_union_2; assumption)).
+((apply match_ty_union_1; eassumption) || (apply match_ty_union_2; eassumption)).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 07:56:02.340000.*)
+(* Auto-generated comment: At 2019-09-04 07:56:21.130000.*)
 
