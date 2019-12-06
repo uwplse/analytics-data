25c311e2f57d5e44cfea02d8b046e36ddba09bf2
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:34:05 -0800

    user 3, session 4740, part 10

--- diffs-annotated-with-times/3/user-3-session-4740.v
+++ diffs-annotated-with-times/3/user-3-session-4740.v
@@ -18,5 +18,5 @@ Add Search Blacklist "Private_" "_subproof".
 Notation "a === b" := (a = b) (at level 100).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-03 04:35:58.320000.*)
+(* Auto-generated comment: At 2019-09-03 04:36:19.200000.*)
 
