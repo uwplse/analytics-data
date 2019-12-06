c6903e221f10153a36394de5e43ca32932ecd0d0
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:40 -0800

    user 5, session 19, part 17

--- diffs-annotated-with-times/5/user-5-session-19.v
+++ diffs-annotated-with-times/5/user-5-session-19.v
@@ -149,11 +149,16 @@ intro x.
 {
 (intros).
 (assert
-  (eval L (extendEnv env x (eval L env (Int 3)))
+  (eval L
+     (extendEnv env x
+        (eval L env (Choose x (Eq (Int 6) (Times (Var x) (Int 2))))))
      (Eq (Int 6) (Times (Var x) (Int 2))) = L.(vTrue))).
 {
 (apply evalChoose).
+exists (eval L env (Int 3)).
+(apply evalEqTrue).
+(rewrite evalTimes).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-12 19:02:44.690000.*)
+(* Auto-generated comment: At 2019-08-12 19:02:46.680000.*)
 
