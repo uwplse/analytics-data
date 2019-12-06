0b83a6309d0228ba536354878324ec0cbeca0abc
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:49 -0800

    user 8, session 37, part 10

--- diffs-annotated-with-times/8/user-8-session-37.v
+++ diffs-annotated-with-times/8/user-8-session-37.v
@@ -23,11 +23,44 @@ Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqJ4AsTz"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Definition valid_ancillae {W} (c : Circuit W) : Prop :=
-  forall (\206\147 \206\1470 : Ctx) \207\129, \206\147 \226\138\162 c :Circ -> (\226\159\168 \206\1470 | \206\147 \226\138\169 c \226\159\169) \207\129 = (\226\159\168! \206\1470 | \206\147 \226\138\169 c !\226\159\169) \207\129.
-Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqOzxL2O"
+  forall (\206\147 \206\1470 : Ctx) \207\129, \206\147 \226\138\162 c :Circ -> (\226\159\168 \206\1470 | \206\147 \226\138\169 c \226\159\169) \207\129 == (\226\159\168! \206\1470 | \206\147 \226\138\169 c !\226\159\169) \207\129.
+Definition valid_ancillae_box {W1} {W2} (c : Box W1 W2) 
+  \207\129 := Typed_Box c -> denote_box true c \207\129 == denote_box false c \207\129.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqL4MsFn"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
+Definition valid_ancillae' {W} (c : Circuit W) :=
+  forall (\206\147 \206\1470 : Ctx) \207\129,
+  \206\147 \226\138\162 c :Circ -> Mixed_State \207\129 -> trace ((\226\159\168! \206\1470 | \206\147 \226\138\169 c !\226\159\169) \207\129) = 1.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqkof24o"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Definition valid_ancillae_box' {W1} {W2} (c : Box W1 W2) : Prop :=
+  forall \207\129, Typed_Box c -> Mixed_State \207\129 -> trace (denote_box false c \207\129) = 1.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqYDUbNe"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Proposition valid_ancillae_equal :
+  forall W (c : Circuit W), valid_ancillae c <-> valid_ancillae' c.
+Proof.
+(intros).
+(unfold valid_ancillae, valid_ancillae').
+split.
+-
+(intros H \206\147 \206\1470 \207\129 H0 H1).
+(rewrite <- H; trivial).
+(apply mixed_state_trace_1).
+admit.
+-
+(induction c as [| W' W0 g p c IH| IH]).
++
+reflexivity.
++
+(intros H \206\147 \206\1470 H').
+replace (gate g p c) with compose (gate g p (fun p' => output p')) c by auto.
+dependent destruction H'.
+(destruct \206\1471 as [| \206\1471]; try invalid_contradiction).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-12 14:12:34.350000.*)
+(* Auto-generated comment: At 2019-08-12 14:13:17.410000.*)
 
