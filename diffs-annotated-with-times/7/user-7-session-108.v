0b88e5a6f91f8df4b79279670c2b9839ee9bcbc4
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:09 -0800

    user 7, session 108, part 116

--- diffs-annotated-with-times/7/user-7-session-108.v
+++ diffs-annotated-with-times/7/user-7-session-108.v
@@ -52,8 +52,84 @@ Proof.
 (intros k w1).
 exists (S w1).
 (intros v Hm).
+(apply match_ty_exist).
 exists (TExist vX (TRef tX)).
+assumption.
+Qed.
+Lemma not_sem_sub__refint_refflt : ~ ||- [TRef tint]<= [TRef tflt].
+Proof.
+(intros Hcontra).
+specialize (Hcontra 1 0).
+(destruct Hcontra as [w2 Hcontra]).
+(assert (Hm : |-[ 1, 0] TRef tint <$ TRef tint) by (apply match_ty_value_type__reflexive; constructor)).
+specialize (Hcontra _ Hm).
+clear Hm.
+(apply match_ty_ref__inv in Hcontra).
+(destruct Hcontra as [t' [Heq Hcontra]]).
+(inversion Heq; subst).
+clear Heq.
+(destruct Hcontra as [Hcontra _]).
+(assert (Hm : |-[ 0, 0] tint <$ tint) by (apply match_ty_value_type__reflexive; constructor)).
+specialize (Hcontra 0).
+(destruct Hcontra as [w2' Hcontra]).
+specialize (Hcontra _ Hm).
+(apply match_ty_cname__inv in Hcontra).
+(inversion Hcontra).
+Qed.
+Lemma sem_sub__eunion__unione : forall (X : id) (t1 t2 : ty), ||- [TExist X (TUnion t1 t2)]<= [TUnion (TExist X t1) (TExist X t2)].
+Proof.
+(intros X t1 t2 k).
+(intros w1).
+exists w1.
+(intros v Hm).
+(destruct w1).
+-
+(apply match_ty_exist__0_inv in Hm).
+contradiction.
+-
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hmx]).
+(simpl in Hmx).
+(apply match_ty_union__inv in Hmx).
+(destruct Hmx as [Hmx| Hmx]; [ apply match_ty_union_1 | apply match_ty_union_2 ]; apply match_ty_exist; exists tx; assumption).
+Qed.
+Lemma sem_sub__unione__eunion : forall (X : id) (t1 t2 : ty), ||- [TUnion (TExist X t1) (TExist X t2)]<= [TExist X (TUnion t1 t2)].
+Proof.
+(intros X t1 t2 k).
+(intros w1).
+exists w1.
+(intros v Hm).
+(apply match_ty_union__inv in Hm).
+(destruct Hm as [Hm| Hm]).
+-
+(destruct w1).
++
+(apply match_ty_exist__0_inv in Hm).
+contradiction.
++
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hmx]).
+(simpl in Hmx).
+(apply match_ty_exist).
+exists tx.
+(apply match_ty_union_1).
+assumption.
+-
+(destruct w1).
++
+(apply match_ty_exist__0_inv in Hm).
+contradiction.
++
+(apply match_ty_exist__inv in Hm).
+(destruct Hm as [tx Hmx]).
+(simpl in Hmx).
+(apply match_ty_exist).
+exists tx.
+(apply match_ty_union_2).
+assumption.
+Qed.
+Lemma sem_sub__pair_exist_distr_1 : forall (X : id) (t1 t2 : ty), ||- [TPair (TExist X t1) t2]<= [TExist X (TPair t1 t2)].
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-21 13:30:59.770000.*)
+(* Auto-generated comment: At 2019-08-21 13:31:51.530000.*)
 
