4b64f20c6bb6ae03a4cf608d442f0c2e3c93165c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:21:24 -0800

    user 3, session 960, part 348

--- diffs-annotated-with-times/3/user-3-session-960.v
+++ diffs-annotated-with-times/3/user-3-session-960.v
@@ -253,8 +253,7 @@ admit.
 (unfold nat64_from_le, nat64_to_le; intros).
 (rewrite app_length).
 (destruct (le_dec (Datatypes.length (nat_to_le 254 x)) 8)).
-(rewrite repeat_length).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-16 07:21:08.590000.*)
+(* Auto-generated comment: At 2019-08-16 07:21:27.820000.*)
 
