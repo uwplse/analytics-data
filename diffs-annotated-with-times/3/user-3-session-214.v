7c0528fd31400ed0169e8a1549b1b13fcac79ecc
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:18:23 -0800

    user 3, session 214, part 0

---
+++ diffs-annotated-with-times/3/user-3-session-214.v
@@ -0,0 +1,13 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqCYzfGC"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+From Coq Require Import Morphisms.
+From Tactical Require Import ProofAutomation.
+(* Auto-generated comment: Failed. *)
+
+(* Auto-generated comment: At 2019-08-07 14:03:42.600000.*)
+
