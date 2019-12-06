45258907b369fa45dbbac66a914855a79b85ab0b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:53 -0800

    user 8, session 79, part 346

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -919,8 +919,8 @@ Timeout 1 Print restore_dims.
 Timeout 1 Print Ltac restore_dims.
 restore_dims
  simpl; try rewrite size_ntensor; try rewrite L2; simpl; unify_pows_two; lia.
-(rewrite <- (kron_1_l (\226\168\130 l2))).
+(rewrite (kron_1_l_inv (\226\168\130 l2))).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-14 14:21:01.920000.*)
+(* Auto-generated comment: At 2019-08-14 14:21:02.510000.*)
 
