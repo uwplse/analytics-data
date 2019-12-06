10ac39d3e6c1ab88a8785ad40d67dc616fb29757
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:47 -0800

    user 8, session 79, part 147

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -606,8 +606,9 @@ Lemma new_morphism :
   Morphisms.Proper (Morphisms.respectful mat_equiv (flip impl)) (mat_equiv A).
 Proof.
 (intros).
-(compute).
+(unfold Morphisms.Proper).
+(unfold Morphisms.respectful).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-13 18:19:54.550000.*)
+(* Auto-generated comment: At 2019-08-13 18:20:04.360000.*)
 
