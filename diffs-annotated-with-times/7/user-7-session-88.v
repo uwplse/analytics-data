b14ffb7de34037b96c6742b72316d34015393735
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:50 -0800

    user 7, session 88, part 52

--- diffs-annotated-with-times/7/user-7-session-88.v
+++ diffs-annotated-with-times/7/user-7-session-88.v
@@ -310,56 +310,6 @@ Proof.
 (apply mk_nf__sem_eq_k_i).
 Qed.
 Ltac
- solve__value_sem_sub_i_union__inv_depth_le_1 Hv Hsem t'1 t'2 :=
-  pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ Hsem) as Hsemu; destruct Hsemu as [Hsemu| Hsemu];
-   [ apply Nat.le_trans with (| t'1 |) | apply Nat.le_trans with (| t'2 |) ]; tauto || apply Max.le_max_l || apply Max.le_max_r.
-Lemma sem_sub_k_i_nf__inv_depth_le_1 : forall (k : nat) (t t' : ty), InNF( t) -> | t | <= k -> ||-[ k][t]<= [t'] -> | t | <= | t' |.
-Proof.
-(induction k; induction t; induction t'; intros Hnft Hdept Hsem; try (solve [ simpl; constructor ]);
-  try (solve
-   [ match goal with
-     | Hsem:||-[ ?k][?t]<= [?t']
-       |- | ?t | <= | ?t' | =>
-           assert (Hv : value_type t) by constructor; assert (Hm : |-[ k] t <$ t) by (apply match_ty_i__reflexive; assumption); specialize
-            (Hsem _ Hm); contradiction
-     | Hsem:||-[ ?k][TPair ?t1 ?t2]<= [TUnion ?t'1 ?t'2]
-       |- _ =>
-           assert (Hv : value_type (TPair t1 t2)) by (apply pair_in_nf__value_type; assumption);
-            solve__value_sem_sub_i_union__inv_depth_le_1 Hv Hsem t'1 t'2
-     | Hsem:||-[ ?k][?t]<= [TUnion ?t'1 ?t'2]
-       |- | ?t | <= _ => assert (Hv : value_type t) by constructor; solve__value_sem_sub_i_union__inv_depth_le_1 Hv Hsem t'1 t'2
-     | Hsem:||-[ ?k][TPair ?t1 ?t2]<= [?t']
-       |- _ <= | ?t' | =>
-           assert (Hvp : value_type (TPair t1 t2)) by (apply pair_in_nf__value_type; assumption);
-            assert (Hmp : |-[ k] TPair t1 t2 <$ TPair t1 t2) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hmp); contradiction
-     | Hsem:||-[ ?k][TPair _ _]<= [TPair _ _]
-       |- _ =>
-           destruct (in_nf_pair__inv _ _ Hnft) as [Hnft1 Hnft2]; destruct (max_inv_depth_le__inv _ _ _ Hdept) as [Hdept1 Hdept2];
-            destruct (sem_sub_k_i_pair__inv _ _ _ _ _ Hsem) as [Hsem1 Hsem2]; simpl; apply Nat.max_le_compat; auto
-     | Hsem:||-[ ?k][TUnion _ _]<= [_], Hnft:InNF( TUnion _ _), Hdept:| TUnion _ _ | <= _
-       |- _ =>
-           destruct (max_inv_depth_le__inv _ _ _ Hdept) as [Hdept1 Hdept2]; destruct (sem_sub_k_i_union_l__inv _ _ _ _ Hsem) as [HSem1 Hsem2];
-            destruct (in_nf_union__inv _ _ Hnft) as [Hnft1 Hnft2]; rewrite inv_depth_union; apply Nat.max_lub; auto
-     end ])).
--
-(inversion Hdept; subst).
--
-(simpl).
-(apply le_n_S).
-(inversion Hnft; subst).
-(inversion H; subst).
-(simpl in Hdept).
-(apply le_S_n in Hdept).
-(apply IHk; try assumption).
-(assert (Hv : value_type (TRef t)) by constructor).
-(assert (Hm : |-[ S k] TRef t <$ TRef t) by (apply match_ty_i__reflexive; constructor)).
-specialize (Hsem _ Hm).
-(simpl in Hsem).
-(intros v').
-specialize (Hsem v').
-tauto.
-Qed.
-Ltac
  solve__value_sem_sub_i_union__inv_depth_le_2 Hdept' Hv Hsem t'1 t'2 :=
   destruct (max_inv_depth_le__inv _ _ _ Hdept') as [Hdept'1 Hdept'2]; pose proof (value_sem_sub_k_i_union__inv _ Hv _ _ _ Hsem) as Hsemu;
    destruct Hsemu as [Hsemu| Hsemu]; [ apply Nat.le_trans with (| t'1 |) | apply Nat.le_trans with (| t'2 |) ];
@@ -463,7 +413,11 @@ specialize (Hsem _ Hm).
 specialize (Hsem v').
 tauto.
 Qed.
+Lemma sem_sub_k_i_nf__inv_depth_le_1 : forall (k : nat) (t t' : ty), InNF( t) -> | t | <= k -> ||-[ k][t]<= [t'] -> | t | <= | t' |.
+Proof.
+(intros).
+(apply sem_sub_k_i_nf__inv_depth_le; tauto).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-18 07:21:26.660000.*)
+(* Auto-generated comment: At 2019-08-18 07:22:28.930000.*)
 
