d739ae90dfde2ed200089f2e37b6d67ede5dc63b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:30:31 -0800

    user 3, session 3788, part 24

--- diffs-annotated-with-times/3/user-3-session-3788.v
+++ diffs-annotated-with-times/3/user-3-session-3788.v
@@ -32,18 +32,9 @@ Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoPhort"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
-Definition change_marker {T'} (p' : proc T') `{@ProcMarker T p} :
+Definition change_marker {T'} (p' : proc T') `{ProcMarker T p} :
   ProcMarker p' := AProc p'.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqJQt3Qk"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqK0GVtC"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-29 08:55:08.440000.*)
+(* Auto-generated comment: At 2019-08-29 08:55:29.880000.*)
 
