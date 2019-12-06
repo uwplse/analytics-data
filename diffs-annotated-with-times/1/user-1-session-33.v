dfe81c6cd5ba77fd907cf9251e73c91a36360bc6
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:32 -0800

    user 1, session 33, part 1

--- diffs-annotated-with-times/1/user-1-session-33.v
+++ diffs-annotated-with-times/1/user-1-session-33.v
@@ -2,8 +2,6 @@ Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
-Inductive Sorted : Type :=.
+Inductive Sorted :=.
 (* Auto-generated comment: Succeeded. *)
-
-(* Auto-generated comment: At 2019-08-26 23:59:47.830000.*)
 
