384b4b281126cc45ad555bb8d3d1a89f642be8a5
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:19 -0800

    user 7, session 132, part 2

--- diffs-annotated-with-times/7/user-7-session-132.v
+++ diffs-annotated-with-times/7/user-7-session-132.v
@@ -88,7 +88,10 @@ Proof.
 (intros w1).
 exists w1.
 (intros v Hm).
+(destruct k).
+-
+(apply match_ty_ref__weak_inv in Hm; subst).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 08:18:32.050000.*)
+(* Auto-generated comment: At 2019-08-27 08:19:35.270000.*)
 
