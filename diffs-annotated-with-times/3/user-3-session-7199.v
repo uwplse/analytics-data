6c80ed4823932a81eb586a9a0783ddd55f6f4847
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:12 -0800

    user 3, session 7199, part 312

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -393,8 +393,7 @@ step_proc.
 (unfold log_size_ok; autorewrite with list; auto).
 (exists bs; intuition eauto).
 (unfold log_abstraction; intuition eauto).
-(unfold log_length_ok).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 12:10:40.550000.*)
+(* Auto-generated comment: At 2019-09-04 12:10:45.760000.*)
 
