df24c8ff9dd7f8f8c1ca2fde3bfb97c34b614e68
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:36 -0800

    user 1, session 41, part 27

--- diffs-annotated-with-times/1/user-1-session-41.v
+++ diffs-annotated-with-times/1/user-1-session-41.v
@@ -553,8 +553,9 @@ generalize dependent G'.
 (induction H).
 -
 (intros; inversion H0; subst; eauto).
-congruence.
-(* Auto-generated comment: Failed. *)
+Search -Singleton.
+Search -(Singleton _ _ = Singleton _ _).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-05 18:53:11.360000.*)
+(* Auto-generated comment: At 2019-09-05 18:55:55.190000.*)
 
