fa2e2988f4692afd9a72a970309b8b0fa949775c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:41 -0800

    user 8, session 31, part 95

--- diffs-annotated-with-times/8/user-8-session-31.v
+++ diffs-annotated-with-times/8/user-8-session-31.v
@@ -3180,7 +3180,7 @@ Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Definition HOAS_Equiv {W1} {W2} (c1 c2 : Box W1 W2) :=
   forall \207\129 b, denote_box b c1 \207\129 = denote_box b c2 \207\129.
-Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqfmAeQT"
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqJYFMeY"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 (* Auto-generated comment: Succeeded. *)
