1580d615608ef91947138c9fe30c8dd4c8fb55bd
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:46 -0800

    user 7, session 184, part 12

--- diffs-annotated-with-times/7/user-7-session-184.v
+++ diffs-annotated-with-times/7/user-7-session-184.v
@@ -105,7 +105,14 @@ admit.
 +
 subst.
 (rewrite subst_var_eq in *).
+exists (TEV X').
+split.
+reflexivity.
+(induction w'; induction t'; intros Hm'; try contradiction).
+*
+(apply match_ty_union__inv in Hm').
+(destruct Hm' as [Hm'| Hm']; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-02 07:49:32.020000.*)
+(* Auto-generated comment: At 2019-09-02 07:51:02.170000.*)
 
