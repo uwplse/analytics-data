a1a970a05d0ab9f06195a6d310e5289b7295f8fb
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:29 -0800

    user 7, session 158, part 6

--- diffs-annotated-with-times/7/user-7-session-158.v
+++ diffs-annotated-with-times/7/user-7-session-158.v
@@ -52,8 +52,13 @@ Qed.
 Lemma subst_var_neq : forall (X : id) (s : ty) (Y : id), X <> Y -> [X := s] TVar Y = TVar Y.
 Proof.
 (intros X s Y Hneq).
+(destruct (beq_id_false_iff X Y) as [_ Hid]).
+specialize (Hid Hneq).
 (simpl).
+(rewrite Hid).
+reflexivity.
+Qed.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-29 09:16:21.540000.*)
+(* Auto-generated comment: At 2019-08-29 09:17:07.190000.*)
 
