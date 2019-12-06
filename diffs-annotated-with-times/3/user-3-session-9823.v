804d1d26c3971551f2f4b32a5c3f709bdec5b165
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:50:05 -0800

    user 3, session 9823, part 16

--- diffs-annotated-with-times/3/user-3-session-9823.v
+++ diffs-annotated-with-times/3/user-3-session-9823.v
@@ -187,8 +187,11 @@ replace (diskSize (stateDisk state) - 1) with diskSize s in * by lia.
 -
 invert_abstraction.
 (step_proc; intuition eauto).
-(step_proc; intuition).
+(step_proc; intuition eauto).
+{
+(exists s; split; eauto).
+(destruct (a == diskSize s); subst).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-06 05:22:48.720000.*)
+(* Auto-generated comment: At 2019-09-06 05:22:52.630000.*)
 
