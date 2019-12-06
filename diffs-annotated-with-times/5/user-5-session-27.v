ac9cb540ca5d36da506580b5f7dfd1b90838a76d
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:42 -0800

    user 5, session 27, part 0

---
+++ diffs-annotated-with-times/5/user-5-session-27.v
@@ -0,0 +1,10 @@
+Require Import String.
+Require Import ZArith.
+Require Import List.
+Import ListNotations.
+Definition Identifier := string.
+Definition id_eq_dec := string_dec.
+(* Auto-generated comment: Succeeded. *)
+
+(* Auto-generated comment: At 2019-08-28 15:16:30.210000.*)
+
