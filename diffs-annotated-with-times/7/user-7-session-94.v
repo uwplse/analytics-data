10f1e50aea2f5c520e428c2112deafd6de211165
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:53 -0800

    user 7, session 94, part 15

--- diffs-annotated-with-times/7/user-7-session-94.v
+++ diffs-annotated-with-times/7/user-7-session-94.v
@@ -56,27 +56,10 @@ Lemma match_ty_exist__inv : forall (v : ty) (X : id) (t : ty) (k : nat), |-[ S k
 Proof.
 (intros v; induction v; intros X t k Hm; assumption).
 Qed.
-Theorem match_ty__value_type_l : forall (v t : ty) (k : nat), |-[ k] v <$ t -> value_type v.
+Theorem match_ty__value_type_l : forall (k : nat) (v t : ty), |-[ k] v <$ t -> value_type v.
 Proof.
-(intros v t).
-generalize dependent v.
-(induction t; intros v k Hm).
--
-(apply match_ty_cname__inv in Hm; subst).
-constructor.
--
-(apply match_ty_pair__inv in Hm; destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(constructor; [ eapply IHt1 | eapply IHt2 ]; eauto).
--
-(apply match_ty_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ eapply IHt1 | eapply IHt2 ]; eauto).
--
-(apply match_ty_ref__weak_inv in Hm).
-(destruct Hm as [t' Heq]; subst).
-constructor.
--
-(destruct k).
-(destruct v; contradiction).
+(induction k).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-19 09:04:31.580000.*)
+(* Auto-generated comment: At 2019-08-19 09:04:36.090000.*)
 
