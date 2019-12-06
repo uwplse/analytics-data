97348e6e2855abf044b5f74c04e4781b3339bf2b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:12 -0800

    user 7, session 19, part 228

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -115,9 +115,9 @@ Proof.
 (match goal with
  | |- | ?t1 | <= | ?t2 | =>
        assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize (H _ Hm);
-        apply match_ty_i_union__inv in H; rewrite inv_depth_union
+        apply match_ty_i_union__inv in H; rewrite inv_depth_union; destruct H as [Hm1| Hm2]
  end).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 13:33:27.810000.*)
+(* Auto-generated comment: At 2019-08-12 13:33:46.130000.*)
 
