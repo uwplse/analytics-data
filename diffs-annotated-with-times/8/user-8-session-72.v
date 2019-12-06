af3cf1ad2ea86eee2a995f907723165282c4cc7e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:33 -0800

    user 8, session 72, part 60

--- diffs-annotated-with-times/8/user-8-session-72.v
+++ diffs-annotated-with-times/8/user-8-session-72.v
@@ -1279,6 +1279,5 @@ reflexivity.
 (rewrite IHsource_symmetric; trivial).
 specialize @ancilla_free_box_valid as AV.
 (unfold valid_ancillae_box in AV).
-(do 2 (rewrite AV; [  | apply ancilla_free_X_at | type_check ])).
 (* Auto-generated comment: Succeeded. *)
 
