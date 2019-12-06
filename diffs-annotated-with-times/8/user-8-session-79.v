21e6bc4434fba8702955d5610931568ef089c740
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:55 -0800

    user 8, session 79, part 393

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -891,49 +891,7 @@ Fact Toffoli_at_spec :
   (\226\159\166 Toffoli_at n x y z \226\159\167) (\226\168\130 li) =
   \226\168\130 update_at li z (bool_to_matrix ((b1 && b2) \226\138\149 b3)).
 Admitted.
-Lemma init_at_spec :
-  forall (b : bool) (n i : nat) (l1 l2 : list (Square 2)) (A B : Square 2),
-  length l1 = i ->
-  length l2 = n - i ->
-  (forall j, Mixed_State (nth j l1 A)) ->
-  (forall j, Mixed_State (nth j l2 B)) ->
-  i < S n ->
-  (\226\159\166 init_at b n i \226\159\167) (\226\168\130 (l1 ++ l2)) == \226\168\130 (l1 ++ [bool_to_matrix b] ++ l2).
-Proof.
-(intros b n i).
-gen n.
-(induction i).
--
-(intros n l1 l2 A B L1 L2 M1 M2 Lt).
-(destruct l1; inversion L1).
-(simpl in *).
-clear L1 M1 Lt.
-(rewrite strip_one_l_in_eq).
-(rewrite Nat.sub_0_r in L2).
-(rewrite L2 in *).
-Timeout 1 About rewrite_inPar.
-Timeout 1 Print rewrite_inPar.
-Timeout 1 Print Ltac rewrite_inPar.
-Timeout 1 About restore_dims.
-Timeout 1 Print restore_dims.
-Timeout 1 Print Ltac restore_dims.
-restore_dims
- simpl; try rewrite size_ntensor; try rewrite L2; simpl; unify_pows_two; lia.
-(erewrite denote_box_compat).
-2: {
-restore_dims
- simpl; try rewrite size_ntensor; try rewrite L2; simpl; unify_pows_two; lia.
-(rewrite (kron_1_l_inv (\226\168\130 l2))).
-reflexivity.
-}
-Timeout 1 About rewrite_inPar.
-Timeout 1 Print rewrite_inPar.
-Timeout 1 Print Ltac rewrite_inPar.
-Timeout 1 About rewrite_inPar.
-Timeout 1 Print rewrite_inPar.
-Timeout 1 Print Ltac rewrite_inPar.
-(rewrite L2).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-14 14:55:44.130000.*)
+(* Auto-generated comment: At 2019-08-14 14:55:47.590000.*)
 
