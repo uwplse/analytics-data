51baf5e4ec9cf22b1ab430130981d74f1362b65c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:00 -0800

    user 7, session 101, part 55

--- diffs-annotated-with-times/7/user-7-session-101.v
+++ diffs-annotated-with-times/7/user-7-session-101.v
@@ -60,9 +60,46 @@ assumption.
 Qed.
 Lemma not_sem_sub__refeXrefX_eYrefrefY :
   (forall w1 : nat,
-   exists w2 : nat, forall (k : nat) (v : ty), |-[ w1, k] v <$ TRef (TExist vX (TRef tX)) -> |-[ w2, k] v <$ TExist vY (TRef (TRef tY))) ->
-  False Proof.
-(* Auto-generated comment: Failed. *)
+   exists w2 : nat, forall (k : nat) (v : ty), |-[ w1, k] v <$ TRef (TExist vX (TRef tX)) -> |-[ w2, k] v <$ TExist vY (TRef (TRef tY))) -> False.
+Proof.
+(intros Hcontra).
+specialize (Hcontra 1).
+(destruct Hcontra as [w Hcontra]).
+(assert (Hm : |-[ 1, 2] TRef (TExist vX (TRef tX)) <$ TRef (TExist vX (TRef tX))) by (apply match_ty_value_type__reflexive; constructor)).
+specialize (Hcontra _ _ Hm).
+clear Hm.
+(destruct w).
+-
+(apply match_ty_exist__0_inv in Hcontra).
+(apply match_ty_ref__inv in Hcontra).
+(destruct Hcontra as [t' [Heq Href]]).
+(inversion Heq; subst).
+clear Heq.
+(assert (Hm : |-[ 0, 1] TRef tX <$ TRef tX) by (apply match_ty_value_type__reflexive; constructor)).
+specialize (Href (TRef tX)).
+(destruct Href as [Href _]).
+specialize (Href Hm).
+clear Hm.
+(apply match_ty_ref__inv in Href).
+(destruct Href as [t' [Heq Href]]).
+(inversion Heq; subst).
+clear Heq.
+(assert (Hm : |-[ 0, 0] TEV vX <$ TEV vX) by (apply match_ty_value_type__reflexive; constructor)).
+specialize (Href (TEV vX)).
+(destruct Href as [Href _]).
+specialize (Href Hm).
+clear Hm.
+(simpl in Href).
+(inversion Href).
+-
+(apply match_ty_exist__inv in Hcontra).
+(destruct Hcontra as [t Hcontra]).
+(assert (Heq : [vY := t] TRef (TRef tY) = TRef (TRef t)) by reflexivity).
+(rewrite Heq in Hcontra).
+clear Heq.
+(apply match_ty_ref__inv in Hcontra).
+(destruct Hcontra as [t' [Heq Href]]).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-20 10:01:31.060000.*)
+(* Auto-generated comment: At 2019-08-20 10:01:43.960000.*)
 
