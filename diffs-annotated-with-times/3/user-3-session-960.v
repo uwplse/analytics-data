6931f1e129a55197fdb4e29f6b49977ccc2e3ba3
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:21:19 -0800

    user 3, session 960, part 126

--- diffs-annotated-with-times/3/user-3-session-960.v
+++ diffs-annotated-with-times/3/user-3-session-960.v
@@ -81,10 +81,9 @@ Function
    | _ =>
        let base := base_m2 in
        let digit := x mod S (S base_m2) in
-       exist (fun x => x < S (S base_m2)) digit _
-       :: nat_to_le base (x / base)
+       exist (fun x => x < base) digit _ :: nat_to_le base (x / base)
    end.
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-16 06:09:58.550000.*)
+(* Auto-generated comment: At 2019-08-16 06:10:07.240000.*)
 
