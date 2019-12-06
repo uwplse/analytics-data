d15ba2e38367b2c1bb208c7d3c42aadd200d4a9c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:14 -0800

    user 7, session 116, part 46

--- diffs-annotated-with-times/7/user-7-session-116.v
+++ diffs-annotated-with-times/7/user-7-session-116.v
@@ -101,8 +101,11 @@ assumption.
 +
 (simpl).
 Search -beq_id.
-(rewrite false_beq_id).
+(rewrite (false_beq_id _ _ n)).
+(rewrite (false_beq_id _ _ n0)).
+reflexivity.
+Qed.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 07:00:54.260000.*)
+(* Auto-generated comment: At 2019-08-27 07:02:04.180000.*)
 
