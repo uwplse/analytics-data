3bf1f05f2a49dae9af93f19f79a56983ed2cd6a7
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:08 -0800

    user 3, session 7199, part 160

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -267,8 +267,8 @@ Theorem get_len_abstr_ok :
      post := fun r state' => state' = state /\ r = length state;
      recovered := fun _ state' => state' = state |}) get_len recover abstr.
 Proof.
-(unfold get_len; intros).
-(* Auto-generated comment: Succeeded. *)
+(apply spec_weaken).
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 11:28:32.900000.*)
+(* Auto-generated comment: At 2019-09-04 11:28:34.710000.*)
 
