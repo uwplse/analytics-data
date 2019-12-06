406ee4089ba03f320b644174321e50c534e1073f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:08 -0800

    user 7, session 19, part 57

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -74,8 +74,9 @@ Proof.
          apply match_ty_i_union__inv in Hm; destruct Hm as [Hm1| Hm2]; [ apply match_ty_i_union_1 | apply match_ty_i_union_2 ]; auto
    end).
 -
-(destruct v; contradiction).
-(* Auto-generated comment: Succeeded. *)
+(destruct v).
+contradiction.
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 10:01:30.290000.*)
+(* Auto-generated comment: At 2019-08-12 11:02:53.600000.*)
 
