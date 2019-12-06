8c5f4bf951727943d5a004aca2ddd2990587602a
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:15 -0800

    user 8, session 65, part 150

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -984,6 +984,7 @@ Msimpl.
 Timeout 1 About id_kron.
 Timeout 1 Print id_kron.
 restore_dims.
-(rewrite id_kron').
+(rewrite id_kron' by (apply Nat.pow_nonzero; lia)).
+(rewrite id_kron' by (apply Nat.pow_nonzero; lia)).
 (* Auto-generated comment: Succeeded. *)
 
