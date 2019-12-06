9fccd889d0d087456825a48fa512728cbfc973b2
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:55 -0800

    user 7, session 202, part 6

--- diffs-annotated-with-times/7/user-7-session-202.v
+++ diffs-annotated-with-times/7/user-7-session-202.v
@@ -114,11 +114,8 @@ specialize (Hid Hneq).
 (rewrite Hid).
 reflexivity.
 Qed.
-Lemma b_subst_wf_ty : forall (X : id) (t : ty), wf_ty t -> forall s : ty, [BX := s] t = t.
-Proof.
-(intros X t).
-(induction t; intros Hwf s; try (solve [ reflexivity ])).
+Lemma wf_ty_pair__inv : forall (X : id) (t1 t2 : ty), wf_ty X (TPair t1 t2) -> wf_ty X t1 /\ wf_ty X t2.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-03 09:49:56.010000.*)
+(* Auto-generated comment: At 2019-09-03 09:50:24.060000.*)
 
