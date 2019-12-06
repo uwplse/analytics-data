c0e790f36398b94a904b3f6d0e25eb9dc4c043e1
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:10 -0800

    user 7, session 19, part 138

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -109,9 +109,9 @@ Proof.
 (match goal with
  | |- | ?t1 | = | ?t2 | =>
        assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
-        (H _ _ Hv)
+        (H 0 _ Hv)
  end).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 12:26:04.650000.*)
+(* Auto-generated comment: At 2019-08-12 12:26:57.530000.*)
 
