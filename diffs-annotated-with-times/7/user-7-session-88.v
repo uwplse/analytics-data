983ed0a67fef26673835304e4a6c8ce0fd842b3c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:49 -0800

    user 7, session 88, part 4

--- diffs-annotated-with-times/7/user-7-session-88.v
+++ diffs-annotated-with-times/7/user-7-session-88.v
@@ -551,8 +551,7 @@ clear IHk' IHt.
 (apply le_S_n in Htk').
 (split; intros Hm; apply match_ty_i_ref__inv in Hm; destruct Hm as [t' [Heq Href]]; subst; simpl; intros v; pose proof (Href v) as Hrefv).
 *
-(assert (Hdepeq : | t' | = | t |) by apply (sem_eq_k_i__inv_depth_eq_2 _ _ _ Htk Href)).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-16 14:49:07.110000.*)
+(* Auto-generated comment: At 2019-08-16 14:49:17.780000.*)
 
