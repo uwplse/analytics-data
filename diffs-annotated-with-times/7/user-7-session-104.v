d938790dd00ea2008e5aebee169e8a4392a2bd52
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:04 -0800

    user 7, session 104, part 74

--- diffs-annotated-with-times/7/user-7-session-104.v
+++ diffs-annotated-with-times/7/user-7-session-104.v
@@ -47,70 +47,11 @@ Proof.
 exists tx.
 assumption.
 Qed.
-Reserved Notation "'|' t '|'" (at level 20).
-Fixpoint inv_depth (t : ty) :=
-  match t with
-  | TCName _ => 0
-  | TPair t1 t2 => Nat.max (| t1 |) (| t2 |)
-  | TUnion t1 t2 => Nat.max (| t1 |) (| t2 |)
-  | TRef t' => 1 + | t' |
-  | TExist _ t' => | t' |
-  | TVar _ => 0
-  | TEV _ => 0
-  end
-where "'|' t '|'" := (inv_depth t) : btjt_scope.
-Lemma max_inv_depth_le__inv : forall (t1 t2 : ty) (k : nat), Nat.max (| t1 |) (| t2 |) <= k -> | t1 | <= k /\ | t2 | <= k.
+Lemma not_sem_sub__refint_refflt : ~ ||- [TRef tint]<= [TRef tflt].
 Proof.
-(intros t1 t2 k Hle).
-(split; [ eapply Nat.max_lub_l | eapply Nat.max_lub_r ]; eassumption).
-Qed.
-Lemma match_ty__inv_depth : forall (w k : nat) (v t : ty), | v | <= k -> |-[ k, w] v <$ t -> | v | <= | t |.
-Proof.
-(intros w k).
-(induction k).
-(intros v t Hdep Hm).
-(inversion Hdep; subst).
-(rewrite H0).
-(apply le_0_n).
-(intros v t).
-generalize dependent v.
-(induction t; intros v Hdep Hm).
--
-(apply match_ty_cname__inv in Hm; subst).
-constructor.
--
-(apply match_ty_pair__inv in Hm).
-(destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
-(simpl).
-(destruct (max_inv_depth_le__inv _ _ _ Hdep) as [Hdep1 Hdep2]).
-(apply Nat.max_le_compat; [ apply IHt1 | apply IHt2 ]; assumption).
--
-(apply match_ty_union__inv in Hm).
-(destruct Hm as [Hm| Hm]; [ apply Nat.le_trans with (| t1 |) | apply Nat.le_trans with (| t2 |) ]; auto).
-(apply Nat.le_max_l).
-(apply Nat.le_max_r).
--
-(apply match_ty_ref__inv in Hm).
-(destruct Hm as [t' [Heq Href]]; subst).
-admit.
--
-(destruct w).
-(apply match_ty_exist__0_inv in Hm).
-contradiction.
-(apply match_ty_exist__inv in Hm).
-(destruct Hm as [tx Hmx]).
-Abort.
-Lemma sem_eq_k_inv_depth__exists_not : forall (k : nat) (t : ty), | t | <= k -> exists t' : ty, ~ ||-[ k][t']= [t].
-Proof.
-(intros k t).
-exists (TRef t).
 (intros Hcontra).
-(destruct Hcontra as [Hsem1 Hsem2]).
-(pose proof (Hsem1 1) as Hm1).
-(destruct Hm1 as [w2 Hm1]).
-(assert (Hm : |-[ k, 1] TRef t <$ TRef t) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Hm1 _ Hm).
+specialize (Hcontra 1).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 13:26:06.460000.*)
+(* Auto-generated comment: At 2019-08-20 13:26:17.580000.*)
 
