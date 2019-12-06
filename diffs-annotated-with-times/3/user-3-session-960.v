b2628bea28f6a7335b826b5cd0b5551e82173a17
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:21:17 -0800

    user 3, session 960, part 30

--- diffs-annotated-with-times/3/user-3-session-960.v
+++ diffs-annotated-with-times/3/user-3-session-960.v
@@ -69,24 +69,12 @@ Class GoModel : Type :={byte : Type;
 Search -"endian".
 Definition byte_nat := {x : nat | x < 256}.
 #[program]
-Fixpoint nat_to_le (x : nat) {measure x : list byte_nat :=
+Fixpoint nat_to_le base (x : nat) {measure x : list {x : nat | x < S base} :=
   match x with
   | 0 => nil
-  | _ => exist _ (x mod 256) _ :: nat_to_le (x / 256)
+  | _ => exist _ (x mod S base) _ :: nat_to_le (x / S base)
   end.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqP1Pyki"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqoHRNyo"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
-Next Obligation.
-(simpl).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-16 05:31:24.420000.*)
+(* Auto-generated comment: At 2019-08-16 05:32:01.690000.*)
 
