07c353ece3c88975fec89300e3191a3374fa061f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:43 -0800

    user 5, session 33, part 12

--- diffs-annotated-with-times/5/user-5-session-33.v
+++ diffs-annotated-with-times/5/user-5-session-33.v
@@ -128,8 +128,8 @@ Proof.
 (intros).
 (destruct (L.(value_eq_dec) (L.(eval) env t1) (L.(eval) env t2))).
 -
-left.
+idtac.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-01 08:54:27.380000.*)
+(* Auto-generated comment: At 2019-09-01 08:54:31.730000.*)
 
