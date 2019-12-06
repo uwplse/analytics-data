bb138015fd6fe959030b8fa2eae76890ccd0cc6a
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:50 -0800

    user 7, session 192, part 42

--- diffs-annotated-with-times/7/user-7-session-192.v
+++ diffs-annotated-with-times/7/user-7-session-192.v
@@ -47,8 +47,35 @@ Proof.
 (remember (gen_fresh (IdSet.union (FV s) (IdSet.add X (FV t)))) as z).
 (apply match_ty_exist__inv in Hm).
 (destruct Hm as [ti Hm]).
+(apply match_ty_exist).
 exists ti.
+Abort.
+Lemma build_v_full :
+  forall (X X' : id) (tx : ty) (w : nat) (t v : ty),
+  |-[ w] v <$ [X := tx] t ->
+  exists v' : ty,
+    |-[ w] v' <$ [X := TVar X'] t /\
+    (forall (w' : nat) (t' : ty), |-[ w'] v' <$ t' -> (fresh_in_ty X' t' -> |-[ w'] v <$ t') /\ (free_in_ty X' t' -> |-[ w'] v <$ [X' := tx] t')).
+Proof.
+(intros X X' tx).
+(induction w; induction t; intros v Hm).
+-
+(rewrite subst_cname in *).
+exists v.
+split.
+assumption.
+(induction w'; induction t'; intros Hm'; try (solve [ destruct v; contradiction || tauto ])).
++
+(rewrite subst_union).
+(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
+  destruct IHt' as [IHt'a IHt'b]; split; intros HX').
+*
+(destruct (fresh_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
+(apply match_ty_union_1; auto).
+*
+(destruct (either_free_or_fresh_in_ty X' t'1) as [HXt'1| HXt'1]).
+(apply match_ty_union_1; auto).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-02 09:16:20.530000.*)
+(* Auto-generated comment: At 2019-09-02 09:16:23.790000.*)
 
