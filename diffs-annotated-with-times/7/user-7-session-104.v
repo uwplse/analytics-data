35f8bc329d0534d63a5cd66c6dcd9292c7056a51
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:03 -0800

    user 7, session 104, part 16

--- diffs-annotated-with-times/7/user-7-session-104.v
+++ diffs-annotated-with-times/7/user-7-session-104.v
@@ -73,8 +73,7 @@ clear Heq.
 (destruct Href as [Href _]).
 specialize (Href 1).
 (destruct Href as [w2 Hsem]).
-(assert (Hm : |-[ 1, w2] TExist vX (TRef tX) <$ TRef t) by (apply match_ty_value_type__reflexive; constructor)).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 12:21:29.010000.*)
+(* Auto-generated comment: At 2019-08-20 12:21:43.190000.*)
 
