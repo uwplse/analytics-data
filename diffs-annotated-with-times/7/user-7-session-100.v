89714eb5a44a1bd1a5fd616fcacf102de912d1d5
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:57 -0800

    user 7, session 100, part 11

--- diffs-annotated-with-times/7/user-7-session-100.v
+++ diffs-annotated-with-times/7/user-7-session-100.v
@@ -81,8 +81,8 @@ Proof.
 Qed.
 Lemma match_ty_var__inv : forall (v : ty) (X : id) (w k : nat), |-[ w, k] v <$ TVar X -> v = TEV X.
 Proof.
-(intros v; induction v; intros X w k Hm; destruct w; destruct k; reflexivity).
+(intros v; induction v; intros X w k Hm; destruct w; destruct k).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 08:01:57.110000.*)
+(* Auto-generated comment: At 2019-08-20 08:02:07.730000.*)
 
