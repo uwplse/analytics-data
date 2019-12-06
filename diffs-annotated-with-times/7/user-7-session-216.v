f398c5050187611d230faa6f58873ec3fee63f94
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:03 -0800

    user 7, session 216, part 22

--- diffs-annotated-with-times/7/user-7-session-216.v
+++ diffs-annotated-with-times/7/user-7-session-216.v
@@ -325,6 +325,13 @@ Proof.
 (intros).
 reflexivity.
 Qed.
+Lemma f_subst_fvar_eq : forall (X : id) (s : ty), [FX := s] TFVar X = s.
+Proof.
+(intros).
+(simpl).
+(rewrite <- beq_id_refl).
+reflexivity.
+Qed.
 Lemma f_subst_fvar_neq : forall (X : id) (s : ty) (Y : id), X <> Y -> [FX := s] TFVar Y = TFVar Y.
 Proof.
 (intros X s Y Hneq).
@@ -410,10 +417,6 @@ tauto.
 -
 (rewrite f_subst_exist).
 (unfold wf_ty in *; simpl in *).
-Search -IdSet.Empty.
-Search -IdSet.Empty.
-Search -IdSet.remove.
-Search -IdSet.In.
 (destruct (IdSetProps.In_dec i (FBV t))).
 admit.
 admit.
@@ -422,7 +425,10 @@ admit.
 +
 subst.
 (rewrite f_subst_fvar_eq).
+assumption.
++
+(rewrite f_subst_fvar_neq).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 10:06:38.810000.*)
+(* Auto-generated comment: At 2019-09-04 10:07:03.260000.*)
 
