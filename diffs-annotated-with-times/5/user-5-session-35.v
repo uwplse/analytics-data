46b05d0ee8c263dafcbb2ca0225ad45cbf9441f2
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:44 -0800

    user 5, session 35, part 13

--- diffs-annotated-with-times/5/user-5-session-35.v
+++ diffs-annotated-with-times/5/user-5-session-35.v
@@ -224,10 +224,14 @@ intro x.
         (eval L env
            (Choose x
               (And (In (Var x) Ints) (Eq (Int 6) (Times (Var x) (Int 2)))))))
-     (Eq (Int 6) (Times (Var x) (Int 2))) = L.(eval) env (Bool true))).
+     (And (In (Var x) Ints) (Eq (Int 6) (Times (Var x) (Int 2)))) =
+   L.(eval) env (Bool true))).
 {
 (apply evalChoose).
+exists (eval L env (Int 3)).
+(erewrite evalBoolConst).
+apply -> evalEqTrue.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-01 09:42:12.060000.*)
+(* Auto-generated comment: At 2019-09-01 09:42:30.600000.*)
 
