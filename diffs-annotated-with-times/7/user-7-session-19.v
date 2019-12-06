6fd77aab21b565d8d1dea6297d34ef6babfed4e3
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:21 -0800

    user 7, session 19, part 592

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -399,12 +399,12 @@ subst.
 constructor.
 (apply Hsem).
 }
-(unfold sem_sub_i in Hsem).
-(assert (Hm : |-[ kmax] v <$ v) by (apply match_ty_i__reflexive; assumption)).
-specialize (Hsem _ _ Hm).
-(apply match_ty_i_union__inv in Hsem).
-(destruct Hsem; [ left | right ]; unfold sem_sub_i; intros k v' Hm').
+(assert (Hdeple1 : | ta | <= kmax)).
+{
+subst.
+(simpl).
+(apply Nat.max_lub_l).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 12:07:37.630000.*)
+(* Auto-generated comment: At 2019-08-13 12:07:44.870000.*)
 
