5d0eb0ac1e8a690febe1ab9eb4bc705e82f3b37b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:37 -0800

    user 7, session 173, part 14

--- diffs-annotated-with-times/7/user-7-session-173.v
+++ diffs-annotated-with-times/7/user-7-session-173.v
@@ -123,8 +123,8 @@ Notation "'[' x ':=' s ']' t" := (subst x s t) (at level 30) : btjt_scope.
 Lemma triv : forall (X : id) (s : ty) (t1 t2 : ty), [X := s] TPair t1 t2 = TPair ([X := s] t1) ([X := t2] t2).
 Proof.
 (intros X s t1 t2).
-(simpl).
+reflexivity.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-29 14:51:09.880000.*)
+(* Auto-generated comment: At 2019-08-29 14:51:12.870000.*)
 
