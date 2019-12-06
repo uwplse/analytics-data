4acf054b47b41f31cc7dffc8578b65625f037388
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:16 -0800

    user 7, session 19, part 391

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -202,8 +202,8 @@ Proof.
 -
 (assert (Hvp : value_type (TPair t1 t2))).
 constructor.
-baseTactics.solve_by_inverts.
+BaseTactics.solve_by_inverts.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 07:33:23.270000.*)
+(* Auto-generated comment: At 2019-08-13 07:33:48.150000.*)
 
