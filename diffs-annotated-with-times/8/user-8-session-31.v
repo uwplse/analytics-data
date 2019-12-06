2b1b39b4b7141762e1c682d65b21fd9f65d8b006
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:35 -0800

    user 8, session 31, part 2

--- diffs-annotated-with-times/8/user-8-session-31.v
+++ diffs-annotated-with-times/8/user-8-session-31.v
@@ -2185,11 +2185,15 @@ Lemma denote_gate_circuit :
 Proof.
 (intros).
 (unfold denote_circuit).
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coq7RSfdD"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
 (simpl; fold_denotation).
 replace (process_gate g p1 \206\147) with
  (process_gate_pat g p1 \206\147, process_gate_state g p1 \206\147)
  by (symmetry; apply surjective_pairing).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-09 14:02:45.700000.*)
+(* Auto-generated comment: At 2019-08-09 14:02:48.320000.*)
 
