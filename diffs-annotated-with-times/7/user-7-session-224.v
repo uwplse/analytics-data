c06bc9c5d79d30b9c8479909a9d937e609380e1e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:07 -0800

    user 7, session 224, part 29

--- diffs-annotated-with-times/7/user-7-session-224.v
+++ diffs-annotated-with-times/7/user-7-session-224.v
@@ -41,7 +41,28 @@ subst.
 (rewrite b_subst_exist_neq in Hm; try assumption).
 (apply match_ty_exist__0_inv in Hm).
 contradiction.
-(* Auto-generated comment: Failed. *)
+-
+(destruct (beq_idP X i)).
++
+subst.
+(rewrite b_subst_bvar_eq in *).
+exists (TEV X').
+split.
+reflexivity.
+(induction w'; induction t'; intros Hm'; try (solve [ destruct v; contradiction || tauto ])).
+*
+(rewrite f_subst_union).
+(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
+  destruct IHt' as [IHt'a IHt'b]; split; intros HX').
+{
+(destruct (not_f_free_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
+(apply match_ty_union_1; auto).
+}
+{
+(destruct (f_free_in_ty__dec X' t'1) as [HXt'1| HXt'1]).
+{
+(apply match_ty_union_1; auto).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-05 13:13:11.580000.*)
+(* Auto-generated comment: At 2019-09-05 13:16:00.170000.*)
 
