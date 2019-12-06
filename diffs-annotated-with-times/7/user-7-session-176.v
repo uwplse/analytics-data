c56d08f313de592e404f1b09a82f0b6c6d08ee23
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:41 -0800

    user 7, session 176, part 6

--- diffs-annotated-with-times/7/user-7-session-176.v
+++ diffs-annotated-with-times/7/user-7-session-176.v
@@ -143,9 +143,9 @@ Lemma match_ty__match_ty_subst_int : forall (X : id) (w : nat) (t v : ty), |-[ w
 Proof.
 (intros X; induction w; induction t; intros v).
 (intros Hm).
-(apply match_ty_cname__inv in Hm).
 exists v.
+assumption.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-30 06:51:09.510000.*)
+(* Auto-generated comment: At 2019-08-30 06:51:22.890000.*)
 
