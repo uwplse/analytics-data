43a2bf28326ede462a8810c60145418664f2204e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:10 -0800

    user 7, session 19, part 127

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -95,8 +95,19 @@ Proof.
 -
 (destruct k; reflexivity).
 -
-(apply match_ty_i_pair; tauto).
+(apply match_ty_i_pair; auto).
+-
+(destruct k).
+constructor.
+(simpl).
+tauto.
+Qed.
+Lemma match_ty_i_eq__inv_depth_eq :
+  forall t t' : ty, (forall (k : nat) (v : ty), value_type v -> |-[ k] v <$ t <-> |-[ k] v <$ t') -> | t | = | t' |.
+Proof.
+(induction t; induction t'; intros H).
+reflexivity.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 12:17:17.310000.*)
+(* Auto-generated comment: At 2019-08-12 12:18:12.990000.*)
 
