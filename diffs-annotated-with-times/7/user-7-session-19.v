f40516b969961535898bc4ba0cd42cb9244bbc70
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:10 -0800

    user 7, session 19, part 137

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -107,9 +107,11 @@ Lemma match_ty_i_eq__inv_depth_eq :
 Proof.
 (induction t; induction t'; intros H; try reflexivity).
 (match goal with
- | |- | ?t1 | = | ?t2 | => assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption)
+ | |- | ?t1 | = | ?t2 | =>
+       assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
+        (H _ _ Hv)
  end).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 12:25:22.120000.*)
+(* Auto-generated comment: At 2019-08-12 12:26:04.650000.*)
 
