e907a5bd05e5f6bc89be76b73285f4ed6c96c636
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:48 -0800

    user 7, session 85, part 4

--- diffs-annotated-with-times/7/user-7-session-85.v
+++ diffs-annotated-with-times/7/user-7-session-85.v
@@ -123,7 +123,9 @@ Proof.
 (apply mk_nf__sub_d_r; assumption).
 (apply nf_sem_sub_i__sub_d).
 (apply mk_nf__in_nf).
-(* Auto-generated comment: Failed. *)
+(apply sem_sub_i__trans with t1).
+(apply mk_nf__sem_sub_i1).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-16 14:44:05.790000.*)
+(* Auto-generated comment: At 2019-08-16 14:44:08.480000.*)
 
