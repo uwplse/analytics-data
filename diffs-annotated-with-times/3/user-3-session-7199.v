94007e4fb2238f0705c0662eef903abb88ce14d1
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:11 -0800

    user 3, session 7199, part 258

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -341,8 +341,8 @@ step_proc.
 (eapply proc_spec_weaken; eauto).
 (unfold spec_impl; simpl; intuition).
 (exists (a' ++ [a]); intuition eauto).
-(simpl).
+(rewrite app_length).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 11:59:54.510000.*)
+(* Auto-generated comment: At 2019-09-04 12:00:01.350000.*)
 
