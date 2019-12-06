182c812733aa21ebc0cb4d218161c52f4a091d82
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:29:17 -0800

    user 3, session 3474, part 0

---
+++ diffs-annotated-with-times/3/user-3-session-3474.v
@@ -0,0 +1,22 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq08XiB1"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Import Proc Abstraction.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqkikpWJ"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqimR7MN"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
+(* Auto-generated comment: Succeeded. *)
+
+(* Auto-generated comment: At 2019-08-29 07:13:22.270000.*)
+
