877a01b35956181f4bb8911d455048988229032a
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:04 -0800

    user 7, session 104, part 63

--- diffs-annotated-with-times/7/user-7-session-104.v
+++ diffs-annotated-with-times/7/user-7-session-104.v
@@ -82,9 +82,9 @@ constructor.
 (apply match_ty_pair__inv in Hm).
 (destruct Hm as [v1 [v2 [Heq [Hm1 Hm2]]]]; subst).
 (simpl).
-(destruct (max_inv_depth_le__inv v1 v2 (S k)) as [Hdep1 Hdep2]).
+(destruct (max_inv_depth_le__inv _ _ _ Hdep) as [Hdep1 Hdep2]).
 (apply Nat.max_le_compat; [ apply IHt1 | apply IHt2 ]; try assumption).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 13:16:59.270000.*)
+(* Auto-generated comment: At 2019-08-20 13:17:32.110000.*)
 
