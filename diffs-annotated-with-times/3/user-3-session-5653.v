9fa73ca2f1a187f38f0bfd5000bf32e5a0a56a1b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:37:16 -0800

    user 3, session 5653, part 2

--- diffs-annotated-with-times/3/user-3-session-5653.v
+++ diffs-annotated-with-times/3/user-3-session-5653.v
@@ -7,18 +7,7 @@ Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Require Import POCS.
 Require Import LogAPI.
-Require Import OneDiskAPI.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqJHYqdf"
-Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Add Search Blacklist "Raw" "Proofs".
-Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqLuR5ez"
-SearchPattern _.
-Remove Search Blacklist "Raw" "Proofs".
-Unset Search Output Name Only.
-Timeout 1 Print LoadPath.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 05:54:54.330000.*)
+(* Auto-generated comment: At 2019-09-04 05:56:53.670000.*)
 
