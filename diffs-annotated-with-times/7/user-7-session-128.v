4d875215f34c76e59d2e9a314d79553856addfa1
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:18 -0800

    user 7, session 128, part 16

--- diffs-annotated-with-times/7/user-7-session-128.v
+++ diffs-annotated-with-times/7/user-7-session-128.v
@@ -91,8 +91,8 @@ Proof.
 -
 (assert (Hmp : |-[ k, w1'] TPair v v2 <$ TPair t1 t2)).
 {
-(apply match_ty_pair; eapply match_ty__ge_w; try eassumption; subst; [ apply Nat.le_max_l | apply Nat.le_max_r ]).
+(apply match_ty_pair; eapply match_ty__ge_w; try eassumption; subst).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-27 08:05:00.510000.*)
+(* Auto-generated comment: At 2019-08-27 08:05:06.570000.*)
 
