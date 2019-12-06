afbaa5e43c4656994ebc5492bd5a8d07c45cc65d
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:12 -0800

    user 7, session 19, part 210

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -101,18 +101,8 @@ constructor.
 (destruct Hm as [t' Heq]; subst).
 constructor.
 Qed.
-Lemma aaa : forall (k : nat) (t t' : ty), (forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t') -> | t | <= | t' |.
-Proof.
-(induction k; induction t; induction t'; intros H; try (solve [ simpl; constructor ]);
-  try (solve
-   [ match goal with
-     | |- | ?t1 | <= | ?t2 | =>
-           (assert (Hv : value_type t1) by constructor; assert (Hm : |-[ 0] t1 <$ t1) by (apply match_ty_i__reflexive; assumption); specialize
-             (H _ Hm); contradiction) ||
-             (assert (Hv : value_type t2) by constructor; assert (Hm : |-[ 0] t2 <$ t2) by (apply match_ty_i__reflexive; assumption); specialize
-               (H _ Hm); contradiction)
-     end ])).
+Lemma aaa : forall (k : nat) (t t' : ty), forall v : ty, |-[ k] v <$ t -> |-[ k] v <$ t' -> | t | <= | t' |.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-12 13:26:03.040000.*)
+(* Auto-generated comment: At 2019-08-12 13:26:54.210000.*)
 
