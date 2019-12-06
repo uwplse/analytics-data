dcfb6f9ac0e8e643c6de3d736f6d947662191c1e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:17:36 -0800

    user 1, session 41, part 42

--- diffs-annotated-with-times/1/user-1-session-41.v
+++ diffs-annotated-with-times/1/user-1-session-41.v
@@ -555,8 +555,8 @@ generalize dependent G'.
 (induction H).
 -
 (intros; inversion H0; subst; eauto).
-all: (try apply singleton_eq in H1).
-(* Auto-generated comment: Succeeded. *)
+all: (try apply singleton_eq in H1; congruence).
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-06 16:05:08.820000.*)
+(* Auto-generated comment: At 2019-09-06 16:05:29.670000.*)
 
