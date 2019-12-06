3aef56635abf76bf4c657a32e86589f15e11c5f5
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:05 -0800

    user 7, session 219, part 13

--- diffs-annotated-with-times/7/user-7-session-219.v
+++ diffs-annotated-with-times/7/user-7-session-219.v
@@ -144,8 +144,9 @@ reflexivity.
 +
 (rewrite b_subst_exist_neq in *; try assumption).
 (apply match_ty_exist__inv in Hm).
-(destruct Hm as [ti Hm]).
+(destruct Hm as [ti [Hwfi Hm]]).
+(rewrite b_subst_neq__permute in Hm).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 13:50:49.900000.*)
+(* Auto-generated comment: At 2019-09-04 13:52:54.640000.*)
 
