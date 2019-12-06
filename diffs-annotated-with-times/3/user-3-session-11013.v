dab631f056c4acd0d257f0053ebcc7bb5504e93a
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:54:07 -0800

    user 3, session 11013, part 0

---
+++ diffs-annotated-with-times/3/user-3-session-11013.v
@@ -0,0 +1,12 @@
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coq2hhjfQ"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+Require Export LF.Induction.
+(* Auto-generated comment: Succeeded. *)
+
+(* Auto-generated comment: At 2019-09-06 09:55:32.660000.*)
+
