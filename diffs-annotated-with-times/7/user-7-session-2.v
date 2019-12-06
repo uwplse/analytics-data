1c9263f69ed19bcc9c8fb8a6a99cc72aed7c4019
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:50 -0800

    user 7, session 2, part 28

--- diffs-annotated-with-times/7/user-7-session-2.v
+++ diffs-annotated-with-times/7/user-7-session-2.v
@@ -68,8 +68,8 @@ Check unite_pairs_union_t.
 (intros Hnf1' Hnf2 Hn2' Hsub1 Hsub2).
 (rewrite (unite_pairs_union_t t1 t0 t2)).
 (apply sub_r_nf_union_l__inv in Hsub1; try assumption).
-(constructor; [ apply SR_UnionR1 | apply SR_UnionR2 ]; tauto).
+constructor.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-06 12:56:31.200000.*)
+(* Auto-generated comment: At 2019-08-06 12:57:00.490000.*)
 
