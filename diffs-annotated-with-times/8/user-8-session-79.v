a77c6aa2f6ef83a5f938bd8f7e9db70ed5ea3636
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:58 -0800

    user 8, session 79, part 484

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -964,7 +964,8 @@ show_dimensions.
 (repeat rewrite app_length in *).
 (simpl in *).
 replace (length l1 + S (length l2)) with S n in * by lia.
+(simpl in *).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-14 18:07:35.830000.*)
+(* Auto-generated comment: At 2019-08-14 18:07:39.950000.*)
 
