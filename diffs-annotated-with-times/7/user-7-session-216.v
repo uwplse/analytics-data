b2f2cd78e0c1c8fcaecfeab81b7636637865f594
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:58:03 -0800

    user 7, session 216, part 24

--- diffs-annotated-with-times/7/user-7-session-216.v
+++ diffs-annotated-with-times/7/user-7-session-216.v
@@ -427,8 +427,8 @@ subst.
 (rewrite f_subst_fvar_eq).
 assumption.
 +
-(rewrite f_subst_fvar_neq; try assumption).
+(rewrite f_subst_fvar_neq; assumption).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 10:07:42.360000.*)
+(* Auto-generated comment: At 2019-09-04 10:07:51.200000.*)
 
