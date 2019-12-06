6347de11743eb8ca52c7849007f87788c43dd5c9
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:48 -0800

    user 7, session 189, part 3

--- diffs-annotated-with-times/7/user-7-session-189.v
+++ diffs-annotated-with-times/7/user-7-session-189.v
@@ -17,8 +17,13 @@ Qed.
 Lemma free_in_ty__decidable : forall (X : id) (t : ty), Decidable.decidable (free_in_ty X t).
 Proof.
 (intros X t).
-(unfold free).
+(unfold free_in_ty).
+Search -IdSet.In.
+(apply IdSetProps.Dec.MSetDecideAuxiliary.dec_In).
+Qed.
+Lemma fresh_in_ty__decidable : forall (X : id) (t : ty), Decidable.decidable (fresh_in_ty X t).
+Proof.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-02 08:48:14.310000.*)
+(* Auto-generated comment: At 2019-09-02 08:49:34.820000.*)
 
