50607f746b42e5521ade59a6d18a1f214674048b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:47 -0800

    user 7, session 186, part 15

--- diffs-annotated-with-times/7/user-7-session-186.v
+++ diffs-annotated-with-times/7/user-7-session-186.v
@@ -28,8 +28,10 @@ split.
 assumption.
 (induction w'; induction t'; intros Hm'; try (solve [ destruct v; contradiction || tauto ])).
 +
-(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']).
+(apply match_ty_union__inv in Hm'; destruct Hm' as [Hm'| Hm']; [ pose proof IHt'1 as IHt' | pose proof IHt'2 as IHt' ]; specialize (IHt' Hm');
+  destruct IHt' as [IHt'a IHt'b]; split).
+(intros HX'; destruct (fresh_in_ty_union__inv _ _ _ HX') as [HX'1 HX'2]).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-02 08:34:38.950000.*)
+(* Auto-generated comment: At 2019-09-02 08:34:44.140000.*)
 
