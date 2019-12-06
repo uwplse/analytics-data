1c52d22204682c46f5c41a56601e20e3d0e3ce86
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:50:23 -0800

    user 3, session 9904, part 82

--- diffs-annotated-with-times/3/user-3-session-9904.v
+++ diffs-annotated-with-times/3/user-3-session-9904.v
@@ -127,8 +127,8 @@ exfalso.
 intuition.
 +
 (exists (n0 :: s); intuition).
-(lia right; intuition congruence).
-(* Auto-generated comment: Failed. *)
+(right; intuition congruence).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-06 05:44:14.620000.*)
+(* Auto-generated comment: At 2019-09-06 05:44:16.660000.*)
 
