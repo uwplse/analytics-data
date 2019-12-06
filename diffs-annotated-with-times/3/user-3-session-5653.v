a7a86b236d5d0306c0a496fa8b58889506ba2c30
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:37:16 -0800

    user 3, session 5653, part 3

--- diffs-annotated-with-times/3/user-3-session-5653.v
+++ diffs-annotated-with-times/3/user-3-session-5653.v
@@ -4,10 +4,5 @@ Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Private_" "_subproof".
 Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
-Add Search Blacklist "Private_" "_subproof".
-Require Import POCS.
-Require Import LogAPI.
 (* Auto-generated comment: Succeeded. *)
-
-(* Auto-generated comment: At 2019-09-04 05:56:53.670000.*)
 
