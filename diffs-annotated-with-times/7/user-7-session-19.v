b6bb68465f30480f10e4f52b335e30f61d6002df
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:17 -0800

    user 7, session 19, part 443

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -361,9 +361,11 @@ Qed.
 Close Scope btjmdeq_scope.
 Theorem sem_sub_i__sem_sub_deq : forall t1 t2 : ty, (||- [t1]<= [t2])%btjmi -> (||- [t1]<= [t2])%btjmdeq.
 Proof.
-(intros ta; induction ta; intros tb; induction tb; intros Hsem; pose proof Hsem as Hsem'; unfold sem_sub_i, sem_sub_k_i in Hsem';
-  unfold sem_sub_deq, sem_sub_k_deq).
+(intros ta; induction ta; intros tb; induction tb; intros Hsem).
+Show 3.
+16: {
+Abort.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 08:07:57.250000.*)
+(* Auto-generated comment: At 2019-08-13 08:09:18.260000.*)
 
