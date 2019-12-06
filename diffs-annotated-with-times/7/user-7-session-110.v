1c996e9ef7154f88fb91973fbe34bb8b5ce82df6
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:11 -0800

    user 7, session 110, part 27

--- diffs-annotated-with-times/7/user-7-session-110.v
+++ diffs-annotated-with-times/7/user-7-session-110.v
@@ -172,8 +172,24 @@ Lemma match_ty__transitive_on_value_type :
 Proof.
 (induction w; intros v1 v2 t3 Hv2; generalize dependent t3; generalize dependent v1; induction Hv2).
 -
-(intros v1 t3 k w Hm1 Hm2).
+(intros v1 t3 k Hm1 Hm2).
+(apply match_ty_cname__inv in Hm1; subst).
+assumption.
+-
+(intros v0 t3 k Hm1 Hm2).
+(apply match_ty_pair__inv in Hm1).
+(destruct Hm1 as [pv11 [pv12 [Heq [Hmpv11 Hmpv12]]]]; subst).
+(induction t3; try (solve [ destruct k; contradiction ])).
++
+(apply match_ty_pair__inv in Hm2).
+(destruct Hm2 as [pv21 [pv22 [Heq [Hmpv21 Hm22]]]]).
+(inversion Heq; subst).
+auto using match_ty_pair.
++
+(apply match_ty_union__inv in Hm2).
+(destruct Hm2; [ apply match_ty_union_1 | apply match_ty_union_2 ]; tauto).
++
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-26 08:06:20.290000.*)
+(* Auto-generated comment: At 2019-08-26 08:07:01.320000.*)
 
