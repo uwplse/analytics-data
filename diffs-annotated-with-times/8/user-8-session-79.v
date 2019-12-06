3ceac9408b8c19381b31847007a83e78b051d5ed
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:50 -0800

    user 8, session 79, part 258

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -653,8 +653,7 @@ Proof.
 show_dimensions.
 (intros).
 (simpl).
-specialize (kron_1_l (\226\168\130 l2)) as KL.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-14 13:30:22.430000.*)
+(* Auto-generated comment: At 2019-08-14 13:30:33.760000.*)
 
