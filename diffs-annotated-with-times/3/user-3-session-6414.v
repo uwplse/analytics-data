833f0ba3dbaf3e005585fd977967436ed8c389e7
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:39:44 -0800

    user 3, session 6414, part 106

--- diffs-annotated-with-times/3/user-3-session-6414.v
+++ diffs-annotated-with-times/3/user-3-session-6414.v
@@ -116,8 +116,9 @@ Theorem log_length_ok_nil d b :
   diskGet d 0 = Some b -> block_to_addr b = 0 -> log_length_ok d nil.
 Proof.
 (unfold log_length_ok; intros).
-(rewrite H in *).
+(rewrite H in H1; simpl in H1).
+auto.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 10:38:19.020000.*)
+(* Auto-generated comment: At 2019-09-04 10:38:22.150000.*)
 
