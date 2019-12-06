3700cd964e47d544cca0a9901e229a3c007bd238
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:01 -0800

    user 7, session 16, part 71

--- diffs-annotated-with-times/7/user-7-session-16.v
+++ diffs-annotated-with-times/7/user-7-session-16.v
@@ -401,9 +401,9 @@ right.
         subst
  end).
 (match goal with
- | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto || (apply mk_nf_nf__equal; assumption)
+ | IHHcontra:context [ _ -> False ] |- False => apply IHHcontra; try tauto || (apply mk_nf_nf__equal; assumption) || apply mk_nf__in_nf
  end).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-09 12:08:40.950000.*)
+(* Auto-generated comment: At 2019-08-09 12:10:24.190000.*)
 
