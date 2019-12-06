dee5d277a17b97896bf934dd84081635f9eb51cf
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:14 -0800

    user 7, session 19, part 305

--- diffs-annotated-with-times/7/user-7-session-19.v
+++ diffs-annotated-with-times/7/user-7-session-19.v
@@ -154,8 +154,9 @@ Proof.
           assert (Hmv : |-[ k] TCName c <$ TCName c) by (apply match_ty_i__reflexive; assumption); specialize (Hsem _ Hvv Hmv);
           apply match_ty_i_union__inv in Hsem; destruct Hsem; [ left | right ]; unfold sem_sub_k_i; intros v' Hv' Hm';
           apply match_ty_i_cname__inv in Hm'; subst; assumption
+   | Hsem:forall v, value_type v -> |-[ ?k] v <$ TPair ?v1 ?v2 -> _ |- _ => assert (Hvv : value_type (TPair v1 v2)) by (constructor; assumption)
    end).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-13 06:50:35.880000.*)
+(* Auto-generated comment: At 2019-08-13 06:50:55.100000.*)
 
