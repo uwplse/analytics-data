be3e34061b8a01c23be5f0506d60331f772234b7
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:39:44 -0800

    user 3, session 6414, part 80

--- diffs-annotated-with-times/3/user-3-session-6414.v
+++ diffs-annotated-with-times/3/user-3-session-6414.v
@@ -129,9 +129,14 @@ Theorem get_len_ok :
     (fun (_ : unit) state =>
      {|
      pre := True;
-     post := fun r state' => state' = state /\ block_to_addr r = a;
-     recovered := fun _ state' => state' = state |}) get_len d.recover.
-(* Auto-generated comment: Failed. *)
+     post := fun r state' => state' = state /\ r = length state;
+     recovered := fun _ state' => state' = state |}) get_len d.recover abstr.
+Proof.
+(unfold get_len; intros).
+(apply spec_abstraction_compose).
+step_proc.
+(intros).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-09-04 10:32:11.320000.*)
+(* Auto-generated comment: At 2019-09-04 10:32:15.850000.*)
 
