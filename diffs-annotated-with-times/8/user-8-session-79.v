9f3c80dfa5d637691ad89de3f0938033ac6908ea
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:47 -0800

    user 8, session 79, part 152

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -609,8 +609,9 @@ Proof.
 (unfold Morphisms.Proper).
 Timeout 1 About Morphisms.Proper.
 Timeout 1 Print Morphisms.Proper.
-Timeout 1 Print Ltac Morphisms.Proper.
-(* Auto-generated comment: Failed. *)
+(unfold Morphisms.respectful).
+(unfold flip).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-13 18:20:40.350000.*)
+(* Auto-generated comment: At 2019-08-13 18:20:49.140000.*)
 
