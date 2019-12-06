c3804f578b33df0532778203e2988df8174e9130
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:33 -0800

    user 8, session 72, part 51

--- diffs-annotated-with-times/8/user-8-session-72.v
+++ diffs-annotated-with-times/8/user-8-session-72.v
@@ -1249,6 +1249,5 @@ reflexivity.
 (repeat (rewrite inSeq_correct; try unfold compose_super); try apply inSeq_WT;
   try apply unitary_at1_WT; try apply source_symmetric_WT; trivial).
 (apply source_symmetric_WT in H).
-(unfold valid_ancillae_box in IHsource_symmetric).
 (* Auto-generated comment: Succeeded. *)
 
