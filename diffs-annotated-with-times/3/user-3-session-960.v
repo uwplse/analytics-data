0920aa89d393ca563cebcd021bb677d381e551e4
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:21:16 -0800

    user 3, session 960, part 18

--- diffs-annotated-with-times/3/user-3-session-960.v
+++ diffs-annotated-with-times/3/user-3-session-960.v
@@ -81,7 +81,9 @@ Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
 Search -"endian".
 Search -"digit".
-(* Auto-generated comment: Succeeded. *)
+Definition uint64_to_le (x : uint64) : list byte :=
+  if lt_dec x (pow 2 64) then nil else nil.
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-16 05:25:11.020000.*)
+(* Auto-generated comment: At 2019-08-16 05:26:53.090000.*)
 
