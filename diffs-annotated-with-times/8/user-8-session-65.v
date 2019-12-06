502e2a2d95e3098a032410a599ff295ec442d8c8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:22 -0800

    user 8, session 65, part 243

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -1254,6 +1254,6 @@ constructor.
 (apply source_symmetric_WT in H).
 (unfold valid_ancillae_box in IHsource_symmetric).
 (rewrite H0).
-(rewrite IHsource_symmetric; trivial).
+(simpl).
 (* Auto-generated comment: Succeeded. *)
 
