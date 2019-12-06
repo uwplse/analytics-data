1df8dc0c8c7d61dc8367bd0c732a37fa085a4111
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:28 -0800

    user 7, session 24, part 30

--- diffs-annotated-with-times/7/user-7-session-24.v
+++ diffs-annotated-with-times/7/user-7-session-24.v
@@ -57,8 +57,9 @@ where "|-[ k ']' v '<$' t" := (match_ty k v t) : btjm_scope.
 Theorem match_ty__value_type_l : forall (k : nat) (t v : ty), |-[ k] v <$ t -> value_type v.
 Proof.
 (intros k; induction t; induction v; intros Hm; try (solve [ constructor | destruct k; contradiction ])).
-(destruct k; simpl in Hm).
+(destruct k).
+(simpl in Hm).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-15 06:24:25.820000.*)
+(* Auto-generated comment: At 2019-08-15 06:27:54.160000.*)
 
