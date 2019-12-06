3cd0137a8cacae9283bd2fb32bfb63807b09c292
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:29:16 -0800

    user 3, session 3467, part 2

--- diffs-annotated-with-times/3/user-3-session-3467.v
+++ diffs-annotated-with-times/3/user-3-session-3467.v
@@ -6,17 +6,26 @@ Set Printing Depth 50.
 Remove Search Blacklist "Private_" "_subproof".
 Add Search Blacklist "Private_" "_subproof".
 Require Import Proc Abstraction.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqe6hkoh"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqN0Djid"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Add Search Blacklist "Raw" "Proofs".
 Set Search Output Name Only.
-Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqY7R8DM"
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqkKvFf6"
 SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
+Ltac
+ monad_simpl :=
+  repeat
+   match goal with
+   | |- proc_spec _ (Bind (Ret _) _) _ _ =>
+         eapply spec_exec_equiv; [ apply monad_left_id |  ]
+   | |- proc_spec _ (Bind (Bind _ _) _) _ _ =>
+         eapply spec_exec_equiv; [ apply monad_assoc |  ]
+   end.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-29 07:12:31.010000.*)
+(* Auto-generated comment: At 2019-08-29 07:12:50.700000.*)
 
