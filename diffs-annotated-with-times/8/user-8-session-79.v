34cee741c2690cc755dd5edc6f82da615e87d2b6
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:51 -0800

    user 8, session 79, part 274

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -661,19 +661,9 @@ reflexivity.
 Timeout 1 About restore_dims.
 Timeout 1 Print restore_dims.
 Timeout 1 Print Ltac restore_dims.
-(match goal with
- | |- ?A => let A' := restore_dims_rec tac A in
-            replace
-            A
-            with
-            A'
- end).
-2: {
-(apply f_equal_gen; trivial).
-(apply f_equal_gen; trivial).
-(apply f_equal_gen; trivial).
-(apply f_equal_gen; trivial).
-(* Auto-generated comment: Succeeded. *)
+Timeout 1 About length_app.
+Timeout 1 Print length_app.
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-14 13:35:44.020000.*)
+(* Auto-generated comment: At 2019-08-14 13:35:44.530000.*)
 
