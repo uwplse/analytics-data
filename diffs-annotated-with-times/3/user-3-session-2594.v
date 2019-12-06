8c278b26b5654dad3333b04f05c431d6d158910f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:26:09 -0800

    user 3, session 2594, part 6

--- diffs-annotated-with-times/3/user-3-session-2594.v
+++ diffs-annotated-with-times/3/user-3-session-2594.v
@@ -38,10 +38,17 @@ SearchPattern _.
 Remove Search Blacklist "Raw" "Proofs".
 Unset Search Output Name Only.
 Timeout 1 Print LoadPath.
-From Armada Require Import Goose.Machine.
-Opaque Nat.modulo Nat.div.
-#[local]Obligation Tactic := (intros; simpl; subst).
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqgFkdsg"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Raw" "Proofs".
+Set Search Output Name Only.
+Redirect "/var/folders/5x/1mdbpbjd7012l971fq0zkj2w0000gn/T/coqRfXyfi"
+SearchPattern _.
+Remove Search Blacklist "Raw" "Proofs".
+Unset Search Output Name Only.
+Timeout 1 Print LoadPath.
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-16 07:35:19.750000.*)
+(* Auto-generated comment: At 2019-08-16 07:35:23.680000.*)
 
