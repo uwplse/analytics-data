d74f543c0f0deb7e0019fb0928603f4d907bc8fd
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:42:14 -0800

    user 3, session 7199, part 385

--- diffs-annotated-with-times/3/user-3-session-7199.v
+++ diffs-annotated-with-times/3/user-3-session-7199.v
@@ -480,8 +480,12 @@ intuition.
 (descend; intuition eauto).
 }
 (eapply proc_spec_weaken; eauto).
-(unfold spec_impl).
-(* Auto-generated comment: Succeeded. *)
+(unfold spec_impl; simpl; intuition).
+(descend; intuition eauto).
+{
+(descend; intuition eauto).
+(apply log_abstraction_nil).
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-04 12:21:47.470000.*)
+(* Auto-generated comment: At 2019-09-04 12:21:55.530000.*)
 
