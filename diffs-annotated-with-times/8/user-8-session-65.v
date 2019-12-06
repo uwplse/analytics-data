36e2a92dcdc6ca859e33fb915314715713cadac8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:18 -0800

    user 8, session 65, part 191

--- diffs-annotated-with-times/8/user-8-session-65.v
+++ diffs-annotated-with-times/8/user-8-session-65.v
@@ -1252,6 +1252,7 @@ constructor.
   try apply source_symmetric_WT; trivial).
 (unfold compose_super).
 (apply source_symmetric_WT in H).
+(unfold valid_ancillae_box in IHsource_symmetric).
 (rewrite IHsource_symmetric; trivial).
 (* Auto-generated comment: Failed. *)
 
