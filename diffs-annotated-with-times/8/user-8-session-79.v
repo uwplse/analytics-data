5bbba40ebc06261d967e7123429698f62dbb6158
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:59:52 -0800

    user 8, session 79, part 315

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -909,8 +909,8 @@ gen n.
 (simpl in *).
 clear L1 M1 Lt.
 (rewrite strip_one_l_in_eq).
-setoid_rewrite (kron_1_l_inv (\226\168\130 l2)) at 1.
+setoid_rewrite  <- (kron_1_l (\226\168\130 l2)) at 1.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-14 13:58:09.010000.*)
+(* Auto-generated comment: At 2019-08-14 13:58:13.460000.*)
 
