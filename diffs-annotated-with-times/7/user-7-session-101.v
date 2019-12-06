785394ff8c2f5a4362b6158f1b3a8cf7313d9da9
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:00 -0800

    user 7, session 101, part 35

--- diffs-annotated-with-times/7/user-7-session-101.v
+++ diffs-annotated-with-times/7/user-7-session-101.v
@@ -61,52 +61,10 @@ exists (TPair v1 v2).
 contradiction.
 -
 Abort.
-Lemma not_sem_sub__refeXrefX_eYrefrefY : ~ ||- [TRef (TExist vX (TRef tX))]<= [TExist vY (TRef (TRef tY))].
+Lemma not_sem_sub__eXrefX_reft : forall (w k : nat) (t : ty), ~ ||-[ w, k][TExist vX (TRef tX)]<= [TRef t].
 Proof.
-(intros Hcontra).
-(destruct Hcontra as [w Hcontra]).
 (induction w).
--
-specialize (Hcontra 2).
-(assert (Hm : |-[ 0, 2] TRef (TExist vX (TRef tX)) <$ TRef (TExist vX (TRef tX))) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Hcontra _ Hm).
-clear Hm.
-(apply match_ty_exist__0_inv in Hcontra).
-(apply match_ty_ref__inv in Hcontra).
-(destruct Hcontra as [t' [Heq Href]]).
-(inversion Heq; subst).
-clear Heq.
-(assert (Hm : |-[ 0, 1] TRef tX <$ TRef tX) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Href (TRef tX)).
-(destruct Href as [Href _]).
-specialize (Href Hm).
-clear Hm.
-(apply match_ty_ref__inv in Href).
-(destruct Href as [t' [Heq Href]]).
-(inversion Heq; subst).
-clear Heq.
-(assert (Hm : |-[ 0, 0] TEV vX <$ TEV vX) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Href (TEV vX)).
-(destruct Href as [Href _]).
-specialize (Href Hm).
-clear Hm.
-(simpl in Href).
-(inversion Href).
--
-specialize (Hcontra 2).
-(assert (Hm : |-[ S w, 2] TRef (TExist vX (TRef tX)) <$ TRef (TExist vX (TRef tX))) by (apply match_ty_value_type__reflexive; constructor)).
-specialize (Hcontra _ Hm).
-clear Hm.
-(apply match_ty_exist__inv in Hcontra).
-(destruct Hcontra as [t Hcontra]).
-(assert (Heq : [vY := t] TRef (TRef tY) = TRef (TRef t)) by reflexivity).
-(rewrite Heq in Hcontra).
-clear Heq.
-(apply match_ty_ref__inv in Hcontra).
-(destruct Hcontra as [t' [Heq Href]]).
-(inversion Heq; subst).
-clear Heq.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-20 08:45:51.790000.*)
+(* Auto-generated comment: At 2019-08-20 08:45:55.440000.*)
 
