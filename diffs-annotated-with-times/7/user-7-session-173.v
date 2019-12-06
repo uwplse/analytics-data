5feced7c7fd6cea81dba36a7a4bdaca06986da92
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:57:37 -0800

    user 7, session 173, part 37

--- diffs-annotated-with-times/7/user-7-session-173.v
+++ diffs-annotated-with-times/7/user-7-session-173.v
@@ -99,7 +99,37 @@ Fixpoint subst (x : id) (s t : ty) {measure size t : ty :=
   | TVar y => if beq_id x y then s else t
   | TEV y => t
   end.
+Next Obligation.
+(simpl).
+Omega.omega.
+Qed.
+Next Obligation.
+(simpl).
+Omega.omega.
+Qed.
+Next Obligation.
+(simpl).
+Omega.omega.
+Qed.
+Next Obligation.
+(simpl).
+Omega.omega.
+Qed.
+Next Obligation.
+(simpl).
+(rewrite rename__size).
+Omega.omega.
+Qed.
+Notation "'[' x ':=' s ']' t" := (subst x s t) (at level 30) : btjt_scope.
+Lemma triv : forall (X : id) (s : ty) (t1 t2 : ty), [X := s] TPair t1 t2 = TPair ([X := s] t1) ([X := t2] t2).
+Proof.
+(intros X s t1 t2).
+(unfold subst).
+(unfold subst_func).
+(unfold Fix_sub).
+(simpl).
+reflexivity.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-29 14:59:19.720000.*)
+(* Auto-generated comment: At 2019-08-29 14:59:23.900000.*)
 
