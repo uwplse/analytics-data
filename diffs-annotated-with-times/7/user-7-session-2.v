321125559698ba6c1e9debcc96baf3c5d1edaba0
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:55:50 -0800

    user 7, session 2, part 46

--- diffs-annotated-with-times/7/user-7-session-2.v
+++ diffs-annotated-with-times/7/user-7-session-2.v
@@ -170,5 +170,5 @@ generalize dependent t3.
 +
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-06 13:36:29.580000.*)
+(* Auto-generated comment: At 2019-08-06 13:38:11.160000.*)
 
