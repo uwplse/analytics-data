73d585a524586cbd8591e1b6c37fbb313ffb5479
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 15:56:55 -0800

    user 7, session 98, part 7

--- diffs-annotated-with-times/7/user-7-session-98.v
+++ diffs-annotated-with-times/7/user-7-session-98.v
@@ -112,8 +112,24 @@ Proof.
 (apply match_ty_ref__weak_inv in Hm).
 (destruct Hm as [t' Heq]; subst).
 (simpl).
-(split; tauto).
+split.
+constructor.
+(exists t'; reflexivity).
+-
+(apply match_ty_ref__inv in Hm).
+(destruct Hm as [t' [Heq Href]]; subst).
+(simpl).
+exists t'.
+(apply match_ty__reflexive).
+constructor.
+Qed.
+Lemma sem_sub__eXrefX_eYrefY : ||- [TExist vX (TRef tX)]<= [TExist vY (TRef tY)].
+Proof.
+(intros k; destruct k; intros v Hm).
+-
+(apply match_ty_exist__0_inv in Hm).
+(destruct Hm as [Hv [tx Heqx]]).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-19 09:52:08.070000.*)
+(* Auto-generated comment: At 2019-08-19 09:52:11.660000.*)
 
