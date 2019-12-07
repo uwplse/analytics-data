601a7201305507fec707fabc8b6b2dabce4ea9f0
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:43 -0800

    user 8, session 79, part 1252

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1184,7 +1184,8 @@ reflexivity.
 (rewrite id_circ_spec).
 (erewrite kron_compat).
 3: {
-(simpl; specialize (IHb1 \206\147 f \226\140\136 b1 | f \226\140\137); repeat rewrite Nat.add_0_r in *;
-  apply IHb1; trivial).
-(* Auto-generated comment: Failed. *)
+(simpl; rewrite size_ntensor, Nat.mul_1_r; unfold ctx_to_matrix in *;
+  rewrite ctx_to_mat_list_length; specialize (IHb1 \206\147 f \226\140\136 b1 | f \226\140\137);
+  repeat rewrite Nat.add_0_r in *; apply IHb1; trivial).
+(* Auto-generated comment: Succeeded. *)
 
