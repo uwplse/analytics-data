3ad89d36a04dfd4e4f01643ab160ea63f3b3897e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:44 -0800

    user 11, session 12, part 1

--- diffs-annotated-with-times/11/user-11-session-12.v
+++ diffs-annotated-with-times/11/user-11-session-12.v
@@ -15,7 +15,19 @@ Open Scope sum_scope.
 Open Scope monad_scope.
 Open Scope N_scope.
 Anomaly ""Assert_failure printing/ppconstr.ml:399:14"." Please report at http://coq.inria.fr/bugs/.
+Variant message :=
+  | Message_Plain : forall plainMessage : plain_message, _
+  | Message_Cipher : forall cipherMessage : cipher_text plain_message, _.
+Definition Message_Finished (k : shared_key) (verifyData : N) : message :=
+  Message_Cipher (cipher k (PlainMessage_Finished verifyData)).
+Definition Message_Hello (messageRandom : random) (messagePublic : public_key) : message :=
+  Message_Plain (PlainMessage_Hello messageRandom messagePublic).
+Anomaly ""Assert_failure printing/ppconstr.ml:399:14"." Please report at http://coq.inria.fr/bugs/.
+Definition failWith {E} {R} `{exceptE error -< E} (err : error) : itree E R :=
+  vd <- trigger (Throw err);; match vd in void with
+                              end.
+Anomaly ""Assert_failure printing/ppconstr.ml:399:14"." Please report at http://coq.inria.fr/bugs/.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-09 21:18:43.030000.*)
+(* Auto-generated comment: At 2019-09-09 21:18:55.040000.*)
 
