25c768ec2822e964f9917d7a1aec836c1bfda0f9
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:16 -0800

    user 8, session 125, part 120

--- diffs-annotated-with-times/8/user-8-session-125.v
+++ diffs-annotated-with-times/8/user-8-session-125.v
@@ -87,6 +87,5 @@ replace (size_wtype W1 + size_wtype W2)%nat with \226\159\166 W \226\159\167 by 
 (destruct W; try (solve [ inversion HeqW ])).
 (rewrite denote_ctrls_transpose).
 (remember (denote_ctrls (\226\159\166 W3 \226\138\151 W4 \226\159\167) U li) as A).
-(remember (swap_list (\226\159\166 W3 \226\138\151 W4 \226\159\167) li) as S).
 (* Auto-generated comment: Succeeded. *)
 
