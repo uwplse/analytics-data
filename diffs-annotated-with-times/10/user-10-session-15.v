b9024e6729a42de59009ce6fd3d8fc05b44811ac
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:40 -0800

    user 10, session 15, part 5

--- diffs-annotated-with-times/10/user-10-session-15.v
+++ diffs-annotated-with-times/10/user-10-session-15.v
@@ -24,13 +24,13 @@ Timeout 1 Print LoadPath.
 Import ListNotations.
 Fixpoint fib (fuel : nat) (a b : N) : list N :=
   match fuel with
-  | O => print_id []
-  | S fuel => a :: fib fuel b (a + b)
+  | O => []
+  | S fuel => a :: print_id (fib fuel b (a + b))
   end.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqzpkfnT" Print Ltac Signatures.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqFm8j9B" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Eval compute in List.map print_id (fib 10 0 1).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-27 15:24:13.820000.*)
+(* Auto-generated comment: At 2019-08-27 15:30:41.130000.*)
 
