e946a73cf08d9adb6d661158feb055af473ea9f5
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:44 -0800

    user 11, session 13, part 0

---
+++ diffs-annotated-with-times/11/user-11-session-13.v
@@ -0,0 +1,16 @@
+Redirect "/tmp/coq16819LrZ" Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+From Coq Require Import ExtrOcamlIntConv.
+From ExtLib Require Import Applicative StateMonad Monad.
+From ITree Require Import Exception Nondeterminism ITree.
+From SimpleIO Require Import IO_Random SimpleIO.
+From DeepWeb Require Import CryptoLib KvsLib.
+Import ApplicativeNotation FunctorNotation ListNotations MonadNotation SumNotations.
+(* Auto-generated comment: Succeeded. *)
+
+(* Auto-generated comment: At 2019-09-09 21:30:53.600000.*)
+
