16e15c9510c0357bde05df520908dcb3c2ac6057
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:34 -0800

    user 10, session 2, part 2

--- diffs-annotated-with-times/10/user-10-session-2.v
+++ diffs-annotated-with-times/10/user-10-session-2.v
@@ -1,7 +1,2 @@
-Require Import Analytical.Analytics.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqBDrtIb" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Print Analytics.
 (* Auto-generated comment: Succeeded. *)
 
