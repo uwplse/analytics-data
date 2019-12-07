04f3f51940c462bd05ffadf2cfdd9041f65050f5
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:39 -0800

    user 10, session 13, part 0

---
+++ diffs-annotated-with-times/10/user-10-session-13.v
@@ -0,0 +1,15 @@
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqBYq1cI"
+Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+From ReductionEffect Require Import PrintingEffect.
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqrYfIcK" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+(* Auto-generated comment: Succeeded. *)
+
+(* Auto-generated comment: At 2019-08-27 15:00:11.410000.*)
+
