a4c91aeb423028a440ef1715eeab428492ece6c1
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:02 -0800

    user 8, session 79, part 569

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -998,8 +998,16 @@ restore_dims tensor_tac.
 (destruct t; reflexivity).
 -
 (simpl in *).
-rewrite_inPar''.
+(match goal with
+ | |-
+   context [ (@denote_box true ?W ?W' (@inPar ?W1 ?W1' ?W2 ?W2' ?f ?g))
+               (@kron ?m ?n ?o ?p ?\207\1291 ?\207\1292) ] =>
+       let IP := fresh "IP" in
+       specialize (inPar_correct W1 W1' W2 W2' f g true \207\1291 \207\1292) as IP; simpl in *;
+        rewrite size_ntensor in *; simpl in *; try rewrite Nat.mul_1_r in *;
+        rewrite IP; clear IP
+ end; try (solve [ type_check ])).
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-14 19:53:26.600000.*)
+(* Auto-generated comment: At 2019-08-14 19:53:35.090000.*)
 
