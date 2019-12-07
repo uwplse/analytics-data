6d2b8535918b5d1f27ba54841456c06dbd526245
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:21 -0800

    user 8, session 125, part 223

--- diffs-annotated-with-times/8/user-8-session-125.v
+++ diffs-annotated-with-times/8/user-8-session-125.v
@@ -112,6 +112,7 @@ specialize (denote_ctrls_unitary W _ U _ L) as inv.
 replace (size_wtype W1 + size_wtype W2)%nat with \226\159\166 W \226\159\167 by (subst; easy).
 (unfold apply_U, apply_unitary, super).
 (destruct W; inversion HeqW).
-subst H0.
-(* Auto-generated comment: Failed. *)
+clear H0 H1 HeqW.
+(rewrite denote_ctrls_transpose).
+(* Auto-generated comment: Succeeded. *)
 
