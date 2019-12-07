266d61bfbeb7aa3af630d763b8b482848eefec9f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:18 -0800

    user 8, session 125, part 169

--- diffs-annotated-with-times/8/user-8-session-125.v
+++ diffs-annotated-with-times/8/user-8-session-125.v
@@ -112,6 +112,5 @@ easy.
 }
 replace (size_wtype W1 + size_wtype W2)%nat with \226\159\166 W \226\159\167 by (subst; easy).
 (unfold apply_U, apply_unitary, super).
-(destruct W; try (solve [ inversion HeqW ])).
 (* Auto-generated comment: Succeeded. *)
 
