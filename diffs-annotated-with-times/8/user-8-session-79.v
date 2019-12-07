fabb2eb53f6624c775a16ee96ff9e53bafa469f6
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:18 -0800

    user 8, session 79, part 860

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1013,6 +1013,20 @@ specialize inSeq_correct as IS.
 (repeat (rewrite IS; compile_typing compile_WT)).
 (unfold compose_super).
 restore_dims tensor_tac.
-rewrite_inPar'.
-(* Auto-generated comment: Failed. *)
+Timeout 1 About rewrite_inPar'.
+Timeout 1 Print rewrite_inPar'.
+Timeout 1 Print Ltac rewrite_inPar'.
+(match goal with
+ | |-
+   context [ (@denote_box true ?W ?W' (@inPar ?W1 ?W1' ?W2 ?W2' ?f ?g))
+               (@kron ?m ?n ?o ?p ?\207\1291 ?\207\1292) ] =>
+       idtac f;
+        (let IP := fresh "IP" in
+         specialize (inPar_correct W1 W1' W2 W2' f g true \207\1291 \207\1292) as IP; simpl in *;
+          rewrite size_ntensor in *; simpl in *; try rewrite Nat.mul_1_r in *)
+ end; try (solve [ type_check ])).
+Set Printing All.
+(fold NTensor).
+(rewrite IP).
+(* Auto-generated comment: Succeeded. *)
 
