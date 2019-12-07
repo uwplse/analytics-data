91a7fe3013775903cb63969c1e4ff8e6537cb2c0
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:01 -0800

    user 8, session 79, part 548

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -985,8 +985,11 @@ Proof.
 (simpl in *).
 rewrite_inPar''.
 (simpl_rewrite TRUE_spec).
-restore_dims.
-(* Auto-generated comment: Failed. *)
+restore_dims
+ simpl; try rewrite size_ntensor; try rewrite app_length; simpl; unify_pows_two;
+  lia.
+(simpl_rewrite id_circ_spec).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-14 18:43:26.640000.*)
+(* Auto-generated comment: At 2019-08-14 18:54:24.980000.*)
 
