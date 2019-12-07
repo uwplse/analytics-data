9485bf1c2fcf5feb20ba1a0ffe7d66a325a194a8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:32 -0800

    user 8, session 79, part 1094

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1106,6 +1106,6 @@ clear IS.
 9: {
 rewrite_inPar'.
 (repeat rewrite strip_one_l_in_eq).
-(rewrite kron_1_l_inv).
-(* Auto-generated comment: Succeeded. *)
+(rewrite (kron_1_l_inv (ctx_to_matrix \206\147 f))).
+(* Auto-generated comment: Failed. *)
 
