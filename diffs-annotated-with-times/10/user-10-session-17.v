887b513b6d2be9773f15a72a0ccb64ac88f42358
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:41 -0800

    user 10, session 17, part 1

--- diffs-annotated-with-times/10/user-10-session-17.v
+++ diffs-annotated-with-times/10/user-10-session-17.v
@@ -23,6 +23,6 @@ Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqvTdMQo"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
-Eval cbn in Str_nth 0 (fib2 0 1).
+Eval cbn in Str_nth 10 (fib2 0 1).
 (* Auto-generated comment: Succeeded. *)
 
