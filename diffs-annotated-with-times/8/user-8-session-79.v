ec417d59d0af4c01b666a69d38d818c8b459ccee
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:03 -0800

    user 8, session 79, part 606

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1008,5 +1008,5 @@ restore_dims tensor_tac.
  end).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-15 09:35:14.790000.*)
+(* Auto-generated comment: At 2019-08-15 09:35:15.310000.*)
 
