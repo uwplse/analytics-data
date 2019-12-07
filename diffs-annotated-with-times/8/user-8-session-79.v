ebeb3a58935f4359bf84342a86e29daf94c33b7e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:25 -0800

    user 8, session 79, part 988

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1041,6 +1041,5 @@ mat_replace \226\136\1631\226\159\169\226\159\1681\226\136\163 with bool_to_matr
 4: (erewrite kron_compat).
 6: {
 restore_dims tensor_tac.
-specialize (IHb \206\147 f true).
 (* Auto-generated comment: Succeeded. *)
 
