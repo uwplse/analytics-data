44fd6055a008f0553616dd369c719be29af9ecc3
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:39 -0800

    user 10, session 13, part 9

--- diffs-annotated-with-times/10/user-10-session-13.v
+++ diffs-annotated-with-times/10/user-10-session-13.v
@@ -8,18 +8,6 @@ Add Search Blacklist "Private_" "_subproof".
 Timeout 1 Print LoadPath.
 Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coq5lrxpQ" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
-From Coq Require Import NArith Streams.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqznWAeW" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-From ReductionEffect Require Import PrintingEffect.
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqTlmINW" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Timeout 1 Print LoadPath.
-Check map.
-CoFixpoint fib (x y : N) : Stream N := Cons y (fib y (x + y)).
-Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqbF6gTS" Print Ltac Signatures.
-Timeout 1 Print Grammar tactic.
-Eval compute in Str_nth 10 (map print_id (fib 0 1)).
-(* Auto-generated comment: Succeeded. *)
+From Coq Require Import NArith Lists Streams.
+(* Auto-generated comment: Failed. *)
 
