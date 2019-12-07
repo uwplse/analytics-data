a0615f172f37c4ba2a904a75970d7739ecc0058d
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:00 -0800

    user 8, session 79, part 531

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -995,9 +995,10 @@ Timeout 1 Print Ltac rewrite_inPar.
                (@kron ?m ?n ?o ?p ?\207\1291 ?\207\1292) ] =>
        let IP := fresh "IP" in
        specialize (inPar_correct W1 W1' W2 W2' f g true \207\1291 \207\1292) as IP; simpl in *;
-        rewrite size_ntensor in *; try rewrite Nat.mul_1_r in *
+        rewrite size_ntensor in *; try rewrite Nat.mul_1_r in *; 
+        rewrite IP
  end; try (solve [ type_check ])).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-14 18:37:57.330000.*)
+(* Auto-generated comment: At 2019-08-14 18:38:47.540000.*)
 
