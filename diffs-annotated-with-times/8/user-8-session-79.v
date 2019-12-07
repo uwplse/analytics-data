859352a4f1318d3028478cd6279fc5c0b2b21083
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:04 -0800

    user 8, session 79, part 650

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -999,7 +999,7 @@ evar ( e : Square (2 * 2 ^ \226\159\166 \206\147 \226\159\167) ).
 (match goal with
  | |- denote_box _ _ ?A == _ => mat_replace A with e
  end).
-(* Auto-generated comment: Failed. *)
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-15 10:00:30.880000.*)
+(* Auto-generated comment: At 2019-08-15 10:00:31.610000.*)
 
