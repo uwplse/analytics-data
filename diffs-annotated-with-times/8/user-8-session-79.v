769be69f8e1587ff1ee95f506cc17c4fa789786b
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:04 -0800

    user 8, session 79, part 644

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -995,14 +995,13 @@ restore_dims tensor_tac.
 -
 (simpl).
 restore_dims tensor_tac.
-evar ( e : Square (2 * 2 ^ \226\159\166 \206\147 \226\159\167) ).
 (match goal with
  | |- denote_box _ _ ?A == _ =>
        match type of A with
        | ?T => evar ( e : T ); mat_replace A with e
        end
  end).
-(* Auto-generated comment: Succeeded. *)
+(* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-15 09:55:26.610000.*)
+(* Auto-generated comment: At 2019-08-15 09:55:26.910000.*)
 
