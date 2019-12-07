def375c633bc64a20dae9458dd3ede9fc466648e
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:00 -0800

    user 8, session 79, part 523

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -986,7 +986,8 @@ Proof.
 (induction b; intros \206\147 f t H).
 -
 (simpl in *).
+rewrite_inPar''.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-08-14 18:31:42.460000.*)
+(* Auto-generated comment: At 2019-08-14 18:32:54.380000.*)
 
