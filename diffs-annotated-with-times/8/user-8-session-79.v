bac2e7862c5c8fb0473be08dac281dd559729c98
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:22 -0800

    user 8, session 79, part 921

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1017,6 +1017,6 @@ rewrite_inPar'.
 rewrite_inPar'.
 (rewrite strip_one_l_in_eq).
 restore_dims tensor_tac.
-setoid_rewrite  <- (kron_1_l (ctx_to_matrix \206\147 f)).
-(* Auto-generated comment: Succeeded. *)
+(rewrite <- (kron_1_l (ctx_to_matrix \206\147 f))).
+(* Auto-generated comment: Failed. *)
 
