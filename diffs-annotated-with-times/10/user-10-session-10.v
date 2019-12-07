4a581020a131bf4aa7f4bbc97a54a268d610d0b6
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:37 -0800

    user 10, session 10, part 4

--- diffs-annotated-with-times/10/user-10-session-10.v
+++ diffs-annotated-with-times/10/user-10-session-10.v
@@ -270,7 +270,7 @@ Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqiFtR30"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Timeout 1 Print LoadPath.
-Definition network_of_app {E} {nE} `{networkE -< nE} `{exceptE error -< nE} `{E -< NE} 
+Definition network_of_app {E} {nE} `{networkE -< nE} `{exceptE error -< nE} `{E -< nE} 
   (k : shared_key) T (e : (appE id +' E) T) : itree nE T :=
   match e with
   | (ae|) =>
@@ -293,7 +293,9 @@ Definition network_of_app {E} {nE} `{networkE -< nE} `{exceptE error -< nE} `{E 
       end
   | (|e) => trigger e
   end.
-(* Auto-generated comment: Failed. *)
+Redirect "/var/folders/lm/cpf87_lx21n9bgnl4kr72rjm0000gn/T/coqv20nyB" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-22 07:52:14.420000.*)
+(* Auto-generated comment: At 2019-08-22 07:55:15.350000.*)
 
