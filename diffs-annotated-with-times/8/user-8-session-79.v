5691235957c766aa6640f5d0e4cb78e15580e145
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:54 -0800

    user 8, session 79, part 1411

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1170,6 +1170,7 @@ reflexivity.
 }
 6: {
 listify_kron.
-(apply CNOT_at_spec).
-(* Auto-generated comment: Failed. *)
+(simpl_rewrite (CNOT_at_spec \226\140\136 b2 | f \226\140\137 (\226\140\136 b1 | f \226\140\137 \226\138\149 t) (2 + \226\159\166 \206\147 \226\159\167) 1 0); trivial;
+  try omega).
+(* Auto-generated comment: Succeeded. *)
 
