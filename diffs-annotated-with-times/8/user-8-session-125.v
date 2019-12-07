d22fc726ff94e5abe06f8a86883d232c8154b633
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:22 -0800

    user 8, session 125, part 253

--- diffs-annotated-with-times/8/user-8-session-125.v
+++ diffs-annotated-with-times/8/user-8-session-125.v
@@ -182,6 +182,6 @@ matrix_denote.
 restore_dims.
 (repeat rewrite Mmult_1_l).
 restore_dims.
-(rewrite kron_1_l).
-(* Auto-generated comment: Failed. *)
+(rewrite (kron_1_l \226\136\1630\226\159\169)).
+(* Auto-generated comment: Succeeded. *)
 
