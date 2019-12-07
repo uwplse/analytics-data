e8869ed12e94635272511fdc030c0d56dbb64ad2
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:53 -0800

    user 8, session 79, part 1409

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1171,6 +1171,6 @@ reflexivity.
 6: {
 listify_kron.
 restore_dims tensor_dims.
-(rewrite CNOT_at_spec).
-(* Auto-generated comment: Failed. *)
+(apply CNOT_at_spec).
+(* Auto-generated comment: Succeeded. *)
 
