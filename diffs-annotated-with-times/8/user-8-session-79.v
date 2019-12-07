41462a6dde390035770c6cebc118a5b1b06e0fc3
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:40 -0800

    user 8, session 79, part 1206

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1163,7 +1163,5 @@ listify_kron.
 5:
  (restore_dims tensor_tac; simpl; rewrite size_ntensor, Nat.mul_1_r;
    unfold ctx_to_matrix in *; rewrite ctx_to_mat_list_length).
-Set Printing All.
-5: (apply (IHb2 \206\147 f \226\140\136 b2 | f \226\140\137); trivial).
 (* Auto-generated comment: Succeeded. *)
 
