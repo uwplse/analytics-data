b607282ef1f8ec69c83e5c9ebf226111038011b8
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:46 -0800

    user 8, session 79, part 1303

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -918,7 +918,5 @@ restore_dims tensor_dims.
 -
 (simpl).
 listify_kron.
-(simpl_rewrite (CNOT_at_spec (f v) t (S (\226\159\166 \206\147 \226\159\167)) (S (position_of v \206\147)) 0); trivial;
-  try omega).
 (* Auto-generated comment: Succeeded. *)
 
