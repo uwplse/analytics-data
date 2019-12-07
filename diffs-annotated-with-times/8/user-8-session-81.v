b9ba9c1baa610c95efcf31d4d1634c643f2c41a9
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:57 -0800

    user 8, session 81, part 8

--- diffs-annotated-with-times/8/user-8-session-81.v
+++ diffs-annotated-with-times/8/user-8-session-81.v
@@ -725,6 +725,8 @@ Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coq7AcHMp"
 Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 (repeat rewrite Nat.mul_1_l).
-(rewrite kron_1_r).
+(rewrite (kron_1_r A)).
+(rewrite (kron_1_r B)).
+restore_dims.
 (* Auto-generated comment: Failed. *)
 
