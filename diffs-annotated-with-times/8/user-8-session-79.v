0fcee4e2bb80c018a0353162d31320878a1d5458
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:24 -0800

    user 8, session 79, part 956

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1035,6 +1035,6 @@ rewrite_inPar'.
 (rewrite id_circ_spec).
 (rewrite init1_spec).
 mat_replace \226\136\1631\226\159\169\226\159\1681\226\136\163 with bool_to_matrix true by reflexivity.
-(simpl_rewrite (IHb \206\147 f true H)).
+(rewrite (IHb \206\147 f true H)).
 (* Auto-generated comment: Failed. *)
 
