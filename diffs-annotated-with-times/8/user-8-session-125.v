d581ac9407c5e48f30b1b2126e9fe7a85ba2571c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:20 -0800

    user 8, session 125, part 203

--- diffs-annotated-with-times/8/user-8-session-125.v
+++ diffs-annotated-with-times/8/user-8-session-125.v
@@ -122,6 +122,5 @@ replace (size_wtype W1 + size_wtype W2)%nat with \226\159\166 W \226\159\167 by 
 Msimpl.
 (rewrite (Mmult_assoc \207\129 _ A)).
 (rewrite inv by (subst; rewrite size_wtype_length; easy)).
-Msimpl.
 (* Auto-generated comment: Succeeded. *)
 
