4444be22b210359c599a48ad855785463936b523
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:33 -0800

    user 8, session 79, part 1121

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1120,6 +1120,6 @@ rewrite_inPar'.
 (rewrite id_circ_spec).
 restore_dims tensor_tac.
 (erewrite kron_compat).
-3: (apply (IHb1 \206\147 f false)).
+3: (simpl; rewrite size_ntensor; apply (IHb1 \206\147 f false)).
 (* Auto-generated comment: Failed. *)
 
