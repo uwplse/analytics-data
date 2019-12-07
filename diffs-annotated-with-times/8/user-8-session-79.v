e294e7b8de9299b061f70d3c797a9d783a043432
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:06 -0800

    user 8, session 79, part 682

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -995,7 +995,10 @@ restore_dims tensor_tac.
 (destruct t; reflexivity).
 -
 (simpl).
-(* Auto-generated comment: Failed. *)
+listify_kron.
+(simpl_rewrite (CNOT_at_spec (f v) t (S (\226\159\166 \206\147 \226\159\167)) (S (position_of v \206\147)) 0); trivial;
+  try omega).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-15 11:52:37.440000.*)
+(* Auto-generated comment: At 2019-08-15 11:52:50.830000.*)
 
