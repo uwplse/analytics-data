db9928de996e000bade0694075e816d75c528976
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:36 -0800

    user 10, session 7, part 15

--- diffs-annotated-with-times/10/user-10-session-7.v
+++ diffs-annotated-with-times/10/user-10-session-7.v
@@ -265,7 +265,23 @@ Notation sE := (networkE +' exceptE error +' hsgenE +' randomE).
 Notation tE := (nondetE +' sE).
 Print networkE.
 Print error.
-(* Auto-generated comment: Failed. *)
+CoFixpoint match_event {X} (e0 : networkE X) (x0 : X) (t : itree tE unit) : itree tE unit :=
+  match t.(observe) with
+  | RetF r => Ret r
+  | TauF t => Tau (match_event e0 x0 t)
+  | VisF e k =>
+      match e with
+      | (|(ne|)) =>
+          match e0 in (networkE X), ne in (networkE Y) return ((Y -> _) -> X -> _) with
+          | Network_Recv, Network_Recv => id
+          | Network_Send m1, Network_Send m2 =>
+              if m1 = m2 ? then id else fun _ _ => throw Error_UnexpectedMessage
+          | _, _ => fun _ _ => throw Error_UnexpectedBehavior
+          end k x0
+      | (e|) | (||e|) | (|||e|) | (||||e) => vis e (match_event e0 x0 \226\136\152 k)
+      end
+  end.
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-22 03:23:36.870000.*)
+(* Auto-generated comment: At 2019-08-22 03:23:36.960000.*)
 
