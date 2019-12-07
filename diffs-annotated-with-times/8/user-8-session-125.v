f913c9b736079e32a6f8450585459cfb95c00266
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:14 -0800

    user 8, session 125, part 58

--- diffs-annotated-with-times/8/user-8-session-125.v
+++ diffs-annotated-with-times/8/user-8-session-125.v
@@ -36,12 +36,40 @@ Proof.
 Timeout 1 About unitary_gate_unitary.
 Timeout 1 Print unitary_gate_unitary.
 specialize (unitary_gate_unitary U) as inv.
+(unfold WF_Unitary in inv).
 (simpl in *).
 matrix_denote.
 setoid_rewrite denote_unitary_transpose.
 (simpl in *; Msimpl).
 (repeat rewrite Mmult_assoc; try rewrite inv).
+(repeat rewrite <- Mmult_assoc; try rewrite inv).
+Msimpl.
+reflexivity.
+Qed.
+Redirect "/var/folders/m1/0k3qczq13cg04mhs4ww613ww0000gn/T/coqllH2jR"
+Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Lemma unitary_transpose_id : forall W (U : Unitary W), unitary_transpose U \226\137\161 id_circ.
+Proof.
+(intros W U \207\129 safe).
+matrix_denote.
+(rewrite add_fresh_split).
+(rewrite subst_pat_fresh by constructor).
+(unfold denote_db_box).
+(simpl).
+(unfold compose_super, super, pad).
+(repeat rewrite Nat.add_sub).
+(rewrite Nat.sub_diag).
+Msimpl.
+(destruct W; try (solve [ inversion U ])).
+-
+(simpl).
+(unfold denote_pat; simpl).
+(unfold swap_list; simpl).
+(unfold swap_two; simpl).
+Msimpl.
+(rewrite Mmult_assoc).
 (* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-15 14:47:03.240000.*)
+(* Auto-generated comment: At 2019-08-15 14:50:51.220000.*)
 
