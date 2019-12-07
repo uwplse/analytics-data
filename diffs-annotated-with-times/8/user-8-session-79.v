734b6ba24aa67067b0b490014e316c02af4c7b9f
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:00 -0800

    user 8, session 79, part 527

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -986,8 +986,23 @@ Proof.
 (induction b; intros \206\147 f t H).
 -
 (simpl in *).
-rewrite_inPar'.
-(* Auto-generated comment: Failed. *)
+Timeout 1 About rewrite_inPar.
+Timeout 1 Print rewrite_inPar.
+Timeout 1 Print Ltac rewrite_inPar.
+(match goal with
+ | |-
+   context [ (@denote_box true ?W ?W' (@inPar ?W1 ?W1' ?W2 ?W2' ?f ?g))
+               (@kron ?m ?n ?o ?p ?\207\1291 ?\207\1292) ] =>
+       let IP := fresh "IP" in
+       specialize
+       (inPar_correct W1 W1' W2 W2' f g true \207\1291 \207\1292)
+       as
+       IP
+ end; try (solve [ type_check ])).
+show_dimensions.
+(simpl in *; rewrite size_ntensor in *; try rewrite Nat.mul_1_r in *).
+(rewrite IP).
+(* Auto-generated comment: Succeeded. *)
 
-(* Auto-generated comment: At 2019-08-14 18:33:51.180000.*)
+(* Auto-generated comment: At 2019-08-14 18:35:22.410000.*)
 
