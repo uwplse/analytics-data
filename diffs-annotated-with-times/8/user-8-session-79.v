a378b20e18b4f1e9942bdc0168bae8c24d244065
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:00:54 -0800

    user 8, session 79, part 1427

--- diffs-annotated-with-times/8/user-8-session-79.v
+++ diffs-annotated-with-times/8/user-8-session-79.v
@@ -1191,6 +1191,7 @@ rewrite_inPar.
 (erewrite kron_compat).
 3:
  (simpl; rewrite size_ntensor, Nat.mul_1_r; unfold ctx_to_matrix in *; specialize
-   (IHb2 \206\147 f false); repeat rewrite Nat.add_0_r in *; apply IHb1; trivial).
-(* Auto-generated comment: Failed. *)
+   (IHb2 \206\147 f false); repeat rewrite Nat.add_0_r in *; apply IHb2; trivial).
+(rewrite xorb_false_l).
+(* Auto-generated comment: Succeeded. *)
 
