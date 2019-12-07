079d9d4ba2239a530f93181a9dbf554e719bf75c
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:43 -0800

    user 11, session 9, part 0

---
+++ diffs-annotated-with-times/11/user-11-session-9.v
@@ -0,0 +1,20 @@
+Redirect "/tmp/coq16819zKy" Test Search Output Name Only.
+Timeout 1 Print Grammar tactic.
+Add Search Blacklist "Private_" "_subproof".
+Set Printing Depth 50.
+Redirect "/tmp/coq16819Ldx" Print Ltac Signatures.
+Timeout 1 Print Grammar tactic.
+Timeout 1 Print LoadPath.
+Remove Search Blacklist "Private_" "_subproof".
+Add Search Blacklist "Private_" "_subproof".
+From Coq Require Import NArith.
+Definition secret_key := N.
+Definition public_key := N.
+Definition shared_key := N.
+Definition base := 5.
+Definition prime := 23.
+Definition derive_public (k : secret_key) : public_key := base ^ k mod prime.
+(* Auto-generated comment: Succeeded. *)
+
+(* Auto-generated comment: At 2019-09-09 21:12:37.490000.*)
+
