5f38ff74574ad02f0dbc0a5c8165031f000ac7af
Talia Ringer <tringer@cs.washington.edu>
2019-12-06 16:01:46 -0800

    user 11, session 16, part 9

--- diffs-annotated-with-times/11/user-11-session-16.v
+++ diffs-annotated-with-times/11/user-11-session-16.v
@@ -37,8 +37,8 @@ Please report at http://coq.inria.fr/bugs/.
 Redirect "/tmp/coq16819lNO" Print Ltac Signatures.
 Timeout 1 Print Grammar tactic.
 Definition smE := appE exp +' evalE +' nondetE.
-Definition kvs_state exp_ := list connection * list (N * exp_ N)%type.
+Check kvs_state.
 (* Auto-generated comment: Failed. *)
 
-(* Auto-generated comment: At 2019-09-09 22:02:24.350000.*)
+(* Auto-generated comment: At 2019-09-09 22:02:30.910000.*)
 
